6fbea7720efc2b6e02e0d86da1daa00c4599c4a9
Matthew Turk <matthewturk@gmail.com>
2007-02-15 13:20:03 -0800

    [svn r30] oops, now the maximum file size is 10 MB instead of 10K.  :-)
    --HG--
    branch : trunk

--- src/logger.py
+++ src/logger.py
@@ -20,24 +20,24 @@ f = logging.Formatter("%(levelname)-10s %(asctime)s %(message)s")
 # object for a given value of WHATEVER.
 #
 
-fidoHandler = handlers.RotatingFileHandler("fido.log", maxBytes=10*1024, backupCount=10)
+fidoHandler = handlers.RotatingFileHandler("fido.log", maxBytes=10*1024*1024, backupCount=10)
 fidoHandler.setFormatter(f)
 fidoLogger = logging.getLogger("yt.fido")
 fidoLogger.addHandler(fidoHandler)
 
-ravenHandler = handlers.RotatingFileHandler("raven.log", maxBytes=10*1024, backupCount=10)
+ravenHandler = handlers.RotatingFileHandler("raven.log", maxBytes=10*1024*1024, backupCount=10)
 ravenHandler.setFormatter(f)
 ravenLogger = logging.getLogger("yt.raven")
 ravenLogger.addHandler(ravenHandler)
 ravenLogger.setLevel(logging.NOTSET)
 
-lagosHandler = handlers.RotatingFileHandler("lagos.log", maxBytes=10*1024, backupCount=10)
+lagosHandler = handlers.RotatingFileHandler("lagos.log", maxBytes=10*1024*1024, backupCount=10)
 lagosHandler.setFormatter(f)
 lagosLogger = logging.getLogger("yt.lagos")
 lagosLogger.addHandler(lagosHandler)
 lagosLogger.setLevel(logging.NOTSET)
 
-deliveratorHandler = handlers.RotatingFileHandler("deliverator.log", maxBytes=10*1024, backupCount=10)
+deliveratorHandler = handlers.RotatingFileHandler("deliverator.log", maxBytes=10*1024*1024, backupCount=10)
 deliveratorHandler.setFormatter(f)
 deliveratorLogger = logging.getLogger("yt.deliverator")
 deliveratorLogger.addHandler(deliveratorHandler)
