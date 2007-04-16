d805b27ff85f3f621a729a691d0674f1b4eb9556
Matthew Turk <matthewturk@gmail.com>
2007-04-16 14:35:55 -0700

    [svn r90] showing the new "add" operation
    --HG--
    branch : trunk

--- examples/test_clusterfile.py
+++ examples/test_clusterfile.py
@@ -2,5 +2,9 @@ import yt.lagos as lagos
 import yt.raven as raven
 
 myPlot=raven.EnzoHippo(None)
-cl=lagos.AnalyzeClusterOutput("DataDump0011_AnalyzeCluster")
-myPlot.addClusterOutput(cl, "DataDump0011 Stuff")
+cl1=lagos.AnalyzeClusterOutput("DataDump0011_AnalyzeCluster")
+cl2=lagos.AnalyzeClusterOutput("DataDump0011_AnalyzeCluster.Species")
+cl3=lagos.AnalyzeClusterOutput("DataDump0011_AnalyzeCluster.Disk")
+
+clc = cl1 + cl2 + cl3
+myPlot.addClusterOutput(clc, "DataDump0011 Stuff")
