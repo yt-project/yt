f59113af59dcac1bb6bfca468ce37ae527f1e186
Matthew Turk <matthewturk@gmail.com>
2007-05-22 22:37:17 -0700

    [svn r139] numpy setup.py now
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -1,29 +1,58 @@
-from distutils.core import setup, Extension
-import numpy
-import sys, time
+#from numpy.distutils.core import setup, Extension, Configuration
 
-numpyincludedirs=numpy.get_include()
+#_ec = Extension('yt.lagos.EnzoCombine',['src/lagos/EnzoCombine.c'], \
+                #include_dirs=[numpyincludedirs], \
+                #libraries=['m'])
+#DOCLINES = __doc__.split("\n")
 
-if not hasattr(sys, 'version_info') or sys.version_info < (2,2,0,'alpha',0):
-    raise SysError
+import os
+import sys
+import time
 
-_ec = Extension('yt.lagos.EnzoCombine',['src/lagos/EnzoCombine.c'], \
-                include_dirs=[numpyincludedirs], \
-                libraries=['m'])
+if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 
-setup(name = "yt",
-    version = time.strftime("%y%m%d"),
-    description = "A set of classes for manipulating Enzo data",
-    url = "http://www.stanford.edu/~mturk/raven.html",
-    author="Matthew Turk",
-    author_email="mturk@stanford.edu",
-    package_dir={"yt":"src"},
-    packages = ["yt.enki", "yt.enki.mes", "yt.fido", "yt.lagos", "yt", "yt.raven", "yt.deliverator"],
-    scripts = ["scripts/fdigup","scripts/fimport","scripts/frevert","scripts/fbranch", 
-               "scripts/ffetch", "scripts/yt", "scripts/fbury","scripts/fido"],
-    data_files = [('share/doc/yt/', ['examples/test_enki.py',
-      'examples/test_fido.py', 'examples/test_raven.py', 'examples/test_lagos.py',
-      'examples/get_particles_enzorun.py'])],
-    license="GPL-2",
-    ext_modules=[_ec]
-    )
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration(None, parent_package, top_path)
+    config.set_options(ignore_setup_xxx_py=True,
+                       assume_default_configuration=True,
+                       delegate_options_to_subpackages=True,
+                       quiet=False)
+    
+    config.make_config_py()
+    config.add_subpackage('yt','src')
+
+    #config.add_data_files(('numpy',['*.txt','COMPATIBILITY',
+                                    #'scipy_compatibility']))
+
+    #config.get_version('numpy/version.py') # sets config.version
+    
+    return config
+
+def setup_package():
+
+    from numpy.distutils.core import setup
+
+    setup(
+        name = "yt",
+        version = time.strftime("%y%m%d"),
+        description = "A set of classes for manipulating Enzo data",
+        url = "http://www.stanford.edu/~mturk/raven.html",
+        author="Matthew Turk",
+        author_email="mturk@stanford.edu",
+        license="GPL-2",
+        configuration=configuration
+        )
+    return
+
+if __name__ == '__main__':
+    setup_package()
+
+        #package_dir={"yt":"src"},
+        #packages = ["yt.enki", "yt.enki.mes", "yt.fido", "yt.lagos", "yt", "yt.raven", "yt.deliverator"],
+        #scripts = ["scripts/fdigup","scripts/fimport","scripts/frevert","scripts/fbranch", 
+                   #"scripts/ffetch", "scripts/yt", "scripts/fbury","scripts/fido"],
+        #data_files = [('share/doc/yt/', ['examples/test_enki.py',
+          #'examples/test_fido.py', 'examples/test_raven.py', 'examples/test_lagos.py',
+          #'examples/get_particles_enzorun.py'])],
---
+++ src/raven/setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('raven',parent_package,top_path)
+    config.make_config_py() # installs __config__.py
+    return config
