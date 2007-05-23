86dc9c2a3e367a18daf4265c440802d5ade57629
Matthew Turk <matthewturk@gmail.com>
2007-05-22 20:03:44 -0700

    [svn r137] Using basic NumPy Extension class...
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from distutils.core import setup, Extension
-from numarray.numarrayext import NumarrayExtension
+import numpy
 import sys, time
+
+numpyincludedirs=numpy.get_include()
 
 if not hasattr(sys, 'version_info') or sys.version_info < (2,2,0,'alpha',0):
     raise SysError
+
+_ec = Extension('yt.lagos.EnzoCombine',['src/lagos/EnzoCombine.c'], \
+                include_dirs=[numpyincludedirs], \
+                libraries=['m'])
 
 setup(name = "yt",
     version = time.strftime("%y%m%d"),
@@ -14,13 +20,10 @@ setup(name = "yt",
     package_dir={"yt":"src"},
     packages = ["yt.enki", "yt.enki.mes", "yt.fido", "yt.lagos", "yt", "yt.raven", "yt.deliverator"],
     scripts = ["scripts/fdigup","scripts/fimport","scripts/frevert","scripts/fbranch", 
-               "scripts/ffetch", "scripts/yt", "scripts/fbury"],
+               "scripts/ffetch", "scripts/yt", "scripts/fbury","scripts/fido"],
     data_files = [('share/doc/yt/', ['examples/test_enki.py',
       'examples/test_fido.py', 'examples/test_raven.py', 'examples/test_lagos.py',
       'examples/get_particles_enzorun.py'])],
     license="GPL-2",
-    ext_modules=[NumarrayExtension("yt.lagos.EnzoCombine",['src/lagos/EnzoCombine.c'],\
-        include_dirs=["./"],
-        library_dirs=["./"],
-        libraries=['m'])]
+    ext_modules=[_ec]
     )
