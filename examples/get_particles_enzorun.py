1c0ed28273073f9f97fe17fa3f38103a5d1c4779
Matthew Turk <matthewturk@gmail.com>
2007-05-23 16:28:42 -0700

    [svn r142] minor change, prolly doesn't even need to be committed
    --HG--
    branch : trunk

--- examples/get_particles_enzorun.py
+++ examples/get_particles_enzorun.py
@@ -3,12 +3,12 @@ import yt.lagos as lagos
 myRun = lagos.EnzoRun("my_awesome_run")
 
 a = []
-for i in range(1,5):
+for i in range(1,99):
     a.append("galaxy%04i.dir/galaxy%04i" % (i,i))
 
 myRun.addOutputByFilename(a)
 
 # We're putting the arguments in the order they are expected by the function
 
-myRun.runFunction(lagos.EnzoHierarchy.exportParticlesPB, [1,None,100.0], fmt_string="dm%04i.pb")
+#myRun.runFunction(lagos.EnzoHierarchy.exportParticlesPB, [1,None,100.0], fmt_string="dm%04i.pb")
 myRun.runFunction(lagos.EnzoHierarchy.exportParticlesPB, [2,["metallicity_fraction","particle mass"],100.0], fmt_string="stars%04i.pb")
