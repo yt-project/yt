2950ac28b2223a86e0b5c65da936e5b1cca9bb19
Matthew Turk <matthewturk@gmail.com>
2007-06-26 11:59:40 -0700

    [svn r174] Skeleton, taken from HD.py, now included.  not functional yet.  have to rethink some assumptions with MPL, due to it being more static than HD.
    --HG--
    branch : trunk

--- src/raven/backends/MPL.py
+++ src/raven/backends/MPL.py
@@ -187,3 +187,222 @@ def ClusterFilePlot(cls, x, y, xlog=None, ylog=None, fig=None, filename=None,
         canvas.print_figure(filename, format=format)
     else:
         return fig
+
+from collections import defaultdict
+
+engineVals = {}
+skipAxes = ["X WIDTH", "Y WIDTH", "WEIGHT (OPTIONAL)", "DX", "DY"]
+
+axisFieldDict = {'X':'Field1', 'Y':'Field2', 'Z':'Field3'}
+
+def Initialize(*args, **kwargs):
+    engineVals["initialized"] = True
+    return
+
+def CleanUp(*args, **kwargs):
+    pass
+
+class RavenPlot:
+    def __init__(self, data, fields):
+        self.data = data
+        # With matplotlib, we don't need to copy data back and forth.
+        # Unfortunately, it is also harder to plot things, as they may not have
+        # a consistent interface.  Also, the plots are designed to be much more
+        # *static* than in hippodraw, so switching axes is going to be a bit of
+        # a pain.
+        self.im = defaultdict(lambda: "")
+        self["ParameterFile"] = self.data.hierarchy.parameterFilename
+        self.axisNames = {}
+        for field, axis in zip(fields, self.axes_names):
+            if axis.upper() not in ["X","Y","Z"]:
+                continue
+            self.setupAxis(axis, field)
+
+    def __setitem__(self, item, val):
+        self.im[item] = val
+
+    def setAxisToField(self, axis, field):
+        self[field] = self.data[field]
+        self.mdisplay.getDataRep().setAxisBinding(axis, field)
+        self.setupAxis(axis, field)
+
+    def setupAxis(self, axis, field):
+        dataLabel = field
+        if lagos.fieldInfo.has_key(field):
+            dataLabel += " (%s)" % (lagos.fieldInfo[field][0])
+        self.mdisplay.setLabel(axis,dataLabel)
+        if field in lagos.log_fields:
+            self.mdisplay.setLog(axis,True)
+        else:
+            self.mdisplay.setLog(axis,lagos.fieldInfo[field][2])
+        self.axisNames[axis] = field
+        self.im[axisFieldDict[axis]] = field
+
+    def saveImage(self, prefix, format, submit=None):
+        """
+        Save this plot image.  Will generate a filename based on the prefix,
+        format, and the approriate data stored in the plot.
+
+        @param prefix: the prefix to prepend to the filename
+        @type prefix: string
+        @param format: the prefix to append to the filename
+        @type format: string
+        """
+        self.generatePrefix(prefix)
+        fn = ".".join([self.prefix, format])
+        engineVals["canvas"].saveAsImage(self.mdisplay, fn)
+        self["Type"] = self.typeName
+        self["GeneratedAt"] = self.data.hierarchy["CurrentTimeIdentifier"]
+        # Now submit
+        return fn
+
+    def switch_x(self, field):
+        self.setAxisToField('X', field)
+    
+    def switch_y(self, field):
+        self.setAxisToField('Y', field)
+    
+    def switch_z(self, field):
+        self.setAxisToField('Z', field)
+
+    def switch_weight(self, field):
+        self.setAxisToField('Weight (optional)', field)
+
+    def set_xlim(self, xmin, xmax):
+        self.mdisplay.setRange('X', xmin, xmax)
+        
+    def set_ylim(self, ymin, ymax):
+        self.mdisplay.setRange('Y', ymin, ymax)
+
+    def set_zlim(self, zmin, zmax):
+        self.mdisplay.setRange('Z', zmin, zmax)
+
+class LinePlot(RavenPlot):
+    def __init__(self, data, fields):
+        self.axes_names = ["X","Y"]
+        self.plotType = "XY Plot"
+        RavenPlot.__init__(self, data, fields)
+
+    def generatePrefix(self, prefix):
+        self.prefix = prefix + "_%s" % (self.typeName)
+
+    def switch_z(self, *args, **kwargs):
+        pass
+
+    def set_zlim(self, *args, **kwargs):
+        pass
+
+    def addField(self, field):
+        if self.hierarchy.conversionFactors.has_key(field):
+            conv = self.hierarchy.conversionFactors[field]
+        else:
+            conv = 1
+        if not self.tuple.has_key(field):
+            self.tuple[field]=self.data[field]*conv
+
+class ProfilePlot(LinePlot):
+    def __init__(self, data, fields, width=None, unit=None):
+        self.typeName = "RadialProfile"
+        self.plotType = "Profile"
+        RavenPlot.__init__(self, data, fields)
+
+    def generatePrefix(self, prefix):
+        self.prefix = prefix + "_%s" % (self.typeName)
+        self.prefix += "_%s" % (self.field)
+
+class ACProfilePlot(LinePlot):
+    pass
+
+class ScatterPlot(RavenPlot):
+    def __init__(self, data, fields, width=None, unit=None):
+        self.axes_names = ["X","Y"]
+        self.typeName = "RadialScatter"
+        self.plotType = "Scatter Plot"
+        RavenPlot.__init__(self, data, fields)
+
+class VMPlot(RavenPlot):
+    def __init__(self, data, field):
+        fields = ['X', 'Y', field, 'X width', 'Y width']
+        RavenPlot.__init__(self, data, fields)
+        self.axisNames["Z"] = field
+        self.checkColormap(field)
+        self.set_width(1,'1')
+
+    def generatePrefix(self, prefix):
+        self.prefix = "_".join([prefix, self.typeName, \
+            lagos.axis_names[self.data.axis], self.axisNames['Z']])
+        self["Field1"] = self.axisNames["Z"]
+        self["Field2"] = ""
+        self["Field3"] = ""
+
+    def set_width(self, width, unit):
+        self["Unit"] = str(unit)
+        self["Width"] = float(width)
+        if isinstance(unit, types.StringType):
+            unit = self.data.hierarchy[unit]
+        self.width = width / unit
+        self.refreshDisplayWidth()
+
+    def checkColormap(self, field):
+        cmap = lagos.colormap_dict[field]
+        self.mdisplay.setColorMap(cmap)
+
+    def refreshDisplayWidth(self, width=None):
+        if width:
+            self.width = width
+        else:
+            width = self.width
+        l_edge_x = self.data.center[lagos.x_dict[self.data.axis]] - width/2.0
+        r_edge_x = self.data.center[lagos.x_dict[self.data.axis]] + width/2.0
+        l_edge_y = self.data.center[lagos.y_dict[self.data.axis]] - width/2.0
+        r_edge_y = self.data.center[lagos.y_dict[self.data.axis]] + width/2.0
+        self.set_xlim(max(l_edge_x,0.0), min(r_edge_x,1.0))
+        self.set_ylim(max(l_edge_y,0.0), min(r_edge_y,1.0))
+
+    def switch_y(self, *args, **kwargs):
+        pass
+
+    def switch_x(self, *args, **kwargs):
+        pass
+
+    def switch_z(self, field):
+        RavenPlot.switch_z(self, field)
+        self.checkColormap(field)
+
+class SlicePlot(VMPlot):
+    def __init__(self, data, fields):
+        self.typeName = "Slice"
+        VMPlot.__init__(self, data, fields)
+        
+
+class ProjectionPlot(VMPlot):
+    def __init__(self, data, fields):
+        self.typeName = "Projection"
+        VMPlot.__init__(self, data, fields)
+
+class HistogramPlot(RavenPlot):
+    def __init__(self, data, field, width=0, unit=""):
+        self.typeName = "Histogram"
+        self.plotType = "Histogram"
+        RavenPlot.__init__(self, data, [field])
+        self["Width"] = float(width)
+        self["Unit"] = str(unit)
+        self.mdisplay.setLabel('Y',"Entries per Bin")
+
+    def generatePrefix(self, prefix):
+        self.prefix = prefix + "_%s" % (self.typeName)
+        self.prefix += "_%s" % (self.field)
+        self["Field1"] = self.axisNames["X"]
+
+    def switch_y(self, *args, **kwargs):
+        pass
+
+    def set_ylim(self, *args, **kwargs):
+        pass
+
+    def switch_z(self, *args, **kwargs):
+        pass
+
+    def set_zlim(self, *args, **kwargs):
+        pass
+
