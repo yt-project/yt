45db80842e70a53da9a1ba1500231ff96b6535c9
Matthew Turk <matthewturk@gmail.com>
2007-08-02 08:58:04 -0700

    [svn r214] Added SVN version number, if possible, instead of date.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
-#from numpy.distutils.core import setup, Extension, Configuration
-
-#_ec = Extension('yt.lagos.EnzoCombine',['src/lagos/EnzoCombine.c'], \
-                #include_dirs=[numpyincludedirs], \
-                #libraries=['m'])
-#DOCLINES = __doc__.split("\n")
-
-import os
+import os, os.path
 import sys
 import time
+import subprocess
 
 if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 
@@ -23,11 +17,6 @@ def configuration(parent_package='',top_path=None):
     config.make_config_py()
     config.add_subpackage('yt','src')
 
-    #config.add_data_files(('numpy',['*.txt','COMPATIBILITY',
-                                    #'scipy_compatibility']))
-
-    #config.get_version('numpy/version.py') # sets config.version
-    
     return config
 
 def setup_package():
@@ -36,7 +25,7 @@ def setup_package():
 
     setup(
         name = "yt",
-        version = time.strftime("%y%m%d"),
+        version = getVersion(),
         description = "A set of classes for manipulating Enzo data",
         url = "http://www.stanford.edu/~mturk/raven.html",
         author="Matthew Turk",
@@ -46,13 +35,21 @@ def setup_package():
         )
     return
 
+def getVersion():
+    # First we try to SVN it
+    repo = os.path.basename(os.path.realpath(__file__))
+    p=subprocess.Popen("svn info %s" % repo, \
+                              stdout=subprocess.PIPE, \
+                              stderr=subprocess.STDOUT, \
+                              shell=True, executable="/bin/bash")
+    p.wait()
+    version = None
+    for line in p.stdout:
+        if line.startswith("Revision:"):
+            print "VERSION",line.split(":")[-1]
+            version = "SVN-r%s" % (line.split(":")[-1].strip().rstrip())
+    if not version: version = time.strftime("%y%m%d")
+    return version
+
 if __name__ == '__main__':
     setup_package()
-
-        #package_dir={"yt":"src"},
-        #packages = ["yt.enki", "yt.enki.mes", "yt.fido", "yt.lagos", "yt", "yt.raven", "yt.deliverator"],
-        #scripts = ["scripts/fdigup","scripts/fimport","scripts/frevert","scripts/fbranch", 
-                   #"scripts/ffetch", "scripts/yt", "scripts/fbury","scripts/fido"],
-        #data_files = [('share/doc/yt/', ['examples/test_enki.py',
-          #'examples/test_fido.py', 'examples/test_raven.py', 'examples/test_lagos.py',
-          #'examples/get_particles_enzorun.py'])],
