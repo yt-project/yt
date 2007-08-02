7ef48a358a5b7517cf8ffc2ff5cd88a2a2f13b8e
Matthew Turk <matthewturk@gmail.com>
2007-08-02 13:56:39 -0700

    [svn r222] Fixed package hierarchy
    --HG--
    branch : trunk

--- src/raven/deliveration/setup.py
+++ src/raven/deliveration/setup.py
@@ -2,6 +2,6 @@
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
-    config = Configuration('deliverator',parent_package,top_path)
+    config = Configuration('deliveration',parent_package,top_path)
     config.make_config_py() # installs __config__.py
     return config
--- src/raven/setup.py
+++ src/raven/setup.py
@@ -4,6 +4,7 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('raven',parent_package,top_path)
     config.add_subpackage("backends")
+    config.add_subpackage("deliveration")
     config.make_config_py() # installs __config__.py
     #config.add_extension("AMRPixelize", "AMRPixelize.c", libraries=["m"])
     return config
