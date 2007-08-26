c7317fa4f06eefceaf7a016fbbc45680e346870a
Matthew Turk <matthewturk@gmail.com>
2007-08-25 19:36:24 -0700

    [svn r237] Added GPL-3 to repo, and included appropriate header in every py file I wrote.
    --HG--
    branch : trunk

--- src/lagos/DerivedQuantities.py
+++ src/lagos/DerivedQuantities.py
@@ -1,4 +1,24 @@
 """
+Copyright (C) 2007 Matthew Turk.  All Rights Reserved.
+
+This file is part of yt.
+
+yt is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+"""
 Quantities that can be derived from Enzo data that may also required additional
 arguments.  (Standard arguments -- such as the center of a distribution of
 points -- are excluded here, and left to the EnzoDerivedFields.)
--- src/raven/backends/MPL.py
+++ src/raven/backends/MPL.py
@@ -1,4 +1,24 @@
 """
+Copyright (C) 2007 Matthew Turk.  All Rights Reserved.
+
+This file is part of yt.
+
+yt is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+"""
 This is an interface to U{MatPlotLib <http://matplotlib.sf.net>} to plot
 irregularly shaped grids, with the presumption that at any point we could have
 data that is "hidden" in deeper levels of refinement.
