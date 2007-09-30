5896e84a725dce8b86fc72285bd7ba5806dde7d8
Matthew Turk <matthewturk@gmail.com>
2007-09-30 16:47:38 -0700

    [svn r287] Updates to the NewDerivedFields file, including a by-default dependency examiner.
    --HG--
    branch : trunk

--- src/lagos/NewDerivedFields.py
+++ src/lagos/NewDerivedFields.py
@@ -1,8 +1,13 @@
+import types
+from collections import defaultdict
+import numpy as na
+from math import pi
+
 fieldInfo = {}
 
 def AddField(name, function = None, **kwargs):
     if function == None:
-        if not kwargs.has_key(function):
+        if kwargs.has_key("function"):
             function = kwargs.pop("function")
         else:
             # This will fail if it does not exist,
@@ -12,45 +17,139 @@ def AddField(name, function = None, **kwargs):
         name, function, **kwargs
     )
 
-class FieldDetector(DefaultDict):
-    pass
+class FieldDetector(defaultdict):
+    params = defaultdict(lambda: 1)
+    def __init__(self):
+        self.requested = []
+        defaultdict.__init__(self, lambda: na.ones(10))
+    def __missing__(self, item):
+        if fieldInfo.has_key(item):
+            fieldInfo[item](self)
+            return self[item]
+        self.requested.append(item)
+        return defaultdict.__missing__(self, item)
+    def convert(self, item):
+        return 1
 
 class DerivedField:
     def __init__(self, name, function,
                  units = "", projected_units = "",
                  takeLog = True, validator = None):
+        self.name = name
         self.function = function
         self.validator = validator
+        self.takeLog = takeLog
+    def GetValues(self, data):
         pass
-    def GetValues(self, object):
-        pass
-    def CheckAvailable(self, object):
+    def CheckAvailable(self, data):
         if self.validator != None:
-            self.validator(self, object)
+            self.validator(self, data)
     def GetMyDependencies(self):
         pass
     def GetAllDependencies(self):
         pass
-    def __call__(self, object):
-        object[self.name] = self.function(self, object)
+    def __call__(self, data):
+        data[self.name] = self.function(self, data)
 
 def ValidateSpecies(species):
-    if not istype(species, types.ListType):
+    if not isinstance(species, types.ListType):
         species = [species]
-    def CheckSpecies(object):
+    def CheckSpecies(data):
         for s in species:
-            if not object.has_field(s): return False
+            if not data.has_field(s): return False
         return True
 
 def ValidatePropery(prop):
-    if not istype(prop, types.ListType):
+    if not isinstance(prop, types.ListType):
         prop = [prop]
-    def CheckProperty(object):
+    def CheckProperty(data):
         for p in prop:
-            if not hasattr(object,p): return False
+            if not hasattr(data,p): return False
         return True
 
-def _H2I_Fraction(field, object):
-    return object["H2I_Density"]/object["Density"]
-AddField("H2I_Fraction", validator=ValidateSpecies("H2I_Density"))
+speciesList = ["HI","HII","Electron",
+               "HeI","HeII","HeIII",
+               "H2I","H2II","HM",
+               "DI","DII","HDI"]
+def _SpeciesFraction(field, data):
+    sp = field.name.split("_")[0]
+    return data[sp]/data["Density"]
+for species in speciesList:
+    AddField("%s_Fraction" % species,
+             function=_SpeciesFraction,
+             validator=ValidateSpecies("%s_Density" % species))
 
+def _SoundSpeed(field, data):
+    return data.convert("x-velocity") * ( \
+           data.params["Gamma"]*data["Pressure"] / \
+           data["Density"] )**(1.0/2.0)
+AddField("SoundSpeed")
+
+def _MachNumber(field, data):
+    """M{|v|/t_sound}"""
+    return data["VelocityMagnitude"] / data["SoundSpeed"]
+AddField("MachNumber")
+
+def _VelocityMagnitude(field, data):
+    """M{|v|}"""
+    return (data.convert("x-velocity") * ( \
+            data["x-velocity"]**2.0 + \
+            data["y-velocity"]**2.0 + \
+            data["z-velocity"]**2.0 )**(1.0/2.0))
+AddField("VelocityMagnitude", takeLog=False)
+
+def _Pressure(field, data):
+    """M{(Gamma-1.0)*rho*E}"""
+    return (data.params["Gamma"] - 1.0) * \
+           data["Density"] * data["Gas_Energy"]
+AddField("Pressure", validator=ValidateSpecies("Gas_Energy"))
+
+def _Entropy(field, data):
+    return data["Density"]**(-2./3.) * \
+           data["Temperature"]
+AddField("Entropy")
+
+def _DynamicalTime(field, data):
+    """
+    The formulation for the dynamical time is:
+    M{sqrt(3pi/(16*G*rho))} or M{sqrt(3pi/(16G))*rho^-(1/2)}
+    Note that we return in our natural units already
+    """
+    G = data.params["GravitationalConstant"]
+    t_dyn_coeff = (3*pi/(16*G))**0.5 * data.convert("Time")
+    return data["Density"]**(-1./2.) * t_dyn_coeff
+AddField("DynamicalTime")
+
+def _NumberDensity(field, data):
+    # We can assume that we at least have Density
+    fieldData = na.zeros(data["Density"].shape,
+                         dtype = data["Density"].dtype)
+    if data.params["MultiSpecies"] == 0:
+        fieldData += data["Density"] * data.params["mu"]
+    if data.params["MultiSpecies"] > 0:
+        fieldData += data["HI_Density"] / 1.0
+        fieldData += data["HII_Density"] / 1.0
+        fieldData += data["HeI_Density"] / 4.0
+        fieldData += data["HeII_Density"] / 4.0
+        fieldData += data["HeIII_Density"] / 4.0
+        fieldData += data["Electron_Density"] / 1.0
+    if data.params["MultiSpecies"] > 1:
+        fieldData += data["HM_Density"] / 1.0
+        fieldData += data["H2I_Density"] / 2.0
+        fieldData += data["H2II_Density"] / 2.0
+    if data.params["MultiSpecies"] > 2:
+        fieldData += data["DI_Density"] / 2.0
+        fieldData += data["DII_Density"] / 2.0
+        fieldData += data["HDI_Density"] / 3.0
+    return fieldData * data.convert("Density") / mh
+AddField("NumberDensity")
+
+
+
+if __name__ == "__main__":
+    k = fieldInfo.keys()
+    k.sort()
+    for f in k:
+        e = FieldDetector()
+        fieldInfo[f](e)
+        print f + ":", ", ".join(e.requested)
