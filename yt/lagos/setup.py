073273e80f1968f02bc0d1385a0221ea43ce4003
Matthew Turk <matthewturk@gmail.com>
2007-12-08 07:37:28 +0000

    Fixes to the locations of source files in the extensions
    --HG--
    branch : unstable-0.3

--- yt/lagos/setup.py
+++ yt/lagos/setup.py
@@ -5,9 +5,9 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('lagos',parent_package,top_path)
     config.make_config_py() # installs __config__.py
-    config.add_extension("PointCombine", "src/lagos/PointCombine.c", libraries=["m"])
+    config.add_extension("PointCombine", "yt/lagos/PointCombine.c", libraries=["m"])
     sys.argv.extend(["config_fc","--f77flags","'-Dr16 -ffixed-line-length-none -fno-second-underscore -DPYFORT -DNOMETALS -ggdb'"])
     if os.environ.has_key("YT_FORT"):
         config.add_extension("EnzoFortranRoutines", \
-                            ["src/lagos/solve_rate_cool.pyf", "src/lagos/f_src/*.F"])
+                            ["yt/lagos/solve_rate_cool.pyf", "yt/lagos/f_src/*.F"])
     return config
--- yt/raven/backends/setup.py
+++ yt/raven/backends/setup.py
@@ -4,5 +4,5 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('backends',parent_package,top_path)
     config.make_config_py() # installs __config__.py
-    config.add_extension("_MPL", "src/raven/backends/_MPL.c", libraries=["m"])
+    config.add_extension("_MPL", "yt/raven/backends/_MPL.c", libraries=["m"])
     return config
