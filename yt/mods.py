15b4a2c06b2a994d2bea70c7d870bac7ba381a2b
Matthew Turk <matthewturk@gmail.com>
2008-06-19 22:26:35 -0700

    [svn r579] Added an extremely simply convenience module.
    from yt.mods import *
    
    now gives you all three modules you could conceivably want to use on the
    command line.  This is also a quicker way to set up scripts, as you can do:
    
    {{{
    #!python
    
    from yt.mods import *
    
    pf = get_pf()
    }}}
    
    and then you can do stuff!
    
    --HG--
    branch : trunk

---
+++ yt/mods.py
@@ -0,0 +1,35 @@
+"""
+Very simple convenience function for importing all the modules, setting up
+the namespace and getting the last argument on the command line.
+
+@author: U{Matthew Turk<http://www.stanford.edu/~mturk/>}
+@organization: U{KIPAC<http://www-group.slac.stanford.edu/KIPAC/>}
+@contact: U{mturk@slac.stanford.edu<mailto:mturk@slac.stanford.edu>}
+@license:
+  Copyright (C) 2008 Matthew Turk.  All Rights Reserved.
+
+  This file is part of yt.
+
+  yt is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+import yt.lagos as lagos
+import yt.raven as raven
+import yt.fido as fido
+import numpy as na
+import sys
+
+def get_pf():
+    return lagos.EnzoStaticOutput(sys.argv[-1])
