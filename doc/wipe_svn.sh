c22657c1b8cb43e0ded9fab2e5880bbb4538986c
Matthew Turk <matthewturk@gmail.com>
2008-07-31 14:58:16 -0700

    [svn r715] Okay, we're doing away with the problem of the large diffs.  I'm simply deleting the build directory, and we're going to go to a zip-file base distribution method.
    This should save LOTS of space, time, etc etc.  Plus, smaller diffs.
    
    --HG--
    branch : yt-1.0

--- doc/wipe_svn.sh
+++ doc/wipe_svn.sh
@@ -1,23 +1,20 @@
-for ext in txt html css png js doctree pickle json
-do
-    find build/ -name "*.${ext}" -exec rm -v {} \;
-done
-
-PYTHONPATH=`pwd`/../
+export PYTHONPATH=`pwd`/../
 make html
 
 echo "Files deleted since last svn commit:"
 echo " (generated with 'svn stat | grep \! ')"
 echo 
-svn stat | grep \!
+svn stat | grep \! |grep -v build
 echo
 
 echo "Files added since last svn commit:"
 echo " (generated with 'svn stat | grep \? ')"
 echo
-svn stat | grep \?
+svn stat | grep \? | grep -v build/html
 echo
 
 echo "Examples files status:"
 echo
 svn stat ../examples/
+
+zip -r docs_html.zip build/html/
