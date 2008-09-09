38f853065a85b0833096ed0c96baa71a3ba43a8f
Matthew Turk <matthewturk@gmail.com>
2008-09-09 11:15:53 -0700

    [svn r764] Translation now seems to work.
    --HG--
    branch : yt-generalization

--- yt/lagos/OrionFields.py
+++ yt/lagos/OrionFields.py
@@ -19,14 +19,12 @@ translation_dict = {"x-velocity": "xvel",
                     "z-momentum": "zmom"
                    }
 
-add_field("Density", function=lambda a,b: b["density"])
+def _generate_translation(mine, theirs):
+    add_field(theirs, function=lambda a, b: b[mine], take_log=True)
 
-#for f,v in translation_dict.items():
-#    if v not in OrionFieldInfo:
-#        add_field(v, function=lambda a,b: None, take_log=True,
-#                  validators = [ValidateDataField(v)])
-#    def func(a, b):
-#        print "Returning %s (%s)" % (v, f)
-#        return b[v]
-#    print "Setting up translator from %s to %s" % (v, f)
-#    add_field(f, function=func, take_log=True)
+for f,v in translation_dict.items():
+    if v not in OrionFieldInfo:
+        add_field(v, function=lambda a,b: None, take_log=True,
+                  validators = [ValidateDataField(v)])
+    #print "Setting up translator from %s to %s" % (v, f)
+    _generate_translation(v, f)
