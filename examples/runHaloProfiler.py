04c36c42a83ef4c7c6ef6796971d36ad4888e9e2
Britton Smith <brittonsmith@gmail.com>
2008-11-05 11:20:52 -0800

    [svn r883] Fixed HaloProfiler import.
    5 November, 2008
    
    Britton Smith
    
    --HG--
    branch : trunk

--- examples/runHaloProfiler.py
+++ examples/runHaloProfiler.py
@@ -1,5 +1,4 @@
-from yt.mods import *
-from HaloProfiler import *
+from yt.extensions import HaloProfiler
 
 # Instantiate HaloProfiler for this dataset and load parameters from sample_halo_profiles.par.
 q = HaloProfiler("/Users/britton/EnzoRuns/cc_run/DD0232/DD0232","sample_halo_profiles.par")
