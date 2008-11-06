239399b9122fdcc4fbd32927787ca9ade0298f4e
Britton Smith <brittonsmith@gmail.com>
2008-11-05 18:40:50 -0800

    [svn r896] This should actually work now.
    --HG--
    branch : trunk

--- examples/runHaloProfiler.py
+++ examples/runHaloProfiler.py
@@ -1,7 +1,7 @@
-from yt.extensions import HaloProfiler
+import yt.extensions.HaloProfiler as HP
 
 # Instantiate HaloProfiler for this dataset and load parameters from sample_halo_profiles.par.
-q = HaloProfiler("/Users/britton/EnzoRuns/cc_run/DD0232/DD0232","sample_halo_profiles.par")
+q = HP.HaloProfiler("/Users/britton/EnzoRuns/cc_run/DD0232/DD0232","sample_halo_profiler.par")
 
 # Make profiles of all halos in dataset.
 q.makeProfiles()
