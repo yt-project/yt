1aa4f7482c688c121059ec7f0c6b5b335fc7a892
Matthew Turk <matthewturk@gmail.com>
2008-11-05 17:02:28 -0800

    [svn r889] Fixed some setup.py issues
    --HG--
    branch : trunk

---
+++ yt/extensions/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import setuptools
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('extensions',parent_package,top_path)
+    config.make_config_py() # installs __config__.py
+    config.add_subpackage("lightcone")
+    return config
