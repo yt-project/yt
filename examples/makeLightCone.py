49007fbc87e2274a833980b84af71419b9248cef
Britton Smith <brittonsmith@gmail.com>
2008-11-10 11:06:08 -0800

    [svn r910] Added examples of how to create a recycled light cone solution.
    --HG--
    branch : trunk

--- examples/makeLightCone.py
+++ examples/makeLightCone.py
@@ -16,8 +16,27 @@ q.SaveLightConeSolution()
 # customized and remade.
 pc = q.ProjectLightCone('Density')
 
+# Save the light cone stack to an hdf5 file.
+q.SaveLightConeStack()
+
 # Make a weighted light cone projection.
 pc = q.ProjectLightCone('Temperature',weight_field='Density')
 
-# Save the light cone stack to an hdf5 file.
-q.SaveLightConeStack()
+# Save the temperature stack to a different file.
+q.SaveLightConeStack(file='light_cone_temperature.h5')
+
+# Recycle current light cone solution by creating a new solution 
+# that only randomizes the lateral shifts.
+# This will allow the projection objects that have already been made 
+# to be re-used.
+# Just don't use the same random seed as the original.
+q.RecycleLightConeSolution(987654321)
+
+# Save the recycled solution.
+q.SaveLightConeSolution(file='light_cone_recycled.out')
+
+# Make new projection with the recycled solution.
+pc = q.ProjectLightCone('Density')
+
+# Save the stack.
+q.SaveLightConeStack(file='light_cone_recycled.h5')
