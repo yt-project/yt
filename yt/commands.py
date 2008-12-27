9e1dcbbc8a05044b660da2596f849cb72d87304e
Matthew Turk <matthewturk@gmail.com>
2008-12-27 08:33:44 -0800

    [svn r1050] Parse out the "--parallel" if running in parallel.
    --HG--
    branch : trunk

--- yt/commands.py
+++ yt/commands.py
@@ -274,6 +274,8 @@ class YTCommands(cmdln.Cmdln):
         pc.save(os.path.join(opts.output,"%s" % (pf)))
 
 def run_main():
+    if "--parallel" in sys.argv:
+        del sys.argv[sys.argv.index("--parallel")]
     YT = YTCommands()
     sys.exit(YT.main())
 
