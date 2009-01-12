7f78ba44a757f66334c4de4f6e4080decf291af4
Stephen Skory <stephenskory@yahoo.com>
2009-01-11 18:33:43 -0800

    [svn r1101] a HopParticleCallback which prints the actual particles of each halo (similar to ParticleCallback), and a few new useful HopGroup functions in SS_Hop.
    --HG--
    branch : trunk

--- yt/lagos/hop/SS_HopOutput.py
+++ yt/lagos/hop/SS_HopOutput.py
@@ -244,6 +244,12 @@ class HopGroup(object):
     def get_size(self):
         return self.indices.size
 
+    def get_indices(self):
+        return self.indices
+    
+    def get_positions(self):
+        return [self["particle_position_x"],self["particle_position_y"],self["particle_position_z"]]
+
 class HaloFinder(HopList, ParallelAnalysisInterface):
     def __init__(self, pf, threshold=160.0, dm_only=True, padding=0.2):
         self.pf = pf
