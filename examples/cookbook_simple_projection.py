f9d66f173b35bbd78a76ccbf8b80f6fec16c61f9
David Collins <dcollins4096@gmail.com>
2009-02-24 14:21:39 -0800

    [svn r1186] altered structure of script so it looks better on the wiki.
    --HG--
    branch : trunk

--- examples/cookbook_simple_projection.py
+++ examples/cookbook_simple_projection.py
@@ -1,6 +1,9 @@
 
 from yt.mods import *
-pf = lagos.EnzoStaticOutput("/scratch/cbabbage/enzorun/DD0001/data0001") #Substitute your parameter file location.
+
+#Substitute your parameter file location.
+file_name = "/scratch/cbabbage/enzorun/DD0001/data0001") 
+pf = lagos.EnzoStaticOutput(file_name)
 pc = raven.PlotCollection(pf)
 pc.add_slice("Density", 0)
 pc.add_slice("Density", 1)
