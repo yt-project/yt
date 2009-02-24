791695a92b87089ac634949b6b40418926041508
David Collins <dcollins4096@gmail.com>
2009-02-24 14:22:49 -0800

    [svn r1187] simple projection, typo.
    --HG--
    branch : trunk

--- examples/cookbook_simple_projection.py
+++ examples/cookbook_simple_projection.py
@@ -2,10 +2,10 @@
 from yt.mods import *
 
 #Substitute your parameter file location.
-file_name = "/scratch/cbabbage/enzorun/DD0001/data0001") 
+file_name = "/scratch/cbabbage/enzorun/DD0001/data0001"
 pf = lagos.EnzoStaticOutput(file_name)
 pc = raven.PlotCollection(pf)
 pc.add_slice("Density", 0)
 pc.add_slice("Density", 1)
 pc.add_slice("Density", 2)
-print pc.save("AwesomeSimulation")
+print pc.save("enzorun")
