4cfd0c1ee7fc6f3d3447f4ea1d40bdea7dcfaf72
Matthew Turk <matthewturk@gmail.com>
2009-08-25 12:46:57 -0700

    [svn r1411] Made the script match the name
    --HG--
    branch : trunk

--- examples/cookbook_simple_projection.py
+++ examples/cookbook_simple_projection.py
@@ -3,8 +3,8 @@ from yt.mods import *
 pf = load("my_data") # Open "my_data"
 
 pc = PlotCollection(pf)
-pc.add_slice("Density", 0)
-pc.add_slice("Density", 1)
-pc.add_slice("Density", 2)
+pc.add_projection("Density", 0)
+pc.add_projection("Density", 1)
+pc.add_projection("Density", 2)
 
 print pc.save("enzorun")
