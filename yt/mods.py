24a7db7708715244a7205d2d76d2b6f084d2c06c
Matthew Turk <matthewturk@gmail.com>
2009-10-01 13:05:19 -0700

    [svn r1467] Added axis_names, x_dict and y_dict to the import in mods.py
    --HG--
    branch : trunk

--- yt/mods.py
+++ yt/mods.py
@@ -43,7 +43,8 @@ from yt.lagos import EnzoStaticOutput, \
     derived_field, \
     add_field, FieldInfo, EnzoFieldInfo, Enzo2DFieldInfo, OrionFieldInfo, \
     Clump, write_clump_hierarchy, find_clumps, write_clumps, \
-    OrionStaticOutput, HaloFinder, HOPHaloFinder, FOFHaloFinder
+    OrionStaticOutput, HaloFinder, HOPHaloFinder, FOFHaloFinder, \
+    axis_names, x_dict, y_dict
 
 # This is a temporary solution -- in the future, we will allow the user to
 # select this via ytcfg.
