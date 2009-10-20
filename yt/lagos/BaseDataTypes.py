706550ba460c4319bb429435a4e911b626c02e83
Matthew Turk <matthewturk@gmail.com>
2009-10-19 18:36:11 -0700

    [svn r1488] Removed old VTKInterface.py and removed the (experimental, never worked) references to the CUDA version of PointsInVolume.
    --HG--
    branch : trunk

--- yt/lagos/BaseDataTypes.py
+++ yt/lagos/BaseDataTypes.py
@@ -1029,12 +1029,6 @@ class AMRFixedResCuttingPlaneBase(AMR2DData):
                       na.outer(_co[1,:,:], self._y_vec)
         self._pixelmask = na.ones(self.dims*self.dims, dtype='int8')
 
-        try:
-            import IIIPointsInVolumeCUDA as pvc
-            self._pv = pvc.VolumeFinder(self._coord, self.dims)
-        except (ImportError, NoCUDAException):
-            self._pv = None
-
         if node_name is False:
             self._refresh_data()
         else:
@@ -1172,12 +1166,9 @@ class AMRFixedResCuttingPlaneBase(AMR2DData):
 
     def _get_point_indices(self, grid):
         if self._pixelmask.max() == 0: return []
-        if self._pv is not None:
-            k = self._pv(grid)
-        else:
-            k = PV.PointsInVolume(self._coord, self._pixelmask,
-                                  grid.LeftEdge, grid.RightEdge,
-                                  grid.child_mask, just_one(grid['dx']))
+        k = PV.PointsInVolume(self._coord, self._pixelmask,
+                              grid.LeftEdge, grid.RightEdge,
+                              grid.child_mask, just_one(grid['dx']))
         return k
 
     def _gen_node_name(self):
