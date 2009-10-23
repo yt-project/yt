fdc0bbe48f14e306cbd71a5d4c9552689b51a275
Matthew Turk <matthewturk@gmail.com>
2009-10-22 22:00:39 -0700

    ray caster is now extremely slow, but invokes and returns values, strides over grids all at once and also added a test function
    --HG--
    branch : yt

--- yt/extensions/volume_rendering/_cuda_caster.cu
+++ yt/extensions/volume_rendering/_cuda_caster.cu
@@ -24,7 +24,7 @@ License:
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ***********************************************************************/
 
-extern __shared__ float array[];
+//extern __shared__ float array[];
 
 #define NUM_SAMPLES 5
 #define VINDEX(A,B,C) data[((((A)+ci[0])*(ds[1]+1)+((B)+ci[1]))*(ds[2]+1)+ci[2]+(C))]
@@ -66,8 +66,8 @@ __device__ float interpolate(float *data, int ds[3], int *ci, float *dp)
     return dv;
 }
 
-__device__ void eval_transfer(float dt, float dv, float rgba[4],
-                               transfer_function tf)
+__device__ void eval_transfer(float dt, float dv, float *rgba,
+                               transfer_function &tf)
 {
     int i, bin_id;
     float temp, bv, dy, dd, ta;
@@ -86,12 +86,12 @@ __device__ void eval_transfer(float dt, float dv, float rgba[4],
         temp = bv+dd*(dy/tf.dbin);
         rgba[i] += (1.0 - rgba[3])*ta*temp*dt;
     }
-    rgba[3] += (1.0 - rgba[3])*ta*dt;
+    //rgba[3] += (1.0 - rgba[3])*ta*dt;
 }
 
 __device__ void sample_values(float v_pos[3], float v_dir[3],
                    float enter_t, float exit_t, int ci[3], float rgba[4],
-                   transfer_function tf, grid tg)
+                   transfer_function &tf, grid &tg)
 {
     float cp[3], dp[3], dt, t, dv;
     int dti, i;
@@ -107,6 +107,7 @@ __device__ void sample_values(float v_pos[3], float v_dir[3],
         dv = interpolate(tg.data, tg.dims, ci, dp);
         eval_transfer(dt, dv, rgba, tf);
     }
+    rgba[3] += dt;
 }
                    
 
@@ -114,22 +115,30 @@ __device__ void sample_values(float v_pos[3], float v_dir[3],
    We need the grid spatial information, as well as its values.  We also need
    the transfer function, which defines what our image will look like. */
 
-__global__ void ray_cast(float *grid_data,
-                         int dims[3],
-                         float left_edge[3],
-                         float dds[3],
-                         float tf_r[255],
-                         float tf_g[255],
-                         float tf_b[255],
-                         float tf_a[255],
-                         float tf_bounds[2],
-                         float v_dir[3],
+__global__ void ray_cast(int ngrids,
+                         float *grid_data,
+                         int *dims,
+                         float *left_edge,
+                         float *right_edge,
+                         float *tf_r,
+                         float *tf_g,
+                         float *tf_b,
+                         float *tf_a,
+                         float *tf_bounds,
+                         float *v_dir,
                          float *av_pos,
                          float *image_r,
                          float *image_g,
                          float *image_b,
                          float *image_a)
 {
+
+    int cur_ind[3], step[3], x, y, i, direction;
+    float intersect_t = 1.0;
+    float tmax[3];
+    float ti, temp_x, temp_y;
+
+    int offset;
 
     transfer_function tf;
     tf.vs[0] = tf_r;
@@ -141,122 +150,131 @@ __global__ void ray_cast(float *grid_data,
 
     /* Set up the grid, just for convenience */
     grid tg;
+    int grid_i;
 
-    tg.dims[0] = dims[0];
-    tg.dims[1] = dims[1];
-    tg.dims[2] = dims[2];
-
-    tg.left_edge[0] = left_edge[0];
-    tg.left_edge[1] = left_edge[1];
-    tg.left_edge[2] = left_edge[2];
-
-    tg.right_edge[0] = left_edge[0] + dims[0] * dds[0];
-    tg.right_edge[1] = left_edge[1] + dims[1] * dds[1];
-    tg.right_edge[2] = left_edge[2] + dims[2] * dds[2];
-
-    tg.dds[0] = dds[0];
-    tg.dds[1] = dds[1];
-    tg.dds[2] = dds[2];
+    int tidx = (blockDim.x * gridDim.x) * (
+                    blockDim.y * blockIdx.y + threadIdx.y)
+             + (blockDim.x * blockIdx.x + threadIdx.x);
 
     float rgba[4];
-    
-    int idx1 = blockIdx.x * blockDim.x + threadIdx.x;
-    rgba[0] = image_r[idx1];
-    rgba[1] = image_g[idx1];
-    rgba[2] = image_b[idx1];
-    rgba[3] = image_a[idx1];
+    rgba[0] = image_r[tidx];
+    rgba[1] = image_g[tidx];
+    rgba[2] = image_b[tidx];
+    rgba[3] = image_a[tidx];
 
     float v_pos[3];
-    v_pos[0] = av_pos[idx1 + 0];
-    v_pos[1] = av_pos[idx1 + 1];
-    v_pos[2] = av_pos[idx1 + 2];
+    v_pos[0] = av_pos[tidx + 0];
+    v_pos[1] = av_pos[tidx + 1];
+    v_pos[2] = av_pos[tidx + 2];
 
-    /* We integrate our ray */
+    tg.data = grid_data;
 
-    int cur_ind[3], step[3], x, y, i, direction;
-    float intersect_t = 1.0;
-    float intersect[3], tmax[3], tdelta[3];
-    float enter_t, tr, tl, temp_x, temp_y;
+    for(grid_i = 0; grid_i < 500; grid_i++) {
 
-    int offset;
-    int i0 = 0;
+        tg.dims[0] = dims[grid_i + 0];
+        tg.dims[1] = dims[grid_i + 1];
+        tg.dims[2] = dims[grid_i + 2];
 
-    for (i = 0; i < 3; i++)
-    {
-        step[i] = ((v_dir[i] < 0) ? -1 : 1);
-        x = (i + 1) % 3;
-        y = (i + 2) % 3;
-        tl = (tg.left_edge[i] - v_pos[i])/v_dir[i];
-        tr = (tg.right_edge[i] - v_pos[i])/v_dir[i];
-        temp_x = (v_pos[i] + tl*v_dir[x]);
-        temp_y = (v_pos[i] + tl*v_dir[y]);
+        tg.left_edge[0] = left_edge[grid_i + 0];
+        tg.left_edge[1] = left_edge[grid_i + 1];
+        tg.left_edge[2] = left_edge[grid_i + 2];
 
-        if( (tg.left_edge[x] <= temp_x) &&
-            (temp_x <= tg.right_edge[x]) &&
-            (tg.left_edge[y] <= temp_y) &&
-            (temp_y <= tg.right_edge[y]) &&
-            (0.0 <= tl) && (tl < intersect_t) ) intersect_t = tl;
+        tg.right_edge[0] = right_edge[grid_i + 0];
+        tg.right_edge[1] = right_edge[grid_i + 1];
+        tg.right_edge[2] = right_edge[grid_i + 2];
 
-        temp_x = (v_pos[x] + tr*v_dir[x]);
-        temp_y = (v_pos[y] + tr*v_dir[y]);
+        tg.dds[0] = (tg.right_edge[0] - tg.left_edge[0])/tg.dims[0];
+        tg.dds[1] = (tg.right_edge[1] - tg.left_edge[1])/tg.dims[1];
+        tg.dds[2] = (tg.right_edge[2] - tg.left_edge[2])/tg.dims[2];
 
-        if( (tg.left_edge[x] <= temp_x) &&
-            (temp_x <= tg.right_edge[x]) &&
-            (tg.left_edge[y] <= temp_y) &&
-            (temp_y <= tg.right_edge[y]) &&
-            (0.0 <= tr) && (tr < intersect_t) ) intersect_t = tr;
+        /* We integrate our ray */
 
-    }
+        for (i = 0; i < 3; i++)
+        {
+            step[i] = ((v_dir[i] < 0) ? -1 : 1);
+            x = (i + 1) % 3;
+            y = (i + 2) % 3;
 
-    for (i = 0; i < 3; i++)
-    {
-        if ( (tg.left_edge[i] <= v_pos[i]) &&
-             (v_pos[i] <= tg.right_edge[i])) i0++;
-    }
-    if (i0 == 3) intersect_t = 0.0;
+            ti = (tg.left_edge[i] - v_pos[i])/v_dir[i];
+            temp_x = (v_pos[i] + ti*v_dir[x]);
+            temp_y = (v_pos[i] + ti*v_dir[y]);
 
-    if((intersect_t < 0) || (intersect_t > 1.0)) return;
+            if( (tg.left_edge[x] <= temp_x) &&
+                (temp_x <= tg.right_edge[x]) &&
+                (tg.left_edge[y] <= temp_y) &&
+                (temp_y <= tg.right_edge[y]) &&
+                (0.0 <= ti) && (ti < intersect_t) ) intersect_t = ti;
 
-    for (i = 0; i < 3;  i++)
-    {
-        intersect[i] = v_pos[i] + intersect_t * v_dir[i];
-        cur_ind[i] = (int) floor((intersect[i] +
-                                  step[i]*1e-7*tg.dds[i] -
-                                  tg.left_edge[i])/tg.dds[i]);
-        tmax[i] = (((cur_ind[i]+step[i])*tg.dds[i])+
-                     tg.left_edge[i]-v_pos[i])/v_dir[i];
-        if((cur_ind[i] == tg.dims[i]) && (step[i] < 0)) cur_ind[i] -= 1;
-        if((cur_ind[i] < 0) || (cur_ind[i] >= tg.dims[i])) return;
-        if(step[i] > 0) offset = 1;
-        if(step[i] < 0) offset = 0;
-        tmax[i] = (((cur_ind[i]+offset)*tg.dds[i])+tg.left_edge[i]-v_pos[i])/v_dir[i];
-        tdelta[i] = abs(tg.dds[i]/v_dir[i]);
-    }
-    enter_t = intersect_t;
+            ti = (tg.right_edge[i] - v_pos[i])/v_dir[i];
+            temp_x = (v_pos[x] + ti*v_dir[x]);
+            temp_y = (v_pos[y] + ti*v_dir[y]);
 
-    /* This is the primary grid walking loop */
-    while (1) {
-        if(((cur_ind[0] < 0) || (cur_ind[0] >= tg.dims[0]))
-          ||(cur_ind[1] < 0) || (cur_ind[1] >= tg.dims[1])
-          ||(cur_ind[2] < 0) || (cur_ind[2] >= tg.dims[2])) break;
-        if (tmax[0] < tmax[1]) {
-            if (tmax[0] < tmax[2]) {
-                direction = 0;
-            } else {
-                direction = 2;
-            }
-        } else {
-            if (tmax[1] < tmax[2]) {
-                direction = 1;
-            } else {
-                direction = 2;
-            }
+            if( (tg.left_edge[x] <= temp_x) &&
+                (temp_x <= tg.right_edge[x]) &&
+                (tg.left_edge[y] <= temp_y) &&
+                (temp_y <= tg.right_edge[y]) &&
+                (0.0 <= ti) && (ti < intersect_t) ) intersect_t = ti;
+
         }
-        sample_values(v_pos, v_dir, enter_t, tmax[direction],
-                      cur_ind, rgba, tf, tg);
-        cur_ind[direction] += step[direction];
-        enter_t = tmax[direction];
-        tmax[direction] += tdelta[direction];
+
+        if ( (tg.left_edge[0] <= v_pos[0]) &&
+             (v_pos[0] <= tg.right_edge[0]) &&
+             (tg.left_edge[0] <= v_pos[0]) &&
+             (v_pos[0] <= tg.right_edge[0]) &&
+             (tg.left_edge[0] <= v_pos[0]) &&
+             (v_pos[0] <= tg.right_edge[0]))
+                intersect_t = 0.0;
+
+        int skip = 0;
+        if((intersect_t < 0) || (intersect_t > 1.0)) skip=1;
+
+        for (i = 0; i < 3;  i++)
+        {
+            cur_ind[i] = (int) floor(((v_pos[i] + intersect_t * v_dir[i]) +
+                        step[i]*1e-7*tg.dds[i] -
+                        tg.left_edge[i])/tg.dds[i]);
+            tmax[i] = (((cur_ind[i]+step[i])*tg.dds[i])+
+                    tg.left_edge[i]-v_pos[i])/v_dir[i];
+            if((cur_ind[i] == tg.dims[i]) && (step[i] < 0)) cur_ind[i] -= 1;
+            if((cur_ind[i] < 0) || (cur_ind[i] >= tg.dims[i])) skip=1;
+            if(step[i] > 0) offset = 1;
+            if(step[i] < 0) offset = 0;
+            tmax[i] = (((cur_ind[i]+offset)*tg.dds[i])+tg.left_edge[i]-v_pos[i])/v_dir[i];
+        }
+
+        /* This is the primary grid walking loop */
+        while(!(skip) &&
+              !( (cur_ind[0] < 0) || (cur_ind[0] >= tg.dims[0])
+              || (cur_ind[1] < 0) || (cur_ind[1] >= tg.dims[1])
+              || (cur_ind[2] < 0) || (cur_ind[2] >= tg.dims[2])))
+        {
+            if (tmax[0] < tmax[1]) {
+                if (tmax[0] < tmax[2]) {
+                    direction = 0;
+                } else {
+                    direction = 2;
+                }
+            } else {
+                if (tmax[1] < tmax[2]) {
+                    direction = 1;
+                } else {
+                    direction = 2;
+                }
+            }
+            sample_values(v_pos, v_dir, intersect_t, tmax[direction],
+                    cur_ind, rgba, tf, tg);
+            cur_ind[direction] += step[direction];
+            intersect_t = tmax[direction];
+            tmax[direction] += abs(tg.dds[direction]/v_dir[direction]);
+        }
+
+        tg.data += (tg.dims[0] * tg.dims[1] * tg.dims[2]);
+
     }
 
+    __syncthreads();
+    image_r[tidx] = rgba[0];
+    image_g[tidx] = rgba[1];
+    image_b[tidx] = rgba[2];
+    image_a[tidx] = rgba[3];
 }
