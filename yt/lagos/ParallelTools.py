ea901a0e23eba8a2e86cb70477d31615974c7c77
Matthew Turk <matthewturk@gmail.com>
2009-11-02 18:59:50 -0800

    Changed the mechanism for multi-field projection joins, so it now does them correctly, but only one at a time.  The unrolling was causing issues.
    --HG--
    branch : hierarchy-opt

--- yt/lagos/ParallelTools.py
+++ yt/lagos/ParallelTools.py
@@ -782,7 +782,7 @@ class ParallelAnalysisInterface(object):
     def _mpi_catdict(self, data):
         field_keys = data.keys()
         field_keys.sort()
-        size = data[field_keys[0]].size
+        size = data[field_keys[0]].shape[-1]
         # MPI_Scan is an inclusive scan
         sizes = MPI.COMM_WORLD.alltoall( [size]*MPI.COMM_WORLD.size )
         offsets = na.add.accumulate([0] + sizes)[:-1]
@@ -791,8 +791,6 @@ class ParallelAnalysisInterface(object):
             self._barrier()
             dd = data[key]
             rv = _alltoallv_array(dd, arr_size, offsets, sizes)
-            if len(dd.shape) > 1:
-                rv = rv.reshape( (dd.shape[0], rv.size / dd.shape[0]) )
             data[key] = rv
         print MPI.COMM_WORLD.rank, "Leaving"
         return data
@@ -1358,6 +1356,12 @@ def _bcast_array(arr, root = 0):
     return arr
 
 def _alltoallv_array(send, total_size, offsets, sizes):
+    if len(send.shape) > 1:
+        recv = []
+        for i in range(send.shape[0]):
+            recv.append(_alltoallv_array(send[i,:].copy(), total_size, offsets, sizes))
+        recv = na.array(recv)
+        return recv
     offset = offsets[MPI.COMM_WORLD.rank]
     tmp_send = send.view(__tocast)
     recv = na.empty(total_size, dtype=send.dtype)
