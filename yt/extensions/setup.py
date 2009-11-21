bface6759661c46cc64140d3e1d22fdab9f9eabd
Matthew Turk <matthewturk@gmail.com>
2009-11-20 19:56:04 -0800

    Removing spatial extension (for now, maybe forever...)
    --HG--
    branch : yt

--- yt/extensions/setup.py
+++ yt/extensions/setup.py
@@ -7,6 +7,5 @@ def configuration(parent_package='',top_path=None):
     config.make_config_py() # installs __config__.py
     config.make_svn_version_py()
     config.add_subpackage("lightcone")
-    config.add_subpackage("spatial")
     config.add_subpackage("volume_rendering")
     return config
