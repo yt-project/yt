122ac472ad02f6ce66313a3276a7dbc54b86f5e7
Matthew Turk <matthewturk@gmail.com>
2009-12-15 11:57:51 -0800

    Minor additions to the transfer function editor; next step is an image displaying net color at all places and Real code generation.
    --HG--
    branch : yt

--- yt/extensions/volume_rendering/transfer_function_widget.py
+++ yt/extensions/volume_rendering/transfer_function_widget.py
@@ -27,10 +27,10 @@ from TransferFunction import ColorTransferFunction
 
 from enthought.traits.api import \
         HasTraits, Float, List, Instance, Button, Array, CArray, Range, \
-        DelegatesTo, Property, Any
+        DelegatesTo, Property, Any, Code
 from enthought.traits.ui.api import \
     View, Item, HSplit, VSplit, ListEditor, InstanceEditor, ValueEditor, \
-    HGroup, VGroup
+    HGroup, VGroup, CodeEditor, TextEditor
 from enthought.chaco.api import Plot, ArrayPlotData
 from enthought.enable.component_editor import ComponentEditor
 
@@ -44,10 +44,10 @@ class TFGaussian(HasTraits):
     
     width = Float(0.0)
 
-    red = Range(0.0, 1.0)
-    green = Range(0.0, 1.0)
-    blue = Range(0.0, 1.0)
-    alpha = Range(0.0, 1.0)
+    red = Range(0.0, 1.0, 0.2)
+    green = Range(0.0, 1.0, 0.2)
+    blue = Range(0.0, 1.0, 0.2)
+    alpha = Range(0.0, 1.0, 0.2)
 
     traits_view = View(VGroup(
                          HGroup(
@@ -66,34 +66,36 @@ class TFGaussian(HasTraits):
     def _center_default(self):
         return (self.left_edge + self.right_edge)/2.0
 
+    def _width_default(self):
+        return (self.right_edge - self.left_edge)/20.0
+
     def _red_changed(self):
-        self.tf._redraw_fired()
+        self.tf._redraw()
 
     def _green_changed(self):
-        self.tf._redraw_fired()
+        self.tf._redraw()
 
     def _blue_changed(self):
-        self.tf._redraw_fired()
+        self.tf._redraw()
 
     def _alpha_changed(self):
-        self.tf._redraw_fired()
+        self.tf._redraw()
 
     def _center_changed(self):
-        self.tf._redraw_fired()
+        self.tf._redraw()
 
     def _height_changed(self):
-        self.tf._redraw_fired()
+        self.tf._redraw()
 
 class TFColors(HasTraits):
     gaussians = List(Instance(TFGaussian))
     transfer_function = Instance(ColorTransferFunction)
-    limits = CArray(dtype='float', shape=(2,), data=(0.0, 1.0))
 
-    left_edge = Property
-    right_edge = Property
+    left_edge = Float
+    right_edge = Float
 
     add_gaussian = Button
-    redraw = Button
+    generate_code = Button
 
     plot_data = Instance(ArrayPlotData)
     plot = Instance(Plot)
@@ -105,9 +107,9 @@ class TFColors(HasTraits):
                               editor=ListEditor(style='custom'),
                               show_label=False,
                              ),
-                         Item("limits"),
+                         HGroup(Item("left_edge"), Item("right_edge")),
                          HGroup(Item("add_gaussian", show_label = False),
-                                Item("redraw", show_label = False),
+                                Item("generate_code", show_label = False),
                                 ),
                         ),
                        width=500, height=500,
@@ -117,7 +119,9 @@ class TFColors(HasTraits):
         return ArrayPlotData(rx = (0.0, 1.0), ry = (0.0, 0.0),
                              gx = (0.0, 1.0), gy = (0.0, 0.0),
                              bx = (0.0, 1.0), by = (0.0, 0.0),
-                             ax = (0.0, 1.0), ay = (0.0, 0.0))
+                             ax = (0.0, 1.0), ay = (0.0, 0.0),
+                             lx = (0.0, 1.0), ly = (0.0, 0.0),
+                             ux = (0.0, 1.0), uy = (1.0, 1.0))
 
     def _plot_default(self):
         p = Plot(self.plot_data)
@@ -125,13 +129,16 @@ class TFColors(HasTraits):
         p.plot( ("gx", "gy"), type='line', color='green')
         p.plot( ("bx", "by"), type='line', color='blue')
         p.plot( ("ax", "ay"), type='line', color='black')
+        p.plot( ("lx", "ly"), type='line', color='black')
+        p.plot( ("ux", "uy"), type='line', color='black')
         return p
 
     def _add_gaussian_fired(self):
         self.gaussians.append(TFGaussian(tf = self))
 
-    def _redraw_fired(self):
-        self.transfer_function = ColorTransferFunction(self.limits)
+    def _redraw(self):
+        self.transfer_function = ColorTransferFunction(
+                                  (self.left_edge, self.right_edge))
         for g in self.gaussians:
             self.transfer_function.add_gaussian(g.center, g.width,
                                              (g.red, g.green, g.blue, g.alpha))
@@ -139,11 +146,24 @@ class TFColors(HasTraits):
             self.plot_data["%sx" % c] = f.x
             self.plot_data["%sy" % c] = f.y
 
-    def _get_left_edge(self):
-        return self.limits[0]
+    def _generate_code_fired(self):
+        val = ""
+        val += "from yt.extensions.volume_rendering import ColorTransferFunction\n"
+        val += "\n"
+        val += "tf = ColorTransferFunction((%s, %s))\n" % (self.left_edge, self.right_edge)
+        for g in self.gaussians:
+            val += "tf.add_gaussian(%s, %s, (%s, %s, %s, %s))\n" % (
+                    g.center, g.width, g.red, g.green, g.blue, g.alpha)
+        val += "\n"
+        snipped = CodeSnippet(snippet=val)
+        snipped.edit_traits()
 
-    def _get_right_edge(self):
-        return self.limits[1]
+class CodeSnippet(HasTraits):
+    snippet = Code
+
+    # Not using code editor, because it doesn't work for me with Qt4
+    traits_view = View(Item("snippet", editor=TextEditor(multi_line=True)),
+                       width=800, height=600, resizable=True)
 
 if __name__ == "__main__":
     tfc = TFColors()
