52cc5bfc5fabee0e805cb577da2ab7fa0fd8229e
Stephen Skory <stephenskory@yahoo.com>
2010-01-26 15:19:17 -0800

    Merger Tree now reads in the h5 files and finds the likely child halos.
    --HG--
    branch : yt

--- yt/extensions/MergerTree.py
+++ yt/extensions/MergerTree.py
@@ -34,6 +34,7 @@ import os, glob
 import h5py
 import types
 import sqlite3 as sql
+from collections import defaultdict
 
 
 class MergerTree(lagos.ParallelAnalysisInterface):
@@ -68,6 +69,7 @@ class MergerTree(lagos.ParallelAnalysisInterface):
                     glob.glob(os.path.join(dir, 'MergerHalos*h5')) is not []:
                 pass
             else:
+                # Run the halo finder.
                 halos = self.halo_finder_function(pf,
                     threshold=self.halo_finder_threshold, dm_only=True)
                 halos.write_out(os.path.join(dir, 'MergerHalos.out'))
@@ -84,16 +86,21 @@ class MergerTree(lagos.ParallelAnalysisInterface):
             if result != None:
                 continue
             red = pf['CosmologyCurrentRedshift']
+            # Read the halos off the disk using the Halo Profiler tools.
             hp = HP.HaloProfiler(file, halo_list_file='MergerHalos.out',
             halo_list_format={'id':0, 'mass':1, 'numpart':2, 'center':[7, 8, 9], 'velocity':[10, 11, 12], 'r_max':13})
+            mylog.info("Entering halos into database for z=%f" % red)
             for ID,halo in enumerate(hp.all_halos):
                 numpart = int(halo['numpart'])
                 values = (None, currt, red, ID, halo['mass'], numpart,
                 halo['center'][0], halo['center'][1], halo['center'][2],
                 halo['velocity'][0], halo['velocity'][1], halo['velocity'][2],
-                halo['r_max'])
-                self.cursor.execute('INSERT into HALOS VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)', values)
+                halo['r_max'],
+                -1,0.,-1,0.,-1,0.,-1,0.,-1,0.)
+                # 23 question marks.
+                self.cursor.execute('INSERT into HALOS VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', values)
             self.conn.commit()
+            del hp
     
     def _open_database(self):
         # open the database. This creates the database if it doesn't already exist.
@@ -114,39 +121,111 @@ class MergerTree(lagos.ParallelAnalysisInterface):
                 DarkMatterMass FLOAT,\
                 NumPart INTEGER, CenMassX FLOAT, CenMassY FLOAT,\
                 CenMassZ FLOAT, BulkVelX FLOAT, BulkVelY FLOAT, BulkVelZ FLOAT,\
-                MaxRad FLOAT);"
+                MaxRad FLOAT,\
+                ChildHaloID0 INTEGER, ChildHaloFrac0 FLOAT, \
+                ChildHaloID1 INTEGER, ChildHaloFrac1 FLOAT, \
+                ChildHaloID2 INTEGER, ChildHaloFrac2 FLOAT, \
+                ChildHaloID3 INTEGER, ChildHaloFrac3 FLOAT, \
+                ChildHaloID4 INTEGER, ChildHaloFrac4 FLOAT);"
             self.cursor.execute(line)
         except sql.OperationalError:
             return
     
-    def _find_likely_children(self, parents, children):
-        # For each halo in the parent list, identify likely children.
+    def _find_likely_children(self, parentfile, childfile):
+        # For each halo in the parent list, identify likely children in the 
+        # list of children.
         
-        # Build the kdtree for the children.
+        # First, read in the locations of the child halos.
+        child_pf = lagos.EnzoStaticOutput(childfile)
+        child_t = child_pf['CurrentTimeIdentifier']
+        line = "SELECT GlobalHaloID, CenMassX, CenMassY, CenMassZ FROM \
+        HALOS WHERE SnapCurrentTimeIdentifier = %d" % child_t
+        self.cursor.execute(line)
+        
+        # Build the kdtree for the children by looping over the fetched rows.
         child_points = []
-        for i,halo in enumerate(children):
+        for row in self.cursor:
             p = Point()
-            p.data = halo['center']
-            p.ID = i
+            p.data = [row[1], row[2], row[3]]
+            p.ID = row[0]
             child_points.append(p)
         child_kd = buildKdHyperRectTree(child_points[:],10)
 
         # Make these just once.
         neighbors = Neighbors()
-        neighbors.k = self.neighbors
+        neighbors.k = 5
 
-        # Find the closest child halos to this parent.
-        for i,parent in enumerate(parents):
+        # Find the parent points from the database.
+        parent_pf = lagos.EnzoStaticOutput(parentfile)
+        parent_t = parent_pf['CurrentTimeIdentifier']
+        line = "SELECT GlobalHaloID, CenMassX, CenMassY, CenMassZ FROM \
+        HALOS WHERE SnapCurrentTimeIdentifier = %d" % parent_t
+        self.cursor.execute(line)
+
+        # Loop over the returned rows, and find the likely neighbors for the
+        # parents.
+        candidates = {}
+        for row in self.cursor:
             neighbors.points = []
             neighbors.minDistanceSquared = 100. # should make this a function of the simulation
-            getKNN(parent['center'], child_kd, neighbors, 0., [1.]*3)
+            cm = [row[1], row[2], row[3]]
+            getKNN(cm, child_kd, neighbors, 0., [1.]*3)
             nIDs = []
             for n in neighbors.points:
                 nIDs.append(n[1].ID)
-            # Save this to the halo entry.
-            parents[i]['neighbors'] = nIDs
+            # Save this to the halo entry using the GlobalHaloID identifier.
+            # nIDs are also the GlobalHaloIDs.
+            candidates[row[0]] = nIDs
         
-        return parents
+        return candidates
+
+    def _build_h5_refs(self, filename):
+        # For this snapshot, add lists of file names that contain the
+        # particle info for each halo.
+        if not hasattr(self, 'h5files'):
+            self.h5files = defaultdict(dict)
+        file_pf = lagos.EnzoStaticOutput(filename)
+        currt = file_pf['CurrentTimeIdentifier']
+        dir = os.path.dirname(filename)
+        h5txt = os.path.join(dir, 'MergerHalos.txt')
+        lines = file(h5txt)
+        names = set([])
+        for i,line in enumerate(lines):
+            # Get rid of the carriage returns and turn it into a list.
+            line = line.strip().split()
+            self.h5files[currt][i] = line[1:]
+            names.update(line[1:])
+        lines.close()
+        # As long as we're here, open each of the h5 files and store the
+        # pointer in a persistent dict.
+        if not hasattr(self, 'h5fp'):
+            self.h5fp = defaultdict(dict)
+        for name in names:
+            self.h5fp[currt][name] = h5py.File(name)
+        
+    def _compute_child_fraction(self, parentfile, childfile, candidates):
+        # Given a parent and child snapshot, and a list of child candidates,
+        # compute what fraction of the parent halo goes to each of the children.
+        
+        parent_pf = lagos.EnzoStaticOutput(parentfile)
+        child_pf = lagos.EnzoStaticOutput(childfile)
+        parent_currt = parent_pf['CurrentTimeIdentifier']
+        child_currt = child_pf['CurrentTimeIdentifier']
+        
+        for halo in candidates:
+            # Find the SnapHaloID for this parent
+            line = "SELECT SnapHaloID FROM HALOS WHERE GlobalHaloID=%d LIMIT 1;" \
+                % halo
+            self.cursor.execute(line)
+            SnapHaloID = self.cursor.fetchone()[0]
+            # Read in its particle IDs
+            parent_IDs = na.array([], dtype='int64')
+            for h5name in self.h5files[parent_currt][SnapHaloID]:
+                # get the right time dict entry, and then the right h5 file
+                # from that snapshot, and then choose this parent's halo
+                # group, and then the particle IDs.
+                new_IDs = self.h5fp[parent_currt][h5name]['Halo%08d' % SnapHaloID]['particle_index']
+                parent_IDs = na.concatenate(parent_IDs, new_IDs[:])
     
     def _read_particle_ids(self, halos, txt_file):
         # Given a txt_file, read in the particles for each halo.
