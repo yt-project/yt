3b05d21a322f951a49502c9486f5c22fc3d56569
Stephen Skory <stephenskory@yahoo.com>
2010-01-27 16:45:20 -0800

    More improvements on both the machinery and graphviz output of the MergerTree.
    --HG--
    branch : yt

--- yt/extensions/MergerTree.py
+++ yt/extensions/MergerTree.py
@@ -43,6 +43,14 @@ class MergerTree(lagos.ParallelAnalysisInterface):
         self.database = database # the sqlite database of haloes.
         self.halo_finder_function = halo_finder_function # which halo finder to use
         self.halo_finder_threshold = halo_finder_threshold # overdensity threshold
+        # MPI stuff
+        self.mine = self._mpi_get_rank()
+        if self.mine is None:
+            self.mine = 0
+        self.size = self._mpi_get_size()
+        if self.size is None:
+            self.size = 1
+        # Get to work.
         self._open_database()
         self._create_halo_table()
         self._run_halo_finder_add_to_db()
@@ -52,8 +60,8 @@ class MergerTree(lagos.ParallelAnalysisInterface):
         # Loop over the pairs of snapshots to locate likely neighbors, and
         # then use those likely neighbors to compute fractional contributions.
         for pair in zip(self.restart_files[:-1], self.restart_files[1:]):
-            candidates = self._find_likely_children(pair[0], pair[1])
-            self._compute_child_fraction(pair[0], pair[1], candidates)
+            self._find_likely_children(pair[0], pair[1])
+            self._compute_child_fraction(pair[0], pair[1])
         self._close_database()
         self._close_h5fp()
         
@@ -94,51 +102,60 @@ class MergerTree(lagos.ParallelAnalysisInterface):
             hp = HP.HaloProfiler(file, halo_list_file='MergerHalos.out',
             halo_list_format={'id':0, 'mass':1, 'numpart':2, 'center':[7, 8, 9], 'velocity':[10, 11, 12], 'r_max':13})
             mylog.info("Entering halos into database for z=%f" % red)
-            for ID,halo in enumerate(hp.all_halos):
-                numpart = int(halo['numpart'])
-                values = (None, currt, red, ID, halo['mass'], numpart,
-                halo['center'][0], halo['center'][1], halo['center'][2],
-                halo['velocity'][0], halo['velocity'][1], halo['velocity'][2],
-                halo['r_max'],
-                -1,0.,-1,0.,-1,0.,-1,0.,-1,0.)
-                # 23 question marks for 23 data columns.
-                line = ''
-                for i in range(23):
-                    line += '?,'
-                # Pull off the last comma.
-                line = 'INSERT into Halos VALUES (' + line[:-1] + ')'
-                self.cursor.execute(line, values)
-            self.conn.commit()
+            if self.mine == 0:
+                for ID,halo in enumerate(hp.all_halos):
+                    numpart = int(halo['numpart'])
+                    values = (None, currt, red, ID, halo['mass'], numpart,
+                    halo['center'][0], halo['center'][1], halo['center'][2],
+                    halo['velocity'][0], halo['velocity'][1], halo['velocity'][2],
+                    halo['r_max'],
+                    -1,0.,-1,0.,-1,0.,-1,0.,-1,0.)
+                    # 23 question marks for 23 data columns.
+                    line = ''
+                    for i in range(23):
+                        line += '?,'
+                    # Pull off the last comma.
+                    line = 'INSERT into Halos VALUES (' + line[:-1] + ')'
+                    self.cursor.execute(line, values)
+                self.conn.commit()
+            self._barrier()
             del hp
     
     def _open_database(self):
-        # open the database. This creates the database if it doesn't already exist.
-        self.conn = sql.connect(self.database)
+        # open the database. This creates the database file on disk if it
+        # doesn't already exist. Open it first on root, and then on the others.
+        if self.mine == 0:
+            self.conn = sql.connect(self.database)
+        self._barrier()
+        if self.mine != 0:
+            self.conn = sql.connect(self.database)
         self.cursor = self.conn.cursor()
     
     def _close_database(self):
         # close the database cleanly.
-        self.conn.commit()
         self.cursor.close()
 
     def _create_halo_table(self):
-        # Handle the error if it already exists.
-        try:
-            # Create the table that will store the halo data.
-            line = "CREATE TABLE Halos (GlobalHaloID INTEGER PRIMARY KEY,\
-                SnapCurrentTimeIdentifier INTEGER, SnapZ FLOAT, SnapHaloID INTEGER, \
-                DarkMatterMass FLOAT,\
-                NumPart INTEGER, CenMassX FLOAT, CenMassY FLOAT,\
-                CenMassZ FLOAT, BulkVelX FLOAT, BulkVelY FLOAT, BulkVelZ FLOAT,\
-                MaxRad FLOAT,\
-                ChildHaloID0 INTEGER, ChildHaloFrac0 FLOAT, \
-                ChildHaloID1 INTEGER, ChildHaloFrac1 FLOAT, \
-                ChildHaloID2 INTEGER, ChildHaloFrac2 FLOAT, \
-                ChildHaloID3 INTEGER, ChildHaloFrac3 FLOAT, \
-                ChildHaloID4 INTEGER, ChildHaloFrac4 FLOAT);"
-            self.cursor.execute(line)
-        except sql.OperationalError:
-            return
+        if self.mine == 0:
+            # Handle the error if it already exists.
+            try:
+                # Create the table that will store the halo data.
+                line = "CREATE TABLE Halos (GlobalHaloID INTEGER PRIMARY KEY,\
+                    SnapCurrentTimeIdentifier INTEGER, SnapZ FLOAT, SnapHaloID INTEGER, \
+                    DarkMatterMass FLOAT,\
+                    NumPart INTEGER, CenMassX FLOAT, CenMassY FLOAT,\
+                    CenMassZ FLOAT, BulkVelX FLOAT, BulkVelY FLOAT, BulkVelZ FLOAT,\
+                    MaxRad FLOAT,\
+                    ChildHaloID0 INTEGER, ChildHaloFrac0 FLOAT, \
+                    ChildHaloID1 INTEGER, ChildHaloFrac1 FLOAT, \
+                    ChildHaloID2 INTEGER, ChildHaloFrac2 FLOAT, \
+                    ChildHaloID3 INTEGER, ChildHaloFrac3 FLOAT, \
+                    ChildHaloID4 INTEGER, ChildHaloFrac4 FLOAT);"
+                self.cursor.execute(line)
+                self.conn.commit()
+            except sql.OperationalError:
+                pass
+        self._barrier()
     
     def _find_likely_children(self, parentfile, childfile):
         # For each halo in the parent list, identify likely children in the 
@@ -150,6 +167,9 @@ class MergerTree(lagos.ParallelAnalysisInterface):
         line = "SELECT SnapHaloID, CenMassX, CenMassY, CenMassZ FROM \
         Halos WHERE SnapCurrentTimeIdentifier = %d" % child_t
         self.cursor.execute(line)
+        
+        mylog.info("Finding likely parents for z=%1.5f child halos." % \
+            child_pf["CosmologyCurrentRedshift"])
         
         # Build the kdtree for the children by looping over the fetched rows.
         child_points = []
@@ -184,7 +204,7 @@ class MergerTree(lagos.ParallelAnalysisInterface):
                 nIDs.append(n[1].ID)
             candidates[row[0]] = nIDs
         
-        return candidates
+        self.candidates = candidates
 
     def _build_h5_refs(self, filename):
         # For this snapshot, add lists of file names that contain the
@@ -216,7 +236,7 @@ class MergerTree(lagos.ParallelAnalysisInterface):
             for name in self.h5fp[currt]:
                 self.h5fp[currt][name].close()
         
-    def _compute_child_fraction(self, parentfile, childfile, candidates):
+    def _compute_child_fraction(self, parentfile, childfile):
         # Given a parent and child snapshot, and a list of child candidates,
         # compute what fraction of the parent halo goes to each of the children.
         
@@ -225,8 +245,13 @@ class MergerTree(lagos.ParallelAnalysisInterface):
         parent_currt = parent_pf['CurrentTimeIdentifier']
         child_currt = child_pf['CurrentTimeIdentifier']
         
+        mylog.info("Computing fractional contribututions of particles to z=%1.5f halos." % \
+            child_pf['CosmologyCurrentRedshift'])
+        
         child_percents = {}
-        for halo in candidates:
+        for i,halo in enumerate(self.candidates):
+            if i%self.size != self.mine:
+                continue
             # Read in its particle IDs
             parent_IDs = na.array([], dtype='int64')
             for h5name in self.h5files[parent_currt][halo]:
@@ -248,8 +273,11 @@ class MergerTree(lagos.ParallelAnalysisInterface):
                 temp_percents[child] = float(intersect.size) / parent_IDs.size
             child_percents[halo] = temp_percents
         
-        # Now we write these percents to the existing entry already in the database.
-        for halo in candidates:
+        # Now we prepare a big list of writes to put in the database.
+        write_values = []
+        for i,halo in enumerate(self.candidates):
+            if i%self.size != self.mine:
+                continue
             child_IDs = []
             child_per = []
             for child in candidates[halo]:
@@ -270,14 +298,22 @@ class MergerTree(lagos.ParallelAnalysisInterface):
             # This has the child ID, child percent listed five times, followed
             # by the currt and this parent halo ID (SnapHaloID).
             values = tuple(values)
-            line = 'UPDATE Halos SET ChildHaloID0=?, ChildHaloFrac0=?,\
-            ChildHaloID1=?, ChildHaloFrac1=?,\
-            ChildHaloID2=?, ChildHaloFrac2=?,\
-            ChildHaloID3=?, ChildHaloFrac3=?,\
-            ChildHaloID4=?, ChildHaloFrac4=?\
-            WHERE SnapCurrentTimeIdentifier=? AND SnapHaloID=?;'
-            self.cursor.execute(line, values)
-        self.conn.commit()
+            write_values.append(values)
+
+        # Now we do the actual writing, but making sure that parallel tasks
+        # don't try to write at the same time.
+        line = 'UPDATE Halos SET ChildHaloID0=?, ChildHaloFrac0=?,\
+        ChildHaloID1=?, ChildHaloFrac1=?,\
+        ChildHaloID2=?, ChildHaloFrac2=?,\
+        ChildHaloID3=?, ChildHaloFrac3=?,\
+        ChildHaloID4=?, ChildHaloFrac4=?\
+        WHERE SnapCurrentTimeIdentifier=? AND SnapHaloID=?;'
+        for i in range(self.size):
+            self._barrier()
+            if i == self.mine:
+                for values in write_values:
+                    self.cursor.execute(line, values)
+                self.conn.commit()
 
 class Node(object):
     def __init__(self, CoM, mass, parentIDs, z, color):
@@ -293,18 +329,21 @@ class Link(object):
         self.fractions = []
 
 class MergerTreeDotOutput(object):
-    def __init__(self, halos, database='halos.db',
-            dotfile='MergerTree.dot'):
-        if type(halos) == types.IntType:
-            halos = [halos]
-        newhalos = set(halos)
+    def __init__(self, halos=None, database='halos.db',
+            dotfile='MergerTree.dot', current_time=None, link_min=0.2):
         self.database = database
-        if len(newhalos) == 0:
+        self.link_min = link_min
+        if halos is None:
             mylog.error("Please provide at least one halo to start the tree. Exiting.")
             return None
         result = self._open_database()
         if not result:
             return None
+        if type(halos) == types.IntType:
+            halos = [halos]
+        if current_time is not None:
+            halos = self._translate_haloIDs(halos, current_time)
+        newhalos = set(halos)
         # A key is the GlobalHaloID for this halo, and the content is a
         # Node object.
         self.nodes = {}
@@ -318,8 +357,10 @@ class MergerTreeDotOutput(object):
         self._add_nodes(newhalos)
         # Recurse over parents.
         while len(newhalos) > 0:
+            mylog.info("Finding parents for %d children." % len(newhalos))
             newhalos = self._find_parents(newhalos)
             self._add_nodes(newhalos)
+        mylog.info("Writing out to disk.")
         self._open_dot(dotfile)
         self._write_nodes()
         self._write_links()
@@ -339,6 +380,18 @@ class MergerTreeDotOutput(object):
     
     def _close_database(self):
         self.cursor.close()
+
+    def _translate_haloIDs(self, halos, current_time):
+        # If the input is in the haloID equivalent to SnapHaloID, translate them
+        # to GlobalHaloIDs.
+        new_haloIDs=[]
+        for halo in halos:
+            line = "SELECT GlobalHaloID FROM Halos WHERE SnapHaloID=? AND \
+            SnapCurrentTimeIdentifier=? limit 1;"
+            values = (halo, current_time)
+            self.cursor.execute(line, values)
+            new_haloIDs.append(self.cursor.fetchone()[0])
+        return new_haloIDs
         
     def _find_parents(self, halos):
         # Given a set of halos, find their parents and add that to each of their
@@ -347,21 +400,24 @@ class MergerTreeDotOutput(object):
         # This stores the newly discovered parent halos.
         newhalos = set([])
         for halo in halos:
-            line = 'SELECT GlobalHaloID, ChildHaloFrac0 from Halos\
-            where ChildHaloID0=? or ChildHaloID1=? or ChildHaloID2=?\
-            or ChildHaloID3=? or ChildHaloID4=?'
-            values = (halo,halo,halo,halo,halo)
-            self.cursor.execute(line, values)
-            result = self.cursor.fetchone()
-            while result:
-                pID = result[0]
-                pfrac = result[1]
-                # Store this.
-                self.nodes[halo].parentIDs.append(pID)
-                self.links[pID].childIDs.append(halo)
-                self.links[pID].fractions.append(pfrac)
-                newhalos.add(pID)
+            for i in range(5):
+                line = 'SELECT GlobalHaloID, ChildHaloFrac%d from Halos\
+                where ChildHaloID%d=?;' % (i, i)
+                values = (halo,)
+                self.cursor.execute(line, values)
                 result = self.cursor.fetchone()
+                while result:
+                    pID = result[0]
+                    pfrac = result[1]
+                    if pfrac <= self.link_min:
+                        result = self.cursor.fetchone()
+                        continue
+                    # Store this.
+                    self.nodes[halo].parentIDs.append(pID)
+                    self.links[pID].childIDs.append(halo)
+                    self.links[pID].fractions.append(pfrac)
+                    newhalos.add(pID)
+                    result = self.cursor.fetchone()
         return newhalos
     
     def _add_nodes(self, newhalos):
@@ -420,7 +476,7 @@ class MergerTreeDotOutput(object):
         for parent in self.links:
             this = self.links[parent]
             for child,frac in zip(this.childIDs, this.fractions):
-                if frac > 0.2:
+                if frac > self.link_min:
                     line = '"%d"->"%d" [label="%3.2f%%", color="blue", fontsize=10];\n' \
                         % (parent, child, frac*100.)
                     self.dotfile.write(line)
