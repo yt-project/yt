7584b8894f4cd3d2e278eb138df5ec8efe6bd44a
Sam Skillman <sam.skillman@gmail.com>
2010-02-20 19:25:38 -0700

    Add an image handler for the volume renderer.  Now is capable of hdf5 and pyfits export of the rgba channels from the volume renderer.  There is also an hdf5 import capability.
    --HG--
    branch : yt

---
+++ yt/extensions/volume_rendering/image_handling.py
@@ -0,0 +1,102 @@
+"""
+Export/Import of volume rendered images.
+
+Author: Samuel Skillman <samskillman@gmail.com>
+Affiliation: University of Colorado at Boulder
+Homepage: http://yt.enzotools.org/
+License:
+  Copyright (C) 2010 Samuel Skillman.  All Rights Reserved.
+
+  This file is part of yt.
+
+  yt is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import numpy as na
+import matplotlib; from matplotlib import pylab
+from yt.extensions.volume_rendering import *
+from yt.funcs import *
+import h5py
+try: import pyfits
+except: pass
+
+def export_rgba(image, fn, h5=True, fits=False, ):
+    if h5:
+        f = h5py.File('%s.h5'%fn, "w")
+        f.create_dataset("R", data=image[:,:,0])
+        f.create_dataset("G", data=image[:,:,1])
+        f.create_dataset("B", data=image[:,:,2])
+        f.create_dataset("A", data=image[:,:,3])
+        f.close()
+    if fits:
+        try:
+            hdu = pyfits.PrimaryHDU(image[:,:,0])
+            hdulist = pyfits.HDUList([hdu])
+            hdulist.writeto('%s_r.fits'%fn,clobber=True)
+            hdu = pyfits.PrimaryHDU(image[:,:,1])
+            hdulist = pyfits.HDUList([hdu])
+            hdulist.writeto('%s_g.fits'%fn,clobber=True)
+            hdu = pyfits.PrimaryHDU(image[:,:,2])
+            hdulist = pyfits.HDUList([hdu])
+            hdulist.writeto('%s_b.fits'%fn,clobber=True)
+            hdu = pyfits.PrimaryHDU(image[:,:,3])
+            hdulist = pyfits.HDUList([hdu])
+            hdulist.writeto('%s_a.fits'%fn,clobber=True)
+        except: print 'You do not have pyfits, install before attempting to use fits exporter'
+
+def import_rgba(name, h5=True):
+    if h5:
+        f = h5py.File(name, "r")
+        r = f['R'].value
+        g = f['G'].value
+        b = f['B'].value
+        a = f['A'].value
+    return na.array([r,g,b,a]).swapaxes(0,2).swapaxes(0,1)
+
+def plot_channel(image, name, cmap='gist_heat', log=True, dex=3, zero_factor=1.0e-10, 
+                 label=None, label_color='w', label_size='large'):
+    Nvec = image.shape[0]
+    image[na.isnan(image)] = 0.0
+    ma = image[image>0.0].max()
+    image[image==0.0] = ma*zero_factor
+    if log:
+        mynorm = matplotlib.colors.LogNorm(ma/(10.**dex), ma)
+
+    pylab.clf()
+    pylab.gcf().set_dpi(100)
+    pylab.gcf().set_size_inches((Nvec/100.0, Nvec/100.0))
+    pylab.gcf().subplots_adjust(left=0.0, right=1.0, bottom=0.0, top=1.0, wspace=0.0, hspace=0.0)
+    mycm = pylab.cm.get_cmap(cmap)
+    if log:
+        pylab.imshow(image,cmap=mycm, norm=mynorm, interpolation='nearest')
+    else:
+        pylab.imshow(image,cmap=mycm, interpolation='nearest')
+    if label is not None:
+        pylab.text(20, 20,label, color = label_color, size=label_size) 
+    pylab.savefig("%s_%s.png" % (name,cmap))
+    pylab.clf()
+
+def plot_rgb(image, name, label=None, label_color='w', label_size='large'):
+    Nvec = image.shape[0]
+    image[na.isnan(image)] = 0.0
+    if image.shape[2] == 4:
+        image = image[:,:,:3]
+    pylab.clf()
+    pylab.gcf().set_dpi(100)
+    pylab.gcf().set_size_inches((Nvec/100.0, Nvec/100.0))
+    pylab.gcf().subplots_adjust(left=0.0, right=1.0, bottom=0.0, top=1.0, wspace=0.0, hspace=0.0)
+    pylab.imshow(image, interpolation='nearest')
+    if label is not None:
+        pylab.text(20, 20, label, color = label_color, size=label_size) 
+    pylab.savefig("%s_rgb.png" % name)
+    pylab.clf()
