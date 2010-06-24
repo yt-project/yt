e22bd6f651ed37d14e10ff56d1dac99ee9a3f7fc
Matthew Turk <matthewturk@gmail.com>
2010-06-24 11:36:41 -0700

    [svn r1757] Updating docstrings to compile
    --HG--
    branch : trunk

--- yt/raven/plot_collection.py
+++ yt/raven/plot_collection.py
@@ -787,6 +787,7 @@ class PlotCollection(object):
         yt.lagos.BinnedProfile1D : This is the object that does the
                                    transformation of raw data into a 1D
                                    profile.
+
         Examples
         --------
 
@@ -977,7 +978,7 @@ class PlotCollection(object):
 
         Returns
         -------
-        plot : `yt.raven.PhasePlot`
+        plot : `yt.raven.PlotTypes.PhasePlot`
             The plot that has been added to the PlotCollection.
 
         See Also
@@ -985,15 +986,16 @@ class PlotCollection(object):
         yt.lagos.BinnedProfile2D : This is the object that does the
                                    transformation of raw data into a 1D
                                    profile.
+        
         Examples
         --------
-
         This will show the mass-distribution in the Density-Temperature plane.
-
+        
+        >>> pf = load("RD0005-mine/RedshiftOutput0005")
         >>> reg = pf.h.region([0.1, 0.2, 0.3], [0.0, 0.1, 0.2],
-                              [0.2, 0.3, 0.4])
+        ...                   [0.2, 0.3, 0.4])
         >>> pc.add_phase_object(reg, ["Density", "Temperature", "CellMassMsun"],
-                                weight = None)
+        ...                     weight = None)
         """
         if x_bounds is None:
             x_min, x_max = data_source.quantities["Extrema"](
@@ -1108,6 +1110,7 @@ class PlotCollection(object):
         yt.lagos.BinnedProfile2D : This is the object that does the
                                    transformation of raw data into a 1D
                                    profile.
+
         Examples
         --------
 
