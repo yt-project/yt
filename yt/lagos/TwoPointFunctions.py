025b0a1d732621b5a2d774b64c5e41762f04034e
Matthew Turk <matthewturk@gmail.com>
2010-06-26 12:50:42 -0700

    [svn r1773] Fixing indentation and underlining errors
    --HG--
    branch : trunk

--- yt/lagos/TwoPointFunctions.py
+++ yt/lagos/TwoPointFunctions.py
@@ -586,7 +586,7 @@ class TwoPointFunctions(ParallelAnalysisInterface):
         directory.
         
         Examples
-        -------
+        --------
         >>> tpf.write_out_means()
         """
         for fset in self._fsets:
@@ -618,7 +618,7 @@ class TwoPointFunctions(ParallelAnalysisInterface):
         'function_name.txt' and saved in the current working directory.
         
         Examples
-        -------
+        --------
         >>> tpf.write_out_arrays()
         """
         if self.mine == 0:
@@ -702,6 +702,7 @@ class FcnSet(TwoPointFunctions):
             A pair of values giving the range for the bins.
             A pair of floats (a list), or a list of pairs for N-dim binning.
             Default = None.
+
         Examples
         --------
         >>> f1.set_pdf_params(bin_type='log', bin_range=[5e4, 5.5e13],
