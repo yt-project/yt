3457fbea727517827873b8988d852f26269da394
Matthew Turk <matthewturk@gmail.com>
2010-07-19 22:36:08 -0700

    Switching the order of bin_id creation and field table integration early termination for tiny speedup
    --HG--
    branch : yt

--- yt/_amr_utils/VolumeIntegrator.pyx
+++ yt/_amr_utils/VolumeIntegrator.pyx
@@ -106,9 +106,9 @@ cdef np.float64_t FIT_get_value(FieldInterpolationTable *fit,
     cdef np.float64_t bv, dy, dd, tf
     cdef int bin_id
     if fit.pass_through == 1: return dvs[fit.field_id]
+    if dvs[fit.field_id] > fit.bounds[1] or dvs[fit.field_id] < fit.bounds[0]: return 0.0
     bin_id = <int> ((dvs[fit.field_id] - fit.bounds[0]) * fit.idbin)
     dd = dvs[fit.field_id] - (fit.bounds[0] + bin_id * fit.dbin) # x - x0
-    if bin_id > fit.nbins - 2 or bin_id < 0: return 0.0
     bv = fit.values[bin_id]
     dy = fit.values[bin_id + 1] - bv
     if fit.weight_field_id != -1:
