9a2edb08906b61b53c38b8b8b256989983379092
J.S. Oishi <jsoishi@gmail.com>
2010-08-26 16:53:12 -0700

    forgot to add _mpl_imports.py earlier
    --HG--
    branch : yt2-reorganization

---
+++ yt/visualization/_mpl_imports.py
@@ -0,0 +1,14 @@
+import matplotlib
+matplotlib.rc('contour', negative_linestyle='solid')
+
+import matplotlib.image
+import matplotlib.ticker
+import matplotlib.axes
+import matplotlib.figure
+import matplotlib._image
+import matplotlib.colors
+import matplotlib.colorbar
+import matplotlib.cm
+import matplotlib.collections
+
+
