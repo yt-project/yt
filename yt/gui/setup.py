9bc9fb4c1620c1c115f0c24de0de6e4a0bd6fa53
Matthew Turk <matthewturk@gmail.com>
2010-08-26 12:14:58 -0600

    Removing old reason, moving all the still-useful components to gui
    --HG--
    branch : yt2-reorganization

--- yt/gui/setup.py
+++ yt/gui/setup.py
@@ -4,7 +4,7 @@ import os, sys, os.path
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
-    config = Configuration('reason',parent_package,top_path)
+    config = Configuration('gui',parent_package,top_path)
     config.make_config_py() # installs __config__.py
     config.make_svn_version_py()
     return config
