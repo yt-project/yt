a9e484f14b61b6da15d78680cd4758c4a2414dbc
Matthew Turk <matthewturk@gmail.com>
2010-08-26 10:33:52 -0600

    Now builds in-place
    --HG--
    branch : yt2-reorganization

--- yt/frontends/ramses/setup.py
+++ yt/frontends/ramses/setup.py
@@ -6,8 +6,8 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('ramses',parent_package,top_path)
     config.add_extension("ramses_reader",
-        ["yt/ramses_reader.cpp"],
-        include_dirs=["yt/ramses_headers/"])
+        ["yt/frontends/ramses/ramses_reader.cpp"],
+        include_dirs=["yt/frontends/ramses/ramses_headers/"])
     config.make_config_py() # installs __config__.py
     config.make_svn_version_py()
     return config
