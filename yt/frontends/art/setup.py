f90af68b5055cd37d7f5edcecd55f506f9a75489
Matthew Turk <matthewturk@gmail.com>
2010-10-13 16:02:27 -0700

    This fixes compilation errors.
    --HG--
    branch : yt

--- yt/frontends/art/setup.py
+++ yt/frontends/art/setup.py
@@ -5,9 +5,6 @@ import os, sys, os.path
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('art',parent_package,top_path)
-    config.add_extension("_art_reader",
-        ["yt/frontends/art/_ramses_reader.cpp"],
-        include_dirs=["yt/frontends/art/ramses_headers/"])
     config.make_config_py() # installs __config__.py
     config.make_svn_version_py()
     return config
