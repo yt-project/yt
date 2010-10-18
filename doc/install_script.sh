8274587566cb0b40e9c2303edddd86a93be5ec35
Matthew Turk <matthewturk@gmail.com>
2010-10-18 09:27:17 -0500

    Upgrading numpy and matplotlib to 1.5.0 and 1.0.0, respectively.  Fixing typo in some MPL commands.
    --HG--
    branch : yt

--- doc/install_script.sh
+++ doc/install_script.sh
@@ -229,8 +229,8 @@ fi
 [ $INST_BZLIB -eq 1 ] && get_enzotools bzip2-1.0.5.tar.gz
 [ $INST_PNG -eq 1 ] && get_enzotools libpng-1.2.43.tar.gz
 get_enzotools Python-2.6.3.tgz
-get_enzotools numpy-1.4.1.tar.gz
-get_enzotools matplotlib-0.99.3.tar.gz
+get_enzotools numpy-1.5.0.tar.gz
+get_enzotools matplotlib-1.0.0.tar.gz
 get_enzotools mercurial-1.6.3.tar.gz
 get_enzotools ipython-0.10.tar.gz
 get_enzotools h5py-1.2.0.tar.gz
@@ -367,7 +367,7 @@ echo "Installing distribute"
 echo "Installing pip"
 ( ${DEST_DIR}/bin/easy_install-2.6 pip 2>&1 ) 1>> ${LOG_FILE} || do_exit
 
-do_setup_py numpy-1.4.1 ${NUMPY_ARGS}
+do_setup_py numpy-1.5.0 ${NUMPY_ARGS}
 
 if [ -n "${MPL_SUPP_LDFLAGS}" ]
 then
@@ -387,14 +387,14 @@ then
     export CCFLAGS="${MPL_SUPP_CCFLAGS}"
     echo "Setting CCFLAGS ${CCFLAGS}"
 fi
-do_setup_py matplotlib-0.99.3
-if [-n "${OLD_LDFLAGS}" ]
+do_setup_py matplotlib-1.0.0
+if [ -n "${OLD_LDFLAGS}" ]
 then
     export LDFLAG=${OLD_LDFLAGS}
 fi
-[-n "${OLD_LDFLAGS}" ] && export LDFLAGS=${OLD_LDFLAGS}
-[-n "${OLD_CXXFLAGS}" ] && export CXXFLAGS=${OLD_CXXFLAGS}
-[-n "${OLD_CCFLAGS}" ] && export CCFLAGS=${OLD_CCFLAGS}
+[ -n "${OLD_LDFLAGS}" ] && export LDFLAGS=${OLD_LDFLAGS}
+[ -n "${OLD_CXXFLAGS}" ] && export CXXFLAGS=${OLD_CXXFLAGS}
+[ -n "${OLD_CCFLAGS}" ] && export CCFLAGS=${OLD_CCFLAGS}
 do_setup_py ipython-0.10
 do_setup_py h5py-1.2.0
 
