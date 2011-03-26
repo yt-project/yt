87bcf82bae4345bc818864706d6d8b300721be57
Matthew Turk <matthewturk@gmail.com>
2011-03-26 18:16:56 -0400

    Adding support for exposing RPC using ExtDirect through Bottle.
    --HG--
    branch : yt

--- yt/gui/reason/bottle_mods.py
+++ yt/gui/reason/bottle_mods.py
@@ -24,8 +24,9 @@ License:
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from .bottle import server_names, debug, route, run
+from .bottle import server_names, debug, route, run, request
 import uuid
+from extdirect_router import DirectRouter
 
 route_functions = {}
 
@@ -34,6 +35,22 @@ def preroute(future_route, *args, **kwargs):
         route_functions[future_route] = (args, kwargs, func)
         return func
     return router
+
+class BottleDirectRouter(DirectRouter):
+    # This class implements a mechanism for auto-routing an ExtDirect-callable
+    # object through Bottle.  It should be used as a base class of an object,
+    # and the __init__ function will need to include the keyword argument
+    # 'route' for it to work.
+    def __init__(self, *args, **kwargs):
+        route = kwargs.pop("route")
+        super(BottleDirectRouter, self).__init__(*args, **kwargs)
+        self.__name__ = ""
+        bottle.route(route, method="POST")(self)
+
+    def __call__(self):
+        val = request.body.read()
+        rv = super(BottleDirectRouter, self).__call__(val)
+        return rv
 
 def uuid_serve_functions(pre_routed = None, open_browser=False):
     if pre_routed == None: pre_routed = route_functions
