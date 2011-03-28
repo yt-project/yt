e04123889890a2e9e69c5236dfce694bcf3e8963
Britton Smith <brittonsmith@gmail.com>
2011-03-28 11:00:13 -0400

    Removed fake pf data file.
    --HG--
    branch : yt

--- yt/gui/reason/html/pf_data.js
+++
@@ -1,8 +0,0 @@
-var my_pfs = [{name: "pf1", objects: [
-                              {name: "sp1", type: "sphere"},
-                              {name: "sp2", type: "sphere"}
-				      ]},
-              {name: "pf2", objects: [
-                              {name: 'r1', type: "region"},
-                              {name: 'ad', type: "all_data"}]},
-              {name: "pf3", objects: []}]
