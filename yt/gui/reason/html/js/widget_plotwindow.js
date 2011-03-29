60a79df3929e3dff937f0f064bb60ca0ad19e845
Matthew Turk <matthewturk@gmail.com>
2011-03-28 23:36:38 -0400

    Reformatting the PlotWindow to be an object
    --HG--
    branch : yt

--- yt/gui/reason/html/js/widget_plotwindow.js
+++ yt/gui/reason/html/js/widget_plotwindow.js
@@ -1,41 +1,41 @@
-var NorthButton = new Ext.Button(
-				 {text : 'North',
-				     pageX : 205,
-				     pageY : 10
-//        handler: function(b, e) { window.open("session.py", "_top"); }
-				     }
-				 );
+var WidgetPlotWindow = function(python_varname, viewport) {
+    this.vn = "pw_" + python_varname;
+    this.print_python = function(b, e) {
+        yt_rpc.ExtDirectREPL.execute({code:'print 1,2,3, "' + python_varname + '"'},
+                                     function(f, a)
+                                     {alert(a.result['output']);});
+    }
 
-var EastButton = new Ext.Button(
-				{text:'East',
-				    pageX : 410,
-				    pageY : 205
-				    }
-				);
-var SouthButton = new Ext.Button(
-				 {text:'South',
-				     pageX : 205,
-				     pageY : 410
-				     }
-				 );
-var WestButton = new Ext.Button(
-				{text:'West',
-				    pageX : 10,
-				    pageY : 205
-				    }
-				);
+    viewport.get("center-panel").add(
+                  {
+                  xtype: 'panel',
+                  id: this.vn,
+                  title: this.vn,
+                  iconCls: 'graph',
+                  autoScroll: true,
+                  layout:'absolute',
+                  items: [ 
+                      {xtype:'button',
+                       text: 'North',
+                       x: 205,
+                       y: 10},
+                      {xtype:'button',
+                       text:'East',
+                       x : 410,
+                       y : 205,
+                       handler: this.print_python},
+                      {xtype:'button',
+                       text: 'South',
+                       x: 205,
+                       y: 410},
+                      {xtype: 'button',
+                       text: 'West',
+                       x: 10,
+                       y: 205},
+                       ]
+                  });
 
-
-var PlotPanel = new Ext.Panel(
-			      {
-			      title: 'Plot Window 1',
-				  iconCls: 'graph',
-				  autoScroll: true,
-				  layout:'absolute',
-				  items: [ 
-					  NorthButton,
-					  EastButton,
-					  SouthButton,
-					  WestButton
-					   ]
-				  });
+    viewport.doLayout();
+    this.panel = viewport.get("center-panel").get("pw_" + python_varname);
+    this.panel.doLayout();
+}
