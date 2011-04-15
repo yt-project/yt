148adacd6f9312485cc6c91ac266e1bcfeb609b3
Sam Skillman <sam.skillman@gmail.com>
2011-04-15 15:58:30 -0400

    Adding the first bits of code for the OpenGL grid viewer.  More work to be done.
    --HG--
    branch : yt

---
+++ yt/gui/reason/html/js/widget_gridviewer.js
@@ -0,0 +1,203 @@
+/**********************************************************************
+The Plot Window Widget
+
+Author: Cameron Hummels <chummels@gmail.com>
+Affiliation: Columbia
+Author: Jeffrey S. Oishi <jsoishi@gmail.com>
+Affiliation: KIPAC/SLAC/Stanford
+Author: Britton Smith <brittonsmith@gmail.com>
+Affiliation: MSU
+Author: Matthew Turk <matthewturk@gmail.com>
+Affiliation: NSF / Columbia
+Homepage: http://yt.enzotools.org/
+License:
+  Copyright (C) 2011 Matthew Turk.  All Rights Reserved.
+
+  This file is part of yt.
+
+  yt is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+***********************************************************************/
+
+
+
+var WidgetPlotWindow = function(python_varname, widget_data) {
+    this.id = python_varname;
+    this.widget_data = widget_data;
+
+    viewport.get("center-panel").add(
+        {
+            xtype: 'panel',
+		id: "pw_" + this.id,
+		title: "WebGL Grid Viewer",
+		iconCls: 'graph',
+		autoScroll: true,
+		layout:'absolute',
+		closable: true,
+		
+		function webGLStart() {
+		    this.curX = 0;
+		    this.curY = 0;
+		    this.dist = 0;
+		    function updateBasedOnOffset(camera, offset){
+			camera.position.x = camera.target.x + offset.x;
+			camera.position.y = camera.target.y + offset.y;
+			camera.position.z = camera.target.z + offset.z;
+		    }
+		    function camGetOffset(camera){
+			return PhiloGL.Vec3.sub(camera.position, camera.target)
+			    }
+		    PhiloGL('lesson01-canvas', {
+			    camera: {
+				position: {
+				    x: 0.5, y: 0.5, z: 5
+					},
+				    target: {
+				    x: 0.5, y: 0.5, z: 0.5
+					},
+				    },
+				program: {
+				from: 'ids',
+				    vs: 'shader-vs',
+				    fs: 'shader-fs'
+				    },	
+				events: {
+				onDragStart: function(e) {
+				    pos = {
+					x: e.x,
+					y: e.y
+				    };
+				    this.curX = e.x;
+				    this.curY = e.y;
+				    this.dist = camGetOffset(this.camera).norm();
+				},
+				    onDragEnd: function(e) {
+				    pos = {
+					x: e.x,
+					y: e.y
+				    };
+				},
+				    onDragMove: function(e) {
+				    var c = this.camera;
+				    var off = camGetOffset(c);
+				    // Get Horizontal vector
+				    var horiz = PhiloGL.Vec3.cross(c.up, 
+								   camGetOffset(c))
+					horiz.$scale(1./horiz.norm());
+
+				    if (e.event.button == 0){ // Rotation
+					// Do vertical rotation about horizontal vector
+					var vert_rot = new PhiloGL.Mat4();
+					vert_rot.id();
+					vert_rot.$rotateAxis((e.y-this.curY)/100., horiz);
+					PhiloGL.Mat4.$mulVec3(vert_rot, off);
+					PhiloGL.Mat4.$mulVec3(vert_rot, c.up);
+					c.up.$scale(1./c.up.norm());
+
+					// Do horizontal rotation about up vector
+					var side_rot = new PhiloGL.Mat4();
+					side_rot.id();
+					side_rot.$rotateAxis(-(e.x-this.curX)/100., c.up);
+					side_rot.$mulVec3(off);
+		    
+					// Update current positions
+					this.curX = e.x;
+					this.curY = e.y;
+					this.dist = off.norm();
+					updateBasedOnOffset(c, off);
+					c.update();
+				    } else if (e.event.button = 2){ // Right click - transpose
+					var move_up = c.up.scale(-(e.y-this.curY)/200.);
+					var move_over = horiz.scale(-(e.x-this.curX)/200.);
+					c.position.$add(move_up);
+					c.position.$add(move_over);
+					c.target.$add(move_up);
+					c.target.$add(move_over);
+					// Update current positions
+					this.curX = e.x;
+					this.curY = e.y;
+					c.update();
+				    }
+		
+				},
+				    onMouseWheel: function(e){
+				    e.stop();
+				    var offset = PhiloGL.Vec3.scale(camGetOffset(this.camera),
+								    1.0 - e.wheel/10.);
+				    updateBasedOnOffset(this.camera, offset);
+				    this.camera.update();
+				}
+			    },
+				onError: function() {
+				alert("An error ocurred while loading the application");
+			    },
+				onLoad: function(app) {
+				var gl = app.gl,
+				    canvas = app.canvas,
+				    program = app.program,
+				    scene = app.scene,
+				    camera = app.camera;
+	    
+				gl.viewport(0, 0, canvas.width, canvas.height);
+				gl.clearColor(0, 0, 0, 1);
+				gl.clearDepth(1);
+				gl.enable(gl.DEPTH_TEST);
+				gl.depthFunc(gl.LEQUAL);
+
+				program.setBuffers({
+					'shapeset': {
+					    attribute: 'aVertexPosition',
+						value: new Float32Array(vertex_positions),
+						size: 3
+						},
+		
+					    });
+				camera.modelView.id();
+				draw()
+	    
+				    setInterval(draw, 100/60);
+				//Draw the scene
+				function draw() {
+				    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+				    //Draw Triangle
+				    program.setUniform('uMVMatrix', camera.modelView);
+				    program.setUniform('uPMatrix', camera.projection);
+				    program.setBuffer('triangle');
+				    gl.drawArrays(gl.LINES, 0, n_vertices);	    
+				    examine = camera
+					}
+			    }
+			});  
+		}		
+	    
+	    
+        }
+    );
+
+    viewport.get("center-panel").activate("pw_" + this.id);
+    viewport.doLayout();
+    this.panel = viewport.get("center-panel").get("pw_" + python_varname);
+    this.panel.doLayout();
+    this.image_panel = this.panel.get("image_panel_"+python_varname);
+    var image_dom = this.image_panel.el.dom;
+
+    this.accept_results = function(payload) {
+        this.image_panel.el.dom.src = "data:image/png;base64," + payload['image_data'];
+    }
+
+    yt_rpc.ExtDirectREPL.execute(
+        {code:python_varname + '.zoom(1.0)'},
+        cell_finished);
+}
+
+widget_types['plot_window'] = WidgetPlotWindow;
