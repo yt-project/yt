b2094e883ab0d476d80a7b48d7c38cf4fafe29b5
Cameron Hummels <chummels@astro.columbia.edu>
2011-04-19 15:53:26 -0400

    Cleaned up reason.js formatting and removed old references to initial plot window.
    --HG--
    branch : yt

--- yt/gui/reason/html/js/reason.js
+++ yt/gui/reason/html/js/reason.js
@@ -45,246 +45,208 @@ var handle_result = function(f, a) {
     cell_finished(a.result);
 }
 
-    var handle_payload = function(pp) {
-        cell_finished(pp);
-    }
+var handle_payload = function(pp) {
+    cell_finished(pp);
+}
 
-    var repl_input = new Ext.FormPanel({
-        title: 'YT Input',
-        url: 'push',
-        flex: 0.2,
-        layout: 'fit',
-        padding: 5,
-        items: [{
-            id: 'input_line',
-            xtype: 'textarea',
-            width: '100%',
-            autoScroll: true,
-            name: 'line',
-            allowBlank: 'True',
-            bodyStyle: 'font-family: "monospace";',
-            listeners: {
-                specialkey: function(f, e){
-                    if (e.getKey() == e.ENTER) {
-                        cell_sent();
-                        yt_rpc.ExtDirectREPL.execute({
-                            code:repl_input.get('input_line').getValue()},
-                        handle_result);
-	                }
-	            },
-                afterrender: function(f, e){
-                    //var input_line_drop_target_el = repl_input.get("input_line").el.dom;
-                    var input_line_drop_target_el = repl_input.body.dom;
-
-                    var input_line_drop_target = new Ext.dd.DropTarget(input_line_drop_target_el, {
-                        ddGroup     : 'pfDDgroup',
-                        notifyEnter : function(ddSource, e, data) {
-                            repl_input.body.stopFx();
-                            repl_input.body.highlight();
-                        },
-                        notifyDrop  : function(ddSource, e, data){
-
-                            var varname = data.node.attributes.objdata.varname;
-                            /* There is possibly a better way to do this, where it's also
-                               inserted correctly. */
-                            var line = repl_input.get("input_line");
-                            line.setValue(line.getValue() + varname);
-                            line.focus();
-                            return(true);
-                        }
-                    });
-                },
-            },
-        },],
-    });
-
-    var NorthButton = new Ext.Button({
-        text : 'North',
-	    pageX : 205,
-        pageY : 10
-//        handler: function(b, e) { window.open("session.py", "_top"); }
-    });
-
-    var EastButton = new Ext.Button({
-        text:'East',
-        pageX : 410,
-        pageY : 205
-    });
-
-    var SouthButton = new Ext.Button({
-        text:'South',
-        pageX : 205,
-        pageY : 410
-    });
-
-    var WestButton = new Ext.Button({
-        text:'West',
-        pageX : 10,
-        pageY : 205
-    });
-
-    var OutputContainer = new Ext.Panel({
-        title: 'YT Output',
-        id: 'output_container',
+var repl_input = new Ext.FormPanel({
+    title: 'YT Input',
+    url: 'push',
+    flex: 0.2,
+    layout: 'fit',
+    padding: 5,
+    items: [{
+        id: 'input_line',
+        xtype: 'textarea',
+        width: '100%',
         autoScroll: true,
-        flex: 0.8,
-        items: []
-    });
-
-    var PlotPanel = new Ext.Panel({
-        title: 'Plot Window 1',
-        iconCls: 'graph',
-        autoScroll: true,
-        layout:'absolute',
-        items: [ 
-            NorthButton,
-            EastButton,
-            SouthButton,
-            WestButton
-        ]
-    });
-
-    var examine;
-    var notebook;
-
-
-    var treePanel = new Ext.tree.TreePanel({
-        iconCls: 'nav',
-        id: 'tree-panel',
-        layout: 'anchor',
-        region:'west',
-        split: true,
-        anchor: '100% -35',
-        minSize: 150,
-        autoScroll: true,
-        rootVisible: false,
-        ddGroup: 'pfDDgroup',
-        enableDD: true,
-        root:new Ext.tree.TreeNode({
-            expanded:true,
-            leaf:false,
-            text:''
-        }),
+        name: 'line',
+        allowBlank: 'True',
+        bodyStyle: 'font-family: "monospace";',
         listeners: {
-                render: {
-                    fn: function() {
-                        Ext.getBody().on("contextmenu", Ext.emptyFn,
-                            null, {preventDefault: true});
-                    }
-                },
-                contextmenu: {
-                    fn: function(node, event){
-                        if (node.attributes.objdata.type == 'obj') return;
-                        var rightClickMenu = new Ext.menu.Menu({
-                            items: [
-                                {
-                                    text: 'View Grids',
-                                    handler: getGridViewerHandler(node),
-                                }, {
-                                    text: 'Open slice',
-                                    handler: getSliceHandler(node),
-                                }, {
-                                    text: 'Open projection',
-                                    handler: getProjectionHandler(node),
-                                }, {
-                                    text: 'View Streamlines',
-                                    handler: getStreamlineViewerHandler(node),
-                                }
-                            ]
-                        });
-                        rightClickMenu.showAt(event.xy);
-                    }
+            specialkey: function(f, e){
+                if (e.getKey() == e.ENTER) {
+                    cell_sent();
+                    yt_rpc.ExtDirectREPL.execute({
+                        code:repl_input.get('input_line').getValue()},
+                    handle_result);
                 }
-          }
-    });
+            },
+            afterrender: function(f, e){
+                //var input_line_drop_target_el = repl_input.get("input_line").el.dom;
+                var input_line_drop_target_el = repl_input.body.dom;
 
-    var status_panel;
-    var logging_store = new Ext.data.Store({
-        fields: [{name:'record'}],
-        reader: new Ext.data.ArrayReader({}, [{name: 'record'}]),
-    });
+                var input_line_drop_target = new Ext.dd.DropTarget(input_line_drop_target_el, {
+                    ddGroup     : 'pfDDgroup',
+                    notifyEnter : function(ddSource, e, data) {
+                        repl_input.body.stopFx();
+                        repl_input.body.highlight();
+                    },
+                    notifyDrop  : function(ddSource, e, data){
 
-    var heartbeat_request = false;
-    var task_runner = new Ext.util.TaskRunner();
-    var heartbeat;
+                        var varname = data.node.attributes.objdata.varname;
+                        /* There is possibly a better way to do this, where it's also inserted correctly. */
+                        var line = repl_input.get("input_line");
+                        line.setValue(line.getValue() + varname);
+                        line.focus();
+                        return(true);
+                    }
+                });
+            },
+        },
+    },],
+});
 
 
-    Ext.onReady(function(){
-       Ext.BLANK_IMAGE_URL = 'resources/resources/images/default/s.gif';
+
+
+
+var OutputContainer = new Ext.Panel({
+    title: 'YT Output',
+    id: 'output_container',
+    autoScroll: true,
+    flex: 0.8,
+    items: []
+});
+
+var examine;
+var notebook;
+
+var treePanel = new Ext.tree.TreePanel({
+    iconCls: 'nav',
+    id: 'tree-panel',
+    layout: 'anchor',
+    region:'west',
+    split: true,
+    anchor: '100% -35',
+    minSize: 150,
+    autoScroll: true,
+    rootVisible: false,
+    ddGroup: 'pfDDgroup',
+    enableDD: true,
+    root:new Ext.tree.TreeNode({
+        expanded:true,
+        leaf:false,
+        text:''
+    }),
+    listeners: {
+        render: {
+            fn: function() {
+                Ext.getBody().on("contextmenu", Ext.emptyFn,
+                null, {preventDefault: true});
+            }
+        },
+        contextmenu: {
+            fn: function(node, event){
+                if (node.attributes.objdata.type == 'obj') return;
+                var rightClickMenu = new Ext.menu.Menu({
+                    items: [
+                        {
+                            text: 'View Grids',
+                            handler: getGridViewerHandler(node),
+                        }, {
+                            text: 'Open slice',
+                            handler: getSliceHandler(node),
+                        }, {
+                            text: 'Open projection',
+                            handler: getProjectionHandler(node),
+                        }, {
+                            text: 'View Streamlines',
+                            handler: getStreamlineViewerHandler(node),
+                        }
+                    ]
+                });
+                rightClickMenu.showAt(event.xy);
+            }
+        }
+    }
+});
+
+var status_panel;
+var logging_store = new Ext.data.Store({
+    fields: [{name:'record'}],
+    reader: new Ext.data.ArrayReader({}, [{name: 'record'}]),
+});
+
+var heartbeat_request = false;
+var task_runner = new Ext.util.TaskRunner();
+var heartbeat;
+
+
+Ext.onReady(function(){
+    Ext.BLANK_IMAGE_URL = 'resources/resources/images/default/s.gif';
 
     // NOTE: This is an example showing simple state management. During development,
     // it is generally best to disable state management as dynamically-generated ids
     // can change across page loads, leading to unpredictable results.  The developer
     // should ensure that stable state ids are set for stateful components in real apps.
     // it's a cold day for pontooning.
-        Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+    Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
 
     // Go ahead and create the TreePanel now so that we can use it below
-        viewport = new Ext.Viewport({
-            layout: 'border',
-            items: [
-		    // lazily created panel (xtype:'panel' is default)
-                {
-                    xtype: 'grid',
-                    store: logging_store,
-                    defaults: { width: 800 },
-                    columns: [ {id:'record', 
-                        sortable: false,
-                        width:800} ],
-                    autofill: true,
-                    region: 'south',
-                    id: "status-region",
-                    cls: "status-logger",
-                    split: true,
-                    height: 100,
-                    maxSize: 200,
-                    collapsible: true,
-                    title: 'Status',
-                    margins: '0 0 0 0',
-                }, {
-                    region: 'west',
-                    id: 'west-panel', // see Ext.getCmp() below
-                    title: 'Data Objects',
-                    split: true,
-                    width: 200,
-                    minSize: 175,
-                    maxSize: 400,
-                    collapsible: true,
-                    margins: '0 0 0 5',
-                    layout: {
-                        type: 'anchor',
+    viewport = new Ext.Viewport({
+        layout: 'border',
+        items: [
+		// lazily created panel (xtype:'panel' is default)
+            {
+                xtype: 'grid',
+                store: logging_store,
+                defaults: { width: 800 },
+                columns: [ {id:'record', 
+                    sortable: false,
+                    width:800} ],
+                autofill: true,
+                region: 'south',
+                id: "status-region",
+                cls: "status-logger",
+                split: true,
+                height: 100,
+                maxSize: 200,
+                collapsible: true,
+                title: 'Status',
+                margins: '0 0 0 0',
+            }, {
+                region: 'west',
+                id: 'west-panel', // see Ext.getCmp() below
+                title: 'Data Objects',
+                split: true,
+                width: 200,
+                minSize: 175,
+                maxSize: 400,
+                collapsible: true,
+                margins: '0 0 0 5',
+                layout: {
+                    type: 'anchor',
+                },
+                items: [{
+                        xtype: 'toolbar',
+                        items: [ main_menu ],
                     },
-                    items: [
-                        {xtype: 'toolbar',
-                          items: [ main_menu ],
-                        },
-                        treePanel,
-                    ]
+                    treePanel,
+                ]
 		  // in this instance the TabPanel is not wrapped by another panel
 		  // since no title is needed, this Panel is added directly
 		  // as a Container
-                },{
-                    xtype: 'tabpanel',
-                    region: 'center', 
-                    id: 'center-panel',
-                    deferredRender: false,
-                    activeTab: 0,     
-                    items: [
-                        {
-                            title: 'YT',
-                            id: 'notebook',
-                            layout: 'vbox',
-                            layoutConfig: {align:'stretch'},
-                            closable: false,
-                            autoScroll: false,
-                            iconCls: 'console',
-                            items: [repl_input, OutputContainer]
-                        }, 
-//                        PlotPanel
-                    ]
-                }
-            ]
-        });
+            },{
+                xtype: 'tabpanel',
+                region: 'center', 
+                id: 'center-panel',
+                deferredRender: false,
+                activeTab: 0,     
+                items: [{
+                        title: 'YT',
+                        id: 'notebook',
+                        layout: 'vbox',
+                        layoutConfig: {align:'stretch'},
+                        closable: false,
+                        autoScroll: false,
+                        iconCls: 'console',
+                        items: [repl_input, OutputContainer]
+                    }, 
+                ]
+            }
+        ]
+    });
 
 // get a reference to the HTML element with id "hideit" and add a click listener to it 
     console.log('Mitchell!\nPardon me! Mitchell!')
@@ -335,4 +297,4 @@ var handle_result = function(f, a) {
 
     task_runner.start(heartbeat);
                          
-    });
+});
