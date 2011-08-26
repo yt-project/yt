5d61a760a21a2fbf977667c01fb935961b495bb1
Matthew Turk <matthewturk@gmail.com>
2011-08-26 10:45:29 -0400

    Bumping version of unstable branch to 2.3dev.
    --HG--
    branch : yt

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ build_src.build_src.generate_a_pyrex_source = generate_a_pyrex_source
 
 import setuptools
 
-VERSION = "2.2dev"
+VERSION = "2.3dev"
 
 if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 
