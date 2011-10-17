16b73dd42d4bef050ff6e4d341dc1e9ebce9c56a
Cameron Hummels <chummels@astro.columbia.edu>
2011-10-17 12:36:16 -0400

    Added halo composition hash tests to make strict identicality tests between the outputs from all three different methods for halo finding.
    --HG--
    branch : yt

--- yt/utilities/answer_testing/halo_tests.py
+++ yt/utilities/answer_testing/halo_tests.py
@@ -2,6 +2,7 @@ from yt.mods import *
 import matplotlib; matplotlib.use("Agg")
 import pylab
 from output_tests import SingleOutputTest, YTStaticOutputTest, create_test
+import hashlib
 
 # Tests the number of halos returned by the HOP halo finder on a dataset
 class TestHaloCountHOP(YTStaticOutputTest):
@@ -82,7 +83,88 @@ class TestHaloComposition(YTStaticOutputTest):
                 return False
         return True
     
-    def plot(self):
-        return []
-
 create_test(TestHaloComposition, "halo_composition_test", threshold=80.0)
+
+# Tests the content of the halos returned by the HOP halo finder on a dataset 
+# by comparing the hash of the arrays of all the particles contained in each
+# halo.
+class TestHaloCompositionHashHOP(YTStaticOutputTest):
+    threshold=80.0
+    
+    def run(self):
+        # Find the haloes using vanilla HOP.
+        haloes = HaloFinder(self.pf, threshold=self.threshold, dm_only=False)
+        # The result is a list of the arrays of the particle IDs, for
+        # each halo
+        IDs = []
+        for halo in haloes:
+            IDs.append(halo["particle_index"])
+        self.result = IDs
+    
+    def compare(self, old_result):
+        # All the lists of arrays should be identical.  To check this
+        # faster, we take the 256-bit hash of these lists and compare them
+        result_hash = hashlib.sha256(self.result.tostring()).hexdigest()
+        old_result_hash = hashlib.sha256(old_result.tostring()).hexdigest()
+        if result_hash == old_result_hash:
+            return True
+        else:
+            return False
+
+create_test(TestHaloCompositionHashHOP, "halo_composition_test_hash_HOP", threshold=80.0)
+
+# Tests the content of the halos returned by the FOF halo finder on a dataset 
+# by comparing the hash of the arrays of all the particles contained in each
+# halo.
+class TestHaloCompositionHashFOF(YTStaticOutputTest):
+    threshold=80.0
+    
+    def run(self):
+        # Find the haloes using vanilla FOF.
+        haloes = FOFHaloFinder(self.pf, threshold=self.threshold, dm_only=False)
+        # The result is a list of the arrays of the particle IDs, for
+        # each halo
+        IDs = []
+        for halo in haloes:
+            IDs.append(halo["particle_index"])
+        self.result = IDs
+    
+    def compare(self, old_result):
+        # All the lists of arrays should be identical.  To check this
+        # faster, we take the 256-bit hash of these lists and compare them
+        result_hash = hashlib.sha256(self.result.tostring()).hexdigest()
+        old_result_hash = hashlib.sha256(old_result.tostring()).hexdigest()
+        if result_hash == old_result_hash:
+            return True
+        else:
+            return False
+
+create_test(TestHaloCompositionHashFOF, "halo_composition_test_hash_FOF", threshold=80.0)
+
+# Tests the content of the halos returned by the Parallel HOP halo finder on a 
+# dataset by comparing the hash of the arrays of all the particles contained 
+# in each halo.
+class TestHaloCompositionHashPHOP(YTStaticOutputTest):
+    threshold=80.0
+    
+    def run(self):
+        # Find the haloes using parallel HOP.
+        haloes = parallelHF(self.pf, threshold=self.threshold, dm_only=False)
+        # The result is a list of the arrays of the particle IDs, for
+        # each halo
+        IDs = []
+        for halo in haloes:
+            IDs.append(halo["particle_index"])
+        self.result = IDs
+    
+    def compare(self, old_result):
+        # All the lists of arrays should be identical.  To check this
+        # faster, we take the 256-bit hash of these lists and compare them
+        result_hash = hashlib.sha256(self.result.tostring()).hexdigest()
+        old_result_hash = hashlib.sha256(old_result.tostring()).hexdigest()
+        if result_hash == old_result_hash:
+            return True
+        else:
+            return False
+
+create_test(TestHaloCompositionHashPHOP, "halo_composition_test_hash_PHOP", threshold=80.0)
