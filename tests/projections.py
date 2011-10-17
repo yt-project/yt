e9f17ac2b00930638378e8f56ad2407b6babab97
Britton Smith <brittonsmith@gmail.com>
2011-10-17 12:06:45 -0400

    Added off-axis projection test.
    --HG--
    branch : yt

--- tests/projections.py
+++ tests/projections.py
@@ -1,7 +1,7 @@
 from yt.utilities.answer_testing.output_tests import \
     SingleOutputTest, create_test
 from yt.utilities.answer_testing.hydro_tests import \
-    TestProjection, TestGasDistribution
+    TestProjection, TestOffAxisProjection, TestGasDistribution
 from fields_to_test import field_list
 
 for axis in range(3):
@@ -9,6 +9,15 @@ for axis in range(3):
         create_test(TestProjection, "projection_test_%s_%s" % (axis, field),
                     field = field, axis = axis)
         create_test(TestProjection, "projection_test_%s_%s_Density" % (axis, field),
+                    field = field, axis = axis, weight_field = "Density")
+
+for axis in range(3):
+    for field in field_list:
+        create_test(TestOffAxisProjection, "off-axis_projection_test_%s_%s" % 
+                    (axis, field),
+                    field = field, axis = axis)
+        create_test(TestOffAxisProjection, "off-axis_projection_test_%s_%s_Density" % 
+                    (axis, field),
                     field = field, axis = axis, weight_field = "Density")
 
 for field in field_list:
