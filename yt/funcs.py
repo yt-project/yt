3b27b85e6933addc9af7bb8229345556a92daaa1
Britton Smith <brittonsmith@gmail.com>
2011-11-20 13:44:40 -0500

    Put fallback method for getting memory usage inside the if tree so it doesn't get called all the time.
    --HG--
    branch : yt

--- yt/funcs.py
+++ yt/funcs.py
@@ -125,14 +125,13 @@ def get_memory_usage():
     Returning resident size in megabytes
     """
     pid = os.getpid()
-    fallback = float(os.popen('ps -o rss= -p %d' % pid).read()) / 1024
     try:
         pagesize = resource.getpagesize()
     except NameError:
-        return fallback
+        return float(os.popen('ps -o rss= -p %d' % pid).read()) / 1024
     status_file = "/proc/%s/statm" % (pid)
     if not os.path.isfile(status_file):
-        return fallback
+        return float(os.popen('ps -o rss= -p %d' % pid).read()) / 1024
     line = open(status_file).read()
     size, resident, share, text, library, data, dt = [int(i) for i in line.split()]
     return resident * pagesize / (1024 * 1024) # return in megs
