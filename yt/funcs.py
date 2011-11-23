616efba36c572442f09c0dd515710e8e8e18ecf9
Matthew Turk <matthewturk@gmail.com>
2011-11-23 17:12:24 -0500

    Adding missing import
    --HG--
    branch : yt

--- yt/funcs.py
+++ yt/funcs.py
@@ -24,7 +24,7 @@ License:
 """
 
 import time, types, signal, inspect, traceback, sys, pdb, os
-import warnings, struct
+import warnings, struct, subprocess
 from math import floor, ceil
 
 from yt.utilities.exceptions import *
