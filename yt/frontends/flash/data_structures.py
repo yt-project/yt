47a1824f60071644abb2381a9fc557eea1bec191
Matthew Turk <matthewturk@gmail.com>
2012-01-18 13:28:09 -0500

    Adding a call that will initialize the FLASH conversion factor defaultdict for every field found in the file.
    --HG--
    branch : yt

--- yt/frontends/flash/data_structures.py
+++ yt/frontends/flash/data_structures.py
@@ -167,6 +167,8 @@ class FLASHHierarchy(AMRHierarchy):
                 continue
             available = na.all([f in self.field_list for f in fd.requested])
             if available: self.derived_field_list.append(field)
+        [self.parameter_file.conversion_factors[field] 
+         for field in self.field_list]
         for field in self.field_list:
             if field not in self.derived_field_list:
                 self.derived_field_list.append(field)
