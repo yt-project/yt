15d59d1e9e4a027ed82c07da2cb83dcc1f454fdd
Nathan Goldbaum <goldbaum@ucolick.org>
2012-01-24 13:19:30 -0600

    Fixes a bug I introduced in the parallel MosaicFisheyeCamera when using clip_ratio
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/camera.py
+++ yt/visualization/volume_rendering/camera.py
@@ -1138,7 +1138,7 @@ class MosaicFisheyeCamera(Camera):
                             final_image[i*nx:(i+1)*nx, j*ny:(j+1)*ny,:] = arr
                             del arr
                     if clip_ratio is not None:
-                        write_bitmap(final_image, fn, clip_ratio*final_image.max())
+                        write_bitmap(final_image, fn, clip_ratio*final_image.std())
                     else:
                         write_bitmap(final_image, fn)
                 else:
