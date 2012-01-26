31fde199154b5982ca40f3a484818d9b4b35cb79
Andrew Myers <atmyers@berkeley.edu>
2012-01-26 13:22:59 -0800

    merging in some changes I accidently overrode
    --HG--
    branch : yt

--- yt/frontends/orion/data_structures.py
+++ yt/frontends/orion/data_structures.py
@@ -341,7 +341,7 @@ class OrionHierarchy(AMRHierarchy):
         mylog.debug("Creating grid objects")
         self.grids = na.concatenate([level.grids for level in self.levels])
         self.grid_levels = na.concatenate([level.ngrids*[level.level] for level in self.levels])
-        self.grid_levels = self.grid_levels.reshape((self.num_grids,1))
+        self.grid_levels = na.array(self.grid_levels.reshape((self.num_grids,1)),dtype='int32')
         grid_dcs = na.concatenate([level.ngrids*[self.dx[level.level]] for level in self.levels],axis=0)
         self.grid_dxs = grid_dcs[:,0].reshape((self.num_grids,1))
         self.grid_dys = grid_dcs[:,1].reshape((self.num_grids,1))
@@ -521,6 +521,7 @@ class OrionStaticOutput(StaticOutput):
                 param, vals = map(strip,map(rstrip,line.split("=")))
             except ValueError:
                 mylog.error("ValueError: '%s'", line)
+                continue
             if orion2enzoDict.has_key(param):
                 paramName = orion2enzoDict[param]
                 t = map(parameterDict[paramName], vals.split())
@@ -541,7 +542,7 @@ class OrionStaticOutput(StaticOutput):
 
         self.parameters["TopGridRank"] = len(self.parameters["TopGridDimensions"])
         self.dimensionality = self.parameters["TopGridRank"]
-        self.domain_dimensions = self.parameters["TopGridDimensions"]
+        self.domain_dimensions = na.array(self.parameters["TopGridDimensions"],dtype='int32')
         self.refine_by = self.parameters["RefineBy"]
 
         if self.parameters.has_key("ComovingCoordinates") and bool(self.parameters["ComovingCoordinates"]):
