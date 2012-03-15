e518027da07b18324a8ff9d7ab27208eee70bfc3
Nathan Goldbaum <goldbaum@ucolick.org>
2012-03-14 19:41:33 -0700

    Backing out a change that accidentally made it into a PR.
    --HG--
    branch : yt

--- yt/frontends/flash/data_structures.py
+++ yt/frontends/flash/data_structures.py
@@ -379,7 +379,7 @@ class FLASHStaticOutput(StaticOutput):
     def _is_valid(self, *args, **kwargs):
         try:
             fileh = h5py.File(args[0],'r')
-            if "integer scalars" in fileh["/"].keys():
+            if "bounding box" in fileh["/"].keys():
                 fileh.close()
                 return True
             fileh.close()
