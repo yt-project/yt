3067e5df52497bb1e52db2f8dcc88ff8cc9440c3
Christopher Moody <cemoody@ucsc.edu>
2012-04-13 17:46:24 -0700

    updated to tip
    --HG--
    branch : yt

--- yt/frontends/art/setup.py
+++ yt/frontends/art/setup.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 import setuptools
-import os
-import sys
-import os.path
+import os, sys, os.path
 
-
-def configuration(parent_package='', top_path=None):
+def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
-    config = Configuration('art', parent_package, top_path)
-    config.make_config_py()  # installs __config__.py
+    config = Configuration('art',parent_package,top_path)
+    config.make_config_py() # installs __config__.py
     #config.make_svn_version_py()
     return config
