4ece1349ec128deccba30a0eaa7dc09d10b5654d
Sam Skillman <sam.skillman@gmail.com>
2012-06-04 18:33:52 -0600

    Adding InteractiveCamera to vr/api.py, and it works.
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/api.py
+++ yt/visualization/volume_rendering/api.py
@@ -39,4 +39,4 @@ from image_handling import export_rgba, import_rgba, \
                            plot_channel, plot_rgb
 from camera import Camera, PerspectiveCamera, StereoPairCamera, \
     off_axis_projection, FisheyeCamera, MosaicFisheyeCamera, \
-    HEALpixCamera
+    HEALpixCamera, InteractiveCamera
