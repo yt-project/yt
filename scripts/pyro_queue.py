2aafc98a5feaa9315f47fa6f0353a6766235b8c5
Britton Smith <brittonsmith@gmail.com>
2012-06-23 22:47:01 -0400

    Pyro server script now writes HMAC key to a file that only the user has permissions to.
    --HG--
    branch : yt

--- scripts/pyro_queue.py
+++ scripts/pyro_queue.py
@@ -1,3 +1,4 @@
+import os
 import Pyro4
 import uuid
 
@@ -14,10 +15,16 @@ my_rank = comm.comm.rank
 if my_rank == 0:
     my_q = PyroQueueRoot(comm)
     Pyro4.config.HMAC_KEY = uuid.uuid4().hex
-    print "HMAC KEY", Pyro4.config.HMAC_KEY
+    key_file = 'reason.key'
+    fd = os.open(key_file, os.O_CREAT, 0600)
+    os.close(fd)
+    out_file = file(key_file, 'w')
+    out_file.write("HMAC KEY: %s\n" % Pyro4.config.HMAC_KEY)
+    out_file.close()
+    mylog.info('See %s for HMAC key.', key_file)
     Pyro4.Daemon.serveSimple(
         {my_q: "yt.executor"},
-        ns=False)
+        ns=False, verbose=True)
 else:
     my_q = PyroQueueNonRoot(comm)
     my_q.run()
