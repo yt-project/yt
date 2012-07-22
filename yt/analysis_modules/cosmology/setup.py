5af0605d32f2108d748e304aab63681aa1aa4b69
Britton Smith <brittonsmith@gmail.com>
2012-07-21 20:28:42 -0400

    Adding setup for cosmology module.
    --HG--
    branch : yt

---
+++ yt/analysis_modules/cosmology/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import setuptools
+import os
+import sys
+import os.path
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('cosmology', parent_package, top_path)
+    config.make_config_py()  # installs __config__.py
+    #config.make_svn_version_py()
+    config.add_subpackage("light_cone")
+    return config
