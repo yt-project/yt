1c0f1c2eafe0c4a7ce4f6a23df5243bdc1c18071
Britton Smith <brittonsmith@gmail.com>
2012-07-21 20:23:29 -0400

    Removing light cone generator from setup.
    --HG--
    branch : yt

--- yt/analysis_modules/setup.py
+++ yt/analysis_modules/setup.py
@@ -17,7 +17,6 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage("hierarchy_subset")
     config.add_subpackage("level_sets")
     config.add_subpackage("light_ray")
-    config.add_subpackage("light_cone")
     config.add_subpackage("radial_column_density")
     config.add_subpackage("simulation_handler")
     config.add_subpackage("spectral_integrator")
