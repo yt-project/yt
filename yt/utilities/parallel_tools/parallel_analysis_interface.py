14938414ba21578cb3334618c63dcbfcf84d7e85
Matthew Turk <matthewturk@gmail.com>
2012-08-02 15:15:43 -0400

    This fixes not being able to do derived quantities.
    --HG--
    branch : yt-3.0

--- yt/utilities/parallel_tools/parallel_analysis_interface.py
+++ yt/utilities/parallel_tools/parallel_analysis_interface.py
@@ -430,13 +430,11 @@ def parallel_objects(objects, njobs = 0, storage = None, barrier = True,
             break
     if parallel_capable:
         communication_system.push_with_ids(all_new_comms[my_new_id].tolist())
-    obj_ids = na.arange(len(objects))
 
     to_share = {}
     # If our objects object is slice-aware, like time series data objects are,
     # this will prevent intermediate objects from being created.
-    oiter = itertools.izip(obj_ids[my_new_id::njobs],
-                           objects[my_new_id::njobs])
+    oiter = itertools.islice(enumerate(objects), my_new_id, None, njobs)
     for result_id, obj in oiter:
         if storage is not None:
             rstore = ResultsStorage()
