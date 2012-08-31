e0abde2af860ce926a852e541497728db4171223
Kacper Kowalik <xarthisius.kk@gmail.com>
2012-08-31 15:07:50 +0200

    Draw grid ids only on the highest available level of refinement
    --HG--
    branch : yt

--- yt/visualization/plot_modifications.py
+++ yt/visualization/plot_modifications.py
@@ -348,9 +348,13 @@ class GridBoundaryCallback(PlotCallback):
             plot._axes.hold(True)
             plot._axes.add_collection(grid_collection)
             if self.draw_ids:
-                ids = [g.id for g in plot.data._grids]
-                for n in visible.nonzero()[0]:
-                    plot._axes.text(left_edge_x[n]+(2*(xx1-xx0)/xpix),left_edge_y[n]+(2*(yy1-yy0)/ypix),ids[n],clip_on=True)
+                visible_gids = visible.nonzero()[0]
+                ids = [g.id for g in plot.data._grids
+                       if na.any(g.child_mask) and g.id in visible_gids]
+                for gid in ids:
+                    plot._axes.text(left_edge_x[gid] + (2 * (xx1 - xx0) / xpix),
+                                    left_edge_y[gid] + (2 * (yy1 - yy0) / ypix),
+                                    gid, clip_on=True)
             plot._axes.hold(False)
 
 class StreamlineCallback(PlotCallback):
