305d7857742b5845a737c28d831626cdb61bd5e1
Matthew Turk <matthewturk@gmail.com>
2012-10-19 08:01:53 -0400

    Actually testing for NaNs -- and fixing some imports, moving some files.
    --HG--
    branch : yt

--- yt/data_objects/tests/test_derived_quantities.py
+++ yt/data_objects/tests/test_derived_quantities.py
@@ -1,6 +1,4 @@
 from yt.testing import *
-from yt.data_objects.profiles import \
-    BinnedProfile1D, BinnedProfile2D, BinnedProfile3D
 import numpy as np
 
 def setup():
@@ -9,7 +7,8 @@ def setup():
 
 def test_extrema():
     for nprocs in [1, 2, 4, 8]:
-        pf = fake_random_pf(16, nprocs = nprocs)
+        pf = fake_random_pf(16, nprocs = nprocs, fields = ("Density",
+                "x-velocity", "y-velocity", "z-velocity"))
         sp = pf.h.sphere("c", (0.25, '1'))
         (mi, ma), = sp.quantities["Extrema"]("Density")
         yield assert_equal, mi, np.nanmin(sp["Density"])
@@ -18,3 +17,8 @@ def test_extrema():
         (mi, ma), = dd.quantities["Extrema"]("Density")
         yield assert_equal, mi, np.nanmin(dd["Density"])
         yield assert_equal, ma, np.nanmax(dd["Density"])
+        sp = pf.h.sphere("max", (0.25, '1'))
+        yield assert_equal, np.any(np.isnan(sp["RadialVelocity"])), True
+        (mi, ma), = dd.quantities["Extrema"]("RadialVelocity")
+        yield assert_equal, mi, np.nanmin(dd["RadialVelocity"])
+        yield assert_equal, ma, np.nanmax(dd["RadialVelocity"])
