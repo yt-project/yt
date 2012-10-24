989fc6c49aadd5fb080f5aace38cd766be70c93c
Matthew Turk <matthewturk@gmail.com>
2012-10-24 15:01:55 -0400

    Oops, could get too small of spheres, as well as we don't really want to double up our index of iteration.  Also, use vectors.
    --HG--
    branch : yt

--- yt/data_objects/tests/test_ellipsoid.py
+++ yt/data_objects/tests/test_ellipsoid.py
@@ -2,6 +2,7 @@ from yt.testing import *
 
 def setup():
     from yt.config import ytcfg
+    ytcfg["yt","loglevel"] = "50"
     ytcfg["yt","__withintesting"] = "True"
 
 def test_ellipsoid():
@@ -11,24 +12,25 @@ def test_ellipsoid():
           [0.8, 0.8, 0.8]]
     for nprocs in [1, 2, 4, 8]:
         pf = fake_random_pf(64, nprocs = nprocs)
+        min_dx = 2.0/pf.domain_dimensions
         ABC = np.random.random((3, 12)) * 0.1
         e0s = np.random.random((3, 12))
         tilts = np.random.random((3, 12))
         ABC[:,0] = 0.1
-
         for i in range(12):
             for c in cs:
                 A, B, C = reversed(sorted(ABC[:,i]))
+                A = max(A, min_dx[0])
+                B = max(B, min_dx[1])
+                C = max(C, min_dx[2])
                 e0 = e0s[:,i]
                 tilt = tilts[:,i]
                 ell = pf.h.ellipsoid(c, A, B, C, e0, tilt)
                 yield assert_equal, np.all(ell["Radius"] <= A), True
                 all_in = True
-                for i in xrange(ell["Radius"].size):
-                    pos = np.array([ell[ax][i] for ax in 'xyz'])
-                    v = 0.0
-                    v += (pos * ell._e0).sum() / ell._A
-                    v += (pos * ell._e1).sum() / ell._B
-                    v += (pos * ell._e2).sum() / ell._C
-                    all_in = (v <= 1.0) and all_in
-                yield assert_equal, all_in, True
+                pos = np.array([ell[ax] for ax in 'xyz'])
+                v  = np.zeros_like(ell["Radius"])
+                v += (pos * ell._e0[:,None]).sum(axis=0) / ell._A
+                v += (pos * ell._e1[:,None]).sum(axis=0) / ell._B
+                v += (pos * ell._e2[:,None]).sum(axis=0) / ell._C
+                yield assert_equal, np.all(v <= 1.0), True
