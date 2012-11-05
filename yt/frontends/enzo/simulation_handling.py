8c5aef3dc6db0a1f5f018be7b29c37302679ef85
Britton Smith <brittonsmith@gmail.com>
2012-11-05 16:24:13 -0500

    Enzo simulation time series only loads pfs that actually exist.
    --HG--
    branch : yt

--- yt/frontends/enzo/simulation_handling.py
+++ yt/frontends/enzo/simulation_handling.py
@@ -241,9 +241,13 @@ class EnzoSimulation(SimulationTimeSeries):
             if my_initial_time == my_times[my_indices[0] - 1]: my_indices[0] -= 1
             my_outputs = my_all_outputs[my_indices[0]:my_indices[1]]
 
-        TimeSeriesData.__init__(self, outputs=[output['filename'] for output in my_outputs],
-                                parallel=parallel)
-        mylog.info("%d outputs loaded into time series." % len(my_outputs))
+        init_outputs = []
+        for output in my_outputs:
+            if os.path.exists(output['filename']):
+                init_outputs.append(output['filename'])
+            
+        TimeSeriesData.__init__(self, outputs=init_outputs, parallel=parallel)
+        mylog.info("%d outputs loaded into time series." % len(init_outputs))
 
     def _parse_parameter_file(self):
         """
