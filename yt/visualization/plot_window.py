a80dc8eed9fb8a2affedcd0959a060729c23e5c5
Cameron Hummels <chummels@astro.columbia.edu>
2012-11-09 18:45:09 -0500

    Fixed a bug with pyparsing where it got renamed in matplotlib v1.2.0.
    --HG--
    branch : yt

--- yt/visualization/plot_window.py
+++ yt/visualization/plot_window.py
@@ -27,10 +27,18 @@ License:
 import base64
 import matplotlib.figure
 from matplotlib.mathtext import MathTextParser
+
+# Some magic for dealing with pyparsing being included or not
+# included in matplotlib (not in gentoo, yes in everything else)
+# Also accounting for the fact that in 1.2.0, pyparsing got renamed.
 try:
-    from matplotlib.pyparsing import ParseFatalException
+    if version.LooseVersion(matplotlib.__version__) < version.LooseVersion("1.2.0"):
+        from matplotlib.pyparsing import ParseFatalException
+    else:
+        from matplotlib.pyparsing_py2 import ParseFatalException
 except ImportError:
     from pyparsing import ParseFatalException
+
 import cStringIO
 import types
 import __builtin__
