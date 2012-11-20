8469f0aa2e7f299f80c0f06c8c9f65378716fbe1
John ZuHone <jzuhone@gmail.com>
2012-11-20 13:04:37 -0500

    Undoing a change that wasn't ready yet
    --HG--
    branch : yt

--- yt/utilities/initial_conditions.py
+++ yt/utilities/initial_conditions.py
@@ -67,47 +67,6 @@ class CoredSphere(FluidOperator):
             val = ((r[ind] - cr2) / (r2 - cr2))**0.5 * (outer_val - inner_val)
             grid[field][ind] = val + inner_val
 
-class BetaModelSphere(FluidOperator):
-    def __init__(self, beta, core_radius, radius, center, fields):
-        self.radius = radius
-        self.center = center
-        self.fields = fields
-        self.core_radius = core_radius
-        self.beta = beta
-        
-    def __call__(self, grid, sub_select = None):
-        r = np.zeros(grid.ActiveDimensions, dtype="float64")
-        r2 = self.radius**2
-        cr2 = self.core_radius**2
-        for i, ax in enumerate("xyz"):
-            np.add(r, (grid[ax] - self.center[i])**2.0, r)            
-        ind = (r <= r2)
-        if sub_select is not None:
-            ind &= sub_select            
-        for field, core_val in self.fields.iteritems() :
-            val = core_val*(1.+r[ind]/cr2)**(-1.5*self.beta)
-            grid[field][ind] = val
-
-class NFWModelSphere(FluidOperator):
-    def __init__(self, scale_radius, radius, center, fields):
-        self.radius = radius
-        self.center = center
-        self.fields = fields
-        self.scale_radius = scale_radius
-        
-    def __call__(self, grid, sub_select = None):
-        r = np.zeros(grid.ActiveDimensions, dtype="float64")
-        for i, ax in enumerate("xyz"):
-            np.add(r, (grid[ax] - self.center[i])**2.0, r)
-        np,sqrt(r,r)
-        ind = (r <= self.radius)
-        r /= scale.radius
-        if sub_select is not None:
-            ind &= sub_select
-        for field, scale_val in self.fields.iteritems() :
-            val = scale_val/(r[ind]*(1.+r[ind])**2)
-            grid[field][ind] = val
-            
 class RandomFluctuation(FluidOperator):
     def __init__(self, fields):
         self.fields = fields
