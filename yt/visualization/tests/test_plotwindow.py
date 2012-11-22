4776f4b07253e03d16b7aab500778b33d5746e37
Nathan Goldbaum <goldbaum@ucolick.org>
2012-11-21 17:12:32 -0800

    Only removing files created by the plotwindow tests.
    --HG--
    branch : yt

--- yt/visualization/tests/test_plotwindow.py
+++ yt/visualization/tests/test_plotwindow.py
@@ -7,20 +7,22 @@ def setup():
     from yt.config import ytcfg
     ytcfg["yt","__withintesting"] = "True"
 
-def teardown_func():
-    fns = glob.glob("*.png")
+def teardown_func(fns):
     for fn in fns:
         os.remove(fn)
 
 def test_plotwindow():
     pf = fake_random_pf(64)
+    fns = []
     for dim in [0,1,2]:
         slc = SlicePlot(pf, dim, 'Density')
+        fns.append(slc.save()[0])
         prj = ProjectionPlot(pf, dim, 'Density')
-        slc.save()
-        prj.save()
+        fns.append(prj.save()[0])
     normal = [1,1,1]
     oaslc = OffAxisSlicePlot(pf, normal, 'Density')
+    fns.append(oaslc.save()[0])
     oaprj = OffAxisProjectionPlot(pf, normal, 'Density')
-    teardown_func()
+    fns.append(oaprj.save()[0])
+    teardown_func(fns)
     
