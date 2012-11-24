ec05cf186698df89771dd83692f4cbc4524d7260
John ZuHone <jzuhone@gmail.com>
2012-11-23 23:28:14 -0500

    1) Adding CICSample_3, which performs the reverse operation of CICDeposit_3. 1st-order interpolation of values on the grid to particle positions. Also added a test which interpolates the grid coordinates to the particle positions. Since the coordinates vary linearly, the interpolation and the actual value of the particle coordinate should be identical in this case.
    2) Added a couple new fluid operators for the beta model and the NFW model, common fitting functions for galaxy cluster gas profiles and dark matter halo profiles.
    
    --HG--
    branch : yt

--- yt/utilities/lib/CICDeposit.pyx
+++ yt/utilities/lib/CICDeposit.pyx
@@ -117,6 +117,63 @@ def sample_field_at_positions(np.ndarray[np.float64_t, ndim=3] arr,
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
+def CICSample_3(np.ndarray[np.float64_t, ndim=1] posx,
+                np.ndarray[np.float64_t, ndim=1] posy,
+                np.ndarray[np.float64_t, ndim=1] posz,
+                np.ndarray[np.float64_t, ndim=1] sample,
+                np.int64_t npositions,
+                np.ndarray[np.float64_t, ndim=3] field,
+                np.ndarray[np.float64_t, ndim=1] leftEdge,
+                np.ndarray[np.int32_t, ndim=1] gridDimension,
+                np.float64_t cellSize):
+    
+    cdef int i1, j1, k1, n
+    cdef double xpos, ypos, zpos
+    cdef double fact, edge0, edge1, edge2
+    cdef double le0, le1, le2
+    cdef float dx, dy, dz, dx2, dy2, dz2
+    
+    edge0 = (<float> gridDimension[0]) - 0.5001
+    edge1 = (<float> gridDimension[1]) - 0.5001
+    edge2 = (<float> gridDimension[2]) - 0.5001
+    fact = 1.0 / cellSize
+    
+    le0 = leftEdge[0] 
+    le1 = leftEdge[1] 
+    le2 = leftEdge[2] 
+                                                    
+    for n in range(npositions):
+        
+        # Compute the position of the central cell
+        xpos = fmin(fmax((posx[n] - le0)*fact, 0.5001), edge0)
+        ypos = fmin(fmax((posy[n] - le1)*fact, 0.5001), edge1)
+        zpos = fmin(fmax((posz[n] - le2)*fact, 0.5001), edge2)
+        
+        i1  = <int> (xpos + 0.5)
+        j1  = <int> (ypos + 0.5)
+        k1  = <int> (zpos + 0.5)
+        
+        # Compute the weights
+        dx = (<float> i1) + 0.5 - xpos
+        dy = (<float> j1) + 0.5 - ypos
+        dz = (<float> k1) + 0.5 - zpos
+        dx2 =  1.0 - dx
+        dy2 =  1.0 - dy
+        dz2 =  1.0 - dz
+
+        # Interpolate from field onto the particle
+        sample[n] = (field[i1-1,j1-1,k1-1] * dx  * dy  * dz +
+                     field[i1  ,j1-1,k1-1] * dx2 * dy  * dz +
+                     field[i1-1,j1  ,k1-1] * dx  * dy2 * dz +
+                     field[i1  ,j1  ,k1-1] * dx2 * dy2 * dz +
+                     field[i1-1,j1-1,k1  ] * dx  * dy  * dz2 +
+                     field[i1  ,j1-1,k1  ] * dx2 * dy  * dz2 +
+                     field[i1-1,j1  ,k1  ] * dx  * dy2 * dz2 +
+                     field[i1  ,j1  ,k1  ] * dx2 * dy2 * dz2)
+                                                                                                                        
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
 def assign_particles_to_cells(np.ndarray[np.int32_t, ndim=1] levels, #for cells
                               np.ndarray[np.float32_t, ndim=2] left_edges, #many cells
                               np.ndarray[np.float32_t, ndim=2] right_edges,
