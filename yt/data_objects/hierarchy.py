ef87ce4c4af1e58cffbea9cc8d22b3610dca66c2
Sam Skillman <sam.skillman@gmail.com>
2012-11-29 16:47:01 -0700

    Use overlap_proj for dim<3 simulations.
    --HG--
    branch : yt

--- yt/data_objects/hierarchy.py
+++ yt/data_objects/hierarchy.py
@@ -136,6 +136,12 @@ class AMRHierarchy(ObjectFindingMixin, ParallelAnalysisInterface):
             mylog.warning("Refine by something other than two: reverting to"
                         + " overlap_proj")
             self.proj = self.overlap_proj
+        if self.pf.dimensionality < 3 and hasattr(self, 'proj') and \
+            hasattr(self, 'overlap_proj'):
+            mylog.warning("Dimensionality less than 3: reverting to"
+                        + " overlap_proj")
+            self.proj = self.overlap_proj
+
         self.object_types.sort()
 
     def _setup_unknown_fields(self):
