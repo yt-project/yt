5890d2aa5798cc9234b0b7356f9f822ec60bac91
Cameron Hummels <chummels@astro.columbia.edu>
2012-11-28 18:02:26 -0700

    Adding in support for a run-time tolerance (for comparison precision) to be set in the enzo testing suite.
    --HG--
    branch : yt

--- yt/config.py
+++ yt/config.py
@@ -59,6 +59,8 @@ ytcfgDefaults = dict(
     hub_url = 'https://hub.yt-project.org/upload',
     hub_api_key = '',
     ipython_notebook = 'False',
+    answer_testing_tolerance = '3',
+    answer_testing_bitwise = 'False'
     )
 # Here is the upgrade.  We're actually going to parse the file in its entirety
 # here.  Then, if it has any of the Forbidden Sections, it will be rewritten
