9676005bda9bbff50d7da7bc660c76ec5e7bfb1e
Nathan Goldbaum <goldbaum@ucolick.org>
2012-12-17 10:12:35 -0800

    Adding tests for direct png, ps, eps, and pdf plot window plots.
    --HG--
    branch : yt

--- yt/visualization/tests/test_plotwindow.py
+++ yt/visualization/tests/test_plotwindow.py
@@ -8,21 +8,36 @@ def setup():
     ytcfg["yt","__withintesting"] = "True"
 
 def teardown_func(fns):
-    for fn in fns:
+    for fn in np.unique(fns):
         os.remove(fn)
+
+def assert_fn(fn, fns):
+    if fn is not None:
+        print fn, fns[-1]
+        assert fn == fns[-1]
 
 def test_plotwindow():
     pf = fake_random_pf(64)
     fns = []
-    for dim in [0,1,2]:
-        slc = SlicePlot(pf, dim, 'Density')
-        fns.append(slc.save()[0])
-        prj = ProjectionPlot(pf, dim, 'Density')
-        fns.append(prj.save()[0])
-    normal = [1,1,1]
-    oaslc = OffAxisSlicePlot(pf, normal, 'Density')
-    fns.append(oaslc.save()[0])
-    oaprj = OffAxisProjectionPlot(pf, normal, 'Density')
-    fns.append(oaprj.save()[0])
+    test_flnms = [None, 'test.png', 'test.eps', 
+                  'test.ps', 'test.pdf']
+    for fn in test_flnms:
+        for dim in [0,1,2]:
+            slc = SlicePlot(pf, dim, 'Density')
+            fns.append(slc.save(fn)[0])
+            assert_fn(fn, fns)
+
+            prj = ProjectionPlot(pf, dim, 'Density')
+            fns.append(prj.save(fn)[0])
+            assert_fn(fn, fns)
+        normal = [1,1,1]
+        oaslc = OffAxisSlicePlot(pf, normal, 'Density')
+        fns.append(oaslc.save(fn)[0])
+        assert_fn(fn,fns)
+
+        oaprj = OffAxisProjectionPlot(pf, normal, 'Density')
+        fns.append(oaprj.save(fn)[0])
+        assert_fn(fn, fns)
+    
     teardown_func(fns)
     
