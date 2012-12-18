c37d762cac04029e444c3fe8ec9d38428c52ec4e
Sam Leitner <sam.leitner@gmail.com>
2012-12-18 00:13:40 -0500

    fix to iterating pos if dimensions are not all the same.
    --HG--
    branch : yt-3.0

--- yt/geometry/selection_routines.pyx
+++ yt/geometry/selection_routines.pyx
@@ -26,7 +26,7 @@ License:
 import numpy as np
 cimport numpy as np
 cimport cython
-from stdlib cimport malloc, free
+from libc.stdlib cimport malloc, free
 from fp_utils cimport fclip, iclip
 from cython.parallel import prange, parallel, threadid
 from selection_routines cimport SelectorObject
@@ -159,9 +159,9 @@ cdef class SelectorObject:
                     self.recursively_select_octs(
                         octree.root_mesh[i][j][k],
                         pos, dds, mask)
-                    pos[2] += dds[2]
-                pos[1] += dds[2]
-            pos[0] += dds[2]
+                    pos[2] += dds[2] 
+                pos[1] += dds[1]
+            pos[0] += dds[0] 
         return mask.astype("bool")
 
     @cython.boundscheck(False)
