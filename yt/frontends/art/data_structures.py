removed yt-2.4 particle references updated to correct density units
--HG--
branch : yt-3.0

yt/frontends/art/data_structures.py
@@ -52,19 +52,13 @@ import yt.utilities.lib as amr_utils
 
 from .definitions import *
 from io import _read_child_mask_level
-from io import read_particles
-from io import read_stars
-from io import spread_ages
 from io import _count_art_octs
 from io import _read_art_level_info
-from io import _read_art_child
 from io import _skip_record
 from io import _read_record
 from io import _read_frecord
-from io import _read_record_size
 from io import _read_struct
 from io import b2t
-
 
 import yt.frontends.ramses._ramses_reader as _ramses_reader
 
@@ -84,22 +78,19 @@ class ARTGrid(AMRGridPatch):
     _id_offset = 0
 
     def __init__(self, id, hierarchy, level, locations,start_index, le,re,gd,
-            child_mask=None,nop=0):
+            child_mask=None):
         AMRGridPatch.__init__(self, id, filename = hierarchy.hierarchy_filename,
                               hierarchy = hierarchy)
         start_index =start_index 
+        self.id = id
         self.Level = level
         self.Parent = []
         self.Children = []
         self.locations = locations
         self.start_index = start_index.copy()
-        
         self.LeftEdge = le
         self.RightEdge = re
         self.ActiveDimensions = gd
-        self.NumberOfParticles=nop
-        for particle_field in particle_fields:
-            setattr(self,particle_field,np.array([]))
 
     def _setup_dx(self):
         id = self.id - self._id_offset
@@ -329,67 +320,6 @@ class ARTHierarchy(AMRHierarchy):
                     start_index,le,re,gd))
                 gi += 1
         self.grids = np.empty(len(grids), dtype='object')
-        if not self.pf.skip_particles and self.pf.file_particle_data:
-            lspecies = self.pf.parameters['lspecies']
-            wspecies = self.pf.parameters['wspecies']
-            self.pf.particle_position,self.pf.particle_velocity= \
-                read_particles(self.pf.file_particle_data,
-                        self.pf.parameters['Nrow'])
-            nparticles = lspecies[-1]
-            if not np.all(self.pf.particle_position[nparticles:]==0.0):
-                mylog.info('WARNING: unused particles discovered from lspecies')
-            self.pf.particle_position = self.pf.particle_position[:nparticles]
-            self.pf.particle_velocity = self.pf.particle_velocity[:nparticles]
-            self.pf.particle_position  /= self.pf.domain_dimensions 
-            self.pf.particle_type = np.zeros(nparticles,dtype='int')
-            self.pf.particle_mass = np.zeros(nparticles,dtype='float64')
-            self.pf.particle_star_index = len(wspecies)-1
-            a=0
-            for i,(b,m) in enumerate(zip(lspecies,wspecies)):
-                if i == self.pf.particle_star_index:
-                    assert m==0.0
-                    sa,sb = a,b
-                else:
-                    assert m>0.0
-                self.pf.particle_type[a:b] = i #particle type
-                self.pf.particle_mass[a:b] = m #mass in code units
-                a=b
-            if not self.pf.skip_stars and self.pf.file_particle_stars: 
-                (nstars_rs,), mass, imass, tbirth, metallicity1, metallicity2, \
-                        ws_old,ws_oldi,tdum,adum \
-                     = read_stars(self.pf.file_particle_stars)
-                self.pf.nstars_rs = nstars_rs     
-                self.pf.nstars_pa = b-a
-                inconsistent=self.pf.particle_type==self.pf.particle_star_index
-                if not nstars_rs==np.sum(inconsistent):
-                    mylog.info('WARNING!: nstars is inconsistent!')
-                del inconsistent
-                if nstars_rs > 0 :
-                    n=min(1e2,len(tbirth))
-                    birthtimes= b2t(tbirth,n=n)
-                    birthtimes = birthtimes.astype('float64')
-                    assert birthtimes.shape == tbirth.shape    
-                    birthtimes*= 1.0e9 #from Gyr to yr
-                    birthtimes*= 365*24*3600 #to seconds
-                    ages = self.pf.current_time-birthtimes
-                    spread = self.pf.spread_age
-                    if type(spread)==type(5.5):
-                        ages = spread_ages(ages,spread=spread)
-                    elif spread:
-                        ages = spread_ages(ages)
-                    idx = self.pf.particle_type == self.pf.particle_star_index
-                    for psf in particle_star_fields:
-                        if getattr(self.pf,psf,None) is None:
-                            setattr(self.pf,psf,
-                                    np.zeros(nparticles,dtype='float64'))
-                    self.pf.particle_age[sa:sb] = ages
-                    self.pf.particle_mass[sa:sb] = mass
-                    self.pf.particle_mass_initial[sa:sb] = imass
-                    self.pf.particle_creation_time[sa:sb] = birthtimes
-                    self.pf.particle_metallicity1[sa:sb] = metallicity1
-                    self.pf.particle_metallicity2[sa:sb] = metallicity2
-                    self.pf.particle_metallicity[sa:sb]  = metallicity1\
-                                                          + metallicity2
         for gi,g in enumerate(grids):    
             self.grids[gi]=g
                     
@@ -412,7 +342,7 @@ class ARTHierarchy(AMRHierarchy):
                                 self.grid_levels, mask)
             parents = self.grids[mask.astype("bool")]
             if len(parents) > 0:
-                g.Parent.extend((p for p in parents.tolist()
+                g.Parent.extend((p for p in parents.tolist() \ 
                         if p.locations[0,0] == g.locations[0,0]))
                 for p in parents: p.Children.append(g)
             #Now we do overlapping siblings; note that one has to "win" with
@@ -429,22 +359,6 @@ class ARTHierarchy(AMRHierarchy):
             g._prepare_grid()
             g._setup_dx()
             #instead of gridding particles assign them all to the root grid
-            if gi==0:
-                for particle_field in particle_fields:
-                    source = getattr(self.pf,particle_field,None)
-                    if source is None:
-                        for i,ax in enumerate('xyz'):
-                            pf = particle_field.replace('_%s'%ax,'')
-                            source = getattr(self.pf,pf,None)
-                            if source is not None:
-                                source = source[:,i]
-                                break
-                    if source is not None:
-                        mylog.info("Attaching %s to the root grid",
-                                    particle_field)
-                        g.NumberOfParticles = source.shape[0]
-                        setattr(g,particle_field,source)
-                g.particle_index = np.arange(g.NumberOfParticles)
         pb.finish()
         self.max_level = self.grid_levels.max()
 
@@ -481,7 +395,8 @@ class ARTStaticOutput(StaticOutput):
     _fieldinfo_known = KnownARTFields
     
     def __init__(self, file_amr, storage_filename = None,
-            skip_particles=False,skip_stars=False,limit_level=None,
+            skip_particles=False,skip_stars=False,
+            limit_level=None,
             spread_age=True,data_style='art'):
         self.data_style = data_style
         self._find_files(file_amr)
@@ -505,22 +420,34 @@ class ARTStaticOutput(StaticOutput):
         affix = os.path.basename(file_amr).replace(prefix,'')
         affix = affix.replace(suffix,'')
         affix = affix.replace('_','')
+        full_affix = affix
         affix = affix[1:-1]
         dirname = os.path.dirname(file_amr)
-        for filetype, pattern in filename_pattern.items():
-            #sometimes the affix is surrounded by an extraneous _
-            #so check for an extra character on either side
-            check_filename = dirname+'/'+pattern%('?%s?'%affix)
-            filenames = glob.glob(check_filename)
-            if len(filenames)==1:
-                setattr(self,"file_"+filetype,filenames[0])
-                mylog.info('discovered %s',filetype)
-            elif len(filenames)>1:
-                setattr(self,"file_"+filetype,None)
-                mylog.info("Ambiguous number of files found for %s",
-                        check_filename)
-            else:
-                setattr(self,"file_"+filetype,None)
+        for fp in (filename_pattern_hf,filename_pattern):
+            for filetype, pattern in fp.items():
+                #if this attribute is already set skip it
+                if getattr(self,"file_"+filetype,None) is not None:
+                    continue
+                #sometimes the affix is surrounded by an extraneous _
+                #so check for an extra character on either side
+                check_filename = dirname+'/'+pattern%('?%s?'%affix)
+                filenames = glob.glob(check_filename)
+                if len(filenames)>1:
+                    check_filename_strict = \
+                            dirname+'/'+pattern%('?%s'%full_affix[1:])
+                    filenames = glob.glob(check_filename_strict)
+
+                if len(filenames)==1:
+                    setattr(self,"file_"+filetype,filenames[0])
+                    mylog.info('discovered %s:%s',filetype,filenames[0])
+                elif len(filenames)>1:
+                    setattr(self,"file_"+filetype,None)
+                    mylog.info("Ambiguous number of files found for %s",
+                            check_filename)
+                    for fn in filenames:
+                        faffix = float(affix)
+                else:
+                    setattr(self,"file_"+filetype,None)
 
     def __repr__(self):
         return self.basename.rsplit(".", 1)[0]
@@ -567,7 +494,7 @@ class ARTStaticOutput(StaticOutput):
         #v0 =  r0 * 50.0*1.0e5 * np.sqrt(self.omega_matter)  #cm/s
         v0 = 50.0*r0*np.sqrt(Om0)
         t0 = r0/v0
-        #rho0 = 1.8791e-29 * hubble**2.0 * self.omega_matter
+        rho1 = 1.8791e-29 * hubble**2.0 * self.omega_matter
         rho0 = 2.776e11 * hubble**2.0 * Om0
         tr = 2./3. *(3.03e5*r0**2.0*wmu*self.omega_matter)*(1.0/(aexpn**2))     
         aM0 = rho0 * (boxh/hubble)**3.0 / ng**3.0
@@ -578,6 +505,7 @@ class ARTStaticOutput(StaticOutput):
         cf['v0']=v0
         cf['t0']=t0
         cf['rho0']=rho0
+        cf['rho1']=rho1
         cf['tr']=tr
         cf['aM0']=aM0
 
@@ -585,7 +513,7 @@ class ARTStaticOutput(StaticOutput):
         cf['Pressure'] = P0 #already cgs
         cf['Velocity'] = v0/aexpn*1.0e5 #proper cm/s
         cf["Mass"] = aM0 * 1.98892e33
-        cf["Density"] = rho0*(aexpn**-3.0)
+        cf["Density"] = rho1*(aexpn**-3.0)
         cf["GasEnergy"] = rho0*v0**2*(aexpn**-5.0)
         cf["Potential"] = 1.0
         cf["Entropy"] = S_0
@@ -597,12 +525,8 @@ class ARTStaticOutput(StaticOutput):
             self.conversion_factors[particle_field] =  1.0
         for ax in 'xyz':
             self.conversion_factors["%s-velocity" % ax] = 1.0
-            self.conversion_factors["particle_velocity_%s"%ax] = cf['Velocity']
         for unit in sec_conversion.keys():
             self.time_units[unit] = 1.0 / sec_conversion[unit]
-        self.conversion_factors['particle_mass'] = cf['Mass']
-        self.conversion_factors['particle_creation_time'] =  31556926.0
-        self.conversion_factors['Msun'] = 5.027e-34 
 
     def _parse_parameter_file(self):
         """

yt/frontends/art/fields.py
@@ -172,7 +172,7 @@ def _temperature(field, data):
     dg /= data.pf.conversion_factors["GasEnergy"]
     dd = data["Density"].astype('float64')
     dd /= data.pf.conversion_factors["Density"]
-    tr = dg/dd*data.pf.tr
+    tr = dg/dd*data.pf.conversion_factors['tr']
     #ghost cells have zero density?
     tr[np.isnan(tr)] = 0.0
     #dd[di] = -1.0
@@ -242,64 +242,3 @@ ARTFieldInfo["Metal_Density"]._projected_units = r""
 
 
 #Particle fields
-
-def ParticleMass(field,data):
-    return data['particle_mass']
-add_field("ParticleMass",function=ParticleMass,units=r"\rm{g}",particle_type=True)
-
-
-#Derived particle fields
-
-def ParticleMassMsun(field,data):
-    return data['particle_mass']*data.pf['Msun']
-add_field("ParticleMassMsun",function=ParticleMassMsun,units=r"\rm{g}",particle_type=True)
-
-def _creation_time(field,data):
-    pa = data["particle_age"]
-    tr = np.zeros(pa.shape,dtype='float')-1.0
-    tr[pa>0] = pa[pa>0]
-    return tr
-add_field("creation_time",function=_creation_time,units=r"\rm{s}",particle_type=True)
-
-def mass_dm(field, data):
-    tr = np.ones(data.ActiveDimensions, dtype='float32')
-    idx = data["particle_type"]<5
-    #make a dumb assumption that the mass is evenly spread out in the grid
-    #must return an array the shape of the grid cells
-    if np.sum(idx)>0:
-        tr /= np.prod(data['CellVolumeCode']*data.pf['mpchcm']**3.0) #divide by the volume
-        tr *= np.sum(data['particle_mass'][idx])*data.pf['Msun'] #Multiply by total contaiend mass
-        print tr.shape
-        return tr
-    else:
-        return tr*1e-9
-
-add_field("particle_cell_mass_dm", function=mass_dm, units = r"\mathrm{M_{sun}}",
-        validators=[ValidateSpatial(0)],        
-        take_log=False,
-        projection_conversion="1")
-
-def _spdensity(field, data):
-    grid_mass = np.zeros(data.ActiveDimensions, dtype='float32')
-    if data.star_mass.shape[0] ==0 : return grid_mass 
-    amr_utils.CICDeposit_3(data.star_position_x,
-                           data.star_position_y,
-                           data.star_position_z,
-                           data.star_mass.astype('float32'),
-                           data.star_mass.shape[0],
-                           grid_mass, 
-                           np.array(data.LeftEdge).astype(np.float64),
-                           np.array(data.ActiveDimensions).astype(np.int32), 
-                           np.float64(data['dx']))
-    return grid_mass 
-
-#add_field("star_density", function=_spdensity,
-#          validators=[ValidateSpatial(0)], convert_function=_convertDensity)
-
-def _simple_density(field,data):
-    mass = np.sum(data.star_mass)
-    volume = data['dx']*data.ActiveDimensions.prod().astype('float64')
-    return mass/volume
-
-add_field("star_density", function=_simple_density,
-          validators=[ValidateSpatial(0)], convert_function=_convertDensity)

yt/frontends/art/io.py
@@ -58,16 +58,13 @@ class IOHandlerART(BaseIOHandler):
                         0   - there are no children; the cell is a leaf
             iOctNb :    >0   - pointers to neighbouring cells 
             iOctPs :         - coordinates of Oct centers
-            
             iOctLL1:         - doubly linked list of octs
             iOctLL2:         - doubly linked list of octs
-            
             tl - current  time moment for level L
             tlold - previous time moment for level L
             dtl - dtime0/2**iTimeBin
             dtlold -  previous time step for level L
             iSO - sweep order
-            
             hvar(1,*) - gas density 
             hvar(2,*) - gas energy 
             hvar(3,*) - x-momentum 
@@ -76,13 +73,9 @@ class IOHandlerART(BaseIOHandler):
             hvar(6,*) - pressure
             hvar(7,*) - Gamma
             hvar(8,*) - internal energy 
-
             var (1,*) - total density 
             var (2,*) - potential (new)
             var (3,*) - potential (old)
-            
-            
-            
         """
         
         if level in self.level_data: return
@@ -120,18 +113,6 @@ class IOHandlerART(BaseIOHandler):
     def clear_level(self, level):
         self.level_data.pop(level, None)
 
-    def _read_particle_field(self, grid, field):
-        dat = getattr(grid,field,None)
-        if dat is not None: 
-            return dat
-        starfield = field.replace('star','particle')
-        dat = getattr(grid,starfield,None)
-        if dat is not None:
-            psi = grid.pf.particle_star_index
-            idx = grid.particle_type==psi
-            return dat[idx]
-        raise KeyError
-        
     def _read_data_set(self, grid, field):
         if field in particle_fields:
             return self._read_particle_field(grid, field)
@@ -292,21 +273,15 @@ def read_particles(file,Nrow):
     data = np.squeeze(np.dstack(pages)).T # x,y,z,vx,vy,vz
     return data[:,0:3],data[:,3:]
 
-def read_stars(file):
-    fh = open(file,'rb')
-    tdum,adum   = _read_frecord(fh,'>d')
-    nstars      = _read_frecord(fh,'>i')
-    ws_old, ws_oldi = _read_frecord(fh,'>d')
-    mass    = _read_frecord(fh,'>f') 
-    imass   = _read_frecord(fh,'>f') 
-    tbirth  = _read_frecord(fh,'>f') 
-    if fh.tell() < os.path.getsize(file):
-        metallicity1 = _read_frecord(fh,'>f') 
-    if fh.tell() < os.path.getsize(file):
-        metallicity2 = _read_frecord(fh,'>f')     
-    assert fh.tell() == os.path.getsize(file)
-    return  nstars, mass, imass, tbirth, metallicity1, metallicity2,\
-            ws_old,ws_oldi,tdum,adum
+def read_star_field(file,field=None):
+    data = {}
+    with open(file,'rb') as fh:
+        for dtype, variables in star_struct:
+            if field in variables or dtype=='>d' or dtype=='>d':
+                data[field] = _read_frecord(fh,'>f')
+            else:
+                _skip_record(fh)
+    return data.pop(field),data
 
 def _read_child_mask_level(f, level_child_offsets,level,nLevel,nhydro_vars):
     f.seek(level_child_offsets[level])
@@ -475,9 +450,8 @@ def spread_ages(ages,logger=None,spread=1.0e7*365*24*3600):
         #lage=rage
         if logger: logger(i)
     #we didn't get the last iter
-    i=ages.shape[0]-1
-    n = i-lidx #n stars affected
-    rage = ages[i]
+    n = agesd.shape[0]-lidx
+    rage = ages[-1]
     lage = max(rage-spread,0.0)
-    agesd[lidx:i]=np.linspace(lage,rage,n)
+    agesd[lidx:]=np.linspace(lage,rage,n)
     return agesd