f751ea83ea03be854aa91eb499dcf685d761e825
Sam Skillman <samskillman@gmail.com>
2013-02-17 18:27:47 -0700

    cam.save_image should rescale by default.  Otherwise images are very transparent.
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/camera.py
+++ yt/visualization/volume_rendering/camera.py
@@ -603,9 +603,9 @@ class Camera(ParallelAnalysisInterface):
     def save_image(self, image, fn=None, clip_ratio=None, transparent=False):
         if self.comm.rank == 0 and fn is not None:
             if transparent:
-                image.write_png(fn, clip_ratio=clip_ratio, rescale=False)
+                image.write_png(fn, clip_ratio=clip_ratio, rescale=True)
             else:
-                image.write_png(fn, clip_ratio=clip_ratio, rescale=False,
+                image.write_png(fn, clip_ratio=clip_ratio, rescale=True,
                                 background='black')
 
     def initialize_source(self):
