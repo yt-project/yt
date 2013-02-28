ec0e4869d6a32f2b6d4e05db79a78c9a0f68977c
Nathan Goldbaum <goldbaum@ucolick.org>
2013-02-27 22:51:58 -0800

    Making the Radius fields more memory efficient.
    --HG--
    branch : yt

--- yt/data_objects/universal_fields.py
+++ yt/data_objects/universal_fields.py
@@ -791,22 +791,23 @@ add_field("ParticleAngularMomentumZ", function=_ParticleAngularMomentumZ,
          units=r"\rm{g}\/\rm{cm}^2/\rm{s}", particle_type=True,
          validators=[ValidateParameter('center')])
 
-def get_radius(positions, data):
-    c = data.get_field_parameter("center")
-    n_tup = tuple([1 for i in range(positions.ndim-1)])
-    center = np.tile(np.reshape(c, (positions.shape[0],)+n_tup),(1,)+positions.shape[1:])
-    periodicity = data.pf.periodicity
-    if any(periodicity):
-        period = data.pf.domain_right_edge - data.pf.domain_left_edge
-        return periodic_dist(positions, center, period, periodicity)
-    else:
-        return euclidean_dist(positions, center)
+def get_radius(data, field_prefix):
+    center = data.get_field_parameter("center")
+    DW = data.pf.domain_right_edge - data.pf.domain_left_edge
+    radius = np.zeros(data[field_prefix+"x"].shape, dtype='float64')
+    for i, ax in enumerate('xyz'):
+        r = np.abs(data[field_prefix+"%s" % ax] - center[i])
+        if data.pf.periodicity[i] == True:
+            radius += np.minimum(r, np.abs(DW[i]-r))**2.0
+        else:
+            radius += r**2.0
+    np.sqrt(radius, radius)
+    return radius
+
 def _ParticleRadius(field, data):
-    positions = np.array([data["particle_position_%s" % ax] for ax in 'xyz'])
-    return get_radius(positions, data)
+    return get_radius(data, "particle_position_")
 def _Radius(field, data):
-    positions = np.array([data['x'], data['y'], data['z']])
-    return get_radius(positions, data)
+    return get_radius(data, "")
 
 def _ConvertRadiusCGS(data):
     return data.convert("cm")
