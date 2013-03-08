83907d5597e7a1ac63cc0e70fbed66a009671f90
Christopher Moody <cemoody@ucsc.edu>
2013-03-08 15:20:54 -0800

    answer testing update
    --HG--
    branch : yt-3.0

--- yt/frontends/art/tests/test_outputs.py
+++ yt/frontends/art/tests/test_outputs.py
@@ -31,15 +31,15 @@ from yt.utilities.answer_testing.framework import \
     data_dir_load
 from yt.frontends.art.api import ARTStaticOutput
 
-_fields = ("Density","particle_mass")
+_fields = ("Density","particle_mass",("all","particle_position_x"))
 
 sfg1 = "10MpcBox_csf512_a0.330.d"
-@requires_pf(sfg1)
+@requires_pf(sfg1,big_data=True)
 def test_sfg1():
     pf = data_dir_load(sfg1)
-    yield assert_equal, str(pf), "sfg1"
+    yield assert_equal, str(pf), "10MpcBox_csf512_a0.330.d"
     dso = [ None, ("sphere", ("max", (0.1, 'unitary')))]
-    for field in fields:
+    for field in _fields:
         for axis in [0, 1, 2]:
             for ds in dso:
                 for weight_field in [None, "Density"]:
