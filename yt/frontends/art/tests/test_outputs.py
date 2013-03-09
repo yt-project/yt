c85bb6f40385c278dc904cfa730f1af683b5a969
Christopher Moody <cemoody@ucsc.edu>
2013-03-09 12:28:29 -0800

    autopep8 on tests
    --HG--
    branch : yt-3.0

--- yt/frontends/art/tests/test_outputs.py
+++ yt/frontends/art/tests/test_outputs.py
@@ -31,14 +31,16 @@ from yt.utilities.answer_testing.framework import \
     data_dir_load
 from yt.frontends.art.api import ARTStaticOutput
 
-_fields = ("Density","particle_mass",("all","particle_position_x"))
+_fields = ("Density", "particle_mass", ("all", "particle_position_x"))
 
 sfg1 = "10MpcBox_csf512_a0.330.d"
-@requires_pf(sfg1,big_data=True)
+
+
+@requires_pf(sfg1, big_data=True)
 def test_sfg1():
     pf = data_dir_load(sfg1)
     yield assert_equal, str(pf), "10MpcBox_csf512_a0.330.d"
-    dso = [ None, ("sphere", ("max", (0.1, 'unitary')))]
+    dso = [None, ("sphere", ("max", (0.1, 'unitary')))]
     for field in _fields:
         for axis in [0, 1, 2]:
             for ds in dso:
