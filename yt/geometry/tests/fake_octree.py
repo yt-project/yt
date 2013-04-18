2cbfe27e509a5a6b90ee7a8f00da227b4d3f529a
Christopher Moody <juxtaposicion@gmail.com>
2013-04-17 17:57:04 -0700

    tests are ok… need to stop adding octs on a # leafs condition, not a # of octs condition
    --HG--
    branch : yt-3.0

--- yt/geometry/tests/fake_octree.py
+++ yt/geometry/tests/fake_octree.py
@@ -8,18 +8,23 @@ dn = 2
 dd = np.ones(3,dtype='i4')*dn
 dle = np.ones(3,dtype='f8')*0.0
 dre = np.ones(3,dtype='f8')
-fsub = 0.10
-domain = 0
+fsub = 0.25
+domain = 1
 
 oct_handler = RAMSESOctreeContainer(dd,dle,dre)
 create_fake_octree(oct_handler, nocts, max_level, dd, dle, dre, fsub)
 print "filled"
-print oct_handler.check(domain, print_all=1)
-mask = np.ones(nocts,dtype='bool')
-print mask
+print oct_handler.check(1, print_all=1)
+mask = np.ones((nocts,8),dtype='bool')
 cell_count = nocts*8
-level_counts = np.array([nocts]) # not used anyway
-fc = oct_handler.fcoords(domain,mask,cell_count)
+level_counts = oct_handler.count_levels(max_level, 1, mask)
+print level_counts
+print "fcoords"
+fc = oct_handler.fcoords(domain,mask,cell_count,level_counts)
+print level_counts, level_counts.sum()
+print [np.unique(fc[:,ax]).shape[0] for ax in range(3)]
 print fc
 print fc.shape
+import pdb; pdb.set_trace()
 
+#Now take the particles and recreate the same octree
