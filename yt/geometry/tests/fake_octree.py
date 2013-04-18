b1929aa7405c22bfa612f6cf47ee5f7573581c57
Christopher Moody <cemoody@ucsc.edu>
2013-04-17 19:22:35 -0700

    cleaned; fixed level counts
    --HG--
    branch : yt-3.0

--- yt/geometry/tests/fake_octree.py
+++ yt/geometry/tests/fake_octree.py
@@ -13,18 +13,10 @@ domain = 1
 
 oct_handler = RAMSESOctreeContainer(dd,dle,dre)
 create_fake_octree(oct_handler, nocts, max_level, dd, dle, dre, fsub)
-print "filled"
-print oct_handler.check(1, print_all=1)
 mask = np.ones((nocts,8),dtype='bool')
 cell_count = nocts*8
-level_counts = oct_handler.count_levels(max_level, 1, mask)
-print level_counts
-print "fcoords"
-fc = oct_handler.fcoords(domain,mask,cell_count,level_counts)
-print level_counts, level_counts.sum()
-print [np.unique(fc[:,ax]).shape[0] for ax in range(3)]
-print fc
-print fc.shape
-import pdb; pdb.set_trace()
+oct_counts = oct_handler.count_levels(max_level, 1, mask)
+level_counts = np.concatenate(([0,],np.cumsum(oct_counts)))
+fc = oct_handler.fcoords(domain,mask,cell_count, level_counts.copy())
 
 #Now take the particles and recreate the same octree
