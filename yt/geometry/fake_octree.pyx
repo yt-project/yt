25a76140eb0a4c485f7fe34baa4ade9ada766fcc
Christopher Moody <cemoody@ucsc.edu>
2013-04-17 22:38:05 -0700

    cleaned fake_octree code
    --HG--
    branch : yt-3.0

--- yt/geometry/fake_octree.pyx
+++ yt/geometry/fake_octree.pyx
@@ -32,68 +32,61 @@ cimport cython
 
 from oct_container cimport Oct, RAMSESOctreeContainer
 
-# Defined only by N leaves
-# Randomly decide if a branch should be subdivide, recurse one level if so
-# Once done, create a position array of len(leafes) with smoothing lengths = oct_size
-
-# Note that with this algorithm the octree won't be balanced once you hit
-# the maximum number of desired leaves
-
-# Use next_child(domain, int[3] octant, Oct parent)
-
+# Create a balanced octree by a random walk that recursively
+# subdivides
 def create_fake_octree(RAMSESOctreeContainer oct_handler,
-                       long noct,
+                       long max_noct,
                        long max_level,
                        np.ndarray[np.int32_t, ndim=1] ndd,
                        np.ndarray[np.float64_t, ndim=1] dle,
                        np.ndarray[np.float64_t, ndim=1] dre,
                        float fsubdivide):
-    cdef int[3] ind #hold the octant index
     cdef int[3] dd #hold the octant index
+    cdef int[3] ind #hold the octant index
     cdef long i
-    cdef long cur_noct = 0
-    cdef long cur_leaf= 0
+    cdef long cur_leaf = 0
+    cdef long leaves = 0
+    cdef np.ndarray[np.uint8_t, ndim=2] mask
     for i in range(3):
         ind[i] = 0
         dd[i] = ndd[i]
-    assert dd[0]*dd[1]*dd[2] <= noct
-    print 'starting'
-    print ind[0], ind[1], ind[2]
-    print 'allocate'
-    print noct
-    oct_handler.allocate_domains([noct])
-    print 'n_assigned', oct_handler.domains[0].n_assigned
-    print 'parent'
+    oct_handler.allocate_domains([max_noct])
     parent = oct_handler.next_root(1, ind)
     parent.domain = 1
     cur_leaf = 8 #we've added one parent...
-    print 'subdiv'
-    while oct_handler.domains[0].n_assigned < noct:
-        cur_noct = subdivide(oct_handler,ind, dd, cur_leaf, parent, 0, 0, noct,
-                  max_level, fsubdivide)
+    mask = np.ones((max_noct,8),dtype='uint8')
+    while oct_handler.domains[0].n_assigned < max_noct:
+        print "root: nocts ", oct_handler.domains[0].n_assigned
+        cur_leaf = subdivide(oct_handler, parent, ind, dd, cur_leaf, 0,
+                             max_noct, max_level, fsubdivide, mask)
+                             
+    leaves = oct_handler.count_leaves(mask)
+    assert cur_leaf == leaves
 
-cdef long subdivide(RAMSESOctreeContainer oct_handler, int ind[3], 
-               int dd[3], long cur_leaf,
-               Oct *parent, long cur_level, long cur_noct,
-               long noct, long max_level, float fsubdivide):
-    print cur_level, ' na ', oct_handler.domains[0].n_assigned, 
-    print ' n', oct_handler.domains[0].n,
-    print 'pos ', parent.pos[0], parent.pos[1], parent.pos[2]
+cdef long subdivide(RAMSESOctreeContainer oct_handler, 
+                    Oct *parent,
+                    int ind[3], int dd[3], 
+                    long cur_leaf, long cur_level, 
+                    long max_noct, long max_level, float fsubdivide,
+                    np.ndarray[np.uint8_t, ndim=2] mask):
+    print "child", parent.ind, ind[0], ind[1], ind[2], cur_leaf, cur_level
     cdef int ddr[3]
     cdef long i,j,k
     cdef float rf #random float from 0-1
     if cur_level >= max_level: 
-        return cur_noct
-    if oct_handler.domains[0].n_assigned >= noct: 
-        return cur_noct
+        return cur_leaf
+    if oct_handler.domains[0].n_assigned >= max_noct:
+        return cur_leaf
     for i in range(3):
         ind[i] = <int> ((rand() * 1.0 / RAND_MAX) * dd[i])
         ddr[i] = 2
     rf = rand() * 1.0 / RAND_MAX
     if rf > fsubdivide:
-        #this will mark the octant ind as subdivided
+        if parent.children[ind[0]][ind[1]][ind[2]] == NULL:
+            cur_leaf += 7 
         oct = oct_handler.next_child(1, ind, parent)
         oct.domain = 1
-        subdivide(oct_handler, ind, ddr, oct, cur_level + 1, 
-                  cur_noct+ 1, noct, max_level, fsubdivide)
-    return cur_noct
+        cur_leaf = subdivide(oct_handler, oct, ind, ddr, cur_leaf, 
+                             cur_level + 1, max_noct, max_level, 
+                             fsubdivide, mask)
+    return cur_leaf
