621b25e965267ddcd14b0c0c4159bade86399b1d
Matthew Turk <matthewturk@gmail.com>
2013-05-01 07:23:23 -0400

    Updating version to 2.5.2
    --HG--
    branch : stable

--- setup.py
+++ setup.py
@@ -155,7 +155,7 @@ build_src.build_src.generate_a_pyrex_source = generate_a_pyrex_source
 
 import setuptools
 
-VERSION = "2.5.1"
+VERSION = "2.5.2"
 
 if os.path.exists('MANIFEST'):
     os.remove('MANIFEST')
