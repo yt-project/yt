Removed calculate_tipsy_units and fixed /h length units
--HG--
branch : yt-3.0

yt/frontends/sph/data_structures.py
@@ -255,7 +255,7 @@ class ParticleStaticOutput(StaticOutput):
         mpch.update(mpc_conversion)
         unit_base = self._unit_base or {}
         for unit in mpc_conversion:
-            mpch['%sh' % unit] = mpch[unit] / self.hubble_constant
+            mpch['%sh' % unit] = mpch[unit] * self.hubble_constant
             mpch['%shcm' % unit] = (mpch["%sh" % unit] / 
                     (1 + self.current_redshift))
             mpch['%scm' % unit] = mpch[unit] / (1 + self.current_redshift)
@@ -481,26 +481,3 @@ class TipsyStaticOutput(ParticleStaticOutput):
     def _is_valid(self, *args, **kwargs):
         # We do not allow load() of these files.
         return False
-
-    @classmethod
-    def calculate_tipsy_units(self, hubble_constant, box_size):
-        # box_size in cm, or else in a unit we can convert to cm
-        # hubble_constant is assumed to be in units scaled to 100 km / s / Mpc
-        hubble_hertz = hubble_constant / (km_per_pc * 1e4)
-        if isinstance(box_size, types.TupleType):
-            if not isinstance(box_size[1], types.StringTypes):
-                raise RuntimeError
-            conversion = getattr(pcons, "cm_per_%s" % box_size[1], None)
-            if conversion is None:
-                raise RuntimeError
-            box_size = box_size[0] * conversion
-        print hubble_hertz, box_size
-        units = {}
-        units['length'] = box_size
-        units['density'] = 3.0 * hubble_hertz**2 / \
-                          (8.0 * np.pi * gravitational_constant_cgs)
-        # density is in g/cm^3
-        units['mass'] = units['density'] * units['length']**3.0
-        units['time'] = 1.0 / np.sqrt(gravitational_constant_cgs * units['density'])
-        units['velocity'] = units['length'] / units['time']
-        return units