0e0068e9113a380290832eb3bbb4ad71b67eae06
Matthew Turk <matthewturk@gmail.com>
2013-06-29 15:01:11 -0400

    Not 100% happy about this, but updated ParticleOctree tests.
    We should add more tests of AlwaysSelector and so on.  Out of scope at the
    moment.
    
    --HG--
    branch : yt-3.0

--- yt/geometry/tests/test_particle_octree.py
+++ yt/geometry/tests/test_particle_octree.py
@@ -1,38 +1,41 @@
 from yt.testing import *
 import numpy as np
-from yt.geometry.oct_container import ParticleOctreeContainer
+from yt.geometry.particle_oct_container import ParticleOctreeContainer
+from yt.geometry.oct_container import _ORDER_MAX
+from yt.utilities.lib.geometry_utils import get_morton_indices
 import time, os
 
 NPART = 32**3
-NDIM = 64
 DLE = np.array([0.0, 0.0, 0.0])
 DRE = np.array([10.0, 10.0, 10.0])
+dx = (DRE-DLE)/(2**_ORDER_MAX)
 
 def test_add_particles_random():
     np.random.seed(int(0x4d3d3d3))
     pos = np.random.normal(0.5, scale=0.05, size=(NPART,3)) * (DRE-DLE) + DLE
+    # Now convert to integers
     for i in range(3):
         np.clip(pos[:,i], DLE[i], DRE[i], pos[:,i])
+    # Convert to integers
+    pos = np.floor((pos - DLE)/dx).astype("uint64")
+    morton = get_morton_indices(pos)
+    morton.sort()
     for ndom in [1, 2, 4, 8]:
-        octree = ParticleOctreeContainer((NDIM, NDIM, NDIM), DLE, DRE)
+        octree = ParticleOctreeContainer((1, 1, 1), DLE, DRE)
         octree.n_ref = 32
-        for dom in range(ndom):
-            octree.add(pos[dom::ndom,:], dom)
+        for dom, split in enumerate(np.array_split(morton, ndom)):
+            octree.add(split)
         octree.finalize()
         # This visits every oct.
-        lin_count = octree.linearly_count()
         tc = octree.recursively_count()
         total_count = np.zeros(len(tc), dtype="int32")
         for i in sorted(tc):
             total_count[i] = tc[i]
-        yield assert_equal, lin_count, total_count.sum()
-        mask = np.ones((total_count.sum(), 8), dtype="bool")
+        yield assert_equal, octree.nocts, total_count.sum()
         # This visits every cell -- including those covered by octs.
-        level_count  = octree.count_levels(total_count.size-1, -1, mask)
-        for dom in range(ndom):
-            level_count += octree.count_levels(total_count.size-1, dom, mask)
-        yield assert_equal, level_count[0], NDIM**3 * 8
-        yield assert_equal, level_count, total_count * 8
+        #for dom in range(ndom):
+        #    level_count += octree.count_levels(total_count.size-1, dom, mask)
+        yield assert_equal, total_count, [1, 8, 64, 104, 184, 480, 1680, 1480]
 
 if __name__=="__main__":
     for i in test_add_particles_random():
