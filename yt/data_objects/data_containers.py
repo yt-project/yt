More fixes for units.  This is getting almost sort of functional :)
--HG--
branch : yt-3.0

yt/data_objects/data_containers.py
@@ -123,8 +123,8 @@ class YTDataContainer(object):
 
     def _set_default_field_parameters(self):
         self.field_parameters = {}
-        self.set_field_parameter("center",np.zeros(3,dtype='float64'))
-        self.set_field_parameter("bulk_velocity",np.zeros(3,dtype='float64'))
+        self.set_field_parameter("center",YTArray(np.zeros(3,dtype='float64'),'cm'))
+        self.set_field_parameter("bulk_velocity",YTArray(np.zeros(3,dtype='float64'),'cm/s'))
         self.set_field_parameter("normal",np.array([0,0,1],dtype='float64'))
 
     def _set_center(self, center):
@@ -140,8 +140,8 @@ class YTDataContainer(object):
             center = self.pf.h.find_max(center[4:])[1]
         else:
             center = np.array(center, dtype='float64')
-        self.center = center
-        self.set_field_parameter('center', center)
+        self.center = YTArray(center, 'code_length')
+        self.set_field_parameter('center', self.center)
 
     def get_field_parameter(self, name, default=None):
         """

yt/data_objects/field_info_container.py
@@ -228,7 +228,6 @@ class FieldDetector(defaultdict):
         self.LeftEdge = [0.0, 0.0, 0.0]
         self.RightEdge = [1.0, 1.0, 1.0]
         self.dds = np.ones(3, "float64")
-        self['dx'] = self['dy'] = self['dz'] = np.array([1.0])
         class fake_parameter_file(defaultdict):
             pass
 
@@ -297,10 +296,12 @@ class FieldDetector(defaultdict):
             if item == "Coordinates" or item[1] == "Coordinates" or \
                item == "Velocities" or item[1] == "Velocities":
                 # A vector
-                self[item] = np.ones((self.NumberOfParticles, 3))
+                self[item] = YTArray(np.ones((self.NumberOfParticles, 3)),
+                                     finfo.units)
             else:
                 # Not a vector
-                self[item] = np.ones(self.NumberOfParticles)
+                self[item] = YTArray(np.ones(self.NumberOfParticles),
+                                     finfo.units)
             self.requested.append(item)
             return self[item]
         self.requested.append(item)
@@ -320,10 +321,16 @@ class FieldDetector(defaultdict):
         return YTArray(defaultdict.__missing__(self, field_name),
                        input_units=FI[field_name].units)
 
+    fp_units = {
+        'bulk_velocity' : 'cm/s',
+        'center' : 'cm',
+        'normal' : ''
+        }
+
     def get_field_parameter(self, param, default = None):
         self.requested_parameters.append(param)
         if param in ['bulk_velocity', 'center', 'normal']:
-            return np.random.random(3) * 1e-2
+            return YTArray(np.random.random(3) * 1e-2, self.fp_units[param])
         else:
             return 0.0
 

yt/data_objects/selection_data_containers.py
@@ -136,8 +136,10 @@ class YTRayBase(YTSelectionContainer1D):
     _container_fields = ("t", "dts")
     def __init__(self, start_point, end_point, pf=None, field_parameters=None):
         super(YTRayBase, self).__init__(pf, field_parameters)
-        self.start_point = np.array(start_point, dtype='float64')
-        self.end_point = np.array(end_point, dtype='float64')
+        self.start_point = \
+          YTArray(np.array(start_point, dtype='float64'), 'code_length')
+        self.end_point = \
+          YTArray(np.array(end_point, dtype='float64'), 'code_length')
         self.vec = self.end_point - self.start_point
         #self.vec /= np.sqrt(np.dot(self.vec, self.vec))
         self._set_center(self.start_point)
@@ -366,7 +368,7 @@ class YTCuttingPlaneBase(YTSelectionContainer2D):
     def __init__(self, normal, center, pf = None,
                  north_vector = None, field_parameters = None):
         YTSelectionContainer2D.__init__(self, 4, pf, field_parameters)
-        self._set_center(center)
+        self._set_center(YTArray(center, 'code_length'))
         self.set_field_parameter('center',center)
         # Let's set up our plane equation
         # ax + by + cz + d = 0
@@ -570,7 +572,7 @@ class YTFixedResCuttingPlaneBase(YTSelectionContainer2D):
         # Taken from Cutting Plane
         #
         YTSelectionContainer2D.__init__(self, 4, pf, field_parameters)
-        self._set_center(center)
+        self._set_center(YTArray(center, 'code_length'))
         self.width = width
         self.dims = dims
         self.dds = self.width / self.dims
@@ -749,8 +751,8 @@ class YTDiskBase(YTSelectionContainer3D):
         YTSelectionContainer3D.__init__(self, center, fields, pf, **kwargs)
         self._norm_vec = np.array(normal)/np.sqrt(np.dot(normal,normal))
         self.set_field_parameter("normal", self._norm_vec)
-        self._height = fix_length(height, self.pf)
-        self._radius = fix_length(radius, self.pf)
+        self._height = fix_length(height)
+        self._radius = fix_length(radius)
         self._d = -1.0 * np.dot(self._norm_vec, self.center)
 
     def _get_list_of_grids(self):
@@ -923,7 +925,7 @@ class YTSphereBase(YTSelectionContainer3D):
     def __init__(self, center, radius, pf = None, field_parameters = None):
         super(YTSphereBase, self).__init__(center, pf, field_parameters)
         # Unpack the radius, if necessary
-        radius = fix_length(radius, self.pf)
+        radius = fix_length(radius)
         if radius < self.hierarchy.get_smallest_dx():
             raise YTSphereTooSmall(pf, radius, self.hierarchy.get_smallest_dx())
         self.set_field_parameter('radius',radius)
@@ -971,9 +973,9 @@ class YTEllipsoidBase(YTSelectionContainer3D):
         # make sure the smallest side is not smaller than dx
         if C < self.hierarchy.get_smallest_dx():
             raise YTSphereTooSmall(pf, C, self.hierarchy.get_smallest_dx())
-        self._A = A
-        self._B = B
-        self._C = C
+        self._A = YTArray(A, 'code_length')
+        self._B = YTArray(B, 'code_length')
+        self._C = YTArray(C, 'code_length')
         self._e0 = e0 = e0 / (e0**2.0).sum()**0.5
         self._tilt = tilt
         

yt/data_objects/universal_fields.py
@@ -500,7 +500,7 @@ def _averaged_density(field, data):
         weight_field += data["cell_mass"][sl]
 
     # Now some fancy footwork
-    new_field2 = np.zeros((nx, ny, nz))
+    new_field2 = YTArray(np.zeros((nx, ny, nz)), 'g/cm**3')
     new_field2[1:-1, 1:-1, 1:-1] = new_field / weight_field
     return new_field2
 
@@ -530,7 +530,7 @@ def _velocity_divergence(field, data):
         f += data["z-velocity"][1:-1,1:-1,sl_right]/ds
         f -= data["z-velocity"][1:-1,1:-1,sl_left ]/ds
     new_field = YTArray(np.zeros(data["x-velocity"].shape,
-                                 dtype=np.float64), 'cm/s')
+                                 dtype=np.float64), '1/s')
     new_field[1:-1,1:-1,1:-1] = f
     return new_field
 
@@ -689,7 +689,8 @@ add_field("particle_angular_momentum_z", function=_particle_angular_momentum_z,
 def get_radius(data, field_prefix):
     center = data.get_field_parameter("center")
     DW = data.pf.domain_right_edge - data.pf.domain_left_edge
-    radius = np.zeros(data[field_prefix+"x"].shape, dtype='float64')
+    radius = YTArray(np.zeros(data[field_prefix+"x"].shape, dtype='float64'),
+                     'cm')
     r = radius.copy()
     if any(data.pf.periodicity):
         rdw = radius.copy()
@@ -773,7 +774,7 @@ add_field("cutting_plane_velocity_y",
 def _cutting_plane_magnetic_field_x(field, data):
     x_vec, y_vec, z_vec = [data.get_field_parameter("cp_%s_vec" % (ax))
                            for ax in 'xyz']
-    b_vec = np.array([data["B%s" % ax] for ax in 'xyz'])
+    b_vec = np.array([data["magnetic_field_%s" % ax] for ax in 'xyz'])
     return np.dot(x_vec, b_vec)
 add_field("cutting_plane_magnetic_field_x",
           function=_cutting_plane_magnetic_field_x,
@@ -782,7 +783,7 @@ add_field("cutting_plane_magnetic_field_x",
 def _cutting_plane_magnetic_field_y(field, data):
     x_vec, y_vec, z_vec = [data.get_field_parameter("cp_%s_vec" % (ax))
                            for ax in 'xyz']
-    b_vec = np.array([data["B%s" % ax] for ax in 'xyz'])
+    b_vec = np.array([data["magnetic_field_%s" % ax] for ax in 'xyz'])
     return np.dot(y_vec, b_vec)
 add_field("cutting_plane_magnetic_field_y",
           function=_cutting_plane_magnetic_field_y,
@@ -911,7 +912,7 @@ def _vorticity_squared(field, data):
         sl_left = slice(None,-2,None)
         sl_right = slice(2,None,None)
         div_fac = 2.0
-    new_field = np.zeros(data["x-velocity"].shape)
+    new_field = YTArray(np.zeros(data["x-velocity"].shape), 'cm/s')
     dvzdy = (data["z-velocity"][1:-1,sl_right,1:-1] -
              data["z-velocity"][1:-1,sl_left,1:-1]) \
              / (div_fac*data["dy"].flat[0])
@@ -955,7 +956,8 @@ def _pressure_gradient_x(field, data):
         sl_left = slice(None,-2,None)
         sl_right = slice(2,None,None)
         div_fac = 2.0
-    new_field = np.zeros(data["pressure"].shape, dtype=np.float64)
+    new_field = YTArray(np.zeros(data["pressure"].shape, dtype=np.float64),
+                        'dyne/cm**3')
     ds = div_fac * data['dx'].flat[0]
     new_field[1:-1,1:-1,1:-1]  = data["pressure"][sl_right,1:-1,1:-1]/ds
     new_field[1:-1,1:-1,1:-1] -= data["pressure"][sl_left ,1:-1,1:-1]/ds
@@ -971,7 +973,8 @@ def _pressure_gradient_y(field, data):
         sl_left = slice(None,-2,None)
         sl_right = slice(2,None,None)
         div_fac = 2.0
-    new_field = np.zeros(data["pressure"].shape, dtype=np.float64)
+    new_field = YTArray(np.zeros(data["pressure"].shape, dtype=np.float64),
+                        'dyne/cm**3')
     ds = div_fac * data['dy'].flat[0]
     new_field[1:-1,1:-1,1:-1]  = data["pressure"][1:-1,sl_right,1:-1]/ds
     new_field[1:-1,1:-1,1:-1] -= data["pressure"][1:-1,sl_left ,1:-1]/ds
@@ -987,7 +990,8 @@ def _pressure_gradient_z(field, data):
         sl_left = slice(None,-2,None)
         sl_right = slice(2,None,None)
         div_fac = 2.0
-    new_field = np.zeros(data["pressure"].shape, dtype=np.float64)
+    new_field = YTArray(np.zeros(data["pressure"].shape, dtype=np.float64),
+                        'dyne/cm**3')
     ds = div_fac * data['dz'].flat[0]
     new_field[1:-1,1:-1,1:-1]  = data["pressure"][1:-1,1:-1,sl_right]/ds
     new_field[1:-1,1:-1,1:-1] -= data["pressure"][1:-1,1:-1,sl_left ]/ds
@@ -1017,7 +1021,8 @@ def _density_gradient_x(field, data):
         sl_left = slice(None,-2,None)
         sl_right = slice(2,None,None)
         div_fac = 2.0
-    new_field = np.zeros(data["density"].shape, dtype=np.float64)
+    new_field = YTArray(np.zeros(data["density"].shape, dtype=np.float64),
+                        'g/cm**4')
     ds = div_fac * data['dx'].flat[0]
     new_field[1:-1,1:-1,1:-1]  = data["density"][sl_right,1:-1,1:-1]/ds
     new_field[1:-1,1:-1,1:-1] -= data["density"][sl_left ,1:-1,1:-1]/ds
@@ -1032,7 +1037,8 @@ def _density_gradient_y(field, data):
         sl_left = slice(None,-2,None)
         sl_right = slice(2,None,None)
         div_fac = 2.0
-    new_field = np.zeros(data["density"].shape, dtype=np.float64)
+    new_field = YTArray(np.zeros(data["density"].shape, dtype=np.float64),
+                        'g/cm**4')
     ds = div_fac * data['dy'].flat[0]
     new_field[1:-1,1:-1,1:-1]  = data["density"][1:-1,sl_right,1:-1]/ds
     new_field[1:-1,1:-1,1:-1] -= data["density"][1:-1,sl_left ,1:-1]/ds
@@ -1047,7 +1053,8 @@ def _density_gradient_z(field, data):
         sl_left = slice(None,-2,None)
         sl_right = slice(2,None,None)
         div_fac = 2.0
-    new_field = np.zeros(data["density"].shape, dtype=np.float64)
+    new_field = YTArray(np.zeros(data["density"].shape, dtype=np.float64),
+                        'g/cm**4')
     ds = div_fac * data['dz'].flat[0]
     new_field[1:-1,1:-1,1:-1]  = data["density"][1:-1,1:-1,sl_right]/ds
     new_field[1:-1,1:-1,1:-1] -= data["density"][1:-1,1:-1,sl_left ]/ds
@@ -1104,7 +1111,8 @@ def _vorticity_x(field, data):
         sl_left = slice(None,-2,None)
         sl_right = slice(2,None,None)
         div_fac = 2.0
-    new_field = np.zeros(data["z-velocity"].shape, dtype=np.float64)
+    new_field = \
+      YTArray(np.zeros(data["z-velocity"].shape, dtype=np.float64), '1/s')
     new_field[1:-1,1:-1,1:-1] = (data["z-velocity"][1:-1,sl_right,1:-1] -
                                  data["z-velocity"][1:-1,sl_left,1:-1]) \
                                  / (div_fac*data["dy"].flat[0])
@@ -1122,7 +1130,8 @@ def _vorticity_y(field, data):
         sl_left = slice(None,-2,None)
         sl_right = slice(2,None,None)
         div_fac = 2.0
-    new_field = np.zeros(data["z-velocity"].shape, dtype=np.float64)
+    new_field = \
+      YTArray(np.zeros(data["z-velocity"].shape, dtype=np.float64), '1/s')
     new_field[1:-1,1:-1,1:-1] = (data["x-velocity"][1:-1,1:-1,sl_right] -
                                  data["x-velocity"][1:-1,1:-1,sl_left]) \
                                  / (div_fac*data["dz"].flat[0])
@@ -1140,7 +1149,8 @@ def _vorticity_z(field, data):
         sl_left = slice(None,-2,None)
         sl_right = slice(2,None,None)
         div_fac = 2.0
-    new_field = np.zeros(data["x-velocity"].shape, dtype=np.float64)
+    new_field = \
+      YTArray(np.zeros(data["z-velocity"].shape, dtype=np.float64), '1/s')
     new_field[1:-1,1:-1,1:-1] = (data["y-velocity"][sl_right,1:-1,1:-1] -
                                  data["y-velocity"][sl_left,1:-1,1:-1]) \
                                  / (div_fac*data["dx"].flat[0])
@@ -1201,7 +1211,7 @@ def _vorticity_growth_magnitude(field, data):
     result = np.sqrt(data["vorticity_growth_x"]**2 +
                      data["vorticity_growth_y"]**2 +
                      data["vorticity_growth_z"]**2)
-    dot = np.zeros(result.shape)
+    dot = YTArray(np.zeros(result.shape), '1/s')
     for ax in "xyz":
         dot += data["vorticity_%s" % ax] * data["vorticity_growth_%s" % ax]
     result = np.sign(dot) * result
@@ -1236,16 +1246,16 @@ add_field("vorticity_growth_timescale", function=_vorticity_growth_timescale,
 
 def _vorticity_radiation_pressure_x(field, data):
     rho = data["density"].astype(np.float64)
-    return (data["radiation_acceleration_y"] * data["gradDensity_z"] -
-            data["radiation_acceleration_z"] * data["gradDensity_y"]) / rho
+    return (data["radiation_acceleration_y"] * data["density_gradient_z"] -
+            data["radiation_acceleration_z"] * data["density_gradient_y"]) / rho
 def _vorticity_radiation_pressure_y(field, data):
     rho = data["density"].astype(np.float64)
-    return (data["radiation_acceleration_z"] * data["gradDensity_x"] -
-            data["radiation_acceleration_x"] * data["gradDensity_z"]) / rho
+    return (data["radiation_acceleration_z"] * data["density_gradient_x"] -
+            data["radiation_acceleration_x"] * data["density_gradient_z"]) / rho
 def _vorticity_radiation_pressure_z(field, data):
     rho = data["density"].astype(np.float64)
-    return (data["radiation_acceleration_x"] * data["gradDensity_y"] -
-            data["radiation_acceleration_y"] * data["gradDensity_x"]) / rho
+    return (data["radiation_acceleration_x"] * data["density_gradient_y"] -
+            data["radiation_acceleration_y"] * data["density_gradient_x"]) / rho
 
 for ax in 'xyz':
     n = "vorticity_radiation_pressure_%s" % ax
@@ -1292,7 +1302,7 @@ def _vorticity_radiation_pressure_growth_magnitude(field, data):
     result = np.sqrt(data["vorticity_radiation_pressure_growth_x"]**2 +
                      data["vorticity_radiation_pressure_growth_y"]**2 +
                      data["vorticity_radiation_pressure_growth_z"]**2)
-    dot = np.zeros(result.shape)
+    dot = YTArray(np.zeros(result.shape), '1/s')
     for ax in "xyz":
         dot += data["Vorticity%s" % ax] * data["vorticity_growth_%s" % ax]
     result = np.sign(dot) * result

yt/data_objects/yt_array.py
@@ -499,6 +499,9 @@ class YTArray(np.ndarray):
     def __eq__(self, other):
         """ Test if this is equal to the object on the right. """
         # Check that other is a YTArray.
+        if other == None:
+            # self is a YTArray, so it can't be None.
+            return False
         if isinstance(other, YTArray):
             if not self.units.same_dimensions_as(other.units):
                 raise YTUnitOperationError("equal", self.units, other.units)
@@ -510,6 +513,8 @@ class YTArray(np.ndarray):
     def __ne__(self, other):
         """ Test if this is not equal to the object on the right. """
         # Check that the other is a YTArray.
+        if other == None:
+            return True
         if isinstance(other, YTArray):
             if not self.units.same_dimensions_as(other.units):
                 raise YTUnitOperationError("not equal", self.units, other.units)
@@ -573,7 +578,8 @@ class YTArray(np.ndarray):
             else:
                 out_arr.units = unit
         else:
-            raise RuntimeError("Only unary and binary operators are allowed.")
+            raise RuntimeError("Operation is not defined.\n"
+                               "context: %s" % str(context))
         if out_arr.size == 1 and out_arr.size != self.size:
             return out_arr[0]
         return super(YTArray, self).__array_wrap__(out_arr, context)