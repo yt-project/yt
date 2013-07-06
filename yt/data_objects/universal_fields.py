cb0ea651a68e208cb0f3d63376b0872a20b2a1d7
Nathan Goldbaum <goldbaum@ucolick.org>
2013-07-05 21:55:11 -0700

    Fixing a unit issue in universal_fields.py.
    --HG--
    branch : yt

--- yt/data_objects/universal_fields.py
+++ yt/data_objects/universal_fields.py
@@ -1415,7 +1415,7 @@ def _VorticityGrowthTimescale(field, data):
     domegax_dt = data["VorticityX"] / data["VorticityGrowthX"]
     domegay_dt = data["VorticityY"] / data["VorticityGrowthY"]
     domegaz_dt = data["VorticityZ"] / data["VorticityGrowthZ"]
-    return np.sqrt(domegax_dt**2 + domegay_dt**2 + domegaz_dt)
+    return np.sqrt(domegax_dt**2 + domegay_dt**2 + domegaz_dt**2)
 add_field("VorticityGrowthTimescale", function=_VorticityGrowthTimescale,
           validators=[ValidateSpatial(1, 
                       ["x-velocity", "y-velocity", "z-velocity"])],
