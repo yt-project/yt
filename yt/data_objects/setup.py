f450f7452b1d209f40f767eecf540dbd7e64bf73
Nathan Goldbaum <goldbaum@ucolick.org>
2013-08-06 14:54:12 -0700

    Fixing an error that breaks source distributions.  Closes #620.
    --HG--
    branch : yt

--- yt/data_objects/setup.py
+++ yt/data_objects/setup.py
@@ -8,7 +8,7 @@ import os.path
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('data_objects', parent_package, top_path)
-    config.make_config_py()  # installs __config__.py
     config.add_subpackage("tests")
+    config.make_config_py()  # installs __config__.py
     #config.make_svn_version_py()
     return config
