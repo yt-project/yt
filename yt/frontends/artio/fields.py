2f6d5ab738bbd9a8e775372582a47b4961adc35a
Matthew Turk <matthewturk@gmail.com>
2013-08-14 16:31:00 -0400

    Clear up some particle type adding for ARTIO.
    Note that this also includes changing add_field to add_artio_field for
    particle_mass.
    
    --HG--
    branch : yt-3.0

--- yt/frontends/artio/fields.py
+++ yt/frontends/artio/fields.py
@@ -248,24 +248,23 @@ ARTIOFieldInfo["Metal_Density"]._projected_units = r""
 ##################################################
 #Particle fields
 
-for ax in 'xyz':
-    pf = "particle_velocity_%s" % ax
-    add_artio_field(pf, function=NullFunc,
-                    particle_type=True)
+for ptype in ("nbody", "stars"):
+    for ax in 'xyz':
+        add_artio_field((ptype, "particle_velocity_%s" % ax),
+                        function=NullFunc,
+                        particle_type=True)
+        add_artio_field((ptype, "particle_position_%s" % ax),
+                        function=NullFunc,
+                        particle_type=True)
 
-for ax in 'xyz':
-    pf = "particle_position_%s" % ax
-    add_artio_field(pf, function=NullFunc,
-                    particle_type=True)
-
-def _convertParticleMass(data):
-    return np.float64(data.convert('particle_mass'))
-add_field("particle_mass",
-          function=NullFunc,
-          convert_function=_convertParticleMass,
-          units=r"\rm{g}",
-          particle_type=True)
-add_artio_field("particle_index", function=NullFunc, particle_type=True)
+    def _convertParticleMass(data):
+        return np.float64(data.convert('particle_mass'))
+    add_artio_field((ptype, "particle_mass"),
+              function=NullFunc,
+              convert_function=_convertParticleMass,
+              units=r"\rm{g}",
+              particle_type=True)
+    add_artio_field((ptype, "particle_index"), function=NullFunc, particle_type=True)
 
 #add_artio_field("creation_time", function=NullFunc, particle_type=True)
 def _particle_age(field, data):
