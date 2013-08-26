26b8c7395e6bf06b8a82e6b2425067194c3b7364
Matthew Turk <matthewturk@gmail.com>
2013-08-26 10:34:05 -0400

    Initial sketch of Halo Catalog setup.  Creating operator registry.
    --HG--
    branch : yt-3.0

--- yt/analysis_modules/halo_analysis/halo_catalog.py
+++ yt/analysis_modules/halo_analysis/halo_catalog.py
@@ -27,4 +27,28 @@ License:
 """
 
 class HaloCatalog(object):
-    pass
+    def __init__(self, pf, finding_method, data_source = None):
+        self.pf = pf
+        if not callable(finding_method):
+            finding_method = hf_registry.find(finding_method)
+        self.finding_method = finding_method
+
+        # Now we initialize our callbacks, quantities and filters
+        self.filters = []
+        self.quantity_operators = []
+        self.callbacks = []
+
+    def add_callback(self, callback):
+        if not callable(callback):
+            callback = callback_registry.find(callback)
+        self.callbacks.append(callback)
+
+    def add_filter(self, filter):
+        if not callable(filter):
+            filter = filter_registry.find(filter)
+        self.filters.append(filter)
+
+    def add_quantity(self, quantity):
+        if not callable(quantity):
+            quantity = quantity_registry.find(quantity)
+        self.quantity_operators.append(quantity)
