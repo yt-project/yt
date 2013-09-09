79bfc025e07b4b08d41f46dfecfd26735d9e55bf
Matthew Turk <matthewturk@gmail.com>
2013-09-09 10:30:20 -0400

    Updating ARTIO tests with sample data.
    --HG--
    branch : yt-3.0

--- yt/frontends/artio/tests/test_outputs.py
+++ yt/frontends/artio/tests/test_outputs.py
@@ -31,21 +31,21 @@ from yt.utilities.answer_testing.framework import \
     FieldValuesTest
 from yt.frontends.artio.api import ARTIOStaticOutput
 
-_fields = ("Temperature", "Density", "VelocityMagnitude") 
+_fields = ("Temperature", "Density", "VelocityMagnitude",
+           ("deposit", "all_density"), ("deposit", "all_count")) 
 
-aiso5 = "artio/aiso_a0.9005.art"
-@requires_pf(aiso5)
-def test_aiso5():
-    pf = data_dir_load(aiso5)
-    yield assert_equal, str(pf), "aiso_a0.9005.art"
+sizmbhloz = "sizmbhloz-clref04SNth-rs9_a0.9011/sizmbhloz-clref04SNth-rs9_a0.9011.art"
+@requires_pf(sizmbhloz)
+def test_sizmbhloz():
+    pf = data_dir_load(sizmbhloz)
+    yield assert_equal, str(pf), "sizmbhloz-clref04SNth-rs9_a0.9011.art"
     dso = [ None, ("sphere", ("max", (0.1, 'unitary')))]
     for field in _fields:
         for axis in [0, 1, 2]:
             for ds in dso:
                 for weight_field in [None, "Density"]:
                     yield PixelizedProjectionValuesTest(
-                        aiso5, axis, field, weight_field,
+                        sizmbhloz, axis, field, weight_field,
                         ds)
                 yield FieldValuesTest(
-                        aiso5, field, ds)
-
+                        sizmbhloz, field, ds)
