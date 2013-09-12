23844ecc7f0d76faf65b222d00c92cd2e33576be
Matthew Turk <matthewturk@gmail.com>
2013-09-12 13:49:58 -0400

    Finishing conversion of licenses and license headers.
    --HG--
    branch : yt-3.0

--- yt/utilities/lib/alt_ray_tracers.pyx
+++ yt/utilities/lib/alt_ray_tracers.pyx
@@ -1,26 +1,17 @@
-"""Ray tracing algorithms for rays in non-cartesian coordinate systems.
-
-Author: Anthony Scopatz <scopatz@gmail.com>
-Affiliation: University of Chicago
-Homepage: http://yt-project.org/
-License:
-  Copyright (C) 2012 Anthony Scopatz.  All Rights Reserved.
-
-  This file is part of yt.
-
-  yt is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 
 import numpy as np
 cimport numpy as np
