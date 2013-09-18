339149f4293c4998e1c7de8dc5c783fbee4ae71f
Matthew Turk <matthewturk@gmail.com>
2013-09-18 09:13:18 -0400

    Typo.  Sorry, Fido!
    --HG--
    branch : yt-3.0

--- yt/data_objects/unstructured_mesh.py
+++ yt/data_objects/unstructured_mesh.py
@@ -17,7 +17,8 @@ import weakref
 import numpy as np
 
 from yt.funcs import mylog
-from yt.exceptions import YTParticleDepositionNotImplemented
+from yt.utilities.exceptions import \
+    YTParticleDepositionNotImplemented
 from yt.utilities.lib import \
     fill_fcoords, fill_fwidths
 
