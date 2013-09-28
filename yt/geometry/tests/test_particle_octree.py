07d4b72fb00a9bfff253500b65c604864de8b6f0
Matthew Turk <matthewturk@gmail.com>
2013-09-28 10:27:57 -0400

    Adding a first set of tests for ParticleRegions.
    --HG--
    branch : yt-3.0

--- yt/geometry/tests/test_particle_octree.py
+++ yt/geometry/tests/test_particle_octree.py
@@ -1,6 +1,8 @@
 from yt.testing import *
 import numpy as np
-from yt.geometry.particle_oct_container import ParticleOctreeContainer
+from yt.geometry.particle_oct_container import \
+    ParticleOctreeContainer, \
+    ParticleRegions
 from yt.geometry.oct_container import _ORDER_MAX
 from yt.utilities.lib.geometry_utils import get_morton_indices
 from yt.frontends.stream.api import load_particles
@@ -87,6 +89,29 @@ def test_particle_overrefine():
                 yield assert_equal, v1[a].size * f, v2[a].size
             cv2 = dd2["CellVolumeCode"].sum(dtype="float64")
             yield assert_equal, cv1, cv2
+
+def test_particle_regions():
+    np.random.seed(int(0x4d3d3d3))
+    # We are going to test having 31, 127, 128 and 257 data files.
+    for nfiles in [31, 127, 128, 257]:
+        # Now we create particles 
+        N = min(nfiles, 256)
+        reg = ParticleRegions([0.0, 0.0, 0.0, 0.0],
+                              [nfiles, nfiles, nfiles],
+                              [N, N, N], nfiles)
+        Y, Z = np.mgrid[0.1 : nfiles - 0.1 : nfiles * 1j,
+                        0.1 : nfiles - 0.1 : nfiles * 1j]
+        X = 0.5 * np.ones(Y.shape, dtype="float64")
+        pos = np.array([X.ravel(),Y.ravel(),Z.ravel()],
+            dtype="float64").transpose()
+        for i in range(nfiles):
+            reg.add_data_file(pos, i)
+            pos[:,0] += 1.0
+        for mask in reg.masks:
+            maxs = np.unique(mask.max(axis=-1).max(axis=-1))
+            mins = np.unique(mask.min(axis=-1).min(axis=-1))
+            yield assert_equal, maxs, mins
+            yield assert_equal, maxs, np.unique(mask)
 
 if __name__=="__main__":
     for i in test_add_particles_random():
