088070de4a70b5f5860458d30fced8bdd66ff0df
Matthew Turk <matthewturk@gmail.com>
2013-10-03 13:17:52 -0400

    Slimming down the Index object, as it will be merged into Dataset.
    --HG--
    branch : yt-3.0

--- yt/geometry/geometry_handler.py
+++ yt/geometry/geometry_handler.py
@@ -54,9 +54,6 @@ class Index(ParallelAnalysisInterface):
 
         self._initialize_state_variables()
 
-        mylog.debug("Initializing data storage.")
-        self._initialize_data_storage()
-
         # Must be defined in subclass
         mylog.debug("Setting up classes.")
         self._setup_classes()
@@ -79,10 +76,6 @@ class Index(ParallelAnalysisInterface):
         mylog.debug("Setting up derived fields")
         self._setup_derived_fields()
 
-    def __del__(self):
-        if self._data_file is not None:
-            self._data_file.close()
-
     def _detect_fields_backup(self):
         # grab fields from backup file as well, if present
         return
@@ -101,9 +94,7 @@ class Index(ParallelAnalysisInterface):
             return
 
     def _initialize_state_variables(self):
-        self._parallel_locking = False
-        self._data_file = None
-        self._data_mode = None
+        # TODO: Remove?
         self._max_locations = {}
         self.num_grids = None
 
@@ -281,151 +272,14 @@ class Index(ParallelAnalysisInterface):
         return self.region(c,
             pf.domain_left_edge, pf.domain_right_edge)
 
-    def _initialize_data_storage(self):
-        if not ytcfg.getboolean('yt','serialize'): return
-        fn = self.pf.storage_filename
-        if fn is None:
-            if os.path.isfile(os.path.join(self.directory,
-                                "%s.yt" % self.pf.unique_identifier)):
-                fn = os.path.join(self.directory,"%s.yt" % self.pf.unique_identifier)
-            else:
-                fn = os.path.join(self.directory,
-                        "%s.yt" % self.parameter_file.basename)
-        dir_to_check = os.path.dirname(fn)
-        if dir_to_check == '':
-            dir_to_check = '.'
-        # We have four options:
-        #    Writeable, does not exist      : create, open as append
-        #    Writeable, does exist          : open as append
-        #    Not writeable, does not exist  : do not attempt to open
-        #    Not writeable, does exist      : open as read-only
-        exists = os.path.isfile(fn)
-        if not exists:
-            writeable = os.access(dir_to_check, os.W_OK)
-        else:
-            writeable = os.access(fn, os.W_OK)
-        writeable = writeable and not ytcfg.getboolean('yt','onlydeserialize')
-        # We now have our conditional stuff
-        self.comm.barrier()
-        if not writeable and not exists: return
-        if writeable:
-            try:
-                if not exists: self.__create_data_file(fn)
-                self._data_mode = 'a'
-            except IOError:
-                self._data_mode = None
-                return
-        else:
-            self._data_mode = 'r'
-
-        self.__data_filename = fn
-        self._data_file = h5py.File(fn, self._data_mode)
-
-    def __create_data_file(self, fn):
-        # Note that this used to be parallel_root_only; it no longer is,
-        # because we have better logic to decide who owns the file.
-        f = h5py.File(fn, 'a')
-        f.close()
-
     def _setup_data_io(self):
         if getattr(self, "io", None) is not None: return
         self.io = io_registry[self.dataset_type]()
-
-    def _save_data(self, array, node, name, set_attr=None, force=False, passthrough = False):
-        """
-        Arbitrary numpy data will be saved to the region in the datafile
-        described by *node* and *name*.  If data file does not exist, it throws
-        no error and simply does not save.
-        """
-
-        if self._data_mode != 'a': return
-        try:
-            node_loc = self._data_file[node]
-            if name in node_loc and force:
-                mylog.info("Overwriting node %s/%s", node, name)
-                del self._data_file[node][name]
-            elif name in node_loc and passthrough:
-                return
-        except:
-            pass
-        myGroup = self._data_file['/']
-        for q in node.split('/'):
-            if q: myGroup = myGroup.require_group(q)
-        arr = myGroup.create_dataset(name,data=array)
-        if set_attr is not None:
-            for i, j in set_attr.items(): arr.attrs[i] = j
-        self._data_file.flush()
-
-    def _reload_data_file(self, *args, **kwargs):
-        if self._data_file is None: return
-        self._data_file.close()
-        del self._data_file
-        self._data_file = h5py.File(self.__data_filename, self._data_mode)
-
-    save_data = parallel_splitter(_save_data, _reload_data_file)
 
     def _get_data_reader_dict(self):
         dd = { 'pf' : self.parameter_file, # Already weak
                'hierarchy': weakref.proxy(self) }
         return dd
-
-    def _reset_save_data(self,round_robin=False):
-        if round_robin:
-            self.save_data = self._save_data
-        else:
-            self.save_data = parallel_splitter(self._save_data, self._reload_data_file)
-
-    def save_object(self, obj, name):
-        """
-        Save an object (*obj*) to the data_file using the Pickle protocol,
-        under the name *name* on the node /Objects.
-        """
-        s = cPickle.dumps(obj, protocol=-1)
-        self.save_data(np.array(s, dtype='c'), "/Objects", name, force = True)
-
-    def load_object(self, name):
-        """
-        Load and return and object from the data_file using the Pickle protocol,
-        under the name *name* on the node /Objects.
-        """
-        obj = self.get_data("/Objects", name)
-        if obj is None:
-            return
-        obj = cPickle.loads(obj.value)
-        if iterable(obj) and len(obj) == 2:
-            obj = obj[1] # Just the object, not the pf
-        if hasattr(obj, '_fix_pickle'): obj._fix_pickle()
-        return obj
-
-    def get_data(self, node, name):
-        """
-        Return the dataset with a given *name* located at *node* in the
-        datafile.
-        """
-        if self._data_file == None:
-            return None
-        if node[0] != "/": node = "/%s" % node
-
-        myGroup = self._data_file['/']
-        for group in node.split('/'):
-            if group:
-                if group not in myGroup:
-                    return None
-                myGroup = myGroup[group]
-        if name not in myGroup:
-            return None
-
-        full_name = "%s/%s" % (node, name)
-        try:
-            return self._data_file[full_name][:]
-        except TypeError:
-            return self._data_file[full_name]
-
-    def _close_data_file(self):
-        if self._data_file:
-            self._data_file.close()
-            del self._data_file
-            self._data_file = None
 
     def _add_object_class(self, name, class_name, base, dd):
         self.object_types.append(name)
