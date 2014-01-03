0706f1b32c0ffc119fe06f98bfd18833b4b3326f
John ZuHone <jzuhone@gmail.com>
2014-01-03 16:31:06 -0500

    Fixing up FLASH fields for unitrefactor.
    --HG--
    branch : yt-3.0

--- yt/frontends/flash/fields.py
+++ yt/frontends/flash/fields.py
@@ -43,6 +43,8 @@ pres_units = "code_mass/(code_length*code_time**2)"
 erg_units = "code_mass * (code_length/code_time)**2"
 rho_units = "code_mass / code_length**3"
 
+Na_code = Na.in_units("1/code_mass")
+
 class FLASHFieldInfo(FieldInfoContainer):
     known_other_fields = (
         ("velx", ("code_length/code_time", ["velocity_x"], None)),
@@ -53,8 +55,6 @@ class FLASHFieldInfo(FieldInfoContainer):
         ("pres", (pres_units, ["pressure"], None)),
         ("gpot", ("code_length**2/code_time**2", ["gravitational_potential"], None)),
         ("gpol", ("code_length**2/code_time**2", [], None)),
-        ("eint", ("code_length**2/code_time**2", ["thermal_energy"], None)),
-        ("ener", ("code_length**2/code_time**2", ["total_energy"], None)),
         ("tion", ("code_temperature", [], None)),
         ("tele", ("code_temperature", [], None)),
         ("trad", ("code_temperature", [], None)),
@@ -66,7 +66,7 @@ class FLASHFieldInfo(FieldInfoContainer):
         ("erad", (erg_units, [], "Radiation Internal Energy")),
         ("pden", (rho_units, [], None)),
         ("depo", ("code_length**2/code_time**2", [], None)),
-        ("ye", ("code_length**2/code_time**2", [], None)),
+        ("ye", ("", [], None)),
         ("magp", (pres_units, [], None)),
         ("divb", ("code_magnetic*code_length", [], None)),
         ("game", ("", [], "\gamma_e\/\rm{(ratio\/of\/specific\/heats)}")),
@@ -101,85 +101,65 @@ class FLASHFieldInfo(FieldInfoContainer):
     )
 
     def setup_fluid_fields(self):
-        # Now we conditionally load a few other things.
-        #if self.pf.parameters["MultiSpecies"] > 0:
-        #    self.setup_species_fields()
-        #self.setup_energy_field()
         for i in range(1, 1000):
             self.add_output_field(("flash", "r{0:03}".format(i)), 
                 units = "",
                 display_name="Energy Group {0}".format(i))
-
-def _ThermalEnergy(fields, data) :
-    try:
-        return data["eint"]
-    except:
-        pass
-    try:
-        return data["Pressure"] / (data.pf.gamma - 1.0) / data["Density"]
-    except:
-        pass
-    if data.has_field_parameter("mu") :
-        mu = data.get_field_parameter("mu")
-    else:
-        mu = 0.6
-    return kboltz*data["Density"]*data["Temperature"]/(mu*mh) / (data.pf.gamma - 1.0)
-
-def _TotalEnergy(fields, data) :
-    try:
-        etot = data["ener"]
-    except:
-        etot = data["ThermalEnergy"] + 0.5 * (
-            data["x-velocity"]**2.0 +
-            data["y-velocity"]**2.0 +
-            data["z-velocity"]**2.0)
-    try:
-        etot += data['magp']/data["Density"]
-    except:
-        pass
-    return etot
-
-# See http://flash.uchicago.edu/pipermail/flash-users/2012-October/001180.html
-# along with the attachment to that e-mail for details
-def GetMagRescalingFactor(pf):
-    if pf['unitsystem'].lower() == "cgs":
-         factor = 1
-    elif pf['unitsystem'].lower() == "si":
-         factor = np.sqrt(4*np.pi/1e7)
-    elif pf['unitsystem'].lower() == "none":
-         factor = np.sqrt(4*np.pi)
-    else:
-        raise RuntimeError("Runtime parameter unitsystem with "
-                           "value %s is unrecognized" % pf['unitsystem'])
-    return factor
-
-## Derived FLASH Fields
-def _nele(field, data):
-    return data['dens'] * data['ye'] * Na
-#add_field('nele', function=_nele, take_log=True, units="cm**-3")
-#add_field('edens', function=_nele, take_log=True, units="cm**-3")
-
-def _nion(field, data):
-    return data['dens'] * data['sumy'] * Na
-#add_field('nion', function=_nion, take_log=True, units="cm**-3")
-
-
-def _abar(field, data):
-    try:
-        return 1.0 / data['sumy']
-    except:
-        pass
-    return data['dens']*Na*kboltz*data['temp']/data['pres']
-#add_field('abar', function=_abar, take_log=False)
-	
-
-def _NumberDensity(fields,data) :
-    try:
-        return data["nele"]+data["nion"]
-    except:
-        pass
-    return data['pres']/(data['temp']*kboltz)
-#add_field("NumberDensity", function=_NumberDensity,
-#        units=r'\rm{cm}^{-3}')
+        # Add energy fields
+        def ekin(data):
+            ek = data["flash","velx"]**2
+            if data.pf.dimensionality >= 2:
+                ek += data["flash","vely"]**2
+            if data.pf.dimensionality == 3:
+                ek += data["flash","velz"]**2
+            return 0.5*ek
+        if ("flash","ener") in self.field_list:
+            self.add_output_field(("flash","ener"),
+                                  units="code_length**2/code_time**2")
+            self.alias(("gas","total_energy"),("flash","ener"),
+                       units="erg/g")
+        else:
+            def _ener(field, data):
+                ener = data["flash","eint"]+ekin(data)
+                try:
+                    ener += data["flash","magp"]/data["flash","dens"]
+                except:
+                    pass
+                return ener
+            self.add_field(("gas","total_energy"), function=_ener,
+                           units="erg/g")
+        if ("flash","eint") in self.field_list:
+            self.add_output_field(("flash","eint"),
+                                  units="code_length**2/code_time**2")
+            self.alias(("gas","thermal_energy"),("flash","eint"),
+                       units="erg/g")
+        else:
+            def _eint(field, data):
+                eint = data["flash","ener"]-ekin(data)
+                try:
+                    eint -= data["flash","magp"]/data["flash","dens"]
+                except:
+                    pass
+                return eint
+            self.add_field(("gas","thermal_energy"), function=_eint,
+                           units="erg/g")
+        ## Derived FLASH Fields
+        def _nele(field, data):
+            return data["flash","dens"]*data["flash","ye"]*Na_code
+        self.add_field(('flash','nele'), function=_nele, units="code_length**-3")
+        self.add_field(('flash','edens'), function=_nele, units="code_length**-3")
+        def _nion(field, data):
+            return data["flash","dens"]*data["flash","sumy"]*Na_code
+        self.add_field(('flash','nion'), function=_nion, units="code_length**-3")
+        def _abar(field, data):
+            try:
+                return data["flash","abar"]
+            except:
+                return 1.0/data["flash","sumy"]
+        self.add_field(("flash","abar"), function=_abar, units="1")
+        def _number_density(fields,data) :
+            return (data["nele"]+data["nion"])
+        self.add_field(("gas","number_density"), function=_number_density,
+                       units="cm**-3")
 
 
