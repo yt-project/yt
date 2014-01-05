8c620149893f4af70a5b6d1f7b0868c0c5b6ab97
Britton Smith <brittonsmith@gmail.com>
2014-01-05 09:47:51 -0500

    Updating cosmology in stellar spectrum generator.
    --HG--
    branch : yt-3.0

--- yt/analysis_modules/star_analysis/sfr_spectrum.py
+++ yt/analysis_modules/star_analysis/sfr_spectrum.py
@@ -18,8 +18,8 @@ import h5py
 import math, itertools
 
 from yt.funcs import *
-from yt.frontends.enzo.simulation_handling import \
-     EnzoCosmology
+from yt.utilities.cosmology import \
+    Cosmology
 from yt.utilities.physical_constants import \
     sec_per_year, \
     speed_of_light_cgs
@@ -79,13 +79,12 @@ class StarFormationRate(object):
         else:
             self.mode = 'data_source'
         # Set up for time conversion.
-        self.cosm = EnzoCosmology(
+        self.cosm = Cosmology(
              hubble_constant = self._pf.hubble_constant,
              omega_matter = self._pf.omega_matter,
-             omega_lambda = self._pf.omega_lambda,
-             initial_redshift = self._pf['CosmologyInitialRedshift'])
+             omega_lambda = self._pf.omega_lambda)
         # Find the time right now.
-        self.time_now = self.cosm.ComputeTimeFromRedshift(
+        self.time_now = self.cosm.t_from_z(
             self._pf.current_redshift) # seconds
         # Build the distribution.
         self.build_dist()
@@ -151,7 +150,7 @@ class StarFormationRate(object):
         for i, time in enumerate((self.time_bins[1:] + self.time_bins[:-1])/2.):
             self.time.append(time * tc / YEAR)
             self.lookback_time.append((self.time_now - time * tc)/YEAR)
-            self.redshift.append(self.cosm.ComputeRedshiftFromTime(time * tc))
+            self.redshift.append(self.cosm.z_from_t(time * tc))
             self.Msol_yr.append(self.mass_bins[i] / \
                 (self.time_bins_dt[i] * tc / YEAR))
             self.Msol_yr_vol.append(self.mass_bins[i] / \
@@ -270,15 +269,14 @@ class SpectrumBuilder(object):
         elif model == "salpeter":
             self.model = SALPETER
         # Set up for time conversion.
-        self.cosm = EnzoCosmology(
-             hubble_constant = self._pf.hubble_constant),
+        self.cosm = Cosmology(
+             hubble_constant = self._pf.hubble_constant,
              omega_matter = self._pf.omega_matter,
-             omega_lambda = self._pf.omega_lambda,
-             initial_redshift = self._pf['CosmologyInitialRedshift'])
+             omega_lambda = self._pf.omega_lambda)
         # Find the time right now.
         
         if time_now is None:
-            self.time_now = self.cosm.ComputeTimeFromRedshift(
+            self.time_now = self.cosm.t_from_z(
                 self._pf.current_redshift) # seconds
         else:
             self.time_now = time_now
