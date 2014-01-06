96d69500ccb86cead94b3774e9d62040dfa1ebf0
Matthew Turk <matthewturk@gmail.com>
2014-01-06 13:19:38 -0500

    Adding on header_spec option to GadgetStaticOutput.
    --HG--
    branch : yt-3.0

--- yt/frontends/sph/definitions.py
+++ yt/frontends/sph/definitions.py
@@ -3,3 +3,22 @@ gadget_ptypes = ("Gas", "Halo", "Disk", "Bulge", "Stars", "Bndry")
 ghdf5_ptypes  = ("PartType0", "PartType1", "PartType2", "PartType3",
                  "PartType4", "PartType5")
 
+gadget_header_specs = dict(
+    default      = (('Npart', 6, 'i'),
+                    ('Massarr', 6, 'd'),
+                    ('Time', 1, 'd'),
+                    ('Redshift', 1, 'd'),
+                    ('FlagSfr', 1, 'i'),
+                    ('FlagFeedback', 1, 'i'),
+                    ('Nall', 6, 'i'),
+                    ('FlagCooling', 1, 'i'),
+                    ('NumFiles', 1, 'i'),
+                    ('BoxSize', 1, 'd'),
+                    ('Omega0', 1, 'd'),
+                    ('OmegaLambda', 1, 'd'),
+                    ('HubbleParam', 1, 'd'),
+                    ('FlagAge', 1, 'i'),
+                    ('FlagMEtals', 1, 'i'),
+                    ('NallHW', 6, 'i'),
+                    ('unused', 16, 'i')),
+)
