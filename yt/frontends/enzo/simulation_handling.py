Fixing EnzoSimulation class to work correctly with the new units system.
--HG--
branch : yt-3.0

yt/frontends/enzo/simulation_handling.py
@@ -21,6 +21,9 @@ import os
 
 from yt.data_objects.time_series import \
     SimulationTimeSeries, TimeSeriesData
+from yt.data_objects.yt_array import \
+    YTArray, \
+    YTQuantity
 from yt.utilities.cosmology import \
     Cosmology
 from yt.utilities.definitions import \
@@ -31,6 +34,10 @@ from yt.utilities.exceptions import \
     NoStoppingCondition
 from yt.utilities.parallel_tools.parallel_analysis_interface import \
     parallel_objects
+from yt.utilities.physical_constants import \
+    gravitational_constant_cgs as G
+from yt.utilities.units import \
+     UnitRegistry
 
 from yt.convenience import \
     load
@@ -76,8 +83,25 @@ class EnzoSimulation(SimulationTimeSeries):
         SimulationTimeSeries.__init__(self, parameter_filename,
                                       find_outputs=find_outputs)
 
+    def _set_units(self):
+        self.unit_registry = UnitRegistry()
+        import yt.utilities.units as units
+        self.unit_registry.lut["code_time"] = (1.0, units.time)
+        if self.cosmological_simulation:
+            # Instantiate EnzoCosmology object for units and time conversions.
+            self.enzo_cosmology = \
+              EnzoCosmology(self.parameters['CosmologyHubbleConstantNow'],
+                            self.parameters['CosmologyOmegaMatterNow'],
+                            self.parameters['CosmologyOmegaLambdaNow'],
+                            0.0, self.parameters['CosmologyInitialRedshift'])
+
+            self.time_unit = self.enzo_cosmology.time_unit.in_units("s")
+        else:
+            self.time_unit = self.quan(self.parameters["TimeUnits"], "s")
+        self.unit_registry.modify("code_time", self.time_unit)
+
     def get_time_series(self, time_data=True, redshift_data=True,
-                        initial_time=None, final_time=None, time_units='1',
+                        initial_time=None, final_time=None,
                         initial_redshift=None, final_redshift=None,
                         initial_cycle=None, final_cycle=None,
                         times=None, redshifts=None, tolerance=None,
@@ -103,24 +127,24 @@ class EnzoSimulation(SimulationTimeSeries):
             Whether or not to include redshift outputs when gathering
             datasets for time series.
             Default: True.
-        initial_time : float
-            The earliest time for outputs to be included.  If None,
-            the initial time of the simulation is used.  This can be
-            used in combination with either final_time or
-            final_redshift.
+        initial_time : tuple of type (float, str)
+            The earliest time for outputs to be included.  This should be 
+            given as the value and the string representation of the units.
+            For example, (5.0, "Gyr").  If None, the initial time of the 
+            simulation is used.  This can be used in combination with 
+            either final_time or final_redshift.
             Default: None.
-        final_time : float
-            The latest time for outputs to be included.  If None,
-            the final time of the simulation is used.  This can be
-            used in combination with either initial_time or
-            initial_redshift.
+        final_time : tuple of type (float, str)
+            The latest time for outputs to be included.  This should be 
+            given as the value and the string representation of the units.
+            For example, (13.7, "Gyr"). If None, the final time of the 
+            simulation is used.  This can be used in combination with either 
+            initial_time or initial_redshift.
             Default: None.
-        times : array_like
-            A list of times for which outputs will be found.
+        times : tuple of type (float array, str)
+            A list of times for which outputs will be found and the units 
+            of those values.  For example, ([0, 1, 2, 3], "s").
             Default: None.
-        time_units : str
-            The time units used for requesting outputs by time.
-            Default: '1' (code units).
         initial_redshift : float
             The earliest redshift for outputs to be included.  If None,
             the initial redshift of the simulation is used.  This can be
@@ -167,8 +191,8 @@ class EnzoSimulation(SimulationTimeSeries):
         >>> from yt.mods import *
         >>> es = simulation("my_simulation.par", "Enzo")
         
-        >>> es.get_time_series(initial_redshift=10, final_time=13.7,
-                               time_units='Gyr', redshift_data=False)
+        >>> es.get_time_series(initial_redshift=10, final_time=(13.7, "Gyr"), 
+                               redshift_data=False)
 
         >>> es.get_time_series(redshifts=[3, 2, 1, 0])
 
@@ -213,8 +237,7 @@ class EnzoSimulation(SimulationTimeSeries):
         # Apply selection criteria to the set.
         if times is not None:
             my_outputs = self._get_outputs_by_time(times, tolerance=tolerance,
-                                                   outputs=my_all_outputs,
-                                                   time_units=time_units)
+                                                   outputs=my_all_outputs)
 
         elif redshifts is not None:
             my_outputs = self._get_outputs_by_redshift(redshifts, tolerance=tolerance,
@@ -236,22 +259,33 @@ class EnzoSimulation(SimulationTimeSeries):
 
         else:
             if initial_time is not None:
-                my_initial_time = initial_time / self.time_units[time_units]
+                if isinstance(initial_time, float):
+                    initial_time = self.quan(initial_time, "code_time")
+                elif isinstance(initial_time, tuple) and len(initial_time) == 2:
+                    initial_time = self.quan(*initial_time)
+                elif not isinstance(initial_time, YTArray):
+                    raise RuntimeError("Error: initial_time must be given as a float or tuple of (value, units).")
             elif initial_redshift is not None:
-                my_initial_time = self.enzo_cosmology.t_from_z(initial_redshift) / \
-                    self.enzo_cosmology.time_units
+                my_initial_time = self.enzo_cosmology.t_from_z(initial_redshift)
             else:
                 my_initial_time = self.initial_time
 
             if final_time is not None:
-                my_final_time = final_time / self.time_units[time_units]
+                if isinstance(final_time, float):
+                    final_time = self.quan(final_time, "code_time")
+                elif isinstance(final_time, tuple) and len(final_time) == 2:
+                    final_time = self.quan(*final_time)
+                elif not isinstance(final_time, YTArray):
+                    raise RuntimeError("Error: final_time must be given as a float or tuple of (value, units).")
+                my_final_time = final_time.in_units("s")
             elif final_redshift is not None:
-                my_final_time = self.enzo_cosmology.t_from_z(final_redshift) / \
-                    self.enzo_cosmology.time_units
+                my_final_time = self.enzo_cosmology.t_from_z(final_redshift)
             else:
                 my_final_time = self.final_time
 
-            my_times = np.array(map(lambda a:a['time'], my_all_outputs))
+            my_initial_time.convert_to_units("s")
+            my_final_time.convert_to_units("s")
+            my_times = np.array([a['time'] for a in my_all_outputs])
             my_indices = np.digitize([my_initial_time, my_final_time], my_times)
             if my_initial_time == my_times[my_indices[0] - 1]: my_indices[0] -= 1
             my_outputs = my_all_outputs[my_indices[0]:my_indices[1]]
@@ -368,34 +402,33 @@ class EnzoSimulation(SimulationTimeSeries):
 
         if not self.cosmological_simulation: return
         for output in self.all_redshift_outputs:
-            output['time'] = self.enzo_cosmology.t_from_z(output['redshift']) / \
-                self.enzo_cosmology.time_units
+            output['time'] = self.enzo_cosmology.t_from_z(output['redshift'])
         self.all_redshift_outputs.sort(key=lambda obj:obj['time'])
 
     def _calculate_time_outputs(self):
         "Calculate time outputs and their redshifts if cosmological."
 
+        self.all_time_outputs = []
         if self.final_time is None or \
             not 'dtDataDump' in self.parameters or \
             self.parameters['dtDataDump'] <= 0.0: return []
 
-        self.all_time_outputs = []
         index = 0
-        current_time = self.initial_time
-        while current_time <= self.final_time + self.parameters['dtDataDump']:
+        current_time = self.initial_time.copy()
+        dt_datadump = self.quan(self.parameters['dtDataDump'], "code_time")
+        while current_time <= self.final_time + dt_datadump:
             filename = os.path.join(self.parameters['GlobalDir'],
                                     "%s%04d" % (self.parameters['DataDumpDir'], index),
                                     "%s%04d" % (self.parameters['DataDumpName'], index))
 
-            output = {'index': index, 'filename': filename, 'time': current_time}
+            output = {'index': index, 'filename': filename, 'time': current_time.copy()}
             output['time'] = min(output['time'], self.final_time)
             if self.cosmological_simulation:
-                output['redshift'] = self.enzo_cosmology.z_from_t(
-                    current_time * self.enzo_cosmology.time_units)
+                output['redshift'] = self.enzo_cosmology.z_from_t(current_time)
 
             self.all_time_outputs.append(output)
             if np.abs(self.final_time - current_time) / self.final_time < 1e-4: break
-            current_time += self.parameters['dtDataDump']
+            current_time += dt_datadump
             index += 1
 
     def _calculate_cycle_outputs(self):
@@ -443,7 +476,7 @@ class EnzoSimulation(SimulationTimeSeries):
 
             self.all_outputs = self.all_time_outputs + self.all_redshift_outputs
             if self.parameters['CycleSkipDataDump'] <= 0:
-                self.all_outputs.sort(key=lambda obj:obj['time'])
+                self.all_outputs.sort(key=lambda obj:obj['time'].to_ndarray())
 
     def _calculate_simulation_bounds(self):
         """
@@ -455,26 +488,18 @@ class EnzoSimulation(SimulationTimeSeries):
 
         # Convert initial/final redshifts to times.
         if self.cosmological_simulation:
-            # Instantiate EnzoCosmology object for units and time conversions.
-            self.enzo_cosmology = \
-              EnzoCosmology(self.parameters['CosmologyHubbleConstantNow'],
-                            self.parameters['CosmologyOmegaMatterNow'],
-                            self.parameters['CosmologyOmegaLambdaNow'],
-                            0.0, self.parameters['CosmologyInitialRedshift'])
-            self.initial_time = self.enzo_cosmology.t_from_z(self.initial_redshift) / \
-                self.enzo_cosmology.time_units
-            self.final_time = self.enzo_cosmology.t_from_z(self.final_redshift) / \
-                self.enzo_cosmology.time_units
+            self.initial_time = self.enzo_cosmology.t_from_z(self.initial_redshift)
+            self.final_time = self.enzo_cosmology.t_from_z(self.final_redshift)
 
         # If not a cosmology simulation, figure out the stopping criteria.
         else:
             if 'InitialTime' in self.parameters:
-                self.initial_time = self.parameters['InitialTime']
+                self.initial_time = self.quan(self.parameters['InitialTime'], "code_time")
             else:
-                self.initial_time = 0.
+                self.initial_time = self.quan(0., "code_time")
 
             if 'StopTime' in self.parameters:
-                self.final_time = self.parameters['StopTime']
+                self.final_time = self.quan(self.parameters['StopTime'], "code_time")
             else:
                 self.final_time = None
             if not ('StopTime' in self.parameters or
@@ -501,20 +526,6 @@ class EnzoSimulation(SimulationTimeSeries):
         self.parameters['dtDataDump'] = 0.
         self.parameters['CycleSkipDataDump'] = 0.
         self.parameters['TimeUnits'] = 1.
-
-    def _set_time_units(self):
-        """
-        Set up a dictionary of time units conversions.
-        """
-
-        self.time_units = {}
-        if self.cosmological_simulation:
-            self.parameters['TimeUnits'] = 2.52e17 / np.sqrt(self.omega_matter) \
-                / self.hubble_constant / (1 + self.initial_redshift)**1.5
-        self.time_units['1'] = 1.
-        self.time_units['seconds'] = self.parameters['TimeUnits']
-        for unit in sec_conversion.keys():
-            self.time_units[unit] = self.parameters['TimeUnits'] / sec_conversion[unit]
 
     def _find_outputs(self):
         """
@@ -572,7 +583,7 @@ class EnzoSimulation(SimulationTimeSeries):
                     pf = load(filename)
                     if pf is not None:
                         my_storage.result = {'filename': filename,
-                                             'time': pf.current_time}
+                                             'time': pf.current_time.in_units("s")}
                         if pf.cosmological_simulation:
                             my_storage.result['redshift'] = pf.current_redshift
                 except YTOutputNotIdentified:
@@ -608,14 +619,15 @@ class EnzoSimulation(SimulationTimeSeries):
 
         """
 
-        values = ensure_list(values)
+        if not isinstance(values, np.ndarray):
+            values = ensure_list(values)
         if outputs is None:
             outputs = self.all_outputs
         my_outputs = []
         if not outputs:
             return my_outputs
         for value in values:
-            outputs.sort(key=lambda obj:np.fabs(value - obj[key]))
+            outputs.sort(key=lambda obj:np.abs(value - obj[key]))
             if (tolerance is None or np.abs(value - outputs[0][key]) <= tolerance) \
                     and outputs[0] not in my_outputs:
                 my_outputs.append(outputs[0])
@@ -651,14 +663,14 @@ class EnzoSimulation(SimulationTimeSeries):
         return self._get_outputs_by_key('redshift', redshifts, tolerance=tolerance,
                                      outputs=outputs)
 
-    def _get_outputs_by_time(self, times, tolerance=None, outputs=None,
-                             time_units='1'):
+    def _get_outputs_by_time(self, times, tolerance=None, outputs=None):
         r"""Get datasets at or near to given times.
 
         Parameters
         ----------
-        times: array_like
-            A list of times, given in code units as floats.
+        times: tuple of type (float array, str)
+            A list of times for which outputs will be found and the units 
+            of those values.  For example, ([0, 1, 2, 3], "s").
         tolerance : float
             If not None, do not return a dataset unless the time is
             within the tolerance value.  If None, simply return the
@@ -668,9 +680,6 @@ class EnzoSimulation(SimulationTimeSeries):
             The list of outputs from which to choose.  If None,
             self.all_outputs is used.
             Default: None.
-        time_units : str
-            The units of the list of times.
-            Default: '1' (code units).
 
         Examples
         --------
@@ -678,7 +687,12 @@ class EnzoSimulation(SimulationTimeSeries):
 
         """
 
-        times = np.array(times) / self.time_units[time_units]
+        if not isinstance(times, YTArray):
+            if isinstance(times, tuple) and len(times) == 2:
+                times = YTArray(*times)
+            else:
+                times = YTArray(times, "code_time")
+        times = times.in_units("s")
         return self._get_outputs_by_key('time', times, tolerance=tolerance,
                                         outputs=outputs)
 
@@ -705,6 +719,7 @@ class EnzoCosmology(Cosmology):
                            omega_lambda=omega_lambda,
                            omega_curvature=omega_curvature)
         self.initial_redshift = initial_redshift
-        self.initial_time = self.t_from_z(self.initial_redshift)
-        self.time_units = 2.52e17 / np.sqrt(self.omega_matter) / \
-          self.hubble_constant / np.power(1 + self.initial_redshift, 1.5)
+        # time units = 1 / sqrt(4 * pi * G rho_0 * (1 + z_i)**3),
+        # rho_0 = (3 * Omega_m * h**2) / (8 * pi * G)
+        self.time_unit = ((1.5 * self.omega_matter * self.hubble_constant**2 * 
+                           (1 + self.initial_redshift)**3)**-0.5).in_units("s")