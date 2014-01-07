c2eebdbcec7f63a9fb39b9b068eb5338ad98dddb
Nathan Goldbaum <goldbaum@ucolick.org>
2014-01-06 21:17:33 -0800

    Add a header and license blurb for unit_symbols.py.
    --HG--
    branch : yt-3.0

--- yt/units/unit_symbols.py
+++ yt/units/unit_symbols.py
@@ -1,3 +1,16 @@
+"""
+A place to statically create unit quantities.
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 from yt.units.yt_array import YTQuantity as quan
 
 #
