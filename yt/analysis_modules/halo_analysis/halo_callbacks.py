ac9e0db0cbbc34717a1caedd5100a48c7d717ef9
Britton Smith <brittonsmith@gmail.com>
2014-01-20 13:45:45 +0000

    Making sure units are the same for coordinates and center in radius calculation since it uses np.subtract.
    --HG--
    branch : yt-3.0

--- yt/analysis_modules/halo_analysis/halo_callbacks.py
+++ yt/analysis_modules/halo_analysis/halo_callbacks.py
@@ -16,10 +16,14 @@ Halo callback object
 import h5py
 
 from yt.data_objects.profiles import \
-     create_profile
+     Profile1D
 from yt.data_objects.yt_array import \
      YTArray, YTQuantity
-
+from yt.funcs import \
+     ensure_list
+from yt.utilities.exceptions import YTUnitConversionError
+from yt.utilities.logger import ytLogger as mylog
+     
 from .operator_registry import \
     callback_registry
 
@@ -46,7 +50,7 @@ def halo_sphere(halo, radius_field="virial_radius", factor=1.0):
     ----------
     halo : Halo object
         The Halo object to be provided by the HaloCatalog.
-    radius_field : str
+    radius_field : string
         Field to be retrieved from the quantities dictionary as 
         the basis of the halo radius.
         Default: "virial_radius".
@@ -57,12 +61,59 @@ def halo_sphere(halo, radius_field="virial_radius", factor=1.0):
         
     """
 
-    center = YTArray([halo.quantities['particle_position_%s' % axis] \
-                      for axis in "xyz"]) / \
-      halo.halo_catalog.data_pf.length_unit
-    radius = factor * halo.quantities[radius_field] / \
-      halo.halo_catalog.data_pf.length_unit
-    sphere = halo.halo_catalog.data_pf.h.sphere(center, (radius, "code_length"))
-    setattr(halo, "object", sphere)
+    dpf = halo.halo_catalog.data_pf
+    hpf = halo.halo_catalog.halos_pf
+    center = dpf.arr([halo.quantities['particle_position_%s' % axis] \
+                      for axis in "xyz"]) / dpf.length_unit
+    radius = factor * halo.quantities[radius_field] / dpf.length_unit
+    sphere = dpf.h.sphere(center, (radius, "code_length"))
+    setattr(halo, "data_object", sphere)
 
 add_callback("sphere", halo_sphere)
+
+def profile(halo, x_field, y_fields, x_bins=32, x_range=None, x_log=True,
+            weight_field="cell_mass"):
+    r"""
+    Create a 1d profile.
+
+    Parameter
+    ---------
+    halo : Halo object
+        The Halo object to be provided by the HaloCatalog.
+    x_field : string
+        The binning field for the profile.
+    y_fields : string or list of strings
+        The fields to be profiled.
+    x_bins : int
+        The number of bins in the profile.
+        Default: 32
+    x_range : (float, float)
+        The range of the x_field.  If None, the extrema are used.
+        Default: None
+    x_log : bool
+        Flag for logarithmmic binning.
+        Default: True
+    weight_field : string
+        Weight field for profiling.
+        Default : "cell_mass"
+
+    """
+
+    dpf = halo.halo_catalog.data_pf
+    
+    if x_range is None:
+        x_range = halo.data_object.quantities["Extrema"](x_field)[0]
+        # temporary check until derived quantities are fixed
+        # right now derived quantities do not work with units, so once they do, let us know
+        try:
+            x_range[0]._unit_repr_check_same("cm")
+            raise RuntimeError("Looks like derived quantities have been fixed.  Fix this code!")
+        except YTUnitConversionError:
+            # for now, Extrema return dimensionless, but assume it is code_length
+            x_range = [dpf.arr(x.to_ndarray(), "code_length") for x in x_range]
+            print halo.data_object.radius.in_units("Mpc")
+            print x_range
+        #    my_profile = Profile1D(halo.data_object
+    return
+
+add_callback("profile", profile)
