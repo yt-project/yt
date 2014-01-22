Fix units and field name.
--HG--
branch : yt-3.0

yt/fields/fluid_fields.py
@@ -195,9 +195,9 @@ def setup_fluid_fields(registry, ftype = "gas", slice_info = None):
     setup_gradient_fields(registry, (ftype, "density"), "g / cm**3",
                           slice_info)
 
-def setup_gradient_fields(registry, field, field_units, slice_info = None):
-    assert(isinstance(field, tuple))
-    ftype, fname = field
+def setup_gradient_fields(registry, grad_field, field_units, slice_info = None):
+    assert(isinstance(grad_field, tuple))
+    ftype, fname = grad_field
     if slice_info is None:
         sl_left = slice(None, -2, None)
         sl_right = slice(2, None, None)
@@ -215,11 +215,13 @@ def setup_gradient_fields(registry, field, field_units, slice_info = None):
         def func(field, data):
             # We need to set up stencils
             # This is based on enzo parameters and should probably be changed.    
-            new_field = data.pf.arr(np.zeros(data[field].shape, dtype=np.float64),
+            new_field = data.pf.arr(np.zeros(data[grad_field].shape,
+                                dtype=np.float64),
                                 field_units)
+            new_field /= data['dx'] # This fixes our units for in-place ops.
             ds = div_fac * data['dx']
-            new_field[slice_3d]  = data[field][slice_3dr]/ds[slice_3d]
-            new_field[slice_3d] -= data[field][slice_3dl]/ds[slice_3d]
+            new_field[slice_3d]  = data[grad_field][slice_3dr]/ds[slice_3d]
+            new_field[slice_3d] -= data[grad_field][slice_3dl]/ds[slice_3d]
             return new_field
         return func
 
@@ -227,7 +229,7 @@ def setup_gradient_fields(registry, field, field_units, slice_info = None):
         f = grad_func(axi, ax)
         registry.add_field((ftype, "%s_gradient_%s" % (fname, ax)),
                  function = f,
-                 validators = [ValidateSpatial(1, [field])],
+                 validators = [ValidateSpatial(1, [grad_field])],
                  units = "%s / cm" % field_units)
     
     def _gradient_magnitude(field, data):
@@ -236,5 +238,5 @@ def setup_gradient_fields(registry, field, field_units, slice_info = None):
                        data[ftype, "%s_gradient_z" % fname]**2)
     registry.add_field((ftype, "%s_gradient_magnitude" % fname),
              function = _gradient_magnitude,
-             validators = [ValidateSpatial(1, [field])],
+             validators = [ValidateSpatial(1, [grad_field])],
              units = "%s / cm" % field_units)