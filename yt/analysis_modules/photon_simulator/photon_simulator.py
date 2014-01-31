5c7402b16dd648cfac5b2a445bffaec45ade61b3
John ZuHone <jzuhone@gmail.com>
2014-01-31 17:41:12 -0500

    Missed this
    --HG--
    branch : yt

--- yt/analysis_modules/photon_simulator/photon_simulator.py
+++ yt/analysis_modules/photon_simulator/photon_simulator.py
@@ -447,7 +447,7 @@ class PhotonList(object):
         responses : list of strings, optional
             The names of the ARF and/or RMF files to convolve the photons with.
         convolve_energies : boolean, optional
-            If this is set, the photon energies will be convolved with the RMF>
+            If this is set, the photon energies will be convolved with the RMF.
             
         Examples
         --------
@@ -533,7 +533,7 @@ class PhotonList(object):
             else:
                 if redshift_new is None:
                     zobs = 0.0
-                    D_A = dist_new[0]*mpc_conversion["kpc"]/mpc_conversion[dist[1]]
+                    D_A = dist_new[0]*mpc_conversion["kpc"]/mpc_conversion[dist_new[1]]
                 else:
                     zobs = redshift_new
                     D_A = self.cosmo.AngularDiameterDistance(0.0,zobs)*1000.
