9e765042b3d8ff72d5ad132a63f5887d5a4b87b7
John ZuHone <jzuhone@gmail.com>
2014-02-05 00:52:58 -0500

    Adding velocity_unit for Athena.
    --HG--
    branch : yt-3.0

--- yt/frontends/athena/data_structures.py
+++ yt/frontends/athena/data_structures.py
@@ -372,6 +372,7 @@ class AthenaStaticOutput(StaticOutput):
             if not isinstance(val, tuple):
                 val = (val, cgs)
             setattr(self, "%s_unit" % unit, self.quan(val[0], val[1]))
+        self.velocity_unit = self.length_unit/self.time_unit
         self.magnetic_unit = np.sqrt(4*np.pi * self.mass_unit /
                                   (self.time_unit**2 * self.length_unit))
         self.magnetic_unit.convert_to_units("gauss")
