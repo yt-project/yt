221d06dfb939e76eeff181dd8fb590031589a29f
Matthew Turk <matthewturk@gmail.com>
2014-02-05 15:58:44 -0500

    Removing unused and obsolete code.
    --HG--
    branch : yt-3.0

--- yt/gui/setup.py
+++ yt/gui/setup.py
@@ -8,7 +8,6 @@ import os.path
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('gui', parent_package, top_path)
-    config.add_subpackage('opengl_widgets')
     config.add_subpackage('reason')
     config.make_config_py()  # installs __config__.py
     #config.make_svn_version_py()
