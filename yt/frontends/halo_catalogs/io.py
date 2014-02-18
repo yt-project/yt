HaloCatalog frontend can now query data (units still wrong).
--HG--
branch : yt-3.0

yt/frontends/halo_catalogs/io.py
@@ -14,7 +14,9 @@ Halo catalog data-file handling function
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+import h5py
 import numpy as np
+
 from yt.utilities.exceptions import *
 from yt.funcs import mylog
 
@@ -141,12 +143,10 @@ class IOHandlerHaloCatalogHDF5(BaseIOHandler):
                 data_files.update(obj.data_files)
         for data_file in data_files:
             pcount = data_file.header['num_halos']
-            with open(data_file.filename, "rb") as f:
-                f.seek(data_file._position_offset, os.SEEK_SET)
-                halos = np.fromfile(f, dtype=halo_dt, count = pcount)
-                x = halos['particle_position_x'].astype("float64")
-                y = halos['particle_position_y'].astype("float64")
-                z = halos['particle_position_z'].astype("float64")
+            with h5py.File(data_file.filename, "r") as f:
+                x = f['particle_position_x'].value.astype("float64")
+                y = f['particle_position_y'].value.astype("float64")
+                z = f['particle_position_z'].value.astype("float64")
                 yield "halos", (x, y, z)
 
     def _read_particle_fields(self, chunks, ptf, selector):
@@ -161,18 +161,16 @@ class IOHandlerHaloCatalogHDF5(BaseIOHandler):
                 data_files.update(obj.data_files)
         for data_file in data_files:
             pcount = data_file.header['num_halos']
-            with open(data_file.filename, "rb") as f:
+            with h5py.File(data_file.filename, "r") as f:
                 for ptype, field_list in sorted(ptf.items()):
-                    f.seek(data_file._position_offset, os.SEEK_SET)
-                    halos = np.fromfile(f, dtype=halo_dt, count = pcount)
-                    x = halos['particle_position_x'].astype("float64")
-                    y = halos['particle_position_y'].astype("float64")
-                    z = halos['particle_position_z'].astype("float64")
+                    x = f['particle_position_x'].value.astype("float64")
+                    y = f['particle_position_y'].value.astype("float64")
+                    z = f['particle_position_z'].value.astype("float64")
                     mask = selector.select_points(x, y, z)
                     del x, y, z
                     if mask is None: continue
                     for field in field_list:
-                        data = halos[field][mask].astype("float64")
+                        data = f[field][mask].astype("float64")
                         yield (ptype, field), data
 
     def _initialize_index(self, data_file, regions):
@@ -181,23 +179,19 @@ class IOHandlerHaloCatalogHDF5(BaseIOHandler):
         mylog.debug("Initializing index % 5i (% 7i particles)",
                     data_file.file_id, pcount)
         ind = 0
-        with open(data_file.filename, "rb") as f:
-            f.seek(data_file._position_offset, os.SEEK_SET)
-            halos = np.fromfile(f, dtype=halo_dt, count = pcount)
-            pos = np.empty((halos.size, 3), dtype="float64")
-            # These positions are in Mpc, *not* "code" units
+        with h5py.File(data_file.filename, "r") as f:
+            pos = np.empty((pcount, 3), dtype="float64")
             pos = data_file.pf.arr(pos, "code_length")
-            dx = np.finfo(halos['particle_position_x'].dtype).eps
+            dx = np.finfo(f['particle_position_x'].dtype).eps
             dx = 2.0*self.pf.quan(dx, "code_length")
-            pos[:,0] = halos["particle_position_x"]
-            pos[:,1] = halos["particle_position_y"]
-            pos[:,2] = halos["particle_position_z"]
+            pos[:,0] = f["particle_position_x"].value
+            pos[:,1] = f["particle_position_y"].value
+            pos[:,2] = f["particle_position_z"].value
             # These are 32 bit numbers, so we give a little lee-way.
             # Otherwise, for big sets of particles, we often will bump into the
             # domain edges.  This helps alleviate that.
             np.clip(pos, self.pf.domain_left_edge + dx,
                          self.pf.domain_right_edge - dx, pos)
-            #del halos
             if np.any(pos.min(axis=0) < self.pf.domain_left_edge) or \
                np.any(pos.max(axis=0) > self.pf.domain_right_edge):
                 raise YTDomainOverflow(pos.min(axis=0),
@@ -212,7 +206,7 @@ class IOHandlerHaloCatalogHDF5(BaseIOHandler):
         return morton
 
     def _count_particles(self, data_file):
-        return {'halos': data_file.header['n_halos']}
+        return {'halos': data_file.header['num_halos']}
 
     def _identify_fields(self, data_file):
         fields = [("halos", f) for f in halo_dt.fields if