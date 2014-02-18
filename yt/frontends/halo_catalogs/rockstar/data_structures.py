Moving rockstar and halo_catalog frontends into separate directories and fixing units of halo catalog frontend.
--HG--
branch : yt-3.0

yt/frontends/halo_catalogs/rockstar/data_structures.py
@@ -1,5 +1,5 @@
 """
-Data structures for a generic SPH/Gadget frontend.
+Data structures for Rockstar frontend.
 
 
 
@@ -14,7 +14,6 @@ Data structures for a generic SPH/Gadget frontend.
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-import h5py
 import numpy as np
 import stat
 import weakref
@@ -24,8 +23,7 @@ import time
 import os
 
 from .fields import \
-    RockstarFieldInfo, \
-    HaloCatalogFieldInfo
+    RockstarFieldInfo
 
 from yt.utilities.cosmology import Cosmology
 from yt.geometry.particle_geometry_handler import \
@@ -39,8 +37,7 @@ from yt.units.yt_array import \
     YTQuantity
 
 from .definitions import \
-    header_dt, \
-    halo_dt
+    header_dt
 
 class RockstarBinaryFile(ParticleFile):
     def __init__(self, pf, io, filename, file_id):
@@ -111,65 +108,5 @@ class RockstarStaticOutput(StaticOutput):
         with open(args[0], "rb") as f:
             header = fpu.read_cattrs(f, header_dt)
             if header['magic'] == 18077126535843729616:
-                return True
-        return False
-
-class HaloCatalogHDF5File(ParticleFile):
-    def __init__(self, pf, io, filename, file_id):
-        with h5py.File(filename, "r") as f:
-            self.header = dict((field, f.attrs[field]) \
-                               for field in f.attrs.keys())
-
-        super(HaloCatalogHDF5File, self).__init__(pf, io, filename, file_id)
-    
-class HaloCatalogStaticOutput(StaticOutput):
-    _hierarchy_class = ParticleGeometryHandler
-    _file_class = HaloCatalogHDF5File
-    _field_info_class = HaloCatalogFieldInfo
-    _particle_mass_name = "particle_mass"
-    _particle_coordinates_name = "Coordinates"
-    _suffix = ".h5"
-
-    def __init__(self, filename, data_style="halocatalog_hdf5",
-                 n_ref = 16, over_refine_factor = 1):
-        self.n_ref = n_ref
-        self.over_refine_factor = over_refine_factor
-        super(HaloCatalogStaticOutput, self).__init__(filename, data_style)
-
-    def _parse_parameter_file(self):
-        with h5py.File(self.parameter_filename, "r") as f:
-            hvals = dict((key, f.attrs[key]) for key in f.attrs.keys())
-        self.dimensionality = 3
-        self.refine_by = 2
-        self.unique_identifier = \
-            int(os.stat(self.parameter_filename)[stat.ST_CTIME])
-        prefix = ".".join(self.parameter_filename.rsplit(".", 2)[:-2])
-        self.filename_template = "%s.%%(num)s%s" % (prefix, self._suffix)
-        self.file_count = len(glob.glob(prefix + "*" + self._suffix))
-
-        for attr in ["cosmological_simulation", "current_time", "current_redshift",
-                     "hubble_constant", "omega_matter", "omega_lambda",
-                     "domain_left_edge", "domain_right_edge"]:
-            setattr(self, attr, hvals[attr])
-        self.periodicity = (True, True, True)
-        self.particle_types = ("halos")
-        self.particle_types_raw = ("halos")
-
-        nz = 1 << self.over_refine_factor
-        self.domain_dimensions = np.ones(3, "int32") * nz
-        self.parameters.update(hvals)
-
-    def _set_code_unit_attributes(self):
-        self.length_unit = self.quan(1.0, "cm")
-        self.mass_unit = self.quan(1.0, "g")
-        self.velocity_unit = self.quan(1.0, "cm / s")
-        self.time_unit = self.quan(1.0, "s")
-
-    @classmethod
-    def _is_valid(self, *args, **kwargs):
-        if not args[0].endswith(".h5"): return False
-        with h5py.File(args[0], "r") as f:
-            if "data_type" in f.attrs and \
-              f.attrs["data_type"] == "halo_catalog":
                 return True
         return False