1ac1bcd44efe4dbcb3b3eb9cc09281d1097dcf96
Matthew Turk <matthewturk@gmail.com>
2014-03-03 10:22:26 -0500

    ParticleMassMsun => particle_mass
    --HG--
    branch : yt-3.0

--- doc/source/cookbook/sum_mass_in_sphere.py
+++ doc/source/cookbook/sum_mass_in_sphere.py
@@ -7,10 +7,10 @@ pf = load("Enzo_64/DD0029/data0029")
 sp = pf.h.sphere("max", (1.0, "mpc"))
 
 # Use the TotalQuantity derived quantity to sum up the
-# values of the cell_mass and ParticleMassMsun fields
+# values of the cell_mass and particle_mass fields
 # within the sphere.
 baryon_mass, particle_mass = sp.quantities["TotalQuantity"](
-        ["cell_mass", "ParticleMassMsun"])
+        ["cell_mass", "particle_mass"])
 
 print "Total mass in sphere is %0.5e (gas = %0.5e / particles = %0.5e)" % \
             (baryon_mass + particle_mass, baryon_mass, particle_mass)
