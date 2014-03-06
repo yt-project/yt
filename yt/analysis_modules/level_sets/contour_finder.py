Fixing code units and contained now doesn't check edges.
--HG--
branch : yt-3.0

yt/analysis_modules/level_sets/contour_finder.py
@@ -32,8 +32,7 @@ def identify_contours(data_source, field, min_val, max_val,
     contours = {}
     empty_mask = np.ones((1,1,1), dtype="uint8")
     node_ids = []
-    DLE = data_source.pf.domain_left_edge.in_units("code_length")
-    DLE = DLE.ndarray_view()
+    DLE = data_source.pf.domain_left_edge
     for (g, node, (sl, dims, gi)) in data_source.tiles.slice_traverse():
         node.node_ind = len(node_ids)
         nid = node.node_id
@@ -45,11 +44,11 @@ def identify_contours(data_source, field, min_val, max_val,
         total_contours += new_contours.shape[0]
         tree.add_contours(new_contours)
         # Now we can create a partitioned grid with the contours.
+        LE = (DLE + g.dds * gi).in_units("code_length").ndarray_view()
+        RE = LE + (dims * g.dds).in_units("code_length").ndarray_view()
         pg = PartitionedGrid(g.id,
             [contour_ids.view("float64")], empty_mask,
-            DLE + g.dds * gi, 
-            DLE + g.dds * (gi + dims),
-            dims.astype("int64"))
+            LE, RE, dims.astype("int64"))
         contours[nid] = (g.Level, node.node_ind, pg, sl)
     node_ids = np.array(node_ids)
     trunk = data_source.tiles.tree.trunk