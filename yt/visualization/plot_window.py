c3271bdb421849af90da8bd38a29fed53c76e74f
Nathan Goldbaum <goldbaum@ucolick.org>
2014-03-14 20:38:50 -0700

    Refactoring, cleaning up.
    This also fixes an issue with colorbar and axis toggling not persisting when
    plots get modified.
    
    --HG--
    branch : yt-3.0

--- yt/visualization/plot_window.py
+++ yt/visualization/plot_window.py
@@ -239,7 +239,7 @@ class PlotWindow(ImagePlotContainer):
     _frb = None
     def __init__(self, data_source, bounds, buff_size=(800,800), antialias=True,
                  periodic=True, origin='center-window', oblique=False,
-                 window_size=8.0, fields=None, fontsize=18, setup=False):
+                 window_size=6.0, fields=None, fontsize=18, setup=False):
         if not hasattr(self, "pf"):
             self.pf = data_source.pf
             ts = self._initialize_dataset(self.pf)
@@ -707,7 +707,11 @@ class PWViewerMPL(PlotWindow):
             fig = None
             axes = None
             cax = None
-            if self.plots.has_key(f):
+            draw_colorbar = True
+            draw_axes = True
+            if f in self.plots:
+                draw_colorbar = self.plots[f]._draw_colorbar
+                draw_axes = self.plots[f]._draw_axes
                 if self.plots[f].figure is not None:
                     fig = self.plots[f].figure
                     axes = self.plots[f].axes
@@ -791,6 +795,9 @@ class PWViewerMPL(PlotWindow):
                 label.set_fontproperties(fp)
 
             self.run_callbacks(f)
+
+            self.plots[f]._toggle_axes(draw_axes)
+            self.plots[f]._toggle_colorbar(draw_colorbar)
 
             if self._font_color is not None:
                 ax = self.plots[f].axes
@@ -1518,23 +1525,31 @@ class PWViewerExtJS(PlotWindow):
         else:
             self._field_transform[field] = linear_transform
 
+
 class WindowPlotMPL(ImagePlotMPL):
     def __init__(self, data, cbname, cmap, extent, zlim, figure_size, fontsize,
-                 aspect, figure, axes, cax):
+                 unit_aspect, figure, axes, cax):
         self._draw_colorbar = True
         self._draw_axes = True
         self._fontsize = fontsize
-        self._fontscale = float(fontsize) / 18.0
         self._figure_size = figure_size
-        self._extent = extent
 
-        # set default layout
+        # Compute layout
+        fontscale = float(fontsize) / 18.0
+        if fontscale < 1.0:
+            fontscale = np.sqrt(fontscale)
+
+        self._cb_size = 0.0375*figure_size
+        self._ax_text_size = [0.9*fontscale, 0.7*fontscale]
+        self._top_buff_size = 0.30*fontscale
+        self._aspect = ((extent[1] - extent[0])/(extent[3] - extent[2]))
+
         size, axrect, caxrect = self._get_best_layout()
 
         super(WindowPlotMPL, self).__init__(
             size, axrect, caxrect, zlim, figure, axes, cax)
 
-        self._init_image(data, cbname, cmap, extent, aspect)
+        self._init_image(data, cbname, cmap, extent, unit_aspect)
 
         self.image.axes.ticklabel_format(scilimits=(-2, 3))
         if cbname == 'linear':
@@ -1542,92 +1557,6 @@ class WindowPlotMPL(ImagePlotMPL):
             self.cb.formatter.set_powerlimits((-2, 3))
             self.cb.update_ticks()
 
-    def _toggle_axes(self, choice):
-        self._draw_axes = choice
-        self.axes.get_xaxis().set_visible(choice)
-        self.axes.get_yaxis().set_visible(choice)
-        size, axrect, caxrect = self._get_best_layout()
-        self.axes.set_position(axrect)
-        self.cax.set_position(caxrect)
-        self.figure.set_size_inches(*size)
-
-    def _toggle_colorbar(self, choice):
-        self._draw_colorbar = choice
-        self.cax.set_visible(choice)
-        size, axrect, caxrect = self._get_best_layout()
-        self.axes.set_position(axrect)
-        self.cax.set_position(caxrect)
-        self.figure.set_size_inches(*size)
-
-    def hide_axes(self):
-        self._toggle_axes(False)
-        return self
-
-    def show_axes(self):
-        self._toggle_axes(True)
-        return self
-
-    def hide_colorbar(self):
-        self._toggle_colorbar(False)
-        return self
-
-    def show_colorbar(self):
-        self._toggle_colorbar(True)
-        return self
-
-    def _get_best_layout(self):
-        norm_size = self._figure_size/8.
-
-        if self._draw_colorbar:
-            cb_frac = .3*norm_size
-            cb_text_frac = 1.2*norm_size*self._fontscale
-        else:
-            cb_frac = 0.0
-            cb_text_frac = 0.0
-
-        if self._draw_axes:
-            x_ax_frac = 1.0*norm_size*self._fontscale
-            y_ax_frac = 0.75*norm_size*self._fontscale
-            top_buff = 0.3*norm_size
-        else:
-            x_ax_frac = 0.0
-            y_ax_frac = 0.0
-            top_buff = 0.0
-
-        extent = self._extent
-        aspect = (extent[1] - extent[0])/(extent[3] - extent[2])
-
-        # Ensure the figsize along the long axis is always equal to _figure_size
-        if aspect >= 1.0:
-            ximsize = self._figure_size
-            yimsize = self._figure_size/aspect
-        if aspect < 1.0:
-            ximsize = self._figure_size*aspect
-            yimsize = self._figure_size
-
-        xbins = np.array([x_ax_frac, ximsize, cb_frac, cb_text_frac])
-        ybins = np.array([y_ax_frac, yimsize, top_buff])
-
-        size = [xbins.sum(), ybins.sum()]
-
-        x_frac_widths = xbins/size[0]
-        y_frac_widths = ybins/size[1]
-
-        axrect = (
-            x_frac_widths[0],
-            y_frac_widths[0],
-            x_frac_widths[1],
-            y_frac_widths[1],
-        )
-
-        caxrect = (
-            x_frac_widths[0]+x_frac_widths[1],
-            y_frac_widths[0],
-            x_frac_widths[2],
-            y_frac_widths[1],
-        )
-
-        return size, axrect, caxrect
 
 def SlicePlot(pf, normal=None, fields=None, axis=None, *args, **kwargs):
     r"""
