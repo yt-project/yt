130374ff9011fbeaedc2d84cacf2b889c31a2c9d
Matthew Turk <matthewturk@gmail.com>
2014-03-18 10:01:27 -0400

    Adding more YTEP-0019 switching.
    --HG--
    branch : yt-3.0

--- yt/mods.py
+++ yt/mods.py
@@ -20,9 +20,8 @@ from __future__ import absolute_import
 # ALL IMPORTS GO HERE
 #
 
-# First module imports
-import numpy as np # For modern purposes
-import numpy # In case anyone wishes to use it by name
+import os
+from yt import *
 
 # This next item will handle most of the actual startup procedures, but it will
 # also attempt to parse the command line and set up the global state of various
@@ -33,89 +32,13 @@ import numpy # In case anyone wishes to use it by name
 import yt.startup_tasks as __startup_tasks
 unparsed_args = __startup_tasks.unparsed_args
 
-from yt.funcs import \
-    iterable, \
-    get_memory_usage, \
-    print_tb, \
-    rootonly, \
-    insert_ipython, \
-    get_pbar, \
-    only_on_root, \
-    is_root, \
-    get_version_stack, \
-    get_yt_supp, \
-    get_yt_version, \
-    parallel_profile, \
-    enable_plugins, \
-    memory_checker, \
-    deprecated_class
-from yt.utilities.logger import ytLogger as mylog
 from yt.config import ytcfg, ytcfg_defaults
-import yt.utilities.physical_constants as physical_constants
-import yt.units as units
-from yt.units.yt_array import YTArray, YTQuantity
 
 from yt.utilities.logger import level as __level
 if __level >= int(ytcfg_defaults["loglevel"]):
     # This won't get displayed.
     mylog.debug("Turning off NumPy error reporting")
     np.seterr(all = 'ignore')
-
-from yt.fields.api import \
-    field_plugins, \
-    DerivedField, \
-    FieldDetector, \
-    FieldInfoContainer, \
-    ValidateParameter, \
-    ValidateDataField, \
-    ValidateProperty, \
-    ValidateSpatial, \
-    ValidateGridType, \
-    add_field, \
-    derived_field
-
-from yt.data_objects.api import \
-    BinnedProfile1D, BinnedProfile2D, BinnedProfile3D, \
-    DatasetSeries, \
-    ImageArray, particle_filter, create_profile, \
-    Profile1D, Profile2D, Profile3D
-
-from yt.frontends.api import _frontend_container
-frontends = _frontend_container()
-
-from yt.frontends.stream.api import \
-    load_uniform_grid, load_amr_grids, \
-    load_particles, load_hexahedral_mesh, load_octree
-
-# For backwards compatibility
-GadgetDataset = frontends.sph.GadgetDataset
-GadgetStaticOutput = deprecated_class(GadgetDataset)
-TipsyDataset = frontends.sph.TipsyDataset
-TipsyStaticOutput = deprecated_class(TipsyDataset)
-
-# Now individual component imports from the visualization API
-from yt.visualization.api import \
-    PlotCollection, PlotCollectionInteractive, \
-    get_multi_plot, FixedResolutionBuffer, ObliqueFixedResolutionBuffer, \
-    write_bitmap, write_image, \
-    apply_colormap, scale_image, write_projection, \
-    SlicePlot, AxisAlignedSlicePlot, OffAxisSlicePlot, \
-    ProjectionPlot, OffAxisProjectionPlot, \
-    show_colormaps, ProfilePlot, PhasePlot
-
-from yt.visualization.volume_rendering.api import \
-    off_axis_projection
-
-from yt.utilities.parallel_tools.parallel_analysis_interface import \
-    parallel_objects
-
-from yt.convenience import \
-    load, simulation
-
-# Import some helpful math utilities
-from yt.utilities.math_utils import \
-    ortho_find, quartiles, periodic_position
-
 
 # We load plugins.  Keep in mind, this can be fairly dangerous -
 # the primary purpose is to allow people to have a set of functions
