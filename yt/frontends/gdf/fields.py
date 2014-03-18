ba11dd0a6fdd89d98fbc6f5a2ad4ecdf11a35037
Matthew Turk <matthewturk@gmail.com>
2014-03-18 14:39:53 -0400

    Fixing aliases for density
    --HG--
    branch : yt-3.0

--- yt/frontends/gdf/fields.py
+++ yt/frontends/gdf/fields.py
@@ -24,12 +24,12 @@ from yt.fields.field_info_container import \
 
 class GDFFieldInfo(FieldInfoContainer):
     known_other_fields = (
-        ("density", ("g/cm**3", [], None)),
+        ("density", ("g/cm**3", ["density"], None)),
         ("specific_energy", ("erg / g", ["thermal_energy"], None)),
-        ("pressure", ("erg/g", [], None)),
-        ("velocity_x", ("cm/s", [], None)),
-        ("velocity_y", ("cm/s", [], None)),
-        ("velocity_z", ("cm/s", [], None)),
+        ("pressure", ("", ["pressure"], None)),
+        ("velocity_x", ("cm/s", ["velocity_x"], None)),
+        ("velocity_y", ("cm/s", ["velocity_y"], None)),
+        ("velocity_z", ("cm/s", ["velocity_z"], None)),
         ("mag_field_x", ("gauss", ["magnetic_field_x"], None)),
         ("mag_field_y", ("gauss", ["magnetic_field_y"], None)),
         ("mag_field_z", ("gauss", ["magnetic_field_z"], None)),
