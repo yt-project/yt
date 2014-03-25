9cebe2689bd225a41364ab26895bf58ab4f8f09b
Sam Skillman <samskillman@gmail.com>
2014-03-24 23:43:34 -0700

    Adding pitch/yaw/roll to camera, moving pieces from orientation into camera.
    --HG--
    branch : yt-3.0

--- yt/visualization/volume_rendering/camera.py
+++ yt/visualization/volume_rendering/camera.py
@@ -16,6 +16,7 @@ Import the components of the volume rendering extension
 from yt.funcs import iterable
 from yt.utilities.orientation import Orientation
 from yt.units.yt_array import YTArray
+from yt.utilities.math_utils import get_rotation_matrix
 import numpy as np
 
 
@@ -48,7 +49,8 @@ class Camera(Orientation):
         width = 1.5 * data_source.pf.domain_width.max()
         (xmi, xma), (ymi, yma), (zmi, zma) = \
             data_source.quantities['Extrema'](['x', 'y', 'z'])
-        width = np.sqrt((xma-xmi)**2 + (yma-ymi)**2 + (zma-zmi)**2)
+        width = np.sqrt((xma-xmi)**2 + (yma-ymi)**2 + (zma-zmi)**2) /\
+            np.sqrt(3)
         focus = data_source.get_field_parameter('center')
 
         if iterable(width) and len(width) > 1 and isinstance(width[1], str):
@@ -68,3 +70,112 @@ class Camera(Orientation):
         super(Camera, self).__init__(self.focus - self.position,
                                      self.north_vector, steady_north=True)
         self._moved = True
+
+    def switch_orientation(self, normal_vector=None, north_vector=None):
+        r"""Change the view direction based on any of the orientation parameters.
+
+        This will recalculate all the necessary vectors and vector planes related
+        to an orientable object.
+
+        Parameters
+        ----------
+        normal_vector: array_like, optional
+            The new looking vector.
+        north_vector : array_like, optional
+            The 'up' direction for the plane of rays.  If not specific,
+            calculated automatically.
+        """
+        if north_vector is None:
+            north_vector = self.north_vector
+        if normal_vector is None:
+            normal_vector = self.normal_vector
+        self._setup_normalized_vectors(normal_vector, north_vector)
+
+    def switch_view(self, normal_vector=None, north_vector=None):
+        r"""Change the view based on any of the view parameters.
+
+        This will recalculate the orientation and width based on any of
+        normal_vector, width, center, and north_vector.
+
+        Parameters
+        ----------
+        normal_vector: array_like, optional
+            The new looking vector.
+        north_vector : array_like, optional
+            The 'up' direction for the plane of rays.  If not specific,
+            calculated automatically.
+        """
+        if north_vector is None:
+            north_vector = self.north_vector
+        if normal_vector is None:
+            normal_vector = self.normal_vector
+        self.switch_orientation(normal_vector=normal_vector,
+                                         north_vector=north_vector)
+        self._moved = True
+
+    def pitch(self, theta):
+        r"""Rotate by a given angle about the horizontal axis
+
+        Pitch the view.
+
+        Parameters
+        ----------
+        theta : float, in radians
+             Angle (in radians) by which to pitch the view.
+
+        Examples
+        --------
+
+        >>> cam.roll(np.pi/4)
+        """
+        rot_vector = self.unit_vectors[0]
+        R = get_rotation_matrix(theta, rot_vector)
+        self.switch_view(
+            normal_vector=np.dot(R, self.unit_vectors[2]),
+            north_vector=np.dot(R, self.unit_vectors[1]))
+        if self.steady_north:
+            self.north_vector = self.unit_vectors[1]
+
+    def yaw(self, theta):
+        r"""Rotate by a given angle about the vertical axis
+
+        Yaw the view.
+
+        Parameters
+        ----------
+        theta : float, in radians
+             Angle (in radians) by which to yaw the view.
+
+        Examples
+        --------
+
+        >>> cam.roll(np.pi/4)
+        """
+        rot_vector = self.unit_vectors[1]
+        R = get_rotation_matrix(theta, rot_vector)
+        self.switch_view(
+            normal_vector=np.dot(R, self.unit_vectors[2]))
+
+    def roll(self, theta):
+        r"""Rotate by a given angle about the view normal axis
+
+        Roll the view.
+
+        Parameters
+        ----------
+        theta : float, in radians
+             Angle (in radians) by which to roll the view.
+
+        Examples
+        --------
+
+        >>> cam.roll(np.pi/4)
+        """
+        rot_vector = self.unit_vectors[2]
+        R = get_rotation_matrix(theta, rot_vector)
+        self.switch_view(
+            normal_vector=np.dot(R, self.unit_vectors[2]),
+            north_vector=np.dot(R, self.unit_vectors[1]))
+        if self.steady_north:
+            self.north_vector = np.dot(R, self.north_vector)
+
