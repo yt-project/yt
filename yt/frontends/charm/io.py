c41014824da411be88e0b069cb80d2606be67f6e
Andrew Myers <atmyers2@gmail.com>
2014-03-30 14:52:50 -0700

    getting a little more code reuse
    --HG--
    branch : yt

--- yt/frontends/charm/io.py
+++ yt/frontends/charm/io.py
@@ -29,6 +29,16 @@ class IOHandlerCharmHDF5(BaseIOHandler):
         BaseIOHandler.__init__(self, *args, **kwargs)
         self.pf = pf
         self._handle = pf._handle
+        self._particle_field_index = {'position_x': 0,
+                                      'position_y': 1,
+                                      'position_z': 2,
+                                      'velocity_x': 3,
+                                      'velocity_y': 4,
+                                      'velocity_z': 5,
+                                      'acceleration_x': 6,
+                                      'acceleration_y': 7,
+                                      'acceleration_z': 8,
+                                      'mass': 9}
 
     _field_dict = None
     @property
@@ -63,22 +73,11 @@ class IOHandlerCharmHDF5(BaseIOHandler):
 
     def _read_particles(self, grid, name):
 
-        particle_field_index = {'position_x': 0,
-                                'position_y': 1,
-                                'position_z': 2,
-                                'velocity_x': 3,
-                                'velocity_y': 4,
-                                'velocity_z': 5,
-                                'acceleration_x': 6,
-                                'acceleration_y': 7,
-                                'acceleration_z': 8,
-                                'mass': 9}
-
-        field_index = particle_field_index[name]
+        field_index = self._particle_field_index[name]
         lev = 'level_%s' % grid.Level
 
         particles_per_grid = self._handle[lev]['particles:offsets'].value
-        items_per_particle = len(particle_field_index)
+        items_per_particle = len(self._particle_field_index)
 
         # compute global offset position
         offsets = items_per_particle * np.cumsum(particles_per_grid)
@@ -104,36 +103,14 @@ class IOHandlerCharm2DHDF5(IOHandlerCharmHDF5):
         BaseIOHandler.__init__(self, *args, **kwargs)
         self.pf = pf
         self._handle = pf._handle
+        self._particle_field_index = {'position_x': 0,
+                                      'position_y': 1,
+                                      'velocity_x': 2,
+                                      'velocity_y': 3,
+                                      'acceleration_x': 4,
+                                      'acceleration_y': 5,
+                                      'mass': 6}
 
-    def _read_particles(self, grid, name):
-
-        particle_field_index = {'position_x': 0,
-                                'position_y': 1,
-                                'velocity_x': 2,
-                                'velocity_y': 3,
-                                'acceleration_x': 4,
-                                'acceleration_y': 5,
-                                'mass': 6}
-
-        field_index = particle_field_index[name]
-        lev = 'level_%s' % grid.Level
-
-        particles_per_grid = self._handle[lev]['particles:offsets'].value
-        items_per_particle = len(particle_field_index)
-
-        # compute global offset position
-        offsets = items_per_particle * np.cumsum(particles_per_grid)
-        offsets = np.append(np.array([0]), offsets)
-        offsets = np.array(offsets, dtype=np.int64)
-            
-        grid_levels = np.array([g.Level for g in self.pf.h.grids])
-        grid_ids    = np.array([g.id    for g in self.pf.h.grids])
-        grid_level_offset = grid_ids[np.where(grid_levels == grid.Level)[0][0]]
-        lo = grid.id - grid_level_offset
-        hi = lo + 1
-
-        data = self._handle[lev]['particles:data'][offsets[lo]:offsets[hi]]
-        return data[field_index::items_per_particle]
 
 class IOHandlerCharm1DHDF5(IOHandlerCharmHDF5):
     _data_style = "charm1d_hdf5"
@@ -144,30 +121,7 @@ class IOHandlerCharm1DHDF5(IOHandlerCharmHDF5):
         BaseIOHandler.__init__(self, *args, **kwargs)
         self.pf = pf
         self._handle = pf._handle
-
-    def _read_particles(self, grid, name):
-
-        particle_field_index = {'position_x': 0,
-                                'velocity_x': 1,
-                                'acceleration_x': 2,
-                                'mass': 3}
-
-        field_index = particle_field_index[name]
-        lev = 'level_%s' % grid.Level
-
-        particles_per_grid = self._handle[lev]['particles:offsets'].value
-        items_per_particle = len(particle_field_index)
-
-        # compute global offset position
-        offsets = items_per_particle * np.cumsum(particles_per_grid)
-        offsets = np.append(np.array([0]), offsets)
-        offsets = np.array(offsets, dtype=np.int64)
-            
-        grid_levels = np.array([g.Level for g in self.pf.h.grids])
-        grid_ids    = np.array([g.id    for g in self.pf.h.grids])
-        grid_level_offset = grid_ids[np.where(grid_levels == grid.Level)[0][0]]
-        lo = grid.id - grid_level_offset
-        hi = lo + 1
-
-        data = self._handle[lev]['particles:data'][offsets[lo]:offsets[hi]]
-        return data[field_index::items_per_particle]
+        self._particle_field_index = {'position_x': 0,
+                                      'velocity_x': 1,
+                                      'acceleration_x': 2,
+                                      'mass': 3}
