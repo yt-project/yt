d2ab0e9cd81d40d8c027c079376ebdedcf5f2446
Matthew Turk <matthewturk@gmail.com>
2014-04-02 16:54:35 -0400

    These two were missed when local_only was added.
    --HG--
    branch : yt-3.0

--- yt/geometry/particle_geometry_handler.py
+++ yt/geometry/particle_geometry_handler.py
@@ -163,7 +163,7 @@ class ParticleIndex(Index):
                 g = og
             yield YTDataChunk(dobj, "spatial", [g])
 
-    def _chunk_io(self, dobj, cache = True):
+    def _chunk_io(self, dobj, cache = True, local_only = False):
         oobjs = getattr(dobj._current_chunk, "objs", dobj._chunk_info)
         for subset in oobjs:
             yield YTDataChunk(dobj, "io", [subset], None, cache = cache)
