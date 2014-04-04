8f7c62a9884a12ffa47736336fed2c280b9da48f
Matthew Turk <matthewturk@gmail.com>
2014-04-04 15:08:28 -0400

    Adding explanatory text.
    --HG--
    branch : yt-3.0

--- yt/data_objects/data_containers.py
+++ yt/data_objects/data_containers.py
@@ -794,7 +794,9 @@ class YTSelectionContainer2D(YTSelectionContainer):
         if (self.pf.geometry == "cylindrical" and self.axis == 1) or \
             (self.pf.geometry == "polar" and self.axis == 2):
             if center is not None and center != (0.0, 0.0):
-                raise NotImplementedError
+                raise NotImplementedError(
+                    "Currently we only support images centered at R=0." +
+                    "We plan to generalize this in the near future")
             from yt.visualization.fixed_resolution import CylindricalFixedResolutionBuffer
             if iterable(width): radius = max(width)
             if iterable(resolution): resolution = max(resolution)
