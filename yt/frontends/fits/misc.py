b5408bc531c4149f23a7df123d7c5ca132408d9c
John ZuHone <jzuhone@gmail.com>
2014-04-10 11:56:55 -0400

    A bit more work on getting WCS coordinates on plot window plots.
    --HG--
    branch : yt-3.0

--- yt/frontends/fits/misc.py
+++ yt/frontends/fits/misc.py
@@ -20,8 +20,27 @@ def force_aspect(ax,aspect=1):
     extent = im[0].get_extent()
     ax.set_aspect(abs((extent[1]-extent[0])/(extent[3]-extent[2]))/aspect)
 
+def convert_ticks(ticks, to_hours=False):
+    deg_ticks = ticks.astype("int")
+    min_ticks = ((ticks - deg_ticks)*60.).astype("int")
+    sec_ticks = ((((ticks - deg_ticks)*60.)-min_ticks)*60.).astype("int")
+    deg_string = "d"
+    if to_hours:
+        deg_ticks = (deg_ticks*24./360.).astype("int")
+        deg_string = "h"
+    return ["%02d%s%02dm%02ds" % (dt, deg_string, mt, st)
+            for dt, mt, st in zip(deg_ticks, min_ticks, sec_ticks)]
+
 def set_onaxis_wcs(pw):
-    return
+
+    ax = pw.plots.values()[0].axes
+    xpix = ax.get_xticks()
+    ypix = ax.get_xticks()
+    ra_ticks, dummy = pw.ds.wcs_2d.wcs_pix2world(xpix, ypix[0]*np.ones((len(xpix))), 1)
+    dummy, dec_ticks = pw.ds.wcs_2d.wcs_pix2world(xpix[0]*np.ones((len(ypix))), ypix, 1)
+    if pw.ds.dimensionality == 3:
+        vlim = pw.ds.wcs_1d.wcs_pix2world([pw.xlim[0], pw.xlim[1]], 1)[0]
+
     if pw.axis == pw.ds.ra_axis:
         xname = "Dec"
         yname = pw.ds.vel_name
@@ -41,11 +60,19 @@ def set_onaxis_wcs(pw):
     for k,v in pw.plots.iteritems():
         v.axes.set_xlabel(r"%s (%s)" % (xname, xunit))
         v.axes.set_ylabel(r"%s (%s)" % (yname, yunit))
-        v.axes.set_aspect('auto')
+        if xname == "Dec":
+            v.axes.xaxis.set_ticklabels(convert_ticks(dec_ticks), size=14)
+        if yname == "Dec":
+            v.axes.yaxis.set_ticklabels(convert_ticks(dec_ticks), size=14)
+        if xname == "RA":
+            v.axes.xaxis.set_ticklabels(convert_ticks(ra_ticks, to_hours=True), size=14)
+        if yname == pw.ds.vel_name:
+            extent = (pw.xlim[0].value, pw.xlim[1].value, vlim[0], vlim[1])
+            v.image.set_extent(extent)
 
 class FITSSlicePlot(AxisAlignedSlicePlot):
 
-    def __init__(self, ds, axis, fields, set_wcs=True, **kwargs):
+    def __init__(self, ds, axis, fields, set_wcs=False, **kwargs):
 
         if isinstance(axis, basestring):
             if axis in ds.axis_names:
@@ -64,13 +91,13 @@ class FITSSlicePlot(AxisAlignedSlicePlot):
         self._set_wcs()
         super(FITSSlicePlot, self).show()
 
-    def save(self, *args, **kwargs):
+    def save_wcs(self, *args, **kwargs):
         self._set_wcs()
         super(FITSSlicePlot, self).save(*args, **kwargs)
 
 class FITSOffAxisSlicePlot(OffAxisSlicePlot):
 
-    def __init__(self, ds, normal, fields, set_wcs=True, **kwargs):
+    def __init__(self, ds, normal, fields, set_wcs=False, **kwargs):
 
         self.ds = ds
         my_normal = normal
@@ -86,7 +113,7 @@ class FITSOffAxisSlicePlot(OffAxisSlicePlot):
 
 class FITSProjectionPlot(ProjectionPlot):
 
-    def __init__(self, ds, axis, fields, set_wcs=True, **kwargs):
+    def __init__(self, ds, axis, fields, set_wcs=False, **kwargs):
 
         self.ds = ds
         if isinstance(axis, basestring):
@@ -112,7 +139,7 @@ class FITSProjectionPlot(ProjectionPlot):
 
 class FITSOffAxisProjectionPlot(OffAxisProjectionPlot):
 
-    def __init__(self, ds, normal, fields, set_wcs=True, **kwargs):
+    def __init__(self, ds, normal, fields, set_wcs=False, **kwargs):
 
         self.ds = ds
         my_normal = normal
