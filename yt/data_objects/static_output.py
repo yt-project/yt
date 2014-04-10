Removing code units from the default symbol lut.  Fixing everywhere that used this construction.
--HG--
branch : yt-3.0

yt/data_objects/static_output.py
@@ -176,6 +176,7 @@ class Dataset(object):
 
         self._create_unit_registry()
         self._parse_parameter_file()
+        self.set_units()
         self._setup_coordinate_handler()
 
         # Because we need an instantiated class to check the pf's existence in
@@ -187,7 +188,6 @@ class Dataset(object):
             pass
         self.print_key_parameters()
 
-        self.set_units()
         self._set_derived_attrs()
         self._setup_classes()
 
@@ -632,7 +632,7 @@ class Dataset(object):
             DW = np.zeros(3)
         else:
             DW = self.arr(self.domain_right_edge - self.domain_left_edge, "code_length")
-        self.unit_registry.modify("unitary", DW.max())
+        self.unit_registry.add("unitary", float(DW.max()), DW.units.dimensions)
 
     _arr = None
     @property

yt/units/yt_array.py
@@ -14,7 +14,6 @@ YTArray class.
 #-----------------------------------------------------------------------------
 
 import copy
-
 import numpy as np
 
 from functools import wraps
@@ -29,7 +28,7 @@ from numpy import \
     isfinite, isinf, isnan, signbit, copysign, nextafter, modf, frexp, \
     floor, ceil, trunc, fmax, fmin
 
-from yt.units.unit_object import Unit
+from yt.units.unit_object import Unit, UnitParseError
 from yt.units.unit_registry import UnitRegistry
 from yt.units.dimensions import dimensionless
 from yt.utilities.exceptions import \
@@ -241,9 +240,19 @@ class YTArray(np.ndarray):
     def __new__(cls, input_array, input_units=None, registry=None, dtype=np.float64):
         if input_array is NotImplemented:
             return input_array
+        if registry is None and isinstance(input_units, basestring):
+            if input_units.startswith('code_'):
+                raise UnitParseError(
+                    "Code units used without referring to a dataset. \n"
+                    "Perhaps you meant to do something like this instead: \n"
+                    "ds.arr(%s, \"%s\")" % (input_array, input_units)
+                    )
         if isinstance(input_array, YTArray):
             if input_units is None:
-                pass
+                if registry is None:
+                    pass
+                else:
+                    input_array.units.registry = registry
             elif isinstance(input_units, Unit):
                 input_array.units = input_units
             else: