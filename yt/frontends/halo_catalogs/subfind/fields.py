d784631b437227785402d368db8ed15f8fe5d0be
Britton Smith <brittonsmith@gmail.com>
2014-04-14 12:26:59 +0100

    Adding some field aliases.
    --HG--
    branch : yt-3.0

--- yt/frontends/halo_catalogs/subfind/fields.py
+++ yt/frontends/halo_catalogs/subfind/fields.py
@@ -14,35 +14,40 @@ Subfind-specific fields
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-import numpy as np
-
 from yt.funcs import mylog
 from yt.fields.field_info_container import \
     FieldInfoContainer
 from yt.units.yt_array import \
     YTArray
 
-from yt.utilities.physical_constants import \
-    mh, \
-    mass_sun_cgs
-
-m_units = "g"
-p_units = "cm"
-v_units = "cm / s"
-r_units = "cm"
+m_units = "1e10 * Msun/h"
+p_units = "Mpccm/h"
+v_units = "1e5 * cmcm / s"
 
 class SubfindFieldInfo(FieldInfoContainer):
     known_other_fields = (
     )
 
     known_particle_fields = (
-        ("particle_identifier", ("", [], None)),
-        ("particle_position_x", (p_units, [], None)),
-        ("particle_position_y", (p_units, [], None)),
-        ("particle_position_z", (p_units, [], None)),
-        ("particle_velocity_x", (v_units, [], None)),
-        ("particle_velocity_y", (v_units, [], None)),
-        ("particle_velocity_z", (v_units, [], None)),
-        ("particle_mass", (m_units, [], "Virial Mass")),
-        ("virial_radius", (r_units, [], "Virial Radius")),
+        ("CenterOfMass_0", (p_units, ["particle_position_x"], None)),
+        ("CenterOfMass_1", (p_units, ["particle_position_y"], None)),
+        ("CenterOfMass_2", (p_units, ["particle_position_z"], None)),
+        ("CenterOfMassVelocity_0", (v_units, ["particle_velocity_x"], None)),
+        ("CenterOfMassVelocity_1", (v_units, ["particle_velocity_y"], None)),
+        ("CenterOfMassVelocity_2", (v_units, ["particle_velocity_z"], None)),
+        ("Mass", (m_units, ["particle_mass"], None)),
+        ("Halo_M_Crit200", (m_units, ["Virial Mass"], None)),
+        ("Halo_M_Crit2500", (m_units, [], None)),
+        ("Halo_M_Crit500", (m_units, [], None)),
+        ("Halo_M_Mean200", (m_units, [], None)),
+        ("Halo_M_Mean2500", (m_units, [], None)),
+        ("Halo_M_Mean500", (m_units, [], None)),
+        ("Halo_M_TopHat200", (m_units, [], None)),
+        ("Halo_R_Crit200", (p_units, ["Virial Radius"], None)),
+        ("Halo_R_Crit2500", (p_units, [], None)),
+        ("Halo_R_Crit500", (p_units, [], None)),
+        ("Halo_R_Mean200", (p_units, [], None)),
+        ("Halo_R_Mean2500", (p_units, [], None)),
+        ("Halo_R_Mean500", (p_units, [], None)),
+        ("Halo_R_TopHat200", (p_units, [], None)),
 )
