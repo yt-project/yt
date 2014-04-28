Fixing calculation of end redshift of light ray.
--HG--
branch : yt

yt/analysis_modules/cosmological_observation/light_ray/light_ray.py
@@ -396,8 +396,9 @@ class LightRay(CosmologySplice):
                   vector_length(my_segment['start'], my_segment['end']) * \
                   self.cosmology.HubbleConstantNow * \
                   self.cosmology.ExpansionFactor(my_segment['redshift'])
-                next_redshift = np.sqrt((1. + h_vel / speed_of_light_cgs) /
-                                         (1. - h_vel / speed_of_light_cgs)) - 1.
+                next_redshift = my_segment["redshift"] + \
+                  np.sqrt((1. + h_vel / speed_of_light_cgs) /
+                          (1. - h_vel / speed_of_light_cgs)) + 1.
             elif my_segment['next'] is None:
                 next_redshift = self.near_redshift
             else: