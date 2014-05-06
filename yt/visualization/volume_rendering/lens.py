cbaabb801d8779ef08a9573835a855a9cd0bcb50
Matthew Turk <matthewturk@gmail.com>
2014-05-06 10:35:53 -0400

    Rotation matrices aren't set correctly for the fisheye, and speeding things up.
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/lens.py
+++ yt/visualization/volume_rendering/lens.py
@@ -217,7 +217,7 @@ class FisheyeLens(Lens):
         return self.current_image
 
     def get_sampler_params(self, camera, render_source):
-        vp = arr_fisheye_vectors(camera.resolution[0], self.fov)
+        vp = -arr_fisheye_vectors(camera.resolution[0], self.fov)
         vp.shape = (camera.resolution[0]**2, 1, 3)
         vp2 = vp.copy()
         for i in range(3):
@@ -262,8 +262,9 @@ class FisheyeLens(Lens):
         # vector, and we need the reverse.
         lpos = camera.position - pos
         mag = (lpos * lpos).sum(axis=1)**0.5
+        lpos /= mag[:,None]
         dz = mag / self.radius
-        theta = np.arccos(lpos[:,2] / mag) 
+        theta = np.arccos(lpos[:,2])
         fov_rad = self.fov * np.pi / 180.0
         r = 2.0 * theta / fov_rad
         phi = np.arctan2(lpos[:,1], lpos[:,0])
