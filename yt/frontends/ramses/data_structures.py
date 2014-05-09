Fixing RAMSES density and mass units.
--HG--
branch : yt-3.0

yt/frontends/ramses/data_structures.py
@@ -489,7 +489,8 @@ class RAMSESDataset(Dataset):
         length_unit = self.parameters['unit_l'] * self.parameters['boxlen']
         rho_u = self.parameters['unit_d']
         # We're not multiplying by the boxlength here.
-        mass_unit = rho_u * self.parameters['unit_l']**3
+        mass_unit = rho_u * (self.parameters['unit_l'] *
+                             self.parameters['boxlen'])**3
         time_unit = self.parameters['unit_t']
 
         magnetic_unit = np.sqrt(4*np.pi * mass_unit /