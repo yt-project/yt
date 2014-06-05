14ec4bfb30c99315ad888b2ba627317e50ec6fc8
Nathan Goldbaum <goldbaum@ucolick.org>
2014-06-05 01:13:21 -0700

    Be much more careful about sanitizing data object centers.  Closes #848
    The selector objects expect data in code units, so we need to be careful to set everything as such.
    
    input YTArrays may come from a completely different dataset, as is the case in the halo catalog
    code, so we also need to make sure we conver to CGS if passed a YTArray or list of quantities.
    
    --HG--
    branch : yt-3.0

--- yt/data_objects/data_containers.py
+++ yt/data_objects/data_containers.py
@@ -137,19 +137,25 @@ class YTDataContainer(object):
             self.center = None
             self.set_field_parameter('center', self.center)
             return
+        elif isinstance(center, YTArray):
+            self.center = self.pf.arr(center.in_cgs())
+            self.center.convert_to_units('code_length')
         elif isinstance(center, (types.ListType, types.TupleType, np.ndarray)):
-            center = self.pf.arr(center, 'code_length')
+            if isinstance(center[0], YTQuantity):
+                self.center = self.pf.arr([c.in_cgs() for c in center])
+                self.center.convert_to_units('code_length')
+            else:
+                self.center = self.pf.arr(center, 'code_length')
         elif isinstance(center, basestring):
             if center.lower() in ("c", "center"):
-                center = self.pf.domain_center
+                self.center = self.pf.domain_center
              # is this dangerous for race conditions?
             elif center.lower() in ("max", "m"):
-                center = self.pf.h.find_max(("gas", "density"))[1]
+                self.center = self.pf.h.find_max(("gas", "density"))[1]
             elif center.startswith("max_"):
-                center = self.pf.h.find_max(center[4:])[1]
+                self.center = self.pf.h.find_max(center[4:])[1]
         else:
-            center = np.array(center, dtype='float64')
-        self.center = self.pf.arr(center, 'code_length')
+            self.center = self.pf.arr(center, 'code_length', dtype='float64')
         self.set_field_parameter('center', self.center)
 
     def get_field_parameter(self, name, default=None):
--- yt/units/yt_array.py
+++ yt/units/yt_array.py
@@ -284,7 +284,7 @@ class YTArray(np.ndarray):
             return input_array
         elif isinstance(input_array, np.ndarray):
             pass
-        elif iterable(input_array):
+        elif iterable(input_array) and input_array:
             if isinstance(input_array[0], YTArray):
                 return YTArray(np.array(input_array, dtype=dtype),
                                input_array[0].units)
