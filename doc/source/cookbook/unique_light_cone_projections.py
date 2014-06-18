2324cbd9ca319a8b5c6e29a021f27f4618c55dcc
Cameron Hummels <chummels@gmail.com>
2014-06-18 12:29:40 -0700

    Updating unique_light_cone_projections recipe.
    --HG--
    branch : yt-3.0

--- doc/source/cookbook/unique_light_cone_projections.py
+++ doc/source/cookbook/unique_light_cone_projections.py
@@ -1,5 +1,5 @@
-from yt.mods import *
-from yt.analysis_modules.cosmological_observation.light_cone.api import *
+import yt
+from yt.analysis_modules.cosmological_observation.light_cone.light_cone import LightCone
 
 # Instantiate a light cone.
 lc = LightCone("enzo_tiny_cosmology/32Mpc_32.enzo", 'Enzo', 0, 0.1,
@@ -15,9 +15,9 @@ lc = LightCone("enzo_tiny_cosmology/32Mpc_32.enzo", 'Enzo', 0, 0.1,
 # The recycle=True setting tells the code to first attempt
 # to use solutions with the same projection axes as other
 # solutions.  This will save time when making the projection.
-find_unique_solutions(lc, max_overlap=0.10, failures=50,
-                      seed=123456789, recycle=True,
-                      solutions=10, filename='LC_U/unique.dat')
+yt.find_unique_solutions(lc, max_overlap=0.10, failures=50,
+                         seed=123456789, recycle=True,
+                         solutions=10, filename='LC_U/unique.dat')
 
 # Choose the field to be projected.
 field = 'SZY'
@@ -25,7 +25,7 @@ field = 'SZY'
 # Make light cone projections with each of the random seeds
 # found above.  All output files will be written with unique
 # names based on the random seed numbers.
-project_unique_light_cones(lc, 'LC_U/unique.dat', field,
-                           save_stack=False,
-                           save_final_image=True,
-                           save_slice_images=False)
+yt.project_unique_light_cones(lc, 'LC_U/unique.dat', field,
+                              save_stack=False,
+                              save_final_image=True,
+                              save_slice_images=False)
