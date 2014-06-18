77a23211456525e984047a2821a995840e0571bc
John ZuHone <jzuhone@gmail.com>
2014-06-18 14:25:24 +0100

    This should resolve the FITS answer testing issues.
    --HG--
    branch : yt-3.0

--- yt/frontends/fits/tests/test_outputs.py
+++ yt/frontends/fits/tests/test_outputs.py
@@ -19,13 +19,14 @@ from yt.utilities.answer_testing.framework import \
     small_patch_amr, \
     big_patch_amr, \
     data_dir_load
+from ..data_structures import FITSDataset
 
 _fields = ("intensity")
 
 m33 = "radio_fits/m33_hi.fits"
 @requires_pf(m33, big_data=True)
 def test_m33():
-    pf = data_dir_load(m33, nan_mask=0.0)
+    pf = data_dir_load(m33, cls=FITSDataset, nan_mask=0.0)
     yield assert_equal, str(pf), "m33_hi.fits"
     for test in small_patch_amr(m33, _fields):
         test_m33.__name__ = test.description
@@ -36,7 +37,7 @@ _fields = ("temperature")
 grs = "radio_fits/grs-50-cube.fits"
 @requires_pf(grs)
 def test_grs():
-    pf = data_dir_load(grs, nan_mask=0.0)
+    pf = data_dir_load(grs, cls=FITSDataset, nan_mask=0.0)
     yield assert_equal, str(pf), "grs-50-cube.fits"
     for test in small_patch_amr(grs, _fields):
         test_grs.__name__ = test.description
@@ -47,7 +48,7 @@ _fields = ("x-velocity","y-velocity","z-velocity")
 vf = "UniformGrid/velocity_field_20.fits"
 @requires_pf(vf)
 def test_velocity_field():
-    pf = data_dir_load(bf)
+    pf = data_dir_load(bf, cls=FITSDataset)
     yield assert_equal, str(pf), "velocity_field_20.fits"
     for test in small_patch_amr(vf, _fields):
         test_velocity_field.__name__ = test.description
