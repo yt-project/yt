ac2f8b3901b4fa0f6f68faa9d0fed686d0410d9c
Cameron Hummels <chummels@gmail.com>
2014-06-17 19:25:05 -0700

    Updating simple_phase recipe to set the mass units correctly.
    --HG--
    branch : yt-3.0

--- doc/source/cookbook/simple_phase.py
+++ doc/source/cookbook/simple_phase.py
@@ -13,6 +13,9 @@ my_sphere = ds.sphere("c", (100.0, "kpc"))
 plot = yt.PhasePlot(my_sphere, "density", "temperature", "cell_mass",
                  weight_field=None)
 
+# Set the units of mass to be in solar masses (not the default in cgs)
+plot.set_unit('cell_mass', 'Msun')
+
 # Save the image.
 # Optionally, give a string as an argument
 # to name files with a keyword.
