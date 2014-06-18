f6c373a4fde8cd5567ba7d3a4f8317f901041108
John ZuHone <jzuhone@gmail.com>
2014-06-18 18:04:46 +0100

    These lists were not set up correctly
    --HG--
    branch : yt-3.0

--- yt/frontends/fits/tests/test_outputs.py
+++ yt/frontends/fits/tests/test_outputs.py
@@ -21,35 +21,35 @@ from yt.utilities.answer_testing.framework import \
     data_dir_load
 from ..data_structures import FITSDataset
 
-_fields = ("intensity")
+_fields_m33 = ("intensity",)
 
 m33 = "radio_fits/m33_hi.fits"
 @requires_pf(m33, big_data=True)
 def test_m33():
     pf = data_dir_load(m33, cls=FITSDataset, kwargs={"nan_mask":0.0})
     yield assert_equal, str(pf), "m33_hi.fits"
-    for test in small_patch_amr(m33, _fields):
+    for test in small_patch_amr(m33, _fields_m33, input_center="c", input_weight="ones"):
         test_m33.__name__ = test.description
         yield test
 
-_fields = ("temperature")
+_fields_grs = ("temperature",)
 
 grs = "radio_fits/grs-50-cube.fits"
 @requires_pf(grs)
 def test_grs():
     pf = data_dir_load(grs, cls=FITSDataset, kwargs={"nan_mask":0.0})
     yield assert_equal, str(pf), "grs-50-cube.fits"
-    for test in small_patch_amr(grs, _fields):
+    for test in small_patch_amr(grs, _fields_grs, input_center="c", input_weight="ones"):
         test_grs.__name__ = test.description
         yield test
 
-_fields = ("x-velocity","y-velocity","z-velocity")
+_fields_vels = ("x-velocity","y-velocity","z-velocity")
 
 vf = "UniformGrid/velocity_field_20.fits"
 @requires_pf(vf)
 def test_velocity_field():
     pf = data_dir_load(bf, cls=FITSDataset)
     yield assert_equal, str(pf), "velocity_field_20.fits"
-    for test in small_patch_amr(vf, _fields):
+    for test in small_patch_amr(vf, _fields_vels):
         test_velocity_field.__name__ = test.description
         yield test
