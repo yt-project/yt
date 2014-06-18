6b856ddb5a45915a2ffe23713fb2b3143fd3294d
Cameron Hummels <chummels@gmail.com>
2014-06-18 10:36:34 -0700

    Updating surface_plot recipe.
    --HG--
    branch : yt-3.0

--- doc/source/cookbook/surface_plot.py
+++ doc/source/cookbook/surface_plot.py
@@ -1,20 +1,32 @@
+import yt
+import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
-from yt.mods import *
 
-pf = load("IsolatedGalaxy/galaxy0030/galaxy0030")
-sphere = pf.sphere("max", (1.0, "mpc"))
-surface = pf.surface(sphere, "density", 1e-25)
-colors = apply_colormap(np.log10(surface["temperature"]), cmap_name="hot")
+# Load the dataset
+ds = yt.load("IsolatedGalaxy/galaxy0030/galaxy0030")
 
+# Create a sphere object centered on the highest density point in the simulation
+# with radius 1 Mpc
+sphere = ds.sphere("max", (1.0, "Mpc"))
+
+# Identify the isodensity surface in this sphere with density = 1e-24 g/cm^3
+surface = ds.surface(sphere, "density", 1e-24)
+
+# Color this isodensity surface according to the log of the temperature field
+colors = yt.apply_colormap(np.log10(surface["temperature"]), cmap_name="hot")
+
+# Create a 3D matplotlib figure for visualizing the surface
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 p3dc = Poly3DCollection(surface.triangles, linewidth=0.0)
 
+# Set the surface colors in the right scaling [0,1]
 p3dc.set_facecolors(colors[0,:,:]/255.)
 ax.add_collection(p3dc)
 ax.auto_scale_xyz(surface.vertices[0,:], surface.vertices[1,:], surface.vertices[2,:])
 ax.set_aspect(1.0)
 
-plt.savefig("%s_Surface.png" % pf)
+# Save the figure
+plt.savefig("%s_Surface.png" % ds)
