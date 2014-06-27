d1dce85bfd4ebe3b12b215108292896b2cf22c36
Hilary Egan <hilaryye@gmail.com>
2014-06-27 12:06:23 -0600

    Make annotate particles work with ptype
    --HG--
    branch : yt-3.0

--- yt/visualization/plot_modifications.py
+++ yt/visualization/plot_modifications.py
@@ -958,7 +958,7 @@ class ParticleCallback(PlotCallback):
     region = None
     _descriptor = None
     def __init__(self, width, p_size=1.0, col='k', marker='o', stride=1.0,
-                 ptype=None, stars_only=False, dm_only=False,
+                 ptype='all', stars_only=False, dm_only=False,
                  minimum_mass=None, alpha=1.0):
         PlotCallback.__init__(self)
         self.width = width
@@ -988,11 +988,12 @@ class ParticleCallback(PlotCallback):
         axis_names = plot.data.pf.coordinates.axis_name
         field_x = "particle_position_%s" % axis_names[xax]
         field_y = "particle_position_%s" % axis_names[yax]
-        gg = ( ( reg[field_x] >= x0 ) & ( reg[field_x] <= x1 )
-           &   ( reg[field_y] >= y0 ) & ( reg[field_y] <= y1 ) )
-        if self.ptype is not None:
-            gg &= (reg["particle_type"] == self.ptype)
-            if gg.sum() == 0: return
+        pt = self.ptype
+        gg = ( ( reg[pt, field_x] >= x0 ) & ( reg[pt, field_x] <= x1 )
+           &   ( reg[pt, field_y] >= y0 ) & ( reg[pt, field_y] <= y1 ) )
+        #if self.ptype is not None:
+        #    gg &= (reg["particle_type"] == self.ptype)
+        #    if gg.sum() == 0: return
         if self.stars_only:
             gg &= (reg["creation_time"] > 0.0)
             if gg.sum() == 0: return
@@ -1004,8 +1005,8 @@ class ParticleCallback(PlotCallback):
             if gg.sum() == 0: return
         plot._axes.hold(True)
         px, py = self.convert_to_plot(plot,
-                    [np.array(reg[field_x][gg][::self.stride]),
-                     np.array(reg[field_y][gg][::self.stride])])
+                    [np.array(reg[pt, field_x][gg][::self.stride]),
+                     np.array(reg[pt, field_y][gg][::self.stride])])
         plot._axes.scatter(px, py, edgecolors='None', marker=self.marker,
                            s=self.p_size, c=self.color,alpha=self.alpha)
         plot._axes.set_xlim(xx0,xx1)
