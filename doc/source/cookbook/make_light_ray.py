031de596d46065ce704a3c253f033d934d072e6a
Britton Smith <brittonsmith@gmail.com>
2014-07-05 17:05:48 +0100

    Updating lightray recipe.
    --HG--
    branch : yt-3.0

--- doc/source/cookbook/make_light_ray.py
+++ doc/source/cookbook/make_light_ray.py
@@ -1,10 +1,6 @@
-### THIS RECIPE IS CURRENTLY BROKEN IN YT-3.0
-### DO NOT TRUST THIS RECIPE UNTIL THIS LINE IS REMOVED
-
 import os
 import sys
 import yt
-from yt.analysis_modules.halo_profiler.api import HaloProfiler
 from yt.analysis_modules.cosmological_observation.light_ray.light_ray import \
      LightRay
 
@@ -19,51 +15,11 @@ lr = LightRay("enzo_tiny_cosmology/32Mpc_32.enzo",
               use_minimum_datasets=True,
               time_data=False)
 
-# Configure the HaloProfiler.
-# These are keyword arguments given when creating a
-# HaloProfiler object.
-halo_profiler_kwargs = {'halo_list_file': 'HopAnalysis.out',
-                        'output_dir' : 'halo_analysis'}
-
-# Create a list of actions for the HaloProfiler to take.
-halo_profiler_actions = []
-
-# Each item in the list is a dictionary containing three things:
-# 1. 'function' - the function to be called.
-# 2. 'args' - a list of arguments given with the function.
-# 3. 'kwargs' - a dictionary of keyword arguments.
-
-# Add a virial filter.
-halo_profiler_actions.append({'function': HaloProfiler.add_halo_filter,
-                              'args': [VirialFilter],
-                              'kwargs': {'must_be_virialized':False,
-                                         'overdensity_field':'ActualOverdensity',
-                                         'virial_overdensity':100,
-                                         'virial_filters':[['TotalMassMsun','>','1e5']],
-                                         'virial_quantities':['TotalMassMsun','RadiusMpc']}})
-
-# Add a call to make the profiles.
-halo_profiler_actions.append({'function': HaloProfiler.make_profiles,
-                              'kwargs': {'filename': "VirializedHalos.out"}})
-
-# Specify the desired halo list is the filtered list.
-# If 'all' is given instead, the full list will be used.
-halo_list = 'filtered'
-
-# Put them all into one dictionary.
-halo_profiler_parameters=dict(halo_profiler_kwargs=halo_profiler_kwargs,
-                              halo_profiler_actions=halo_profiler_actions,
-                              halo_list=halo_list)
-
 # Make a light ray, and set njobs to -1 to use one core
 # per dataset.
 lr.make_light_ray(seed=123456789,
                   solution_filename='LR/lightraysolution.txt',
                   data_filename='LR/lightray.h5',
                   fields=['temperature', 'density'],
-                  get_nearest_halo=True,
-                  nearest_halo_fields=['TotalMassMsun_100',
-                                       'RadiusMpc_100'],
-                  halo_profiler_parameters=halo_profiler_parameters,
                   get_los_velocity=True,
                   njobs=-1)
