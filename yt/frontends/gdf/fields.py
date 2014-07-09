c709e48b12d9f962dc2372116f24d25a5c90f3d3
John ZuHone <jzuhone@gmail.com>
2014-07-09 10:06:36 -0500

    Updated field definitions
    --HG--
    branch : yt-3.0

--- yt/frontends/gdf/fields.py
+++ yt/frontends/gdf/fields.py
@@ -25,13 +25,14 @@ from yt.fields.field_info_container import \
 class GDFFieldInfo(FieldInfoContainer):
     known_other_fields = (
         ("density", ("g/cm**3", ["density"], None)),
-        ("specific_energy", ("erg / g", ["thermal_energy"], None)),
-        ("pressure", ("", ["pressure"], None)),
+        ("specific_energy", ("erg/g", ["thermal_energy"], None)),
+        ("pressure", ("erg/cm**3", ["pressure"], None)),
+        ("temperature", ("K", ["temperature"], None)),
         ("velocity_x", ("cm/s", ["velocity_x"], None)),
         ("velocity_y", ("cm/s", ["velocity_y"], None)),
         ("velocity_z", ("cm/s", ["velocity_z"], None)),
-        ("mag_field_x", ("gauss", ["magnetic_field_x"], None)),
-        ("mag_field_y", ("gauss", ["magnetic_field_y"], None)),
-        ("mag_field_z", ("gauss", ["magnetic_field_z"], None)),
+        ("magnetic_field_x", ("gauss", ["magnetic_field_x"], None)),
+        ("magnetic_field_y", ("gauss", ["magnetic_field_y"], None)),
+        ("magnetic_field_z", ("gauss", ["magnetic_field_z"], None)),
     )
     known_particle_fields = ()
