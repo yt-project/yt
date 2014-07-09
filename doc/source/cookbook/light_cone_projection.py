8cc05cd6532f2ea43a2989015479d8be55f7df05
Britton Smith <brittonsmith@gmail.com>
2014-07-09 16:07:47 +0100

    Updating light cone recipe.
    --HG--
    branch : yt-3.0

--- doc/source/cookbook/light_cone_projection.py
+++ doc/source/cookbook/light_cone_projection.py
@@ -1,12 +1,8 @@
-### THIS RECIPE IS CURRENTLY BROKEN IN YT-3.0
-### DO NOT TRUST THIS RECIPE UNTIL THIS LINE IS REMOVED
-
 import yt
-from yt.analysis_modules.cosmological_observation.light_cone.light_cone import LightCone
+from yt.analysis_modules.cosmological_observation.light_cone.light_cone import \
+     LightCone
 
-# Create a LightCone object extending from z = 0 to z = 0.1
-# with a 600 arcminute field of view and a resolution of
-# 60 arcseconds.
+# Create a LightCone object extending from z = 0 to z = 0.1.
 
 # We have already set up the redshift dumps to be
 # used for this, so we will not use any of the time
@@ -14,20 +10,20 @@ from yt.analysis_modules.cosmological_observation.light_cone.light_cone import L
 lc = LightCone('enzo_tiny_cosmology/32Mpc_32.enzo',
                'Enzo', 0., 0.1,
                observer_redshift=0.0,
-               field_of_view_in_arcminutes=600.0,
-               image_resolution_in_arcseconds=60.0,
                time_data=False)
 
 # Calculate a randomization of the solution.
 lc.calculate_light_cone_solution(seed=123456789)
 
 # Choose the field to be projected.
-field = 'SZY'
+field = 'szy'
 
+# Use the LightCone object to make a projection with a 600 arcminute 
+# field of view and a resolution of 60 arcseconds.
 # Set njobs to -1 to have one core work on each projection
-# in parallel.  Set save_slice_images to True to see an
-# image for each individual slice.
-lc.project_light_cone(field, save_stack=False,
+# in parallel.
+lc.project_light_cone((600.0, "arcmin"), (60.0, "arcsec"), field,
+                      save_stack=True,
                       save_final_image=True,
-                      save_slice_images=False,
+                      save_slice_images=True,
                       njobs=-1)
