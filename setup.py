d93800de11f7faabf8d6a1d3349220473a86beb2
Sam Skillman <samskillman@gmail.com>
2014-07-12 11:51:05 -0500

    Adding install_requires of thingking.
    --HG--
    branch : yt-3.0

--- setup.py
+++ setup.py
@@ -279,6 +279,7 @@ def setup_package():
         data_files=REASON_FILES,
         cmdclass={'build_py': my_build_py, 'build_forthon': BuildForthon,
                   'build_src': my_build_src, 'install_data': my_install_data},
+        install_requires=["thingking"],
     )
     return
 
