8d1fdaa1396269b53eb4090171970f6d22b95758
Britton Smith <brittonsmith@gmail.com>
2014-07-19 17:05:32 -0500

    Updating recipe.
    --HG--
    branch : yt-3.0

--- doc/source/cookbook/find_clumps.py
+++ doc/source/cookbook/find_clumps.py
@@ -1,35 +1,24 @@
-### THIS RECIPE IS CURRENTLY BROKEN IN YT-3.0
-### DO NOT TRUST THIS RECIPE UNTIL THIS LINE IS REMOVED
-
 import numpy as np
 
 import yt
-from yt.analysis_modules.level_sets.api import (Clump, find_clumps,
-                                                get_lowest_clumps,
-                                                write_clump_index,
-                                                write_clumps)
+from yt.analysis_modules.level_sets.api import *
 
-fn = "IsolatedGalaxy/galaxy0030/galaxy0030"  # parameter file to load
-# this is the field we look for contours over -- we could do
-# this over anything.  Other common choices are 'AveragedDensity'
-# and 'Dark_Matter_Density'.
-field = "density"
+fn = "IsolatedGalaxy/galaxy0030/galaxy0030" # parameter file to load
+field = "Density" # this is the field we look for contours over -- we could do
+                  # this over anything.  Other common choices are 'AveragedDensity'
+                  # and 'Dark_Matter_Density'.
+step = 2.0 # This is the multiplicative interval between contours.
 
-step = 2.0  # This is the multiplicative interval between contours.
+ds = yt.load(fn) # load data
 
-ds = yt.load(fn)  # load data
-
-# We want to find clumps over the entire dataset, so we'll just grab the whole
-# thing!  This is a convenience parameter that prepares an object that covers
-# the whole domain.  Note, though, that it will load on demand and not before!
-data_source = ds.disk([0.5, 0.5, 0.5], [0., 0., 1.],
-                      (1., 'kpc'), (1., 'kpc'))
+data_source = ds.disk([0.5, 0.5, 0.5], [0., 0., 1.], 
+                      (8, 'kpc'), (1, 'kpc'))
 
 # Now we set some sane min/max values between which we want to find contours.
 # This is how we tell the clump finder what to look for -- it won't look for
 # contours connected below or above these threshold values.
-c_min = 10**np.floor(np.log10(data_source[field]).min())
-c_max = 10**np.floor(np.log10(data_source[field]).max() + 1)
+c_min = 10**np.floor(np.log10(data_source[field]).min()  )
+c_max = 10**np.floor(np.log10(data_source[field]).max()+1)
 
 # keep only clumps with at least 20 cells
 function = 'self.data[\'%s\'].size > 20' % field
@@ -46,22 +35,18 @@ find_clumps(master_clump, c_min, c_max, step)
 
 # As it goes, it appends the information about all the sub-clumps to the
 # master-clump.  Among different ways we can examine it, there's a convenience
-# function for outputting the full index to a file.
-f = open('%s_clump_index.txt' % ds, 'w')
-write_clump_index(master_clump, 0, f)
-f.close()
+# function for outputting the full hierarchy to a file.
+write_clump_index(master_clump, 0, "%s_clump_hierarchy.txt" % ds)
 
 # We can also output some handy information, as well.
-f = open('%s_clumps.txt' % ds, 'w')
-write_clumps(master_clump, 0, f)
-f.close()
+write_clumps(master_clump,0, "%s_clumps.txt" % ds)
 
-# We can traverse the clump index to get a list of all of the 'leaf' clumps
+# We can traverse the clump hierarchy to get a list of all of the 'leaf' clumps
 leaf_clumps = get_lowest_clumps(master_clump)
 
 # If you'd like to visualize these clumps, a list of clumps can be supplied to
 # the "clumps" callback on a plot.  First, we create a projection plot:
-prj = yt.ProjectionPlot(ds, 2, field, center='c', width=(20, 'kpc'))
+prj = yt.ProjectionPlot(ds, 2, field, center='c', width=(20,'kpc'))
 
 # Next we annotate the plot with contours on the borders of the clumps
 prj.annotate_clumps(leaf_clumps)
