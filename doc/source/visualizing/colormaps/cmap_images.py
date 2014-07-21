8cf5e75f68cd9ff6c9143afe157a5b668e36a647
Nathan Goldbaum <ngoldbau@ucsc.edu>
2014-07-21 15:32:52 -0700

    Removing unnecessary path indirection from cmap_images.py.
    --HG--
    branch : yt-3.0

--- doc/source/visualizing/colormaps/cmap_images.py
+++ doc/source/visualizing/colormaps/cmap_images.py
@@ -2,7 +2,7 @@ import yt
 import matplotlib.cm as cm
 
 # Load the dataset.
-ds = yt.load(os.path.join(ytcfg.get("yt", "test_data_dir"), "IsolatedGalaxy/galaxy0030/galaxy0030"))
+ds = yt.load("IsolatedGalaxy/galaxy0030/galaxy0030")
 
 # Create projections using each colormap available.
 p = yt.ProjectionPlot(ds, "z", "density", weight_field = "density", width=0.4)
