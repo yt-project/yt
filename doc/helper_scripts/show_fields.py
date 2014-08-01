218d3fb179536e82cb3e32c6284a439f7be24b2f
Matthew Turk <matthewturk@gmail.com>
2014-08-01 08:14:22 -0500

    Updating show_fields.py script
    --HG--
    branch : yt-3.0

--- doc/helper_scripts/show_fields.py
+++ doc/helper_scripts/show_fields.py
@@ -1,10 +1,55 @@
 import inspect
 from yt.mods import *
+from yt.testing import *
+import numpy as np
+from yt.utilities.cosmology import \
+     Cosmology
+from yt.utilities.definitions import \
+    mpc_conversion, sec_conversion
+from yt.frontends.stream.fields import \
+    StreamFieldInfo
+from yt.fields.derived_field import NullFunc
+from yt.units.yt_array import YTArray
 
+fields, units = [], []
 
-def islambda(f):
-    return inspect.isfunction(f) and \
-           f.__name__ == (lambda: True).__name__
+for fname, (code_units, aliases, dn) in StreamFieldInfo.known_other_fields:
+    fields.append(("gas", fname))
+    units.append(code_units)
+base_ds = fake_random_ds(4, fields = fields, units = units)
+base_ds.index
+base_ds.cosmological_simulation = 1
+base_ds.cosmology = Cosmology()
+from yt.config import ytcfg
+ytcfg["yt","__withintesting"] = "True"
+np.seterr(all = 'ignore')
+
+def _strip_ftype(field):
+    if not isinstance(field, tuple):
+        return field
+    elif field[0] == "all":
+        return field
+    return field[1]
+
+np.random.seed(int(0x4d3d3d3))
+units = [base_ds._get_field_info(*f).units for f in fields]
+fields = [_strip_ftype(f) for f in fields]
+ds = fake_random_ds(16, fields = fields, units = units)
+ds.parameters["HydroMethod"] = "streaming"
+ds.parameters["EOSType"] = 1.0
+ds.parameters["EOSSoundSpeed"] = 1.0
+ds.conversion_factors["Time"] = 1.0
+ds.conversion_factors.update( dict((f, 1.0) for f in fields) )
+ds.gamma = 5.0/3.0
+ds.current_redshift = 0.0001
+ds.cosmological_simulation = 1
+ds.hubble_constant = 0.7
+ds.omega_matter = 0.27
+ds.omega_lambda = 0.73
+ds.cosmology = Cosmology(hubble_constant=ds.hubble_constant,
+                         omega_matter=ds.omega_matter,
+                         omega_lambda=ds.omega_lambda,
+                         unit_registry=ds.unit_registry)
 
 header = r"""
 .. _field-list:
@@ -12,10 +57,10 @@ header = r"""
 Field List
 ==========
 
-This is a list of all fields available in ``yt``.  It has been organized by the
-type of code that each field is supported by.  "Universal" fields are available
-everywhere, "Enzo" fields in Enzo datasets, "Orion" fields in Orion datasets,
-and so on.
+This is a list of many of the fields available in ``yt``.  We have attempted to
+include most of the fields that are accessible through the plugin system,
+however it is possible to generate many more permutations, particularly through
+vector operations.
 
 Try using the ``ds.field_list`` and ``ds.derived_field_list`` to view the
 native and derived fields available for your dataset respectively. For example
@@ -28,41 +73,27 @@ to display the native fields in alphabetical order:
   for i in sorted(ds.field_list):
     print i
 
-.. note:: Universal fields will be overridden by a code-specific field.
-
-.. rubric:: Table of Contents
-
-.. contents::
-   :depth: 2
-   :local:
-   :backlinks: none
 """
 
 print header
 
 seen = []
 
-
 def print_all_fields(fl):
     for fn in sorted(fl):
         df = fl[fn]
         f = df._function
-        cv = df._convert_function
-        if [f, cv] in seen:
-            continue
-        seen.append([f, cv])
-        print "%s" % (df.name)
-        print "+" * len(df.name)
+        s = "%s" % (df.name,)
+        print s
+        print "+" * len(s)
         print
-        if len(df._units) > 0:
-            print "   * Units: :math:`%s`" % (df._units)
-        if len(df._projected_units) > 0:
-            print "   * Projected Units: :math:`%s`" % (df._projected_units)
+        if len(df.units) > 0:
+            print "   * Units: :math:`%s`" % (df.units)
         print "   * Particle Type: %s" % (df.particle_type)
         print
         print "**Field Source**"
         print
-        if islambda(f):
+        if f == NullFunc:
             print "No source available."
             print
             continue
@@ -72,66 +103,6 @@ def print_all_fields(fl):
             for line in inspect.getsource(f).split("\n"):
                 print "  " + line
             print
-        print "**Convert Function Source**"
-        print
-        if islambda(cv):
-            print "No source available."
-            print
-            continue
-        else:
-            print ".. code-block:: python"
-            print
-            for line in inspect.getsource(cv).split("\n"):
-                print "  " + line
-            print
 
-
-print "Universal Field List"
-print "--------------------"
-print
-print_all_fields(FieldInfo)
-
-print "Enzo-Specific Field List"
-print "------------------------"
-print
-print_all_fields(EnzoFieldInfo)
-
-print "Orion-Specific Field List"
-print "-------------------------"
-print
-print_all_fields(OrionFieldInfo)
-
-print "FLASH-Specific Field List"
-print "-------------------------"
-print
-print_all_fields(FLASHFieldInfo)
-
-print "Athena-Specific Field List"
-print "--------------------------"
-print
-print_all_fields(AthenaFieldInfo)
-
-print "Nyx-Specific Field List"
-print "-----------------------"
-print
-print_all_fields(NyxFieldInfo)
-
-print "Chombo-Specific Field List"
-print "--------------------------"
-print
-print_all_fields(ChomboFieldInfo)
-
-print "Pluto-Specific Field List"
-print "--------------------------"
-print
-print_all_fields(PlutoFieldInfo)
-
-print "Grid-Data-Format-Specific Field List"
-print "------------------------------------"
-print
-print_all_fields(GDFFieldInfo)
-
-print "Generic-Format (Stream) Field List"
-print "----------------------------------"
-print
-print_all_fields(StreamFieldInfo)
+ds.index
+print_all_fields(ds.field_info)
