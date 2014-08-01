611be33d24105767be3c6a1a60a46c1bb7f264cd
Matthew Turk <matthewturk@gmail.com>
2014-08-01 13:22:30 -0500

    This last little bit fixes the clumps.
    --HG--
    branch : yt-3.0

--- yt/analysis_modules/level_sets/contour_finder.py
+++ yt/analysis_modules/level_sets/contour_finder.py
@@ -34,13 +34,14 @@ def identify_contours(data_source, field, min_val, max_val,
     DLE = data_source.ds.domain_left_edge
     total_vol = None
     selector = getattr(data_source, "base_object", data_source).selector
+    masks = dict((g.id, m) for g, m in data_source.blocks)
     for (g, node, (sl, dims, gi)) in data_source.tiles.slice_traverse():
         node.node_ind = len(node_ids)
         nid = node.node_id
         node_ids.append(nid)
         values = g[field][sl].astype("float64")
         contour_ids = np.zeros(dims, "int64") - 1
-        mask = selector.fill_mask(g)[sl].astype("uint8")
+        mask = masks[g.id][sl].astype("uint8")
         total_contours += gct.identify_contours(values, contour_ids,
                                                 mask, total_contours)
         new_contours = tree.cull_candidates(contour_ids)
