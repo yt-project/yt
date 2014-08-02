7adbc3196fd9da8de44f664e8ba6d333aac126d5
Matthew Turk <matthewturk@gmail.com>
2014-08-01 22:11:21 -0500

    Make test_connected_sets consistent with find_clumps.
    This change doesn't make much of a difference, since we're just testing
    internal consistency.
    
    --HG--
    branch : yt-3.0

--- yt/data_objects/tests/test_connected_sets.py
+++ yt/data_objects/tests/test_connected_sets.py
@@ -8,6 +8,6 @@ g30 = "IsolatedGalaxy/galaxy0030/galaxy0030"
 def test_connected_sets():
     ds = data_dir_load(g30)
     data_source = ds.disk([0.5, 0.5, 0.5], [0., 0., 1.],
-                          (1, 'kpc'), (1, 'kpc'))
+                          (8, 'kpc'), (1, 'kpc'))
     yield ExtractConnectedSetsTest(g30, data_source, ("gas", "density"),
                                    5, 1e-24, 8e-24)
