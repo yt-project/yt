d6b954fa09529a18acefc107ce805ce365aeb259
John ZuHone <jzuhone@gmail.com>
2014-08-06 14:50:41 -0400

    Using different datasets for the answer tests
    --HG--
    branch : yt

--- yt/frontends/athena/tests/test_outputs.py
+++ yt/frontends/athena/tests/test_outputs.py
@@ -21,28 +21,20 @@ from yt.utilities.answer_testing.framework import \
     data_dir_load
 from yt.frontends.athena.api import AthenaDataset
 
-parameters_sloshing = {"time_unit":(1.0,"Myr"),
-                       "length_unit":(1.0,"Mpc"),
-                       "mass_unit":(1.0e14,"Msun")}
+_fields_cloud = ("scalar[0]", "density", "total_energy")
 
-parameters_stripping = {"time_unit":3.086e14,
-                        "length_unit":8.0236e22,
-                        "mass_unit":9.999e-30*8.0236e22**3}
-
-_fields_sloshing = ("temperature", "density", "magnetic_energy")
-
-sloshing = "MHDSloshing/id0/virgo_cluster.0055.vtk"
-@requires_ds(sloshing, big_data=True)
-def test_sloshing():
-    ds = data_dir_load(sloshing, kwargs={"parameters":parameters_sloshing})
-    yield assert_equal, str(ds), "virgo_cluster.0055"
-    for test in small_patch_amr(sloshing, _fields_sloshing):
-        test_sloshing.__name__ = test.description
+cloud = "ShockCloud/id0/Cloud.0050.vtk"
+@requires_ds(cloud)
+def test_cloud():
+    ds = data_dir_load(cloud)
+    yield assert_equal, str(ds), "Cloud.0050"
+    for test in small_patch_amr(cloud, _fields_cloud):
+        test_cloud.__name__ = test.description
         yield test
 
 _fields_blast = ("temperature", "density", "velocity_magnitude")
 
-blast = "MHDBlast/Blast.0100.vtk"
+blast = "MHDBlast/id0/Blast.0100.vtk"
 @requires_ds(blast)
 def test_blast():
     ds = data_dir_load(blast)
@@ -50,6 +42,10 @@ def test_blast():
     for test in small_patch_amr(blast, _fields_blast):
         test_blast.__name__ = test.description
         yield test
+
+parameters_stripping = {"time_unit":3.086e14,
+                        "length_unit":8.0236e22,
+                        "mass_unit":9.999e-30*8.0236e22**3}
 
 _fields_stripping = ("temperature", "density", "specific_scalar[0]")
 
