621a749731172adca0ee660938941fdd97e7b099
Douglas Rudd <drudd@uchicago.edu>
2014-09-05 16:42:23 -0500

    Removed unnecessary alpha
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/transfer_functions.py
+++ yt/visualization/volume_rendering/transfer_functions.py
@@ -403,7 +403,6 @@ class ColorTransferFunction(MultiVariateTransferFunction):
         >>> tf = ColorTransferFunction( (-10.0, -5.0) )
         >>> tf.add_gaussian(-9.0, 0.01, [1.0, 0.0, 0.0, 1.0])
         """
-        alpha = height[3]
         for tf, v in zip(self.funcs, height):
             tf.add_gaussian(location, width, v)
 
