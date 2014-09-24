7d9cd288d006ad7bd1cfb1f25f3370a60a8026b9
Kacper Kowalik <xarthisius.kk@gmail.com>
2014-09-24 13:42:00 -0500

    revert autopep8 changes
    --HG--
    branch : yt

--- yt/frontends/gdf/data_structures.py
+++ yt/frontends/gdf/data_structures.py
@@ -5,13 +5,13 @@ Data structures for GDF.
 
 """
 
-# ----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 # Copyright (c) 2013, yt Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
-# ----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 
 import h5py
 import types
