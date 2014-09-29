aeb342a74c3096d82360d6041cb1105e3deb2620
Cameron Hummels <chummels@gmail.com>
2014-09-29 08:02:13 -0700

    Removing unnecessary change to QuadTree cython code.
    --HG--
    branch : yt

--- yt/utilities/lib/QuadTree.pyx
+++ yt/utilities/lib/QuadTree.pyx
@@ -113,9 +113,7 @@ cdef class QuadTree:
     cdef np.int64_t last_dims[2]
 
     def __cinit__(self, np.ndarray[np.int64_t, ndim=1] top_grid_dims,
-                  int nvals, bounds, style = None, method = "integrate"):
-        if style is not None:
-            method = style
+                  int nvals, bounds, method = "integrate"):
         if method == "integrate":
             self.combine = QTN_add_value
         elif method == "mip":
