Fixing perspective rendering, some units issues
--HG--
branch : yt-3.0

yt/visualization/volume_rendering/camera.py
@@ -42,7 +42,7 @@ class Camera(Orientation):
         self.set_lens(lens_type)
         if data_source is not None:
             data_source = data_source_or_all(data_source)
-            self.set_defaults_from_data_source(data_source)
+        self.set_defaults_from_data_source(data_source)
 
         super(Camera, self).__init__(self.focus - self.position,
                                      self.north_vector, steady_north=False)
@@ -71,9 +71,11 @@ class Camera(Orientation):
         if iterable(width) and len(width) > 1 and isinstance(width[1], str):
             width = data_source.pf.quan(width[0], input_units=width[1])
             # Now convert back to code length for subsequent manipulation
-            width = width.in_units("code_length").value
+            width = width.in_units("code_length")  # .value
         if not iterable(width):
-            width = (width, width, width)  # left/right, top/bottom, front/back
+            width = data_source.pf.arr([width, width, width],
+                                       input_units='code_length')
+            # left/right, top/bottom, front/back
         if not isinstance(width, YTArray):
             width = data_source.pf.arr(width, input_units="code_length")
         if not isinstance(focus, YTArray):
@@ -88,7 +90,7 @@ class Camera(Orientation):
 
     def set_width(self, width):
         if not iterable(width):
-            width = [width, width, width] # No way to get code units.
+            width = np.array([width, width, width])  # Can't get code units.
         self.width = width
         self.switch_orientation()
 

yt/visualization/volume_rendering/tests/test_lenses.py
@@ -21,35 +21,39 @@ from yt.visualization.volume_rendering.render_source import VolumeSource
 from time import time
 
 #pf = fake_random_pf(8)
-#w = (pf.domain_width[0]*30).in_units('code_length')
-#sc = Scene()
-#cam = Camera(pf, lens_type='perspective')
-#print "WIDTH: ", w
-#cam.set_width(w)
-#vol = VolumeSource(pf, field=('gas', 'density'))
-#sc.set_default_camera(cam)
-#sc.add_source(vol)
-#
-#t = -time()
-#sc.render('test_perspective.png', clip_ratio=None)
-#t += time()
-#print 'Total time: %e' % t
+pf = load('/home/skillman/kipac/data/IsolatedGalaxy/galaxy0030/galaxy0030')
+w = (pf.domain_width*30).in_units('code_length')
+w = pf.arr(w, 'code_length')
+sc = Scene()
+cam = Camera(pf, lens_type='perspective')
+print "WIDTH: ", w
+cam.set_width(w)
+vol = VolumeSource(pf, field=('gas', 'density'))
+sc.set_default_camera(cam)
+sc.add_source(vol)
+
+t = -time()
+sc.render('test_perspective.png', clip_ratio=None)
+t += time()
+print 'Total time: %e' % t
 
 pf = load('/home/skillman/kipac/data/IsolatedGalaxy/galaxy0030/galaxy0030')
 ds = pf.h.sphere(pf.domain_center, pf.domain_width[0] / 100)
 sc = Scene()
 cam = Camera(pf, lens_type='fisheye')
-cam.lens.fov=180.0
-cam.resolution=(512,512)
-cam.set_width(1.0)
-v,c = pf.find_max('density')
+cam.lens.fov = 180.0
+cam.resolution = (512,512)
+cam.set_width(pf.domain_width)
+v, c = pf.find_max('density')
+c = pf.arr(c, 'code_length')
 p = pf.domain_center.copy()
-cam.set_position(c)
+cam.set_position(c-0.05*pf.domain_width)
 #pf.field_info[('gas','density')].take_log=False
 vol = VolumeSource(pf, field=('gas', 'density'))
 tf = vol.transfer_function
-tf.grey_opacity=True
-#tf.map_to_colormap(tf.x_bounds[0], tf.x_bounds[1], scale=3000.0, colormap='RdBu')
+tf.grey_opacity = True
+# tf.map_to_colormap(tf.x_bounds[0], tf.x_bounds[1], scale=3000.0, 
+#                    colormap='RdBu')
 sc.set_default_camera(cam)
 sc.add_source(vol)
 