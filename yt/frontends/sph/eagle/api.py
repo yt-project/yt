2ebe8f9d8dbf8377faafa8f18dd86692ef42f53b
Britton Smith <brittonsmith@gmail.com>
2014-10-22 11:07:41 -0400

    Moving EAGLE frontend to its own directory.
    --HG--
    branch : yt

---
+++ yt/frontends/sph/eagle/api.py
@@ -0,0 +1,19 @@
+"""
+API for EAGLE frontend
+
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2014, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from .data_structures import \
+    EagleDataset, \
+    EagleNetworkDataset
