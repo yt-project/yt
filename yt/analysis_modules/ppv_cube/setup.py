dbc49477459e95be8cb09114c1c18060b5345d5d
John ZuHone <jzuhone@gmail.com>
2014-10-24 13:21:11 -0400

    Adding a PPVCube test.
    --HG--
    branch : yt

--- yt/analysis_modules/ppv_cube/setup.py
+++ yt/analysis_modules/ppv_cube/setup.py
@@ -11,7 +11,7 @@ def configuration(parent_package='', top_path=None):
     config.add_extension("ppv_utils", 
                          ["yt/analysis_modules/ppv_cube/ppv_utils.pyx"],
                          libraries=["m"])
-    #config.add_subpackage("tests")
+    config.add_subpackage("tests")
     config.make_config_py()  # installs __config__.py
     #config.make_svn_version_py()
     return config
