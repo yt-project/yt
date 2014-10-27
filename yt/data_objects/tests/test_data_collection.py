7da4047572ee151a16371a9e2d3e83e0e0520ae6
Douglas Rudd <drudd@uchicago.edu>
2014-10-27 14:04:19 -0500

    Fix test_data_collection test
    --HG--
    branch : yt

--- yt/data_objects/tests/test_data_collection.py
+++ yt/data_objects/tests/test_data_collection.py
@@ -8,7 +8,7 @@ def test_data_collection():
     # We decompose in different ways
     for nprocs in [1, 2, 4, 8]:
         ds = fake_random_ds(16, nprocs = nprocs)
-        coll = ds.data_collection(ds.domain_center, ds.index.grids)
+        coll = ds.data_collection(ds.index.grids)
         crho = coll["density"].sum(dtype="float64").to_ndarray()
         grho = np.sum([g["density"].sum(dtype="float64") for g in ds.index.grids],
                       dtype="float64")
