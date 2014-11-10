d06621c7cc50c06c12073e31b50afd8eec168b79
John ZuHone <jzuhone@gmail.com>
2014-11-09 20:23:39 -0500

    Chunking for photon_simulator
    --HG--
    branch : yt

--- yt/analysis_modules/photon_simulator/photon_models.py
+++ yt/analysis_modules/photon_simulator/photon_models.py
@@ -26,12 +26,11 @@ import numpy as np
 from yt.funcs import *
 from yt.utilities.physical_constants import mp, kboltz
 from yt.utilities.parallel_tools.parallel_analysis_interface import \
-     communication_system
-from yt import units
+     communication_system, parallel_objects
 
-N_TBIN = 10000
-TMIN = 8.08e-2
-TMAX = 50.
+n_kT = 10000
+kT_min = 8.08e-2
+kT_max = 50.
 
 comm = communication_system.communicators[-1]
 
@@ -77,129 +76,125 @@ class ThermalPhotonModel(PhotonModel):
                 
         vol_scale = 1.0/np.prod(ds.domain_width.in_cgs().to_ndarray())
 
-        num_cells = data_source["temperature"].shape[0]
-        start_c = comm.rank*num_cells/comm.size
-        end_c = (comm.rank+1)*num_cells/comm.size
-        
-        kT = data_source["kT"][start_c:end_c].v
-        vol = data_source["cell_volume"][start_c:end_c].in_cgs().v
-        EM = (data_source["density"][start_c:end_c]/mp).v**2
-        EM *= 0.5*(1.+self.X_H)*self.X_H*vol
+        my_kT_min, my_kT_max = data_source.quantities.extrema("kT")
 
-        data_source.clear_data()
-
-        if isinstance(self.Zmet, basestring):
-            metalZ = data_source[self.Zmet][start_c:end_c].to_ndarray()
-        else:
-            metalZ = self.Zmet*np.ones(EM.shape)
-        
-        data_source.clear_data()
-
-        idxs = np.argsort(kT)
-        dshape = idxs.shape
-
-        kT_bins = np.linspace(TMIN, max(kT[idxs][-1], TMAX), num=N_TBIN+1)
-        dkT = kT_bins[1]-kT_bins[0]
-        kT_idxs = np.digitize(kT[idxs], kT_bins)
-        kT_idxs = np.minimum(np.maximum(1, kT_idxs), N_TBIN) - 1
-        bcounts = np.bincount(kT_idxs).astype("int")
-        bcounts = bcounts[bcounts > 0]
-        n = int(0)
-        bcell = []
-        ecell = []
-        for bcount in bcounts:
-            bcell.append(n)
-            ecell.append(n+bcount)
-            n += bcount
-        kT_idxs = np.unique(kT_idxs)
-        
         self.spectral_model.prepare()
         energy = self.spectral_model.ebins
-    
-        cell_em = EM[idxs]*vol_scale
-    
-        number_of_photons = np.zeros(dshape, dtype='uint64')
-        energies = []
-    
-        u = np.random.random(cell_em.shape)
+
+        citer = self.data_source.chunks(["kT","cell_volume","density"], "io")
+        num_chunks = len(citer)
+        pbar = get_pbar("Generating Photons", num_chunks)
+
+        ck = 0
+
+        photons = {}
+        photons["x"] = []
+        photons["y"] = []
+        photons["z"] = []
+        photons["vx"] = []
+        photons["vy"] = []
+        photons["vz"] = []
+        photons["dx"] = []
+        photons["Energy"] = []
+        photons["NumberOfPhotons"] = []
+
+        for chunk in parallel_objects(citer):
+
+            kT = chunk["kT"].v
+            vol = chunk["cell_volume"].in_cgs().v
+            EM = (chunk["density"]/mp).v**2
+            EM *= 0.5*(1.+self.X_H)*self.X_H*vol
+
+            if isinstance(self.Zmet, basestring):
+                metalZ = chunk[self.Zmet].v
+            else:
+                metalZ = self.Zmet*chunk["ones"]
+
+            idxs = np.argsort(kT)
+            dshape = idxs.shape
+
+            kT_bins = np.linspace(kT_min, max(my_kT_max, kT_max), num=n_kT+1)
+            dkT = kT_bins[1]-kT_bins[0]
+            kT_idxs = np.digitize(kT[idxs], kT_bins)
+            kT_idxs = np.minimum(np.maximum(1, kT_idxs), n_kT) - 1
+            bcounts = np.bincount(kT_idxs).astype("int")
+            bcounts = bcounts[bcounts > 0]
+            n = int(0)
+            bcell = []
+            ecell = []
+            for bcount in bcounts:
+                bcell.append(n)
+                ecell.append(n+bcount)
+                n += bcount
+            kT_idxs = np.unique(kT_idxs)
+
+            cell_em = EM[idxs]*vol_scale
+
+            u = np.random.random(cell_em.shape)
+
+            for i, ikT in enumerate(kT_idxs):
+
+                ibegin = bcell[i]
+                iend = ecell[i]
+                kT = kT_bins[ikT] + 0.5*dkT
         
-        pbar = get_pbar("Generating Photons", dshape[0])
+                em_sum_c = cell_em[ibegin:iend].sum()
+                em_sum_m = (metalZ[ibegin:iend]*cell_em[ibegin:iend]).sum()
 
-        for i, ikT in enumerate(kT_idxs):
+                cspec, mspec = self.spectral_model.get_spectrum(kT)
+                cspec *= dist_fac*em_sum_c/vol_scale
+                mspec *= dist_fac*em_sum_m/vol_scale
 
-            ibegin = bcell[i]
-            iend = ecell[i]
-            kT = kT_bins[ikT] + 0.5*dkT
-        
-            em_sum_c = cell_em[ibegin:iend].sum()
-            em_sum_m = (metalZ[ibegin:iend]*cell_em[ibegin:iend]).sum()
+                cumspec_c = np.cumsum(cspec.ndarray_view())
+                counts_c = cumspec_c[:]/cumspec_c[-1]
+                counts_c = np.insert(counts_c, 0, 0.0)
+                tot_ph_c = cumspec_c[-1]*area.value*exp_time.value
 
-            cspec, mspec = self.spectral_model.get_spectrum(kT)
-            cspec *= dist_fac*em_sum_c/vol_scale
-            mspec *= dist_fac*em_sum_m/vol_scale
+                cumspec_m = np.cumsum(mspec.ndarray_view())
+                counts_m = cumspec_m[:]/cumspec_m[-1]
+                counts_m = np.insert(counts_m, 0, 0.0)
+                tot_ph_m = cumspec_m[-1]*area.value*exp_time.value
 
-            cumspec_c = np.cumsum(cspec.ndarray_view())
-            counts_c = cumspec_c[:]/cumspec_c[-1]
-            counts_c = np.insert(counts_c, 0, 0.0)
-            tot_ph_c = cumspec_c[-1]*area.value*exp_time.value
-
-            cumspec_m = np.cumsum(mspec.ndarray_view())
-            counts_m = cumspec_m[:]/cumspec_m[-1]
-            counts_m = np.insert(counts_m, 0, 0.0)
-            tot_ph_m = cumspec_m[-1]*area.value*exp_time.value
-
-            for icell in xrange(ibegin, iend):
+                for icell in xrange(ibegin, iend):
             
-                cell_norm_c = tot_ph_c*cell_em[icell]/em_sum_c
-                cell_n_c = np.uint64(cell_norm_c) + np.uint64(np.modf(cell_norm_c)[0] >= u[icell])
+                    cell_norm_c = tot_ph_c*cell_em[icell]/em_sum_c
+                    cell_n_c = np.uint64(cell_norm_c) + np.uint64(np.modf(cell_norm_c)[0] >= u[icell])
             
-                cell_norm_m = tot_ph_m*metalZ[icell]*cell_em[icell]/em_sum_m
-                cell_n_m = np.uint64(cell_norm_m) + np.uint64(np.modf(cell_norm_m)[0] >= u[icell])
+                    cell_norm_m = tot_ph_m*metalZ[icell]*cell_em[icell]/em_sum_m
+                    cell_n_m = np.uint64(cell_norm_m) + np.uint64(np.modf(cell_norm_m)[0] >= u[icell])
             
-                cell_n = cell_n_c + cell_n_m
+                    cell_n = cell_n_c + cell_n_m
 
-                if cell_n > 0:
-                    number_of_photons[icell] = cell_n
-                    randvec_c = np.random.uniform(size=cell_n_c)
-                    randvec_c.sort()
-                    randvec_m = np.random.uniform(size=cell_n_m)
-                    randvec_m.sort()
-                    cell_e_c = np.interp(randvec_c, counts_c, energy)
-                    cell_e_m = np.interp(randvec_m, counts_m, energy)
-                    energies.append(np.concatenate([cell_e_c,cell_e_m]))
-                
-                pbar.update(icell)
+                    if cell_n > 0:
+                        randvec_c = np.random.uniform(size=cell_n_c)
+                        randvec_c.sort()
+                        randvec_m = np.random.uniform(size=cell_n_m)
+                        randvec_m.sort()
+                        cell_e_c = np.interp(randvec_c, counts_c, energy)
+                        cell_e_m = np.interp(randvec_m, counts_m, energy)
+                        photons["x"].append(chunk["x"][icell])
+                        photons["y"].append(chunk["y"][icell])
+                        photons["z"].append(chunk["z"][icell])
+                        photons["vx"].append(chunk["velocity_x"][icell])
+                        photons["vy"].append(chunk["velocity_y"][icell])
+                        photons["vz"].append(chunk["velocity_z"][icell])
+                        photons["NumberOfPhotons"].append(cell_n)
+                        photons["Energy"].append(np.concatenate([cell_e_c,cell_e_m]))
+
+            ck += 1
+            pbar.update(ck)
 
         pbar.finish()
-            
-        active_cells = number_of_photons > 0
-        idxs = idxs[active_cells]
-        
-        photons = {}
 
         src_ctr = parameters["center"]
 
-        x = data_source["x"][start_c:end_c][idxs]
-        y = data_source["y"][start_c:end_c][idxs]
-        z = data_source["z"][start_c:end_c][idxs]
-        dx = data_source["dx"][start_c:end_c][idxs]
-
-        data_source.clear_data()
-
-        vx = data_source["velocity_x"][start_c:end_c][idxs]
-        vy = data_source["velocity_y"][start_c:end_c][idxs]
-        vz = data_source["velocity_z"][start_c:end_c][idxs]
-
-        data_source.clear_data()
-
-        photons["x"] = (x[idxs]-src_ctr[0]).in_units("kpc")
-        photons["y"] = (y[idxs]-src_ctr[1]).in_units("kpc")
-        photons["z"] = (z[idxs]-src_ctr[2]).in_units("kpc")
-        photons["vx"] = vx[idxs].in_units("km/s")
-        photons["vy"] = vy[idxs].in_units("km/s")
-        photons["vz"] = vz[idxs].in_units("km/s")
-        photons["dx"] = dx[idxs].in_units("kpc")
-        photons["NumberOfPhotons"] = number_of_photons[active_cells]
-        photons["Energy"] = np.concatenate(energies)*units.keV
+        photons["x"] = (ds.arr(photons["x"])-src_ctr[0]).in_units("kpc")
+        photons["y"] = (ds.arr(photons["y"])-src_ctr[1]).in_units("kpc")
+        photons["z"] = (ds.arr(photons["z"])-src_ctr[2]).in_units("kpc")
+        photons["vx"] = ds.arr(photons["vx"]).in_units("km/s")
+        photons["vy"] = ds.arr(photons["vy"]).in_units("km/s")
+        photons["vz"] = ds.arr(photons["vz"]).in_units("km/s")
+        photons["dx"] = ds.arr(photons["dx"]).in_units("kpc")
+        photons["Energy"] = ds.arr(np.concatenate(photons["Energy"]), "keV")
     
         return photons
