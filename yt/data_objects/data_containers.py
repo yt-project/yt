13ea836d7804c3939321c25a66259597dfd34e5a
Nathan Goldbaum <ngoldbau@ucsc.edu>
2014-11-19 10:59:12 -0800

    Removing an unneeded conditional.
    --HG--
    branch : yt

--- yt/data_objects/data_containers.py
+++ yt/data_objects/data_containers.py
@@ -561,8 +561,6 @@ class YTSelectionContainer(YTDataContainer, ParallelAnalysisInterface):
         if data_source is not None:
             if data_source.ds is not self.ds:
                 raise RuntimeError("Attempted to construct a DataContainer with a data_source from a different DataSet", ds, data_source.ds)
-            else:
-                pass
             if data_source._dimensionality < self._dimensionality:
                 raise RuntimeError("Attempted to construct a DataContainer with a data_source of lower dimensionality (%u vs %u)" %
                                     (data_source._dimensionality, self._dimensionality))
