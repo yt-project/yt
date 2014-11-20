First pass at correcting periodic rvec calculation
--HG--
branch : yt

yt/fields/field_functions.py
@@ -49,14 +49,26 @@ def get_radius(data, field_prefix):
 
 def get_periodic_rvec(data):
     coords = obtain_rvec(data)
+   # return coords
     if sum(data.ds.periodicity) == 0: return coords
     le = data.ds.domain_left_edge.in_units("code_length").d
     dw = data.ds.domain_width.in_units("code_length").d
     for i in range(coords.shape[0]):
         if not data.ds.periodicity[i]: continue
         coords[i, ...] -= le[i]
-        coords[i, ...] = np.min([np.abs(np.mod(coords[i, ...],  dw[i])),
-                                 np.abs(np.mod(coords[i, ...], -dw[i]))],
-                                 axis=0)
-        coords[i, ...] += le[i]
+        #figure out which measure is less
+        mins = np.argmin([np.abs(np.mod(coords[i, ...],  dw[i])),
+                         np.abs(np.mod(coords[i, ...], -dw[i]))],
+                         axis=0)
+        temp_coords = np.mod(coords[i, ...], dw[i])
+
+        #Where second measure is better, updating temporary coords
+        ii = np.where(mins==1)
+        temp_coords[ii] = np.mod(coords[i, ...], -dw[i])[ii]
+
+        # Putting the temporary coords into the actual storage
+        coords[i, ...] = temp_coords
+
+        coords[i, ...] + le[i]
+
     return coords