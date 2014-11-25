a60b2a48524fd8b899cf19f746e49ad45ad413d7
Aaron Smith <asmith@astro.as.utexas.edu>
2014-11-25 12:49:20 -0600

    Other instance of /path/to/file.x.y/data.i.hdf5
    --HG--
    branch : yt

--- yt/frontends/gadget/data_structures.py
+++ yt/frontends/gadget/data_structures.py
@@ -178,7 +178,9 @@ class GadgetDataset(ParticleDataset):
                        hvals["Time"], self.current_time)
         self.parameters = hvals
 
-        prefix = self.parameter_filename.split(".", 1)[0]
+        prefix = os.path.abspath(
+            os.path.join(os.path.dirname(self.parameter_filename),
+                         os.path.basename(self.parameter_filename).split(".", 1)[0]))
 
         if hvals["NumFiles"] > 1:
             self.filename_template = "%s.%%(num)s%s" % (prefix, self._suffix)
@@ -309,8 +311,10 @@ class GadgetHDF5Dataset(GadgetDataset):
         self.cosmological_simulation = 1
         self.periodicity = (True, True, True)
 
-        prefix = self.parameter_filename.rsplit("/", 1)
-        prefix = os.path.abspath(prefix[0]+"/"+prefix[-1].split(".", 1)[0])
+        prefix = os.path.abspath(
+            os.path.join(os.path.dirname(self.parameter_filename),
+                         os.path.basename(self.parameter_filename).split(".", 1)[0]))
+
         suffix = self.parameter_filename.rsplit(".", 1)[-1]
         self.filename_template = "%s.%%(num)i.%s" % (prefix, suffix)
         self.file_count = self.parameters["NumFilesPerSnapshot"]
