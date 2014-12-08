acce4e1a73dbbdbf19ead3cf083278379e0d943f
Cameron Hummels <chummels@gmail.com>
2014-12-08 15:27:37 -0700

    Making sure radmc3d exporter test has the necessary dataset to run the nosetest.
    --HG--
    branch : yt

--- yt/analysis_modules/radmc3d_export/tests/test_radmc3d_exporter.py
+++ yt/analysis_modules/radmc3d_export/tests/test_radmc3d_exporter.py
@@ -11,12 +11,16 @@ Unit test for the RADMC3D Exporter analysis module
 #-----------------------------------------------------------------------------
 
 import yt
+from yt.testing import *
 from yt.analysis_modules.radmc3d_export.api import RadMC3DWriter
+from yt.config import ytcfg
 import tempfile
 import os
 import shutil
 
+ISO_GAL = "IsolatedGalaxy/galaxy0030/galaxy0030"
 
+@requires_file(ISO_GAL)
 def test_radmc3d_exporter_continuum():
     """
     This test is simply following the description in the docs for how to
@@ -38,7 +42,7 @@ def test_radmc3d_exporter_continuum():
     def _DustTemperature(field, data):
         return 0.*data["temperature"] + data.ds.quan(10,'K')
     
-    ds = yt.load("IsolatedGalaxy/galaxy0030/galaxy0030")
+    ds = yt.load(ISO_GAL)
     ds.add_field(("gas", "dust_density"), function=_DustDensity, units="g/cm**3")
     ds.add_field(("gas", "dust_temperature"), function=_DustTemperature, units="K")
     writer = RadMC3DWriter(ds)
