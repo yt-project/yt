6523b5511d7e4ec0102457031035206e4502142c
Matthew Turk <matthewturk@gmail.com>
2014-12-08 17:16:55 -0600

    Address issues for FITS cubes.
    --HG--
    branch : yt

--- yt/geometry/coordinates/spec_cube_coordinates.py
+++ yt/geometry/coordinates/spec_cube_coordinates.py
@@ -17,6 +17,8 @@ Cartesian fields
 import numpy as np
 from .cartesian_coordinates import \
     CartesianCoordinateHandler
+from .coordinate_handler import \
+    _get_coord_fields
 
 class SpectralCubeCoordinateHandler(CartesianCoordinateHandler):
 
@@ -57,6 +59,38 @@ class SpectralCubeCoordinateHandler(CartesianCoordinateHandler):
 
         self.axis_field = {}
         self.axis_field[self.ds.spec_axis] = _spec_axis
+
+    def setup_fields(self, registry):
+        if self.ds.no_cgs_equiv_length == False:
+            return super(self, SpectralCubeCoordinateHandler
+                    ).setup_fields(registry)
+        for axi, ax in enumerate('xyz'):
+            f1, f2 = _get_coord_fields(axi)
+            def _get_length_func():
+                def _length_func(field, data):
+                    return 1.0
+                return _length_func
+            registry.add_field(("index", "d%s" % ax), function = f1,
+                               display_field = False,
+                               units = "code_length")
+            registry.add_field(("index", "path_element_%s" % ax),
+                               function = _get_length_func(),
+                               display_field = False,
+                               units = "")
+            registry.add_field(("index", "%s" % ax), function = f2,
+                               display_field = False,
+                               units = "code_length")
+        def _cell_volume(field, data):
+            rv  = data["index", "dx"].copy(order='K')
+            rv *= data["index", "dy"]
+            rv *= data["index", "dz"]
+            return rv
+        registry.add_field(("index", "cell_volume"), function=_cell_volume,
+                           display_field=False, units = "code_length**3")
+        registry.check_derived_fields(
+            [("index", "dx"), ("index", "dy"), ("index", "dz"),
+             ("index", "x"), ("index", "y"), ("index", "z"),
+             ("index", "cell_volume")])
 
     def convert_to_cylindrical(self, coord):
         raise NotImplementedError
