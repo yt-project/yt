bea18be34dddc5c5f37c93c77a73db13a9a4d115
Matthew Turk <matthewturk@gmail.com>
2014-12-16 09:08:06 -0600

    Use PATRAN ordering for faces
    --HG--
    branch : yt

--- yt/utilities/lib/misc_utilities.pyx
+++ yt/utilities/lib/misc_utilities.pyx
@@ -667,16 +667,38 @@ def pixelize_aitoff(np.ndarray[np.float64_t, ndim=1] theta,
     return img
 
 # Six faces, two vectors for each, two indices for each vector.  The function
-# below unrolls how these are defined.  Order is bottom, front, right, left,
-# back, top.
-# This is [6][2][2]
+# below unrolls how these are defined.  Some info can be found at:
+# http://www.mscsoftware.com/training_videos/patran/Reverb_help/index.html#page/Finite%20Element%20Modeling/elem_lib_topics.16.8.html
+# This is [6][2][2] in shape.
+# Here are the faces and their four edges each:
+# F1    1   2   3   4
+# F2    5   6   7   8
+# F3    1   10  5   9
+# F4    2   11  6   10
+# F5    3   12  7   11
+# F6    4   9   8   12
+#
+# The edges are then defined by:
+# E1    1 2
+# E2    2 6
+# E3    6 5
+# E4    5 1
+# E5    4 3
+# E6    3 7
+# E7    7 8
+# E8    8 4
+# E9    1 4
+# E10   2 3
+# E11   6 7
+# E12   5 8
+# Now we unroll these here ...
 cdef np.uint8_t ***face_defs = [
-               [[0, 1], [2, 1]],
-               [[0, 1], [5, 1]],
-               [[2, 1], [5, 1]],
-               [[4, 0], [3, 0]],
-               [[6, 2], [3, 2]],
-               [[4, 5], [6, 5]]
+   [[0, 1], [1, 5]],
+   [[3, 2], [2, 6]],
+   [[0, 1], [1, 2]],
+   [[1, 5], [5, 6]],
+   [[5, 4], [4, 7]],
+   [[4, 0], [0, 3]],
 ]
 
 # This function accepts a set of eight vertices (for a hexahedron) that are
