ddb8f5cb6bfa143cf54c4a9bffd9ec28d0cf5e35
Nathan Goldbaum <ngoldbau@ucsc.edu>
2014-12-31 09:11:21 -0800

    Backed out changeset d29867e24d3e
    --HG--
    branch : yt

--- doc/extensions/notebook_sphinxext.py
+++ doc/extensions/notebook_sphinxext.py
@@ -209,7 +209,7 @@ def setup(app):
     retdict = dict(
         version='0.1',
         parallel_read_safe=True,
-        parallel_write_safe=False
+        parallel_write_safe=True
     )
 
     return retdict
