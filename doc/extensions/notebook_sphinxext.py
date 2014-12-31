9383a8a9d1424f583f179f72e33f32816899249b
Nathan Goldbaum <goldbaum@ucolick.org>
2014-12-30 19:39:41 -0800

    Prevent writing from happening in parallel.
    This is a rather unsatisfactory solution for a persistent memory leak in the
    docs build. The issue is inside sphinx and causes a memory leak during the write
    phase of the docs build. Since the issue only happens in parallel, forcing a
    erial write phase avoids the issue at the cost of a significantly lengthier
    build process.
    
    --HG--
    branch : yt

--- doc/extensions/notebook_sphinxext.py
+++ doc/extensions/notebook_sphinxext.py
@@ -209,7 +209,7 @@ def setup(app):
     retdict = dict(
         version='0.1',
         parallel_read_safe=True,
-        parallel_write_safe=True
+        parallel_write_safe=False
     )
 
     return retdict
