b60ed0127c7ec5d28d5fc0b992677331bd89f261
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-01-07 14:49:58 -0800

    Use full field tuple to access ElectronAbundance
    --HG--
    branch : yt

--- yt/frontends/gadget/fields.py
+++ yt/frontends/gadget/fields.py
@@ -34,7 +34,7 @@ class GadgetFieldInfo(SPHFieldInfo):
                 # Assume cosmic abundances
                 x_H = 0.76
                 gamma = 5.0/3.0
-                a_e = data['ElectronAbundance']
+                a_e = data[ptype, 'ElectronAbundance']
                 mu = 4.0 / (3.0 * x_H + 1.0 + 4.0 * x_H * a_e)
                 ret = data[ptype, "InternalEnergy"]*(gamma-1)*mu*mp/kb
                 return ret.in_units('K')
