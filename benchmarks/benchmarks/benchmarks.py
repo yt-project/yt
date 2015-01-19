ec58f0f3ebfa24cadd65304ce24b0d2ce3a4ad59
Matthew Turk <matthewturk@gmail.com>
2015-01-19 11:28:48 -0600

    Put Kacper's benchmarks into benchmark.py
    --HG--
    branch : yt

--- benchmarks/benchmarks/benchmarks.py
+++ benchmarks/benchmarks/benchmarks.py
@@ -1,36 +1,31 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
+import numpy as np
+from yt import YTArray, YTQuantity
+
+def time_quantity_init_scalar1():
+    3.0 * YTQuantity(1, "m/s")
 
 
-class TimeSuite:
-    """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
-    def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
-
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
-
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
-
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
-
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
+def time_quantity_init_scalar2():
+    YTQuantity(3.0, "m/s")
 
 
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+def time_quantity_init_array():
+    np.arange(100000) * YTQuantity(1, "m/s")
+
+
+def time_quantity_init_array2():
+    YTArray(np.arange(100000), "m/s")
+
+
+def time_quantity_scalar_conversion():
+    YTQuantity(3.0, "m/s").in_units("km/hr")
+
+
+def time_quantity_array_conversion():
+    YTArray(np.arange(100000), "m/s").in_units("km/hr")
+
+
+def time_quantity_ufunc_sin():
+    np.sin(YTArray(np.arange(10000), "degree"))
