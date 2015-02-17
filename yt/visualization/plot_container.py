f632f0f03cea8f55bdfbc52d0928252f7a623e78
John ZuHone <jzuhone@gmail.com>
2015-02-17 15:27:21 -0500

    Allow a suffix to be specified to save()
    --HG--
    branch : yt

--- yt/visualization/plot_container.py
+++ yt/visualization/plot_container.py
@@ -536,7 +536,7 @@ class ImagePlotContainer(object):
         return self
 
     @ensure_callbacks
-    def save(self, name=None, mpl_kwargs=None):
+    def save(self, name=None, suffix=None, mpl_kwargs=None):
         """saves the plot to disk.
 
         Parameters
@@ -544,6 +544,9 @@ class ImagePlotContainer(object):
         name : string
            The base of the filename.  If name is a directory or if name is not
            set, the filename of the dataset is used.
+        suffix : string
+           Specify the image type by its suffix. If not specified, the filename
+           will be queried for the image type. 
         mpl_kwargs : dict
            A dict of keyword arguments to be passed to matplotlib.
 
@@ -559,11 +562,12 @@ class ImagePlotContainer(object):
             os.mkdir(name)
         if os.path.isdir(name) and name != str(self.ds):
             name = name + (os.sep if name[-1] != os.sep else '') + str(self.ds)
-        suffix = get_image_suffix(name)
-        if suffix != '':
-            for k, v in self.plots.iteritems():
-                names.append(v.save(name, mpl_kwargs))
-            return names
+        if suffix is None:
+            suffix = get_image_suffix(name)
+            if suffix != '':
+                for k, v in self.plots.iteritems():
+                    names.append(v.save(name, mpl_kwargs))
+                return names
         axis = self.ds.coordinates.axis_name.get(
             self.data_source.axis, '')
         weight = None
@@ -584,6 +588,8 @@ class ImagePlotContainer(object):
                 n = "%s_%s_%s" % (name, type, k.replace(' ', '_'))
             if weight:
                 n += "_%s" % (weight)
+            if suffix != '':
+                n = ".".join([n,suffix])
             names.append(v.save(n, mpl_kwargs))
         return names
 
