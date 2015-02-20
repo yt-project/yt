ce3d0d88e13cd6c8b274d62b781db7b49a792ea6
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-02-19 18:43:57 -0800

    Ensure particle fields are tested in the field unit tests
    --HG--
    branch : yt

--- yt/fields/tests/test_fields.py
+++ yt/fields/tests/test_fields.py
@@ -13,11 +13,23 @@ def setup():
     global base_ds
     # Make this super teeny tiny
     fields, units = [], []
+    particle_fields, particle_field_units = [], []
+
+    particle_fields = \
+        ['particle_position', 'particle_position_x', 'particle_position_y',
+         'particle_position_z', 'particle_velocity', 'particle_velocity_x',
+         'particle_velocity_y', 'particle_velocity_z', 'particle_mass']
+    particle_field_units = \
+        ['code_length']*4 + ['code_velocity']*4 + ['code_mass']
 
     for fname, (code_units, aliases, dn) in StreamFieldInfo.known_other_fields:
         fields.append(("gas", fname))
         units.append(code_units)
-    base_ds = fake_random_ds(4, fields = fields, units = units)
+
+    base_ds = fake_random_ds(
+        4, fields=fields, units=units, particle_fields=particle_fields,
+        particle_field_units=particle_field_units, particles=10)
+
     base_ds.index
     base_ds.cosmological_simulation = 1
     base_ds.cosmology = Cosmology()
@@ -51,12 +63,17 @@ _base_fields = (("gas", "density"),
                 ("gas", "velocity_y"),
                 ("gas", "velocity_z"))
 
-def realistic_ds(fields, nprocs):
+def realistic_ds(fields, particle_fields, nprocs):
     np.random.seed(int(0x4d3d3d3))
     units = [base_ds._get_field_info(*f).units for f in fields]
+    punits = [base_ds._get_field_info('io', f).units for f in particle_fields]
     fields = [_strip_ftype(f) for f in fields]
-    ds = fake_random_ds(16, fields = fields, units = units,
-                        nprocs = nprocs)
+
+    ds = fake_random_ds(16, fields=fields, units=units, nprocs=nprocs,
+                        particle_fields=particle_fields,
+                        particle_field_units=punits,
+                        particles=base_ds.stream_handler.particle_count[0][0])
+
     ds.parameters["HydroMethod"] = "streaming"
     ds.parameters["EOSType"] = 1.0
     ds.parameters["EOSSoundSpeed"] = 1.0
@@ -77,7 +94,7 @@ def realistic_ds(fields, nprocs):
 def _strip_ftype(field):
     if not isinstance(field, tuple):
         return field
-    elif field[0] == "all":
+    elif field[0] in ("all", "io"):
         return field
     return field[1]
 
@@ -105,13 +122,23 @@ class TestFieldAccess(object):
         self.nproc = nproc
 
     def __call__(self):
-        if self.field_name in base_ds.field_list:
-            # Don't know how to test this.  We need some way of having fields
-            # that are fallbacks be tested, but we don't have that now.
-            return
+
         field = base_ds._get_field_info(*self.field_name)
         deps = field.get_dependencies(ds = base_ds)
-        fields = deps.requested + list(_base_fields)
+        requested = deps.requested
+        particle_fields = \
+            ['particle_position', 'particle_position_x', 'particle_position_y',
+             'particle_position_z', 'particle_velocity', 'particle_velocity_x',
+             'particle_velocity_y', 'particle_velocity_z', 'particle_mass']
+        fields = list(_base_fields)
+
+        for rf in requested:
+            if field.particle_type:
+                if rf not in particle_fields:
+                    particle_fields.append(rf[1])
+            else:
+                fields.append(rf)
+
         skip_grids = False
         needs_spatial = False
         for v in field.validators:
@@ -121,7 +148,9 @@ class TestFieldAccess(object):
                 skip_grids = True
             if hasattr(v, "ghost_zones"):
                 needs_spatial = True
-        ds = realistic_ds(fields, self.nproc)
+
+        ds = realistic_ds(fields, particle_fields, self.nproc)
+
         # This gives unequal sized grids as well as subgrids
         dd1 = ds.all_data()
         dd2 = ds.all_data()
@@ -130,8 +159,7 @@ class TestFieldAccess(object):
         dd2.field_parameters.update(sp)
         v1 = dd1[self.field_name]
         # No more conversion checking
-        if not field.particle_type:
-            assert_equal(v1, dd1[self.field_name])
+        assert_equal(v1, dd1[self.field_name])
         if not needs_spatial:
             with field.unit_registry(dd2):
                 res = field._function(field, dd2)
@@ -143,10 +171,13 @@ class TestFieldAccess(object):
                 v1 = g[self.field_name]
                 g.clear_data()
                 g.field_parameters.update(sp)
-                for ax in 'xyz':
-                    assert_array_equal(g[ax].shape, v1.shape)
                 r1 = field._function(field, g)
-                assert_array_equal(r1.shape, v1.shape)
+                if field.particle_type:
+                    assert_equal(v1.shape[-1], g.NumberOfParticles)
+                else:
+                    assert_array_equal(r1.shape, v1.shape)
+                    for ax in 'xyz':
+                        assert_array_equal(g[ax].shape, v1.shape)
                 with field.unit_registry(g):
                     res = field._function(field, g)
                     assert_array_equal(v1.shape, res.shape)
@@ -155,16 +186,17 @@ class TestFieldAccess(object):
 
 def test_all_fields():
     for field in sorted(base_ds.field_info):
-        if not isinstance(field, tuple):
-            field = ("unknown", field)
-        finfo = base_ds._get_field_info(*field)
-        if isinstance(field, tuple):
-            fname = field[0]
-        else:
-            fname = field
-        if field[0] == "deposit": continue
-        if field[1].find("beta_p") > -1: continue
-        if finfo.particle_type: continue
+        if field[1].find("beta_p") > -1:
+            continue
+        if field[0] == "deposit":
+            # I see segmentation faults during garbage collection if I include
+            # deposit fields
+            continue
+        if field in base_ds.field_list:
+            # Don't know how to test this.  We need some way of having fields
+            # that are fallbacks be tested, but we don't have that now.
+            continue
+
         for nproc in [1, 4, 8]:
             test_all_fields.__name__ = "%s_%s" % (field, nproc)
             yield TestFieldAccess(field, nproc)
