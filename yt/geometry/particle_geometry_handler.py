89746c4a37db7becb80bef6ff5dc7a0cc435a6ec
Matthew Turk <matthewturk@gmail.com>
2015-02-27 09:03:55 -0600

    Divide by the number of cells in the root grid to get smallest dx.
    --HG--
    branch : yt

--- yt/geometry/particle_geometry_handler.py
+++ yt/geometry/particle_geometry_handler.py
@@ -59,7 +59,7 @@ class ParticleIndex(Index):
         """
         Returns (in code units) the smallest cell size in the simulation.
         """
-        dx = 1.0/(2**self.oct_handler.max_level)
+        dx = self.domain_dimensions(2**self.oct_handler.max_level)
         dx *= (self.dataset.domain_right_edge -
                self.dataset.domain_left_edge)
         return dx.min()
