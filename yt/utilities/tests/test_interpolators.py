1e1b667eb3f5dcfcc0831e437f9fe3ad8dfd4e83
Matthew Turk <matthewturk@gmail.com>
2015-03-03 20:42:40 -0600

    Adding tests for ghost_zone_interpolate, including failing one.
    --HG--
    branch : yt

--- yt/utilities/tests/test_interpolators.py
+++ yt/utilities/tests/test_interpolators.py
@@ -1,5 +1,7 @@
 from yt.testing import *
 import yt.utilities.linear_interpolators as lin
+from yt.utilities.lib.Interpolators import \
+        ghost_zone_interpolate
 
 def setup():
     pass
@@ -62,3 +64,34 @@ def test_linear_interpolator_3d():
              bins + shifts["z"]), "xyz", True)
     yield assert_array_almost_equal, tfi(fv), random_data, 15
     
+
+def test_ghost_zone_extrapolation():
+    ds = fake_random_ds(16)
+
+    g = ds.index.grids[0]
+    for i, ax in enumerate('xyz'):
+        xc = g[ax]
+
+        xv = g.get_vertex_centered_data("x", no_ghost=True)
+        tf = lin.TrilinearFieldInterpolator(xc,
+                (g.LeftEdge[0] + g.dds[0]/2.0,
+                    g.RightEdge[0] - g.dds[0]/2.0,
+                 g.LeftEdge[1] + g.dds[1]/2.0,
+                    g.RightEdge[1] - g.dds[1]/2.0,
+                 g.LeftEdge[2] + g.dds[2]/2.0,
+                    g.RightEdge[2] - g.dds[2]/2.0),
+                ["x", "y", "z"], truncate = True)
+
+        lx,ly,lz = np.mgrid[g.LeftEdge[0]:g.RightEdge[0]:(g.ActiveDimensions[0]+1)*1j,
+                            g.LeftEdge[1]:g.RightEdge[1]:(g.ActiveDimensions[1]+1)*1j,
+                            g.LeftEdge[2]:g.RightEdge[2]:(g.ActiveDimensions[2]+1)*1j]
+        xi  = tf({'x':lx, 'y':ly, 'z':lz})
+
+        xz = np.zeros(g.ActiveDimensions+1)
+        ghost_zone_interpolate(1, xc, np.array([0.5, 0.5, 0.5], dtype="f8"),
+                                  xz, np.array([0.0, 0.0, 0.0], dtype="f8"))
+
+        ii = (lx, ly, lz)[i]
+        yield assert_array_equal, ii, xv
+        yield assert_array_equal, ii, xi
+        yield assert_array_equal, ii, xz
