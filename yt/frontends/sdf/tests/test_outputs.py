ed22b4b3509cef05a4d3aec9f5b1275a9cbf0ce7
Sam Skillman <samskillman@gmail.com>
2015-03-10 12:27:11 -0700

    Testing if the internet is on
    --HG--
    branch : yt

--- yt/frontends/sdf/tests/test_outputs.py
+++ yt/frontends/sdf/tests/test_outputs.py
@@ -18,8 +18,21 @@ from yt.visualization.api import ProjectionPlot
 
 _fields = (('deposit','all_cic'))
 
+import urllib2
+
+scivis_data = "http://darksky.slac.stanford.edu/scivis2015/data/ds14_scivis_0128/ds14_scivis_0128_e4_dt04_1.0000"
+
+# Answer on http://stackoverflow.com/questions/3764291/checking-network-connection
+def internet_on():
+    try:
+        response=urllib2.urlopen(scivis_data,timeout=1)
+        return True
+    except urllib2.URLError as err: pass
+    return False
+
 def test_scivis():
-    ds = SDFDataset("http://darksky.slac.stanford.edu/scivis2015/data/ds14_scivis_0128/ds14_scivis_0128_e4_dt04_1.0000")
+    if not internet_on(): return
+    ds = SDFDataset(scivis_data)
     yield assert_equal, str(ds), "ds14_scivis_0128_e4_dt04_1.0000"
     ad = ds.all_data()
     assert np.unique(ad['particle_position_x']).size > 1
