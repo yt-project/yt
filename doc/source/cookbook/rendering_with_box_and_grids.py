10a530262a8f43993853ae634ec440532917ff19
Sam Skillman <samskillman@gmail.com>
2015-03-17 19:21:48 -0600

    Needs ds's in the arguments for the annotates.
    --HG--
    branch : yt

--- doc/source/cookbook/rendering_with_box_and_grids.py
+++ doc/source/cookbook/rendering_with_box_and_grids.py
@@ -7,16 +7,16 @@ ds = yt.load("Enzo_64/DD0043/data0043")
 im, sc = yt.volume_render(ds, ('gas','density'))
 sc.get_source(0).transfer_function.grey_opacity=True
 
-sc.annotate_domain()
+sc.annotate_domain(ds)
 im = sc.render()
 im.write_png("%s_vr_domain.png" % ds)
 
-sc.annotate_grids()
+sc.annotate_grids(ds)
 im = sc.render()
 im.write_png("%s_vr_grids.png" % ds)
 
 # Here we can draw the coordinate vectors on top of the image by processing
 # it through the camera. Then save it out.
-sc.annotate_axes()
+sc.annotate_axes(ds)
 im = sc.render()
 im.write_png("%s_vr_coords.png" % ds)
