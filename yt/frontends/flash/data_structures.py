b6615f4802025ec9a7fae5de576adb2379115a1e
John ZuHone <jzuhone@gmail.com>
2015-04-08 15:36:17 -0400

    Just remove it altogether
    --HG--
    branch : yt

--- yt/frontends/flash/data_structures.py
+++ yt/frontends/flash/data_structures.py
@@ -54,7 +54,6 @@ class FLASHHierarchy(GridIndex):
 
     grid = FLASHGrid
     _preload_implemented = True
-    _grid_chunksize = 1000
     
     def __init__(self,ds,dataset_type='flash_hdf5'):
         self.dataset_type = dataset_type
