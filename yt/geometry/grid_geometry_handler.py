ed5e3e6c87e0be7f582964babba37855c5dfefe9
John ZuHone <jzuhone@gmail.com>
2015-04-09 14:33:26 -0400

    Don't go to all this trouble if chunk_ngrid == 0
    --HG--
    branch : yt

--- yt/geometry/grid_geometry_handler.py
+++ yt/geometry/grid_geometry_handler.py
@@ -359,9 +359,12 @@ class GridIndex(Index):
         # many grids all at once.
         if chunk_sizing == "auto":
             chunk_ngrids = dobj._chunk_info.shape[0]
-            nproc = np.float(ytcfg.getint("yt", "__global_parallel_size"))
-            chunking_factor = np.ceil(self._grid_chunksize*nproc/chunk_ngrids).astype("int")
-            size = max(self._grid_chunksize//chunking_factor, 1)
+            if chunk_ngrids > 0:
+                nproc = np.float(ytcfg.getint("yt", "__global_parallel_size"))
+                chunking_factor = np.ceil(self._grid_chunksize*nproc/chunk_ngrids).astype("int")
+                size = max(self._grid_chunksize//chunking_factor, 1)
+            else:
+                size = self._grid_chunksize
         elif chunk_sizing == "config_file":
             size = ytcfg.getint("yt", "chunk_size")
         elif chunk_sizing == "just_one":
