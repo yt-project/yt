e799809e7e3d202c57413ba49c118f48e5485e3c
Nathan Goldbaum <goldbaum@ucolick.org>
2015-04-09 13:47:53 -0700

    Add a test for the particle_filter decorator.
    --HG--
    branch : yt

--- yt/utilities/tests/test_particle_union.py
+++ yt/utilities/tests/test_particle_union.py
@@ -1,29 +1,47 @@
 from __future__ import print_function
 import yt
 from yt.testing import requires_file
-from yt.data_objects.particle_filters import add_particle_filter
+from yt.data_objects.particle_filters import \
+    add_particle_filter, particle_filter
 
 
 # Dataset required for this test
 iso_galaxy = 'IsolatedGalaxy/galaxy0030/galaxy0030'
 
 
-# Stars function for stars particle filter
-# Define particle filter to filter stars younger than 100 Myr
-def Stars(pfilter, data):
-    return (data.ds.current_time - data["all",
-                                        "creation_time"]).in_units('Myr') > 0
-
-# Simple test to create a particle filter, then access the deposition field
-# (was a problem in previous versions on this dataset because there are chunks
-#  with no stars in them).
-
-
 @requires_file(iso_galaxy)
-def test_particle_filter():
-    add_particle_filter("stars", function=Stars, filtered_type='all',
+def test_add_particle_filter():
+    """Test particle filters created via add_particle_filter
+
+    This accesses a deposition field using hte particle filter, which was a
+    problem in previous versions on this dataset because there are chunks with
+    no stars in them.
+
+    """
+
+    def stars(pfilter, data):
+        filter_field = (pfilter.filtered_type, "creation_time")
+        return (data.ds.current_time - data[filter_field]) > 0
+
+    add_particle_filter("stars", function=stars, filtered_type='all',
                         requires=["creation_time"])
     ds = yt.load(iso_galaxy)
     ds.add_particle_filter('stars')
     ad = ds.all_data()
-    print(ad['deposit', 'stars_cic'])
+    ad['deposit', 'stars_cic']
+    assert True
+
+@requires_file(iso_galaxy)
+def test_particle_filter():
+    """Test the particle_filter decorator"""
+
+    @particle_filter(filtered_type='all', requires=['creation_time'])
+    def stars(pfilter, data):
+        filter_field = (pfilter.filtered_type, "creation_time")
+        return (data.ds.current_time - data[filter_field]) > 0
+
+    ds = yt.load(iso_galaxy)
+    ds.add_particle_filter('stars')
+    ad = ds.all_data()
+    ad['deposit', 'stars_cic']
+    assert True
