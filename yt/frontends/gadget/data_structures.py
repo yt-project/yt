676931628d95e345a5891c5a73a38cd462133ecc
langmm <none@none>
2015-05-20 15:16:59 -0500

    Fixed confusion between ART and Gadget raw binary snapshots using particle number.
    --HG--
    branch : yt

--- yt/frontends/gadget/data_structures.py
+++ yt/frontends/gadget/data_structures.py
@@ -262,18 +262,33 @@ class GadgetDataset(ParticleDataset):
         # First int32 is 256 for a Gadget2 binary file with SnapFormat=1,
         # 8 for a Gadget2 binary file with SnapFormat=2 file, 
         # or the byte swapped equivalents (65536 and 134217728).
-        (r,) = struct.unpack('<I',f.read(4))
-        f.close()
-        if r == 256:
-            return True, '<'
-        elif r == 65536:
-            return True, '>'
+        # The int32 following the header (first 4+256 bytes) must equal this
+        # number.
+        (rhead,) = struct.unpack('<I',f.read(4))
+        # Use value to check endianess
+        if rhead == 256:
+            endianswap = '<'
+        elif rhead == 65536:
+            endianswap = '>'
         # Disabled for now (does any one still use SnapFormat=2?)
         # If so, alternate read would be needed based on header.
-        # elif r == 8:
+        # elif rhead == 8:
         #     return True, '<'
-        # elif r == 134217728:
+        # elif rhead == 134217728:
         #     return True, '>'
+        else:
+            f.close()
+            return False, 1
+        # Read in particle number from header
+        np0 = sum(struct.unpack(endianswap+'IIIIII',f.read(6*4)))
+        # Read in size of position block. It should be 4 bytes per float, 
+        # with 3 coordinates (x,y,z) per particle. (12 bytes per particle)
+        f.seek(4+256+4,0)
+        np1 = struct.unpack(endianswap+'I',f.read(4))[0]/(4*3)
+        f.close()
+        # Compare
+        if np0 == np1:
+            return True, endianswap
         else:
             return False, 1
 
