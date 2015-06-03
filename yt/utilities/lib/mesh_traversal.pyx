58f01f4d8488aaef68b62befc3814165d7773d66
Andrew Myers <atmyers2@gmail.com>
2015-06-02 21:51:10 -0700

    getting a MeshSampler class to compile
    --HG--
    branch : yt

--- yt/utilities/lib/mesh_traversal.pyx
+++ yt/utilities/lib/mesh_traversal.pyx
@@ -5,9 +5,7 @@ cimport pyembree.rtcore as rtc
 cimport pyembree.rtcore_ray as rtcr
 cimport pyembree.rtcore_geometry as rtcg
 cimport pyembree.rtcore_scene as rtcs
-
-def hello_world():
-    print "Hello!"
+from grid_traversal cimport ImageSampler
 
 cdef void error_printer(const rtc.RTCError code, const char *_str):
     print "ERROR CAUGHT IN EMBREE"
@@ -23,3 +21,6 @@ cdef class EmbreeVolume:
 
     def __dealloc__(self):
         rtcs.rtcDeleteScene(self.scene_i)
+
+cdef class MeshSampler(ImageSampler):
+    pass
