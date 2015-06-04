234d181ba66aff7d386cce9a47666436825ac3b2
Allyson Julian <julian3@illinois.edu>
2015-06-04 15:40:33 -0500

    Encapsulate some more stuff.
    --HG--
    branch : yt

--- yt/utilities/exodus_reader.py
+++ yt/utilities/exodus_reader.py
@@ -6,40 +6,42 @@ from netCDF4 import Dataset
 class ExodusData:
     def __init__(self, filename):
         self.filename = filename
-        self.dataset = []
         self.coords = []
         self.connects = []
         self.data = []
 
     def load_dataset(self):
-        self.dataset = Dataset(self.filename).variables
+        dataset = Dataset(self.filename).variables
+        nelem = dataset["eb_status"][:].shape[0]
+        varnames = self.get_var_names(dataset)
+        nodnames = self.get_nod_names(dataset)
 
-        nelem = self.dataset["eb_status"][:].shape[0]
-
-        varnames = [self.sanitize_string(v.tostring()) for v in
-                    self.dataset["name_elem_var"][:]]
-
-        nodnames = [self.sanitize_string(v.tostring()) for v in
-                    self.dataset["name_nod_var"][:]]
-
-        coord = np.array([self.dataset["coord%s" % ax][:]
+        coord = np.array([dataset["coord%s" % ax][:]
                          for ax in 'xyz']).transpose().copy()
 
         for i in range(nelem):
-            self.connects.append(self.dataset["connect%s" % (i+1)][:].astype("i8"))
+            self.connects.append(dataset["connect%s" % (i+1)][:].astype("i8"))
             ci = self.connects[-1]
             self.coords.append(coord)  # Same for all
             vals = {}
 
             for j, v in enumerate(varnames):
-                vals['gas', v] = self.dataset["vals_elem_var%seb%s" % (j+1, i+1)][:].astype("f8")[-1,:]
+                vals['gas', v] = dataset["vals_elem_var%seb%s" % (j+1, i+1)][:].astype("f8")[-1,:]
 
             for j, v in enumerate(nodnames):
                 # We want just for this set of nodes all the node variables
                 # Use (ci - 1) to get these values
-                vals['gas', v] = self.dataset["vals_nod_var%s" % (j+1)][:].astype("f8")[-1, ci - 1, ...]
+                vals['gas', v] = dataset["vals_nod_var%s" % (j+1)][:].astype("f8")[-1, ci - 1, ...]
 
             self.data.append(vals)
+
+    def get_var_names(self, dataset):
+        return [self.sanitize_string(v.tostring()) for v in
+                dataset["name_elem_var"][:]]
+
+    def get_nod_names(self, dataset):
+        return [self.sanitize_string(v.tostring()) for v in
+                dataset["name_nod_var"][:]]
 
     def sanitize_string(self, s):
         s = "".join(_ for _ in takewhile(lambda a: a in string.printable, s))
