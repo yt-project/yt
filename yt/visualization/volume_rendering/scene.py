461324344f585631f0cbbd341896083559c3df95
Andrew Myers <atmyers2@gmail.com>
2015-06-09 16:37:52 -0700

    the _reshape_image function has been removed, so don't call it here
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/scene.py
+++ yt/visualization/volume_rendering/scene.py
@@ -191,7 +191,6 @@ class Scene(object):
 
         for k, source in self._iter_transparent_sources():
             im = source.render(camera, zbuffer=opaque)
-        im = camera.lens._reshape_image(im, camera)
         return im
 
     def camera():
