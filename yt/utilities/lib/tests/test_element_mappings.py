aa45744dc4b49f8c796b68e51e7adcf4cfc614c7
Andrew Myers <atmyers2@gmail.com>
2015-06-26 10:55:45 -0700

    add file for testing element mappings
    --HG--
    branch : yt

---
+++ yt/utilities/lib/tests/test_element_mappings.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from yt.testing import *
+from yt.utilities.lib.element_mappings import \
+    P1Mapping2D, \
+    P1Mapping3D, \
+    Q1Mapping2D, \
+    Q1Mapping3D
+
+
+def setup():
+    pass
+
+
+def test_sample():
+    pass
