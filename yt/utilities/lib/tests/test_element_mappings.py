f7fb44bd593d75e9e98afe708c6fec434916beb2
Andrew Myers <atmyers2@gmail.com>
2015-06-26 11:14:48 -0700

    filling in some tests
    --HG--
    branch : yt

--- yt/utilities/lib/tests/test_element_mappings.py
+++ yt/utilities/lib/tests/test_element_mappings.py
@@ -12,5 +12,40 @@ def setup():
     pass
 
 
-def test_sample():
+def test_P1Mapping2D():
+    NV = 3
+    NDIM = 2
+    vertices = np.empty((NV, NDIM))
+
+    vertices[0, 0] = 0.1
+    vertices[0, 1] = 0.2
+
+    vertices[1, 0] = 0.6
+    vertices[1, 1] = 0.3
+
+    vertices[2, 0] = 0.2
+    vertices[2, 1] = 0.7
+
+    physical_x[0] = 0.4
+    physical_x[1] = 0.4
+
+    field_values = np.empty(NV)
+    field_values[0] = 1.0
+    field_values[1] = 2.0
+    field_values[2] = 3.0
+
+    physical_x = np.empty(NDIM)
+    for i in range(NV):
+        physical_x = vertices[i]
+
+
+def test_P1Mapping3D():
+    pass
+
+
+def test_Q1Mapping2D():
+    pass
+
+
+def test_Q1Mapping3D():
     pass
