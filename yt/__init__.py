dc1e2fcfae33b44c369463acba0e39b96db1cd8d
AJ <astrohckr@gmail.com>
2015-07-06 14:49:05 -0500

    Revert, revert
    --HG--
    branch : yt

--- yt/__init__.py
+++ yt/__init__.py
@@ -134,7 +134,7 @@ frontends = _frontend_container()
 from yt.frontends.stream.api import \
     load_uniform_grid, load_amr_grids, \
     load_particles, load_hexahedral_mesh, load_octree, \
-    hexahedral_connectivity, load_unstructured_mesh
+    hexahedral_connectivity
 
 # For backwards compatibility
 GadgetDataset = frontends.gadget.GadgetDataset
