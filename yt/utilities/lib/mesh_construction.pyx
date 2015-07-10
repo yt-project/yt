6248b4539c4405a025810d499a1f9b01983d4d33
Andrew Myers <atmyers2@gmail.com>
2015-07-10 00:06:00 -0700

    we don't really need this base class
    --HG--
    branch : yt

--- yt/utilities/lib/mesh_construction.pyx
+++ yt/utilities/lib/mesh_construction.pyx
@@ -27,131 +27,7 @@ cdef extern from "mesh_construction.h":
     int triangulate_tetra[MAX_NUM_TRI][3]
 
 
-cdef class TriangleMesh:
-    r'''
-
-    This class constructs a polygon mesh with triangular elements and 
-    adds it to the scene. 
-
-    Parameters
-    ----------
-
-    scene : YTEmbreeScene
-        This is the scene to which the constructed polygons will be
-        added.
-    vertices : a np.ndarray of floats. 
-        This specifies the x, y, and z coordinates of the vertices in 
-        the polygon mesh. This should either have the shape 
-        (num_triangles, 3, 3), or the shape (num_vertices, 3), depending
-        on the value of the `indices` parameter.
-    indices : either None, or a np.ndarray of ints
-        If None, then vertices must have the shape (num_triangles, 3, 3).
-        In this case, `vertices` specifices the coordinates of each
-        vertex of each triangle in the mesh, with vertices being 
-        duplicated if they are shared between triangles. For example,
-        if indices is None, then vertices[2][1][0] should give you 
-        the x-coordinate of the 2nd vertex of the 3rd triangle.
-        If indices is a np.ndarray, then it must have the shape
-        (num_triangles, 3), and `vertices` must have the shape
-        (num_vertices, 3). In this case, indices[2][1] tells you 
-        the index of the 2nd vertex of the 3rd triangle in `indices`,
-        while vertices[5][2] tells you the z-coordinate of the 6th
-        vertex in the mesh. Note that the indexing is assumed to be
-        zero-based. In this setup, vertices can be shared between
-        triangles, and the number of vertices can be less than 3 times
-        the number of triangles.
-            
-    '''
-
-    cdef Vertex* vertices
-    cdef Triangle* indices
-    cdef unsigned int mesh
-    cdef double* field_data
-    cdef rtcg.RTCFilterFunc filter_func
-    cdef int tpe, vpe
-    cdef int[MAX_NUM_TRI][3] tri_array
-    cdef long* element_indices
-    cdef MeshDataContainer datac
-
-    def __init__(self, YTEmbreeScene scene,
-                 np.ndarray vertices,
-                 np.ndarray indices = None):
-
-        if indices is None:
-            self._build_from_flat(scene, vertices)
-        else:
-            self._build_from_indices(scene, vertices, indices)
-
-    cdef void _build_from_flat(self, YTEmbreeScene scene, 
-                               np.ndarray tri_vertices):
-        cdef int i, j
-        cdef int nt = tri_vertices.shape[0]
-        # In this scheme, we don't share any vertices.  This leads to cracks,
-        # but also means we have exactly three times as many vertices as
-        # triangles.
-        cdef unsigned int mesh = rtcg.rtcNewTriangleMesh(scene.scene_i,
-                        rtcg.RTC_GEOMETRY_STATIC, nt, nt*3, 1) 
-        
-        cdef Vertex* vertices = <Vertex*> rtcg.rtcMapBuffer(scene.scene_i, mesh,
-                        rtcg.RTC_VERTEX_BUFFER)
-
-        for i in range(nt):
-            for j in range(3):
-                vertices[i*3 + j].x = tri_vertices[i,j,0]
-                vertices[i*3 + j].y = tri_vertices[i,j,1]
-                vertices[i*3 + j].z = tri_vertices[i,j,2]
-        rtcg.rtcUnmapBuffer(scene.scene_i, mesh, rtcg.RTC_VERTEX_BUFFER)
-
-        cdef Triangle* triangles = <Triangle*> rtcg.rtcMapBuffer(scene.scene_i,
-                        mesh, rtcg.RTC_INDEX_BUFFER)
-        for i in range(nt):
-            triangles[i].v0 = i*3 + 0
-            triangles[i].v1 = i*3 + 1
-            triangles[i].v2 = i*3 + 2
-
-        rtcg.rtcUnmapBuffer(scene.scene_i, mesh, rtcg.RTC_INDEX_BUFFER)
-        self.vertices = vertices
-        self.indices = triangles
-        self.mesh = mesh
-
-    cdef void _build_from_indices(self, YTEmbreeScene scene,
-                                  np.ndarray tri_vertices,
-                                  np.ndarray tri_indices):
-        cdef int i
-        cdef int nv = tri_vertices.shape[0]
-        cdef int nt = tri_indices.shape[0]
-
-        cdef unsigned int mesh = rtcg.rtcNewTriangleMesh(scene.scene_i,
-                                        rtcg.RTC_GEOMETRY_STATIC, nt, nv, 1)
-
-        # set up vertex and triangle arrays. In this case, we just read
-        # them directly from the inputs
-        cdef Vertex* vertices = <Vertex*> rtcg.rtcMapBuffer(scene.scene_i, mesh,
-                                                    rtcg.RTC_VERTEX_BUFFER)
-
-        for i in range(nv):
-                vertices[i].x = tri_vertices[i, 0]
-                vertices[i].y = tri_vertices[i, 1]
-                vertices[i].z = tri_vertices[i, 2]
-
-        rtcg.rtcUnmapBuffer(scene.scene_i, mesh, rtcg.RTC_VERTEX_BUFFER)
-
-        cdef Triangle* triangles = <Triangle*> rtcg.rtcMapBuffer(scene.scene_i,
-                        mesh, rtcg.RTC_INDEX_BUFFER)
-
-        for i in range(nt):
-            triangles[i].v0 = tri_indices[i][0]
-            triangles[i].v1 = tri_indices[i][1]
-            triangles[i].v2 = tri_indices[i][2]
-
-        rtcg.rtcUnmapBuffer(scene.scene_i, mesh, rtcg.RTC_INDEX_BUFFER)
-
-        self.vertices = vertices
-        self.indices = triangles
-        self.mesh = mesh
-
-
-cdef class ElementMesh(TriangleMesh):
+cdef class ElementMesh:
     r'''
 
     Currently, we handle non-triangular mesh types by converting them 
@@ -181,6 +57,16 @@ cdef class ElementMesh(TriangleMesh):
         http://homepages.cae.wisc.edu/~tautges/papers/cnmev3.pdf
             
     '''
+
+    cdef Vertex* vertices
+    cdef Triangle* indices
+    cdef unsigned int mesh
+    cdef double* field_data
+    cdef rtcg.RTCFilterFunc filter_func
+    cdef int tpe, vpe
+    cdef int[MAX_NUM_TRI][3] tri_array
+    cdef long* element_indices
+    cdef MeshDataContainer datac
 
     def __init__(self, YTEmbreeScene scene,
                  np.ndarray vertices, 
