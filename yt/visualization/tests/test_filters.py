8729c04f4708b8936dbb973b0f170ed6abf9a151
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-07-28 14:23:18 -0500

    Rewrite test_filters to get rid of another race condition in nose, run tests only if scipy is installed
    --HG--
    branch : yt

--- yt/visualization/tests/test_filters.py
+++ yt/visualization/tests/test_filters.py
@@ -10,28 +10,23 @@ Tests for frb filters
 
 """
 
-from yt.testing import fake_amr_ds
+from yt.testing import fake_amr_ds, requires_module
 
 
-class TestFilters():
+@requires_module("scipy")
+def test_white_noise_filter(self):
+    ds = fake_amr_ds(fields=("density",))
+    p = ds.proj("density", "z")
+    frb = p.to_frb((1, 'unitary'), 64)
+    frb.apply_white_noise()
+    frb.apply_white_noise(1e-3)
+    frb["density"]
 
-    @classmethod
-    def setup_class(cls):
-        ds = fake_amr_ds(fields=("density",))
-        p = ds.proj("density", "z")
-        cls.frb = p.to_frb((1, 'unitary'), 64)
 
-    def teardown(self):
-        try:
-            del self.frb["density"]
-        except KeyError:
-            pass
-
-    def test_white_noise_filter(self):
-        self.frb.apply_white_noise()
-        self.frb.apply_white_noise(1e-3)
-        self.frb["density"]
-
-    def test_gauss_beam_filter(self):
-        self.frb.apply_gauss_beam(nbeam=15, sigma=1.0)
-        self.frb["density"]
+@requires_module("scipy")
+def test_gauss_beam_filter(self):
+    ds = fake_amr_ds(fields=("density",))
+    p = ds.proj("density", "z")
+    frb = p.to_frb((1, 'unitary'), 64)
+    frb.apply_gauss_beam(nbeam=15, sigma=1.0)
+    frb["density"]
