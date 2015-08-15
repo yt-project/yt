aa76dce6a722baca63ec4d1972d1ff2dc6c168da
Andrew Myers <atmyers2@gmail.com>
2015-08-15 12:43:23 -0700

    implementing read fluid selection
    --HG--
    branch : yt

--- yt/frontends/exodus_ii/io.py
+++ yt/frontends/exodus_ii/io.py
@@ -20,16 +20,19 @@ from yt.utilities.file_handler import \
     NetCDF4FileHandler
 from yt.utilities.logger import ytLogger as mylog
 
+
 class IOHandlerExodusII(BaseIOHandler):
     _particle_reader = False
     _dataset_type = "exodus_ii"
     _node_types = ("diffused", "convected")
     _INDEX_OFFSET = 1
 
-    def __init__(self, filename):
-        self.filename = filename
-        exodus_ii_handler       = NetCDF4FileHandler(filename)
-        self.ds  = exodus_ii_handler.dataset
+    def __init__(self, ds):
+        self.filename = ds.index_filename
+        exodus_ii_handler = NetCDF4FileHandler(self.filename)
+        self.handler = exodus_ii_handler.dataset
+        super(IOHandlerExodusII, self).__init__(ds)
+        self.io_fields = ds._get_nod_names()
 
     def _read_particle_coords(self, chunks, ptf):
         pass
@@ -44,14 +47,27 @@ class IOHandlerExodusII(BaseIOHandler):
         # dict gets returned at the end and it should be flat, with selected
         # data.  Note that if you're reading grid data, you might need to
         # special-case a grid selector object.
-        chunks = list(chunks) # chunks in this case correspond to mesh_id or slices in the ExodusII data
+        chunks = list(chunks)  # chunks in this case correspond to mesh_id or slices in the ExodusII data
+        chunk = chunks[0]
+        mesh_id = chunk.objs[0].mesh_id
+        ci = self.handler.variables['connect%d' % (mesh_id + 1)][:] - 1
+        nodes_per_element = ci.shape[1]
         rv = {}
         for field in fields:
             ftype, fname = field
-            ind = _INDEX_OFFSET
+            rv[field] = np.empty((size, nodes_per_element), dtype="float64")
+        ngrids = sum(len(chunk.objs) for chunk in chunks)
+        mylog.debug("Reading %s cells of %s fields in %s blocks",
+                    size, [fname for ftype, fname in fields], ngrids)
+        for field in fields:
+            ind = 0
+            ftype, fname = field
+            field_ind = self.io_fields.index(fname)
+            fdata = self.handler.variables['vals_nod_var%d' % (field_ind + 1)]
+            data = fdata[0][ci]
             for chunk in chunks:
-                rv[field] = chunk
-
+                for g in chunk.objs:
+                    ind += g.select(selector, data, rv[field], ind)  # caches
         return rv
 
     def _read_chunk_data(self, chunk, fields):
