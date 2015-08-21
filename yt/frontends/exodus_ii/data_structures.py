94ccd35ee3e1ca89c64bfc87e56b551672056380
Andrew Myers <atmyers2@gmail.com>
2015-08-21 15:49:22 -0500

    don't load memory intensive things into self.parameters
    --HG--
    branch : yt

--- yt/frontends/exodus_ii/data_structures.py
+++ yt/frontends/exodus_ii/data_structures.py
@@ -46,11 +46,11 @@ class ExodusIIUnstructuredIndex(UnstructuredIndex):
         self._connectivity_length = self.meshes[0].connectivity_indices.shape[1]
 
     def _initialize_mesh(self):
-        coords = self.dataset.parameters['coordinates']
+        coords = self.ds._read_coordinates()
         self.meshes = [ExodusIIUnstructuredMesh(
             mesh_id, self.index_filename, conn_ind, coords, self)
                        for mesh_id, conn_ind in
-                       enumerate(self.dataset.parameters['connectivity'])]
+                       enumerate(self.ds._read_connectivity())]
 
     def _setup_data_io(self):
         self.io = io_registry[self.dataset_type](self.ds)
@@ -77,11 +77,11 @@ class ExodusIIDataset(Dataset):
 
         self.parameter_filename         = filename
         self.fluid_types += self._get_fluid_types()
+        self.step                       = step
         Dataset.__init__(self, filename, dataset_type,
                          units_override = units_override)
         self.index_filename             = filename
         self.storage_filename           = storage_filename
-        self.step                       = step
 
     def _set_code_unit_attributes(self):
         # This is where quantities are created that represent the various
@@ -98,29 +98,31 @@ class ExodusIIDataset(Dataset):
         # self.magnetic_unit = self.quan(1.0, "gauss")
 
     def _parse_parameter_file(self):
-        self._load_variables()
+        self._handle = NetCDF4FileHandler(self.parameter_filename)
+        self._vars = self._handle.dataset.variables
         self._read_glo_var()
-        self.dimensionality             = self.parameters['coor_names'].shape[0]
+        self.dimensionality = self._vars['coor_names'].shape[0]
         self.parameters['info_records'] = self._load_info_records()
-        self.unique_identifier          = self._get_unique_identifier()
-        self.current_time               = self._get_current_time()
-        self.parameters['num_meshes']   = self.parameters['eb_status'].shape[0]
-        self.parameters['elem_names']   = self._get_elem_names()
-        self.parameters['nod_names']    = self._get_nod_names()
-        self.parameters['coordinates']  = self._load_coordinates()
-        self.parameters['connectivity'] = self._load_connectivity()
-        self.data                       = self._load_data()
-        self.domain_left_edge           = self._load_domain_edge(0)
-        self.domain_right_edge          = self._load_domain_edge(1)
-        self.periodicity                = (False, False, False)
-        self.domain_dimensions          = np.ones(3, "int32")
-        self.cosmological_simulation    = 0
-        self.current_redshift           = 0
-        self.omega_lambda               = 0
-        self.omega_matter               = 0
-        self.hubble_constant            = 0
-        self.refine_by                  = 0
-        self.num_steps                  = len(self.parameters['time_whole'])
+        self.unique_identifier = self._get_unique_identifier()
+        self.num_steps = len(self._vars['time_whole'])
+        self.current_time = self._get_current_time()
+        self.parameters['num_meshes'] = self._vars['eb_status'].shape[0]
+        self.parameters['elem_names'] = self._get_elem_names()
+        self.parameters['nod_names'] = self._get_nod_names()
+        self.domain_left_edge = self._load_domain_edge(0)
+        self.domain_right_edge = self._load_domain_edge(1)
+        self.periodicity = (False, False, False)
+
+        # These attributes don't really make sense for unstructured
+        # mesh data, but yt warns if they are not present, so we set
+        # them to dummy values here.
+        self.domain_dimensions = np.ones(3, "int32")
+        self.cosmological_simulation = 0
+        self.current_redshift = 0
+        self.omega_lambda = 0
+        self.omega_matter = 0
+        self.hubble_constant = 0
+        self.refine_by = 0
 
     def _get_fluid_types(self):
         handle = NetCDF4FileHandler(self.parameter_filename).dataset
@@ -152,7 +154,7 @@ class ExodusIIDataset(Dataset):
         names = self._get_glo_names()
         if not names:
             return
-        values = self.parameters['vals_glo_var'][:].transpose()
+        values = self._vars['vals_glo_var'][:].transpose()
         for name, value in zip(names, values):
             self.parameters[name] = value
 
@@ -161,20 +163,23 @@ class ExodusIIDataset(Dataset):
         Returns parsed version of the info_records.
         """
         try:
-            return load_info_records(self.parameters['info_records'])
+            return load_info_records(self._vars['info_records'])
         except (KeyError, TypeError):
             mylog.warning("No info_records found")
             return []
 
     def _get_unique_identifier(self):
         try:
-            return self.parameters['info_records']['Version Info']['Executable Timestamp']
-        except (KeyError, TypeError):
+            return self._vars['info_records']['Version Info']['Executable Timestamp']
+        except (KeyError, TypeError, IndexError):
             return self.parameter_filename.__hash__()
 
     def _get_current_time(self):
         try:
-            return self.parameters['time_whole'][0]
+            return self._vars['time_whole'][self.step]
+        except IndexError:
+            raise RuntimeError("Invalid step number, max is %d" \
+                               % (self.num_steps - 1))            
         except (KeyError, TypeError):
             return 0.0
 
@@ -184,7 +189,7 @@ class ExodusIIDataset(Dataset):
         """
         try:
             return [sanitize_string(v.tostring()) for v in
-                    self.parameters["name_glo_var"]]
+                    self._vars["name_glo_var"]]
         except (KeyError, TypeError):
             mylog.warning("name_glo_var not found")
             return []
@@ -195,7 +200,7 @@ class ExodusIIDataset(Dataset):
         """
         try:
             return [sanitize_string(v.tostring()) for v in
-                    self.parameters["name_elem_var"]]
+                    self._vars["name_elem_var"]]
         except (KeyError, TypeError):
             mylog.warning("name_elem_var not found")
             return []
@@ -206,12 +211,12 @@ class ExodusIIDataset(Dataset):
         """
         try:
             return [sanitize_string(v.tostring()) for v in
-                    self.parameters["name_nod_var"]]
+                    self._vars["name_nod_var"]]
         except (KeyError, TypeError):
             mylog.warning("name_nod_var not found")
             return []
 
-    def _load_coordinates(self):
+    def _read_coordinates(self):
         """
         Loads the coordinates for the mesh
         """
@@ -222,43 +227,43 @@ class ExodusIIDataset(Dataset):
 
         mylog.info("Loading coordinates")
 
-        if 'coord' in self.parameters.keys():
+        try:
             return np.array([coord for coord in
-                             self.parameters["coord"][:]]).transpose().copy()
-        else:
-            return np.array([self.parameters["coord%s" % ax][:]
+                             self._vars["coord"][:]]).transpose().copy()
+        except KeyError:
+            return np.array([self._vars["coord%s" % ax][:]
                              for ax in coord_axes]).transpose().copy()
 
-    def _load_connectivity(self):
+    def _read_connectivity(self):
         """
         Loads the connectivity data for the mesh
         """
         mylog.info("Loading connectivity")
         connectivity = []
         for i in range(self.parameters['num_meshes']):
-            connectivity.append(self.parameters["connect%d" % (i+1)][:].astype("i8"))
+            connectivity.append(self._vars["connect%d" % (i+1)][:].astype("i8"))
         return connectivity
 
-    def _load_data(self):
-        """
-        Loads the fluid data
-        """
-        data = []
-        for i in range(self.parameters['num_meshes']):
-            ci = self.parameters['connectivity'][i]
-            vals = {}
+    # def _load_data(self):
+    #     """
+    #     Loads the fluid data
+    #     """
+    #     data = []
+    #     for i in range(self.parameters['num_meshes']):
+    #         ci = self.parameters['connectivity'][i]
+    #         vals = {}
 
-            for j, elem_name in enumerate(self.parameters['elem_names']):
-                vals['gas', elem_name] = self.parameters["vals_elem_var%seb%s" % (j+1, i+1)][:].astype("f8")[-1,:]
+    #         for j, elem_name in enumerate(self.parameters['elem_names']):
+    #             vals['gas', elem_name] = self.parameters["vals_elem_var%seb%s" % (j+1, i+1)][:].astype("f8")[-1,:]
 
-            for j, nod_name in enumerate(self.parameters['nod_names']):
-                # We want just for this set of nodes all the node variables
-                # Use (ci - 1) to get these values
-                vals['gas', nod_name] = self.parameters["vals_nod_var%s" % (j+1)][:].astype("f8")[-1, ci - 1, ...]
+    #         for j, nod_name in enumerate(self.parameters['nod_names']):
+    #             # We want just for this set of nodes all the node variables
+    #             # Use (ci - 1) to get these values
+    #             vals['gas', nod_name] = self.parameters["vals_nod_var%s" % (j+1)][:].astype("f8")[-1, ci - 1, ...]
 
-            data.append(vals)
+    #         data.append(vals)
 
-        return data
+    #     return data
 
     def _load_domain_edge(self, domain_idx):
         """
@@ -268,9 +273,9 @@ class ExodusIIDataset(Dataset):
         - domain_idx: 0 corresponds to the left edge, 1 corresponds to the right edge
         """
         if domain_idx == 0:
-            return self.parameters['coordinates'].min(axis=0)
+            return self._read_coordinates().min(axis=0)
         if domain_idx == 1:
-            return self.parameters['coordinates'].max(axis=0)        
+            return self._read_coordinates().max(axis=0)
 
     @classmethod
     def _is_valid(self, *args, **kwargs):
