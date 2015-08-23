c78da991be5cbfac14684496c8516c5359469dc5
Andrew Myers <atmyers2@gmail.com>
2015-08-22 21:18:18 -0700

    adding a rendering test for the fake hexahedral ds
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/tests/test_mesh_render.py
+++ yt/visualization/volume_rendering/tests/test_mesh_render.py
@@ -14,21 +14,25 @@ Test Surface Mesh Rendering
 import yt
 import numpy as np
 from yt.testing import fake_tetrahedral_ds
+from yt.testing import fake_hexahedral_ds
 from yt.testing import requires_module
 from yt.visualization.volume_rendering.render_source import MeshSource
 from yt.visualization.volume_rendering.camera import Camera
-from yt.frontends.stream.sample_data.tetrahedral_mesh import \
-    _connectivity, \
-    _coordinates
+
 
 @requires_module("pyembree")
 def test_surface_mesh_render():
     ds = fake_tetrahedral_ds()
     ms = MeshSource(ds, ('connect1', 'test'))
     cam = Camera(ds)
-    im = ms.render(cam)
-    return im
+    im1 = ms.render(cam)
 
+    ds = fake_hexahedral_ds()
+    ms = MeshSource(ds, ('connect1', 'test'))
+    cam = Camera(ds)
+    im2 = ms.render(cam)
+    
+    return im1, im2
 
 if __name__ == "__main__":
-    im = test_surface_mesh_render()
+    im1, im2 = test_surface_mesh_render()
