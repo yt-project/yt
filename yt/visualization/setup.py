ed678b093534099a5c74d09c2e4fc7c2a83c2493
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-08-24 13:13:11 -0500

    Ensure mapserver package gets deployed in source installs
    --HG--
    branch : yt

--- yt/visualization/setup.py
+++ yt/visualization/setup.py
@@ -4,6 +4,7 @@
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('visualization', parent_package, top_path)
+    config.add_subpackage("mapserver")
     config.add_subpackage("volume_rendering")
     config.add_subpackage("tests")
     config.make_config_py()  # installs __config__.py
