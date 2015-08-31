92047140df36f37097825b2af3a2e90f491930c2
Kenza Arraki <karraki@gmail.com>
2015-08-30 22:51:42 -0600

    Backporting PR #1764 https://bitbucket.org/yt_analysis/yt/pull-requests/1764
    --HG--
    branch : stable

--- yt/frontends/art/data_structures.py
+++ yt/frontends/art/data_structures.py
@@ -193,7 +193,7 @@ class ARTDataset(Dataset):
         particle header, star files, etc.
         """
         base_prefix, base_suffix = filename_pattern['amr']
-        aexpstr = 'a'+file_amr.rsplit('a',1)[1].replace(base_suffix,'')
+        numericstr = file_amr.rsplit('_',1)[1].replace(base_suffix,'')
         possibles = glob.glob(os.path.dirname(os.path.abspath(file_amr))+"/*")
         for filetype, (prefix, suffix) in filename_pattern.items():
             # if this attribute is already set skip it
@@ -201,7 +201,10 @@ class ARTDataset(Dataset):
                 continue
             match = None
             for possible in possibles:
-                if possible.endswith(aexpstr+suffix):
+                if possible.endswith(numericstr+suffix):
+                    if os.path.basename(possible).startswith(prefix):
+                        match = possible
+                elif possible.endswith(suffix):
                     if os.path.basename(possible).startswith(prefix):
                         match = possible
             if match is not None:
