7822cdbaea0371b5d4f56588ffefbd664cb9c689
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-09-14 23:17:58 -0500

    [png writer] handle API change in matplotlib 1.5
    --HG--
    branch : yt

--- yt/utilities/png_writer.py
+++ yt/utilities/png_writer.py
@@ -10,19 +10,29 @@ Writing PNGs
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+import matplotlib
 import matplotlib._png as _png
 from yt.extern.six.moves import cStringIO
+from distutils.version import LooseVersion
+
+MPL_VERSION = LooseVersion(matplotlib.__version__)
 
 def write_png(buffer, filename, dpi=100):
     width = buffer.shape[1]
     height = buffer.shape[0]
-    _png.write_png(buffer, width, height, filename, dpi)
+    if MPL_VERSION < LooseVersion("1.5.0"):
+        _png.write_png(buffer, width, height, fileobj, dpi)
+    else:
+        _png.write_png(buffer, filename, dpi)
 
 def write_png_to_string(buffer, dpi=100, gray=0):
     width = buffer.shape[1]
     height = buffer.shape[0]
     fileobj = cStringIO()
-    _png.write_png(buffer, width, height, fileobj, dpi)
+    if MPL_VERSION < LooseVersion("1.5.0"):
+        _png.write_png(buffer, width, height, fileobj, dpi)
+    else:
+        _png.write_png(buffer, filename, dpi)
     png_str = fileobj.getvalue()
     fileobj.close()
     return png_str
