ca2b9248c763d4f1a9500036643f5a71aa0885e8
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-09-21 14:30:37 -0500

    Refactor MPL version selection to avoid repeat calls to LooseVersion
    --HG--
    branch : yt

--- yt/utilities/png_writer.py
+++ yt/utilities/png_writer.py
@@ -16,23 +16,29 @@ from yt.extern.six.moves import cStringIO
 from distutils.version import LooseVersion
 
 MPL_VERSION = LooseVersion(matplotlib.__version__)
+MPL_API_2_VERSION = LooseVersion("1.5.0")
+
+if MPL_VERSION < MPL_API_2_VERSION:
+    MPL_API_VERSION = 1
+else:
+    MPL_API_VERSION = 2
+
+def call_png_write_png(buffer, width, height, filename, dpi):
+    if MPL_API_VERSION == 1:
+        _png.write_png(buffer, width, height, filename, dpi)
+    else:
+        _png.write_png(buffer, filename, dpi)
 
 def write_png(buffer, filename, dpi=100):
     width = buffer.shape[1]
     height = buffer.shape[0]
-    if MPL_VERSION < LooseVersion("1.5.0"):
-        _png.write_png(buffer, width, height, filename, dpi)
-    else:
-        _png.write_png(buffer, filename, dpi)
+    call_png_write_png(buffer, width, height, filename, dpi)
 
 def write_png_to_string(buffer, dpi=100, gray=0):
     width = buffer.shape[1]
     height = buffer.shape[0]
     fileobj = cStringIO()
-    if MPL_VERSION < LooseVersion("1.5.0"):
-        _png.write_png(buffer, width, height, fileobj, dpi)
-    else:
-        _png.write_png(buffer, fileobj, dpi)
+    call_png_write_png(buffer, width, height, fileobj, dpi)
     png_str = fileobj.getvalue()
     fileobj.close()
     return png_str
