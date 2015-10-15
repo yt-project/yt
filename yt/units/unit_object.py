4992b795e9bef5b0f83a182a6dd82ca0be55b18e
Andrew Myers <atmyers2@gmail.com>
2015-10-15 16:43:13 -0700

    Give a better error message when you try to use code units with a YTArray that has not beed defined using ds.arr
    --HG--
    branch : yt

--- yt/units/unit_object.py
+++ yt/units/unit_object.py
@@ -545,8 +545,13 @@ def _lookup_unit_symbol(symbol_str, unit_symbol_lut):
             return (unit_data[0] * prefix_value, unit_data[1])
 
     # no dice
-    raise UnitParseError("Could not find unit symbol '%s' in the provided " \
-                         "symbols." % symbol_str)
+    if symbol_str.startswith('code_'):
+        raise UnitParseError(
+            "Code units have not been defined. \n"
+            "Try creating the array or quantity using ds.arr instead: \n")
+    else:
+        raise UnitParseError("Could not find unit symbol '%s' in the provided " \
+                             "symbols." % symbol_str)
 
 def validate_dimensions(dimensions):
     if isinstance(dimensions, Mul):
