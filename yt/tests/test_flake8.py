1e7a5bf39ec96c89ed76a8881994b9a1388521dc
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-11-05 21:26:58 -0600

    Adding a test for flake8 issues to the unit tests
    --HG--
    branch : yt

---
+++ yt/tests/test_flake8.py
@@ -0,0 +1,21 @@
+import subprocess
+import yt
+import os
+
+from yt.testing import requires_module
+
+
+@requires_module('flake8')
+def test_flake8():
+    yt_dir = os.path.dirname(os.path.abspath(yt.__file__))
+    initial_dir = os.getcwd()
+    os.chdir(yt_dir)
+    output_file = os.path.sep.join([os.path.dirname(initial_dir), 'flake8.out'])
+    output_string = "--output-file=%s" % output_file
+    subprocess.call(['flake8', output_string, os.curdir])
+    os.chdir(initial_dir)
+    with open(output_file) as f:
+        flake8_output = f.readlines()
+    if flake8_output != []:
+        raise AssertionError(
+            "flake8 found style errors:\n\n%s" % "\n".join(flake8_output))
