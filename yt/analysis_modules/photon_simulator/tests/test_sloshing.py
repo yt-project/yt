314d5b1e4c00265ebf51f2aa2e29b609a2bf34b2
John ZuHone <jzuhone@gmail.com>
2015-11-17 17:11:24 -0500

    Documenting the new features
    --HG--
    branch : yt

--- yt/analysis_modules/photon_simulator/tests/test_sloshing.py
+++ yt/analysis_modules/photon_simulator/tests/test_sloshing.py
@@ -18,7 +18,6 @@ from yt.config import ytcfg
 from yt.testing import requires_file
 from yt.utilities.answer_testing.framework import requires_ds, \
     GenericArrayTest, data_dir_load
-import numpy as np
 from numpy.testing import assert_array_equal
 from numpy.random import RandomState
 import os
