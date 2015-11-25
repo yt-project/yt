2db05eced9f30c4c70c1fe197124f8c7599cc124
Andrew Myers <atmyers2@gmail.com>
2015-11-25 11:55:01 -0800

    Add new class for answer testing phase plot modification functions
    --HG--
    branch : yt

--- yt/utilities/answer_testing/framework.py
+++ yt/utilities/answer_testing/framework.py
@@ -52,6 +52,7 @@ from yt.utilities.command_line import get_yt_version
 import matplotlib.image as mpimg
 import yt.visualization.plot_window as pw
 import yt.visualization.particle_plots as pp
+from yt.visualization.profile_plotter import PhasePlot
 
 mylog = logging.getLogger('nose.plugins.answer-testing')
 run_big_data = False
@@ -350,7 +351,6 @@ class AnswerTestingTest(object):
 
     def create_plot(self, ds, plot_type, plot_field, plot_axis, plot_kwargs = None):
         # plot_type should be a string
-        # plot_args should be a tuple
         # plot_kwargs should be a dict
         if plot_type is None:
             raise RuntimeError('Must explicitly request a plot type')
@@ -731,6 +731,37 @@ class PlotWindowAttributeTest(AnswerTestingTest):
                                 self.plot_axis, self.plot_kwargs)
         for r in self.callback_runners:
             r(self, plot)
+        attr = getattr(plot, self.attr_name)
+        attr(*self.attr_args[0], **self.attr_args[1])
+        tmpfd, tmpname = tempfile.mkstemp(suffix='.png')
+        os.close(tmpfd)
+        plot.save(name=tmpname)
+        image = mpimg.imread(tmpname)
+        os.remove(tmpname)
+        return [zlib.compress(image.dumps())]
+
+    def compare(self, new_result, old_result):
+        compare_image_lists(new_result, old_result, self.decimals)
+
+class PhasePlotAttributeTest(AnswerTestingTest):
+    _type_name = "PhasePlotAttribute"
+    _attrs = ('plot_type', 'plot_field', 'attr_name', 'attr_args')
+    def __init__(self, ds_fn, x_field, y_field, z_field, 
+                 attr_name, attr_args, decimals, plot_type='PhasePlot'):
+        super(PhasePlotAttributeTest, self).__init__(ds_fn)
+        self.data_source = self.ds.all_data()
+        self.plot_type = plot_type
+        self.x_field = x_field
+        self.y_field = y_field
+        self.z_field = z_field
+        self.plot_kwargs = {}
+        self.attr_name = attr_name
+        self.attr_args = attr_args
+        self.decimals = decimals
+
+    def run(self):
+        plot = PhasePlot(*(self.data_source, self.x_field, self.y_field, 
+                           self.z_field), **self.plot_kwargs)
         attr = getattr(plot, self.attr_name)
         attr(*self.attr_args[0], **self.attr_args[1])
         tmpfd, tmpname = tempfile.mkstemp(suffix='.png')
