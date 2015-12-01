40ebda2a742323cf62b7c3ca6f698e087b40b3e5
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-12-01 12:04:53 -0600

    Use assert_equal in test_index_array
    --HG--
    branch : yt

--- yt/units/tests/test_index_array.py
+++ yt/units/tests/test_index_array.py
@@ -17,33 +17,34 @@ Test ndarray subclass that handles indexing along dimensions with units.
 import yt.units as u
 import numpy as np
 
+from yt.testing import assert_equal
 from yt.units.index_array import IndexArray
 
 def test_index_array_multiplication():
     vals = np.random.random((100, 3))
     index = IndexArray(vals, input_units=[u.km, u.g, u.s])
     index *= index
-    
-    assert(index.units[0] == (u.km * u.km).units)
-    assert(index.units[1] == (u.g * u.g).units)
-    assert(index.units[2] == (u.s * u.s).units)
+
+    assert_equal(index.units[0], (u.km * u.km).units)
+    assert_equal(index.units[1], (u.g * u.g).units)
+    assert_equal(index.units[2], (u.s * u.s).units)
 
     index = IndexArray(vals, input_units=[u.km, u.g, u.s])
 
     index *= u.km
 
-    assert(index.units[0] == (u.km * u.km).units)
-    assert(index.units[1] == (u.km * u.g).units)
-    assert(index.units[2] == (u.km * u.s).units)
+    assert_equal(index.units[0], (u.km * u.km).units)
+    assert_equal(index.units[1], (u.km * u.g).units)
+    assert_equal(index.units[2], (u.km * u.s).units)
 
     index2 = index * 2
 
-    assert(index.units[0] == index2.units[0])
-    assert(index.units[1] == index2.units[1])
-    assert(index.units[2] == index2.units[2])
+    assert_equal(index.units[0], index2.units[0])
+    assert_equal(index.units[1], index2.units[1])
+    assert_equal(index.units[2], index2.units[2])
 
     index3 = 2 * index
 
-    assert(index.units[0] == index3.units[0])
-    assert(index.units[1] == index3.units[1])
-    assert(index.units[2] == index3.units[2])
+    assert_equal(index.units[0], index3.units[0])
+    assert_equal(index.units[1], index3.units[1])
+    assert_equal(index.units[2], index3.units[2])
