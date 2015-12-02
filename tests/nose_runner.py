023e83f6156d8e53299fe374012e36da1abdfe56
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-12-01 21:13:37 -0600

    Simplify nose_runner
    --HG--
    branch : yt

--- tests/nose_runner.py
+++ tests/nose_runner.py
@@ -12,20 +12,25 @@ answers_dir = os.path.join(test_dir, "answers")
 with open('tests/tests_%i.%i.yaml' % sys.version_info[:2], 'r') as obj:
     tests = yaml.load(obj)
 
-base_argv = ['fake', '--local-dir=%s' % answers_dir, '-v', '-s', '--nologcapture',
+base_argv = ['--local-dir=%s' % answers_dir, '-v', '-s', '--nologcapture',
              '--with-answer-testing', '--answer-big-data', '--local']
-
-def run_job(answer):
-    global base_argv, answers_dir, tests
-    argv = []
+args = []
+for answer in list(tests.keys()):
+    argv = [answer]
     argv += base_argv
     argv.append('--xunit-file=%s.xml' % answer)
     argv.append('--answer-name=%s' % answer)
     argv += tests[answer]
+    args.append(argv)
+
+def run_job(argv):
+    answer = argv[0]
     if not os.path.isdir(os.path.join(answers_dir, answer)):
         nose.run(argv=argv + ['--answer-store'],
                  addplugins=[AnswerTesting()], exit=False)
     nose.run(argv=argv, addplugins=[AnswerTesting()], exit=False)
 
 pool = mp.Pool(processes=mp.cpu_count())
-results = pool.map(run_job, list(tests.keys()))
+results = pool.map(run_job, args)
+pool.close()
+pool.join()
