e9e203e47699bc6546d0b08efc09abd60fe383eb
John ZuHone <jzuhone@gmail.com>
2015-12-02 16:47:53 -0600

    adding unit systems to fluid and geometric fields
    --HG--
    branch : yt

--- yt/fields/fluid_fields.py
+++ yt/fields/fluid_fields.py
@@ -49,21 +49,23 @@ def setup_fluid_fields(registry, ftype = "gas", slice_info = None):
     else:
         sl_left, sl_right, div_fac = slice_info
 
-    create_vector_fields(registry, "velocity", "cm / s", ftype, slice_info)
+    unit_system = registry.ds.unit_system
+
+    create_vector_fields(registry, "velocity", unit_system["velocity"], ftype, slice_info)
 
     def _cell_mass(field, data):
         return data[ftype, "density"] * data[ftype, "cell_volume"]
 
     registry.add_field((ftype, "cell_mass"),
         function=_cell_mass,
-        units="g")
+        units=unit_system["mass"])
 
     def _sound_speed(field, data):
         tr = data.ds.gamma * data[ftype, "pressure"] / data[ftype, "density"]
         return np.sqrt(tr)
     registry.add_field((ftype, "sound_speed"),
              function=_sound_speed,
-             units="cm/s")
+             units=unit_system["velocity"])
 
     def _radial_mach_number(field, data):
         """ Radial component of M{|v|/c_sound} """
@@ -79,7 +81,7 @@ def setup_fluid_fields(registry, ftype = "gas", slice_info = None):
                                               + data[ftype, "velocity_z"]**2.0 )
     registry.add_field((ftype, "kinetic_energy"),
                        function = _kin_energy,
-                       units = "erg / cm**3")
+                       units = unit_system["pressure"])
 
     def _mach_number(field, data):
         """ M{|v|/c_sound} """
@@ -100,7 +102,7 @@ def setup_fluid_fields(registry, ftype = "gas", slice_info = None):
 
     registry.add_field((ftype, "courant_time_step"),
              function=_courant_time_step,
-             units="s")
+             units=unit_system["time"])
 
     def _pressure(field, data):
         """ M{(Gamma-1.0)*rho*E} """
@@ -110,7 +112,7 @@ def setup_fluid_fields(registry, ftype = "gas", slice_info = None):
 
     registry.add_field((ftype, "pressure"),
              function=_pressure,
-             units="dyne/cm**2")
+             units=unit_system["pressure"])
 
     def _kT(field, data):
         return (kboltz*data[ftype, "temperature"]).in_units("keV")
@@ -143,7 +145,7 @@ def setup_fluid_fields(registry, ftype = "gas", slice_info = None):
         return data[ftype, "metal_density"] * data[ftype, "cell_volume"]
     registry.add_field((ftype, "metal_mass"),
                        function=_metal_mass,
-                       units="g")
+                       units=unit_system["mass"])
 
     def _number_density(field, data):
         field_data = np.zeros_like(data["gas", "%s_number_density" % \
@@ -153,7 +155,7 @@ def setup_fluid_fields(registry, ftype = "gas", slice_info = None):
         return field_data
     registry.add_field((ftype, "number_density"),
                        function = _number_density,
-                       units="cm**-3")
+                       units=unit_system["number_density"])
     
     def _mean_molecular_weight(field, data):
         return (data[ftype, "density"] / (mh * data[ftype, "number_density"]))
@@ -161,17 +163,18 @@ def setup_fluid_fields(registry, ftype = "gas", slice_info = None):
               function=_mean_molecular_weight,
               units="")
 
-    setup_gradient_fields(registry, (ftype, "pressure"), "dyne/cm**2",
+    setup_gradient_fields(registry, (ftype, "pressure"), unit_system["pressure"],
                           slice_info)
 
-    setup_gradient_fields(registry, (ftype, "density"), "g / cm**3",
+    setup_gradient_fields(registry, (ftype, "density"), unit_system["density"],
                           slice_info)
 
-    create_averaged_field(registry, "density", "g/cm**3",
+    create_averaged_field(registry, "density", unit_system["density"],
                           ftype=ftype, slice_info=slice_info,
                           weight="cell_mass")
 
 def setup_gradient_fields(registry, grad_field, field_units, slice_info = None):
+    from yt.units.unit_object import Unit
     assert(isinstance(grad_field, tuple))
     ftype, fname = grad_field
     if slice_info is None:
@@ -197,10 +200,8 @@ def setup_gradient_fields(registry, grad_field, field_units, slice_info = None):
             return new_field
         return func
 
-    if field_units != "":
-        grad_units = "(%s) / cm" % field_units
-    else:
-        grad_units = "1 / cm"
+    field_units = Unit(field_units, registry=registry.ds.unit_registry)
+    grad_units = field_units / registry.ds.unit_system["length"]
 
     for axi, ax in enumerate('xyz'):
         f = grad_func(axi, ax)
