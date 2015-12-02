a8275596e138294cb983b7b04f038a3ccdc954f7
John ZuHone <jzuhone@gmail.com>
2015-12-02 11:48:56 -0600

    Automatically add unit system registries
    --HG--
    branch : yt

--- yt/units/unit_systems.py
+++ yt/units/unit_systems.py
@@ -15,14 +15,17 @@ from yt.extern.six import string_types
 from yt.units import dimensions
 from yt.units.unit_object import Unit
 
+unit_system_registry = {}
+
 class UnitSystem(object):
-    def __init__(self, length_unit, mass_unit, time_unit,
+    def __init__(self, name, length_unit, mass_unit, time_unit,
                  temperature_unit, angle_unit):
         self.units_map = {dimensions.length: length_unit,
                           dimensions.mass: mass_unit,
                           dimensions.time: time_unit,
                           dimensions.temperature: temperature_unit,
                           dimensions.angle: angle_unit}
+        unit_system_registry[name] = self
 
     def __getitem__(self, key):
         if isinstance(key, string_types):
@@ -44,31 +47,25 @@ class UnitSystem(object):
             key = getattr(dimensions, key)
         self.units_map[key] = Unit(value)
 
-cgs_unit_system = UnitSystem("cm", "g", "s", "K", "radian")
+cgs_unit_system = UnitSystem("cgs", "cm", "g", "s", "K", "radian")
 cgs_unit_system[dimensions.energy] = "erg"
 cgs_unit_system[dimensions.pressure] = "dyne/cm**2"
 cgs_unit_system[dimensions.force] = "dyne"
 cgs_unit_system[dimensions.magnetic_field_cgs] = "gauss"
 
-mks_unit_system = UnitSystem("m", "kg", "s", "K", "radian")
+mks_unit_system = UnitSystem("mks", "m", "kg", "s", "K", "radian")
 mks_unit_system[dimensions.current_mks] = "A"
 mks_unit_system[dimensions.energy] = "J"
 mks_unit_system[dimensions.pressure] = "Pa"
 mks_unit_system[dimensions.force] = "N"
 mks_unit_system[dimensions.magnetic_field_mks] = "T"
 
-imperial_unit_system = UnitSystem("ft", "lbm", "s", "R", "radian")
+imperial_unit_system = UnitSystem("imperial", "ft", "lbm", "s", "R", "radian")
 imperial_unit_system[dimensions.force] = "lbf"
 
-galactic_unit_system = UnitSystem("kpc", "Msun", "Myr", "K", "radian")
+galactic_unit_system = UnitSystem("galactic", "kpc", "Msun", "Myr", "K", "radian")
 galactic_unit_system[dimensions.energy] = "keV"
 galactic_unit_system[dimensions.magnetic_field_cgs] = "uG"
 
-geographic_unit_system = UnitSystem("mile", "lbm", "s", "K", "radian")
+geographic_unit_system = UnitSystem("geographic", "mile", "lbm", "s", "K", "radian")
 geographic_unit_system[dimensions.pressure] = "atm"
-
-unit_system_registry = {"cgs": cgs_unit_system,
-                        "mks": mks_unit_system,
-                        "imperial": imperial_unit_system,
-                        "galactic": galactic_unit_system,
-                        "geographic": geographic_unit_system}
