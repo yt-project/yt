6196b4e6c7be258e002d7f56767fa2d40ccbe1d4
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-12-01 19:00:58 -0600

    Add parallel nose runner, store all information for answer testing in a dedicated yaml file
    --HG--
    branch : yt

---
+++ tests/nose_runner.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import yaml
+import multiprocessing as mp
+import nose
+from yt.config import ytcfg
+from yt.utilities.answer_testing.framework import AnswerTesting
+
+test_dir = ytcfg.get("yt", "test_data_dir")
+answers_dir = os.path.join(test_dir, "answers")
+
+with open('tests/tests_%i.%i.yaml' % sys.version_info[:2], 'r') as obj:
+    tests = yaml.load(obj)
+
+base_argv = ['fake', '--local-dir=%s' % answers_dir, '-v', '-s', '--nologcapture',
+             '--with-answer-testing', '--answer-big-data', '--local']
+
+def run_job(answer):
+    global base_argv, answers_dir
+    argv = base_argv
+    argv.append('--xunit-file=%s.xml' % answer)
+    argv.append('--answer-name=%s' % answer)
+    argv += tests[answer]
+    if not os.path.isdir(os.path.join(answers_dir, answer)):
+        nose.run(argv=argv + ['--answer-store'],
+                 addplugins=[AnswerTesting()], exit=False)
+    nose.run(argv=argv, addplugins=[AnswerTesting()], exit=False)
+
+pool = mp.Pool(processes=mp.cpu_count())
+results = pool.map(run_job, list(tests.keys()))
