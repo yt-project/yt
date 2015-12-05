f65c63982c08de2b2fb550789d39d514f26d23dc
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-12-05 16:41:19 -0600

    Use Ellipsis operator to handle slixing during IndexArray unit conversions
    --HG--
    branch : yt

--- yt/units/index_array.py
+++ yt/units/index_array.py
@@ -202,18 +202,12 @@ class IndexArray(YTArray):
 
         new_units = []
         new_values = self.v
-        ndim = len(new_values.shape)
 
         for i, (sunit, unit) in enumerate(zip(self.units, units)):
             nu = sunit._unit_repr_check_same(unit)
             new_units.append(nu)
             conversion_factor, offset = sunit.get_conversion_factor(nu)
-            if ndim == 2:
-                new_values[:, i] *= conversion_factor
-            elif ndim == 1:
-                new_values[i] *= conversion_factor
-            else:
-                raise NotImplementedError
+            new_values[..., i] *= conversion_factor
 
         new_array = IndexArray(new_values, new_units)
 
