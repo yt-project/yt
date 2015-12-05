27e4e0bcf8ba689086512b17c6649126242a72d0
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-12-05 16:55:09 -0600

    Remove import of unused exception
    --HG--
    branch : yt

--- yt/units/index_array.py
+++ yt/units/index_array.py
@@ -31,8 +31,6 @@ from yt.units.yt_array import \
     YTQuantity
 from yt.units.unit_object import \
     Unit
-from yt.utilities.exceptions import \
-    YTImmutableUnitsError
 
 ELLIPSIS_TYPE = type(Ellipsis)
 
