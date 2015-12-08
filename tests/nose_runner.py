fde809c9d1ec47d4e426c0040724afcc44ad1282
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-12-08 11:10:15 -0600

    Use mp.Process instead of mp.Pool
    --HG--
    branch : yt

--- tests/nose_runner.py
+++ tests/nose_runner.py
@@ -3,34 +3,46 @@ import os
 import yaml
 import multiprocessing as mp
 import nose
+import glob
 from yt.config import ytcfg
 from yt.utilities.answer_testing.framework import AnswerTesting
 
-test_dir = ytcfg.get("yt", "test_data_dir")
-answers_dir = os.path.join(test_dir, "answers")
-
-with open('tests/tests_%i.%i.yaml' % sys.version_info[:2], 'r') as obj:
-    tests = yaml.load(obj)
-
-base_argv = ['--local-dir=%s' % answers_dir, '-v', '-s', '--nologcapture',
-             '--with-answer-testing', '--answer-big-data', '--local']
-args = []
-for answer in list(tests.keys()):
-    argv = [answer]
-    argv += base_argv
-    argv.append('--xunit-file=%s.xml' % answer)
-    argv.append('--answer-name=%s' % answer)
-    argv += tests[answer]
-    args.append(argv)
-
 def run_job(argv):
+    cur_stderr = sys.stderr
+    sys.stderr = open(str(os.getpid()) + ".out", "w")
     answer = argv[0]
+    test_dir = ytcfg.get("yt", "test_data_dir")
+    answers_dir = os.path.join(test_dir, "answers")
     if not os.path.isdir(os.path.join(answers_dir, answer)):
         nose.run(argv=argv + ['--answer-store'],
                  addplugins=[AnswerTesting()], exit=False)
     nose.run(argv=argv, addplugins=[AnswerTesting()], exit=False)
+    sys.stderr = cur_stderr
 
-pool = mp.Pool(processes=mp.cpu_count())
-results = pool.map(run_job, args)
-pool.close()
-pool.join()
+if __name__ == "__main__":
+    test_dir = ytcfg.get("yt", "test_data_dir")
+    answers_dir = os.path.join(test_dir, "answers")
+    with open('tests/tests_%i.%i.yaml' % sys.version_info[:2], 'r') as obj:
+        tests = yaml.load(obj)
+
+    base_argv = ['--local-dir=%s' % answers_dir, '-v', '-s', '--nologcapture',
+                 '--with-answer-testing', '--answer-big-data', '--local']
+    args = []
+    for answer in list(tests.keys()):
+        argv = [answer]
+        argv += base_argv
+        argv.append('--xunit-file=%s.xml' % answer)
+        argv.append('--answer-name=%s' % answer)
+        argv += tests[answer]
+        args.append(argv)
+    
+    processes = [mp.Process(target=run_job, args=(args[i],))
+                 for i in range(len(args))]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
+    for fname in glob.glob("*.out"):
+        with open(fname, 'r') as fin:
+            print(fin.read())
+        os.remove(fname)
