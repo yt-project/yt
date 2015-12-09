606137fe7898b7977aca32c3d57444b8278362fa
John ZuHone <jzuhone@gmail.com>
2015-12-09 00:26:44 -0500

    Updating the magnetic field test
    --HG--
    branch : yt

--- yt/fields/tests/test_magnetic_fields.py
+++ yt/fields/tests/test_magnetic_fields.py
@@ -1,6 +1,7 @@
 import numpy as np
 from yt.utilities.physical_constants import mu_0
 from yt.testing import assert_almost_equal, fake_random_ds
+from yt.frontends.stream.api import load_uniform_grid
 
 def setup():
     from yt.config import ytcfg
@@ -8,40 +9,44 @@ def setup():
 
 def test_magnetic_fields():
 
-    fields = ("magnetic_field_x", "magnetic_field_y", "magnetic_field_z")
-    cgs_units = ("gauss",)*3
-    mks_units = ("T",)*3
+    ddims = (16,16,16)
+    data1 = {"magnetic_field_x":(np.random.random(size=ddims),"T"),
+             "magnetic_field_y":(np.random.random(size=ddims),"T"),
+             "magnetic_field_z":(np.random.random(size=ddims),"T")}
+    data2 = {}
+    for field in data1:
+        data2[field] = (data1[field][0]*1.0e4, "gauss")
 
-    ds_cgs = fake_random_ds(16, fields=fields, units=cgs_units, nprocs=4)
-    ds_mks = fake_random_ds(16, fields=fields, units=mks_units, nprocs=4,
-                            unit_system="mks")
+    ds1 = load_uniform_grid(data1, ddims, unit_system="cgs")
+    ds2 = load_uniform_grid(data2, ddims, unit_system="mks")
 
-    ds_cgs.index
-    ds_mks.index
+    ds1.index
+    ds2.index
 
-    dd_cgs = ds_cgs.all_data()
-    dd_mks = ds_mks.all_data()
+    dd1 = ds1.all_data()
+    dd2 = ds2.all_data()
 
-    assert ds_cgs.field_info["gas","magnetic_field_strength"].units == "gauss"
-    assert ds_cgs.field_info["gas","magnetic_field_poloidal"].units == "gauss"
-    assert ds_cgs.field_info["gas","magnetic_field_toroidal"].units == "gauss"
-    assert ds_mks.field_info["gas","magnetic_field_strength"].units == "T"
-    assert ds_mks.field_info["gas","magnetic_field_poloidal"].units == "T"
-    assert ds_mks.field_info["gas","magnetic_field_toroidal"].units == "T"
+    assert ds1.fields.gas.magnetic_field_strength.units == "gauss"
+    assert ds1.fields.gas.magnetic_field_poloidal.units == "gauss"
+    assert ds1.fields.gas.magnetic_field_toroidal.units == "gauss"
+    assert ds2.fields.gas.magnetic_field_strength.units == "T"
+    assert ds2.fields.gas.magnetic_field_poloidal.units == "T"
+    assert ds2.fields.gas.magnetic_field_toroidal.units == "T"
 
-    emag_cgs = (dd_cgs["magnetic_field_x"]**2 +
-                dd_cgs["magnetic_field_y"]**2 +
-                dd_cgs["magnetic_field_z"]**2)/(8.0*np.pi)
-    emag_cgs.convert_to_units("dyne/cm**2")
+    emag1 = (dd1["magnetic_field_x"]**2 +
+             dd1["magnetic_field_y"]**2 +
+             dd1["magnetic_field_z"]**2)/(8.0*np.pi)
+    emag1.convert_to_units("dyne/cm**2")
 
-    emag_mks = (dd_mks["magnetic_field_x"]**2 +
-                dd_mks["magnetic_field_y"]**2 +
-                dd_mks["magnetic_field_z"]**2)/(2.0*mu_0)
-    emag_mks.convert_to_units("Pa")
+    emag2 = (dd2["magnetic_field_x"]**2 +
+             dd2["magnetic_field_y"]**2 +
+             dd2["magnetic_field_z"]**2)/(2.0*mu_0)
+    emag2.convert_to_units("Pa")
 
-    yield assert_almost_equal, emag_cgs, dd_cgs["magnetic_energy"]
-    yield assert_almost_equal, emag_mks, dd_mks["magnetic_energy"]
+    yield assert_almost_equal, emag1, dd1["magnetic_energy"]
+    yield assert_almost_equal, emag2, dd2["magnetic_energy"]
 
-    assert str(emag_cgs.units) == str(dd_cgs["magnetic_energy"].units)
-    assert str(emag_mks.units) == str(dd_mks["magnetic_energy"].units)
+    assert str(emag1.units) == str(dd1["magnetic_energy"].units)
+    assert str(emag2.units) == str(dd2["magnetic_energy"].units)
 
+    yield assert_almost_equal, emag1.in_cgs(), emag2.in_cgs()
