6d4fb0a6691559b4bc2bcd640e25b1fcba3f19b6
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-12-17 01:01:39 -0600

    Sandbox test_varia
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/tests/test_varia.py
+++ yt/visualization/volume_rendering/tests/test_varia.py
@@ -10,45 +10,69 @@ Miscellaneous tests for VR infrastructure
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+import os
+import tempfile
+import shutil
 import numpy as np
 
 import yt
 from yt.testing import \
     fake_random_ds
+from unittest import TestCase
 
-def test_simple_scene_creation():
-    ds = fake_random_ds(32)
-    yt.create_scene(ds)
+def setup():
+    """Test specific setup."""
+    from yt.config import ytcfg
+    ytcfg["yt", "__withintesting"] = "True"
 
-def test_modify_transfer_function():
-    ds = fake_random_ds(32)
-    im, sc = yt.volume_render(ds)
+class VariousVRTests(TestCase):
+    # This toggles using a temporary directory. Turn off to examine images.
+    use_tmpdir = True
+    
+    def setUp(self):
+        if self.use_tmpdir:
+            self.curdir = os.getcwd()
+            # Perform I/O in safe place instead of yt main dir
+            self.tmpdir = tempfile.mkdtemp()
+            os.chdir(self.tmpdir)
+        else:
+            self.curdir, self.tmpdir = None, None
 
-    volume_source = sc.get_source(0)
-    tf = volume_source.transfer_function
-    tf.clear()
-    tf.grey_opacity = True
-    tf.add_layers(3, colormap='RdBu')
-    sc.render()
+        self.ds = fake_random_ds(32)
+    
+    def tearDown(self):
+        if self.use_tmpdir:
+            os.chdir(self.curdir)
+            shutil.rmtree(self.tmpdir)
 
-def test_multiple_fields():
-    ds = fake_random_ds(32)
-    im, sc = yt.volume_render(ds)
+    def test_simple_scene_creation(self):
+        yt.create_scene(self.ds)
 
-    volume_source = sc.get_source(0)
-    volume_source.set_fields([('gas', 'velocity_x'), ('gas', 'density')])
-    sc.render()
+    def test_modify_transfer_function(self):
+        im, sc = yt.volume_render(self.ds)
 
-def test_rotation_volume_rendering():
-    ds = fake_random_ds(32)
-    im, sc = yt.volume_render(ds)
-
-    angle = 2*np.pi
-    frames = 10
-    for i in range(frames):
-        sc.camera.yaw(angle/frames)
+        volume_source = sc.get_source(0)
+        tf = volume_source.transfer_function
+        tf.clear()
+        tf.grey_opacity = True
+        tf.add_layers(3, colormap='RdBu')
         sc.render()
 
-def test_simple_volume_rendering():
-    ds = fake_random_ds(32)
-    im, sc = yt.volume_render(ds, sigma_clip=4.0)
+    def test_multiple_fields(self):
+        im, sc = yt.volume_render(self.ds)
+
+        volume_source = sc.get_source(0)
+        volume_source.set_fields([('gas', 'velocity_x'), ('gas', 'density')])
+        sc.render()
+
+    def test_rotation_volume_rendering(self):
+        im, sc = yt.volume_render(self.ds)
+
+        angle = 2 * np.pi
+        frames = 10
+        for i in range(frames):
+            sc.camera.yaw(angle / frames)
+            sc.render()
+
+    def test_simple_volume_rendering(self):
+        im, sc = yt.volume_render(self.ds, sigma_clip=4.0)
