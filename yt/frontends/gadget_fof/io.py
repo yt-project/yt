6ff355ce48219f1add94d62be23a75d9f98d6961
Britton Smith <brittonsmith@gmail.com>
2015-12-23 08:54:59 -0600

    Import cleanup.
    --HG--
    branch : yt

--- yt/frontends/gadget_fof/io.py
+++ yt/frontends/gadget_fof/io.py
@@ -15,17 +15,16 @@ GadgetFOF data-file handling function
 #-----------------------------------------------------------------------------
 
 from collections import defaultdict
-
 from yt.utilities.on_demand_imports import _h5py as h5py
 import numpy as np
 
-from yt.utilities.exceptions import YTDomainOverflow
 from yt.funcs import mylog
-
+from yt.utilities.exceptions import \
+    YTDomainOverflow
 from yt.utilities.io_handler import \
     BaseIOHandler
-
-from yt.utilities.lib.geometry_utils import compute_morton
+from yt.utilities.lib.geometry_utils import \
+    compute_morton
 
 class IOHandlerGadgetFOFHDF5(BaseIOHandler):
     _dataset_type = "gadget_fof_hdf5"
