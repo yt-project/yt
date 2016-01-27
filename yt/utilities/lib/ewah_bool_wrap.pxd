35af648932b06aaa4b480b156da0f079c4d4139e
Matthew Turk <matthewturk@gmail.com>
2016-01-27 15:57:28 -0600

    Add a keys map to BoolArrayCollection.
    --HG--
    branch : yt

--- yt/utilities/lib/ewah_bool_wrap.pxd
+++ yt/utilities/lib/ewah_bool_wrap.pxd
@@ -2,6 +2,7 @@ cimport numpy as np
 
 cdef class BoolArrayCollection:
     cdef void* ewah_coll
+    cdef void* ewah_keys
 
     cdef void _set(self, np.uint64_t i1, np.uint64_t i2)
     cdef bint _get(self, np.uint64_t i1, np.uint64_t i2)
--- yt/utilities/lib/ewah_bool_wrap.pyx
+++ yt/utilities/lib/ewah_bool_wrap.pyx
@@ -20,11 +20,15 @@ from yt.utilities.lib.ewah_bool_array cimport \
 cdef class BoolArrayCollection:
 
     def __cinit__(self):
+        cdef ewah_bool_array *ewah_keys = new ewah_bool_array()
         cdef ewah_map *ewah_coll = new ewah_map()
+        self.ewah_keys = <void *> ewah_keys
         self.ewah_coll = <void *> ewah_coll
 
     cdef void _set(self, np.uint64_t i1, np.uint64_t i2):
+        cdef ewah_bool_array *ewah_keys = <ewah_bool_array *> self.ewah_keys
         cdef ewah_map *ewah_coll = <ewah_map *> self.ewah_coll
+        ewah_keys[0].set(i1)
         # Note the 0 here, for dereferencing
         ewah_coll[0][i1].set(i2)
 
@@ -32,13 +36,17 @@ cdef class BoolArrayCollection:
         self._set(i1, i2)
 
     cdef bint _get(self, np.uint64_t i1, np.uint64_t i2):
+        cdef ewah_bool_array *ewah_keys = <ewah_bool_array *> self.ewah_keys
         cdef ewah_map *ewah_coll = <ewah_map *> self.ewah_coll
         # Note the 0 here, for dereferencing
+        if not ewah_keys[0].get(i1): return 0
         return ewah_coll[0][i1].get(i2)
 
     def get(self, i1, i2):
         return self._get(i1, i2)
 
     def __dealloc__(self):
+        cdef ewah_bool_array *ewah_keys = <ewah_bool_array *> self.ewah_keys
         cdef ewah_map *ewah_coll = <ewah_map *> self.ewah_coll
+        del ewah_keys
         del ewah_coll
