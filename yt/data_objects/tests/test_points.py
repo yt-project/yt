053548db629a5e878fe8383bd2fdff0552119567
Nathan Goldbaum <ngoldbau@ucsc.edu>
2016-01-28 09:45:47 -0600

    Backporting PR #1950 https://bitbucket.org/yt_analysis/yt/pull-requests/1950
    --HG--
    branch : stable

--- yt/data_objects/tests/test_points.py
+++ yt/data_objects/tests/test_points.py
@@ -6,5 +6,55 @@ def setup():
     ytcfg["yt","__withintesting"] = "True"
 
 def test_domain_point():
-    ds = fake_random_ds(16, fields = ("density"))
+    ds = fake_random_ds(16, particles=nparticles)
     p = ds.point(ds.domain_center)
+
+    # ensure accessing one field works, store for comparison later
+    point_den = p['density']
+    point_vel = p['velocity_x']
+
+    ad = ds.all_data()
+    ppos = ad['all', 'particle_position']
+
+    fpoint_den = ds.find_field_values_at_point('density', ds.domain_center)
+
+    fpoint_den_vel = ds.find_field_values_at_point(
+        ['density', 'velocity_x'], ds.domain_center)
+
+    assert_equal(point_den, fpoint_den)
+    assert_equal(point_den, fpoint_den_vel[0])
+    assert_equal(point_vel, fpoint_den_vel[1])
+
+    ppos_den = ds.find_field_values_at_points('density', ppos)
+    ppos_vel = ds.find_field_values_at_points('velocity_x', ppos)
+    ppos_den_vel = ds.find_field_values_at_points(
+        ['density', 'velocity_x'], ppos)
+
+    assert_equal(ppos_den.shape, (nparticles,))
+    assert_equal(ppos_vel.shape, (nparticles,))
+    assert_equal(len(ppos_den_vel), 2)
+    assert_equal(ppos_den_vel[0], ppos_den)
+    assert_equal(ppos_den_vel[1], ppos_vel)
+
+g30 = "IsolatedGalaxy/galaxy0030/galaxy0030"
+
+@requires_file(g30)
+def test_fast_find_field_values_at_points():
+    ds = yt.load(g30)
+    ad = ds.all_data()
+    # right now this is slow for large numbers of particles, so randomly
+    # sample 100 particles
+    nparticles = 100
+    ppos = ad['all', 'particle_position']
+    ppos = ppos[np.random.random_integers(0, len(ppos), size=nparticles)]
+
+    ppos_den = ds.find_field_values_at_points('density', ppos)
+    ppos_vel = ds.find_field_values_at_points('velocity_x', ppos)
+    ppos_den_vel = ds.find_field_values_at_points(
+        ['density', 'velocity_x'], ppos)
+
+    assert_equal(ppos_den.shape, (nparticles,))
+    assert_equal(ppos_vel.shape, (nparticles,))
+    assert_equal(len(ppos_den_vel), 2)
+    assert_equal(ppos_den_vel[0], ppos_den)
+    assert_equal(ppos_den_vel[1], ppos_vel)
