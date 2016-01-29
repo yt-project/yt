097e511d13b85240c3b8460495530cfd7761b80c
John ZuHone <jzuhone@gmail.com>
2016-01-29 11:54:19 -0500

    Test another dataset here
    --HG--
    branch : yt

--- yt/units/tests/test_unit_systems.py
+++ yt/units/tests/test_unit_systems.py
@@ -29,37 +29,38 @@ def test_unit_systems():
     assert goofy_unit_system["magnetic_field_mks"] == Unit("lbm/(hr**2*mA)")
     assert "goofy" in unit_system_registry
 
+test_units = {}
+test_units["mks"] = {"density": "kg/m**3",
+                     "kinetic_energy": "Pa",
+                     "velocity_magnitude": "m/s",
+                     "velocity_divergence": "1/s",
+                     "density_gradient_x": "kg/m**4"}
+test_units["imperial"] = {"density": "lbm/ft**3",
+                          "kinetic_energy": "lbf/ft**2",
+                          "velocity_magnitude": "ft/s",
+                          "velocity_divergence": "1/s",
+                          "density_gradient_x": "lbm/ft**4"}
+test_units["galactic"] = {"density": "Msun/kpc**3",
+                          "kinetic_energy": "Msun/(Myr**2*kpc)",
+                          "velocity_magnitude": "kpc/Myr",
+                          "velocity_divergence": "1/Myr",
+                          "density_gradient_x": "Msun/kpc**4"}
+test_units["code"] = {"density": "code_mass/code_length**3",
+                      "kinetic_energy": "code_mass/(code_length*code_time**2)",
+                      "velocity_magnitude": "code_velocity",
+                      "velocity_divergence": "code_velocity/code_length",
+                      "density_gradient_x": "code_mass/code_length**4"}
+
+test_fields = ["density",
+               "kinetic_energy",
+               "velocity_divergence",
+               "density_gradient_x",
+               "velocity_magnitude"]
+
 gslr = "GasSloshingLowRes/sloshing_low_res_hdf5_plt_cnt_0300"
 @requires_file(gslr)
-def test_fields_diff_systems():
+def test_fields_diff_systems_sloshing():
     ytcfg["yt","skip_dataset_cache"] = "True"
-    test_fields = ["density",
-                   "kinetic_energy",
-                   "velocity_divergence",
-                   "density_gradient_x",
-                   "velocity_magnitude"]
-
-    test_units = {}
-    test_units["mks"] = {"density": "kg/m**3",
-                         "kinetic_energy": "Pa",
-                         "velocity_magnitude": "m/s",
-                         "velocity_divergence": "1/s",
-                         "density_gradient_x": "kg/m**4"}
-    test_units["imperial"] = {"density": "lbm/ft**3",
-                              "kinetic_energy": "lbf/ft**2",
-                              "velocity_magnitude": "ft/s",
-                              "velocity_divergence": "1/s",
-                              "density_gradient_x": "lbm/ft**4"}
-    test_units["galactic"] = {"density": "Msun/kpc**3",
-                              "kinetic_energy": "Msun/(Myr**2*kpc)",
-                              "velocity_magnitude": "kpc/Myr",
-                              "velocity_divergence": "1/Myr",
-                              "density_gradient_x": "Msun/kpc**4"}
-    test_units["code"] = {"density": "code_mass/code_length**3",
-                          "kinetic_energy": "code_mass/(code_length*code_time**2)",
-                          "velocity_magnitude": "code_velocity",
-                          "velocity_divergence": "code_velocity/code_length",
-                          "density_gradient_x": "code_mass/code_length**4"}
 
     ds_cgs = load(gslr)
     dd_cgs = ds_cgs.sphere("c", (100., "kpc"))
@@ -71,6 +72,26 @@ def test_fields_diff_systems():
             if us == "code":
                 # For this dataset code units are cgs
                 v1 = dd_cgs[field]
+            else:
+                v1 = dd_cgs[field].in_base(us)
+            v2 = dd[field]
+            assert_almost_equal(v1.v, v2.v)
+            assert str(v2.units) == test_units[us][field]
+
+etc = "enzo_tiny_cosmology/DD0046/DD0046"
+@requires_file(etc)
+def test_fields_diff_systems_etc():
+    ytcfg["yt","skip_dataset_cache"] = "True"
+
+    ds_cgs = load(etc)
+    dd_cgs = ds_cgs.sphere("max", (500., "kpc"))
+
+    for us in test_units:
+        ds = load(etc, unit_system=us)
+        dd = ds.sphere("max", (500.,"kpc"))
+        for field in test_fields:
+            if us == "code":
+                v1 = dd_cgs[field].in_units(test_units["code"][field])
             else:
                 v1 = dd_cgs[field].in_base(us)
             v2 = dd[field]
