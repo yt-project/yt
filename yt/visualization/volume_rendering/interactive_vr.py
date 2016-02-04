87da4ee434b0495bc2bc00ff1fdbcf7e4910ecb4
Andrew Myers <atmyers2@gmail.com>
2016-02-03 21:50:43 -0800

    invert y axis
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/interactive_vr.py
+++ yt/visualization/volume_rendering/interactive_vr.py
@@ -76,7 +76,7 @@ class TrackballCamera(object):
             z = 0.0
         else:
             z = np.sqrt(1.0 - mag**2)
-        return np.array([x, y, z])
+        return np.array([x, -y, z])
 
     def update_orientation(self, start_x, start_y, end_x, end_y):
         old = self._map_to_surface(start_x, start_y)
