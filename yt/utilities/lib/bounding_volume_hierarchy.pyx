a1cb13f8e8ae6df162a39614ac28de89c846ac0f
Andrew Myers <atmyers2@gmail.com>
2016-02-05 16:08:50 -0800

    adding ray / bounding box intersection check
    --HG--
    branch : yt

--- yt/utilities/lib/bounding_volume_hierarchy.pyx
+++ yt/utilities/lib/bounding_volume_hierarchy.pyx
@@ -13,10 +13,11 @@ cdef extern from "mesh_construction.h":
 cdef struct Ray:
     np.float64_t origin[3]
     np.float64_t direction[3]
+    np.float64_t inv_dir[3]
     np.float64_t data_val
     np.float64_t t_near
     np.float64_t t_far
-    np.int64_t  elem_id
+    np.int64_t elem_id
 
 # axis-aligned bounding box
 cdef struct BBox:
@@ -33,7 +34,9 @@ cdef struct BVHNode:
     
 # triangle data structure
 cdef struct Triangle:
-    np.int64_t v0, v1, v2
+    np.float64_t p0[3]
+    np.float64_t p1[3]
+    np.float64_t p2[3]
     np.int64_t elem_id
     np.float64_t centroid[3]
     BBox bbox
@@ -62,6 +65,154 @@ cdef void cross(const np.float64_t* a,
     c[2] = a[0]*b[1] - a[1]*b[0]
 
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cdef np.int64_t ray_triangle_intersect(Ray* ray, const Triangle* tri):
+    # guts of this are cribbed from "3D Math Primer for Graphics 
+    # and Game Development", section A.16
+
+    # edge vectors
+    cdef np.float64_t e1[3]
+    cdef np.float64_t e2[3]
+    cdef int i
+    for i in range(3):
+        e1[i] = tri.p1[i] - tri.p0[i];
+        e2[i] = tri.p2[i] - tri.p1[i];
+        
+    # normal vector
+    cdef np.float64_t N[3]
+    cross(e1, e1, N);
+
+    cdef np.float64_t dotprod = dot(N, ray.direction)
+    if not dotprod < 0.0:
+        # ray is travelling the wrong direction
+        return False
+
+    # distance along ray
+    cdef np.float64_t d = dot(N, tri.p0)
+    cdef np.float64_t t = d - dot(N, ray.origin) 
+
+    if not t <= 0.0:
+        # ray origin is behind triangle
+        return False
+
+    t /= dotprod
+
+    if not t >= ray.t_far:
+        # closer intersection already found
+        return False
+
+#    assert(t >= 0.0)
+#    assert(t <= ray.t_far)
+    
+    # 3D point of intersection
+    cdef np.float64_t p[3]
+    for i in range(3):
+        p[i] = ray.origin[i] + ray.direction[i]*t
+        
+    cdef np.float64_t u0, u1, u2, v0, v1, v2
+        
+    if fabs(N[0]) > fabs(N[1]):
+        if fabs(N[0]) > fabs(N[2]):
+            u0 = p[1]      - tri.p0[1]
+            u1 = tri.p1[1] - tri.p0[1]
+            u2 = tri.p2[1] - tri.p0[1]
+
+            v0 = p[2]      - tri.p0[2]
+            v1 = tri.p1[2] - tri.p0[2]
+            v2 = tri.p2[2] - tri.p0[2]
+    
+        else:
+            u0 = p[0]      - tri.p0[0]
+            u1 = tri.p1[0] - tri.p0[0]
+            u2 = tri.p2[0] - tri.p0[0]
+            
+            v0 = p[1]      - tri.p0[1]
+            v1 = tri.p1[1] - tri.p0[1]
+            v2 = tri.p2[1] - tri.p0[1]
+    else:
+        if fabs(N[1]) > fabs(N[2]):
+            u0 = p[0]      - tri.p0[0]
+            u1 = tri.p1[0] - tri.p0[0]
+            u2 = tri.p2[0] - tri.p0[0]
+
+            v0 = p[2]      - tri.p0[2]
+            v1 = tri.p1[2] - tri.p0[2]
+            v2 = tri.p2[2] - tri.p0[2]
+        else:
+            u0 = p[0]      - tri.p0[0]
+            u1 = tri.p1[0] - tri.p0[0]
+            u2 = tri.p2[0] - tri.p0[0]
+
+            v0 = p[1]      - tri.p0[1]
+            v1 = tri.p1[1] - tri.p0[1]
+            v2 = tri.p2[1] - tri.p0[1]
+
+    cdef np.float64_t temp = u1*v2 - u2*v1
+
+    if not (temp is not  0.0):
+        # denominator is invalid
+        return False
+
+    temp = 1.0/temp
+
+    cdef np.float64_t a = (u0*v2 - u2*v0)*temp
+    if not (a >= 0.0):
+        # barycentric coord out of range
+        return False
+
+    cdef np.float64_t b = (u1*v0 - u0*v1)*temp
+    if not (b >= 0.0):
+        # barycentric coord out of range
+        return False
+
+    cdef np.float64_t c = 1.0 - a - b
+    if not (c >= 0.0):
+        # barycentric coord out of range
+        return False
+
+    # we have a hit, update ray
+    ray.t_far = t
+    ray.elem_id = tri.elem_id
+
+    return True
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cdef np.int64_t ray_bbox_intersect(Ray* ray, const BBox* bbox):
+
+    cdef np.float64_t tx1 = (bbox.left_edge[0]  - 
+                             ray.origin[0])*ray.inv_dir[0]
+    cdef np.float64_t tx2 = (bbox.right_edge[0] -
+                             ray.origin[0])*ray.inv_dir[0]
+ 
+    cdef np.float64_t tmin = fmin(tx1, tx2)
+    cdef np.float64_t tmax = fmax(tx1, tx2)
+
+    cdef np.float64_t ty1 = (bbox.left_edge[1]  -
+                             ray.origin[1])*ray.inv_dir[1]
+    cdef np.float64_t ty2 = (bbox.right_edge[1] -
+                             ray.origin[1])*ray.inv_dir[1]
+ 
+    tmin = fmax(tmin, fmin(ty1, ty2))
+    tmax = fmin(tmax, fmax(ty1, ty2))
+
+    cdef np.float64_t tz1 = (bbox.left_edge[2]  -
+                             ray.origin[2])*ray.inv_dir[2]
+    cdef np.float64_t tz2 = (bbox.right_edge[2] -
+                             ray.origin[2])*ray.inv_dir[2]
+ 
+    tmin = fmax(tmin, fmin(tz1, tz2))
+    tmax = fmin(tmax, fmax(tz1, tz2))
+
+    if not (tmax > 0):
+        return False
+ 
+    return tmax >= tmin;
+
 cdef class BVH:
     cdef BVHNode* root
     cdef Triangle* triangles
@@ -81,9 +232,7 @@ cdef class BVH:
         
         cdef np.int64_t i, j, k
         cdef np.int64_t offset, tri_index
-        cdef np.float64_t[:] p0
-        cdef np.float64_t[:] p1
-        cdef np.float64_t[:] p2
+        cdef np.int64_t v0, v1, v2
         cdef Triangle* tri
         for i in range(num_elem):
             offset = 12*i
@@ -91,16 +240,16 @@ cdef class BVH:
                 tri_index = offset + j
                 tri = &(self.triangles[tri_index])
                 tri.elem_id = i
-                tri.v0 = indices[i][triangulate_hex[j][0]]
-                tri.v1 = indices[i][triangulate_hex[j][1]]
-                tri.v2 = indices[i][triangulate_hex[j][2]]                
-                p0 = vertices[tri.v0]
-                p1 = vertices[tri.v1]
-                p2 = vertices[tri.v2]
+                v0 = indices[i][triangulate_hex[j][0]]
+                v1 = indices[i][triangulate_hex[j][1]]
+                v2 = indices[i][triangulate_hex[j][2]]
                 for k in range(3):
-                    tri.centroid[k] = (p0[k] + p1[k] + p2[k]) / 3.0
-                    tri.bbox.left_edge[k] = fmin(fmin(p0[k], p1[k]), p2[k])
-                    tri.bbox.right_edge[k] = fmax(fmax(p0[k], p1[k]), p2[k])
+                    tri.p0[k] = vertices[v0][k]
+                    tri.p1[k] = vertices[v1][k]
+                    tri.p2[k] = vertices[v2][k]
+                    tri.centroid[k] = (tri.p0[k] + tri.p1[k] + tri.p2[k]) / 3.0
+                    tri.bbox.left_edge[k] = fmin(fmin(tri.p0[k], tri.p1[k]), tri.p2[k])
+                    tri.bbox.right_edge[k] = fmax(fmax(tri.p0[k], tri.p1[k]), tri.p2[k])
                     
         self.root = self.build(0, num_tri)
 
@@ -134,123 +283,6 @@ cdef class BVH:
                 box.right_edge[j] = fmin(box.right_edge[j], 
                                          self.triangles[i].bbox.right_edge[j])
         node.bbox = box       
-
-    @cython.boundscheck(False)
-    @cython.wraparound(False)
-    @cython.cdivision(True)
-    cdef np.int64_t _ray_triangle_intersect(self, Ray* ray, np.int64_t tri_index):
-        # cribbed from 3D Math Primer for Graphics and Game Development, section A.16
-        cdef np.float64_t p0[3]
-        cdef np.float64_t p1[3]
-        cdef np.float64_t p2[3]
-        cdef np.float64_t e1[3]
-        cdef np.float64_t e2[3]
-
-        cdef Triangle* tri
-        tri = &(self.triangles[tri_index])
-        
-        cdef int i
-        for i in range(3):
-            p0[i] = self.vertices[tri.v0][i]
-            p1[i] = self.vertices[tri.v1][i]
-            p2[i] = self.vertices[tri.v2][i]
-            e1[i] = p1[i] - p0[i];
-            e2[i] = p2[i] - p1[i];
-        
-        cdef np.float64_t N[3]
-        cross(e1, e1, N);
-        cdef np.float64_t dotprod = dot(N, ray.direction)
-
-        if not dotprod < 0.0:
-            # ray is travelling the wrong direction
-            return False
-
-        cdef np.float64_t d = dot(N, p0)
-        cdef np.float64_t t = d - dot(N, ray.origin) 
-
-        if not t <= 0.0:
-            # ray origin is behind triangle
-            return False
-
-        t /= dotprod
-
-        if not t >= ray.t_far:
-            # closer intersection already found
-            return False
-
-        assert(t >= 0.0)
-        assert(t <= ray.t_far)
-
-        cdef np.float64_t p[3]
-        for i in range(3):
-            p[i] = ray.origin[i] + ray.direction[i]*t
-        
-        cdef np.float64_t u0, u1, u2, v0, v1, v2
-        
-        if fabs(N[0]) > fabs(N[1]):
-            if fabs(N[0]) > fabs(N[2]):
-                u0 = p[1]  - p0[1]
-                u1 = p1[1] - p0[1]
-                u2 = p2[1] - p0[1]
-
-                v0 = p[2]  - p0[2]
-                v1 = p1[2] - p0[2]
-                v2 = p2[2] - p0[2]
-    
-            else:
-                u0 = p[0]  - p0[0]
-                u1 = p1[0] - p0[0]
-                u2 = p2[0] - p0[0]
-
-                v0 = p[1]  - p0[1]
-                v1 = p1[1] - p0[1]
-                v2 = p2[1] - p0[1]
-        else:
-            if fabs(N[1]) > fabs(N[2]):
-                u0 = p[0]  - p0[0]
-                u1 = p1[0] - p0[0]
-                u2 = p2[0] - p0[0]
-
-                v0 = p[2]  - p0[2]
-                v1 = p1[2] - p0[2]
-                v2 = p2[2] - p0[2]
-            else:
-                u0 = p[0]  - p0[0]
-                u1 = p1[0] - p0[0]
-                u2 = p2[0] - p0[0]
-
-                v0 = p[1]  - p0[1]
-                v1 = p1[1] - p0[1]
-                v2 = p2[1] - p0[1]
-
-        cdef np.float64_t temp = u1*v2 - u2*v1
-
-        if not (temp is not  0.0):
-            # denominator is invalid
-            return False
-
-        temp = 1.0/temp
-
-        cdef np.float64_t a = (u0*v2 - u2*v0)*temp
-        if not (a >= 0.0):
-            # barycentric coord out of range
-            return False
-
-        cdef np.float64_t b = (u1*v0 - u0*v1)*temp
-        if not (b >= 0.0):
-            # barycentric coord out of range
-            return False
-
-        cdef np.float64_t c = 1.0 - a - b
-        if not (c >= 0.0):
-            # barycentric coord out of range
-            return False
-
-        # we have a hit, update ray
-        ray.t_far = t
-        ray.elem_id = tri.elem_id
-
-        return True
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
