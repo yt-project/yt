e1134d013e3869f148959a32a9392ee690c1d01a
Matthew Turk <matthewturk@gmail.com>
2016-02-24 21:10:11 -0600

    Adding a first pass at a set of shader objects
    --HG--
    branch : yt

---
+++ yt/visualization/volume_rendering/shader_objects.py
@@ -0,0 +1,73 @@
+import OpenGL as GL
+
+class ShaderProgram(object):
+    def __init__(self, vertex_shader = None, fragment_shader = None):
+        # Don't allow just one.  Either neither or both.
+        if vertex_shader is None and fragment_shader is None:
+            pass
+        elif None not in (vertex_shader, fragment_shader):
+            self.link(vertex_shader, fragment_shader)
+        else:
+            raise RuntimeError
+
+    def link(self, vertex_shader, fragment_shader):
+        # There are more types of shaders, but for now we only allow v&f.
+        self.program = GL.glCreateProgram()
+        if not isinstance(vertex_shader, VertexShader):
+            vertex_shader = VertexShader(vertex_shader)
+        if not isinstance(fragment_shader, FragmentShader):
+            fragment_shader = FragmentShader(fragment_shader)
+        self.vertex_shader = vertex_shader
+        self.fragment_shader = fragment_shader
+        GL.glAttachShader(self.program, vertex_shader)
+        GL.glAttachShader(self.program, fragment_shader)
+        GL.glLinkProgram(program)
+        result = GL.glGetProgramiv(self.program, GL.GL_LINK_STATUS)
+        if not result:
+            raise RuntimeError(GL.glGetProgramInfoLog(self.program))
+
+class Shader(object):
+    shader = None
+    def __init__(self, source = None):
+        if source:
+            self.compile(source)
+
+    def _get_source(self, source):
+        if ";" in source:
+            # This is probably safe, right?  Enh, probably.
+            return source
+        if os.path.isfile(source):
+            sh_directory = ''
+        else:
+            sh_directory = os.path.join(os.path.dirname(__file__), "shaders")
+        fn = os.path.join(sh_directory, source)
+        if not os.path.isfile(fn):
+            raise YTInvalidShaderType(source)
+        return open(fn, 'r').read()
+
+    def compile(self, source, parameters = None):
+        if parameters is not None:
+            raise NotImplementedError
+        source = self._get_source(source)
+        shader_type_enum = getattr(GL,
+            'GL_%s_SHADER' % self.shader_type.upper())
+        shader = GL.glCreateShader(shader_type_enum)
+        # We could do templating here if we wanted.
+        self.shader_source = source
+        GL.glShaderSource(shader, source)
+        GL.glCompileShader(shader)
+        result = GL.glGetShaderiv(shader, GL.GL_COMPILE_STATUS)
+        if not(result):
+            raise RuntimeError(GL.glGetShaderInfoLog(shader))
+        self.shader = shader
+
+    def __del__(self):
+        # This is not guaranteed to be called
+        if self.shader is not None:
+            GL.glDeleteShader(self.shader)
+
+class FragmentShader(Shader):
+    shader_type = "fragment"
+
+class VertexShader(Shader):
+    shader_type = "vertex"
