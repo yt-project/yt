5e4877580926c5c71210684727a559616df47fc2
Andrew Myers <atmyers2@gmail.com>
2016-02-26 19:56:30 -0800

    adding some mesh rendering answer tests
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/tests/test_mesh_render.py
+++ yt/visualization/volume_rendering/tests/test_mesh_render.py
@@ -11,11 +11,18 @@ Test Surface Mesh Rendering
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-from yt.testing import fake_tetrahedral_ds
-from yt.testing import fake_hexahedral_ds
-from yt.testing import requires_module
-from yt.visualization.volume_rendering.render_source import MeshSource
-from yt.visualization.volume_rendering.camera import Camera
+from yt.testing import \
+    fake_tetrahedral_ds, \
+    fake_hexahedral_ds, \
+    requires_module
+from yt.utilities.answer_testing.framework import \
+    requires_ds, \
+    data_dir_load, \
+    GenericImageTest
+from yt.visualization.volume_rendering.api import \
+    MeshSource, \
+    Camera, \
+    create_scene
 
 
 @requires_module("pyembree")
@@ -38,3 +45,60 @@ def test_surface_mesh_render():
         images.append(im)
 
     return images
+
+
+hex8 = "MOOSE_sample_data/out.e-s010"
+hex8_fields = [('connect1', 'diffused'), ('connect2', 'convected')]
+
+@requires_ds(hex8)
+@requires_module("pyembree")
+def test_hex8_render():
+    for field in hex8_fields:
+        ds = data_dir_load(hex8, kwargs={'step':-1})
+        sc = create_scene(ds, field)
+        im = sc.render()
+
+        def mesh_render_image_func(filename_prefix):
+            return im.write_image(filename_prefix)
+
+        test = GenericImageTest(ds, mesh_render_image_func, 12)
+        test.prefix = "render_answers_hex8_%s_%s" % field
+        yield test
+
+
+tet4 = "MOOSE_sample_data/high_order_elems_tet4_refine_out.e"
+tet4_fields = [("connect1", "u")]
+
+@requires_ds(tet4)
+@requires_module("pyembree")
+def test_tet4_render():
+    for field in tet4_fields:
+        ds = data_dir_load(tet4, kwargs={'step':-1})
+        sc = create_scene(ds, field)
+        im = sc.render()
+
+        def mesh_render_image_func(filename_prefix):
+            return im.write_image(filename_prefix)
+
+        test = GenericImageTest(ds, mesh_render_image_func, 12)
+        test.prefix = "render_answers_tet4_%s_%s" % field
+        yield test
+
+
+hex20 = "MOOSE_sample_data/mps_out.e"
+hex20_fields = [('connect2', 'temp')]
+
+@requires_ds(hex20)
+@requires_module("pyembree")
+def test_hex20_render():
+    for field in hex20_fields:
+        ds = data_dir_load(hex20, kwargs={'step':-1})
+        sc = create_scene(ds, field)
+        im = sc.render()
+
+        def mesh_render_image_func(filename_prefix):
+            return im.write_image(filename_prefix)
+
+        test = GenericImageTest(ds, mesh_render_image_func, 12)
+        test.prefix = "render_answers_hex20_%s_%s" % field
+        yield test
