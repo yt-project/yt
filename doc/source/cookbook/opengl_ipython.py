38f1ba8d4c7ca8d34ac28b7e4b7073cfffc8f891
Matthew Turk <matthewturk@gmail.com>
2016-03-01 11:58:41 -0600

    Fixing cookbook.
    --HG--
    branch : yt

--- doc/source/cookbook/opengl_ipython.py
+++ doc/source/cookbook/opengl_ipython.py
@@ -10,18 +10,9 @@ rc = RenderingContext(1280, 960)
 scene = SceneGraph()
 collection = BlockCollection()
 
-N = 64
-x, y, z = np.mgrid[0:1:N*1j, 0:1:N*1j, 0:1:N*1j]
-c = [-0.05, -0.05, -0.05]
-oor2 = ((x-c[0])**2 + (y-c[1])**2 + (z-c[2])**2)**-0.5
-np.clip(oor2, 1e-6, 1e60, oor2)
-data = {'x_field': 10**x, 'y_field': 10**y, 'z_field': 10**z, 'sphere':oor2}
-
-ds = yt.load_uniform_grid(data, [N, N, N], 1.0, nprocs=4)
 ds = yt.load("IsolatedGalaxy/galaxy0030/galaxy0030")
 
 dd = ds.all_data()
-#collection.add_data(dd, "sphere")
 collection.add_data(dd, "density")
 
 scene.add_collection(collection)
