b7f74fe1aeffd61933d526455815e926b3adeed6
Kacper Kowalik <xarthisius.kk@gmail.com>
2016-03-10 12:34:50 -0600

    Restore cookbook recipe with comments
    --HG--
    branch : yt

--- doc/source/cookbook/opengl_vr.py
+++ doc/source/cookbook/opengl_vr.py
@@ -1,4 +1,28 @@
 import yt
+import numpy as np
+from yt.visualization.volume_rendering.interactive_vr import \
+    SceneGraph, BlockCollection, TrackballCamera
+from yt.visualization.volume_rendering.interactive_loop import \
+    RenderingContext
 
 ds = yt.load("IsolatedGalaxy/galaxy0030/galaxy0030")
-yt.interactive_render(ds)
+
+# Create GLUT window
+rc = RenderingContext(1280, 960)
+
+# Create a 3d Texture from all_data()
+collection = BlockCollection()
+dd = ds.all_data()
+collection.add_data(dd, "density")
+
+# Initiliaze basic Scene and pass the data
+scene = SceneGraph()
+scene.add_collection(collection)
+
+# Create default camera
+position = (1.0, 1.0, 1.0)
+c = TrackballCamera(position=position, focus=ds.domain_center,
+                    near_plane=0.1)
+
+# Start rendering loop
+rc.start_loop(scene, c)
