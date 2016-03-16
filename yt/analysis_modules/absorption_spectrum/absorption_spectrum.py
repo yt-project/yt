afefe2607d7d91f6cd06d438506928ab51154815
Cameron Hummels <chummels@gmail.com>
2016-03-16 11:49:03 -0700

    Shifting absorption spectrum method order to retain logical grouping.
    --HG--
    branch : yt

--- yt/analysis_modules/absorption_spectrum/absorption_spectrum.py
+++ yt/analysis_modules/absorption_spectrum/absorption_spectrum.py
@@ -234,6 +234,42 @@ class AbsorptionSpectrum(object):
         del field_data
         return (self.lambda_field, self.flux_field)
 
+    def _apply_observing_redshift(self, field_data, use_peculiar_velocity,
+                                 observing_redshift):
+        """
+        Change the redshifts of individual absorbers to account for the 
+        redshift at which the observer sits.
+
+        The intermediate redshift that is seen by an observer
+        at a redshift other than z=0 is z12, where z1 is the
+        observing redshift and z2 is the emitted photon's redshift
+        Hogg (2000) eq. 13:
+
+        1 + z12 = (1 + z2) / (1 + z1)
+        """
+        if observing_redshift == 0.:
+            # This is already assumed in the generation of the LightRay
+            redshift = field_data['redshift']
+            if use_peculiar_velocity:
+                redshift_eff = field_data['redshift_eff']
+        else:
+            # The intermediate redshift that is seen by an observer
+            # at a redshift other than z=0 is z12, where z1 is the
+            # observing redshift and z2 is the emitted photon's redshift
+            # Hogg (2000) eq. 13:
+            # 1 + z12 = (1 + z2) / (1 + z1)
+            redshift = ((1 + field_data['redshift']) / \
+                        (1 + observing_redshift)) - 1.
+            # Combining cosmological redshift and doppler redshift
+            # into an effective redshift is found in Peacock's
+            # Cosmological Physics eqn 3.75:
+            # 1 + z_eff = (1 + z_cosmo) * (1 + z_doppler)
+            if use_peculiar_velocity:
+                redshift_eff = ((1 + redshift) * \
+                                (1 + field_data['redshift_dopp'])) - 1.
+
+        return redshift, redshift_eff
+
     def _add_continua_to_spectrum(self, field_data, use_peculiar_velocity,
                                   observing_redshift=0.):
         """
@@ -275,42 +311,6 @@ class AbsorptionSpectrum(object):
                 self.tau_field[left_index[lixel]:right_index[lixel]] += line_tau
                 pbar.update(i)
             pbar.finish()
-
-    def _apply_observing_redshift(self, field_data, use_peculiar_velocity,
-                                 observing_redshift):
-        """
-        Change the redshifts of individual absorbers to account for the 
-        redshift at which the observer sits.
-
-        The intermediate redshift that is seen by an observer
-        at a redshift other than z=0 is z12, where z1 is the
-        observing redshift and z2 is the emitted photon's redshift
-        Hogg (2000) eq. 13:
-
-        1 + z12 = (1 + z2) / (1 + z1)
-        """
-        if observing_redshift == 0.:
-            # This is already assumed in the generation of the LightRay
-            redshift = field_data['redshift']
-            if use_peculiar_velocity:
-                redshift_eff = field_data['redshift_eff']
-        else:
-            # The intermediate redshift that is seen by an observer
-            # at a redshift other than z=0 is z12, where z1 is the
-            # observing redshift and z2 is the emitted photon's redshift
-            # Hogg (2000) eq. 13:
-            # 1 + z12 = (1 + z2) / (1 + z1)
-            redshift = ((1 + field_data['redshift']) / \
-                        (1 + observing_redshift)) - 1.
-            # Combining cosmological redshift and doppler redshift
-            # into an effective redshift is found in Peacock's
-            # Cosmological Physics eqn 3.75:
-            # 1 + z_eff = (1 + z_cosmo) * (1 + z_doppler)
-            if use_peculiar_velocity:
-                redshift_eff = ((1 + redshift) * \
-                                (1 + field_data['redshift_dopp'])) - 1.
-
-        return redshift, redshift_eff
 
     def _add_lines_to_spectrum(self, field_data, use_peculiar_velocity,
                                output_absorbers_file, subgrid_resolution=10,
