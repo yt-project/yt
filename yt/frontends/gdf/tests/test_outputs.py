ea47425ee40abea38199911d162ea63f1e3ff1f0
Miguel de Val-Borro <miguel@archlinux.net>
2016-03-19 23:14:34 -0400

    GDF answer tests
    --HG--
    branch : yt

---
+++ yt/frontends/gdf/tests/test_outputs.py
@@ -0,0 +1,40 @@
+"""
+GDF frontend tests
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2016, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from yt.testing import \
+    assert_equal, \
+    requires_file
+from yt.utilities.answer_testing.framework import \
+    requires_ds, \
+    small_patch_amr, \
+    data_dir_load
+from yt.frontends.gdf.api import GDFDataset
+
+_fields = ("density", "velocity_x")
+
+wt = "sedov/sedov_tst_0004.h5"
+
+@requires_ds(wt)
+def test_sedov_tunnel():
+    ds = data_dir_load(wt)
+    yield assert_equal, str(ds), "sedov_tst_0004"
+    for test in small_patch_amr(ds, _fields):
+        test_sedov_tunnel.__name__ = test.description
+        yield test
+
+
+@requires_file(wt)
+def test_GDFDataset():
+    assert isinstance(data_dir_load(wt), GDFDataset)
