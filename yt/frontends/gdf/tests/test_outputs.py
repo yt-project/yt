bb7c3682d450e0c611c4971025b04eee9f2d6e55
Miguel de Val-Borro <miguel@archlinux.net>
2016-03-22 11:53:38 -0400

    Add units_override test for GDF frontend
    --HG--
    branch : yt

--- yt/frontends/gdf/tests/test_outputs.py
+++ yt/frontends/gdf/tests/test_outputs.py
@@ -15,7 +15,8 @@ GDF frontend tests
 
 from yt.testing import \
     assert_equal, \
-    requires_file
+    requires_file, \
+    units_override_check
 from yt.utilities.answer_testing.framework import \
     requires_ds, \
     small_patch_amr, \
@@ -24,17 +25,23 @@ from yt.frontends.gdf.api import GDFDataset
 
 _fields = ("density", "velocity_x")
 
-wt = "sedov/sedov_tst_0004.h5"
+sedov = "sedov/sedov_tst_0004.h5"
 
-@requires_ds(wt)
+@requires_ds(sedov)
 def test_sedov_tunnel():
-    ds = data_dir_load(wt)
+    ds = data_dir_load(sedov)
     yield assert_equal, str(ds), "sedov_tst_0004"
     for test in small_patch_amr(ds, _fields):
         test_sedov_tunnel.__name__ = test.description
         yield test
 
 
-@requires_file(wt)
+@requires_file(sedov)
 def test_GDFDataset():
-    assert isinstance(data_dir_load(wt), GDFDataset)
+    assert isinstance(data_dir_load(sedov), GDFDataset)
+
+
+@requires_file(sedov)
+def test_units_override():
+    for test in units_override_check(sedov):
+        yield test
