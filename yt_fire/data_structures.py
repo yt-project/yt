8f7c542a48b77f1f4e01229881b3f4b97805a2d7
Britton Smith <brittonsmith@gmail.com>
2016-04-12 16:23:08 +0100

    Adding is_valid for FIRE.
    --HG--
    branch : yt

--- yt_fire/data_structures.py
+++ yt_fire/data_structures.py
@@ -14,6 +14,8 @@ Data structures for FIRE frontend.
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+from yt.utilities.on_demand_imports import _h5py as h5py
+
 from yt.data_objects.static_output import \
     ParticleFile
 from yt.frontends.gadget.data_structures import \
@@ -27,3 +29,21 @@ class FIREDataset(GadgetHDF5Dataset):
     _field_info_class = FIREFieldInfo
     _particle_mass_name = "Masses"
     _suffix = ".hdf5"
+
+    @classmethod
+    def _is_valid(self, *args, **kwargs):
+        need_groups = ['Header']
+        veto_groups = ['FOF', 'Group', 'Subhalo']
+        valid = True
+        try:
+            fh = h5py.File(args[0], mode='r')
+            valid = all(ng in fh["/"] for ng in need_groups) and \
+              not any(vg in fh["/"] for vg in veto_groups)
+            dmetal = "/PartType0/Metallicity"
+            if dmetal not in fh or fh[dmetal].shape[1] not in (11, 17):
+                valid = False
+            fh.close()
+        except:
+            valid = False
+            pass
+        return valid
