b5def055b351ff5f1807131ec38f07232123dae7
Kacper Kowalik <xarthisius.kk@gmail.com>
2016-04-18 15:18:17 -0500

    Revert import move
    --HG--
    branch : yt

--- yt/utilities/file_handler.py
+++ yt/utilities/file_handler.py
@@ -13,13 +13,13 @@ A wrapper class for h5py file objects.
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+from yt.utilities.on_demand_imports import _h5py as h5py
 from distutils.version import LooseVersion
 
 class HDF5FileHandler(object):
     handle = None
 
     def __init__(self, filename):
-        from yt.utilities.on_demand_imports import _h5py as h5py
         self.handle = h5py.File(filename, 'r')
 
     def __del__(self):
