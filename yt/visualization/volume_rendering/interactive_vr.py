43fa82651005a7d8f4e99fab24361e506e44ef9b
Andrew Myers <atmyers2@gmail.com>
2016-04-19 13:11:06 -0700

    more triangulation arrays
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/interactive_vr.py
+++ yt/visualization/volume_rendering/interactive_vr.py
@@ -88,6 +88,18 @@ triangulate_hex = np.array([
     [3, 6, 2], [3, 7, 6]]
 )
 
+triangulate_tetra = np.array([
+    [0, 1, 2], [0, 1, 3],
+    [0, 2, 3], [1, 2, 3]]
+)
+
+triangulate_wedge = np.array([
+    [0, 1, 2], [0, 3, 1],
+    [1, 3, 4], [0, 2, 3],
+    [2, 5, 3], [1, 4, 2],
+    [2, 4, 5], [3, 5, 4]]
+)
+
 
 class IDVCamera(object):
     '''Camera object used in the Interactive Data Visualization
