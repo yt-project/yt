9b268483f1fee45356efd9ed51b5e50550babba9
Britton Smith <brittonsmith@gmail.com>
2016-04-21 11:39:52 +0100

    Moving files into correct place.
    --HG--
    branch : yt

--- yt/frontends/fire/api.py
+++ yt/frontends/fire/api.py
@@ -7,15 +7,12 @@ API for FIRE frontend.
 """
 
 #-----------------------------------------------------------------------------
-# Copyright (c) 2016, Britton Smith.
+# Copyright (c) 2016, yt Development Team
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
-
-from yt.funcs import \
-    mylog
 
 from .data_structures import \
     FIREDataset
