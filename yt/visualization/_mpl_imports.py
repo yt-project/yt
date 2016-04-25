caf1dce01ea9682961fa3d7abef9970d0a66d906
Nathan Goldbaum <ngoldbau@illinois.edu>
2016-04-25 13:06:35 -0500

    plotting: remove unused imports in _mpl_imports
    --HG--
    branch : yt

--- yt/visualization/_mpl_imports.py
+++ yt/visualization/_mpl_imports.py
@@ -1,16 +1,6 @@
 import matplotlib
 matplotlib.rc('contour', negative_linestyle='solid')
 
-import matplotlib.image
-import matplotlib.ticker
-import matplotlib.axes
-import matplotlib.figure
-import matplotlib._image
-import matplotlib.colors
-import matplotlib.colorbar
-import matplotlib.cm
-import matplotlib.collections
-
 from matplotlib.backends.backend_agg import \
     FigureCanvasAgg
 
