6f2fc4aafface44dc2e0e50bcc70863d6488c437
John ZuHone <jzuhone@gmail.com>
2016-04-30 15:59:42 -0400

    Handling pressure and thermal energy correctly
    --HG--
    branch : yt

--- yt/frontends/athena_pp/fields.py
+++ yt/frontends/athena_pp/fields.py
@@ -32,8 +32,6 @@ class AthenaPPFieldInfo(FieldInfoContainer):
     known_other_fields = (
         ("rho", (rho_units, ["density"], None)),
         ("dens", (rho_units, ["density"], None)),
-        ("pgas", (pres_units, ["pressure"], None)),
-        ("Etot", (pres_units, [], None)),
         ("B1", (b_units, [], None)),
         ("B2", (b_units, [], None)),
         ("B3", (b_units, [], None)),
@@ -62,6 +60,26 @@ class AthenaPPFieldInfo(FieldInfoContainer):
                 print(comp)
                 self.add_field(("gas","%s_%s" % (vel_prefix, comp)),
                                function=velocity_field(i+1), units=unit_system["velocity"])
+        # Figure out thermal energy field
+        if ("athena++","pgas") in self.field_list:
+            self.add_output_field(("athena++","pgas"),
+                                  units=pres_units)
+            self.alias(("gas","pressure"),("athena++","pgas"),
+                       units=unit_system["pressure"])
+            def _thermal_energy(field, data):
+                return data["athena++","pgas"] / \
+                       (data.ds.gamma-1.)/data["athena++","rho"]
+        elif ("athena++","Etot") in self.field_list:
+            self.add_output_field(("athena++","Etot"),
+                                  units=pres_units)
+            def _thermal_energy(field, data):
+                eint = data["athena++", "Etot"] - data["gas","kinetic_energy"]
+                if ("athena++", "B1") in self.field_list:
+                    eint -= data["gas","magnetic_energy"]
+                return eint/data["athena++","dens"]
+        self.add_field(("gas","thermal_energy"),
+                       function=_thermal_energy,
+                       units=unit_system["specific_energy"])
         # Add temperature field
         def _temperature(field, data):
             if data.has_field_parameter("mu"):
