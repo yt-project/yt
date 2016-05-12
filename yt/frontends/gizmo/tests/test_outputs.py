6541bb3fd84ea2b9bc29433a8fc4fbe4af3ffc42
Britton Smith <brittonsmith@gmail.com>
2016-05-12 16:53:15 +0100

    Change one more FIRE to Gizmo.
    --HG--
    branch : yt

--- yt/frontends/gizmo/tests/test_outputs.py
+++ yt/frontends/gizmo/tests/test_outputs.py
@@ -41,7 +41,7 @@ fields = OrderedDict(
 @requires_ds(FIRE_m12i)
 def test_GizmoDataset():
     ds = data_dir_load(FIRE_m12i)
-    assert isinstance(ds, FIREDataset)
+    assert isinstance(ds, GizmoDataset)
     for test in sph_answer(ds, 'FIRE_m12i', 4786950, fields):
         test_GizmoDataset.__name__ = test.description
         yield test
