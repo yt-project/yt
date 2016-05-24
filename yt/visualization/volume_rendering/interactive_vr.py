58f2cd31668dff16849c72ba37e50c1f6906ee0a
Andrew Myers <atmyers2@gmail.com>
2016-05-23 23:17:53 -0700

    fix triangle winding.
    --HG--
    branch : yt

--- yt/visualization/volume_rendering/interactive_vr.py
+++ yt/visualization/volume_rendering/interactive_vr.py
@@ -79,28 +79,6 @@ FULLSCREEN_QUAD = np.array(
      +1.0, +1.0, 0.0], dtype=np.float32
 )
 
-triangulate_hex = np.array([
-    [0, 2, 1], [0, 3, 2],
-    [4, 5, 6], [4, 6, 7],
-    [0, 1, 5], [0, 5, 4],
-    [1, 2, 6], [1, 6, 5],
-    [0, 7, 3], [0, 4, 7],
-    [3, 6, 2], [3, 7, 6]]
-)
-
-triangulate_tetra = np.array([
-    [0, 1, 3], [2, 3, 1],
-    [0, 3, 2], [0, 2, 1]]
-)
-
-triangulate_wedge = np.array([
-    [3, 0, 1], [4, 3, 1],
-    [2, 5, 4], [2, 4, 1],
-    [0, 3, 2], [2, 3, 5],
-    [3, 4, 5], [0, 2, 1]]
-)
-
-
 class IDVCamera(object):
     '''Camera object used in the Interactive Data Visualization
 
