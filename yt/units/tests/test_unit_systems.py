511653a4297c2bc926a3bf1f55b243f93da326af
Nathan Goldbaum <ngoldbau@illinois.edu>
2016-06-06 11:45:04 -0500

    use code_pressure units when necessary
    --HG--
    branch : yt

--- yt/units/tests/test_unit_systems.py
+++ yt/units/tests/test_unit_systems.py
@@ -46,7 +46,7 @@ test_units["galactic"] = {"density": "Msun/kpc**3",
                           "velocity_divergence": "1/Myr",
                           "density_gradient_x": "Msun/kpc**4"}
 test_units["code"] = {"density": "code_mass/code_length**3",
-                      "kinetic_energy": "code_mass/(code_length*code_time**2)",
+                      "kinetic_energy": "code_pressure",
                       "velocity_magnitude": "code_velocity",
                       "velocity_divergence": "code_velocity/code_length",
                       "density_gradient_x": "code_mass/code_length**4"}
