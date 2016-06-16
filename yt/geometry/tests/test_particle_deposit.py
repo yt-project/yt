0b207fc0fc098d617fde8e647e9e5e5e208d81f0
Matthew Turk <matthewturk@gmail.com>
2016-06-16 16:10:55 -0500

    Adding checks for CIC against bounds
    --HG--
    branch : yt

---
+++ yt/geometry/tests/test_particle_deposit.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from yt.utilities.exceptions import \
+    YTBoundsDefinitionError
+
+from yt.testing import \
+    fake_random_ds, \
+    assert_equal, \
+    assert_rel_equal
+from numpy.testing import \
+    assert_raises
+
+def test_cic_deposit():
+    ds = fake_random_ds(64, nprocs = 8, particles=64**3)
+    my_reg = ds.arbitrary_grid(ds.domain_left_edge, ds.domain_right_edge,
+            dims=[1, 800, 800])
+    f = ("deposit", "all_cic")
+    assert_raises(YTBoundsDefinitionError, my_reg.__getitem__, f)
