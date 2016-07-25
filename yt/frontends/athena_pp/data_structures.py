dde1abf2c6d7bfd68244978383e0176fcd2541e9
John ZuHone <jzuhone@gmail.com>
2016-07-25 00:58:02 -0400

    Fix temperature units
    --HG--
    branch : yt

--- yt/frontends/athena_pp/data_structures.py
+++ yt/frontends/athena_pp/data_structures.py
@@ -264,7 +264,8 @@ class AthenaPPDataset(Dataset):
         """
         if "length_unit" not in self.units_override:
             self.no_cgs_equiv_length = True
-        for unit, cgs in [("length", "cm"), ("time", "s"), ("mass", "g")]:
+        for unit, cgs in [("length", "cm"), ("time", "s"), ("mass", "g"),
+                          ("temperature", "K")]:
             # We set these to cgs for now, but they may be overridden later.
             mylog.warning("Assuming 1.0 = 1.0 %s", cgs)
             setattr(self, "%s_unit" % unit, self.quan(1.0, cgs))
@@ -276,6 +277,9 @@ class AthenaPPDataset(Dataset):
             self.magnetic_unit = np.sqrt(4*np.pi * self.mass_unit /
                                          (self.time_unit**2 * self.length_unit))
         self.magnetic_unit.convert_to_units("gauss")
+        temp_unit = getattr(self, "temperature_unit", None)
+        if temp_unit is None:
+            self.temperature_unit = self.quan(1.0, "K")
         self.velocity_unit = self.length_unit / self.time_unit
 
     def _parse_parameter_file(self):
