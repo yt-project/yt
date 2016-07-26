c05b90ceb7edec5fed56320327c02ad9c45263df
Kacper Kowalik <xarthisius.kk@gmail.com>
2016-07-26 13:11:54 -0500

    Add preamble
    --HG--
    branch : yt

--- yt/utilities/tests/test_config.py
+++ yt/utilities/tests/test_config.py
@@ -1,3 +1,12 @@
+# -*- coding: UTF-8 -*-
+#-----------------------------------------------------------------------------
+# Copyright (c) 2016, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 import contextlib
 import mock
 import os
