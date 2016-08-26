467ec0471b867104519ebb718b1b32b85457ec08
Nathan Goldbaum <ngoldbau@illinois.edu>
2016-08-26 15:27:34 -0500

    remove try/except. use 'age' instead of 'particle_age', add 'age' to ramses 'particle_age' aliases
    --HG--
    branch : yt

--- yt/fields/particle_fields.py
+++ yt/fields/particle_fields.py
@@ -175,16 +175,13 @@ def particle_deposition_functions(ptype, coord_name, mass_name, registry):
                 function=function, units=unit_system["velocity"], take_log=False,
                 validators=[ValidateSpatial(0)])
 
-    try : 
-        for method, name in zip(("cic", "sum"), ("cic", "nn")):
-            function = _get_density_weighted_deposit_field(
-                "particle_age", "s", method)
-            registry.add_field(
-                ("deposit", ("%s_"+name+"_age") % (ptype)),
-                function=function, units=unit_system["time"], take_log=False,
-                  validators=[ValidateSpatial(0)])
-    except :
-        pass
+    for method, name in zip(("cic", "sum"), ("cic", "nn")):
+        function = _get_density_weighted_deposit_field(
+            "age", "s", method)
+        registry.add_field(
+            ("deposit", ("%s_"+name+"_age") % (ptype)),
+            function=function, units=unit_system["time"], take_log=False,
+            validators=[ValidateSpatial(0)])
 
     # Now some translation functions.
 
