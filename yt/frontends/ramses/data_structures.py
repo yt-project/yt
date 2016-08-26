0fcd6e92734191caa499cf1f6fef7d6e3afafac7
Mark Richardson <Mark.Richardson.Work@gmail.com>
2016-08-27 00:31:49 +0100

    Backporting PR #2346 https://bitbucket.org/yt_analysis/yt/pull-requests/2346
    --HG--
    branch : stable

--- yt/frontends/ramses/data_structures.py
+++ yt/frontends/ramses/data_structures.py
@@ -43,6 +43,9 @@ from yt.geometry.oct_container import \
     RAMSESOctreeContainer
 from yt.arraytypes import blankRecordArray
 
+from yt.utilities.lib.cosmology_time import \
+    friedman
+
 class RAMSESDomainFile(object):
     _last_mask = None
     _last_selector_id = None
@@ -620,7 +623,6 @@ class RAMSESDataset(Dataset):
                 dom, mi, ma = f.readline().split()
                 self.hilbert_indices[int(dom)] = (float(mi), float(ma))
         self.parameters.update(rheader)
-        self.current_time = self.parameters['time'] * self.parameters['unit_t']
         self.domain_left_edge = np.zeros(3, dtype='float64')
         self.domain_dimensions = np.ones(3, dtype='int32') * \
                         2**(self.min_level+1)
@@ -642,6 +644,23 @@ class RAMSESDataset(Dataset):
             self.hubble_constant = rheader["H0"] / 100.0 # This is H100
         self.max_level = rheader['levelmax'] - self.min_level - 1
         f.close()
+
+
+        if self.cosmological_simulation == 0:
+            self.current_time = self.parameters['time'] * self.parameters['unit_t']
+        else :
+            self.tau_frw, self.t_frw, self.dtau, self.n_frw, self.time_tot = \
+                friedman( self.omega_matter, self.omega_lambda, 1. - self.omega_matter - self.omega_lambda )
+
+            age = self.parameters['time']
+            iage = 1 + int(10.*age/self.dtau)
+            iage = np.min([iage,self.n_frw/2 + (iage - self.n_frw/2)/10])
+
+            self.time_simu = self.t_frw[iage  ]*(age-self.tau_frw[iage-1])/(self.tau_frw[iage]-self.tau_frw[iage-1])+ \
+                             self.t_frw[iage-1]*(age-self.tau_frw[iage  ])/(self.tau_frw[iage-1]-self.tau_frw[iage])
+ 
+            self.current_time = (self.time_tot + self.time_simu)/(self.hubble_constant*1e7/3.08e24)/self.parameters['unit_t']
+
 
     @classmethod
     def _is_valid(self, *args, **kwargs):
