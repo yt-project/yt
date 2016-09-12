65e2a91b889a903238c95cfd6fe7d6fae9cd7d24
Alex Lindsay <al007@illinois.edu>
2016-09-12 14:09:41 -0500

    Second order triangle sampler routines for exodus viewing.
    - Required additions to element_mappings.pxd and pixelization_routines.pyx.
    - Also added @atmyer's line changes fixing vertex fields for non-Hex8 mesh
      types in geometry_handler.py.
    
    --HG--
    branch : yt

--- yt/utilities/lib/pixelization_routines.pyx
+++ yt/utilities/lib/pixelization_routines.pyx
@@ -30,7 +30,8 @@ from yt.utilities.lib.element_mappings cimport \
     S2Sampler3D, \
     P1Sampler2D, \
     Q1Sampler2D, \
-    W1Sampler3D
+    W1Sampler3D, \
+    T2Sampler2D
 
 cdef extern from "pixelization_constants.h":
     enum:
@@ -201,7 +202,7 @@ def pixelize_cartesian(np.ndarray[np.float64_t, ndim=1] px,
                                 my_array[j,i] += (dsp * overlap1) * overlap2
                             else:
                                 my_array[j,i] = dsp
-                            
+
     return my_array
 
 @cython.cdivision(True)
@@ -309,10 +310,10 @@ def pixelize_cylinder(np.ndarray[np.float64_t, ndim=1] radius,
     cdef np.float64_t r_i, theta_i, dr_i, dtheta_i, dthetamin
     cdef np.float64_t costheta, sintheta
     cdef int i, pi, pj
-    
+
     imax = radius.argmax()
     rmax = radius[imax] + dradius[imax]
-          
+
     if input_img is None:
         img = np.zeros((buff_size[0], buff_size[1]))
         img[:] = np.nan
@@ -363,7 +364,7 @@ def pixelize_cylinder(np.ndarray[np.float64_t, ndim=1] radius,
             sintheta = math.sin(theta_i)
             while r_i < r0 + dr_i:
                 if rmax <= r_i:
-                    r_i += 0.5*dx 
+                    r_i += 0.5*dx
                     continue
                 y = r_i * costheta
                 x = r_i * sintheta
@@ -374,7 +375,7 @@ def pixelize_cylinder(np.ndarray[np.float64_t, ndim=1] radius,
                     if img[pi, pj] != img[pi, pj]:
                         img[pi, pj] = 0.0
                     img[pi, pj] = field[i]
-                r_i += 0.5*dx 
+                r_i += 0.5*dx
             theta_i += dthetamin
 
     return img
@@ -412,7 +413,7 @@ def pixelize_aitoff(np.ndarray[np.float64_t, ndim=1] theta,
     cdef np.float64_t s2 = math.sqrt(2.0)
     cdef np.float64_t xmax, ymax, xmin, ymin
     nf = field.shape[0]
-    
+
     if input_img is None:
         img = np.zeros((buff_size[0], buff_size[1]))
         img[:] = np.nan
@@ -551,7 +552,7 @@ def pixelize_element_mesh(np.ndarray[np.float64_t, ndim=2] coords,
                           np.ndarray[np.int64_t, ndim=2] conn,
                           buff_size,
                           np.ndarray[np.float64_t, ndim=2] field,
-                          extents, 
+                          extents,
                           int index_offset = 0):
     cdef np.ndarray[np.float64_t, ndim=3] img
     img = np.zeros(buff_size, dtype="float64")
@@ -594,6 +595,8 @@ def pixelize_element_mesh(np.ndarray[np.float64_t, ndim=2] coords,
         sampler = P1Sampler2D()
     elif ndim == 2 and nvertices == 4:
         sampler = Q1Sampler2D()
+    elif ndim == 2 and nvertices == 6:
+        sampler = T2Sampler2D()
     else:
         raise YTElementTypeNotRecognized(ndim, nvertices)
 
@@ -602,7 +605,7 @@ def pixelize_element_mesh(np.ndarray[np.float64_t, ndim=2] coords,
         if buff_size[2] != 1:
             raise RuntimeError("Slices of 2D datasets must be "
                                "perpendicular to the 'z' direction.")
-    
+
     # allocate temporary storage
     vertices = <np.float64_t *> malloc(ndim * sizeof(np.float64_t) * nvertices)
     field_vals = <np.float64_t *> malloc(sizeof(np.float64_t) * num_field_vals)
