51b49ad6fb55fc0ba1fe6945c3dbef64db910865
Matthew Turk <matthewturk@gmail.com>
2016-11-04 14:52:39 -0500

    Py3 fix
    --HG--
    branch : yt

--- yt/data_objects/tests/test_clone.py
+++ yt/data_objects/tests/test_clone.py
@@ -12,12 +12,12 @@ def test_clone_sphere():
       "velocity_x", "velocity_y", "velocity_z"))
     sp0 = ds.sphere(ds.domain_center, 0.25)
 
-    assert_equal(sp0.keys(), [])
+    assert_equal(list(sp0.keys()), [])
 
     sp1 = sp0.clone()
     sp0["density"]
-    assert_equal(sp0.keys(), (("gas","density"),))
-    assert_equal(sp1.keys(), [])
+    assert_equal(list(sp0.keys()), (("gas","density"),))
+    assert_equal(list(sp1.keys()), [])
 
     sp1["density"]
 
