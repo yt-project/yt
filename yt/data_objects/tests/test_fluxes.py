c6f0bd8c49795fbedb7a10f2df83c8797ea6346d
Nathan Goldbaum <ngoldbau@illinois.edu>
2016-12-01 14:34:11 -0600

    Convert isocontour tests to not yield asserts
    --HG--
    branch : yt

--- yt/data_objects/tests/test_fluxes.py
+++ yt/data_objects/tests/test_fluxes.py
@@ -11,9 +11,9 @@ def test_flux_calculation():
     ds = fake_random_ds(64, nprocs = 4)
     dd = ds.all_data()
     surf = ds.surface(dd, "x", 0.51)
-    yield assert_equal, surf["x"], 0.51
+    assert_equal(surf["x"], 0.51)
     flux = surf.calculate_flux("ones", "zeros", "zeros", "ones")
-    yield assert_almost_equal, flux, 1.0, 12
+    assert_almost_equal(flux.value, 1.0, 12)
 
 def test_sampling():
     ds = fake_random_ds(64, nprocs = 4)
@@ -21,4 +21,4 @@ def test_sampling():
     for i, ax in enumerate('xyz'):
         surf = ds.surface(dd, ax, 0.51)
         surf.get_data(ax, "vertex")
-        yield assert_equal, surf.vertex_samples[ax], surf.vertices[i,:]
+        assert_equal(surf.vertex_samples[ax], surf.vertices[i,:])
