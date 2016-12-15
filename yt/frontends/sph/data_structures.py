9b1578e1b3f3ec2d5e9fc7376ebdc0492148d417
Bili Dong <qobilidop@gmail.com>
2016-12-14 17:20:01 -0800

    Recover the copyright info
    It was deleted accidentally.
    
    --HG--
    branch : yt

--- yt/frontends/sph/data_structures.py
+++ yt/frontends/sph/data_structures.py
@@ -7,6 +7,14 @@ Data structures for SPH frontends.
 """
 from __future__ import print_function
 
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 from yt.data_objects.static_output import \
     ParticleDataset
 from yt.fields.particle_fields import \
