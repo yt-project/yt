f84c35779278f11e9b3962f22f2c3187d4fd254d
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-01-04 16:55:16 -0600

    Fix issues in implementation of ray-sphere selection
    --HG--
    branch : yt

--- yt/geometry/selection_routines.pyx
+++ yt/geometry/selection_routines.pyx
@@ -17,6 +17,7 @@ Geometry selection routines.
 import numpy as np
 cimport numpy as np
 cimport cython
+from libc.math cimport sqrt
 from libc.stdlib cimport malloc, free
 from libc.stdio cimport printf
 from yt.utilities.lib.fp_utils cimport fclip, iclip, fmax, fmin, imin, imax
@@ -54,6 +55,13 @@ cdef np.int64_t fnv_hash(unsigned char[:] octets):
         hash_val = hash_val ^ octet
         hash_val = hash_val * 16777619
     return hash_val
+
+cdef inline np.float64_t dot(np.float64_t* v1,
+                             np.float64_t* v2) nogil:
+    return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+
+cdef inline np.float64_t norm(np.float64_t* v) nogil:
+    return sqrt(dot(v, v))
 
 # These routines are separated into a couple different categories:
 #
@@ -1584,33 +1592,22 @@ cdef class RaySelector(SelectorObject):
     cdef int select_point(self, np.float64_t pos[3]) nogil:
         # two 0-volume constructs don't intersect
         return 0
-
-    # Due to limited knowledge of Cython, the codes below (until select_sphere)
-    # might need refactoring. - Bili
-
-    cdef np.float64_t* _diff(self, np.float64_t v1[3], np.float64_t v2[3]) nogil:
-        cdef int i
-        cdef np.float64_t v_diff[3]
-        for i in range(3):
-            v_diff[i] = self.difference(v1[i], v2[i], i)
-        return v_diff
-
-    cdef np.float64_t _dot(self, np.float64_t v1[3], np.float64_t v2[3]) nogil:
-        return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
-
-    cdef np.float64_t _norm(self, np.float64_t v[3]) nogil:
-        return self._dot(v, v)**0.5
-
+    
     @cython.boundscheck(False)
     @cython.wraparound(False)
     @cython.cdivision(True)
     cdef int select_sphere(self, np.float64_t pos[3], np.float64_t radius) nogil:
-        cdef np.float64_t length = self._norm(self.vec)
-        cdef np.float64_t* r = self._diff(pos, self.p1)
-        cdef np.float64_t l = self._dot(r, self.vec) / length
+
+        cdef int i
+        cdef np.float64_t length = norm(self.vec)
+        cdef np.float64_t r[3]
+        for i in range(3):
+            r[i] = self.periodic_difference(pos[i], self.p1[i], i)
+        cdef np.float64_t l = dot(r, self.vec) / length
         # Note that `dl` here is the geometric intersection.
-        cdef np.float64_t dl_hlf_sqr = radius**2 + l**2 - self._dot(r, r)
+        cdef np.float64_t dl_hlf_sqr = radius**2 + l*l - dot(r, r)
         cdef np.float64_t dl_hlf
+
         if dl_hlf_sqr > 0:
             dl_hlf = dl_hlf_sqr**0.5
             if (l + dl_hlf > 0) and (l - dl_hlf < length):
