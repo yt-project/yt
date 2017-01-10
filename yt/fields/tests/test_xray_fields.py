f3e8ed54da6e964272e85d523f87ace387c2ad80
John ZuHone <jzuhone@gmail.com>
2017-01-10 15:37:34 -0500

    Swap table types
    --HG--
    branch : yt

--- yt/fields/tests/test_xray_fields.py
+++ yt/fields/tests/test_xray_fields.py
@@ -11,22 +11,21 @@ def setup():
 
 sloshing = "GasSloshingLowRes/sloshing_low_res_hdf5_plt_cnt_0300"
 @requires_ds(sloshing, big_data=True)
-def test_sloshing_cloudy():
+def test_sloshing_apec():
     ds = data_dir_load(sloshing)
-    fields = add_xray_emissivity_field(ds, 0.5, 7.0, table_type="cloudy", 
+    fields = add_xray_emissivity_field(ds, 0.5, 7.0, table_type="apec", 
                                        metallicity=0.3)
     for test in small_patch_amr(ds, fields):
-        test_sloshing_cloudy.__name__ = test.description
+        test_sloshing_apec.__name__ = test.description
         yield test
 
 d9p = "D9p_500/10MpcBox_HartGal_csf_a0.500.d"
 @requires_ds(d9p, big_data=True)
-def test_d9p_apec():
-    ds = data_dir_load(sloshing)
+def test_d9p_cloudy():
+    ds = data_dir_load(d9p)
     fields = add_xray_emissivity_field(ds, 0.5, 2.0, redshift=ds.current_redshift,
-                                       table_type="apec",
-                                       cosmology=ds.cosmology,
+                                       table_type="cloudy", cosmology=ds.cosmology,
                                        metallicity=("gas", "metallicity"))
     for test in small_patch_amr(ds, fields):
-        test_d9p_apec.__name__ = test.description
+        test_d9p_cloudy.__name__ = test.description
         yield test
