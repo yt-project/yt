0a1f561d428433d2ca59834231c673139b10a91c
John ZuHone <jzuhone@gmail.com>
2017-01-10 15:31:23 -0500

    Use a different dataset for apec test, which actually has a metallicity field. We’ll need to bump the test number
    --HG--
    branch : yt

--- yt/fields/tests/test_xray_fields.py
+++ yt/fields/tests/test_xray_fields.py
@@ -13,19 +13,20 @@ sloshing = "GasSloshingLowRes/sloshing_low_res_hdf5_plt_cnt_0300"
 @requires_ds(sloshing, big_data=True)
 def test_sloshing_cloudy():
     ds = data_dir_load(sloshing)
-    fields = add_xray_emissivity_field(ds, 0.5, 7.0, redshift=0.05,
-                                       table_type="cloudy", metallicity=0.3)
+    fields = add_xray_emissivity_field(ds, 0.5, 7.0, table_type="cloudy", 
+                                       metallicity=0.3)
     for test in small_patch_amr(ds, fields):
         test_sloshing_cloudy.__name__ = test.description
         yield test
 
-ecp = "enzo_cosmology_plus/DD0046/DD0046"
-@requires_ds(ecp, big_data=True)
-def test_ecp_apec():
+d9p = "D9p_500/10MpcBox_HartGal_csf_a0.500.d"
+@requires_ds(d9p, big_data=True)
+def test_d9p_apec():
     ds = data_dir_load(sloshing)
-    fields = add_xray_emissivity_field(ds, 0.5, 7.0, redshift=0.05,
+    fields = add_xray_emissivity_field(ds, 0.5, 2.0, redshift=ds.current_redshift,
                                        table_type="apec",
+                                       cosmology=ds.cosmology,
                                        metallicity=("gas", "metallicity"))
     for test in small_patch_amr(ds, fields):
-        test_ecp_apec.__name__ = test.description
+        test_d9p_apec.__name__ = test.description
         yield test
