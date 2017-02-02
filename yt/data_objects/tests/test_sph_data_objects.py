7e7be2a05ea722dd70376b5709367ee8a0ecaf22
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-02-02 16:12:16 -0600

    add tests for sphere, disk, and ray
    --HG--
    branch : yt

--- yt/data_objects/tests/test_sph_data_objects.py
+++ yt/data_objects/tests/test_sph_data_objects.py
@@ -45,13 +45,82 @@ def test_region():
         # test that regions enclosing a particle's smoothing region
         # correctly select SPH particles
         for i in range(-1, 2):
-            left_edge = np.array([le + i*0.1 for le in left_edge])
-            right_edge = np.array([re + i*0.1 for re in right_edge])
-            # check if we went off the edge of the domain
-            whl = left_edge < ds.domain_left_edge
-            left_edge[whl] = ds.domain_left_edge[whl]
-            whr = left_edge < ds.domain_left_edge
-            left_edge[whr] = ds.domain_left_edge[whr]
-            reg = ds.box(left_edge, right_edge)
-            assert_equal(reg['gas', 'density'].shape[0], answer)
+            for j in range(-1, 2):
+                le = np.array([le + i*0.1 for le in left_edge])
+                re = np.array([re + j*0.1 for re in right_edge])
 
+                # check if we went off the edge of the domain
+                whl = le < ds.domain_left_edge
+                le[whl] = ds.domain_left_edge[whl]
+                whr = re > ds.domain_right_edge
+                re[whr] = ds.domain_right_edge[whr]
+
+                reg = ds.box(le, re)
+                assert_equal(reg['gas', 'density'].shape[0], answer)
+
+SPHERE_ANSWERS = {
+    ((0, 0, 0), 4): 7,
+    ((0, 0, 0), 3): 7,
+    ((0, 0, 0), 2): 6,
+    ((0, 0, 0), 1): 4,
+    ((0, 0, 0), 0.5): 1,
+    ((1, 0, 0), 0.5): 1,
+    ((1, 0, 0), 1.0): 2,
+    ((0, 1, 0), 1.0): 3,
+    ((0, 0, 1), 1.0): 3,
+}
+
+def test_sphere():
+    ds = fake_sph_orientation_ds()
+    for (center, radius), answer in SPHERE_ANSWERS.items():
+        # test that spheres enclosing a particle's smoothing region
+        # correctly select SPH particles
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                cent = np.array([c + i*0.1 for c in center])
+                rad = radius + 0.1*j
+                sph = ds.sphere(cent, rad)
+                assert_equal(sph['gas', 'density'].shape[0], answer)
+
+DISK_ANSWERS = {
+    ((0, 0, 0), (0, 0, 1), 4, 3): 7,
+    ((0, 0, 0), (0, 0, 1), 4, 2): 6,
+    ((0, 0, 0), (0, 0, 1), 4, 1): 5,
+    ((0, 0, 0), (0, 0, 1), 4, 0.5): 4,
+    ((0, 0, 0), (0, 1, 0), 4, 3): 7,
+    ((0, 0, 0), (0, 1, 0), 4, 2): 7,
+    ((0, 0, 0), (0, 1, 0), 4, 1): 6,
+    ((0, 0, 0), (0, 1, 0), 4, 0.5): 5,
+    ((0, 0, 0), (1, 0, 0), 4, 3): 7,
+    ((0, 0, 0), (1, 0, 0), 4, 2): 7,
+    ((0, 0, 0), (1, 0, 0), 4, 1): 7,
+    ((0, 0, 0), (1, 0, 0), 4, 0.5): 6,
+    ((-0.5, -0.5, -0.5), (1, 1, 1), 4, 4): 7,
+}
+
+def test_disk():
+    ds = fake_sph_orientation_ds()
+    for (center, normal, radius, height), answer in DISK_ANSWERS.items():
+        # test that disks enclosing a particle's smoothing region
+        # correctly select SPH particles
+        for i in range(-1, 2):
+            cent = np.array([c + i*0.1 for c in center])
+            disk = ds.disk(cent, normal, radius, height)
+            assert_equal(disk['gas', 'density'].shape[0], answer)
+
+RAY_ANSWERS = {
+    ((0, 0, 0), (3, 0, 0)): 2,
+    ((0, 0, 0), (0, 3, 0)): 3,
+    ((0, 0, 0), (0, 0, 3)): 4,
+    ((0, 1, 0), (0, 2, 0)): 2,
+    ((1, 0, 0), (0, 2, 0)): 2,
+}
+
+def test_ray():
+    ds = fake_sph_orientation_ds()
+    for (start_point, end_point), answer in RAY_ANSWERS.items():
+        for i in range(-1, 2):
+            start = np.array([s + i*0.1 for s in start_point])
+            end = np.array([e + i*0.1 for e in end_point])
+            ray = ds.ray(start, end)
+            assert_equal(ray['gas', 'density'].shape[0], answer)
