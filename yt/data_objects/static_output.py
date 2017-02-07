dfa8e822a3ff0db07b598c72b7bbc19f349be25b
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-02-07 11:00:42 -0600

    Use contiguous arrays for deposit fields. Fixes #1319
    --HG--
    branch : stable

--- yt/data_objects/static_output.py
+++ yt/data_objects/static_output.py
@@ -1148,16 +1148,15 @@ class Dataset(object):
             Create a grid field for particle quantities using given method.
             """
             pos = data[ptype, "particle_position"]
+            fields = [data[ptype, deposit_field]]
             if method == 'weighted_mean':
-                d = data.ds.arr(data.deposit(pos, [data[ptype, deposit_field],
-                                                   data[ptype, weight_field]],
-                                             method=method, kernel_name=kernel_name),
-                                             input_units=units)
+                fields.append(data[ptype, weight_field])
+            fields = [np.ascontiguousarray(f) for f in fields]
+            d = data.deposit(pos, fields, method=method,
+                             kernel_name=kernel_name)
+            d = data.ds.arr(d, input_units=units)
+            if method == 'weighted_mean':
                 d[np.isnan(d)] = 0.0
-            else:
-                d = data.ds.arr(data.deposit(pos, [data[ptype, deposit_field]],
-                                             method=method, kernel_name=kernel_name),
-                                             input_units=units)
             return d
 
         self.add_field(
