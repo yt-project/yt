13cdc60bb1af6f69f7c1128277b44037585b9fd7
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-03-06 12:27:48 -0800

    Updating version numbers
    --HG--
    branch : stable

--- yt/__init__.py
+++ yt/__init__.py
@@ -72,7 +72,7 @@ categories goes here.
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-__version__ = "3.3.4"
+__version__ = "3.3.5"
 
 # First module imports
 import numpy as np # For modern purposes
