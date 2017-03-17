dd352c0d37f0e8b98ad1c7752a5a63734fda873d
Britton Smith <brittonsmith@gmail.com>
2017-03-17 11:24:17 -0700

    Moving filename setup into a class method so I can subclass it.
    --HG--
    branch : yt

--- yt/geometry/particle_geometry_handler.py
+++ yt/geometry/particle_geometry_handler.py
@@ -69,19 +69,17 @@ class ParticleIndex(Index):
     def convert(self, unit):
         return self.dataset.conversion_factors[unit]
 
-    def _initialize_particle_handler(self):
-        self._setup_data_io()
+    def _setup_filenames(self):
         template = self.dataset.filename_template
         ndoms = self.dataset.file_count
         cls = self.dataset._file_class
-        if ndoms > 1:
-            self.data_files = \
-              [cls(self.dataset, self.io, template % {'num':i}, i)
-               for i in range(ndoms)]
-        else:
-            self.data_files = \
-              [cls(self.dataset, self.io,
-                   self.dataset.parameter_filename, 0)]
+        self.data_files = \
+          [cls(self.dataset, self.io, template % {'num':i}, i)
+           for i in range(ndoms)]
+
+    def _initialize_particle_handler(self):
+        self._setup_data_io()
+        self._setup_filenames()
 
         index_ptype = self.index_ptype
         if index_ptype == "all":
