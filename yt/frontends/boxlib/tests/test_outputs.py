15d7a8c59c0d2fcabd28c749f636194f4b0adfa8
Michael Zingale <michael.zingale@stonybrook.edu>
2017-04-03 11:48:14 -0400

    add Nyx field list test on particle-less dataset
    --HG--
    branch : yt

--- yt/frontends/boxlib/tests/test_outputs.py
+++ yt/frontends/boxlib/tests/test_outputs.py
@@ -225,3 +225,26 @@ def test_WarpXDataset():
 def test_units_override():
     for test in units_override_check(rt):
         yield test
+
+nyx_no_particles = "nyx_sedov_plt00086"
+@requires_file(nyx_no_particles)
+def test_nyx_no_part():
+    assert isinstance(data_dir_load(nyx_no_particles), NyxDataset)
+
+    fields = sorted(
+        [('boxlib', 'H'), ('boxlib', 'He'), ('boxlib', 'MachNumber'),
+         ('boxlib', 'Ne'), ('boxlib', 'Rank'), ('boxlib', 'StateErr'),
+         ('boxlib', 'Temp'), ('boxlib', 'X(H)'), ('boxlib', 'X(He)'),
+         ('boxlib', 'density'), ('boxlib', 'divu'), ('boxlib', 'eint_E'),
+         ('boxlib', 'eint_e'), ('boxlib', 'entropy'), ('boxlib', 'forcex'),
+         ('boxlib', 'forcey'), ('boxlib', 'forcez'), ('boxlib', 'kineng'),
+         ('boxlib', 'logden'), ('boxlib', 'magmom'), ('boxlib', 'magvel'),
+         ('boxlib', 'magvort'), ('boxlib', 'pressure'), ('boxlib', 'rho_E'),
+         ('boxlib', 'rho_H'), ('boxlib', 'rho_He'), ('boxlib', 'rho_e'),
+         ('boxlib', 'soundspeed'), ('boxlib', 'x_velocity'), ('boxlib', 'xmom'),
+         ('boxlib', 'y_velocity'), ('boxlib', 'ymom'), ('boxlib', 'z_velocity'),
+         ('boxlib', 'zmom')])
+
+    ds = data_dir_load(nyx_no_particles)
+    assert_equal(sorted(ds.field_list, fields))
+
