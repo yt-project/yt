61c0df7dd49c7c8f085d9177c5fe8c60be467f97
Andrew Myers <atmyers2@gmail.com>
2017-04-19 11:32:58 -0700

    fix test name.
    --HG--
    branch : yt

--- yt/visualization/tests/test_raw_field_slices.py
+++ yt/visualization/tests/test_raw_field_slices.py
@@ -47,7 +47,7 @@ _raw_field_names =  [('raw', 'Bx'),
                      ('raw', 'jy'),
                      ('raw', 'jz')]
 @requires_ds(raw_fields)
-def test_tri2():
+def test_raw_field_slices():
     ds = data_dir_load(raw_fields)
     for field in _raw_field_names:
         yield compare(ds, field, "answers_raw_%s" % field[1])
