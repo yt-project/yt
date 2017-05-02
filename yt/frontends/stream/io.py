99cf4b747de4c9ed178f7f703b2c045041852076
atmyers <atmyers2@gmail.com>
2017-05-02 11:17:23 -0700

    remove unused variable.

--- yt/frontends/stream/io.py
+++ yt/frontends/stream/io.py
@@ -283,7 +283,6 @@ class IOHandlerStreamUnstructured(BaseIOHandler):
             ind = 0
             ftype, fname = field
             if ftype == "all":
-                mesh_ids = [mesh.mesh_id + 1 for mesh in self.ds.index.mesh_union]
                 objs = [mesh for mesh in self.ds.index.mesh_union]
             else:
                 mesh_ids = [int(ftype[-1])]
