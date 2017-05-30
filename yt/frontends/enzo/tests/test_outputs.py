98b2e1e4a6a359d423a45db12fc4b61579deb274
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-05-30 12:02:29 -0500

    add failing test

--- yt/frontends/enzo/tests/test_outputs.py
+++ yt/frontends/enzo/tests/test_outputs.py
@@ -28,6 +28,8 @@ from yt.utilities.answer_testing.framework import \
     data_dir_load, \
     AnalyticHaloMassFunctionTest, \
     SimulatedHaloMassFunctionTest
+from yt.visualization.plot_window import \
+    SlicePlot
 from yt.frontends.enzo.api import EnzoDataset
 from yt.frontends.enzo.fields import NODAL_FLAGS
 
@@ -42,6 +44,7 @@ enzotiny = "enzo_tiny_cosmology/DD0046/DD0046"
 toro1d = "ToroShockTube/DD0001/data0001"
 kh2d = "EnzoKelvinHelmholtz/DD0011/DD0011"
 mhdctot = "MHDCTOrszagTang/DD0004/data0004"
+dnz = "DeeplyNestedZoom/DD0025/data0025"
 
 def check_color_conservation(ds):
     species_names = ds.field_info.species_names
@@ -191,3 +194,17 @@ def test_face_centered_mhdct_fields():
     assert (ad['BxF'].sum(axis=-1)/2 == ad['Bx']).all()
     assert (ad['ByF'].sum(axis=-1)/2 == ad['By']).all()
     assert (ad['BzF'].sum(axis=-1)/2 == ad['Bz']).all()
+
+@requires_file(dnz)
+def test_deeply_nested_zoom():
+    ds = data_dir_load(dnz)
+
+    # carefully chosen to just barely miss a grid in the middle of the image
+    center = [0.4915073260199302, 0.5052605316800006, 0.4905805557500548]
+    
+    plot = SlicePlot(ds, 'z', 'density', width=(0.001, 'pc'),
+                     center=center)
+
+    image = plot.frb['density']
+
+    assert (image > 0).all()
