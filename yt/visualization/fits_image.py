7d8939e7c0a0bcee79737bb45323667a76b7e46d
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-06-12 10:25:40 -0500

    [lgtm] Fix mutable default argument in function definition

--- yt/visualization/fits_image.py
+++ yt/visualization/fits_image.py
@@ -461,7 +461,7 @@ class FITSImageData(object):
         return cls(data)
 
     def create_sky_wcs(self, sky_center, sky_scale,
-                       ctype=["RA---TAN","DEC--TAN"],
+                       ctype=("RA---TAN","DEC--TAN"),
                        crota=None, cd=None, pc=None,
                        wcsname="celestial",
                        replace_old_wcs=True):
@@ -493,6 +493,7 @@ class FITSImageData(object):
         old_wcs = self.wcs
         naxis = old_wcs.naxis
         crval = [sky_center[0], sky_center[1]]
+        ctype = list(ctype)
         if isinstance(sky_scale, YTQuantity):
             scaleq = sky_scale
         else:
