7e7832b45ba9bfc0b1bf9f8d085a1453144003b9
qobilidop <qobilidop@gmail.com>
2017-07-19 09:55:52 +0800

    Set proper Hubble constant in test

--- yt/frontends/ahf/tests/test_outputs.py
+++ yt/frontends/ahf/tests/test_outputs.py
@@ -29,13 +29,18 @@ _fields = ('particle_position_x', 'particle_position_y',
 ahf_halos = 'ahf_halos/snap_N64L16_135.parameter'
 
 
+def load(filename):
+    return data_dir_load(filename, kwargs={'hubble_constant': 0.7})
+
+
 @requires_ds(ahf_halos)
 def test_fields_ahf_halos():
-    ds = data_dir_load(ahf_halos)
+    ds = load(ahf_halos)
     assert_equal(str(ds), os.path.basename(ahf_halos))
     for field in _fields:
         yield FieldValuesTest(ahf_halos, field, particle_type=True)
 
+
 @requires_file(ahf_halos)
 def test_AHFHalosDataset():
-    assert isinstance(data_dir_load(ahf_halos), AHFHalosDataset)
+    assert isinstance(load(ahf_halos), AHFHalosDataset)
