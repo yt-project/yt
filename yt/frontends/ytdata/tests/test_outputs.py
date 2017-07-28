2fae14c18d0661c0dc54e365a303353d69987c20
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-07-28 11:54:57 -0500

    Restore support for visualizing YTSpatialPlotDataset instances

--- yt/frontends/ytdata/tests/test_outputs.py
+++ yt/frontends/ytdata/tests/test_outputs.py
@@ -26,7 +26,9 @@ from yt.frontends.ytdata.api import \
     save_as_dataset
 from yt.testing import \
     assert_allclose_units, \
-    assert_equal
+    assert_equal, \
+    assert_fname, \
+    fake_random_ds
 from yt.utilities.answer_testing.framework import \
     requires_ds, \
     data_dir_load, \
@@ -34,6 +36,9 @@ from yt.utilities.answer_testing.framework import \
 from yt.units.yt_array import \
     YTArray, \
     YTQuantity
+from yt.visualization.plot_window import \
+    SlicePlot, \
+    ProjectionPlot
 from yt.visualization.profile_plotter import \
     ProfilePlot, \
     PhasePlot
@@ -221,5 +226,35 @@ def test_nonspatial_data():
     new_ds = load(full_fn)
     assert isinstance(new_ds, YTNonspatialDataset)
     yield YTDataFieldTest(full_fn, "density", geometric=False)
+    os.chdir(curdir)
+    shutil.rmtree(tmpdir)
+
+def test_plot_data():
+    tmpdir = tempfile.mkdtemp()
+    curdir = os.getcwd()
+    os.chdir(tmpdir)
+    ds = fake_random_ds(16)
+
+    plot = SlicePlot(ds, 'z', 'density')
+    plot.data_source.save_as_dataset('slice.h5')
+    ds_slice = load('slice.h5')
+    p = SlicePlot(ds_slice, 'z', 'density')
+    fn = p.save()
+    assert_fname(fn[0])
+
+    plot = ProjectionPlot(ds, 'z', 'density')
+    plot.data_source.save_as_dataset('proj.h5')
+    ds_proj = load('slice.h5')
+    p = ProjectionPlot(ds_proj, 'z', 'density')
+    fn = p.save()
+    assert_fname(fn[0])
+
+    plot = SlicePlot(ds, [1, 1, 1], 'density')
+    plot.data_source.save_as_dataset('oas.h5')
+    ds_oas = load('oas.h5')
+    p = SlicePlot(ds_oas, [1, 1, 1], 'density')
+    fn = p.save()
+    assert_fname(fn[0])
+
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
