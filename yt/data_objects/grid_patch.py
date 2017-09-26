54337fc66252af46c2b3bebb73848c1377c5cf40
Yi-Hao Chen <ychen@astro.wisc.edu>
2017-09-26 15:03:16 -0500

    Call __init__ on the superclass and remove redundant initializations.

--- yt/data_objects/grid_patch.py
+++ yt/data_objects/grid_patch.py
@@ -58,8 +58,7 @@ class AMRGridPatch(YTSelectionContainer):
     OverlappingSiblings = None
 
     def __init__(self, id, filename=None, index=None):
-        self.field_data = YTFieldData()
-        self.field_parameters = {}
+        super(AMRGridPatch, self).__init__(index.dataset, None)
         self.id = id
         self._child_mask = self._child_indices = self._child_index_mask = None
         self.ds = index.dataset
@@ -69,8 +68,6 @@ class AMRGridPatch(YTSelectionContainer):
         self._last_mask = None
         self._last_count = -1
         self._last_selector_id = None
-        self._current_particle_type = 'all'
-        self._current_fluid_type = self.ds.default_fluid_type
 
     def get_global_startindex(self):
         """
