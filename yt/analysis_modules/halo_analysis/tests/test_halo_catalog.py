c938aad48c3da232331f57889539f00ccc3733bc
Britton Smith <brittonsmith@gmail.com>
2017-10-02 10:49:41 -0700

    Handle dataset loading more carefully.

--- yt/analysis_modules/halo_analysis/tests/test_halo_catalog.py
+++ yt/analysis_modules/halo_analysis/tests/test_halo_catalog.py
@@ -39,25 +39,27 @@ class HaloQuantityTest(AnswerTestingTest):
     _type_name = "HaloQuantity"
     _attrs = ()
 
-    def __init__(self, data_ds, halos_ds):
-        self.data_ds = data_dir_load(data_ds)
-        self.halos_ds = data_dir_load(halos_ds)
-        self.ds = data_ds
+    def __init__(self, data_ds_fn, halos_ds_fn):
+        self.data_ds_fn = data_ds_fn
+        self.halos_ds_fn = halos_ds_fn
+        self.ds = data_dir_load(data_ds_fn)
 
     def run(self):
         curdir = os.getcwd()
         tmpdir = tempfile.mkdtemp()
         os.chdir(tmpdir)
 
+        dds = data_dir_load(self.data_ds_fn)
+        hds = data_dir_load(self.halos_ds_fn)
         hc = HaloCatalog(
-            data_ds=self.data_ds, halos_ds=self.halos_ds,
-            output_dir=os.path.join(tmpdir, str(self.data_ds)))
+            data_ds=dds, halos_ds=hds,
+            output_dir=os.path.join(tmpdir, str(dds)))
         hc.add_callback("sphere")
         hc.add_quantity("nstars")
         hc.create()
 
-        fn = os.path.join(tmpdir, str(self.data_ds),
-                          "%s.0.h5" % str(self.data_ds))
+        fn = os.path.join(tmpdir, str(dds),
+                          "%s.0.h5" % str(dds))
         ds = load(fn)
         ad = ds.all_data()
         mi, ma = ad.quantities.extrema("nstars")
@@ -78,6 +80,4 @@ e64 = "Enzo_64/DD0043/data0043"
 @requires_ds(rh0)
 @requires_ds(e64)
 def test_halo_quantity():
-    hds = data_dir_load(rh0)
-    dds = data_dir_load(e64)
-    yield HaloQuantityTest(dds, hds)
+    yield HaloQuantityTest(e64, rh0)
