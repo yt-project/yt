eef8da642cb84789432a25900fb61a5d9e985b42
Corentin Cadiou <contact@cphyc.me>
2017-11-11 14:37:29 +0100

    add test

--- yt/frontends/ramses/tests/test_outputs.py
+++ yt/frontends/ramses/tests/test_outputs.py
@@ -204,3 +204,34 @@ def test_ramses_sink():
 
     for field in expected_fields:
         assert(('sink', 'field') not in ds.field_list)
+
+
+ramses_new_format = "ramses_new_format/output_00011/info_00011.txt"
+@requires_file(ramses_new_format)
+def test_new_format():
+    expected_particle_fields = [
+        ('io', 'particle_birth_time'),
+        ('io', 'particle_family'),
+        ('io', 'particle_identity'),
+        ('io', 'particle_levelp'),
+        ('io', 'particle_mass'),
+        ('io', 'particle_metallicity'),
+        ('io', 'particle_position_x'),
+        ('io', 'particle_position_y'),
+        ('io', 'particle_position_z'),
+        ('io', 'particle_tag'),
+        ('io', 'particle_velocity_x'),
+        ('io', 'particle_velocity_y'),
+        ('io', 'particle_velocity_z')]
+
+    ds = yt.load(ramses_new_format)
+    ad = ds.all_data()
+
+    # Check all the expected fields exist and can be accessed
+    for f in expected_particle_fields:
+        assert(f in ds.field_list)
+        ad[f]
+
+    # Check there is only stars with tag 0 (it should be right)
+    assert(all(ad['particle_family'] == 2))
+    assert(all(ad['particle_tag'] == 0))
