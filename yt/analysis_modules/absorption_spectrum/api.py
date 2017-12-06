d4b57d20b933c20d65edbbd5705169db99e3fbdf
Britton Smith <brittonsmith@gmail.com>
2017-12-06 13:39:21 -0800

    Adding deprecation warnings for LightRay and AbsorptionSpectrum and updating links to yt_attic.

--- yt/analysis_modules/absorption_spectrum/api.py
+++ yt/analysis_modules/absorption_spectrum/api.py
@@ -13,6 +13,14 @@ API for absorption_spectrum
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+from yt.funcs import issue_deprecation_warning
+
+issue_deprecation_warning(
+    "Development of the AbsorptionSpectrum module has been moved to the "
+    "Trident package. This version is deprecated and will be removed from yt "
+    "in a future release. See https://github.com/trident-project/trident "
+    "for further information.")
+
 from .absorption_spectrum import \
     AbsorptionSpectrum
 
