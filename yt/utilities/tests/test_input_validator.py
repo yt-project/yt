0ad9fbfac63eaf30f33eb68573cb8f1cf12bd9d3
git-abhishek <36498066+git-abhishek@users.noreply.github.com>
2018-06-12 03:26:54 -0400

    Input validation for disk

---
+++ yt/utilities/tests/test_input_validator.py
@@ -0,0 +1,15 @@
+from nose.tools import assert_raises, assert_equal
+
+from yt.testing import fake_random_ds
+from yt.utilities.exceptions import YTInvalidArgumentType
+
+
+def test_bad_disk_input():
+    # Fixes 1768
+    ds = fake_random_ds(16)
+    with assert_raises(YTInvalidArgumentType) as ex:
+        disk = ds.disk(ds.domain_center, [0, 0, 1], ds.domain_center, (20, 'kpc'))
+        disk['density']
+    desired = "One of the arguments to disk (selector) does not match" \
+              " its signature type."
+    assert_equal(str(ex.exception)[:50], desired[:50])
