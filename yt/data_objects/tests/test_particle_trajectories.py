96d42ca9764b7d4c3d1c026d6512ff55adb24c70
git-abhishek <36498066+git-abhishek@users.noreply.github.com>
2018-06-14 05:54:28 -0400

    removed real dataset from the tests

--- yt/data_objects/tests/test_particle_trajectories.py
+++ yt/data_objects/tests/test_particle_trajectories.py
@@ -1,14 +1,9 @@
-import glob
-import os
 import numpy as np
-from numpy.testing import \
-    assert_raises
+from numpy.testing import assert_raises
 
 from yt.config import ytcfg
 from yt.data_objects.time_series import DatasetSeries
-from yt.utilities.answer_testing.framework import \
-    requires_ds, \
-    GenericArrayTest
+from yt.utilities.answer_testing.framework import GenericArrayTest
 from yt.testing import fake_particle_ds
 from yt.utilities.exceptions import YTIllDefinedParticleData
 
@@ -21,12 +16,16 @@ data_path = ytcfg.get("yt", "test_data_dir")
 pfields = ["particle_position_x", "particle_position_y", "particle_position_z"]
 vfields = ["particle_velocity_x", "particle_velocity_y", "particle_velocity_z"]
 
-@requires_ds("Orbit/orbit_hdf5_chk_0000")
 def test_orbit_traj():
-    fields = ["particle_velocity_x", "particle_velocity_y", "particle_velocity_z"]
-    my_fns = glob.glob(os.path.join(data_path, "Orbit/orbit_hdf5_chk_00[0-9][0-9]"))
-    my_fns.sort()
-    ts = DatasetSeries(my_fns)
+    n_particles = 16 ** 2
+    ids = np.arange(n_particles, dtype=int) % (n_particles // 2)
+    data = {'particle_index': ids}
+    fields = ['particle_type', 'density', 'particle_position_x',
+              'particle_position_y', 'particle_position_z', 'particle_index']
+    units = ['dimensionless', 'g/cm**3', 'cm', 'cm', 'cm', '1']
+    ts = DatasetSeries([fake_particle_ds(fields=fields, negative=[False] * 6,
+                                         units=units, npart=n_particles,
+                                         data=data) for i in range(5)])
     ds = ts[0]
     traj = ts.particle_trajectories([1, 2], fields=fields, suppress_logging=True)
     for field in pfields+vfields:
@@ -34,15 +33,19 @@ def test_orbit_traj():
             return traj[field]
         yield GenericArrayTest(ds, field_func, args=[field])
 
-@requires_ds("enzo_tiny_cosmology/DD0000/DD0000")
 def test_etc_traj():
-    fields = ["particle_velocity_x", "particle_velocity_y", "particle_velocity_z"]
-    my_fns = glob.glob(os.path.join(data_path, "enzo_tiny_cosmology/DD000[0-9]/*.hierarchy"))
-    my_fns.sort()
-    ts = DatasetSeries(my_fns)
+    n_particles = 16**2
+    ids = np.arange(n_particles, dtype=int) % (n_particles // 2)
+    data = {'particle_index': ids}
+    fields = ['particle_type', 'density', 'particle_position_x',
+              'particle_position_y', 'particle_position_z', 'particle_index']
+    units = ['dimensionless', 'g/cm**3', 'cm', 'cm', 'cm', '1']
+    ts = DatasetSeries([fake_particle_ds(fields=fields, negative=[False]*6,
+                                         units=units, npart=n_particles,
+                                         data=data) for i in range(5)])
     ds = ts[0]
-    sp = ds.sphere("max", (0.5, "Mpc"))
-    indices = sp["particle_index"][sp["particle_type"] == 1][:5]
+    sp = ds.sphere(ds.domain_center / 2.0, (0.5, "Mpc"))
+    indices = sp["particle_index"][sp["particle_type"] <= 0.5][:5]
     traj = ts.particle_trajectories(indices, fields=fields, suppress_logging=True)
     traj.add_fields(["density"])
     for field in pfields+vfields+["density"]:
