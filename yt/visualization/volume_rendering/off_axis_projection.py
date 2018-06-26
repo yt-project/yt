Fixed bounds and Rotation Matrix

yt/visualization/volume_rendering/off_axis_projection.py
@@ -116,12 +116,12 @@ def off_axis_projection(data_source, center, normal_vector,
         ptype = data_source._sph_ptype
         buf = np.zeros((resolution[0], resolution[1]), dtype='float64')
 
-        x_min = data_source.domain_left_edge[0]
-        x_max = data_source.domain_right_edge[0]
-        y_min = data_source.domain_left_edge[1]
-        y_max = data_source.domain_right_edge[1]
-        z_min = data_source.domain_left_edge[2]
-        z_max = data_source.domain_right_edge[2]
+        x_min = center[0] - width[0] / 2
+        x_max = center[0] + width[0] / 2
+        y_min = center[1] - width[1] / 2
+        y_max = center[1] + width[1] / 2
+        z_min = center[2] - width[2] / 2
+        z_max = center[2] + width[2] / 2
 
         data_source = data_source_or_all(data_source)
 

yt/visualization/volume_rendering/tests/test_off_axis_SPH.py
@@ -14,15 +14,32 @@ def test_create_projection():
     resolution = (512, 512)
     normal_vector = np.array([0., 0., 1.])
     buf = OffAP.off_axis_projection(ds,
-                              [16000., 16000., 16000.],
+                              [32000., 32000., 32000.],
                               normal_vector,
                               [64000., 64000., 64000.],
                               resolution,
                               'particle_mass')
     np.seterr(divide='ignore')
-    plt.imsave('sph_img.png', np.log10(buf))
-    #plt.imsave('sph_img.png', buf)
+    #plt.imsave('sph_img.png', np.log10(buf))
+    plt.imsave('sph_img.png', buf)
 
-    
+def make_gif():
+    normal_vector = np.array([-2., 2., -5])
+    ds = yt.load('../../../../../Data/IsothermalCollapse/snap_505', unit_base={'UnitLength_in_cm': 5.0e16, 
+                 'UnitMass_in_g': 1.98992e33, 'UnitVelocity_in_cm_per_s': 46385.190}, 
+                 bounding_box = [[-3, 3], [-3, 3], [-3, 3]])    
+    resolution = (512, 512)
+    for i in range(160):
+        normal_vector[2] += 0.0625
+        buf = OffAP.off_axis_projection(ds,
+                              [0., 0., 0.],
+                              normal_vector,
+                              [6., 6., 6.],
+                              resolution,
+                              'particle_mass')
+        plt.imsave('images/img_' + str(i) + '.png',
+                   buf)
+        print('img_' + str(i) + '.png')
 if __name__ == "__main__":
     test_create_projection()
+    #make_gif()