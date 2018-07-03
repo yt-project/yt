5543e46b393dd147fb5a8859b2e5a72cd7c87e61
Corentin Cadiou <contact@cphyc.me>
2018-07-04 00:10:05 +0200

    Add test

--- yt/visualization/tests/test_filters.py
+++ yt/visualization/tests/test_filters.py
@@ -4,7 +4,8 @@ Tests for frb filters
 """
 
 from yt.testing import fake_amr_ds, requires_module
-
+import yt
+import numpy as np
 
 @requires_module("scipy")
 def test_white_noise_filter():
@@ -21,5 +22,20 @@ def test_gauss_beam_filter():
     ds = fake_amr_ds(fields=("density",), units=("g/cm**3",))
     p = ds.proj(("gas", "density"), "z")
     frb = p.to_frb((1, "unitary"), 64)
-    frb.apply_gauss_beam(nbeam=15, sigma=1.0)
+    frb.apply_gauss_beam(sigma=1.0)
     frb[("gas", "density")]
+
+
+@requires_module("scipy")
+def test_filter_wiring():
+    from scipy.ndimage import gaussian_filter
+    ds = fake_amr_ds(fields=("density", ))
+    p = yt.SlicePlot(ds, 'x', 'density')
+    data_before = p.frb['density'].value
+    sigma = 2
+    p.frb.apply_gauss_beam(sigma)
+    data_after = p.frb['density'].value
+
+    # Check that the frb yields the same result
+    assert np.allclose(gaussian_filter(data_before, sigma),
+                       data_after)
