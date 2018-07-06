a4db2c8834be57133a21133b6b51ed6adb53e1f8
Corentin Cadiou <contact@cphyc.me>
2018-07-06 17:50:55 +0200

    More thorough checks

--- yt/visualization/tests/test_filters.py
+++ yt/visualization/tests/test_filters.py
@@ -38,11 +38,29 @@ def test_filter_wiring():
     from scipy.ndimage import gaussian_filter
     ds = fake_amr_ds(fields=("density", ))
     p = yt.SlicePlot(ds, 'x', 'density')
-    data_before = p.frb['density'].value
+
+    frb1 = p.frb
+    data_orig = frb1['density'].value
+
     sigma = 2
     p.frb.apply_gauss_beam(sigma)
-    data_after = p.frb['density'].value
+    frb2 = p.frb
+    data_gauss = frb2['density'].value
 
-    # Check that the frb yields the same result
-    assert np.allclose(gaussian_filter(data_before, sigma),
-                       data_after)
+    p.frb.apply_white_noise()
+    frb3 = p.frb
+    data_white = frb3['density'].value
+
+    # We check the frb have changed
+    assert frb1 != frb2
+    assert frb1 != frb3
+    assert frb2 != frb3
+
+    # We check the resulting image are different each time
+    assert np.allclose(data_orig, data_gauss) is False
+    assert np.allclose(data_orig, data_white) is False
+    assert np.allclose(data_gauss, data_white) is False
+
+    # Check the gaussian filtering should is ok
+    assert np.allclose(gaussian_filter(data_orig, sigma),
+                       data_gauss)
