b0915f95a4a283d3c242bf46eb4b494af8a60579
zssherman <shermanx1991@gmail.com>
2018-08-10 15:45:52 -0500

    ENH: Added MapFeaturesCallback. New callback allows for the annotation of map features such as coastlines, gridlines and terrain.

--- yt/visualization/plot_modifications.py
+++ yt/visualization/plot_modifications.py
@@ -3150,3 +3150,167 @@ class CellEdgesCallback(PlotCallback):
         )
         plot._axes.set_xlim(xx0, xx1)
         plot._axes.set_ylim(yy0, yy1)
+
+class MapFeaturesCallback(PlotCallback):
+    """
+    Annotate map features.  This is done through a second call to add map
+    features to the radar cartopy plot.
+
+    Parameters
+    ----------
+    tile : instance
+        Cartopy image tile instance to be used in the plot. Example:
+        cartopy.io.img_tiles.StamenTerrain()
+    embelish : bool
+        When the second image is overlaid, it will have this level of alpha
+        transparency. Default is 1.0 (fully-opaque).
+    resolution : '10m', '50m', '110m'
+        Resolution of NaturalEarthFeatures to use. See Cartopy documentation
+        for details.
+    zorder : float
+        If a tile is applied, zorder will set the order of when the tile is
+        plotted. Lower numbers are plotted first. Default is 0.0.
+    lat_lines, lon_lines : array or None
+        Locations at which to draw latitude and longitude lines.
+        None will not draw latitude and longitude lines.
+
+    """
+    _type_name = "map_features"
+    _supported_geometries = ("cartesian")
+    def __init__(self, tile=None, embelish=False, resolution="110m",
+                 zorder=0.0, lat_lines=None, lon_lines=None):
+        PlotCallback.__init__(self)
+        self.tile = tile
+        self.embelish = embelish
+        self.resolution = resolution
+        self.zorder = zorder
+        self.lat_lines = lat_lines
+        self.lon_lines = lon_lines
+
+    def __call__(self, plot):
+        import cartopy
+        from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+        try:
+            import shapely.geometry as sgeom
+            from copy import copy
+            _LAMBERT_GRIDLINES = True
+        except ImportError:
+            _LAMBERT_GRIDLINES = False
+
+        if self.tile is not None:
+            plot._axes.add_image(self.tile, 8, zorder=self.zorder)
+
+        if self.embelish is True:
+            state_provinces = cartopy.feature.NaturalEarthFeature(
+                category="cultural",
+                name="admin_1_states_provinces_lines",
+                scale=self.resolution,
+                facecolor="None")
+            plot._axes.add_feature(state_provinces, edgecolor="gray")
+            plot._axes.coastlines(resolution=self.resolution)
+
+        if lat_lines and lon_lines is not None:
+            if plot._axes.projection in [cartopy.crs.PlateCarree(),
+                                         cartopy.crs.Mercator()]:
+
+                plot._axes.set_xticks(
+                    self.lon_lines, crs=cartopy.crs.PlateCarree())
+                plot._axes.set_yticks(
+                    self.lat_lines, crs=cartopy.crs.PlateCarree())
+                lon_formatter = LongitudeFormatter(zero_direction_label=True)
+                lat_formatter = LatitudeFormatter()
+                plot._axes.xaxis.set_major_formatter(lon_formatter)
+                plot._axes.yaxis.set_major_formatter(lat_formatter)
+                plot._axes.gridlines(xlocs=lon_lines, ylocs=lat_lines)
+            elif isinstance(ax.projection, cartopy.crs.LambertConformal):
+                plot.figure.canvas.draw()
+                plot_axes.gridlines(xlocs=lon_lines, ylocs=lat_lines)
+
+                # Label the end-points of the gridlines using the custom
+                # tick makers:
+                plot._axes.xaxis.set_major_formatter(
+                    cartopy.mpl.gridliner.LONGITUDE_FORMATTER)
+                plot._axes.yaxis.set_major_formatter(
+                    cartopy.mpl.gridliner.LATITUDE_FORMATTER)
+                if _LAMBERT_GRIDLINES:
+                    lambert_xticks(plot._axes, self.lon_lines)
+                    lambert_yticks(plot._axes, self.lat_lines)
+            else:
+                plot._axes.gridlines(
+                    xlocs=self.lon_lines, ylocs=self.lat_lines)
+
+    # These methods are a hack to allow gridlines when the projection is lambert
+    # http://nbviewer.jupyter.org/gist/ajdawson/dd536f786741e987ae4e
+
+    def find_side(ls, side):
+        """ Given a shapely LineString which is assumed to be rectangular,
+        return the line corresponding to a given side of the rectangle. """
+        minx, miny, maxx, maxy = ls.bounds
+        points = {'left': [(minx, miny), (minx, maxy)],
+                  'right': [(maxx, miny), (maxx, maxy)],
+                  'bottom': [(minx, miny), (maxx, miny)],
+                  'top': [(minx, maxy), (maxx, maxy)]}
+        return sgeom.LineString(points[side])
+
+    def lambert_xticks(ax, ticks):
+        """ Draw ticks on the bottom x-axis of a Lambert Conformal
+        projection. """
+        def te(xy):
+            return xy[0]
+
+        def lc(t, n, b):
+            return np.vstack((np.zeros(n) + t, np.linspace(b[2], b[3], n))).T
+
+        xticks, xticklabels = _lambert_ticks(ax, ticks, 'bottom', lc, te)
+        ax.xaxis.tick_bottom()
+        ax.set_xticks(xticks)
+        ax.set_xticklabels([ax.xaxis.get_major_formatter()(xtick) for
+                            xtick in xticklabels])
+
+    def lambert_yticks(ax, ticks):
+        """ Draw ticks on the left y-axis of a Lambert Conformal
+        projection. """
+        def te(xy):
+            return xy[1]
+
+        def lc(t, n, b):
+            return np.vstack((np.linspace(b[0], b[1], n), np.zeros(n) + t)).T
+
+        yticks, yticklabels = _lambert_ticks(ax, ticks, 'left', lc, te)
+        ax.yaxis.tick_left()
+        ax.set_yticks(yticks)
+        ax.set_yticklabels([ax.yaxis.get_major_formatter()(ytick) for
+                            ytick in yticklabels])
+
+    def _lambert_ticks(ax, ticks, tick_location, line_constructor,
+                       tick_extractor):
+        """ Get the tick locations and labels for a Lambert Conformal
+        projection. """
+        outline_patch = sgeom.LineString(
+            ax.outline_patch.get_path().vertices.tolist())
+        axis = find_side(outline_patch, tick_location)
+        n_steps = 30
+        extent = ax.get_extent(cartopy.crs.PlateCarree())
+        _ticks = []
+        for t in ticks:
+            xy = line_constructor(t, n_steps, extent)
+            proj_xyz = ax.projection.transform_points(
+                cartopy.crs.Geodetic(), xy[:, 0], xy[:, 1])
+            xyt = proj_xyz[..., :2]
+            ls = sgeom.LineString(xyt.tolist())
+            locs = axis.intersection(ls)
+            if not locs:
+                tick = [None]
+            else:
+                tick = tick_extractor(locs.xy)
+            _ticks.append(tick[0])
+        # Remove ticks that aren't visible:
+        ticklabels = copy(ticks)
+        while True:
+            try:
+                index = _ticks.index(None)
+            except ValueError:
+                break
+            _ticks.pop(index)
+            ticklabels = np.delete(ticklabels, index)
+        return _ticks, ticklabels
