2e2aed5d197ce5e5d24a8fd044f93a67a8dc112a
git-abhishek <36498066+git-abhishek@users.noreply.github.com>
2018-08-10 12:31:09 -0400

    Update test case to answer test to be run on Travis
    This test will test against real dataset on Travis

--- yt/visualization/tests/test_raw_field_slices.py
+++ yt/visualization/tests/test_raw_field_slices.py
@@ -12,6 +12,8 @@ Tests for making slices through raw fields
 #-----------------------------------------------------------------------------
 
 import yt
+from nose.plugins.attrib import attr
+from yt.testing import ANSWER_TEST_TAG
 from yt.utilities.answer_testing.framework import \
     requires_ds, \
     data_dir_load, \
@@ -46,9 +48,12 @@ _raw_field_names =  [('raw', 'Bx'),
                      ('raw', 'jx'),
                      ('raw', 'jy'),
                      ('raw', 'jz')]
+
+@attr(ANSWER_TEST_TAG)
 @requires_ds(raw_fields)
 def test_raw_field_slices():
     ds = data_dir_load(raw_fields)
     for field in _raw_field_names:
-        yield compare(ds, field, "answers_raw_%s" % field[1])
-
+        test = compare(ds, field, "answers_raw_%s" % field[1])
+        test.answer_name = "raw_field_slices"
+        yield test
