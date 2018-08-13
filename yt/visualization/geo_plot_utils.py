c205a41070c6ae672eae4f2c93dd4f2723b15d9e
Madicken Munk <madicken.munk@gmail.com>
2018-08-13 11:07:53 -0500

    add in remaining cartopy projections

--- yt/visualization/geo_plot_utils.py
+++ yt/visualization/geo_plot_utils.py
@@ -1,16 +1,29 @@
-def cartopy_importer(transform_name):
-        def _func(*args, **kwargs):
-            import cartopy.crs
-            return getattr(cartopy.crs, transform_name)(*args, **kwargs)
-        return _func
-
-
 valid_transforms={}
 
-for mpl_transform in ['PlateCarree', 'Mollweide', 'Orthographic', 'Robinson']:
-    valid_transforms[mpl_transform] = cartopy_importer(mpl_transform)
+transform_list = ['PlateCarree', 'LambertConformal', 'LabmbertCylindrical',
+                  'Mercator', 'Miller', 'Mollweide', 'Orthographic',
+                  'Robinson', 'Stereographic', 'TransverseMercator',
+                  'InterruptedGoodeHomolosine', 'RotatedPole', 'OGSB',
+                  'EuroPP', 'Geostationary', 'Gnomonic', 'NorthPolarStereo',
+                  'OSNI', 'SouthPolarStereo', 'AlbersEqualArea',
+                  'AzimuthalEquidistant', 'Sinusoidal', 'UTM',
+                  'NearsidePerspective', 'LambertAzimuthalEqualArea']
+
+def cartopy_importer(transform_name):
+    def _func(*args, **kwargs):
+        import cartopy.crs
+        return getattr(cartopy.crs, transform_name)(*args, **kwargs)
+    return _func
 
 def get_mpl_transform(mpl_proj):
+    # first check to see if the tranform dict is empty, if it is fill it with
+    # the cartopy functions
+    if not valid_transforms:
+        for mpl_transform in transform_list:
+            valid_transforms[mpl_transform] = cartopy_importer(mpl_transform)
+
+    # check to see if mpl_proj is a string or tuple, and construct args and
+    # kwargs to pass to cartopy function based on that.
     key = None
     if isinstance (mpl_proj, str):
         key = mpl_proj
