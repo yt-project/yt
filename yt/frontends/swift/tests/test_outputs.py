b9ea661c416f94489aa6fb7813859959594a85b4
ashkelly <a.j.kelly@durham.ac.uk>
2018-08-14 14:37:11 +0100

    fixed the swift dataset tests

--- yt/frontends/swift/tests/test_outputs.py
+++ yt/frontends/swift/tests/test_outputs.py
@@ -16,14 +16,15 @@ Swift frontend tests
 
 from yt import load
 
+import numpy as np
+
 from yt.testing import requires_file
 from yt.frontends.swift.api import SwiftDataset
 from yt.testing import assert_almost_equal
 from yt.utilities.on_demand_imports import _h5py as h5py
 
-
-keplerian_ring = "KeplerianRing/keplerian_ring_test_data.hdf5"
-EAGLE_6 = "EAGLE_6/eagle_00010.hdf5"
+keplerian_ring = "KeplerianRing/keplerian_ring_0020.hdf5"
+EAGLE_6 = "EAGLE_6/eagle_0005.hdf5"
 
 # Combined the tests for loading a file and ensuring the units have been
 # implemented correctly to save time on re-loading a dataset
@@ -32,53 +33,80 @@ def test_non_cosmo_dataset():
     ds = load(keplerian_ring)
     assert(type(ds) == SwiftDataset)
 
-    # now we have the file, lets test the units
     field = ('gas', 'density')
     ad = ds.all_data()
-    ytval = ad[field]
+    yt_density = ad[field]
+    yt_coords = ad[(field[0], 'position')]
 
     # load some data the old fashioned way
     fh = h5py.File(keplerian_ring, "r")
     part_data = fh['PartType0']
+
+    # set up a conversion factor by loading the unit mas and unit length in cm,
+    # and then converting to proper coordinates
     units = fh["Units"]
     units = dict(units.attrs)
-    conv_factor = float(units["Unit mass in cgs (U_M)"])
-    conv_factor /= float(units["Unit length in cgs (U_L)"])**3
-    h5val = part_data["Density"][:].astype("float64") * conv_factor
+    density_factor = float(units["Unit mass in cgs (U_M)"])
+    density_factor /= float(units["Unit length in cgs (U_L)"])**3
+
+    # now load the raw density and coordinates
+    raw_density = part_data["Density"][:].astype("float64") * density_factor
+    raw_coords = part_data["Coordinates"][:].astype("float64")
     fh.close()
 
+    # sort by the positions - yt often loads in a different order
+    ind_raw = np.lexsort((raw_coords[:, 2], raw_coords[:, 1],
+                          raw_coords[:, 0]))
+    ind_yt = np.lexsort((yt_coords[:, 2], yt_coords[:, 1], yt_coords[:, 0]))
+    raw_density = raw_density[ind_raw]
+    yt_density = yt_density[ind_yt]
+
     # make sure we are comparing fair units
-    assert(str(ytval.units) == 'g/cm**3')
+    assert(str(yt_density.units) == 'g/cm**3')
 
     # make sure the actual values are the same
-    assert_almost_equal(h5val, ytval.d)
-
-    return
+    assert_almost_equal(yt_density.d, raw_density)
 
 @requires_file(EAGLE_6)
 def test_cosmo_dataset():
     ds = load(EAGLE_6)
     assert(type(ds) == SwiftDataset)
 
-    # lets test the cosmology units
     field = ('gas', 'density')
     ad = ds.all_data()
-    ytval = ad[field]
+    yt_density = ad[field]
+    yt_coords = ad[(field[0], 'position')]
 
     # load some data the old fashioned way
-    fh = h5py.File(keplerian_ring, "r")
+    fh = h5py.File(EAGLE_6, "r")
     part_data = fh['PartType0']
+
+    # set up a conversion factor by loading the unit mas and unit length in cm,
+    # and then converting to proper coordinates
     units = fh["Units"]
     units = dict(units.attrs)
-    conv_factor = float(units["Unit mass in cgs (U_M)"])
-    conv_factor /= float(units["Unit length in cgs (U_L)"])**3
-    h5val = part_data["Density"][:].astype("float64") * conv_factor
+    density_factor = float(units["Unit mass in cgs (U_M)"])
+    density_factor /= float(units["Unit length in cgs (U_L)"])**3
+
+    # add the redshift factor
+    header = fh["Header"]
+    header = dict(header.attrs)
+    density_factor *= (1.0 + float(header["Redshift"]))**3
+
+    # now load the raw density and coordinates
+    raw_density = part_data["Density"][:].astype("float64") * density_factor
+    raw_coords = part_data["Coordinates"][:].astype("float64")
     fh.close()
 
+    # sort by the positions - yt often loads in a different order
+    ind_raw = np.lexsort((raw_coords[:, 2], raw_coords[:, 1],
+                          raw_coords[:, 0]))
+    ind_yt = np.lexsort((yt_coords[:, 2], yt_coords[:, 1], yt_coords[:, 0]))
+    raw_density = raw_density[ind_raw]
+    yt_density = yt_density[ind_yt]
+
     # make sure we are comparing fair units
-    assert(str(ytval.units) == 'g/cm**3')
+    assert(str(yt_density.units) == 'g/cm**3')
 
     # make sure the actual values are the same
-    assert_almost_equal(h5val, ytval.d)
-
-    return
+    assert_almost_equal(yt_density.d, raw_density)
