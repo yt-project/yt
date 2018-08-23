0a6db9e2e5d1dce142a546ff97b2c4ba66e295c0
Nathan Goldbaum <ngoldbau@illinois.edu>
2018-08-23 13:53:55 -0500

    use an explicit field type to get the smoothing length unit

--- yt/visualization/volume_rendering/off_axis_projection.py
+++ yt/visualization/volume_rendering/off_axis_projection.py
@@ -204,7 +204,7 @@ def off_axis_projection(data_source, center, normal_vector,
 
             # Assure that the path length unit is in the default length units
             # for the dataset by scaling the units of the smoothing length
-            path_length_unit = data_source.ds._get_field_info('smoothing_length').units
+            path_length_unit = data_source.ds._get_field_info((ptype, 'smoothing_length')).units
             path_length_unit = Unit(path_length_unit, registry=data_source.ds.unit_registry)
             default_path_length_unit = data_source.ds.unit_system['length']
             buf *= data_source.ds.quan(1, path_length_unit).in_units(default_path_length_unit)
