cc94f3fd24d5b1ca2ed4dbe6f2470208409a0e89
Nathan Goldbaum <ngoldbau@illinois.edu>
2018-08-28 09:10:33 -0400

    decorate test with requires_module

--- yt/data_objects/tests/test_glue.py
+++ yt/data_objects/tests/test_glue.py
@@ -1,11 +1,12 @@
 import nose
 
 from yt.config import ytcfg
-from yt.testing import fake_random_ds
+from yt.testing import fake_random_ds, requires_module
 
 def setup_func():
     ytcfg["yt", "__withintesting"] = "True"
 
+@requires_module('glue')
 @nose.with_setup(setup_func)
 def test_glue_data_object():
     ds = fake_random_ds(16)
