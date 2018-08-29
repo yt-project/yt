413b7c14861671150046319011277927454650f5
Corentin Cadiou <contact@cphyc.me>
2018-08-29 18:55:52 +0200

    Add test

--- yt/geometry/tests/test_particle_deposit.py
+++ yt/geometry/tests/test_particle_deposit.py
@@ -2,9 +2,14 @@ from yt.utilities.exceptions import \
     YTBoundsDefinitionError
 
 from yt.testing import \
-    fake_random_ds
+    fake_random_ds, requires_file
 from numpy.testing import \
     assert_raises
+import yt
+from numpy.random import RandomState
+from numpy.testing import assert_array_less
+import numpy as np
+
 
 def test_cic_deposit():
     ds = fake_random_ds(64, nprocs = 8, particles=64**3)
@@ -12,3 +17,25 @@ def test_cic_deposit():
             dims=[1, 800, 800])
     f = ("deposit", "all_cic")
     assert_raises(YTBoundsDefinitionError, my_reg.__getitem__, f)
+
+
+output_00080 = "output_00080/info_00080.txt"
+@requires_file(output_00080)
+def test_mesh_deposition():
+    ds = yt.load(output_00080)
+    ds.add_deposited_mesh_field(('index', 'x'), ptype='all')
+    ds.add_deposited_mesh_field(('index', 'dx'), ptype='all')
+
+    dx = ds.r['all', 'cell_index_dx']
+    x = ds.r['all', 'cell_index_x']
+    xpart = ds.r['all', 'particle_position_x']
+
+    dist = (x - xpart) / dx
+
+    upper = np.ones_like(dx) / 2
+
+    # Some particles are out of their domain, skip them
+    mask = np.isfinite(dist)
+
+    assert_array_less(dist[mask], upper[mask])
+    assert_array_less(-dist[mask], upper[mask])
