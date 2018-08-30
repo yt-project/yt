87bf4305321d9f8a6e4506c83081adc53007b59b
Corentin Cadiou <contact@cphyc.me>
2018-08-30 15:11:31 +0200

    Add failing test

--- yt/geometry/tests/test_particle_deposit.py
+++ yt/geometry/tests/test_particle_deposit.py
@@ -38,3 +38,28 @@ def test_mesh_deposition():
 
     assert_array_less(dist[mask], upper[mask])
     assert_array_less(-dist[mask], upper[mask])
+
+@requires_file(output_00080)
+def test_mesh_deposition_for_filtered_particles():
+    ds = yt.load(output_00080)
+
+    @yt.particle_filter(requires=['particle_position_x'], filtered_type='io')
+    def left(pfilter, data):
+        return data[(pfilter.filtered_type, 'particle_position_x')].to('code_length') < 0.5
+    ds.add_particle_filter('left')
+
+    for f in (('index', 'x'), ('index', 'dx'), ('gas', 'density')):
+        ds.add_deposited_mesh_field(f, ptype='io')
+        ds.add_deposited_mesh_field(f, ptype='left')
+
+    data_sources = (ds.all_data(), ds.box([0]*3, [0.1]*3))
+
+    def test_source(ptype, src):
+        # Test accessing
+        src[ptype, 'cell_index_x']
+        src[ptype, 'cell_index_dx']
+        src[ptype, 'cell_gas_density']
+
+    for ptype in ('io', 'left'):
+        for src in data_sources:
+            yield test_source, ptype, src
