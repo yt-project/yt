18178aad447b08416986281bee3972b91780f243
ashkelly <a.j.kelly@durham.ac.uk>
2018-09-04 16:05:31 +0100

    remove the debug line to raise field error

--- yt/fields/field_info_container.py
+++ yt/fields/field_info_container.py
@@ -61,7 +61,7 @@ class FieldInfoContainer(dict):
     extra_union_fields = ()
 
     def __init__(self, ds, field_list, slice_info = None):
-        self._show_field_errors =[('PartType0', 'vorticity')]
+        self._show_field_errors = []
         self.ds = ds
         # Now we start setting things up.
         self.field_list = field_list
