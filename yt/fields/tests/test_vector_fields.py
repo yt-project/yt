d8d371687a52aebe7242b068b0d9d5e676784387
Britton Smith <brittonsmith@gmail.com>
2018-09-11 16:26:19 -0700

    Adding different normal vectors to vector conversion tests.

--- yt/fields/tests/test_vector_fields.py
+++ yt/fields/tests/test_vector_fields.py
@@ -13,41 +13,57 @@ from yt.testing import \
     fake_random_ds
 from yt.units import km, s
 
+def random_unit_vector(prng):
+    v = prng.random_sample(3)
+    while (v == 0).all():
+        v = prng.random_sample(3)
+    return v / np.sqrt((v**2).sum())
+
 def test_vector_component_conversions():
-    for bv in [[0, 0, 0], [1e5, 1e5, 1e5]]:
-        ds = fake_random_ds(16)
+    prng = np.random.RandomState(8675309)
+    v_rand = np.power(10, prng.random_sample(3) * 5)
+    normals = [[1, 0, 0],
+               [0, 1, 0],
+               [0, 0, 1],
+               random_unit_vector(prng),
+               random_unit_vector(prng)]
 
-        ad = ds.all_data()
+    for bv in [[0, 0, 0], [1e5, 1e5, 1e5], v_rand]:
+        for normal in normals:
+            ds = fake_random_ds(16)
 
-        bulk = bv*km/s
+            ad = ds.all_data()
 
-        ad.set_field_parameter('bulk_velocity', bulk)
+            bulk = bv*km/s
 
-        vmag = ad['velocity_magnitude']
-        vmag_cart = np.sqrt(
-            (ad['velocity_x'] - bulk[0])**2 +
-            (ad['velocity_y'] - bulk[1])**2 +
-            (ad['velocity_z'] - bulk[2])**2
-        )
+            ad.set_field_parameter('bulk_velocity', bulk)
+            ad.set_field_parameter('normal', normal)
 
-        assert_allclose_units(vmag, vmag_cart)
+            vmag = ad['velocity_magnitude']
+            vmag_cart = np.sqrt(
+                (ad['velocity_x'] - bulk[0])**2 +
+                (ad['velocity_y'] - bulk[1])**2 +
+                (ad['velocity_z'] - bulk[2])**2
+            )
 
-        vmag_cyl = np.sqrt(
-            ad['velocity_cylindrical_radius']**2 +
-            ad['velocity_cylindrical_theta']**2 +
-            ad['velocity_cylindrical_z']**2
-        )
+            assert_allclose_units(vmag, vmag_cart)
 
-        assert_allclose_units(vmag, vmag_cyl)
+            vmag_cyl = np.sqrt(
+                ad['velocity_cylindrical_radius']**2 +
+                ad['velocity_cylindrical_theta']**2 +
+                ad['velocity_cylindrical_z']**2
+            )
 
-        vmag_sph = np.sqrt(
-            ad['velocity_spherical_radius']**2 +
-            ad['velocity_spherical_theta']**2 +
-            ad['velocity_spherical_phi']**2
-        )
+            assert_allclose_units(vmag, vmag_cyl)
 
-        assert_allclose_units(vmag, vmag_sph)
+            vmag_sph = np.sqrt(
+                ad['velocity_spherical_radius']**2 +
+                ad['velocity_spherical_theta']**2 +
+                ad['velocity_spherical_phi']**2
+            )
 
-        for d in 'xyz':
-            assert_allclose_units(ad['velocity_%s' % d] - bulk[0],
-                                  ad['relative_velocity_%s' % d])
+            assert_allclose_units(vmag, vmag_sph)
+
+            for i, d in enumerate('xyz'):
+                assert_allclose_units(ad['velocity_%s' % d] - bulk[i],
+                                      ad['relative_velocity_%s' % d])
