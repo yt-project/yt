cdc2c0d12130192136dc1b6714ef0efbd1f21528
Britton Smith <brittonsmith@gmail.com>
2018-09-11 18:55:28 -0700

    Generalize vector conversion test and add version using real data.

--- yt/fields/tests/test_vector_fields.py
+++ yt/fields/tests/test_vector_fields.py
@@ -10,8 +10,11 @@ import numpy as np
 
 from yt.testing import \
     assert_allclose_units, \
-    fake_random_ds
-from yt.units import km, s
+    fake_random_ds, \
+    requires_file
+from yt.utilities.answer_testing.framework import \
+    data_dir_load
+from yt.units import cm, s
 
 def random_unit_vector(prng):
     v = prng.random_sample(3)
@@ -19,51 +22,69 @@ def random_unit_vector(prng):
         v = prng.random_sample(3)
     return v / np.sqrt((v**2).sum())
 
-def test_vector_component_conversions():
+def random_velocity_vector(prng):
+    return 2e5 * prng.random_sample(3) - 1e5
+
+def compare_vector_conversions(data_source):
     prng = np.random.RandomState(8675309)
-    v_rand = np.power(10, prng.random_sample(3) * 5)
-    normals = [[1, 0, 0],
-               [0, 1, 0],
-               [0, 0, 1],
-               random_unit_vector(prng),
-               random_unit_vector(prng)]
+    normals = \
+      [[1, 0, 0],
+       [0, 1, 0],
+       [0, 0, 1]] + \
+       [random_unit_vector(prng) for i in range(2)]
+    bulk_velocities = \
+      [random_velocity_vector(prng) for i in range(2)]
 
-    for bv in [[0, 0, 0], [1e5, 1e5, 1e5], v_rand]:
+    for bv in bulk_velocities:
+        bulk_velocity = bv*cm/s
+        data_source.set_field_parameter(
+            'bulk_velocity', bulk_velocity)
+        data_source.clear_data()
+
+        vmag = data_source['velocity_magnitude']
+        vrad = data_source['velocity_spherical_radius']
+
         for normal in normals:
-            ds = fake_random_ds(16)
+            data_source.set_field_parameter('normal', normal)
+            data_source.clear_data()
 
-            ad = ds.all_data()
+            assert_allclose_units(
+                vrad, data_source['velocity_spherical_radius'])
 
-            bulk = bv*km/s
+            vmag_new = data_source['velocity_magnitude']
+            assert_allclose_units(vmag, vmag_new)
 
-            ad.set_field_parameter('bulk_velocity', bulk)
-            ad.set_field_parameter('normal', normal)
-
-            vmag = ad['velocity_magnitude']
             vmag_cart = np.sqrt(
-                (ad['velocity_x'] - bulk[0])**2 +
-                (ad['velocity_y'] - bulk[1])**2 +
-                (ad['velocity_z'] - bulk[2])**2
-            )
-
+                (data_source['velocity_x'] - bulk_velocity[0])**2 +
+                (data_source['velocity_y'] - bulk_velocity[1])**2 +
+                (data_source['velocity_z'] - bulk_velocity[2])**2)
             assert_allclose_units(vmag, vmag_cart)
 
             vmag_cyl = np.sqrt(
-                ad['velocity_cylindrical_radius']**2 +
-                ad['velocity_cylindrical_theta']**2 +
-                ad['velocity_cylindrical_z']**2
-            )
-
+                data_source['velocity_cylindrical_radius']**2 +
+                data_source['velocity_cylindrical_theta']**2 +
+                data_source['velocity_cylindrical_z']**2)
             assert_allclose_units(vmag, vmag_cyl)
 
             vmag_sph = np.sqrt(
-                ad['velocity_spherical_radius']**2 +
-                ad['velocity_spherical_theta']**2 +
-                ad['velocity_spherical_phi']**2
-            )
-
+                data_source['velocity_spherical_radius']**2 +
+                data_source['velocity_spherical_theta']**2 +
+                data_source['velocity_spherical_phi']**2)
             assert_allclose_units(vmag, vmag_sph)
 
             for i, d in enumerate('xyz'):
-                assert_allclose_units(ad['velocity_%s' % d] - bulk[i],
-                                      ad['relative_velocity_%s' % d])
+                assert_allclose_units(
+                    data_source['velocity_%s' % d] - bulk_velocity[i],
+                    data_source['relative_velocity_%s' % d])
+
+def test_vector_component_conversions_fake():
+    ds = fake_random_ds(16)
+    ad = ds.all_data()
+    compare_vector_conversions(ad)
+
+g30 = "IsolatedGalaxy/galaxy0030/galaxy0030"
+@requires_file(g30)
+def test_vector_component_conversions_real():
+    ds = data_dir_load(g30)
+    sp = ds.sphere(ds.domain_center, (10, "kpc"))
+    compare_vector_conversions(sp)
