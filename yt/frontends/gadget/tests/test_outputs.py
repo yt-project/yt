98862bb5ace5b31f7435f4a97f3b365174c88165
John ZuHone <jzuhone@gmail.com>
2018-09-19 22:39:08 -0400

    Add a test

--- yt/frontends/gadget/tests/test_outputs.py
+++ yt/frontends/gadget/tests/test_outputs.py
@@ -30,6 +30,7 @@ from yt.frontends.gadget.api import GadgetHDF5Dataset, GadgetDataset
 from yt.frontends.gadget.testing import fake_gadget_binary
 
 isothermal_h5 = "IsothermalCollapse/snap_505.hdf5"
+bullet_h5 = "ArepoBullet/snapshot_150.hdf5"
 isothermal_bin = "IsothermalCollapse/snap_505"
 BE_Gadget = "BigEndianGadgetBinary/BigEndianGadgetBinary"
 LE_SnapFormat2 = "Gadget3-snap-format2/Gadget3-snap-format2"
@@ -90,7 +91,7 @@ def test_gadget_hdf5():
 @requires_file(keplerian_ring)
 def test_non_cosmo_dataset():
     """
-    Non-cosmological datasets may not have the cosmological parametrs in the
+    Non-cosmological datasets may not have the cosmological parameters in the
     Header. The code should fall back gracefully when they are not present,
     with the Redshift set to 0.
     """
@@ -132,3 +133,21 @@ def test_bigendian_field_access():
     ds = data_dir_load(BE_Gadget)
     data = ds.all_data()
     data['Halo', 'Velocities']
+
+bullet_fields = OrderedDict(
+    [
+        (("gas", "density"), None),
+        (("gas", "temperature"), None),
+        (("gas", "temperature"), ('gas', 'density')),
+        (('gas', 'velocity_magnitude'), None)
+    ]
+)
+
+
+@requires_ds(bullet_h5)
+def test_arepo_bullet():
+    ds = data_dir_load(bullet_h5)
+    for test in sph_answer(ds, 'snapshot_150', 40193369, 
+                           bullet_fields):
+        test_arepo_bullet.__name__ = test.description
+        yield test
