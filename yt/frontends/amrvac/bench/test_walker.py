9a344414f6e27bb848839dea4bc0655494127fc5
Clément Robert <clement.robert@oca.eu>
2018-10-17 20:28:40 +0200

    add docstring to bench, resolve a bug a better decompose tests

--- yt/frontends/amrvac/bench/test_walker.py
+++ yt/frontends/amrvac/bench/test_walker.py
@@ -1,25 +1,41 @@
 import numpy as np
 from walker import main, bundle_grids
 
-def test_walker_simple():
-    level_seq = [1] * 6
-    ix_seq = [(1,1), (2,1), (1,2), (2,2), (1,3), (2,3)]
+def test_walker_donothing():
+    level_seq = [    1,     1,     1,     1,     1,     1]
+    ix_seq    = [(1,1), (2,1), (1,2), (2,2), (1,3), (2,3)]
     nblocks = np.array([2,3])
     patches = bundle_grids(levels=level_seq, ixs=ix_seq)
     res = main(patches, nblocks, ngrids=6)
     expected = patches
     assert deep_equality(res, expected)
 
-def test_walker_skip_levels():
-    level_seq = [    2,     2,     2,     3,     3,     3,     3]
-    ix_seq    = [(1,1), (1,2), (2,1), (3,3), (3,4), (4,3), (4,4)]
+def test_walker_missing_block():
+    level_seq = [    2,     2,     2,     2]
+    ix_seq    = [(1,1), (2,1), (1,2), (2,2)]
     nblocks = np.array([2,2])
+    patches = bundle_grids(level_seq, ix_seq)
+    res = main(patches, nblocks, ngrids=5)
+    expected = bundle_grids(
+        levels=[    1,     2,     2,     2,     2],
+        ixs=   [(1,1), (1,1), (2,1), (1,2), (2,2)]
+    )
+    assert deep_equality(res, expected)
+
+def test_walker_high_levels():
+    level_seq = [    1,     2,     3,     4,     4,     4,     4,     2]
+    ix_seq    = [(1,1), (2,1), (4,1), (7,3), (8,3), (7,4), (8,4), (2,2)]
+    nblocks = np.array([1,1])
     patches = bundle_grids(level_seq, ix_seq)
     res = main(patches, nblocks, ngrids=9)
     expected = bundle_grids(
-        levels=[1,2,2,2,2,3,3,3,3],
-        ixs=[(1,1), (1,1), (1,2), (2,1), (2,2), (3,3), (3,4), (4,3), (4,4)]
+        levels=[    1,     2,     3,     3,     4,     4,     4,     4,     2],
+        ixs=   [(1,1), (2,1), (4,1), (4,2), (7,3), (8,3), (7,4), (8,4), (2,2)]
     )
+    for r,e in zip(res, expected):
+        print(e)
+        print(r)
+        print()
     assert deep_equality(res, expected)
 
 def deep_equality(result, expected):
--- yt/frontends/amrvac/bench/walker.py
+++ yt/frontends/amrvac/bench/walker.py
@@ -1,3 +1,15 @@
+"""This is a prototype subproject
+
+GOAL: write a main() function that can fill in the gaps in the input
+data in terms of grids: amrvac .dat file doesn't mention a level L
+grid if it's been fully refined into (L+1) level grids, so we insert them as we go
+
+
+issues : this routine need an initial guess of the final number of grids (ngrids)
+this is feasible by hand on a small data set but I don't have a solution ready made for that.
+
+"""
+
 from math import ceil
 import numpy as np
 
@@ -16,11 +28,11 @@ def main(patches:list, nblocks:np.ndarray, ngrids:int):
     def xmin(grid:dict):
         """get the lower boundaries of a grid as a fraction of the domain width"""
         block_width = 1/nblocks
-        return (grid['ix']-1) / (nblocks * 2**grid['lvl'])
-    
+        return (grid['ix']-1) / (nblocks * 2**(grid['lvl']-1))
+
     def xmax(grid:dict):
         """get the upper boundaries of a grid as a fraction of the domain width"""
-        return grid['ix'] / (nblocks * 2**grid['lvl'])
+        return grid['ix'] / (nblocks * 2**(grid['lvl']-1))
 
     levels_out = [0]     * ngrids
     ixs_out    = [(0,0)] * ngrids
@@ -35,8 +47,14 @@ def main(patches:list, nblocks:np.ndarray, ngrids:int):
             current_level += 1
             igrid += 1
         if patch['lvl'] == current_level+1:
-            out_of_bounds = np.any(xmin(patch) < xmin(grids[igrid])) or np.any(xmax(patch) > xmax(grids[igrid]))
-            if out_of_bounds:
+            out_of_bounds_inf = np.any(xmin(patch) < xmin(grids[igrid-1]))
+            out_of_bounds_sup = np.any(xmax(patch) > xmax(grids[igrid-1]))
+            print(out_of_bounds_inf, out_of_bounds_sup)#; return
+            print(patch, grids[igrid-1])
+            print(xmin(patch),  xmin(grids[igrid-1]))
+            print(xmax(patch),  xmax(grids[igrid-1]))
+            #return
+            if out_of_bounds_inf or out_of_bounds_sup :
                 grids[igrid] = {'lvl': current_level, 'ix': get_englobante(patch['ix'])}
                 current_level += 1
                 igrid += 1
