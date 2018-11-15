f001a389c74bf498db37443a85c5217d53d9240d
Suoqing Ji <jisuoqing@gmail.com>
2018-11-14 23:00:55 -0800

    remove noise from line integral convolution in zero-value regions

--- yt/utilities/lib/line_integral_convolution.pyx
+++ yt/utilities/lib/line_integral_convolution.pyx
@@ -104,5 +104,8 @@ def line_integral_convolution_2d(
                         &x, &y, &fx, &fy, w, h)
                 l-=1
                 result[i,j] += kernel[l]*texture[x,y]
-                    
+
+            if vectors[i,j,0]==0 and vectors[i,j,1]==0:
+                result[i,j] = 0.
+
     return result
