3e815266bd709fbf042f2cec792a246bedd59766
Britton Smith <brittonsmith@gmail.com>
2018-12-05 11:57:32 +0900

    Add some field aliases.

--- yt/frontends/ytree/fields.py
+++ yt/frontends/ytree/fields.py
@@ -17,10 +17,19 @@ ytree-specific fields
 from yt.fields.field_info_container import \
     FieldInfoContainer
 
+p_units = 'unitary'
+v_units = 'km/s'
 
 class YTreeFieldInfo(FieldInfoContainer):
     known_other_fields = (
     )
 
     known_particle_fields = (
+        ("position_x", (p_units, ('halos', 'particle_position_x'), None)),
+        ("position_y", (p_units, ('halos', 'particle_position_y'), None)),
+        ("position_z", (p_units, ('halos', 'particle_position_z'), None)),
+        ("velocity_x", (v_units, ('halos', 'particle_velocity_x'), None)),
+        ("velocity_y", (v_units, ('halos', 'particle_velocity_y'), None)),
+        ("velocity_z", (v_units, ('halos', 'particle_velocity_z'), None)),
+        ("mass", ('Msun', ('halos', 'particle_mass'), None)),
     )
