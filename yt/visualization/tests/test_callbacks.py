d5711a0b3b09f13b34d95cc12f74a07ccc1ea8ae
Suoqing Ji <jisuoqing@gmail.com>
2018-12-10 17:33:10 -0800

    fix typo

--- yt/visualization/tests/test_callbacks.py
+++ yt/visualization/tests/test_callbacks.py
@@ -374,7 +374,7 @@ def test_velocity_callback():
     with _cleanup_fname() as prefix:
         ds = load(cyl_2d)
         slc = SlicePlot(ds, "theta", "density")
-        slc.annotate_magnetic_field()
+        slc.annotate_velocity()
         assert_fname(slc.save(prefix)[0])
 
     with _cleanup_fname() as prefix:
