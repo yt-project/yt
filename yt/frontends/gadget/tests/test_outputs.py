c420bfe6ddb82901aa7f408e0267a4aca07af50b
Nathan Goldbaum <ngoldbau@illinois.edu>
2019-02-22 12:19:38 -0600

    backporting #2125

--- yt/frontends/gadget/tests/test_outputs.py
+++ yt/frontends/gadget/tests/test_outputs.py
@@ -17,6 +17,8 @@ Gadget frontend tests
 from collections import OrderedDict
 from itertools import product
 import os
+import shutil
+import tempfile
 
 import yt
 from yt.testing import requires_file
@@ -51,16 +53,24 @@ iso_kwargs = dict(bounding_box=[[-3, 3], [-3, 3], [-3, 3]])
 
 def test_gadget_binary():
     header_specs = ['default', 'default+pad32', ['default', 'pad32']]
+    curdir = os.getcwd()
+    tmpdir = tempfile.mkdtemp()
     for header_spec, endian, fmt in product(header_specs, '<>', [1, 2]):
-        fake_snap = fake_gadget_binary(
-            header_spec=header_spec,
-            endian=endian,
-            fmt=fmt
-        )
+        try:
+            fake_snap = fake_gadget_binary(
+                header_spec=header_spec,
+                endian=endian,
+                fmt=fmt
+            )
+        except FileNotFoundError:
+            # sometimes this happens for mysterious reasons
+            pass
         ds = yt.load(fake_snap, header_spec=header_spec)
         assert isinstance(ds, GadgetDataset)
         ds.field_list
         os.remove(fake_snap)
+        os.chdir(curdir)
+        shutil.rmtree(tmpdir)
 
 
 @requires_file(isothermal_h5)
