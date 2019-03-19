307a74a59c5abd7250e8a567e47d1385a7deed99
Cameron Hummels <chummels@gmail.com>
2019-03-19 10:45:22 -0700

    Changing profile default weight field from 'cell_mass' to ('gas', 'mass')

--- yt/data_objects/profiles.py
+++ yt/data_objects/profiles.py
@@ -332,18 +332,18 @@ class ProfileND(ParallelAnalysisInterface):
         >>> ds = yt.load("enzo_tiny_cosmology/DD0046/DD0046")
         >>> ad = ds.all_data()
         >>> profile = yt.create_profile(ad, [("gas", "density"), ("gas", "temperature")],
-        ...                            ("gas", "cell_mass"), weight_field=None,
+        ...                            ("gas", "mass"), weight_field=None,
         ...                             n_bins=(128, 128))
         >>> fn = profile.save_as_dataset()
         >>> prof_ds = yt.load(fn)
-        >>> print (prof_ds.data[("gas", "cell_mass")])
+        >>> print (prof_ds.data[("gas", "mass")])
         (128, 128)
         >>> print (prof_ds.data[("index", "x")].shape) # x bins as 1D array
         (128,)
         >>> print (prof_ds.data[("gas", "density")]) # x bins as 2D array
         (128, 128)
         >>> p = yt.PhasePlot(prof_ds.data, ("gas", "density"), ("gas", "temperature"),
-        ...                  ("gas", "cell_mass"), weight_field=None)
+        ...                  ("gas", "mass"), weight_field=None)
         >>> p.save()
 
         """
@@ -1175,7 +1175,7 @@ def create_profile(
     extrema=None,
     logs=None,
     units=None,
-    weight_field=("gas", "cell_mass"),
+    weight_field=("gas", "mass"),
     accumulation=False,
     fractional=False,
     deposition="ngp",
@@ -1213,7 +1213,7 @@ def create_profile(
     weight_field : str or tuple field identifier
         The weight field for computing weighted average for the profile
         values.  If None, the profile values are sums of the data in
-        each bin. Defaults to "cell_mass".
+        each bin. Defaults to ('gas', 'mass').
     accumulation : bool or list of bools
         If True, the profile values for a bin n are the cumulative sum of
         all the values from bin 0 to n.  If -True, the sum is reversed so
