fix magnetic field units test

yt/data_objects/data_containers.py
@@ -130,10 +130,14 @@ class YTDataContainer(object):
         self.ds.objects.append(weakref.proxy(self))
         mylog.debug("Appending object to %s (type: %s)", self.ds, type(self))
         self.field_data = YTFieldData()
+        if self.ds.unit_system.has_current_mks:
+            mag_unit = "T"
+        else:
+            mag_unit = "G"
         self._default_field_parameters = {
             'center': self.ds.arr(np.zeros(3, dtype='float64'), 'cm'),
             'bulk_velocity': self.ds.arr(np.zeros(3, dtype='float64'), 'cm/s'),
-            'bulk_magnetic_field': self.ds.arr(np.zeros(3, dtype='float64'), 'G'),
+            'bulk_magnetic_field': self.ds.arr(np.zeros(3, dtype='float64'), mag_unit),
             'normal': self.ds.arr([0.0, 0.0, 1.0], ''),
         }
         if field_parameters is None: field_parameters = {}

yt/fields/tests/test_magnetic_fields.py
@@ -30,9 +30,9 @@ def test_magnetic_fields():
     dd2 = ds2.all_data()
     dd3 = ds3.all_data()
 
-    assert ds1.fields.gas.magnetic_field_strength.units == "gauss"
-    assert ds1.fields.gas.magnetic_field_poloidal.units == "gauss"
-    assert ds1.fields.gas.magnetic_field_toroidal.units == "gauss"
+    assert ds1.fields.gas.magnetic_field_strength.units == "G"
+    assert ds1.fields.gas.magnetic_field_poloidal.units == "G"
+    assert ds1.fields.gas.magnetic_field_toroidal.units == "G"
     assert ds2.fields.gas.magnetic_field_strength.units == "T"
     assert ds2.fields.gas.magnetic_field_poloidal.units == "T"
     assert ds2.fields.gas.magnetic_field_toroidal.units == "T"
@@ -52,7 +52,7 @@ def test_magnetic_fields():
 
     emag3 = (dd3["magnetic_field_x"]**2 +
              dd3["magnetic_field_y"]**2 +
-             dd3["magnetic_field_z"]**2)/(8.0*np.pi)
+             dd3["magnetic_field_z"]**2)/(2.0*mu_0)
     emag3.convert_to_units("code_pressure")
 
     assert_almost_equal(emag1, dd1["magnetic_energy"])