4e8c5fc38287986a9112a3dbd3ab09c0e123fbf2
John ZuHone <jzuhone@gmail.com>
2019-03-25 19:57:22 -0400

    Responding to Britton's suggestions

--- yt/frontends/arepo/fields.py
+++ yt/frontends/arepo/fields.py
@@ -1,3 +1,11 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 from yt.frontends.gadget.api import GadgetFieldInfo
 
 class ArepoFieldInfo(GadgetFieldInfo):
--- yt/frontends/arepo/tests/test_outputs.py
+++ yt/frontends/arepo/tests/test_outputs.py
@@ -7,7 +7,7 @@ Arepo frontend tests
 """
 
 #-----------------------------------------------------------------------------
-# Copyright (c) 2015, yt Development Team.
+# Copyright (c) yt Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
@@ -24,11 +24,6 @@ from yt.frontends.arepo.api import ArepoHDF5Dataset
 
 bullet_h5 = "ArepoBullet/snapshot_150.hdf5"
 
-# py2/py3 compat
-try:
-    FileNotFoundError
-except NameError:
-    FileNotFoundError = IOError
 
 @requires_file(bullet_h5)
 def test_arepo_hdf5():
