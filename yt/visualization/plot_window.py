73bcb86bf78a5f1871592dec2b72d890891dac75
Michael Zingale <michael.zingale@stonybrook.edu>
2019-04-23 09:22:12 -0400

    turn on mathtext for a plot_window colorbar

--- yt/visualization/plot_window.py
+++ yt/visualization/plot_window.py
@@ -1788,6 +1788,7 @@ class WindowPlotMPL(ImagePlotMPL):
         self.image.axes.yaxis.set_major_formatter(formatter)
         if cbname == 'linear':
             self.cb.formatter.set_scientific(True)
+            self.cb.formatter.set_useMathText(True)
             self.cb.formatter.set_powerlimits((-2, 3))
             self.cb.update_ticks()
 
