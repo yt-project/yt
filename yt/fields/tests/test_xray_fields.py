65b7d7fdf0fbfff3b2875e4eb8e48a23650546a2
John ZuHone <jzuhone@gmail.com>
2019-04-23 09:33:50 -0400

    Fix whitespace

--- yt/fields/tests/test_xray_fields.py
+++ yt/fields/tests/test_xray_fields.py
@@ -4,9 +4,11 @@ from yt.utilities.answer_testing.framework import \
     requires_ds, can_run_ds, data_dir_load, \
     ProjectionValuesTest, FieldValuesTest
 
+
 def setup():
     from yt.config import ytcfg
     ytcfg["yt","__withintesting"] = "True"
+
 
 def check_xray_fields(ds_fn, fields):
     if not can_run_ds(ds_fn): return
@@ -14,21 +16,25 @@ def check_xray_fields(ds_fn, fields):
     for field in fields:
         for dobj_name in dso:
             for axis in [0, 1, 2]:
-                yield ProjectionValuesTest(ds_fn, axis, field, 
+                yield ProjectionValuesTest(ds_fn, axis, field,
                                            None, dobj_name)
             yield FieldValuesTest(ds_fn, field, dobj_name)
 
+
 sloshing = "GasSloshingLowRes/sloshing_low_res_hdf5_plt_cnt_0300"
+d9p = "D9p_500/10MpcBox_HartGal_csf_a0.500.d"
+
+
 @requires_ds(sloshing, big_data=True)
 def test_sloshing_apec():
     ds = data_dir_load(sloshing)
-    fields = add_xray_emissivity_field(ds, 0.5, 7.0, table_type="apec", 
+    fields = add_xray_emissivity_field(ds, 0.5, 7.0, table_type="apec",
                                        metallicity=0.3)
     for test in check_xray_fields(ds, fields):
         test_sloshing_apec.__name__ = test.description
         yield test
 
-d9p = "D9p_500/10MpcBox_HartGal_csf_a0.500.d"
+
 @requires_ds(d9p, big_data=True)
 def test_d9p_cloudy():
     ds = data_dir_load(d9p)
