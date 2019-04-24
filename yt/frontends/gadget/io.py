b095090a76d46d2f147cf491bb6e33b087899332
John ZuHone <jzuhone@gmail.com>
2019-04-24 17:02:40 -0400

    Move this in here because it's cleaner

--- yt/frontends/gadget/io.py
+++ yt/frontends/gadget/io.py
@@ -128,7 +128,6 @@ class IOHandlerGadgetHDF5(IOHandlerSPH):
                 if data_file.total_particles[ptype] == 0:
                     continue
                 g = f["/%s" % ptype]
-                hsmls = None
                 if getattr(selector, 'is_all_data', False):
                     mask = slice(None, None, None)
                     mask_sum = ei-si
