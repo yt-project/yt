bf7681c06f640a40d931cce60522af23e7f73c63
Matthew Turk <matthewturk@gmail.com>
2019-07-12 12:08:15 -0500

    fixing indent

--- yt/__init__.py
+++ yt/__init__.py
@@ -18,7 +18,7 @@ yt is a toolkit for analyzing and visualizing volumetric data.
 
 import sys
 if sys.version_info[0] < 3:
-        raise Exception("Python 2 no longer supported.  Please install Python 3 for use with yt.")
+    raise Exception("Python 2 no longer supported.  Please install Python 3 for use with yt.")
 
 __version__ = "4.0.dev0"
 
