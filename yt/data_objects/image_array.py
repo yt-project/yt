060206ab110b67ab5b2bfd25c7ff7591dd144831
Britton Smith <brittonsmith@gmail.com>
2019-07-16 14:05:23 +0100

    Put deprecated kwarg at the end to avoid issues with kwargs being passed as args.

--- yt/data_objects/image_array.py
+++ yt/data_objects/image_array.py
@@ -75,8 +75,8 @@ class ImageArray(YTArray):
     Numpy ndarray documentation appended:
 
     """
-    def __new__(cls, input_array, units=None, input_units=None, registry=None, info=None,
-                bypass_validation=False):
+    def __new__(cls, input_array, units=None, registry=None, info=None,
+                bypass_validation=False, input_units=None):
         if input_units is not None:
             warnings.warn("'input_units' is deprecated. Please use 'units'.")
             units = input_units
