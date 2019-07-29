4328c369a49082c3a8989abcbe99aeb33b8587b9
John ZuHone <jzuhone@gmail.com>
2019-07-29 08:13:08 -0400

    Tests  for line of sight velocity

--- yt/fields/tests/test_vector_fields.py
+++ yt/fields/tests/test_vector_fields.py
@@ -69,6 +69,22 @@ def compare_vector_conversions(data_source):
                     data_source['velocity_%s' % d] - bulk_velocity[i],
                     data_source['relative_velocity_%s' % d])
 
+        for i, ax in enumerate('xyz'):
+            data_source.set_field_parameter("axis", i)
+            data_source.clear_data()
+            assert_allclose_units(data_source["velocity_los"], 
+                                  data_source["relative_velocity_%s" % ax])
+
+        data_source.clear_data()
+        ax = [0.1, 0.2, -0.3]
+        data_source.set_field_parameter("axis", ax)
+        ax /= np.sqrt(np.dot(ax,ax))
+        vlos = data_source["relative_velocity_x"]*ax[0]
+        vlos += data_source["relative_velocity_y"]*ax[1]
+        vlos += data_source["relative_velocity_z"]*ax[2]
+        assert_allclose_units(data_source["velocity_los"], vlos)
+
+
 def test_vector_component_conversions_fake():
     ds = fake_random_ds(16)
     ad = ds.all_data()
