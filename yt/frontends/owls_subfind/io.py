Added the conftest file for owls_subfind. In io.py for owls_subfind there was a bug when trying to use np.clip on a YTArray, so I fixed it by first stripping out the array's units, performing the clipping, and then adding the units back in.

yt/frontends/owls_subfind/io.py
@@ -132,8 +132,12 @@ class IOHandlerOWLSSubfindHDF5(BaseIOHandler):
                 # These are 32 bit numbers, so we give a little lee-way.
                 # Otherwise, for big sets of particles, we often will bump into the
                 # domain edges.  This helps alleviate that.
-                np.clip(pos, self.ds.domain_left_edge + dx,
-                             self.ds.domain_right_edge - dx, pos)
+                # Using np.clip on a YTArray currently doesn't work, so we
+                # first strip out the units and then add them back after
+                # clipping
+                pos_clipped = np.clip(pos.v, (self.ds.domain_left_edge + dx).v,
+                             (self.ds.domain_right_edge - dx).v)
+                pos = data_file.ds.arr(pos_clipped, "code_length")
                 if np.any(pos.min(axis=0) < self.ds.domain_left_edge) or \
                    np.any(pos.max(axis=0) > self.ds.domain_right_edge):
                     raise YTDomainOverflow(pos.min(axis=0),