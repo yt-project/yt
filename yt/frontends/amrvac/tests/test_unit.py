9e7633059ead2cc6acb4bb5dcc9005eff6d08536
Clément Robert <cr52@protonmail.com>
2019-09-02 17:07:29 +0200

    first pass

--- yt/frontends/amrvac/tests/test_unit.py
+++ yt/frontends/amrvac/tests/test_unit.py
@@ -1,32 +1,23 @@
 import sys
-if sys.version.startswith("2"):
-    import pathlib2 as pathlib
-else:
-    import pathlib
-import yt
 import numpy as np
-import unittest
 
+import yt
 from yt.utilities.parameter_file_storage import output_type_registry
 from yt.extern.six.moves import builtins
 
-test_dir = pathlib.Path(__file__).parent.resolve()
+# todo : add regression tests (see enzo)
+# todo : add file names
+sample_datasets = dict(
+    blastwave_cartesian_3D = ...,
+    blastwave_polar_2D = ...,
+    blastwave_cylindrical_2D = ...,
+    blastwave_cylindrical_3D = ...,
+    kelvin_helmoltz_cartesian_2D = ...
+}
 
-
-sample_datasets = [str(test_dir/"data"/s) for s in [
-    "blast_wave_Cartesian_3D_MHD/bw_3d0003.dat", # <- reference
-    #"misc/3d_k2pi_1MK0283.dat",
-    # 2D files
-    "blast_wave_Cartesian_2D_HD/bw_2d0003.dat",
-    "Kelvin_Helmoltz_2D/KH2proba0010.dat"
-]]
-
-
-
-class amrvac_unit_tests(unittest.TestCase):
-
+class AMRVAC_Frontend_Tests:
     def test_frontends_contents(self):
-        """Check that our frontend is correctly being registered."""
+        """Check that the frontend is correctly registered."""
         self.assertTrue("amrvac" in dir(yt.frontends))
 
     def test_output_type_registry(self):
@@ -34,26 +25,23 @@ class amrvac_unit_tests(unittest.TestCase):
         keys = list(output_type_registry.keys())
         self.assertTrue("AMRVACDataset" in keys)
 
-    def test_is_amrvac_valid(self):
-        """Check that our format validation fonction works at least on example files"""
-        for file in sample_datasets:
-            self.assertTrue(pathlib.Path(file).is_file())
-        self.assertTrue(output_type_registry["AMRVACDataset"]._is_valid(file))
+    def test_is_valid(self):
+        """Check that validation fonction works at least on example files."""
+        for file in sample_datasets.values():
+            self.assertTrue(output_type_registry["AMRVACDataset"]._is_valid(file))
 
     def test_load_amrvac(self):
-        """Check that that data is amrvac-formated"""
-        for file in sample_datasets:
+        """Check that that data is marked amrvac-formated"""
+        for file in sample_datasets.values():
             ds = yt.load(file)
             self.assertTrue(isinstance(ds, yt.frontends.amrvac.AMRVACDataset))
 
-
-
-    # ========== TESTS FOR DATA LOADING AND INSPECTION ==========
+class AMRVAC_IO_Tests:
     def test_print_stats(self):
         """Check that print_stats() executes normally"""
         for file in sample_datasets:
             ds = yt.load(file)
-            self.assertTrue(ds.print_stats() is None)
+            ds.print_stats()
 
     def test_fields(self):
         """Check for fields in .dat file"""
@@ -67,17 +55,16 @@ class amrvac_unit_tests(unittest.TestCase):
                 for n in range(2, ds.dimensionality):
                     self.assertTrue("b%d" % n in field_labels)
 
-    # ========== TEST FOR DATA READING ==========
+    # data reading
     def test_get_data(self):
-        for file in sample_datasets:
+        for file in sample_datasets.values():
             ds = yt.load(file)
             ad = ds.all_data()
             data = ad.get_data()
 
     def test_grid_dataread(self):
-        for file in sample_datasets:
+        for file in sample_datasets.values():
             ds = yt.load(file)
-
             grids = ds.index.grids
             # select random grid
             g = grids[0]
@@ -86,26 +73,28 @@ class amrvac_unit_tests(unittest.TestCase):
             self.assertTrue(isinstance(rho, yt.units.yt_array.YTArray))
 
     def test_dataread_all(self):
-        for file in sample_datasets:
+        for file in sample_datasets.values():
             ds = yt.load(file)
-
             ad = ds.all_data()
             self.assertTrue(isinstance(ad['rho'], yt.units.yt_array.YTArray))
 
-class PlotTests(unittest.TestCase):
+
+
+class AMRVAC_Plot_Tests(unittest.TestCase):
     def test_projection(self):
-        for file in sample_datasets:
+        """"Check that no error is raised"""
+        for file in sample_datasets.values():
             ds = yt.load(file)
             if ds.dimensionality == 3:
                 p = yt.ProjectionPlot(ds, 'x', 'density')
 
     def test_slice(self):
-        ds = yt.load(sample_datasets[0]) # <- blastwave 3D snapshot
+        """"Check that no error is raised"""
+        ds = yt.load(sample_datasets["blastwave_cartesian_3D"])
         p = yt.SlicePlot(ds, 'x', 'density')
 
 
-
-ds = yt.load(sample_datasets[0]) # <- blastwave 3D snapshot
+ds = yt.load(sample_datasets["blastwave_cartesian_3D"])
 class unit_tests_3Dblast(unittest.TestCase):
     def test_domain_size(self):
         """Check for correct box size, see bw_3d.par"""
