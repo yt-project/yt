b6be1d4d5724fadcf8626ecc6a7883026abb8c2d
Jared <jcoughl3@illinois.edu>
2019-09-04 15:37:32 -0500

    Updated answer tests in test_raw_field_slices.py.

--- yt/visualization/tests/test_raw_field_slices.py
+++ yt/visualization/tests/test_raw_field_slices.py
@@ -12,29 +12,12 @@ Tests for making slices through raw fields
 #-----------------------------------------------------------------------------
 
 import yt
-from yt.utilities.answer_testing.framework import \
-    requires_ds, \
-    data_dir_load, \
-    GenericImageTest
-
+from yt.utilities.answer_testing import utils
 
 def setup():
     """Test specific setup."""
     from yt.config import ytcfg
     ytcfg["yt", "__withintesting"] = "True"
-
-def compare(ds, field, test_prefix, decimals=12):
-    def slice_image(filename_prefix):
-        sl = yt.SlicePlot(ds, 'z', field)
-        sl.set_log('all', False)
-        image_file = sl.save(filename_prefix)
-        return image_file
-
-    slice_image.__name__ = "slice_{}".format(test_prefix)
-    test = GenericImageTest(ds, slice_image, decimals)
-    test.prefix = test_prefix
-    return test
-
 
 raw_fields = "Laser/plt00015"
 _raw_field_names =  [('raw', 'Bx'),
@@ -46,9 +29,17 @@ _raw_field_names =  [('raw', 'Bx'),
                      ('raw', 'jx'),
                      ('raw', 'jy'),
                      ('raw', 'jz')]
+
+@pytest.mark.skipif(not pytest.config.getvalue('--with-answer-testing'),
+    reason="--with-answer-testing not set.")
+@pytest.mark.usefixtures('temp_dir')
 @requires_ds(raw_fields)
 def test_raw_field_slices():
-    ds = data_dir_load(raw_fields)
+    ds = utils.data_dir_load(raw_fields)
     for field in _raw_field_names:
-        yield compare(ds, field, "answers_raw_%s" % field[1])
-
+        sl = yt.SlicePlot(ds, 'z', field)
+        sl.set_log('all', False)
+        image_file = sl.save("slice_answers_raw_{}".format(field[1]))
+        gi_hd += self.generic_image_test(ds, image_file)
+    hashes = {'raw-field-slices' : utils.generate_hash(gi_hd)}
+    utils.handle_hashes(self.save_dir, 'raw-field_slices', hashes, self.answer_store) 
