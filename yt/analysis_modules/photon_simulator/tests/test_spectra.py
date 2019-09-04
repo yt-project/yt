3e26b13528ff98b4dee5cfe76fb5ae360509d8ee
Jared <jcoughl3@illinois.edu>
2019-09-04 16:46:37 -0500

    Updated the answer tests in test_spectra.py.

--- yt/analysis_modules/photon_simulator/tests/test_spectra.py
+++ yt/analysis_modules/photon_simulator/tests/test_spectra.py
@@ -1,9 +1,11 @@
+import pytest
+
 from yt.analysis_modules.photon_simulator.api import \
     TableApecModel, XSpecThermalModel
-from yt.testing import requires_module, fake_random_ds
-from yt.utilities.answer_testing.framework import \
-    GenericArrayTest
 from yt.config import ytcfg
+from yt.testing import requires_module, fake_random_ds
+import yt.utilities.answer_testing.framework as fw
+from yt.utilities.answer_testing import utils
 
 def setup():
     ytcfg["yt", "__withintesting"] = "True"
@@ -12,33 +14,30 @@ xray_data_dir = ytcfg.get("yt", "xray_data_dir")
 
 ds = fake_random_ds(64)
 
-@requires_module("xspec")
-@requires_module("astropy")
-def test_apec():
-
-    settings = {"APECROOT":xray_data_dir+"/apec_v2.0.2"}
-    xmod = XSpecThermalModel("apec", 0.1, 10.0, 10000, thermal_broad=True,
-                             settings=settings)
-    xmod.prepare_spectrum(0.2)
-
-    xcspec, xmspec = xmod.get_spectrum(6.0)
-    spec1 = xcspec+0.3*xmspec
-
-    amod = TableApecModel(xray_data_dir, 0.1, 10.0, 
-                          10000, thermal_broad=True)
-    amod.prepare_spectrum(0.2)
-
-    acspec, amspec = amod.get_spectrum(6.0)
-    spec2 = acspec+0.3*amspec
-
-    def spec1_test():
-        return spec1.v
-    def spec2_test():
-        return spec2.v
-
-    for test in [GenericArrayTest(ds, spec1_test),
-                 GenericArrayTest(ds, spec2_test)]:
-        test_apec.__name__ = test.description
-        yield test
-
-    xmod.cleanup_spectrum()
+@pytest.mark.skipif(not pytest.config.getvalue('--with-answer-testing'),
+    reason="--with-answer-testing not set.")
+class TestSpectra(fw.AnswerTest):
+    @requires_module("xspec")
+    @requires_module("astropy")
+    def test_apec(self):
+        settings = {"APECROOT":xray_data_dir+"/apec_v2.0.2"}
+        xmod = XSpecThermalModel("apec", 0.1, 10.0, 10000, thermal_broad=True,
+                                 settings=settings)
+        xmod.prepare_spectrum(0.2)
+        xcspec, xmspec = xmod.get_spectrum(6.0)
+        spec1 = xcspec+0.3*xmspec
+        amod = TableApecModel(xray_data_dir, 0.1, 10.0, 
+                              10000, thermal_broad=True)
+        amod.prepare_spectrum(0.2)
+        acspec, amspec = amod.get_spectrum(6.0)
+        spec2 = acspec+0.3*amspec
+        def spec1_test():
+            return spec1.v
+        def spec2_test():
+            return spec2.v
+        ga_hd1 = self.generic_array_test(ds, spec1_test)
+        ga_hd2 = self.generic_array_test(ds, spec2_test)
+        hashes = {'generic_array1' : utils.generate_hash(ga_hd1),
+            'generic_array2' : utils.generate_hash(ga_hd2)}
+        utils.handle_hashes(self.save_dir, 'spectra-aspec', hashes, self.answer_store)
+        xmod.cleanup_spectrum()
