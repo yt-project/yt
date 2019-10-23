723381b7d80aaa07d9b2d2647144e2f47207ccf5
Jared <jcoughl3@illinois.edu>
2019-10-23 11:46:24 -0500

    Fixed parametrization in gadget's conftest.py.

--- yt/frontends/gadget/tests/conftest.py
+++ yt/frontends/gadget/tests/conftest.py
@@ -12,23 +12,24 @@ isothermal_h5 = "IsothermalCollapse/snap_505.hdf5"
 
 iso_kwargs = dict(bounding_box=[[-3, 3], [-3, 3], [-3, 3]])
 
+fields = 
+    {
+        ("gas", "density"): None,
+        ("gas", "temperature"): None,
+        ("gas", "temperature"): ('gas', 'density'),
+        ('gas', 'velocity_magnitude'): None,
+        ("deposit", "all_density"): None,
+        ("deposit", "all_count"): None,
+        ("deposit", "all_cic"): None,
+        ("deposit", "PartType0_density"): None
+    }
+
 
 def pytest_generate_tests(metafunc):
     if metafunc.function.__name__ == 'test_iso_collapse':
-        fields = OrderedDict(
-            [
-                (("gas", "density"), None),
-                (("gas", "temperature"), None),
-                (("gas", "temperature"), ('gas', 'density')),
-                (('gas', 'velocity_magnitude'), None),
-                (("deposit", "all_density"), None),
-                (("deposit", "all_count"), None),
-                (("deposit", "all_cic"), None),
-                (("deposit", "PartType0_density"), None),
-            ]
-        )
-        metafunc.parametrize('f', fields, ids=['dens-None', 'temp-None', 'temp-dens',
-            'velocity_magnitude', 'all_dens', 'all_count', 'all_cic', 'PartType0Dens'])
+        metafunc.parametrize('f, w', [(k, v) for k, v in fields.items()],
+            ids=['dens', 'temp-None', 'temp-dens', 'velocity_magnitude', 'all_density',
+            'all_count', 'all_cic', 'PartType0Den'])
 
 
 @pytest.fixture(scope='class')
