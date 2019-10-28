3be780b6768f7af778122133a8c691f17c0b50bd
Jared <jcoughl3@illinois.edu>
2019-10-28 16:17:20 -0500

    Updated sph_answer answer test for flash.

--- yt/frontends/flash/tests/conftest.py
+++ yt/frontends/flash/tests/conftest.py
@@ -14,10 +14,6 @@ fid_1to3_b1 = "fiducial_1to3_b1/fiducial_1to3_b1_hdf5_part_0080"
 dens_turb_mag = 'DensTurbMag/DensTurbMag_hdf5_plt_cnt_0015'
 
 
-axes = [0, 1, 2]
-center = "max"
-ds_objs = [None, ("sphere", (center, (0.1, 'unitary')))]
-weights = [None, "density"]
 
 
 def pytest_generate_tests(metafunc):
@@ -26,21 +22,28 @@ def pytest_generate_tests(metafunc):
     if metafunc.function.__name__ == 'test_wind_tunnel':
         fields = ("temperature", "density")
     if metafunc.function.__name__ in ['test_sloshing', 'test_wind_tunnel']:
+        axes = [0, 1, 2]
+        center = "max"
+        ds_objs = [None, ("sphere", (center, (0.1, 'unitary')))]
+        weights = [None, "density"]
         metafunc.parametrize('a', axes, ids=['0', '1', '2'])
         metafunc.parametrize('d', ds_objs, ids=['None', 'sphere'])
         metafunc.parametrize('w', weights, ids=['None', 'density'])
         metafunc.parametrize('f', fields, ids=[str(i) for i in range(len(fields))])
     if metafunc.function.__name__ == 'test_fid_1to3_b1':
-        fid_1to3_b1_fields = { 
-                ("deposit", "all_density") : None,
-                ("deposit", "all_count") : None,
-                ("deposit", "all_cic") : None,
-                ("deposit", "all_cic_velocity_x") : ("deposit", "all_cic"),
-                ("deposit", "all_cic_velocity_y") : ("deposit", "all_cic"),
-                ("deposit", "all_cic_velocity_z") : ("deposit", "all_cic")
-        }
+        fid_1to3_b1_fields = [ 
+                [("deposit", "all_density") , None],
+                [("deposit", "all_count") , None],
+                [("deposit", "all_cic") , None],
+                [("deposit", "all_cic_velocity_x") , ("deposit", "all_cic")],
+                [("deposit", "all_cic_velocity_y") , ("deposit", "all_cic")],
+                [("deposit", "all_cic_velocity_z") , ("deposit", "all_cic")]
+        ]
+        dso = [None, ('sphere', ('c', (0.1, 'unitary')))]
         metafunc.parametrize('f, w', [(k, v) for k, v in fid_1to3_b1_fields.items()],
             ids=['all_dens', 'all_count', 'all_cic', 'all_cic_vx', 'all_cic_vy', 'all_cic_vz'])
+        metafunc.parametrize('d', dso, ids=['None', 'sphere'])
+        metafunc.parametrize('a', [0, 1, 2], ids=['0', '1', '2'])
 
 
 @pytest.fixture(scope='class')
