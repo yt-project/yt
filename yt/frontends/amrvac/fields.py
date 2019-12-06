33473e092e7dc85ac4af5ba05308f90005b22b4c
Clément Robert <cr52@protonmail.com>
2019-12-06 10:38:53 +0100

    implement most hd derived fields

--- yt/frontends/amrvac/fields.py
+++ yt/frontends/amrvac/fields.py
@@ -56,57 +56,77 @@ class AMRVACFieldInfo(FieldInfoContainer):
         def _v3(field, data):
             return data["gas", "moment_3"] / data["gas", "density"]
 
-        unit_system = self.ds.unit_system
+        us = self.ds.unit_system
         aliases = direction_aliases[self.ds.geometry]
         for idir, alias, func in zip("123", aliases, (_v1, _v2, _v3)):
             if not ("amrvac", "m%s" % idir) in self.field_list:
                 break
             self.add_field(("gas", "velocity_%s" % alias), function=func,
-                            units=unit_system['velocity'], dimensions=dimensions.velocity, sampling_type="cell")
+                            units=us['velocity'],
+                            dimensions=dimensions.velocity,
+                            sampling_type="cell")
             self.alias(("gas", "velocity_%s" % idir), ("gas", "velocity_%s" % alias),
-                        units=unit_system["velocity"])
+                        units=us"velocity"])
             self.alias(("gas", "moment_%s" % alias), ("gas", "moment_%s" % idir),
-                        units=unit_system["density"]*unit_system["velocity"])
+                        units=us["density"]*us["velocity"])
 
 
         setup_magnetic_field_aliases(self, "amrvac", ["mag%s" % ax for ax in "xyz"])
 
 
+        # fields with nested dependencies are defined thereafter by increasing level of complexity
+
+        # kinetic energy depends on velocities
         def _kinetic_energy_density(field, data):
             # devnote : have a look at issue 1301
             return 0.5 * data["gas", "density"] * data["gas", "velocity_amplitude"]**2
 
         self.add_field(("gas", "kinetic_energy_density"), function=_kinetic_energy_density,
-                       ...)
+                        units=us["density"]*us["velocity"]**2,
+                        dimensions=dimensions.density*dimensions.velocity**2,
+                        sampling_type="cell")
 
 
+        # thermal pressure depends kinetic energy and equation of state
         def _thermal_pressure(field, data):
-            gamma = data.ds.gamma
-            namelist = data.ds.namelist
+            ds = data.ds
+            gamma = ds.gamma
+            namelist = ds.namelist
             if namelist.get(["hd_list"], {}).get("hd_ernergy", True):
+                # important note : energy density and pressure are actually expressed in the same unit
                 pth = (gamma - 1) * data["gas", "energy_density"] - data["gas", "kinetic_energy_density"]
-                pth *= unit_factor # no idea what this should be :(
             else:
+                # todo: move this warning so it's only triggered once ?
                 mylog.warning("thermal_pressure field is assumed polytropic when hd_energy = False." \
                               "If your simulation used usr_set_pthermal you should redefine this field.")
                 hd_adiab = namelist.get("hd_list", {}).get("hd_adiab", 1.)
-                #entropy_unit = ... # todo: writeme
+
+                # this complicated unit is required for the equation of state to make physical sense
+                hd_adiab *= ds.mass_unit**(1-gamma) * ds.length_unit**(2+3*(gamma-1)) / ds.time_unit**2
                 pth = hd_adiab * entropy_unit * data["gas", "density"]**gamma
             return pth
 
         self.add_field(("gas", "thermal_pressure"), function=_thermal_pressure,
-                       ...)
+                        units=us["density"]*us["velocity"]**2,
+                        dimensions=dimensions.density*dimensions.velocity**2,
+                        sampling_type="cell")
 
 
+        # sound speed depends on thermal pressure
         def _sound_speed(field, data):
             return np.sqrt(data.ds.gamma * data["thermal_pressure"] / data["gas", "density"])
 
         self.add_field(("gas", "sound_speed"), function=_sound_speed,
-                       ...)
+                        units=us["velocity"],
+                        dimensions=dimensions.velocity,
+                        sampling_type="cell")
 
 
+        # mach number depends on sound speed
         def _mach(field, data):
             return data["gas", "velocity_magnitude"] / data["sound_speed"]
 
         self.add_field(("gas", "mach"), function=_mach,
-                       ...)
+                        units="auto",
+                        dimensions=dimensions.dimensionless,
+                        sampling_type="cell")
