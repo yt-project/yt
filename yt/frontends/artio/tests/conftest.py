d55dbf3a3025579090c895b00c1b81c308bb368a
Jared <jcoughl3@illinois.edu>
2019-12-12 12:41:29 -0600

    Greatly neatened up and simplified the test generation code in artio's conftest.py.

--- yt/frontends/artio/tests/conftest.py
+++ yt/frontends/artio/tests/conftest.py
@@ -14,18 +14,40 @@ sizmbhloz = "sizmbhloz-clref04SNth-rs9_a0.9011/"
 sizmbhloz += "sizmbhloz-clref04SNth-rs9_a0.9011.art"
 
 
+# Test parameters. Format:
+# {test1: {param1 : [(val1, val2,...), (id1, id2,...)], param2 : ...}, test2: ...}
+test_params = {
+    'test_sizmbhloz' : {
+        'd' : [(None, ('sphere', ('max', (0.1, 'unitary')))), ('None', 'sphere')],
+        'a' : [(0, 1, 2), ('0', '1', '2')],
+        'w' : [(None, 'density'), ('None', 'density')],
+        'f' : [
+                (
+                    'temperature',
+                    'density',
+                    'velocity_magnitude',
+                    ('deposit', 'all_density'),
+                    ('deposit', 'all_count')
+                ),
+                (
+                    'temperature',
+                    'density',
+                    'velocity_magnitude',
+                    'dep_all_density',
+                    'dep_all_count'
+                )
+            ]
+        }
+    }
+
+
 def pytest_generate_tests(metafunc):
-    if metafunc.function.__name__ == 'test_sizmbhloz':
-        dso = [None, ("sphere", ("max", (0.1, 'unitary')))]
-        axes = [0, 1, 2]
-        weight_fields = [None, "density"]
-        fields = ("temperature", "density", "velocity_magnitude",
-                   ("deposit", "all_density"), ("deposit", "all_count"))
-        metafunc.parametrize('d', dso, ids=['None', 'sphere'])
-        metafunc.parametrize('a', axes, ids=['0', '1', '2'])
-        metafunc.parametrize('w', weight_fields, ids=['None', 'density'])
-        metafunc.parametrize('f', fields, ids=['temperature', 'density',
-            'velocity_magnitude', 'dep_all_density', 'dep_all_count'])
+    # Loop over each test in test_params
+    for test_name, params in test_params.items():
+        if metafunc.function.__name__ == test_name:
+            # Parametrize
+            for param_name, param_vals in params.items():
+                metafunc.parametrize(param_name, param_vals[0], ids=param_vals[1])
 
 
 @pytest.fixture(scope='class')
