18fc3d21ddbe2d265b02a98c3d52fc3e99513578
Jared <jcoughl3@illinois.edu>
2019-12-12 12:40:12 -0600

    Replaced yaml.load() with yaml.safe_load and changed the ordering of the command-line options in conftest.py.

--- conftest.py
+++ conftest.py
@@ -35,27 +35,17 @@ def pytest_addoption(parser):
     Lets options be passed to test functions.
     """
     parser.addoption(
+        "--with-answer-testing",
+        action="store_true",
+        default=False,
+    )
+    parser.addoption(
         "--answer-store",
         action="store_true",
         default=False,
     )
     parser.addoption(
         "--answer-big-data",
-        action="store_true",
-        default=False,
-    )
-    parser.addoption(
-        "--with-answer-testing",
-        action="store_true",
-        default=False,
-    )
-    parser.addoption(
-        "--with-unit-testing",
-        action="store_true",
-        default=False,
-    )
-    parser.addoption(
-        "--force-override-answers",
         action="store_true",
         default=False,
     )
@@ -75,7 +65,7 @@ def pytest_configure(config):
     # Read the list of answer test classes and their associated answer
     # file
     with open(answer_file_list, 'r') as f:
-        pytest.answer_files = yaml.load(f, Loader=yaml.Loader)
+        pytest.answer_files = yaml.safe_load(f)
     # Register custom marks for answer tests and big data
     config.addinivalue_line('markers', 'answer_test: Run the answer tests.')
     config.addinivalue_line('markers', 'big_data: Run answer tests that require'
@@ -87,13 +77,7 @@ def pytest_configure(config):
 #============================================
 def pytest_collection_modifyitems(config, items):
     """
-    Sets up the marks.
-    
-    pytest version 5.x.x no longer provides access to pytest.config
-    (which was originally used for skipping answer tests when the CL
-    option wasn't set), but pytest 5.x.x is what works with the most
-    recent pytest-xdist, which enables parallizing by class. This
-    function is called once, right after test collection.
+    Decide which tests to skip based on command-line options.
     """
     # Set up the skip marks
     skip_answer = pytest.mark.skip(reason="--with-answer-testing not set.")
@@ -108,24 +92,6 @@ def pytest_collection_modifyitems(config, items):
         # option hasn't been set, skip it
         if "big_data" in item.keywords and not config.getoption("--answer-big-data"):
             item.add_marker(skip_big)
-
-
-#============================================
-#                answer_store
-#============================================
-@pytest.fixture(scope='class')
-def cli_testing_opts(request):
-    """
-    Gets the value of the parameters passed at the command line.
-    """
-    # When using the class scope for a fixture, request has an
-    # attribute called cls, which is the class that's using the
-    # fixture. In this case, we can add the options from the parser as
-    # attributes to the class using the fixture. i.e., we can now do
-    # self.answer_store from the class using this fixture
-    if request.cls is not None:
-        request.cls.answer_store = request.config.getoption("--answer-store")
-        request.cls.save_dir = answer_dir 
 
 
 #============================================
@@ -153,20 +119,48 @@ def temp_dir():
 #============================================
 @pytest.fixture(scope='class')
 def answer_file(request):
-    if request.cls is not None:
-        if request.cls.__name__ in pytest.answer_files:
-            request.cls.answer_file = pytest.answer_files[request.cls.__name__]
-            # Make sure we're not overwriting an existing answer set
-            if os.path.isfile(os.path.join(answer_dir, request.cls.answer_file)):
-                if request.config.getoption('--answer-store'):
-                    raise FileExistsError("Error, attempting to overwrite "
-                        "answer file {}. Either specify a new version or "
-                        "set the `--force-override-answers` option".format(
-                            request.cls.answer_file
-                        )
+    if request.cls.__name__ in pytest.answer_files:
+        answer_file = pytest.answer_files[request.cls.__name__]
+        # Make sure we're not overwriting an existing answer set
+        if os.path.isfile(os.path.join(answer_dir, answer_file)):
+            if request.config.getoption('--answer-store'):
+                raise FileExistsError("Error, attempting to overwrite "
+                    "answer file {}. Either specify a new version or "
+                    "set the `--force-override-answers` option".format(
+                        answer_file
                     )
-        else:
-            assert False
+                )
+    else:
+        assert False
+    request.cls.answer_file = answer_file
+
+
+#============================================
+#                param_list
+#============================================
+def param_list(request):
+    """
+    Saves the non-ds, non-fixture function arguments for saving to
+    the answer file.
+    """
+    test_params = {}
+    func = request.node.function
+    # co_varnames is all of the variable names local to the function
+    # starting with self, then the passed args, then the vars defined
+    # in the function body. This excludes fixture names
+    args = func.__code__.co_varnames[1:func.__code__.co_argcount]
+    # funcargs includes the names and values of all arguments, including
+    # fixtures, so we use args to weed out the fixtures. Need to have
+    # special treatment of the data files loaded in fixtures for the
+    # frontends
+    for key, val in request.node.funcargs.items():
+        if key in args and not key.startswith('ds_'):
+            test_params[key] = val
+    # Convert python-specific data objects (such as tuples) to a more
+    # io-friendly format (in order to not have python-specific anchors
+    # in the answer yaml file)
+    test_params = utils.streamline_for_io(test_params)
+    return test_params
 
 
 #============================================
@@ -185,27 +179,16 @@ def hashing(request):
         request.cls.hashes = {}
     else:
         assert False
-    test_params = {}
     # Yield to the caller in order to actually perform the tests
     yield
-    # Get the test parameters
-    func = request.node.function
-    # co_varnames is all of the variable names local to the function
-    # it starts with self, then the passed args, then the vars defined
-    # in the function body. This excludes fixture names
-    args = func.__code__.co_varnames[1:func.__code__.co_argcount]
-    # funcargs includes the names and values of all arguments, including
-    # fixtures, so we use args to weed out the fixtures. Need to have
-    # special treatment of the data files loaded in fixtures for the
-    # frontends
-    for key, val in request.node.funcargs.items():
-        if key in args and not key.startswith('ds_'):
-            test_params[key] = val
-    request.cls.hashes.update(test_params)
-    # Hash the arrays 
-    hashes = utils.array_to_hash(request.cls.hashes)
-    # Finalize by adding the function name as a key
+    # Get param list
+    params = param_list(request)
+    # Hash the test results
+    hashes = utils.hash_results(request.cls.hashes)
+    # Add the other test parameters
+    hashes.update(params)
+    # Add the function name as the "master" key to the hashes dict
     hashes = {request.node.name : hashes}
     # Either save or compare
-    utils.handle_hashes(request.cls.save_dir, request.cls.answer_file, hashes,
-        request.cls.answer_store)
+    utils.handle_hashes(answer_dir, request.cls.answer_file, hashes,
+        request.config.getoption('--answer-store'))
