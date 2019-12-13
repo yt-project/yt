2bc10810a362a043602df2dc7f1c027eabb3faa9
Jared <jcoughl3@illinois.edu>
2019-12-13 13:31:21 -0600

    Updated test_sloshing.py and the photon_simulator conftest.py file. The writing of the answer file had been failing due to my use of the PhotonData class. That has been restructured by adding the info previously stored in the PhotonData class directly to the tests themselves. This makes these tests a bit slower but makes the IO much smoother.

--- yt/analysis_modules/photon_simulator/tests/conftest.py
+++ yt/analysis_modules/photon_simulator/tests/conftest.py
@@ -1,18 +1,13 @@
 import os
 
-from numpy.random import RandomState
 import pytest
 
-from yt.analysis_modules.photon_simulator.api import \
-    TableApecModel, TableAbsorbModel, \
-    ThermalPhotonModel, PhotonList
 from yt.config import ytcfg
-from yt.utilities.answer_testing import utils
 
 
-gslr = "GasSloshingLowRes/sloshing_low_res_hdf5_plt_cnt_0300"
-test_data_dir = ytcfg.get("yt", "test_data_dir")
 xray_data_dir = ytcfg.get("yt", "xray_data_dir")
+
+
 rmfs = ["pn-med.rmf", "acisi_aimpt_cy17.rmf",
         "aciss_aimpt_cy17.rmf", "nustar.rmf",
         "ah_sxs_5ev_basefilt_20100712.rmf"]
@@ -21,39 +16,9 @@ arfs = ["pn-med.arf", "acisi_aimpt_cy17.arf",
         "sxt-s_120210_ts02um_intallpxl.arf"]
 fs_ids = ['pn-med', 'acisi_aimpt_cy17', 'aciss_aimpty_cy17', 'nustar_3arcminA',
     'sxt-s_120210_ts02um_intallpxl']
-TBABS = os.path.join(xray_data_dir, "tbabs_table.h5")
 
-
-def return_data(data):
-    def _return_data(name):
-        return data
-    return _return_data
-
-
-class PhotonData:
-    def __init__(self, ds):
-        self.prng = RandomState(0x4d3d3d3)
-        A = 2000.
-        self.exp_time = 1.0e4
-        redshift = 0.1
-        self.apec_model = TableApecModel(xray_data_dir, 0.1, 11.0, 10000)
-        self.tbabs_model = TableAbsorbModel(TBABS, 0.1)
-        sphere = ds.sphere("c", (0.1, "Mpc"))
-        self.thermal_model = ThermalPhotonModel(self.apec_model, Zmet=0.3, prng=self.prng)
-        self.photons1 = PhotonList.from_scratch(sphere, redshift, A, self.exp_time,
-                                           self.thermal_model)
-        self.return_data = return_data(self.photons1.photons)
-
-@pytest.fixture(scope='function')
-def photon_data(request):
-    if request.function.__name__ in ['test_sloshing_return_photons', 'test_sloshing_return_events']:
-        ds = utils.data_dir_load(gslr)
-        return PhotonData(ds)
 
 def pytest_generate_tests(metafunc):
     if metafunc.function.__name__ == 'test_sloshing_return_events':
         params = [[a, r] for a, r in zip(arfs, rmfs)]
-        for i in range(len(params)):
-            params[i][0] = os.path.join(xray_data_dir, params[i][0])
-            params[i][1] = os.path.join(xray_data_dir, params[i][1])
         metafunc.parametrize('arf, rmf', params, ids=fs_ids)
--- yt/analysis_modules/photon_simulator/tests/test_sloshing.py
+++ yt/analysis_modules/photon_simulator/tests/test_sloshing.py
@@ -11,21 +11,24 @@ Answer test the photon_simulator analysis module.
 #-----------------------------------------------------------------------------
 import os
 
+from numpy.random import RandomState
 import pytest
 
 from yt.analysis_modules.photon_simulator.api import \
-    PhotonList, EventList, \
-    convert_old_file, merge_files
+    PhotonList, EventList, TableApecModel, TableAbsorbModel, \
+    ThermalPhotonModel, PhotonList, convert_old_file, merge_files
 from yt.config import ytcfg
 from yt.testing import assert_almost_equal
 from yt.units.yt_array import uconcatenate
 import yt.utilities.answer_testing.framework as fw
+from yt.utilities.answer_testing import utils
 
 
 xray_data_dir = ytcfg.get("yt", "xray_data_dir")
 gslr = "GasSloshingLowRes/sloshing_low_res_hdf5_plt_cnt_0300"
 old_photon_file = os.path.join(xray_data_dir, "old_photons.h5")
 old_event_file = os.path.join(xray_data_dir, "old_events.h5")
+TBABS = os.path.join(xray_data_dir, "tbabs_table.h5")
 
 
 def return_data(data):
@@ -34,25 +37,50 @@ def return_data(data):
     return _return_data
 
 
+
 @pytest.mark.answer_test
-@pytest.mark.usefixtures('temp_dir', 'answer_file')
+@pytest.mark.usefixtures('temp_dir', 'answer_file', 'hashing')
 class TestSloshingPhoton(fw.AnswerTest):
-    @pytest.mark.usefixtures('hashing')
-    def test_sloshing_return_photons(self, photon_data):
-        ga_hd = self.generic_array_test(photon_data.return_data, args=['photons'])
+    def test_sloshing_return_photons(self):
+        ds = utils.data_dir_load(gslr)
+        prng = RandomState(0x4d3d3d3)
+        A = 2000.
+        exp_time = 1.0e4
+        redshift = 0.1
+        apec_model = TableApecModel(xray_data_dir, 0.1, 11.0, 10000)
+        tbabs_model = TableAbsorbModel(TBABS, 0.1)
+        sphere = ds.sphere("c", (0.1, "Mpc"))
+        thermal_model = ThermalPhotonModel(apec_model, Zmet=0.3, prng=prng)
+        photons1 = PhotonList.from_scratch(sphere, redshift, A, exp_time,
+                                           thermal_model)
+        rdata = return_data(photons1.photons)
+        ga_hd = self.generic_array_test(rdata, args=['photons'])
         self.hashes.update({'generic_array' : ga_hd})
 
-    @pytest.mark.usefixtures('hashing')
-    def test_sloshing_return_events(self, arf, rmf, photon_data):
-        events1 = photon_data.photons1.project_photons([1.0,-0.5,0.2], responses=[arf,rmf],
-          absorb_model=photon_data.tbabs_model, 
-          convolve_energies=True, prng=photon_data.prng
+    def test_sloshing_return_events(self, arf, rmf):
+        arf = os.path.join(xray_data_dir, arf)
+        rmf = os.path.join(xray_data_dir, rmf)
+        ds = utils.data_dir_load(gslr)
+        prng = RandomState(0x4d3d3d3)
+        A = 2000.
+        exp_time = 1.0e4
+        redshift = 0.1
+        apec_model = TableApecModel(xray_data_dir, 0.1, 11.0, 10000)
+        tbabs_model = TableAbsorbModel(TBABS, 0.1)
+        sphere = ds.sphere("c", (0.1, "Mpc"))
+        thermal_model = ThermalPhotonModel(apec_model, Zmet=0.3, prng=prng)
+        photons1 = PhotonList.from_scratch(sphere, redshift, A, exp_time,
+                                           thermal_model)
+        rdata = return_data(photons1.photons)
+        events1 = photons1.project_photons([1.0,-0.5,0.2], responses=[arf,rmf],
+          absorb_model=tbabs_model, 
+          convolve_energies=True, prng=prng
         )
         events1['xsky']
         return_events = return_data(events1.events)
         ga_hd = self.generic_array_test(return_events, args=[os.path.basename(arf)])
         self.hashes.update({'generic_array_return_events' : ga_hd})
-        photon_data.photons1.write_h5_file("test_photons.h5")
+        photons1.write_h5_file("test_photons.h5")
         events1.write_h5_file("test_events.h5")
         photons2 = PhotonList.from_file("test_photons.h5")
         events2 = EventList.from_h5_file("test_events.h5")
@@ -60,22 +88,22 @@ class TestSloshingPhoton(fw.AnswerTest):
         convert_old_file(old_event_file, "converted_events.h5")
         PhotonList.from_file("converted_photons.h5")
         EventList.from_h5_file("converted_events.h5")
-        for k in photon_data.photons1.keys():
+        for k in photons1.keys():
             if k == "Energy":
-                arr1 = uconcatenate(photon_data.photons1[k])
+                arr1 = uconcatenate(photons1[k])
                 arr2 = uconcatenate(photons2[k])
             else:
-                arr1 = photon_data.photons1[k]
+                arr1 = photons1[k]
                 arr2 = photons2[k]
             assert_almost_equal(arr1, arr2)
         for k in events1.keys():
             assert_almost_equal(events1[k], events2[k])
         nevents = 0
         for i in range(4):
-            events = photon_data.photons1.project_photons([1.0,-0.5,0.2],
-                                             exp_time_new=0.25*photon_data.exp_time,
-                                             absorb_model=photon_data.tbabs_model,
-                                             prng=photon_data.prng)
+            events = photons1.project_photons([1.0,-0.5,0.2],
+                                             exp_time_new=0.25*exp_time,
+                                             absorb_model=tbabs_model,
+                                             prng=prng)
             events.write_h5_file("split_events_%d.h5" % i)
             nevents += len(events["xsky"])
         merge_files(["split_events_%d.h5" % i for i in range(4)],
@@ -83,4 +111,4 @@ class TestSloshingPhoton(fw.AnswerTest):
                     clobber=True)
         merged_events = EventList.from_h5_file("merged_events.h5")
         assert len(merged_events["xsky"]) == nevents
-        assert merged_events.parameters["ExposureTime"] == photon_data.exp_time
+        assert merged_events.parameters["ExposureTime"] == exp_time
