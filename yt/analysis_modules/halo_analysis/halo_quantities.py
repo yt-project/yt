0d1390a9926d5a52f62595d5e585c21e10a43580
Corentin Cadiou <contact@cphyc.me>
2019-12-16 14:41:32 +0000

    Add some missing files

--- yt/analysis_modules/halo_analysis/halo_quantities.py
+++ yt/analysis_modules/halo_analysis/halo_quantities.py
@@ -6,7 +6,7 @@ Halo quantity object
 """
 
 #-----------------------------------------------------------------------------
-# Copyright (c) 2013-2014, yt Development Team.
+# Copyright (c) 2013-2020, yt Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
--- yt/frontends/gadget/api.py
+++ yt/frontends/gadget/api.py
@@ -7,7 +7,7 @@ API for Gadget frontend
 """
 
 #-----------------------------------------------------------------------------
-# Copyright (c) 2014-2015, yt Development Team.
+# Copyright (c) 2014-2020, yt Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
