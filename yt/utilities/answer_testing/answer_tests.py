205c0a3fa0befe78b34f844ae0087f8ac067e403
Jared <jcoughl3@illinois.edu>
2020-01-06 15:49:38 -0600

    Removed old copyright boilerplate from the top of answer_tests.py and framework.py.

--- yt/utilities/answer_testing/answer_tests.py
+++ yt/utilities/answer_testing/answer_tests.py
@@ -1,11 +1,6 @@
 """
 Title: answer_tests.py
 Purpose: Contains answer tests that are used by yt's various frontends
-Notes:
-    Copyright (c) 2013, yt Development Team.
-    Distributed under the terms of the Modified BSD License.
-    The full license is in the file COPYING.txt, distributed with this
-    software.
 """
 import os
 import tempfile
--- yt/utilities/answer_testing/framework.py
+++ yt/utilities/answer_testing/framework.py
@@ -1,11 +1,6 @@
 """
 Title: framework.py
 Purpose: Contains answer tests that are used by yt's various frontends
-Notes:
-    Copyright (c) 2013, yt Development Team.
-    Distributed under the terms of the Modified BSD License.
-    The full license is in the file COPYING.txt, distributed with this
-    software.
 """
 import logging
 import numpy as np
