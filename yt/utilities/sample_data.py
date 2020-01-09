8ac746416adeead5a8d252a4afe9fdb747ec19f2
Matthew Turk <matthewturk@gmail.com>
2020-01-09 14:28:17 -0600

    updating Fido class

--- yt/utilities/sample_data.py
+++ yt/utilities/sample_data.py
@@ -6,19 +6,46 @@ Purpose: Contains functions used for automatic downloading and loading
 import pooch
 
 from yt.config import ytcfg
+import pkg_resources
+import json
 
+## The format of the data registry json:
+##
+## {
+##   'dataset_archive_name.tar.gz': {
+##          'sha256': '...',
+##          'url': '...',
+##          'kwargs': {},
+##          'item_name': 'supplied_to_load'
+##   }
+## }
 
 class Fido:
     r"""
     Container for a pooch object used to fetch remote data that isn't
     already stored locally.
     """
-    def __init__(self):
+    def __init__(self, filename = "sample_data_registry.json", cache_dir = None):
+        self.filename = filename
+        self._retrieve_registry()
+        self._registry = json.load(pkg_resources.resource_stream("yt", self.filename))
+        if cache_dir is None:
+            if os.path.isdir(ytcfg.get("test_data_dir")):
+                cache_dir = ytcfg.get("test_data_dir")
+            else:
+                cache_dir = pooch.os_cache("yt")
         self.fido = pooch.create(
-            path=pooch.os_cache("yt"),
-            registry=None,
+            path=cache_dir,
+            registry={_: self._registry[_]['sha256'] for _ in self._registry},
+            urls={_: self._registry[_]['url'] for _ in self._registry},
             env="YT_DATA_DIR",
         )
         # Load the external registry file. It contains data file names,
         # hashes used for validation, and the url for the data file
-        self.fido.load_registry("sample_data_registry.txt")
+
+    def __getitem__(self, item):
+        if item in self._registry:
+            return self._registry[item]
+        if item + ".tar.gz" in self._registry:
+            return self._registry[item + ".tar.gz"]
+        raise KeyError(item)
