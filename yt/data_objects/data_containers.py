a7d074d28fe59f49a4d0145a51ed01f4ab554928
Clément Robert <cr52@protonmail.com>
2020-02-20 11:02:06 +0100

    monkey patch missing lines

--- yt/data_objects/data_containers.py
+++ yt/data_objects/data_containers.py
@@ -917,6 +917,8 @@ class YTDataContainer(object):
         if axis is None:
             mv, pos0, pos1, pos2 = self.quantities.min_location(field)
             return pos0, pos1, pos2
+        if isinstance(axis, string_types):
+            axis = [axis]
         rv = self.quantities.sample_at_min_field_values(field, axis)
         if len(rv) == 2:
             return rv[1]
