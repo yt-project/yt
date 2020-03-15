4b9f672fc8863c9442ec895e8317f667f3a36a6f
Clément Robert <cr52@protonmail.com>
2020-03-15 23:17:33 +0100

    clarify a messageless error

--- yt/fields/field_info_container.py
+++ yt/fields/field_info_container.py
@@ -399,7 +399,8 @@ class FieldInfoContainer(dict):
         fields_to_check = fields_to_check or list(self.keys())
         for field in fields_to_check:
             mylog.debug("Checking %s", field)
-            if field not in self: raise RuntimeError
+            if field not in self:
+                raise RuntimeError("Could not find field %s" % str(field))
             fi = self[field]
             try:
                 fd = fi.get_dependencies(ds = self.ds)
