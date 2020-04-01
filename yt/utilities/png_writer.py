49c32775ea85431bde1e2f98bf3e896050bfadc7
Matthew Turk <matthewturk@gmail.com>
2020-04-01 14:19:06 -0500

    Update to matplotlib's current png interface

--- yt/utilities/png_writer.py
+++ yt/utilities/png_writer.py
@@ -18,13 +18,14 @@ if PY2:
 else:
     from io import BytesIO as StringIO
 
-def call_png_write_png(buffer, width, height, filename, dpi):
+def call_png_write_png(buffer, width, height, fileobj, dpi):
     _png.write_png(buffer, filename, dpi)
 
 def write_png(buffer, filename, dpi=100):
     width = buffer.shape[1]
     height = buffer.shape[0]
-    call_png_write_png(buffer, width, height, filename, dpi)
+    with open(filename, "wb") as fileobj:
+        call_png_write_png(buffer, width, height, fileobj, dpi)
 
 def write_png_to_string(buffer, dpi=100, gray=0):
     width = buffer.shape[1]
