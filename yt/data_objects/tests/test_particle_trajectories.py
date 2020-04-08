b95247b0769a5f6a145b7ad4f29c420a3064d7ea
Madicken Munk <madicken.munk@gmail.com>
2020-04-07 23:59:06 -0500

    remove answer testing framework from particle traj tests

--- yt/data_objects/tests/test_particle_trajectories.py
+++ yt/data_objects/tests/test_particle_trajectories.py
@@ -4,7 +4,7 @@ from numpy.testing import assert_raises
 from yt.config import ytcfg
 from yt.data_objects.time_series import DatasetSeries
 from yt.utilities.answer_testing.framework import GenericArrayTest
-from yt.testing import fake_particle_ds
+from yt.testing import fake_particle_ds, assert_allclose_units
 from yt.utilities.exceptions import YTIllDefinedParticleData
 from yt.data_objects.particle_filters import particle_filter
 
@@ -19,40 +19,64 @@ vfields = ["particle_velocity_x", "particle_velocity_y", "particle_velocity_z"]
 
 def test_orbit_traj():
     n_particles = 16 ** 2
+    n_ds = 5
     ids = np.arange(n_particles, dtype=int)
-    data = {'particle_index': ids}
-    fields = ['particle_type', 'density', 'particle_position_x',
-              'particle_position_y', 'particle_position_z', 'particle_index']
-    units = ['dimensionless', 'g/cm**3', 'cm', 'cm', 'cm', '1']
-    ts = DatasetSeries([fake_particle_ds(fields=fields, negative=[False] * 6,
-                                         units=units, npart=n_particles,
-                                         data=data) for i in range(5)])
-    ds = ts[0]
-    traj = ts.particle_trajectories([1, 2], fields=fields, suppress_logging=True)
-    for field in pfields+vfields:
-        def field_func(name):
-            return traj[field]
-        yield GenericArrayTest(ds, field_func, args=[field])
+    pos = np.random.rand(n_particles) / 2.
+    data =[
+        {'particle_index': ids.copy(),
+         'particle_position_x': pos.copy() * 2.,
+         'particle_position_y': pos.copy() * 1.,
+         'particle_position_z': pos.copy() * 0.5}
+        for i in range(n_ds)
+        ]
+
+    fields = ['particle_position_x', 'particle_position_y', 'particle_position_z',
+              'particle_index', 'particle_mass']
+    field_units = ['cm', 'cm', 'cm', '1', 'g']
+
+    ts = DatasetSeries([fake_particle_ds(fields=fields, units=field_units,
+                        data=dt, negative=[False]*len(fields),
+                        npart=n_particles)
+                    for dt in data])
+
+    traj = ts.particle_trajectories(ids)
+
+    for field in pfields:
+        d_ax = np.diff(traj[field], axis=1)
+        u_ax = ts.outputs[0].quan(0.0, 'code_length')
+        assert_allclose_units(d_ax, u_ax)
 
 def test_etc_traj():
-    n_particles = 16**2
+    n_particles = 16 ** 2
+    n_ds = 5
     ids = np.arange(n_particles, dtype=int)
-    data = {'particle_index': ids}
-    fields = ['particle_type', 'density', 'particle_position_x',
-              'particle_position_y', 'particle_position_z', 'particle_index']
-    units = ['dimensionless', 'g/cm**3', 'cm', 'cm', 'cm', '1']
-    ts = DatasetSeries([fake_particle_ds(fields=fields, negative=[False]*6,
-                                         units=units, npart=n_particles,
-                                         data=data) for i in range(5)])
+    pos = np.random.rand(n_particles) / 2.
+    data =[
+        {'particle_index': ids.copy(),
+         'particle_position_x': pos.copy() * 2.,
+         'particle_position_y': pos.copy() * 1.,
+         'particle_position_z': pos.copy() * 0.5}
+        for i in range(n_ds)
+        ]
+
+    fields = ['particle_position_x', 'particle_position_y', 'particle_position_z',
+              'particle_index', 'particle_mass', 'particle_type']
+    field_units = ['cm', 'cm', 'cm', '1', 'g', 'dimensionless']
+
+    ts = DatasetSeries([fake_particle_ds(fields=fields, units=field_units,
+                        data=dt, negative=[False]*len(fields),
+                        npart=n_particles)
+                    for dt in data])
+
     ds = ts[0]
     sp = ds.sphere(ds.domain_center / 2.0, (0.5, "Mpc"))
     indices = sp["particle_index"][sp["particle_type"] <= 0.5][:5]
-    traj = ts.particle_trajectories(indices, fields=fields, suppress_logging=True)
-    traj.add_fields(["density"])
-    for field in pfields+vfields+["density"]:
-        def field_func(name):
-            return traj[field]
-        yield GenericArrayTest(ds, field_func, args=[field])
+    traj = ts.particle_trajectories(indices)
+
+    for field in pfields:
+        d_ax = np.diff(traj[field], axis=1)
+        u_ax = ts.outputs[0].quan(0.0, 'code_length')
+        assert_allclose_units(d_ax, u_ax)
 
 def test_uniqueness():
     n_particles = 2
