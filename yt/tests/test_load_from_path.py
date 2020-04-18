91548c7fe9b2c6715a6f42ccbc98646a00974cca
Clément Robert <cr52@protonmail.com>
2020-04-19 00:58:32 +0200

    hopefully make test compatible with windows

--- yt/tests/test_load_from_path.py
+++ yt/tests/test_load_from_path.py
@@ -1,3 +1,4 @@
+import os
 from sys import version_info
 from yt.convenience import _sanitize_load_args
 import pytest
@@ -11,24 +12,24 @@ if PY36:
 class TestSanitizeLoadArgs():
     def test_sanitize_from_simple_path(self):
         # check for basic path
-        p1 = "not/a/real/datafile.hdf5"
+        p1 = os.path.join("not", "a", "real", "datafile.hdf5")
         p2 = Path(p1)
         assert _sanitize_load_args(p1) == _sanitize_load_args(p2)
 
     def test_sanitize_from_two_paths(self):
         # check for more than one path
-        p1 = ["not/a/real/datafile.hdf5", "not/real/either/datafile.hdf5"]
+        p1 = [os.path.join("not", "a", "real", "datafile.hdf5"), os.path.join("not", "real", "either", "datafile.hdf5")]
         p2 = [Path(p) for p in p1]
         assert _sanitize_load_args(*p1) == _sanitize_load_args(*p2)
 
     def test_sanitize_from_user_path(self):
         # check for user "~" card expansion
-        p1 = "~/not/a/real/datafile.hdf5"
+        p1 = os.path.join("~", "not", "a", "real", "datafile.hdf5")
         p2 = Path(p1)
         assert _sanitize_load_args(p1) == _sanitize_load_args(p2)
 
     def test_sanitize_from_wildcard_path(self):
         # check with wildcards
-        p1 = "not/a/real/directory/*.hdf5"
+        p1 = os.path.join("not", "a", "real", "directory", "*.hdf5")
         p2 = Path(p1)
         assert _sanitize_load_args(p1) == _sanitize_load_args(p2)
