8c50c0d45d3346402fe2fcda977cc56500573c6d
Clément Robert <cr52@protonmail.com>
2020-04-18 23:02:33 +0200

    limit testing to python 3.6 or above

--- yt/tests/test_load_from_path.py
+++ yt/tests/test_load_from_path.py
@@ -1,26 +1,34 @@
+from sys import version_info
 from yt.convenience import _sanitize_load_args
-from pathlib import Path
+import pytest
 
-def test_sanitize_from_simple_path():
-    # check for basic path
-    p1 = "not/a/real/datafile.hdf5"
-    p2 = Path(p1)
-    assert _sanitize_load_args(p1) == _sanitize_load_args(p2)
+PY36 = version_info >= (3, 6)
 
-def test_sanitize_from_two_paths():
-    # check for more than one path
-    p1 = ["not/a/real/datafile.hdf5", "not/real/either/datafile.hdf5"]
-    p2 = [Path(p) for p in p1]
-    assert _sanitize_load_args(*p1) == _sanitize_load_args(*p2)
+if PY36:
+    from pathlib import Path
 
-def test_sanitize_from_user_path():
-    # check for user "~" card expansion
-    p1 = "~/not/a/real/datafile.hdf5"
-    p2 = Path(p1)
-    assert _sanitize_load_args(p1) == _sanitize_load_args(p2)
+@pytest.mark.skipif(not PY36, reason="requires python3.6 or higher")
+class TestSanitizeLoadArgs():
+    def test_sanitize_from_simple_path(self):
+        # check for basic path
+        p1 = "not/a/real/datafile.hdf5"
+        p2 = Path(p1)
+        assert _sanitize_load_args(p1) == _sanitize_load_args(p2)
 
-def test_sanitize_from_wildcard_path():
-    # check with wildcards
-    p1 = "not/a/real/directory/*.hdf5"
-    p2 = Path(p1)
-    assert _sanitize_load_args(p1) == _sanitize_load_args(p2)
+    def test_sanitize_from_two_paths(self):
+        # check for more than one path
+        p1 = ["not/a/real/datafile.hdf5", "not/real/either/datafile.hdf5"]
+        p2 = [Path(p) for p in p1]
+        assert _sanitize_load_args(*p1) == _sanitize_load_args(*p2)
+
+    def test_sanitize_from_user_path(self):
+        # check for user "~" card expansion
+        p1 = "~/not/a/real/datafile.hdf5"
+        p2 = Path(p1)
+        assert _sanitize_load_args(p1) == _sanitize_load_args(p2)
+
+    def test_sanitize_from_wildcard_path(self):
+        # check with wildcards
+        p1 = "not/a/real/directory/*.hdf5"
+        p2 = Path(p1)
+        assert _sanitize_load_args(p1) == _sanitize_load_args(p2)
