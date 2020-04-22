07486b1f665ff4dc66fbf3fecc1089a65f4d7f35
Clément Robert <cr52@protonmail.com>
2020-04-22 19:01:07 +0200

    force no xticks in colorbars (circumvent a probable mpl bug)

--- yt/visualization/plot_window.py
+++ yt/visualization/plot_window.py
@@ -1068,6 +1068,7 @@ class PWViewerMPL(PlotWindow):
                 elif self._field_transform[f] == log_transform:
                     if MPL_VERSION >= LooseVersion('2.0.0'):
                         self.plots[f].cax.minorticks_on()
+                        self.plots[f].cax.xaxis.set_visible(False)  # experimental
                     else:
                         mticks = self.plots[f].image.norm(get_log_minorticks(vmin, vmax))
                         self.plots[f].cax.yaxis.set_ticks(mticks, minor=True)
