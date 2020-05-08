9647693f9adf142e88d330da1dba7a5d0fed6f15
ashkelly <a.j.kelly@durham.ac.uk>
2020-05-08 20:43:59 +0100

    fix urllib.request import

--- yt/funcs.py
+++ yt/funcs.py
@@ -21,7 +21,7 @@ import getpass
 from math import floor, ceil
 from numbers import Number as numeric_type
 
-import urllib
+import urllib.request
 from yt.utilities.logger import ytLogger as mylog
 from yt.utilities.lru_cache import lru_cache
 from yt.utilities.exceptions import YTInvalidWidthError
