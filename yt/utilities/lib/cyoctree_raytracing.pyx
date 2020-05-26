ae1dcf14a62a4c2eaaff0da94ed7efde16c6cbd1
Corentin Cadiou <contact@cphyc.me>
2020-05-26 21:31:02 +0100

    Cast ray also calls the image sampler (need tidying though)

--- yt/utilities/lib/cyoctree_raytracing.pyx
+++ yt/utilities/lib/cyoctree_raytracing.pyx
@@ -7,7 +7,11 @@ cimport numpy as np
 import numpy as np
 from libcpp.vector cimport vector
 cimport cython
-from libc.stdlib cimport free
+from cython.parallel import prange, parallel
+from libc.stdlib cimport free, malloc
+
+from .image_samplers cimport ImageSampler, ImageAccumulator
+from .volume_container cimport VolumeContainer
 
 cdef extern from "octree_raytracing.cpp":
     cdef cppclass RayInfo[T]:
@@ -44,7 +48,7 @@ cdef class CythonOctreeRayTracing:
 
     @cython.boundscheck(False)
     @cython.wraparound(False)       
-    def cast_rays(self, double[:, ::1] o, double[:, ::1] d):
+    def cast_rays(self, double[:, ::1] o, double[:, ::1] d, ImageSampler sampler, int num_threads = 0):
         cdef RayInfo[int]** ret
         cdef int Nrays = len(o)
         cdef RayInfo[int]* ri
@@ -56,36 +60,51 @@ cdef class CythonOctreeRayTracing:
         
         ret = self.oct.cast_rays(&o[0,0], &d[0,0], Nrays)
 
-        # print('cast!')
-        # Now pack all the rays in numpy arrays
-        cdef int[:] key_view
+        cdef int* cell_ind
+        cdef double* tval
+
+        cdef int i, j, vi, vj, nx, ny
+        cdef VolumeContainer *vc
+        cdef ImageAccumulator *idata
         cdef int* key_ptr
-        
-        cdef double[:] t_view
         cdef double* t_ptr
-        
-        # print('Taking ownership of data')
-        
-        key_array, t_array = [], []
-        for i in range(Nrays):
-            ri = ret[i]
-            if ri.keys.size() == 0:
-                key_array.append(np.array([], dtype=int))
-                t_array.append(np.array([], dtype=np.float64))
-            else:
+        cdef int[3] index = [1, 1, 1]
+
+        nx = <int>np.round(Nrays**0.5)
+        ny = nx  # TODO: change this
+
+        with nogil, parallel(num_threads=num_threads):
+            idata = <ImageAccumulator *> malloc(sizeof(ImageAccumulator))
+            vc = <VolumeContainer*> malloc(sizeof(VolumeContainer))
+            for j in prange(Nrays, schedule='static'):
+                vj = j % ny
+                vi = (j - vj) / ny
+                ri = ret[j]
+                if ri.keys.size() == 0:
+                    continue
+
+                for i in range(3):  # TODO: change 3 to Nchannel
+                    idata.rgba[i] = 0
+
                 key_ptr = &ri.keys[0]
-                key_view = <int[:ri.keys.size()]> key_ptr
-                key_array.append(np.asarray(key_view))
-                
                 t_ptr = &ri.t[0]
-                t_view = <double[:ri.t.size()]> t_ptr
-                t_array.append(np.asarray(t_view).reshape(-1, 2))
-                
+
+                # Iterate over cells
+                for i in range(ri.keys.size()):
+                    # Now call the sampler on the list of cells
+                    sampler.sample(
+                        vc, 
+                        &o[j, 0],
+                        &d[j, 0],
+                        t_ptr[2*i  ],
+                        t_ptr[2*i+1],
+                        index,
+                        <void *> idata
+                        )
+        # Free memory
+        for i in range(Nrays):
             free(ret[i])
-            
-        # We can now free the *list* of vectors, note that the memory is now managed by numpy!
         free(ret)
-        return key_array, t_array
 
     def __dealloc__(self):
         del self.oct
