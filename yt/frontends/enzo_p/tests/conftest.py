44eaa3025bb22663ac9cdb47edbd3810a1b0f9ee
Jared <jcoughl3@illinois.edu>
2020-06-12 18:52:40 -0500

    Ported over enzo_p tests to pytest.

---
+++ yt/frontends/enzo_p/tests/conftest.py
@@ -0,0 +1,50 @@
+"""
+Title: conftest.py
+Purpose: Contains fixtures for loading data.
+"""
+import pytest
+
+from yt.utilities.answer_testing import utils
+
+
+# Test data
+hello_world = "hello-0210/hello-0210.block_list"
+ep_cosmo = "ENZOP_DD0140/ENZOP_DD0140.block_list"
+
+
+# Test parameters. Format:
+# {test1: {param1 : [(val1, val2,...), (id1, id2,...)], param2 : ...}, test2: ...}
+test_params = {
+    'test_hello_world' : {
+        'f' : [('density', 'total_energy', 'velocity_x', 'velocity_y'),
+                ('density', 'total_energy', 'vx', 'vy')],
+        'a' : [(0, 1, 2), ('0', '1', '2')],
+        'd' : [(None, ('sphere', ('max', (0.25, 'unitary')))), ('None', 'sphere')],
+        'w' : [(None, 'density'), ('None', 'density')]
+    },
+    'test_particle_data' : {
+        'f' : [('particle_position_x', 'particle_position_y', 'particle_position_z',
+                'particle_velocity_x', 'particle_velocity_y', 'particle_velocity_z'),
+                ('x', 'y', 'z', 'vx', 'vy', 'vz')],
+        'd' : [(None, ('sphere', ('max', (0.1, 'unitary')))), ('None', 'sphere')]
+    }
+}
+
+
+def pytest_generate_tests(metafunc):
+    # Loop over each test in test_params
+    for test_name, params in test_params.items():
+        if metafunc.function.__name__ == test_name:
+            # Parametrize
+            for param_name, param_vals in params.items():
+                metafunc.parametrize(param_name, param_vals[0], ids=param_vals[1])
+
+@pytest.fixture(scope='class')
+def ds_hello_world():
+    ds = utils.data_dir_load(hello_world)
+    return ds
+
+@pytest.fixture(scope='class')
+def ds_ep_cosmo():
+    ds = utils.data_dir_load(ep_cosmo)
+    return ds
---
+++ yt/frontends/enzo_p/tests/test_outputs_pytest.py
@@ -0,0 +1,98 @@
+"""
+Title: test_enzo_p.py
+Purpose: Enzo-P frontend tests
+    Copyright (c) 2017, yt Development Team.
+    Distributed under the terms of the Modified BSD License.
+    The full license is in the file COPYING.txt, distributed with this
+    software.
+"""
+import numpy as np
+import pytest
+
+from yt.utilities.on_demand_imports import \
+    _h5py as h5py
+
+from yt.testing import \
+    assert_equal, \
+    requires_file, \
+    assert_array_equal
+from yt.frontends.enzo_p.api import EnzoPDataset
+from yt.utilities.answer_testing.answer_tests import \
+    pixelized_projection_values_test, field_values_test
+from yt.utilities.answer_testing import utils
+
+
+# Test data
+hello_world = "hello-0210/hello-0210.block_list"
+ep_cosmo = "ENZOP_DD0140/ENZOP_DD0140.block_list"
+
+
+# Global field info
+_fields = ("density", "total_energy",
+           "velocity_x", "velocity_y")
+_pfields = ("particle_position_x", "particle_position_y",
+            "particle_position_z", "particle_velocity_x",
+            "particle_velocity_y", "particle_velocity_z")
+
+
+#============================================
+#                TestEnzoP
+#============================================
+@pytest.mark.answer_test
+@pytest.mark.usefixtures('answer_file')
+class TestEnzoP:
+
+    @requires_file(hello_world)
+    def test_EnzoPDataset(self, ds_hello_world):
+        assert isinstance(ds_hello_world, EnzoPDataset)
+
+    @pytest.mark.usefixtures('hashing')
+    @utils.requires_ds(hello_world)
+    def test_hello_world(self, f, a, d, w, ds_hello_world):
+        ds = ds_hello_world
+        ppv = pixelized_projection_values_test(ds, a, f, w, d)
+        self.hashes.update({'pixelized_projection_values' : ppv})
+        fv = field_values_test(ds, f, d)
+        self.hashes.update({'field_values' : fv})
+        dobj = utils.create_obj(ds, d)
+        s1 = dobj["ones"].sum()
+        s2 = sum(mask.sum() for block, mask in dobj.blocks)
+        assert_equal(s1, s2)
+
+    @pytest.mark.usefixtures('hashing')
+    @utils.requires_ds(ep_cosmo)
+    def test_particle_fields(self, f, d, ds_ep_cosmo):
+        ds = ds_ep_cosmo
+        fv = field_values_test(ds, f, d, particle_type=True)
+        self.hashes.update({'field_values' : fv})
+        dobj = utils.create_obj(ds, d)
+        s1 = dobj["ones"].sum()
+        s2 = sum(mask.sum() for block, mask in dobj.blocks)
+        assert_equal(s1, s2)
+
+    @requires_file(hello_world)
+    def test_hierarchy(self, ds_hello_world):
+        ds = ds_hello_world
+        fh = h5py.File(ds.index.grids[0].filename, "r")
+        for grid in ds.index.grids:
+            assert_array_equal(
+                grid.LeftEdge.d, fh[grid.block_name].attrs["enzo_GridLeftEdge"])
+            assert_array_equal(
+                ds.index.grid_left_edge[grid.id], grid.LeftEdge)
+            assert_array_equal(
+                ds.index.grid_right_edge[grid.id], grid.RightEdge)
+            for child in grid.Children:
+                assert (child.LeftEdge >= grid.LeftEdge).all()
+                assert (child.RightEdge <= grid.RightEdge).all()
+                assert_equal(child.Parent.id, grid.id)
+        fh.close()
+
+    @requires_file(ep_cosmo)
+    def test_critical_density(self, ds_ep_cosmo):
+        ds = ds_ep_cosmo
+        c1 = (ds.r["dark", "particle_mass"].sum() +
+              ds.r["gas", "cell_mass"].sum()) / \
+              ds.domain_width.prod() / ds.critical_density
+        c2 = ds.omega_matter * (1 + ds.current_redshift)**3 / \
+          (ds.omega_matter * (1 + ds.current_redshift)**3 + ds.omega_lambda)
+        assert np.abs(c1 - c2) / max(c1, c2) < 1e-3
