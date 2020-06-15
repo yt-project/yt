4f0e888918b622ed31e77a3d37414faf55fb0485
Jared <jcoughl3@illinois.edu>
2020-06-15 14:07:33 -0500

    Fixed nose option error in nose_runner.py.

--- tests/nose_runner.py
+++ tests/nose_runner.py
@@ -81,7 +81,7 @@ def generate_tasks_input():
     tests = yaml.load(data, Loader=yaml.FullLoader)
 
     base_argv = ['-s', '--nologcapture', '--with-xunit',
-        '--ignore-files test_outputs_pytest']
+        '--ignore-files=test_outputs_pytest']
 
     base_answer_argv = ['--local-dir=%s' % answers_dir, '--with-answer-testing',
                         '--answer-big-data', '--local']
