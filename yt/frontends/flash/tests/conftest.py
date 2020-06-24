c9817f62d329fc78723786e0855f5c7d39811dc0
Jared <jcoughl3@illinois.edu>
2020-06-24 17:03:45 -0500

    Ported flash.

--- yt/frontends/flash/tests/conftest.py
+++ yt/frontends/flash/tests/conftest.py
@@ -4,15 +4,6 @@ Purpose: Contains fixtures for loading data.
 """
 import pytest
 
-from yt.utilities.answer_testing import utils
-
-
-# Test data
-sloshing = "GasSloshingLowRes/sloshing_low_res_hdf5_plt_cnt_0300"
-wt = "WindTunnel/windtunnel_4lev_hdf5_plt_cnt_0030"
-fid_1to3_b1 = "fiducial_1to3_b1/fiducial_1to3_b1_hdf5_part_0080"
-dens_turb_mag = 'DensTurbMag/DensTurbMag_hdf5_plt_cnt_0015'
-
 
 # Test parameters. Format:
 # {test1: {param1 : [(val1, val2,...), (id1, id2,...)], param2 : ...}, test2: ...}
@@ -45,32 +36,7 @@ test_params = {
 
 
 def pytest_generate_tests(metafunc):
-    # Loop over each test in test_params
     for test_name, params in test_params.items():
         if metafunc.function.__name__ == test_name:
-            # Parametrize
             for param_name, param_vals in params.items():
                 metafunc.parametrize(param_name, param_vals[0], ids=param_vals[1])
-
-
-@pytest.fixture(scope='class')
-def ds_sloshing():
-    ds = utils.data_dir_load(sloshing)
-    assert str(ds) == "sloshing_low_res_hdf5_plt_cnt_0300"
-    return ds
-
-@pytest.fixture(scope='class')
-def ds_wt():
-    ds = utils.data_dir_load(wt)
-    assert str(ds) == "windtunnel_4lev_hdf5_plt_cnt_0030"
-    return ds
-
-@pytest.fixture(scope='class')
-def ds_fid_1to3_b1():
-    ds = utils.data_dir_load(fid_1to3_b1)
-    return ds
-
-@pytest.fixture(scope='class')
-def ds_dens_turb_mag():
-    ds = utils.data_dir_load(dens_turb_mag)
-    return ds
--- yt/frontends/flash/tests/test_outputs_pytest.py
+++ yt/frontends/flash/tests/test_outputs_pytest.py
@@ -16,8 +16,7 @@ from yt.testing import \
     assert_equal, \
     requires_file, \
     units_override_check
-import yt.utilities.answer_testing.framework as fw
-from yt.utilities.answer_testing import utils
+from yt.utilities.answer_testing.answer_tests import small_patch_amr, sph_answer
 
 # Test data
 sloshing = "GasSloshingLowRes/sloshing_low_res_hdf5_plt_cnt_0300"
@@ -31,71 +30,56 @@ dens_turb_mag = 'DensTurbMag/DensTurbMag_hdf5_plt_cnt_0015'
 #============================================
 @pytest.mark.answer_test
 @pytest.mark.usefixtures('answer_file')
-class TestFlash(fw.AnswerTest):
+class TestFlash:
     #-----
     # test_sloshing
     #-----
     @pytest.mark.big_data
     @pytest.mark.usefixtures('hashing')
-    @utils.requires_ds(sloshing)
-    def test_sloshing(self, a, d, w, f, ds_sloshing):
-        # Run the small_patch_amr test suite
-        self.hashes.update(self.small_patch_amr(ds_sloshing, f, w, a, d))
+    @pytest.mark.parametrize('ds', [sloshing], indirect=True)
+    def test_sloshing(self, a, d, w, f, ds):
+        self.hashes.update(small_patch_amr(ds, f, w, a, d))
 
     #-----
     # test_wind_tunnel
     #-----
     @pytest.mark.usefixtures('hashing')
-    @utils.requires_ds(wt)
-    def test_wind_tunnel(self, a, d, w, f, ds_wt):
-        # Run the small_patch_amr test suite
-        self.hashes.update(self.small_patch_amr(ds_wt, f, w, a, d))
+    @pytest.mark.parametrize('ds', [wt], indirect=True)
+    def test_wind_tunnel(self, a, d, w, f, ds):
+        self.hashes.update(small_patch_amr(ds, f, w, a, d))
 
     #-----
     # test_FLASHDataset
     #-----
-    @requires_file(wt)
-    def test_FLASHDataset(self, ds_wt):
-        assert isinstance(ds_wt, FLASHDataset)
+    @pytest.mark.parametrize('ds', [wt], indirect=True)
+    def test_FLASHDataset(self, ds):
+        assert isinstance(ds, FLASHDataset)
 
     #-----
     # test_units_override
     #-----
     @requires_file(sloshing)
-    def test_units_override(self, ds_sloshing):
-        """
-        Parameters:
-        -----------
-            pass
-
-        Raises:
-        -------
-            pass
-
-        Returns:
-        --------
-            pass
-        """
-        units_override_check(ds_sloshing, sloshing)
+    def test_units_override(self):
+        units_override_check(sloshing)
 
     #-----
     # test_FLASHParticleDataset
     #-----
-    @requires_file(fid_1to3_b1)
-    def test_FLASHParticleDataset(self, ds_fid_1to3_b1):
-        assert isinstance(ds_fid_1to3_b1, FLASHParticleDataset)
+    @pytest.mark.parametrize('ds', [fid_1to3_b1], indirect=True)
+    def test_FLASHParticleDataset(self, ds):
+        assert isinstance(ds, FLASHParticleDataset)
 
     #-----
     # test_Flash25_dataset
     #-----
-    @requires_file(dens_turb_mag)
-    def test_FLASH25_dataset(self, ds_dens_turb_mag):
-        assert_equal(ds_dens_turb_mag.parameters['time'], 751000000000.0)
-        assert_equal(ds_dens_turb_mag.domain_dimensions, np.array([8, 8, 8]))
-        assert_equal(ds_dens_turb_mag.domain_left_edge,
-            ds_dens_turb_mag.arr([-2e18, -2e18, -2e18], 'code_length'))
-        assert_equal(ds_dens_turb_mag.index.num_grids, 73)
-        dd = ds_dens_turb_mag.all_data()
+    @pytest.mark.parametrize('ds', [dens_turb_mag], indirect=True)
+    def test_FLASH25_dataset(self, ds):
+        assert_equal(ds.parameters['time'], 751000000000.0)
+        assert_equal(ds.domain_dimensions, np.array([8, 8, 8]))
+        assert_equal(ds.domain_left_edge,
+            ds.arr([-2e18, -2e18, -2e18], 'code_length'))
+        assert_equal(ds.index.num_grids, 73)
+        dd = ds.all_data()
         dd['density']
 
     #-----
@@ -103,9 +87,9 @@ class TestFlash(fw.AnswerTest):
     #-----
     @pytest.mark.big_data
     @pytest.mark.usefixtures('hashing')
-    @utils.requires_ds(fid_1to3_b1)
-    def test_fid_1to3_b1(self, f, w, d, a, ds_fid_1to3_b1):
-        self.hashes.update(self.sph_answer(ds_fid_1to3_b1,
+    @pytest.mark.parametrize('ds', [fid_1to3_b1], indirect=True)
+    def test_fid_1to3_b1(self, f, w, d, a, ds):
+        self.hashes.update(sph_answer(ds,
             'fiducial_1to3_b1_hdf5_part_0080', 6684119, f, w, d, a
             )
         )
