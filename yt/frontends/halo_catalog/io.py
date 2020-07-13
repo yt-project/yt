0f03ac237e9457f2f750e0553794e094bdb440d7
Britton Smith <brittonsmith@gmail.com>
2020-07-13 13:47:41 +0100

    Remove unused function.

--- yt/frontends/halo_catalog/io.py
+++ yt/frontends/halo_catalog/io.py
@@ -72,32 +72,6 @@ class IOHandlerHaloCatalogHDF5(BaseIOHandler):
                         data = f[field][si:ei][mask].astype("float64")
                         yield (ptype, field), data
 
-    def _initialize_index(self, data_file, regions):
-        pcount = data_file.header["num_halos"]
-        morton = np.empty(pcount, dtype="uint64")
-        mylog.debug(
-            "Initializing index % 5i (% 7i particles)", data_file.file_id, pcount
-        )
-        ind = 0
-        if pcount == 0:
-            return None
-        ptype = "halos"
-        with h5py.File(data_file.filename, mode="r") as f:
-            if not f.keys():
-                return None
-            units = parse_h5_attr(f["particle_position_x"], "units")
-            pos = data_file._get_particle_positions(ptype, f=f)
-            pos = data_file.ds.arr(pos, units).to("code_length")
-            dle = self.ds.domain_left_edge.to("code_length")
-            dre = self.ds.domain_right_edge.to("code_length")
-            if np.any(pos.min(axis=0) < dle) or np.any(pos.max(axis=0) > dre):
-                raise YTDomainOverflow(pos.min(axis=0), pos.max(axis=0), dle, dre)
-            regions.add_data_file(pos, data_file.file_id)
-            morton[ind : ind + pos.shape[0]] = compute_morton(
-                pos[:, 0], pos[:, 1], pos[:, 2], dle, dre
-            )
-        return morton
-
     def _count_particles(self, data_file):
         si, ei = data_file.start, data_file.end
         nhalos = data_file.header["num_halos"]
