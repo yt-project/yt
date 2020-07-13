515c2fb511cf090a58f918fbe3655b5f13842688
Britton Smith <brittonsmith@gmail.com>
2020-07-13 15:47:54 +0100

    Remove imports.

--- yt/frontends/halo_catalog/io.py
+++ yt/frontends/halo_catalog/io.py
@@ -2,13 +2,13 @@ from collections import \
     defaultdict
 import numpy as np
 
-from yt.funcs import mylog, parse_h5_attr
-from yt.units.yt_array import uvstack
-from yt.utilities.exceptions import YTDomainOverflow
-from yt.utilities.io_handler import BaseIOHandler
-from yt.utilities.lib.geometry_utils import compute_morton
+from yt.funcs import \
+    parse_h5_attr
+from yt.units.yt_array import \
+    uvstack
 from yt.utilities.on_demand_imports import _h5py as h5py
-
+from yt.utilities.io_handler import \
+    BaseIOHandler
 
 class IOHandlerYTHaloCatalog(BaseIOHandler):
     _dataset_type = "ythalocatalog"
