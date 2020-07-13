b8f130dbba29f82c7de4d6368ef6777d869569bd
Clément Robert <cr52@protonmail.com>
2020-07-13 11:37:02 +0200

    tests: add test for minimal env (RuntimeError should be raised when attempting to load hdf5 files without h5py installed)

--- yt/tests/test_load_errors.py
+++ yt/tests/test_load_errors.py
@@ -5,6 +5,17 @@ from pathlib import Path
 from yt.convenience import load, simulation
 from yt.testing import assert_raises
 from yt.utilities.exceptions import YTOutputNotIdentified, YTSimulationNotIdentified
+from yt.utilities.answer_testing.framework import requires_ds
+
+os33 = "snapshot_033/snap_033.0.hdf5"
+
+@requires_ds(os33)
+def test_missing_h5py():
+    """This test should be skipped in h5py is installed (only tested on mimimal env)"""
+    try:
+        import h5py # NOQA
+    except ImportError:
+        assert_raises(RuntimeError, load, os33)
 
 def test_load_unexisting_data():
     with tempfile.TemporaryDirectory() as tmpdir:
