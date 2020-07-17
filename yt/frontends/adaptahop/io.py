05330c6cf40de81b0ed069cdd2f39066ff4b7b27
Clément Robert <cr52@protonmail.com>
2020-07-17 22:42:26 +0200

    cleanup: remove remaining occurences of copyright blurb in amrvac and adaptahop frontends

--- yt/frontends/adaptahop/io.py
+++ yt/frontends/adaptahop/io.py
@@ -6,13 +6,6 @@ AdaptaHOP data-file handling function
 
 """
 
-# -----------------------------------------------------------------------------
-# Copyright (c) 2013, yt Development Team.
-#
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-# -----------------------------------------------------------------------------
 
 from operator import attrgetter
 
