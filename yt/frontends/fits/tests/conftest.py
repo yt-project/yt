ffb248469d2575a97a7e2767d34f1348d6ddcbdd
Jared Coughlin <jcoughl3@illinois.edu>
2020-07-20 14:01:08 -0500

    Converted fits pytest tests for yt4.

---
+++ yt/frontends/fits/tests/conftest.py
@@ -0,0 +1,91 @@
+"""
+Title: conftest.py
+Purpose: Contains fixtures for loading data.
+"""
+import pytest
+
+from yt.frontends.fits.data_structures import FITSDataset, \
+    SpectralCubeFITSDataset, \
+    SkyDataFITSDataset, \
+    EventsFITSDataset
+from yt.testing import assert_equal
+from yt.utilities.answer_testing.utils import data_dir_load
+
+
+# Test data
+grs = "radio_fits/grs-50-cube.fits"
+vf = "UnigridData/velocity_field_20.fits"
+acis = "xray_fits/acisf05356N003_evt2.fits"
+A2052 = "xray_fits/A2052_merged_0.3-2_match-core_tmap_bgecorr.fits"
+
+
+_fields_grs = ("temperature",)
+_fields_vels = ("velocity_x","velocity_y","velocity_z")
+_fields_acis = ("counts_0.1-2.0", "counts_2.0-5.0")
+_fields_A2052 = ("flux",)
+
+
+# Test parameters. Format:
+# {test1: {param1 : [(val1, val2,...), (id1, id2,...)], param2 : ...}, test2: ...}
+test_params = {
+    'test_grs' : {
+        'a' : [(0, 1, 2), ('0', '1', '2')],
+        'd' : [(None, ('sphere', ('c', (0.1, 'unitary')))), ('None', 'sphere')],
+        'w' : [(None, 'ones'), ('None', 'ones')],
+        'f' : _fields_grs
+    },
+    'test_velocity_field' : {
+        'a' : [(0, 1, 2), ('0', '1', '2')],
+        'd' : [(None, ('sphere', ('c', (0.1, 'unitary')))), ('None', 'sphere')],
+        'w' : [(None, 'ones'), ('None', 'ones')],
+        'f' : _fields_vels
+    },
+    'test_acis' : {
+        'a' : [(0, 1, 2), ('0', '1', '2')],
+        'd' : [(None, ('sphere', ('c', (0.1, 'unitary')))), ('None', 'sphere')],
+        'w' : [(None, 'ones'), ('None', 'ones')],
+        'f' : _fields_acis
+    },
+    'test_A2052' : {
+        'a' : [(0, 1, 2), ('0', '1', '2')],
+        'd' : [(None, ('sphere', ('c', (0.1, 'unitary')))), ('None', 'sphere')],
+        'w' : [(None, 'ones'), ('None', 'ones')],
+        'f' : _fields_A2052
+    }
+}
+        
+
+
+def pytest_generate_tests(metafunc):
+    for test_name, params in test_params.items():
+        if metafunc.function.__name__ == test_name:
+            for param_name, param_vals in params.items():
+                metafunc.parametrize(param_name, param_vals[0], ids=param_vals[1])
+
+
+@pytest.fixture(scope='class')
+def ds_grs():
+    ds = data_dir_load(grs, cls=SpectralCubeFITSDataset, kwargs={"nan_mask":0.0})
+    assert str(ds) == "grs-50-cube.fits"
+    return ds
+
+@pytest.fixture(scope='class')
+def ds_vf():
+    ds = data_dir_load(vf, cls=FITSDataset)
+    assert str(ds) == "velocity_field_20.fits"
+    return ds
+
+@pytest.fixture(scope='class')
+def ds_acis():
+    ds = data_dir_load(acis, cls=EventsFITSDataset)
+    from yt.frontends.fits.misc import setup_counts_fields
+    ebounds = [(0.1, 2.0), (2.0, 5.0)]
+    setup_counts_fields(ds, ebounds)
+    assert_equal(str(ds), "acisf05356N003_evt2.fits")
+    return ds
+
+@pytest.fixture(scope='class')
+def ds_A2052():
+    ds = data_dir_load(A2052, cls=SkyDataFITSDataset)
+    assert str(ds) == "A2052_merged_0.3-2_match-core_tmap_bgecorr.fits"
+    return ds
---
+++ yt/frontends/fits/tests/test_outputs_pytest.py
@@ -0,0 +1,72 @@
+"""
+Title: test_fits.py
+Purpose: FITS frontend tests
+Notes:
+    Copyright (c) 2013, yt Development Team.
+    Distributed under the terms of the Modified BSD License.
+    The full license is in the file COPYING.txt, distributed with this
+    software.
+"""
+import pytest
+
+from yt.frontends.fits.data_structures import FITSDataset, \
+    SpectralCubeFITSDataset, \
+    SkyDataFITSDataset, \
+    EventsFITSDataset
+from yt.testing import \
+    requires_file, \
+    units_override_check
+from yt.utilities.answer_testing.answer_tests import small_patch_amr
+from yt.utilities.answer_testing.utils import data_dir_load, requires_ds
+
+
+# Test data
+grs = "radio_fits/grs-50-cube.fits"
+vf = "UnigridData/velocity_field_20.fits"
+acis = "xray_fits/acisf05356N003_evt2.fits"
+A2052 = "xray_fits/A2052_merged_0.3-2_match-core_tmap_bgecorr.fits"
+
+
+@pytest.mark.answer_test
+@pytest.mark.usefixtures('answer_file')
+class TestFits:
+
+    @pytest.mark.usefixtures('hashing')
+    @requires_ds(grs)
+    def test_grs(self, f, a, d, w, ds_grs):
+        self.hashes.update(small_patch_amr(ds_grs, f, w, a, d))
+
+    @pytest.mark.usefixtures('hashing')
+    @requires_ds(vf)
+    def test_velocity_field(self, f, a, d, w, ds_vf):
+        self.hashes.update(small_patch_amr(ds_vf, f, w, a, d))
+
+    @pytest.mark.usefixtures('hashing')
+    @requires_ds(acis)
+    def test_acis(self, f, a, d, w, ds_acis):
+        self.hashes.update(small_patch_amr(ds_acis, f, w, a, d))
+
+    @pytest.mark.usefixtures('hashing')
+    @requires_ds(A2052)
+    def test_A2052(self, f, a, d, w, ds_A2052):
+        self.hashes.update(small_patch_amr(ds_A2052, f, w, a, d))
+
+    @requires_file(vf)
+    def test_units_override(self):
+        units_override_check(vf)
+
+    @requires_file(vf)
+    def test_FITSDataset(self, ds_vf):
+        assert isinstance(ds_vf, FITSDataset)
+
+    @requires_file(grs)
+    def test_SpectralCubeFITSDataset(self, ds_grs):
+        assert isinstance(ds_grs, SpectralCubeFITSDataset)
+
+    @requires_file(acis)
+    def test_EventsFITSDataset(self, ds_acis):
+        assert isinstance(ds_acis, EventsFITSDataset)
+
+    @requires_file(A2052)
+    def test_SkyDataFITSDataset(self, ds_A2052):
+        assert isinstance(ds_A2052, SkyDataFITSDataset)
