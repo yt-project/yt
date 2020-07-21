3f3a66c0e1bf2a5702f341107365d6f9d46ad109
Jared Coughlin <jcoughl3@illinois.edu>
2020-07-21 14:51:22 -0500

    Converted moab pytest tests for yt4.

---
+++ yt/frontends/moab/tests/conftest.py
@@ -0,0 +1,27 @@
+"""
+Title: conftest.py
+Purpose: Contains fixtures for loading data.
+"""
+import pytest
+
+
+# Test data
+c5 = "c5/c5.h5m"
+
+
+# Test parameters. Format:
+# {test1: {param1 : [(val1, val2,...), (id1, id2,...)], param2 : ...}, test2: ...}
+test_params = {
+    'test_cantor_5' : {
+        'd' : [(None, ('sphere', ('c', (0.1, 'unitary'))), 
+            ('sphere', ('c', (0.2, 'unitary')))), ('None', 'sphere1', 'sphere2')],
+        'f' : [(("moab", "flux"),), ("flux",)] 
+    }
+}
+
+
+def pytest_generate_tests(metafunc):
+    for test_name, params in test_params.items():
+        if metafunc.function.__name__ == test_name:
+            for param_name, param_vals in params.items():
+                metafunc.parametrize(param_name, param_vals[0], ids=param_vals[1])
---
+++ yt/frontends/moab/tests/test_outputs.py
@@ -0,0 +1,63 @@
+"""
+Title: test_moab.py
+Purpose: Tests of semi-structured meshes in MoabHex8 format.
+Notes:
+    Copyright (c) 2013, yt Development Team.
+    Distributed under the terms of the Modified BSD License.
+    The full license is in the file COPYING.txt, distributed with this
+    software.
+"""
+import numpy as np
+import pytest
+
+from yt.frontends.moab.api import MoabHex8Dataset
+from yt.testing import \
+    assert_equal, \
+    assert_almost_equal, \
+    requires_file, \
+    units_override_check
+from yt.utilities.answer_testing.answer_tests import field_values
+
+
+# Test data
+c5 = "c5/c5.h5m"
+
+
+@pytest.mark.answer_test
+@pytest.mark.usefixtures('answer_file')
+class TestMoab:
+
+    @pytest.mark.usefixtures('hashing')
+    @pytest.mark.parametrize('ds', [c5], indirect=True)
+    def test_cantor_5(self, f, d, ds):
+        fv = field_values(ds, f, d)
+        self.hashes.update({'field_values': fv})
+
+    @pytest.mark.parametrize('ds', [c5], indirect=True)
+    def test_cantor_5_non_param(self, ds):
+        np.random.seed(0x4d3d3d3)
+        dd = ds.all_data()
+        assert_almost_equal(ds.index.get_smallest_dx(), 0.00411522633744843, 10)
+        assert_equal(dd["x"].shape[0], 63*63*63)
+        assert_almost_equal(
+            dd["cell_volume"].in_units("code_length**3").sum(dtype="float64").d,
+            1.0, 10)
+        for offset_1 in [1e-9, 1e-4, 0.1]:
+            for offset_2 in [1e-9, 1e-4, 0.1]:
+                DLE = ds.domain_left_edge
+                DRE = ds.domain_right_edge
+                ray = ds.ray(DLE + offset_1 * DLE.uq,
+                             DRE - offset_2 * DRE.uq)
+                assert_almost_equal(ray["dts"].sum(dtype="float64"), 1.0, 8)
+        for i, p1 in enumerate(np.random.random((5, 3))):
+            for j, p2 in enumerate(np.random.random((5, 3))):
+                ray = ds.ray(p1, p2)
+                assert_almost_equal(ray["dts"].sum(dtype="float64"), 1.0, 8)
+
+    @pytest.mark.parametrize('ds', [c5], indirect=True)
+    def test_MoabHex8Dataset(self, ds):
+        assert isinstance(ds, MoabHex8Dataset)
+
+    @requires_file(c5)
+    def test_units_override(self):
+        units_override_check(c5)
