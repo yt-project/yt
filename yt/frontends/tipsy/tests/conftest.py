8be552acec79c87f4f446cc716618df9288e4886
Jared Coughlin <jcoughl3@illinois.edu>
2020-07-22 02:27:47 -0500

    Converted tipsy pytest tests for yt4.

---
+++ yt/frontends/tipsy/tests/conftest.py
@@ -0,0 +1,62 @@
+"""
+Title: conftest.py
+Purpose: Contains fixtures for loading data.
+"""
+import pytest
+
+
+fields = { 
+    ("all", "particle_mass") : None,
+    ("all", "particle_ones") : None,
+    ("all", "particle_velocity_x") : ("all", "particle_mass"),
+    ("all", "particle_velocity_y") : ("all", "particle_mass"),
+    ("all", "particle_velocity_z") : ("all", "particle_mass"),
+}
+
+tg_sph_fields = { 
+    ('gas', 'density') : None,
+    ('gas', 'temperature') : None,
+    ('gas', 'temperature') : ('gas', 'density'),
+    ('gas', 'velocity_magnitude') : None,
+    ('gas', 'Fe_fraction') : None,
+}
+
+tg_nbody_fields = {
+    ('Stars', 'Metals') : None,
+}
+
+# Test parameters. Format:
+# {test1: {param1 : [(val1, val2,...), (id1, id2,...)], param2 : ...}, test2: ...}
+test_params = {
+    'test_pkdgrav' : {
+        'f, w' : [[(k, v) for k, v in fields.items()],
+            ('mass-None', 'ones-None', 'vx-mass', 'vy-mass', 'vz-mass')],
+        'a' : [(0, 1, 2), ('0', '1', '2')],
+        'd' : [(None, ('sphere', ('c', (0.3, 'unitary')))), ('None', 'sphere')],
+    },
+    'test_gasoline_dmonly' : {
+        'f, w' : [[(k, v) for k, v in fields.items()],
+            ('mass-None', 'ones-None', 'vx-mass', 'vy-mass', 'vz-mass')],
+        'a' : [(0, 1, 2), ('0', '1', '2')],
+        'd' : [(None, ('sphere', ('c', (0.3, 'unitary')))), ('None', 'sphere')],
+    },
+    'test_tipsy_galaxy_sph' : {
+        'f, w' : [[(k, v) for k, v in tg_sph_fields.items()],
+            ('dens-None', 'temp-None', 'temp-dens', 'v-None', 'Fe-None')],
+        'd' : [(None, ('sphere', ('c', (0.1, 'unitary')))), ('None', 'sphere')],
+        'a' : [(0, 1, 2), ('0', '1', '2')]
+    },
+    'test_tipsy_galaxy_nbody' : {
+        'f, w' : [[(k, v) for k, v in tg_nbody_fields.items()],
+            ('metals-None')],
+        'd' : [(None, ('sphere', ('c', (0.1, 'unitary')))), ('None', 'sphere')],
+        'a' : [(0, 1, 2), ('0', '1', '2')]
+    }
+}
+
+
+def pytest_generate_tests(metafunc):
+    for test_name, params in test_params.items():
+        if metafunc.function.__name__ == test_name:
+            for param_name, param_vals in params.items():
+                metafunc.parametrize(param_name, param_vals[0], ids=param_vals[1])
---
+++ yt/frontends/tipsy/tests/test_outputs_pytest.py
@@ -0,0 +1,114 @@
+"""
+Title: test_tipsy.py
+Purpose: Tipsy tests using the AGORA dataset
+Notes:
+    Copyright (c) 2013, yt Development Team.
+    Distributed under the terms of the Modified BSD License.
+    The full license is in the file COPYING.txt, distributed with this
+    software.
+"""
+import pytest
+
+from yt.frontends.tipsy.api import TipsyDataset
+from yt.testing import assert_equal, ParticleSelectionComparison, requires_file
+from yt.utilities.answer_testing.answer_tests import nbody_answer, sph_answer
+from yt.utilities.answer_testing.utils import data_dir_load, requires_ds
+
+
+# Test data
+pkdgrav = "halo1e11_run1.00400/halo1e11_run1.00400"
+gasoline_dmonly = "agora_1e11.00400/agora_1e11.00400"
+tipsy_gal = 'TipsyGalaxy/galaxy.00300'
+
+
+pkdgrav_cosmology_parameters = dict(current_redshift = 0.0,
+    omega_lambda = 0.728,
+    omega_matter = 0.272,
+    hubble_constant = 0.702)
+
+pkdgrav_kwargs = dict(
+    field_dtypes = {"Coordinates": "d"},
+    cosmology_parameters = pkdgrav_cosmology_parameters,
+    unit_base = {'length': (60.0, "Mpccm/h")}
+)
+
+
+@pytest.mark.answer_test
+@pytest.mark.usefixtures('answer_file')
+class TestTipsy:
+
+    @pytest.mark.big_data
+    @pytest.mark.usefixtures('hashing')
+    @requires_ds(pkdgrav, file_check = True)
+    def test_pkdgrav(self, f, a, d, w):
+        ds = data_dir_load(pkdgrav, TipsyDataset, (), kwargs=pkdgrav_kwargs)
+        nb = nbody_answer(ds_pkdgrav, "halo1e11_run1.00400", 26847360, f, w, d, a)
+        self.hashes.update({'nbody_answer' : nb})
+
+    @pytest.mark.big_data
+    @requires_ds(pkdgrav, file_check = True)
+    def test_pkdgrav_psc(self):
+        ds = data_dir_load(pkdgrav, TipsyDataset, (), kwargs=pkdgrav_kwargs)
+        psc = ParticleSelectionComparison(ds)
+        psc.run_defaults()
+
+    @pytest.mark.big_data
+    @pytest.mark.usefixtures('hashing')
+    @requires_ds(gasoline_dmonly, file_check = True)
+    def test_gasoline_dmonly(self, f, a, d, w):
+        cosmology_parameters = dict(current_redshift = 0.0,
+            omega_lambda = 0.728,
+            omega_matter = 0.272,
+            hubble_constant = 0.702)
+        kwargs = dict(cosmology_parameters = cosmology_parameters,
+            unit_base = {'length': (60.0, "Mpccm/h")})
+        ds = data_dir_load(gasoline_dmonly, TipsyDataset, (), kwargs)
+        nb = nbody_answer(ds, "agora_1e11.00400", 10550576, f, w, d, a)
+
+    @pytest.mark.big_data
+    @requires_ds(gasoline_dmonly, file_check = True)
+    def test_gasoline_dmonly_psc(self):
+        cosmology_parameters = dict(current_redshift = 0.0,
+            omega_lambda = 0.728,
+            omega_matter = 0.272,
+            hubble_constant = 0.702)
+        kwargs = dict(cosmology_parameters = cosmology_parameters,
+            unit_base = {'length': (60.0, "Mpccm/h")})
+        ds = data_dir_load(gasoline_dmonly, TipsyDataset, (), kwargs)
+        psc = ParticleSelectionComparison(ds)
+        psc.run_defaults()
+
+    @pytest.mark.usefixtures('hashing')
+    @requires_ds(tipsy_gal)
+    def test_tipsy_galaxy_sph(self, f, w, d, a):
+        ds = data_dir_load(tipsy_gal, kwargs = {'bounding_box': [[-2000, 2000],
+                                                                 [-2000, 2000],
+                                                                 [-2000, 2000]]})
+        # These tests should be re-enabled.  But the holdup is that the region
+        # selector does not offset by domain_left_edge, and we have inelegant
+        # selection using bboxes.
+        #psc = ParticleSelectionComparison(ds)
+        #psc.run_defaults()
+        sph = sph_answer(ds, "galaxy.00300", 315372, f, w, d, a)
+        self.hashes.update({"sph_answer" : sph})
+
+    @pytest.mark.usefixtures('hashing')
+    @requires_ds(tipsy_gal)
+    def test_tipsy_galaxy_nbody(self, f, w, d, a):
+        ds = data_dir_load(tipsy_gal, kwargs = {'bounding_box': [[-2000, 2000],
+                                                                 [-2000, 2000],
+                                                                 [-2000, 2000]]})
+        nb = nbody_answer(ds, "galaxy.00300", 315372, f, w, d, a)
+        self.hashes.update({"nbody_answer" : nb})
+
+    @requires_file(gasoline_dmonly)
+    @requires_file(pkdgrav)
+    def test_TipsyDataset(self):
+        assert isinstance(data_dir_load(pkdgrav, kwargs=pkdgrav_kwargs), TipsyDataset)
+        assert isinstance(data_dir_load(gasoline_dmonly), TipsyDataset)
+
+    @requires_file(tipsy_gal)
+    def test_tipsy_index(self):
+        ds = data_dir_load(tipsy_gal)
+        sl = ds.slice('z', 0.0)
+        assert sl['gas', 'density'].shape[0]!=0
