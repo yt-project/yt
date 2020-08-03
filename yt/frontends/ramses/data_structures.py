d18a2a8f8fa9e430298a68daff186cb00749fcaf
Corentin Cadiou <contact@cphyc.me>
2020-08-03 16:28:17 +0200

    Add informative comment

--- yt/frontends/ramses/data_structures.py
+++ yt/frontends/ramses/data_structures.py
@@ -609,6 +609,10 @@ class RAMSESDataset(Dataset):
         self.storage_filename = storage_filename
 
     def _sanitize_max_level(self, max_level, max_level_convention):
+        # NOTE: the initialisation of the dataset class requires
+        #       sets self.min_level _and_ requires force_max_level
+        #       to be set, so we cannot convert from to yt/ramses
+        #       conventions
         if max_level is None and max_level_convention is None:
             return (999, "yt")
 
