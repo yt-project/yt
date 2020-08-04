ccd1129ef63386fd5322a34259f083fb577d0c58
Clément Robert <cr52@protonmail.com>
2020-08-04 07:10:59 +0200

    add test for implicit cartesian geometry reading

--- yt/data_objects/tests/test_io_non_cartesian_data.py
+++ yt/data_objects/tests/test_io_non_cartesian_data.py
@@ -1,8 +1,12 @@
 import os
 from tempfile import TemporaryDirectory
 
+import numpy as np
+
+from yt.frontends.ytdata.api import save_as_dataset
 from yt.frontends.ytdata.data_structures import YTDataContainerDataset
 from yt.testing import fake_amr_ds, requires_module
+from yt.units import YTQuantity
 
 
 @requires_module("h5py")
@@ -20,3 +24,14 @@ def test_preserve_geometric_properties():
         expected = set(ds1.coordinates.axis_order)
         actual = set([fname for ftype, fname in dfl])
         assert expected.difference(actual) == set()
+
+
+@requires_module("h5py")
+def test_default_to_cartesian():
+    data = {"density": np.random.random(128)}
+    ds_attrs = {"current_time": YTQuantity(10, "Myr")}
+    with TemporaryDirectory() as tmpdir:
+        tmpf = os.path.join(tmpdir, "savefile.h5")
+        fn = save_as_dataset(ds_attrs, tmpf, data)
+        ds2 = YTDataContainerDataset(fn)
+    assert ds2.geometry == "cartesian"
