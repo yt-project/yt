206226744a540d18fcd82f54e24885ce15d278e5
Clément Robert <cr52@protonmail.com>
2020-08-08 20:16:52 +0200

    deprecate yt.simulation in favor of yt.load_simulation

--- yt/loaders.py
+++ yt/loaders.py
@@ -5,8 +5,7 @@ This module gathers all user-facing functions with a `load_` prefix.
 # note: in the future, functions could be moved here instead
 # in which case, this file should be removed from flake8 ignore list in setup.cfg
 
-# note: simulation() should be renamed load_simulation()
-from .convenience import load, simulation
+from .convenience import load, load_simulation
 from .frontends.stream.api import (
     load_amr_grids,
     load_hexahedral_mesh,
--- yt/tests/test_load_errors.py
+++ yt/tests/test_load_errors.py
@@ -2,8 +2,8 @@ import os
 import tempfile
 from pathlib import Path
 
-from yt.convenience import load, simulation
 from yt.data_objects.static_output import Dataset
+from yt.loaders import load, load_simulation
 from yt.testing import assert_raises
 from yt.utilities.exceptions import (
     YTAmbiguousDataType,
@@ -17,7 +17,10 @@ def test_load_nonexistent_data():
     with tempfile.TemporaryDirectory() as tmpdir:
         assert_raises(FileNotFoundError, load, os.path.join(tmpdir, "not_a_file"))
         assert_raises(
-            FileNotFoundError, simulation, os.path.join(tmpdir, "not_a_file"), "Enzo"
+            FileNotFoundError,
+            load_simulation,
+            os.path.join(tmpdir, "not_a_file"),
+            "Enzo",
         )
 
         # this one is a design choice:
@@ -26,7 +29,7 @@ def test_load_nonexistent_data():
         # so we make sure the error raised is not YTSimulationNotIdentified
         assert_raises(
             FileNotFoundError,
-            simulation,
+            load_simulation,
             os.path.join(tmpdir, "not_a_file"),
             "unregistered_simulation_type",
         )
@@ -40,13 +43,13 @@ def test_load_unidentified_data():
         assert_raises(YTOutputNotIdentified, load, empty_file_path)
         assert_raises(
             YTSimulationNotIdentified,
-            simulation,
+            load_simulation,
             tmpdir,
             "unregistered_simulation_type",
         )
         assert_raises(
             YTSimulationNotIdentified,
-            simulation,
+            load_simulation,
             empty_file_path,
             "unregistered_simulation_type",
         )
