fe76895860ea564d97dd3ebb6979f81907b5d4da
Clément Robert <cr52@protonmail.com>
2020-08-10 17:25:51 +0200

    raise YTModuleRemoved in convenience

--- yt/convenience.py
+++ yt/convenience.py
@@ -1,8 +1,3 @@
-# this is a deprecated module
-from .funcs import issue_deprecation_warning
-from .loaders import load, load_simulation, simulation
+from yt.utilities.exceptions import YTModuleRemoved
 
-issue_deprecation_warning(
-    "importing from yt.convenience is deprecated in favor of yt.loaders\n"
-    "This will become an error in yt 4.1"
-)
+raise YTModuleRemoved("yt.convenience", "yt.loaders")
