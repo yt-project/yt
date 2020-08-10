15ef2b327b81135ec51801214cd3070077066d6d
Clément Robert <cr52@protonmail.com>
2020-08-10 10:48:20 +0200

    make refine_amr a method of the StreamDataset class

--- yt/frontends/stream/data_structures.py
+++ yt/frontends/stream/data_structures.py
@@ -21,7 +21,8 @@ from yt.geometry.grid_geometry_handler import GridIndex
 from yt.geometry.oct_container import OctreeContainer
 from yt.geometry.oct_geometry_handler import OctreeIndex
 from yt.geometry.unstructured_mesh_handler import UnstructuredIndex
-from yt.units.yt_array import YTQuantity
+from yt.units.yt_array import YTQuantity, uconcatenate
+from yt.utilities.flagging_methods import FlaggingGrid
 from yt.utilities.io_handler import io_registry
 from yt.utilities.lib.cykdtree import PyKDTree
 from yt.utilities.lib.misc_utilities import get_box_grids_level
@@ -31,7 +32,7 @@ from yt.utilities.lib.particle_kdtree_tools import (
 )
 from yt.utilities.logger import ytLogger as mylog
 
-from .definitions import process_data, set_particle_types
+from .definitions import assign_particle_data, process_data, set_particle_types
 from .fields import StreamFieldInfo
 
 
@@ -350,6 +351,132 @@ class StreamDataset(Dataset):
                 particle_types.add(k[0])
         self.particle_types = tuple(particle_types)
         self.particle_types_raw = self.particle_types
+
+    def refine_amr(
+        self, refinement_criteria, fluid_operators, max_level, callback=None
+    ):
+        r"""Given a base dataset, repeatedly apply refinement criteria and
+        fluid operators until a maximum level is reached.
+
+        Parameters
+        ----------
+        self : ~yt.data_objects.static_output.Dataset
+            This is any static output.  It can also be a stream static output, for
+            instance as returned by `yt.loaders.load_uniform_data`.
+        refinement_critera :
+            list of :class:`~yt.utilities.flagging_methods.FlaggingMethod`
+            These criteria will be applied in sequence to identify cells that need
+            to be refined.
+        fluid_operators :
+            list of :class:`~yt.utilities.initial_conditions.FluidOperator`
+            These fluid operators will be applied in sequence to all resulting
+            grids.
+        max_level : int
+            The maximum level to which the data will be refined
+        callback : function, optional
+            A function that will be called at the beginning of each refinement
+            cycle, with the current dataset.
+
+        Examples
+        --------
+        >>> domain_dims = (32, 32, 32)
+        >>> data = np.zeros(domain_dims) + 0.25
+        >>> fo = [ic.CoredSphere(0.05, 0.3, [0.7,0.4,0.75], {"Density": (0.25, 100.0)})]
+        >>> rc = [fm.flagging_method_registry["overdensity"](8.0)]
+        >>> ug = load_uniform_grid({'Density': data}, domain_dims, 1.0)
+        >>> ds = ug.refine_amr(rc, fo, 5)
+        """
+        from .loaders import load_amr_grids
+
+        # If we have particle data, set it aside for now
+
+        number_of_particles = np.sum(
+            [grid.NumberOfParticles for grid in self.index.grids]
+        )
+
+        if number_of_particles > 0:
+            pdata = {}
+            for field in self.field_list:
+                if not isinstance(field, tuple):
+                    field = ("unknown", field)
+                fi = self._get_field_info(*field)
+                if (
+                    fi.sampling_type == "particle"
+                    and field[0] in self.particle_types_raw
+                ):
+                    pdata[field] = uconcatenate(
+                        [grid[field] for grid in self.index.grids]
+                    )
+            pdata["number_of_particles"] = number_of_particles
+
+        last_gc = self.index.num_grids
+        cur_gc = -1
+        ds = self
+        bbox = np.array(
+            [(ds.domain_left_edge[i], ds.domain_right_edge[i]) for i in range(3)]
+        )
+        while ds.index.max_level < max_level and last_gc != cur_gc:
+            mylog.info(
+                "Refining another level.  Current max level: %s", ds.index.max_level
+            )
+            last_gc = ds.index.grids.size
+            for m in fluid_operators:
+                m.apply(ds)
+            if callback is not None:
+                callback(ds)
+            grid_data = []
+            for g in ds.index.grids:
+                gd = dict(
+                    left_edge=g.LeftEdge,
+                    right_edge=g.RightEdge,
+                    level=g.Level,
+                    dimensions=g.ActiveDimensions,
+                )
+                for field in ds.field_list:
+                    if not isinstance(field, tuple):
+                        field = ("unknown", field)
+                    fi = ds._get_field_info(*field)
+                    if not fi.sampling_type == "particle":
+                        gd[field] = g[field]
+                grid_data.append(gd)
+                if g.Level < ds.index.max_level:
+                    continue
+                fg = FlaggingGrid(g, refinement_criteria)
+                nsg = fg.find_subgrids()
+                for sg in nsg:
+                    LE = sg.left_index * g.dds + ds.domain_left_edge
+                    dims = sg.dimensions * ds.refine_by
+                    grid = ds.smoothed_covering_grid(g.Level + 1, LE, dims)
+                    gd = dict(
+                        left_edge=LE,
+                        right_edge=grid.right_edge,
+                        level=g.Level + 1,
+                        dimensions=dims,
+                    )
+                    for field in ds.field_list:
+                        if not isinstance(field, tuple):
+                            field = ("unknown", field)
+                        fi = ds._get_field_info(*field)
+                        if not fi.sampling_type == "particle":
+                            gd[field] = grid[field]
+                    grid_data.append(gd)
+
+            # todo : use StreamDataset.__init__() instead of this loader function
+            # this is a fast way to build a copy of the dataset
+            # but a lot of data is lost on the way
+            ds = load_amr_grids(grid_data, ds.domain_dimensions, bbox=bbox)
+
+            ds.particle_types_raw = self.particle_types_raw
+            ds.particle_types = ds.particle_types_raw
+
+            # Now figure out where the particles go
+            if number_of_particles > 0:
+                # This will update the stream handler too
+                assign_particle_data(ds, pdata, bbox)
+
+            cur_gc = ds.index.num_grids
+
+        return ds
 
 
 class StreamDictFieldHandler(dict):
--- yt/frontends/stream/definitions.py
+++ yt/frontends/stream/definitions.py
@@ -2,15 +2,13 @@ from collections import defaultdict
 
 import numpy as np
 
-from yt.funcs import iterable
+from yt.funcs import issue_deprecation_warning, iterable
 from yt.geometry.grid_container import GridTree, MatchPointsToGrids
-from yt.units.yt_array import uconcatenate
 from yt.utilities.exceptions import (
     YTInconsistentGridFieldShape,
     YTInconsistentGridFieldShapeGridDims,
     YTInconsistentParticleFieldShape,
 )
-from yt.utilities.flagging_methods import FlaggingGrid
 from yt.utilities.logger import ytLogger as mylog
 
 from .fields import StreamFieldInfo
@@ -233,121 +231,15 @@ def process_data(data, grid_dims=None):
 
 
 def refine_amr(base_ds, refinement_criteria, fluid_operators, max_level, callback=None):
-    r"""Given a base dataset, repeatedly apply refinement criteria and
-    fluid operators until a maximum level is reached.
-
-    Parameters
-    ----------
-    base_ds : ~yt.data_objects.static_output.Dataset
-        This is any static output.  It can also be a stream static output, for
-        instance as returned by `yt.loaders.load_uniform_data`.
-    refinement_critera : list of :class:`~yt.utilities.flagging_methods.FlaggingMethod`
-        These criteria will be applied in sequence to identify cells that need
-        to be refined.
-    fluid_operators : list of :class:`~yt.utilities.initial_conditions.FluidOperator`
-        These fluid operators will be applied in sequence to all resulting
-        grids.
-    max_level : int
-        The maximum level to which the data will be refined
-    callback : function, optional
-        A function that will be called at the beginning of each refinement
-        cycle, with the current dataset.
-
-    Examples
-    --------
-    >>> domain_dims = (32, 32, 32)
-    >>> data = np.zeros(domain_dims) + 0.25
-    >>> fo = [ic.CoredSphere(0.05, 0.3, [0.7,0.4,0.75], {"Density": (0.25, 100.0)})]
-    >>> rc = [fm.flagging_method_registry["overdensity"](8.0)]
-    >>> ug = load_uniform_grid({'Density': data}, domain_dims, 1.0)
-    >>> ds = refine_amr(ug, rc, fo, 5)
-    """
-    from .loaders import load_amr_grids
-
-    # If we have particle data, set it aside for now
-
-    number_of_particles = np.sum(
-        [grid.NumberOfParticles for grid in base_ds.index.grids]
+    issue_deprecation_warning(
+        "yt.refine_amr is now a method of the StreamDataset class"
     )
-
-    if number_of_particles > 0:
-        pdata = {}
-        for field in base_ds.field_list:
-            if not isinstance(field, tuple):
-                field = ("unknown", field)
-            fi = base_ds._get_field_info(*field)
-            if (
-                fi.sampling_type == "particle"
-                and field[0] in base_ds.particle_types_raw
-            ):
-                pdata[field] = uconcatenate(
-                    [grid[field] for grid in base_ds.index.grids]
-                )
-        pdata["number_of_particles"] = number_of_particles
-
-    last_gc = base_ds.index.num_grids
-    cur_gc = -1
-    ds = base_ds
-    bbox = np.array(
-        [(ds.domain_left_edge[i], ds.domain_right_edge[i]) for i in range(3)]
+    return base_ds.refine_amr(
+        refinement_criteria=refinement_criteria,
+        fluid_operators=fluid_operators,
+        max_level=max_level,
+        callback=callback,
     )
-    while ds.index.max_level < max_level and last_gc != cur_gc:
-        mylog.info("Refining another level.  Current max level: %s", ds.index.max_level)
-        last_gc = ds.index.grids.size
-        for m in fluid_operators:
-            m.apply(ds)
-        if callback is not None:
-            callback(ds)
-        grid_data = []
-        for g in ds.index.grids:
-            gd = dict(
-                left_edge=g.LeftEdge,
-                right_edge=g.RightEdge,
-                level=g.Level,
-                dimensions=g.ActiveDimensions,
-            )
-            for field in ds.field_list:
-                if not isinstance(field, tuple):
-                    field = ("unknown", field)
-                fi = ds._get_field_info(*field)
-                if not fi.sampling_type == "particle":
-                    gd[field] = g[field]
-            grid_data.append(gd)
-            if g.Level < ds.index.max_level:
-                continue
-            fg = FlaggingGrid(g, refinement_criteria)
-            nsg = fg.find_subgrids()
-            for sg in nsg:
-                LE = sg.left_index * g.dds + ds.domain_left_edge
-                dims = sg.dimensions * ds.refine_by
-                grid = ds.smoothed_covering_grid(g.Level + 1, LE, dims)
-                gd = dict(
-                    left_edge=LE,
-                    right_edge=grid.right_edge,
-                    level=g.Level + 1,
-                    dimensions=dims,
-                )
-                for field in ds.field_list:
-                    if not isinstance(field, tuple):
-                        field = ("unknown", field)
-                    fi = ds._get_field_info(*field)
-                    if not fi.sampling_type == "particle":
-                        gd[field] = grid[field]
-                grid_data.append(gd)
-
-        ds = load_amr_grids(grid_data, ds.domain_dimensions, bbox=bbox)
-
-        ds.particle_types_raw = base_ds.particle_types_raw
-        ds.particle_types = ds.particle_types_raw
-
-        # Now figure out where the particles go
-        if number_of_particles > 0:
-            # This will update the stream handler too
-            assign_particle_data(ds, pdata, bbox)
-
-        cur_gc = ds.index.num_grids
-
-    return ds
 
 
 def set_particle_types(data):
