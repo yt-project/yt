419356bfc005152b200a941286a490de83ea62b3
Jared Coughlin <jcoughl3@illinois.edu>
2020-08-25 14:45:24 -0500

    Re-linted.

--- yt/frontends/halo_catalog/tests/test_outputs_pytest.py
+++ yt/frontends/halo_catalog/tests/test_outputs_pytest.py
@@ -18,8 +18,7 @@ class TestHaloCatalog:
         rs = np.random.RandomState(3670474)
         n_halos = 100
         fields = [
-            f"particle_{name}"
-            for name in ["mass"] + [f"position_{ax}" for ax in "xyz"]
+            f"particle_{name}" for name in ["mass"] + [f"position_{ax}" for ax in "xyz"]
         ]
         units = ["g"] + ["cm"] * 3
         data = dict(
@@ -41,8 +40,7 @@ class TestHaloCatalog:
         rs = np.random.RandomState(3670474)
         n_halos = 100
         fields = [
-            f"particle_{name}"
-            for name in ["mass"] + [f"position_{ax}" for ax in "xyz"]
+            f"particle_{name}" for name in ["mass"] + [f"position_{ax}" for ax in "xyz"]
         ]
         units = ["g"] + ["cm"] * 3
         data = dict(
