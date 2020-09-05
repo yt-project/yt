ffa97135bcdfac127b43e564c52abbe59d750c06
Clément Robert <cr52@protonmail.com>
2020-09-05 18:23:58 +0200

    more simplifications (ensure_list)

--- yt/visualization/particle_plots.py
+++ yt/visualization/particle_plots.py
@@ -1,7 +1,8 @@
 import numpy as np
+from more_itertools import always_iterable
 
 from yt.data_objects.profiles import create_profile
-from yt.funcs import ensure_list, fix_axis
+from yt.funcs import fix_axis
 from yt.units.yt_array import YTArray
 from yt.visualization.fixed_resolution import ParticleImageBuffer
 from yt.visualization.profile_plotter import PhasePlot
@@ -386,7 +387,7 @@ class ParticlePhasePlot(PhasePlot):
         profile = create_profile(
             data_source,
             [x_field, y_field],
-            ensure_list(z_fields),
+            list(always_iterable(z_fields)),
             n_bins=[x_bins, y_bins],
             weight_field=weight_field,
             deposition=deposition,
