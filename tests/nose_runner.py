fe71a76df94b2256cccdf4ef4072b96e38f0280f
Jared Coughlin <jcoughl3@illinois.edu>
2020-09-09 15:07:11 -0500

    Adds junitxml option in nose_runner.py.

--- tests/nose_runner.py
+++ tests/nose_runner.py
@@ -148,6 +148,7 @@ if __name__ == "__main__":
     pytest_args = [
         f"--local-dir={os.path.join(ytcfg.get('yt', 'test_data_dir'), 'answers')}",
         "-c=pytest_answer.ini",
+        "--junitxml=unittests.xml",
         # f"-n {int(os.environ.get('NUM_WORKERS', 6))}",
         # "--dist loadfile",
         # "--cov=yt/",
