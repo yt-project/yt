d0d9744465ddd17c3f0292e15cfeafdbd87f7d27
Jared Coughlin <jcoughl3@illinois.edu>
2020-09-10 09:48:20 -0500

    Checks --answer-big-data option on Jenkins.

--- tests/nose_runner.py
+++ tests/nose_runner.py
@@ -149,6 +149,7 @@ if __name__ == "__main__":
         f"--local-dir={os.path.join(ytcfg.get('yt', 'test_data_dir'), 'answers')}",
         "-c=pytest_answer.ini",
         "--junitxml=unittests.xml",
+        "--answer-big-data",
         # f"-n {int(os.environ.get('NUM_WORKERS', 6))}",
         # "--dist loadfile",
         # "--cov=yt/",
