fa13d9bbf7fffce5d3d261299dc842d08b4bf7d0
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2020-09-25 11:19:07 -0500

    Split art ds9 test into unit/answer parts

--- yt/frontends/art/tests/test_outputs.py
+++ yt/frontends/art/tests/test_outputs.py
@@ -34,13 +34,17 @@ def test_d9p():
     for field in _fields:
         for axis in [0, 1, 2]:
             for dobj_name in dso:
-                for weight_field in [None, "density"]:
+                for weight_field in [None, ("gas", "density")]:
                     if field[0] not in ds.particle_types:
                         yield PixelizedProjectionValuesTest(
                             d9p, axis, field, weight_field, dobj_name
                         )
             yield FieldValuesTest(d9p, field, dobj_name)
 
+
+@requires_file(d9p)
+def test_d9p_ana_values():
+    ds = data_dir_load(d9p)
     ad = ds.all_data()
     # 'Ana' variable values output from the ART Fortran 'ANA' analysis code
     AnaNStars = 6255
@@ -50,7 +54,7 @@ def test_d9p():
     # The *real* asnwer is 2833405, but yt misses one particle since it lives
     # on a domain boundary. See issue 814. When that is fixed, this test
     # will need to be updated
-    AnaNDM = 2833404
+    AnaNDM = 2_833_404
     assert_equal(ad[("darkmatter", "particle_type")].size, AnaNDM)
     assert_equal(
         (
@@ -70,38 +74,38 @@ def test_d9p():
             npart_read += 1
         assert_equal(npart_read, npart_header)
 
-    AnaBoxSize = YTQuantity(7.1442196564, "Mpc")
-    AnaVolume = YTQuantity(364.640074656, "Mpc**3")
+    AnaBoxSize = YTQuantity(7.144_219_656_4, "Mpc")
+    AnaVolume = YTQuantity(364.640_074_656, "Mpc**3")
     Volume = 1
     for i in ds.domain_width.in_units("Mpc"):
         assert_almost_equal(i, AnaBoxSize)
         Volume *= i
     assert_almost_equal(Volume, AnaVolume)
 
-    AnaNCells = 4087490
+    AnaNCells = 4_087_490
     assert_equal(len(ad[("index", "cell_volume")]), AnaNCells)
 
-    AnaTotDMMass = YTQuantity(1.01191786808255e14, "Msun")
+    AnaTotDMMass = YTQuantity(1.011_917_868_082_55e14, "Msun")
     assert_almost_equal(
         ad[("darkmatter", "particle_mass")].sum().in_units("Msun"), AnaTotDMMass
     )
 
-    AnaTotStarMass = YTQuantity(1776701.3990607238, "Msun")
+    AnaTotStarMass = YTQuantity(1_776_701.399_060_723_8, "Msun")
     assert_almost_equal(
         ad[("stars", "particle_mass")].sum().in_units("Msun"), AnaTotStarMass
     )
 
-    AnaTotStarMassInitial = YTQuantity(2423468.2801332865, "Msun")
+    AnaTotStarMassInitial = YTQuantity(2_423_468.280_133_286_5, "Msun")
     assert_almost_equal(
         ad[("stars", "particle_mass_initial")].sum().in_units("Msun"),
         AnaTotStarMassInitial,
     )
 
-    AnaTotGasMass = YTQuantity(1.7826982029216785e13, "Msun")
+    AnaTotGasMass = YTQuantity(1.782_698_202_921_678_5e13, "Msun")
     assert_almost_equal(ad[("gas", "cell_mass")].sum().in_units("Msun"), AnaTotGasMass)
 
-    AnaTotTemp = YTQuantity(150219844793.39072, "K")  # just leaves
-    assert_equal(ad[("gas", "temperature")].sum(), AnaTotTemp)
+    AnaTotTemp = YTQuantity(150_219_844_793.39072, "K")  # just leaves
+    assert_almost_equal(ad[("gas", "temperature")].sum(), AnaTotTemp)
 
 
 @requires_file(d9p)
