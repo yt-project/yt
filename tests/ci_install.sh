ce5e21dfd2f914bbbe98283bb8dab2cd6f73bacf
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2020-11-11 12:01:05 -0600

    Migrate the test suite to GitHub Actions

---
+++ tests/ci_install.sh
@@ -0,0 +1,69 @@
+set -x   # Show which command is being run
+
+[[ -n ${RUNNER_OS} ]] && export TRAVIS_OS_NAME=${RUNNER_OS}
+
+case ${RUNNER_OS} in
+linux|Linux)
+    sudo apt-get install \
+      libhdf5-serial-dev \
+      libnetcdf-dev \
+      libproj-dev \
+      proj-data \
+      proj-bin \
+      libgeos-dev \
+      libopenmpi-dev
+    ;;
+osx|macOS)
+    sudo mkdir -p /usr/local/man
+    sudo chown -R "${USER}:admin" /usr/local/man
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install hdf5 proj geos open-mpi netcdf ccache
+    ;;
+esac
+
+mkdir -p $HOME/.config/yt
+echo "[yt]" > $HOME/.config/yt/ytrc
+echo "suppressStreamLogging = True" >> $HOME/.config/yt/ytrc
+cat $HOME/.config/yt/ytrc
+cp tests/matplotlibrc .
+
+# Step 1: pre-install required packages
+if [[ "${RUNNER_OS}" == "Windows" ]] && [[ $MINIMAL != 1 ]]; then
+    # Install some dependencies using conda (if not doing a minimal run)
+    CYTHON=$(grep cython tests/test_prerequirements.txt)
+    NUMPY=$(grep numpy tests/test_prerequirements.txt)
+
+    CARTOPY=$(grep cartopy tests/test_requirements.txt)
+    H5PY=$(grep h5py tests/test_requirements.txt)
+    MATPLOTLIB=$(grep matplotlib tests/test_requirements.txt)
+    SCIPY=$(grep scipy tests/test_requirements.txt)
+    conda config --set always_yes yes
+    conda info -a
+    conda install --yes -c conda-forge $CYTHON $NUMPY $CARTOPY $H5PY $MATPLOTLIB $SCIPY
+else
+    python -m pip install --upgrade pip
+    python -m pip install --upgrade wheel
+    python -m pip install --upgrade setuptools
+fi
+
+# Step 2: install required packages (depending on whether the build is minimal)
+if [[ $MINIMAL == 1 ]]; then
+    # Ensure numpy and cython are installed so dependencies that need to be built
+    # don't error out
+    # The first numpy to support py3.6 is 1.12, but numpy 1.13 matches
+    # unyt so we'll match it here.
+    python -m pip install numpy==1.13.3 cython==0.26.1
+    python -m pip install -r tests/test_minimal_requirements.txt
+else
+    # Getting cartopy installed requires getting cython and numpy installed
+    # first; this is potentially going to be fixed with the inclusion of
+    # pyproject.toml in cartopy.
+    # These versions are pinned, so we will need to update/remove them when
+    # the hack is no longer necessary.
+    python -m pip install -r tests/test_prerequirements.txt
+    CFLAGS="$CFLAGS -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H" python -m pip install -r tests/test_requirements.txt
+fi
+
+# Step 3: install yt
+python -m pip install -e .
+
+set +x  # Do not show which command is being run (Travis default)
