43b1c607464c4306496160cf3a4081af4303352b
Corentin Cadiou <contact@cphyc.me>
2020-11-25 17:23:34 +0000

    Isolate test specific to PR

--- yt/frontends/ramses/tests/test_outputs.py
+++ yt/frontends/ramses/tests/test_outputs.py
@@ -55,10 +55,15 @@ def test_output_00080():
 
 @requires_file(output_00080)
 def test_RAMSESDataset():
+    assert isinstance(data_dir_load(output_00080), RAMSESDataset)
+
+
+@requires_file(output_00080)
+def test_RAMSES_alternative_load():
     # Test that we can load a RAMSES dataset by giving it the folder name,
     # the info file name or an amr file name
     base_dir, info_file_fname = os.path.split(output_00080)
-    for fname in (output_00080, base_dir, os.path.join(base_dir, "amr_00080.out00001")):
+    for fname in (base_dir, os.path.join(base_dir, "amr_00080.out00001")):
         assert isinstance(data_dir_load(fname), RAMSESDataset)
 
 
