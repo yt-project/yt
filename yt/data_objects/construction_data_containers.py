ef6bd3f25cf0aabd2835462c090d2e7877534bb1
Britton Smith <brittonsmith@gmail.com>
2020-12-02 16:09:30 +0000

    Always save fields as grid type for ArbitraryGrids.

--- yt/data_objects/construction_data_containers.py
+++ yt/data_objects/construction_data_containers.py
@@ -1174,6 +1174,7 @@ class YTArbitraryGrid(YTCoveringGrid):
     """
 
     _spatial = True
+    _always_grid_fields = True
     _type_name = "arbitrary_grid"
     _con_args = ("left_edge", "right_edge", "ActiveDimensions")
     _container_fields = (
