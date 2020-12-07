ed81687d79454c5cd7e762c67926d5b21dc54501
Clément Robert <cr52@protonmail.com>
2020-12-07 19:02:05 +0100

    expose new func in public api

--- yt/frontends/idefix/api.py
+++ yt/frontends/idefix/api.py
@@ -1,4 +1,5 @@
 from .data_structures import SkeletonDataset, SkeletonGrid, SkeletonHierarchy
+from .dmpfile_io import read_idefix_dmpfile
 from .fields import SkeletonFieldInfo
 from .inifile_io import IdefixConf, read_idefix_inifile, write_idefix_inifile
 from .io import SkeletonIOHandler
