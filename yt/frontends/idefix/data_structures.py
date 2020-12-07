ee693e1ebe0c3320f5bbcb502236a018133f294d
Clément Robert <cr52@protonmail.com>
2020-12-07 21:19:18 +0100

    add minimal code to call yt.load without error

--- yt/frontends/idefix/data_structures.py
+++ yt/frontends/idefix/data_structures.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from yt.data_objects.index_subobjects.grid_patch import AMRGridPatch
 from yt.data_objects.static_output import Dataset
+from yt.funcs import setdefaultattr
 from yt.geometry.grid_geometry_handler import GridIndex
 
 from .fields import IdefixFieldInfo
@@ -108,7 +109,8 @@ class IdefixDataset(Dataset):
         # These can also be set:
         # self.velocity_unit = self.quan(1.0, "cm/s")
         # self.magnetic_unit = self.quan(1.0, "gauss")
-        pass
+        for key, unit in self.__class__.default_units.items():
+            setdefaultattr(self, key, self.quan(1, unit))
 
     def _parse_parameter_file(self):
         # This needs to set up the following items.  Note that these are all
@@ -142,7 +144,8 @@ class IdefixDataset(Dataset):
         #   self.geometry  <= a lower case string
         #                     ("cartesian", "polar", "cylindrical"...)
         #                     (defaults to 'cartesian')
-        pass
+        self.current_time = -1  # required, change this !
+        self.cosmological_simulation = 0  # required. Change this if need be.
 
     @classmethod
     def _is_valid(self, fn, *args, **kwargs):
