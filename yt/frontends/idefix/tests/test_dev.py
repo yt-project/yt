109655150def55674b10cd9bf309cbadddab5df0
Clément Robert <cr52@protonmail.com>
2020-12-07 21:38:31 +0100

    tests: add region selection (failing) test

--- yt/frontends/idefix/tests/test_dev.py
+++ yt/frontends/idefix/tests/test_dev.py
@@ -1,9 +1,16 @@
 import os
 
+from yt.frontends.idefix.api import IdefixDataset
 from yt.loaders import load
 
 test_file = os.path.join(os.environ["IDEFIX_DIR"], "test", "HD", "KHI", "dump.0001.dmp")
 
 
 def test_load():
-    load(test_file)
+    ds = load(test_file)
+    assert isinstance(ds, IdefixDataset)
+
+
+def test_region():
+    ds = load(test_file)
+    ds.r[:]
