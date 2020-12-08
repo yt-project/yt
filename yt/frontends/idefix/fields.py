be94fcc37d5c8f5ff1b60d23076d43065db4b17f
Clément Robert <cr52@protonmail.com>
2020-12-08 13:52:35 +0100

    refactor: a simpler implementation for velocity fields aliases

--- yt/frontends/idefix/fields.py
+++ yt/frontends/idefix/fields.py
@@ -5,24 +5,15 @@ from yt.fields.field_info_container import FieldInfoContainer
 # optionally methods on it that get called which can be subclassed.
 
 
-# this is copied from frontends/amrvac/fields.py
-direction_aliases = {
-    "cartesian": ("x", "y", "z"),
-    "polar": ("r", "theta", "z"),
-    "cylindrical": ("r", "z", "theta"),
-    "spherical": ("r", "theta", "phi"),
-}
-
-
 class IdefixFieldInfo(FieldInfoContainer):
     known_other_fields = (
-        # Each entry here is of the form
-        # ( "name", ("units", ["fields", "to", "alias"], "display_name")),
         ("Vc-RHO", ("code_mass / code_length**3", ["density"], None)),
-        ("Vc-VX1", ("code_length / code_time", ["velocity_1"], None)),
-        ("Vc-VX2", ("code_length / code_time", ["velocity_2"], None)),
-        ("Vc-VX3", ("code_length / code_time", ["velocity_3"], None)),
+        ("Vc-VX1", ("code_length / code_time", ["velocity_x"], None)),
+        ("Vc-VX2", ("code_length / code_time", ["velocity_y"], None)),
+        ("Vc-VX3", ("code_length / code_time", ["velocity_z"], None)),
     )
+    # note that velocity '_x', '_y' and '_z' aliases are meant to be
+    # overwriten according to geometry in self.setup_fluid_aliases
 
     known_particle_fields = (
         # Identical form to above
@@ -37,15 +28,7 @@ class IdefixFieldInfo(FieldInfoContainer):
         # Here we do anything that might need info about the dataset.
         # You can use self.alias, self.add_output_field (for on-disk fields)
         # and self.add_field (for derived fields).
-
-        # setup velocity fields
-        ax_aliases = direction_aliases[self.ds.geometry]
-        for idir, alias in enumerate(ax_aliases, start=1):
-            self.alias(
-                ("gas", f"velocity_{alias}"),
-                ("gas", f"velocity_{idir}"),
-                units=self.ds.unit_system["velocity"],
-            )
+        pass
 
     def setup_particle_fields(self, ptype):
         super(IdefixFieldInfo, self).setup_particle_fields(ptype)
