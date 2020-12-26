016160c2571c8e2e50e846c7b7cb98ecbc6c0f46
Clément Robert <cr52@protonmail.com>
2020-12-26 17:52:18 +0100

    cleanup unused deprecation utils

--- yt/_maintenance/deprecation.py
+++ yt/_maintenance/deprecation.py
@@ -1,20 +1,4 @@
 import warnings
-from functools import wraps
-
-
-def deprecated_class(cls):
-    @wraps(cls)
-    def _func(*args, **kwargs):
-        # Note we use SyntaxWarning because by default, DeprecationWarning is
-        # not shown.
-        warnings.warn(
-            f"This usage is deprecated.  Please use {cls.__name__} instead.",
-            SyntaxWarning,
-            stacklevel=2,
-        )
-        return cls(*args, **kwargs)
-
-    return _func
 
 
 class VisibleDeprecationWarning(UserWarning):
@@ -65,31 +49,3 @@ def issue_deprecation_warning(msg, *, removal, since=None, stacklevel=3):
 
     msg += f"This feature will be removed in v{removal}"
     warnings.warn(msg, VisibleDeprecationWarning, stacklevel=stacklevel)
-
-
-def deprecate(replacement):
-    def real_deprecate(func):
-        """
-        This decorator issues a deprecation warning.
-
-        This can be used like so:
-
-        .. code-block:: python
-
-        @deprecate("new_function")
-        def some_really_old_function(...):
-
-        """
-
-        @wraps(func)
-        def run_func(*args, **kwargs):
-            message = "%s has been deprecated and may be removed without notice!"
-            if replacement is not None:
-                message += f" Use {replacement} instead."
-
-            issue_deprecation_warning(message % func.__name__, stacklevel=2)
-            func(*args, **kwargs)
-
-        return run_func
-
-    return real_deprecate
