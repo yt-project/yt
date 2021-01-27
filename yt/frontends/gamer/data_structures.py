0ba975ddf152893353be1a4a2c1660a4f8bcd334
John ZuHone <jzuhone@gmail.com>
2021-01-27 12:10:07 -0500

    Fix broken tests

--- yt/frontends/gamer/data_structures.py
+++ yt/frontends/gamer/data_structures.py
@@ -347,9 +347,11 @@ class GAMERDataset(Dataset):
         # make aliases to some frequently used variables
         if parameters["Model"] == "Hydro":
             self.gamma = parameters["Gamma"]
-            self.eos = parameters["EoS"]
+            self.eos = parameters.get("EoS", 1)  # Assume gamma-law by default
             # default to 0.6 for old data format
-            self.mu = parameters.get("MolecularWeight", 0.6)
+            self.mu = parameters.get(
+                "MolecularWeight", 0.6
+            )  # Assume ionized primordial by default
             self.mhd = parameters.get("Magnetohydrodynamics", 0)
             self.srhd = parameters.get("SRHydrodynamics", 0)
         else:
