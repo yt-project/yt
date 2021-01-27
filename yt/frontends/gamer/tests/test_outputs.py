a687a73987c50a0fcb05b36228f130c16544ac6d
John ZuHone <jzuhone@gmail.com>
2021-01-27 17:48:32 -0500

    Missed this for gamer

--- yt/frontends/gamer/tests/test_outputs.py
+++ yt/frontends/gamer/tests/test_outputs.py
@@ -54,7 +54,7 @@ mhdvortex = "MHDOrszagTangVortex/Data_000018"
 _fields_mhdvortex = (
     ("gamer", "CCMagX"),
     ("gamer", "CCMagY"),
-    ("gas", "magnetic_energy"),
+    ("gas", "magnetic_energy_density"),
 )
 
 
