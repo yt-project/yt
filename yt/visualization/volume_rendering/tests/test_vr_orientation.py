b1ba589915dc6d9bc056dff56f1f5bc352dbd1e6
Clément Robert <cr52@protonmail.com>
2021-02-08 18:03:39 +0100

    deactivate a randomly failing answer test

--- yt/visualization/volume_rendering/tests/test_vr_orientation.py
+++ yt/visualization/volume_rendering/tests/test_vr_orientation.py
@@ -37,7 +37,12 @@ def test_orientation():
     decimals = 12
     test_name = "vr_orientation"
 
-    for lens_type in ["plane-parallel", "perspective"]:
+    for lens_type in [
+        "perspective",
+        # final name VRImageComparison_UniformGridData_vr_pitch_plane-parallel_0002
+        # deactivated because of a random failure since numpy 0.20.0 and 0.20.1
+        # "plane-parallel"
+    ]:
         frame = 0
 
         cam = sc.add_camera(ds, lens_type=lens_type)
