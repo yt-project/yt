9fe7f2a15656c3d63ee3249235c3536d62bce5e6
Clément Robert <cr52@protonmail.com>
2021-02-23 11:59:57 +0100

    drop pathlib in favour of os.path

--- yt/visualization/_commons.py
+++ yt/visualization/_commons.py
@@ -1,4 +1,6 @@
-from pathlib import Path
+import os
+
+from yt.utilities.logger import ytLogger as mylog
 
 from ._mpl_imports import (
     FigureCanvasAgg,
@@ -11,27 +13,35 @@ AGG_FORMATS = [".png", ".jpg", ".jpeg", ".raw", ".rgba", ".tif", ".tiff"]
 SUPPORTED_FORMATS = AGG_FORMATS + [".eps", ".ps", ".pdf", ".svg"]
 
 
-def validate_image_name(filename, suffix=".png"):
+def validate_image_name(filename, suffix: str = ".png") -> str:
     """
     Build a valid image filename with a specified extension (default to png).
     The suffix parameter is ignored if the input filename has a valid extension already.
     Otherwise, suffix is appended to the filename, replacing any existing extension.
     """
-    fn = Path(filename)
-    if fn.suffix in SUPPORTED_FORMATS:
+    # sanitizing: normalize leading '.'
+    suffix = f".{suffix.lstrip('.')}"
+
+    name, psuffix = os.path.splitext(filename)
+    if psuffix in SUPPORTED_FORMATS:
+        if suffix:
+            mylog.warning(
+                "Ignoring supplied suffix '%s' in favour of '%s'", suffix, psuffix
+            )
         return str(filename)
+    elif psuffix:
+        mylog.warning(
+            "Found unsupported suffix '%s', using '%s' instead.", psuffix, suffix
+        )
 
-    if not suffix.startswith("."):
-        suffix = f".{suffix}"
-
-    return str(fn.with_suffix(suffix))
+    return f"{name}{suffix}"
 
 
 def get_canvas(figure, filename):
 
-    suffix = Path(filename).suffix
+    name, suffix = os.path.splitext(filename)
 
-    if suffix == "":
+    if not suffix:
         raise ValueError(
             f"Can not determine canvas class from filename '{filename}' "
             f"without an extension."
@@ -50,5 +60,5 @@ def get_canvas(figure, filename):
         return FigureCanvasPS(figure)
 
     raise ValueError(
-        f"No matching canvas for filename '{filename}' with extension '{suffix}'"
+        f"No matching canvas for filename '{filename}' with extension '{suffix}'."
     )
