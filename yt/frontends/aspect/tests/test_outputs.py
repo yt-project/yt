fd506f70115ea60035d4b84d13c797c8a37e5ae7
Chris Havlin <chris.havlin@gmail.com>
2021-03-03 16:48:38 -0600

    updating tests

--- yt/frontends/aspect/tests/test_outputs.py
+++ yt/frontends/aspect/tests/test_outputs.py
@@ -1,4 +1,4 @@
-from yt.testing import assert_array_equal, assert_equal, requires_file
+from yt.testing import assert_array_equal, assert_equal
 from yt.utilities.answer_testing.framework import (
     AllFieldValuesTest,
     can_run_ds,
@@ -12,28 +12,13 @@ box_convect = "aspect/output_convection_box_3d/nproc_1/solution/solution-00002.p
 box_convect_par = "aspect/output_convection_box_3d/nproc_4/solution/solution-00001.pvtu"
 
 
-def common_assertions(ds, solution_step, nproc):
-    assert_equal(str(ds), f"solution-0000{solution_step}.pvtu")
-    assert_equal(ds.dimensionality, 3)
-    assert_equal(len(ds.parameters["vtu_files"]), nproc)
-    assert_array_equal(ds.parameters["nod_names"], _node_names)
-
-
-@requires_file(box_convect)
+@requires_ds(box_convect)
 def test_box_convect():
     ds = data_dir_load(box_convect)
-    common_assertions(ds, 2, 1)
-
-
-@requires_file(box_convect_par)
-def test_box_convect_par():
-    ds = data_dir_load(box_convect_par)
-    common_assertions(ds, 1, 4)
-
-
-@requires_ds(box_convect)
-def test_box_convect_values():
-    ds = data_dir_load(box_convect)
+    assert_equal(str(ds), "solution-00002.pvtu")
+    assert_equal(ds.dimensionality, 3)
+    assert_equal(len(ds.parameters["vtu_files"]), 1)
+    assert_array_equal(ds.parameters["nod_names"], _node_names)
     ic = ds.domain_center
     if can_run_ds(ds):
         dso = [None, ("sphere", (ic, (0.25, "unitary")))]
@@ -43,8 +28,12 @@ def test_box_convect_values():
 
 
 @requires_ds(box_convect_par)
-def test_box_convect_par_values():
+def test_box_convect_par():
     ds = data_dir_load(box_convect_par)
+    assert_equal(str(ds), "solution-00001.pvtu")
+    assert_equal(ds.dimensionality, 3)
+    assert_equal(len(ds.parameters["vtu_files"]), 4)
+    assert_array_equal(ds.parameters["nod_names"], _node_names)
     ic = ds.domain_center
     if can_run_ds(ds):
         dso = [None, ("sphere", (ic, (0.25, "unitary")))]
