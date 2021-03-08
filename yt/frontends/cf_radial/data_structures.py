e3dda171471146977af47fdd3023cd8005e8c71f
zssherman <shermanx1991@gmail.com>
2021-03-08 12:25:27 -0600

    MNT: Remove copywrites.

--- yt/frontends/cf_radial/data_structures.py
+++ yt/frontends/cf_radial/data_structures.py
@@ -5,14 +5,6 @@ CF Radial data structures
 
 """
 
-#-----------------------------------------------------------------------------
-# Copyright (c) 2018, yt Development Team.
-#
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-#-----------------------------------------------------------------------------
-
 import os
 import stat
 import weakref
--- yt/frontends/cf_radial/tests/test_outputs.py
+++ yt/frontends/cf_radial/tests/test_outputs.py
@@ -5,14 +5,6 @@ CF-Radial frontend tests
 
 """
 
-#-----------------------------------------------------------------------------
-# Copyright (c) 2013, yt Development Team.
-#
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-#-----------------------------------------------------------------------------
-
 from yt.testing import \
     assert_almost_equal, \
     assert_equal, \
