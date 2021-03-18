e791762e0789d6bb07b224c345f75d02974d2953
Ashley Kelly <ashjkelly0@gmail.co.uk>
2021-03-18 14:30:46 +0000

    Added an extra test.

--- yt/data_objects/tests/test_octree.py
+++ yt/data_objects/tests/test_octree.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-from yt.testing import assert_almost_equal, fake_sph_grid_ds
+from yt.testing import assert_almost_equal, assert_equal, fake_sph_grid_ds
 
 n_ref = 4
 
 
 def test_building_tree():
     """
-    Test function to build an octree and make sure correct number of particles
+    Build an octree and make sure correct number of particles
     """
     ds = fake_sph_grid_ds()
     octree = ds.octree(n_ref=n_ref)
@@ -16,7 +16,7 @@ def test_building_tree():
 
 def test_sph_interpolation_scatter():
     """
-    Just generate an octree, perform some SPH interpolation and check with some
+    Generate an octree, perform some SPH interpolation and check with some
     answer testing
     """
 
@@ -53,7 +53,7 @@ def test_sph_interpolation_scatter():
 
 def test_sph_interpolation_gather():
     """
-    Just generate an octree, perform some SPH interpolation and check with some
+    Generate an octree, perform some SPH interpolation and check with some
     answer testing
     """
     ds = fake_sph_grid_ds(hsml_factor=26.0)
@@ -88,3 +88,32 @@ def test_sph_interpolation_gather():
     )
 
     assert_almost_equal(density.d, answers)
+
+
+def test_octree_properties():
+    """
+    Generate an octree, and test the positions and sizes of the cells.
+    """
+    ds = fake_sph_grid_ds()
+    octree = ds.octree(n_ref=n_ref)
+    
+    depth = octree[("index", "depth")]
+    depth_ans = np.array([0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2,], dtype=np.int64)
+    assert_equal(depth, depth_ans)
+
+    size_ans = np.zeros((depth.shape[0], 3), dtype=np.float64)
+    for i in range(size_ans.shape[0]):
+        size_ans[i, :] = (ds.domain_right_edge - ds.domain_left_edge) / 2.0**depth[i]
+
+    dx = octree[("index", "dx")].d
+    assert_almost_equal(dx, size_ans[:, 0])
+
+    dy = octree[("index", "dy")].d
+    assert_almost_equal(dy, size_ans[:, 1])
+    
+    dz = octree[("index", "dz")].d
+    assert_almost_equal(dz, size_ans[:, 2])
+
+    refined = octree[("index", "refined")]
+    refined_ans = np.array([True]+[False]*7+[True]+[False]*8, dtype=np.bool_)
+    assert_equal(refined, refined_ans)
