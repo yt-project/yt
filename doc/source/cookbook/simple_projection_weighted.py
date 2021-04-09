cc789b51c2f03bf6d62a7b3afb0c8e1d9295ebb6
Corentin Cadiou <c.cadiou@ucl.ac.uk>
2021-04-09 14:04:32 +0200

    Simplify programmatic complexity
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- doc/source/cookbook/simple_projection_weighted.py
+++ doc/source/cookbook/simple_projection_weighted.py
@@ -5,12 +5,7 @@ ds = yt.load("GalaxyClusterMerger/fiducial_1to3_b0.273d_hdf5_plt_cnt_0175")
 
 # Create density-weighted projections of temperature (weighted line integrals)
 
-yt.ProjectionPlot(
-    ds, "x", ("gas", "temperature"), weight_field=("gas", "density")
-).save()
-yt.ProjectionPlot(
-    ds, "y", ("gas", "temperature"), weight_field=("gas", "density")
-).save()
-yt.ProjectionPlot(
-    ds, "z", ("gas", "temperature"), weight_field=("gas", "density")
-).save()
+for normal in "xyz":
+    yt.ProjectionPlot(
+        ds, normal, ("gas", "temperature"), weight_field=("gas", "density")
+    ).save()
