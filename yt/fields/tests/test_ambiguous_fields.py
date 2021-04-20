404920c8a2569d7cc1ce7af11a7ba780d20b3b6e
Corentin Cadiou <corentin.cadiou@cphyc.me>
2021-04-20 14:49:44 +0100

    Make sure to test the behaviour

--- yt/fields/tests/test_ambiguous_fields.py
+++ yt/fields/tests/test_ambiguous_fields.py
@@ -1,10 +1,44 @@
-from yt.testing import assert_equal, assert_raises, fake_particle_ds
-from yt.utilities.exceptions import YTAmbiguousFieldName
+from collections import defaultdict
+
+import pytest
+
+from yt._maintenance.deprecation import VisibleDeprecationWarning
+from yt.testing import fake_amr_ds
 
 
 def test_ambiguous_fails():
-    ds = fake_particle_ds()
-    with assert_raises(YTAmbiguousFieldName) as ex:
-        _ = ds.r[:]["particle_position_x"]
-    assert_equal(ex.exception.fname, "particle_position_x")
-    assert_equal(ex.exception.possible_ftypes, {"all", "nbody"})
+    ds = fake_amr_ds(particles=10)
+    msg = "The requested field name '{}' is ambiguous"
+
+    fnames_to_ftype = defaultdict(list)
+    for ftype, fname in ds.field_list:
+        fnames_to_ftype[fname].append(ftype)
+
+    ambiguous_fnames = [
+        fname for fname, ftypes in fnames_to_ftype.items() if len(ftypes) > 1
+    ]
+    unambiguous_fnames = [
+        fname for fname, ftypes in fnames_to_ftype.items() if len(ftypes) == 1
+    ] + ["zeros", "ones"]
+
+    # Make sure we are testing both cases with a few cases
+    assert len(ambiguous_fnames) > 2
+    assert len(unambiguous_fnames) > 2
+
+    # Test warnings are issued for ambiguous fields
+    for fname in ambiguous_fnames:
+        with pytest.warns(VisibleDeprecationWarning, match=msg.format(fname)):
+            ds.r[fname]
+
+    # Test no warnings are issued for single fname access that aren't ambiguous
+    for fname in unambiguous_fnames:
+        with pytest.warns(None) as record:
+            ds.r[fname]
+        assert len(record) == 0
+
+    # Test no warning are issued for tuple access
+    for ftype, fname in ds.field_list:
+        with pytest.warns(None) as record:
+            ds.r[(ftype, fname)]
+
+        assert len(record) == 0
