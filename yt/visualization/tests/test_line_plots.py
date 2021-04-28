0b6a0bb398a11e93b04df802df980ec96566ef77
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2021-04-28 10:40:16 -0500

    port vis test. FIXUP later

--- yt/visualization/tests/test_line_plots.py
+++ yt/visualization/tests/test_line_plots.py
@@ -1,65 +1,51 @@
-from nose.plugins.attrib import attr
-from nose.tools import assert_raises
+import os
+import pytest
 
 import yt
-from yt.testing import ANSWER_TEST_TAG, assert_equal, fake_random_ds
-from yt.utilities.answer_testing.framework import GenericImageTest
+from yt.testing import assert_equal, fake_random_ds
+from yt.utilities.answer_testing.answer_tests import generic_image
 from yt.visualization.line_plot import _validate_point
 
 
-def setup():
-    """Test specific setup."""
-    from yt.config import ytcfg
+@pytest.mark.no_data
+@pytest.mark.answer_test
+@pytest.mark.usefixtures("hashing", "temp_dir")
+class TestLinePlots:
+    answer_file = None
+    saved_hashes = None
+    answer_version = "000"
 
-    ytcfg["yt", "internals", "within_testing"] = True
+    def test_line_plot(self):
+        ds = fake_random_ds(4)
+        fields = [field for field in ds.field_list if field[0] == "stream"]
+        field_labels = {f: f[1] for f in fields}
+        plot = yt.LinePlot(
+            ds, fields, (0, 0, 0), (1, 1, 0), 1000, field_labels=field_labels
+        )
+        plot.annotate_legend(fields[0])
+        plot.annotate_legend(fields[1])
+        plot.set_x_unit("cm")
+        plot.set_unit(fields[0], "kg/cm**3")
+        plot.annotate_title(fields[0], "Density Plot")
+        for fname in plot.save():
+            gi = generic_image(os.path.join(os.getcwd(), fname))
+            self.hashes.update({f"image_{fname}": gi})
 
-
-def compare(ds, plot, test_prefix, test_name, decimals=12):
-    def image_from_plot(filename_prefix):
-        return plot.save(filename_prefix)
-
-    image_from_plot.__name__ = f"line_{test_prefix}"
-    test = GenericImageTest(ds, image_from_plot, decimals)
-    test.prefix = test_prefix
-    test.answer_name = test_name
-    return test
-
-
-@attr(ANSWER_TEST_TAG)
-def test_line_plot():
-    ds = fake_random_ds(4)
-    fields = [field for field in ds.field_list if field[0] == "stream"]
-    field_labels = {f: f[1] for f in fields}
-    plot = yt.LinePlot(
-        ds, fields, (0, 0, 0), (1, 1, 0), 1000, field_labels=field_labels
-    )
-    plot.annotate_legend(fields[0])
-    plot.annotate_legend(fields[1])
-    plot.set_x_unit("cm")
-    plot.set_unit(fields[0], "kg/cm**3")
-    plot.annotate_title(fields[0], "Density Plot")
-    yield compare(
-        ds, plot, test_prefix="answers_line_plot", test_name="answers_line_plot"
-    )
-
-
-@attr(ANSWER_TEST_TAG)
-def test_multi_line_plot():
-    ds = fake_random_ds(4)
-    fields = [field for field in ds.field_list if field[0] == "stream"]
-    field_labels = {f: f[1] for f in fields}
-    lines = []
-    lines.append(yt.LineBuffer(ds, [0.25, 0, 0], [0.25, 1, 0], 100, label="x = 0.5"))
-    lines.append(yt.LineBuffer(ds, [0.5, 0, 0], [0.5, 1, 0], 100, label="x = 0.5"))
-    plot = yt.LinePlot.from_lines(ds, fields, lines, field_labels=field_labels)
-    plot.annotate_legend(fields[0])
-    plot.annotate_legend(fields[1])
-    yield compare(
-        ds,
-        plot,
-        test_prefix="answers_multi_line_plot",
-        test_name="answers_multi_line_plot",
-    )
+    def test_multi_line_plot(self):
+        ds = fake_random_ds(4)
+        fields = [field for field in ds.field_list if field[0] == "stream"]
+        field_labels = {f: f[1] for f in fields}
+        lines = []
+        lines.append(
+            yt.LineBuffer(ds, [0.25, 0, 0], [0.25, 1, 0], 100, label="x = 0.5")
+        )
+        lines.append(yt.LineBuffer(ds, [0.5, 0, 0], [0.5, 1, 0], 100, label="x = 0.5"))
+        plot = yt.LinePlot.from_lines(ds, fields, lines, field_labels=field_labels)
+        plot.annotate_legend(fields[0])
+        plot.annotate_legend(fields[1])
+        for fname in plot.save():
+            gi = generic_image(os.path.join(os.getcwd(), fname))
+            self.hashes.update({f"image_{fname}": gi})
 
 
 def test_line_buffer():
@@ -77,19 +63,17 @@ def test_line_buffer():
 
 def test_validate_point():
     ds = fake_random_ds(3)
-    with assert_raises(RuntimeError) as ex:
+    with pytest.raises(RuntimeError) as ex:
         _validate_point(0, ds, start=True)
-    assert_equal(str(ex.exception), "Input point must be array-like")
+    assert "Input point must be array-like" in str(ex.value)
 
-    with assert_raises(RuntimeError) as ex:
+    with pytest.raises(RuntimeError) as ex:
         _validate_point(ds.arr([[0], [1]], "code_length"), ds, start=True)
-    assert_equal(str(ex.exception), "Input point must be a 1D array")
+    assert "Input point must be a 1D array" in str(ex.value)
 
-    with assert_raises(RuntimeError) as ex:
+    with pytest.raises(RuntimeError) as ex:
         _validate_point(ds.arr([0, 1], "code_length"), ds, start=True)
-    assert_equal(
-        str(ex.exception), "Input point must have an element for each dimension"
-    )
+    assert "Input point must have an element for each dimension" in str(ex.value)
 
     ds = fake_random_ds([32, 32, 1])
     _validate_point(ds.arr([0, 1], "code_length"), ds, start=True)
