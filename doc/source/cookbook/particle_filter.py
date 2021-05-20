e0cea45020e954a7f154e167fd3088c81644a9ee
Cameron Hummels <chummels@gmail.com>
2021-05-19 19:31:05 -0700

    Updating particle filter recipe.

--- doc/source/cookbook/particle_filter.py
+++ doc/source/cookbook/particle_filter.py
@@ -9,19 +9,19 @@ from yt.data_objects.particle_filters import add_particle_filter
 # times arbitrarily far into the future, so stars with negative ages belong
 # in the old stars filter.
 def stars_10Myr(pfilter, data):
-    age = data.ds.current_time - data["Stars", "creation_time"]
+    age = data.ds.current_time - data[("Stars", "creation_time")]
     filter = np.logical_and(age >= 0, age.in_units("Myr") < 10)
     return filter
 
 
 def stars_100Myr(pfilter, data):
-    age = (data.ds.current_time - data["Stars", "creation_time"]).in_units("Myr")
+    age = (data.ds.current_time - data[("Stars", "creation_time")]).in_units("Myr")
     filter = np.logical_and(age >= 10, age < 100)
     return filter
 
 
 def stars_old(pfilter, data):
-    age = data.ds.current_time - data["Stars", "creation_time"]
+    age = data.ds.current_time - data[("Stars", "creation_time")]
     filter = np.logical_or(age < 0, age.in_units("Myr") >= 100)
     return filter
 
@@ -53,9 +53,9 @@ ds.add_particle_filter("stars_old")
 # What are the total masses of different ages of star in the whole simulation
 # volume?
 ad = ds.all_data()
-mass_young = ad["stars_young", "particle_mass"].in_units("Msun").sum()
-mass_medium = ad["stars_medium", "particle_mass"].in_units("Msun").sum()
-mass_old = ad["stars_old", "particle_mass"].in_units("Msun").sum()
+mass_young = ad[("stars_young", "particle_mass")].in_units("Msun").sum()
+mass_medium = ad[("stars_medium", "particle_mass")].in_units("Msun").sum()
+mass_old = ad[("stars_old", "particle_mass")].in_units("Msun").sum()
 print(f"Mass of young stars = {mass_young:g} Msun")
 print(f"Mass of medium stars = {mass_medium:g} Msun")
 print(f"Mass of old stars = {mass_old:g} Msun")
