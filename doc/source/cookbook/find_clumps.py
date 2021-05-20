da95deb8c2eb1da9efb875cb49dd4ed78eae30bb
Cameron Hummels <chummels@gmail.com>
2021-05-20 13:39:32 -0700

    Update doc/source/cookbook/find_clumps.py
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- doc/source/cookbook/find_clumps.py
+++ doc/source/cookbook/find_clumps.py
@@ -65,9 +65,9 @@ prj.annotate_clumps(leaf_clumps_reloaded)
 prj.save("clumps_reloaded")
 
 # Query fields for clumps in the tree.
-print(cds.tree[("clump", "center_of_mass")])
-print(cds.tree.children[0][("grid", "density")])
-print(cds.tree.children[1][("all", "particle_mass")])
+print(cds.tree["clump", "center_of_mass"])
+print(cds.tree.children[0]["grid", "density"])
+print(cds.tree.children[1]["all", "particle_mass"])
 
 # Get all of the leaf clumps.
 print(cds.leaves)
