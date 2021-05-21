d8632cb38bbf524696c023b18a81cdc7afea9e6c
Cameron Hummels <chummels@gmail.com>
2021-05-21 13:18:48 -0700

    Update doc/source/cookbook/particle_filter_sfr.py
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- doc/source/cookbook/particle_filter_sfr.py
+++ doc/source/cookbook/particle_filter_sfr.py
@@ -19,8 +19,8 @@ filename = "IsolatedGalaxy/galaxy0030/galaxy0030"
 ds = yt.load(filename)
 ds.add_particle_filter("formed_star")
 ad = ds.all_data()
-masses = ad[("formed_star", "particle_mass")].in_units("Msun")
-formation_time = ad[("formed_star", "creation_time")].in_units("yr")
+masses = ad["formed_star", "particle_mass"].in_units("Msun")
+formation_time = ad["formed_star", "creation_time"].in_units("yr")
 
 time_range = [0, 5e8]  # years
 n_bins = 1000
