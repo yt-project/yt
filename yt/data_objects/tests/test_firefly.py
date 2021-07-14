9c7ff7bcdc42c1092a9e9a40c35df582d05c872b
Alex Gurvich <agurvich@u.northwestern.edu>
2021-07-14 15:05:20 -0500

    replace if... raise with assert... comment

--- yt/data_objects/tests/test_firefly.py
+++ yt/data_objects/tests/test_firefly.py
@@ -16,15 +16,12 @@ def test_firefly_JSON_string():
 
     reader.dumpToJSON(write_jsons_to_disk=False)
 
-    if len(reader.JSON) == 0:
-        raise ValueError(
-            "reader.JSON was not output to string correctly."
-            + " either Firefly is damaged or needs a hotfix-- try reinstalling."
-            + " if that doesn't work contact the developers"
-            + " at github.com/ageller/Firefly/issues."
-        )
-
-
+    ## reader.JSON was not output to string correctly
+    ##  either Firefly is damaged or needs a hotfix-- try reinstalling.
+    ##  if that doesn't work contact the developers
+    ##  at github.com/ageller/Firefly/issues.
+    assert len(reader.JSON) > 0
+            
 @requires_module("Firefly")
 def test_firefly_write_to_disk():
     tmpdir = tempfile.mkdtemp()
