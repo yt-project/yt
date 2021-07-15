c17bfeb6cf915d7c09b99765296f942a68fa3836
Clément Robert <cr52@protonmail.com>
2021-07-15 16:13:14 +0200

    FIX: add support for setting up derived fields from lambdas

--- yt/fields/derived_field.py
+++ yt/fields/derived_field.py
@@ -250,10 +250,7 @@ class DerivedField:
         This returns a list of names of fields that this field depends on.
         """
         e = FieldDetector(*args, **kwargs)
-        if self._function.__name__ == "<lambda>":
-            e.requested.append(self.name)
-        else:
-            e[self.name]
+        e[self.name]
         return e
 
     def _get_needed_parameters(self, fd):
