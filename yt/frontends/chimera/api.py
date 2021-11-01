60664456b3bd03130fc5119884a6802fffdc5a1e
Ronan-Hix <61328393+Ronan-Hix@users.noreply.github.com>
2021-11-01 16:35:31 -0400

    Update yt/frontends/chimera/api.py
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- yt/frontends/chimera/api.py
+++ yt/frontends/chimera/api.py
@@ -5,14 +5,6 @@ API for yt.frontends._skeleton
 
 """
 
-# -----------------------------------------------------------------------------
-# Copyright (c) 2013, yt Development Team.
-#
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-# -----------------------------------------------------------------------------
-
 from . import tests
 from .data_structures import ChimeraDataset
 from .fields import ChimeraFieldInfo
