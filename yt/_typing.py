55b01b0e35995d5028807b932c04eee5dde1c9a4
Clément Robert <cr52@protonmail.com>
2021-12-23 12:02:57 +0100

    TYP: tentative mypy upgrade, use explicit TypeAlias in yt._typing

--- yt/_typing.py
+++ yt/_typing.py
@@ -1,4 +1,11 @@
+import sys
 from typing import List, Optional, Tuple
 
-FieldDescT = Tuple[str, Tuple[str, List[str], Optional[str]]]
-KnownFieldsT = Tuple[FieldDescT, ...]
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
+
+FieldDescT: TypeAlias = Tuple[str, Tuple[str, List[str], Optional[str]]]
+KnownFieldsT: TypeAlias = Tuple[FieldDescT, ...]
