49fdba085d0f3c6b853298f758ebbd1d5000abd4
Clément Robert <cr52@protonmail.com>
2022-01-18 13:36:20 +0100

    TST: don't run brew update on MacOS CI (revert #3187)

--- tests/ci_install.sh
+++ tests/ci_install.sh
@@ -16,7 +16,6 @@ linux|Linux)
 osx|macOS)
     sudo mkdir -p /usr/local/man
     sudo chown -R "${USER}:admin" /usr/local/man
-    brew update
     HOMEBREW_NO_AUTO_UPDATE=1 brew install hdf5 proj geos open-mpi netcdf ccache osxfuse
     ;;
 esac
