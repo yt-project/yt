b9285c02a496ea7a019ba8e6e37c6150759c109a
Clément Robert <cr52@protonmail.com>
2022-04-24 14:33:28 +0200

    update tests

---
+++ yt/visualization/tests/test_norm_api_set_unit_and_zlim.py
@@ -0,0 +1,23 @@
+from nose.plugins.attrib import attr
+
+from yt.testing import ANSWER_TEST_TAG, fake_random_ds
+from yt.utilities.answer_testing.framework import GenericImageTest
+from yt.visualization.api import SlicePlot
+
+
+@attr(ANSWER_TEST_TAG)
+def test_sliceplot_set_unit_and_zlim():
+    ds = fake_random_ds(16)
+
+    def create_image(filename_prefix):
+        field = ("gas", "density")
+        p = SlicePlot(ds, "z", field)
+        p.set_unit(field, "kg/m**3")
+        p.set_zlim(field, zmin=0)
+        p.save(filename_prefix)
+
+    test = GenericImageTest(ds, create_image, 12)
+    test.prefix = "test_sliceplot_set_unit_and_zlim"
+    test.answer_name = "sliceplot_set_unit_and_zlim"
+    test_sliceplot_set_unit_and_zlim.__name__ = test.description
+    yield test
---
+++ yt/visualization/tests/test_norm_api_set_zlim_and_unit.py
@@ -0,0 +1,23 @@
+from nose.plugins.attrib import attr
+
+from yt.testing import ANSWER_TEST_TAG, fake_random_ds
+from yt.utilities.answer_testing.framework import GenericImageTest
+from yt.visualization.api import SlicePlot
+
+ds = fake_random_ds(16)
+
+
+@attr(ANSWER_TEST_TAG)
+def test_sliceplot_set_zlim_and_unit():
+    def create_image(filename_prefix):
+        field = ("gas", "density")
+        p = SlicePlot(ds, "z", field)
+        p.set_zlim(field, zmin=0)
+        p.set_unit(field, "kg/m**3")
+        p.save(filename_prefix)
+
+    test = GenericImageTest(ds, create_image, 12)
+    test.prefix = "test_sliceplot_set_zlim_and_unit"
+    test.answer_name = "sliceplot_set_zlim_and_unit"
+    test_sliceplot_set_zlim_and_unit.__name__ = test.description
+    yield test
