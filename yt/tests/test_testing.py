3e16e47a89ac685a594847d8df9474be3ba6814e
Clément Robert <cr52@protonmail.com>
2022-05-03 10:14:16 +0200

    TST: rework a meta test so that normal status is PASS instead of skip

--- yt/tests/test_testing.py
+++ yt/tests/test_testing.py
@@ -1,3 +1,5 @@
+from unittest import SkipTest
+
 import matplotlib
 
 from yt.testing import requires_backend
@@ -6,18 +8,25 @@ active_backend = matplotlib.get_backend()
 inactive_backend = ({"gtkagg", "macosx", "wx", "tkagg"} - {active_backend}).pop()
 
 
-@requires_backend(inactive_backend)
 def test_requires_inactive_backend():
-    # this test cannot pass, check that it's always skipped
-    raise AssertionError("@requires_backend appears to be broken (expected skip)")
+    @requires_backend(inactive_backend)
+    def foo():
+        return
+
+    try:
+        foo()
+    except SkipTest:
+        pass
+    else:
+        raise AssertionError(
+            "@requires_backend appears to be broken (skip was expected)"
+        )
 
 
 def test_requires_active_backend():
-    from unittest import SkipTest
-
     @requires_backend(active_backend)
     def foo():
-        assert True
+        return
 
     try:
         foo()
