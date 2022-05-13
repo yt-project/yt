098b4e0e201896b25da22bed6dcf5d3e37ccfd4e
chrishavlin <chris.havlin@gmail.com>
2022-05-13 17:01:26 -0500

    fix merge

--- yt/utilities/io_handler.py
+++ yt/utilities/io_handler.py
@@ -249,22 +249,6 @@ class BaseParticleIOHandler(BaseIOHandler):
                 data_files.update(obj.data_files)
         yield from sorted(data_files, key=lambda x: (x.filename, x.start))
 
-    def _count_particles_chunks(
-        self,
-        psize: DefaultDict[str, int],
-        chunks,
-        ptf: DefaultDict[str, List[str]],
-        selector,
-    ) -> DefaultDict[str, int]:
-        if getattr(selector, "is_all_data", False):
-            for data_file in self._sorted_chunk_iterator(chunks):
-                for ptype in ptf.keys():
-                    psize[ptype] += data_file.total_particles[ptype]
-        else:
-            # we must apply the selector and count the result
-            psize = self._count_selected_particles(psize, chunks, ptf, selector)
-        return psize
-
 
 class IOHandlerExtracted(BaseIOHandler):
 
