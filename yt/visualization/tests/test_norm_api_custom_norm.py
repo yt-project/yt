1e0e2109f635d51538fe4810da29dab11756a90d
Clément Robert <cr52@protonmail.com>
2022-05-19 20:58:25 +0200

    update tests and answers

--- yt/visualization/tests/test_norm_api_custom_norm.py
+++ yt/visualization/tests/test_norm_api_custom_norm.py
@@ -1,40 +1,29 @@
 import matplotlib
-from matplotlib.colors import LogNorm, Normalize, SymLogNorm
 from nose.plugins.attrib import attr
 from packaging.version import Version
 
-from yt.testing import ANSWER_TEST_TAG, fake_random_ds, skip_case
+from yt.testing import ANSWER_TEST_TAG, fake_random_ds, skipif
 from yt.utilities.answer_testing.framework import GenericImageTest
 from yt.visualization.api import SlicePlot
 
 MPL_VERSION = Version(matplotlib.__version__)
 
 
+@skipif(
+    MPL_VERSION < Version("3.2"),
+    reason=f"TwoSlopeNorm requires MPL 3.2, we have {MPL_VERSION}",
+)
 @attr(ANSWER_TEST_TAG)
 def test_sliceplot_custom_norm():
-    if MPL_VERSION < Version("3.4"):
-        skip_case(
-            reason="in MPL<3.4, SymLogNorm emits a deprecation warning "
-            "that cannot be easily filtered"
-        )
-    # don't import this at top level because it's only available since MPL 3.2
     from matplotlib.colors import TwoSlopeNorm
-
-    norms_to_test = [
-        (Normalize(), "linear"),
-        (LogNorm(), "log"),
-        (TwoSlopeNorm(vcenter=0, vmin=-0.5, vmax=1), "twoslope"),
-        (SymLogNorm(linthresh=0.01, vmin=-1, vmax=1), "symlog"),
-    ]
 
     ds = fake_random_ds(16)
 
     def create_image(filename_prefix):
         field = ("gas", "density")
-        for norm, name in norms_to_test:
-            p = SlicePlot(ds, "z", field)
-            p.set_norm(field, norm=norm)
-            p.save(f"{filename_prefix}_{name}")
+        p = SlicePlot(ds, "z", field)
+        p.set_norm(field, norm=(TwoSlopeNorm(vcenter=0, vmin=-0.5, vmax=1)))
+        p.save(f"{filename_prefix}")
 
     test = GenericImageTest(ds, create_image, 12)
     test.prefix = "test_sliceplot_custom_norm"
