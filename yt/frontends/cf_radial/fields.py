2c51f294ce12b10c90fc8ffb65aae8d51c79159d
Clément Robert <cr52@protonmail.com>
2022-05-23 17:20:46 +0200

    TYP: fix a typing error

--- yt/frontends/cf_radial/fields.py
+++ yt/frontends/cf_radial/fields.py
@@ -4,6 +4,7 @@ CF-radial-specific fields
 
 
 """
+from typing import List
 
 from yt.fields.field_info_container import FieldInfoContainer
 
@@ -12,7 +13,7 @@ class CFRadialFieldInfo(FieldInfoContainer):
     known_other_fields = ()  # fields are set dynamically
     known_particle_fields = ()
     units_to_ignore = ("dBz", "dBZ", "ratio")  # set as nondimensional if found
-    field_units_ignored = []  # fields for which units have been ignored
+    field_units_ignored: List[str] = []  # fields for which units have been ignored
 
     # (find, replace) pairs for sanitizing:
     unit_subs = (("degrees", "degree"), ("meters", "m"), ("_per_", "/"))
