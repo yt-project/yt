f6d0d3ced6983a02b5f1c17f9305b6fde89eb93b
Ryan Farber <rjfarber@umich.edu>
2022-05-25 21:06:11 +0200

    switched back to the standard file to load in the cookbook script

--- doc/source/cookbook/simple_projection_methods.py
+++ doc/source/cookbook/simple_projection_methods.py
@@ -1,8 +1,7 @@
 import yt
 
 # Load the dataset.
-fname = "Shattering_V10_hdf5_plt_cnt_2101"
-ds = yt.load("/Users/ryanjsfx/SCP/ReceivedFiles/Shattering/sR1e5/" + fname)
+ds = yt.load("GalaxyClusterMerger/fiducial_1to3_b0.273d_hdf5_plt_cnt_0175")
 
 # Create projections of temperature (with different methods)
 
@@ -10,4 +9,6 @@ ds = yt.load("/Users/ryanjsfx/SCP/ReceivedFiles/Shattering/sR1e5/" + fname)
 for method in ["integrate", "min", "max"]:
     yt.ProjectionPlot(
         ds, "x", ("gas", "temperature"), method=method
-    ).save(fname + "_Projection_x_temperature_" + method + ".png")
+    ).save(method + ".png")
+## note, the default save name doesn't look at the method
+## so otherwise it'd just overwrite each loop through
