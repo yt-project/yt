e5aa19515b4dab84a25ede7cb690beb08093af5f
rjfarber <rjfarber@umich.edu>
2022-05-26 11:36:24 +0200

    Update doc/source/cookbook/simple_projection_methods.py
    custom file name
    
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- doc/source/cookbook/simple_projection_methods.py
+++ doc/source/cookbook/simple_projection_methods.py
@@ -8,6 +8,4 @@ ds = yt.load("GalaxyClusterMerger/fiducial_1to3_b0.273d_hdf5_plt_cnt_0175")
 
 for method in ["integrate", "min", "max"]:
     proj = yt.ProjectionPlot(ds, "x", ("gas", "temperature"), method=method)
-    proj.save()
-## note, the default save name doesn't look at the method
-## so otherwise it'd just overwrite each loop through
+    proj.save(f"projection_method_{method}.png")
