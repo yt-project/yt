13f676ef5447f45c89b79bba5b0d11045fcf2055
chrishavlin <chris.havlin@gmail.com>
2022-06-28 16:46:34 -0500

    switch back conventions check

--- yt/frontends/cf_radial/data_structures.py
+++ yt/frontends/cf_radial/data_structures.py
@@ -308,7 +308,6 @@ class CFRadialDataset(Dataset):
                 for c in [con, con.lower()]:
                     if hasattr(ds, c):
                         cons += getattr(ds, c)
-                        break
                 is_cfrad = "CF/Radial" in cons
         except (OSError, AttributeError, ImportError):
             return False
