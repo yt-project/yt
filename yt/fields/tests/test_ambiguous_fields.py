9d0e036c09aee1df013b54f4847529f9d0c5f345
Clément Robert <cr52@protonmail.com>
2022-07-04 16:59:14 +0200

    TST: check that fields with the same field name can be referenced with full tuple-keys without a warning or an error

--- yt/fields/tests/test_ambiguous_fields.py
+++ yt/fields/tests/test_ambiguous_fields.py
@@ -12,12 +12,16 @@ def test_ambiguous_fails():
         return data["ones"]
 
     # create a pair of ambiguous field
-    ds.add_field(("io", "mock_field"), _mock_field, sampling_type="cell")
+    ds.add_field(("io", "mock_field"), _mock_field, sampling_type="particle")
     ds.add_field(("gas", "mock_field"), _mock_field, sampling_type="cell")
 
     # Test warnings are issued for ambiguous fields
     with pytest.warns(VisibleDeprecationWarning, match=msg.format("mock_field")):
         ds.r["mock_field"]
+
+    # check that explicit name tuples don't raise a warning
+    ds.r["io", "mock_field"]
+    ds.r["gas", "mock_field"]
 
 
 def test_nameonly_field_with_all_aliases_candidates():
