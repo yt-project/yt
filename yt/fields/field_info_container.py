55a467ff29f17d5e4201fa32ccad848d9a6800b1
Clément Robert <cr52@protonmail.com>
2022-08-06 20:13:55 +0200

    BUG: fix a TypeError (None is not subscriptable)

--- yt/fields/field_info_container.py
+++ yt/fields/field_info_container.py
@@ -237,6 +237,7 @@ class FieldInfoContainer(dict):
                 units = node.get("units", "")
                 aliases = node.get("aliases", [])
                 display_name = node.get("display_name", None)
+                args = units, aliases, display_name
 
             # We allow field_units to override this.  First we check if the
             # field *name* is in there, then the field *tuple*.
