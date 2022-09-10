28c194cc54a91f86c5168881d7abb7bdce440594
Clément Robert <cr52@protonmail.com>
2022-09-10 23:58:26 +0200

    migrate test_line_plots to pytest

--- yt/visualization/tests/test_line_plots.py
+++ yt/visualization/tests/test_line_plots.py
@@ -1,8 +1,7 @@
-from nose.plugins.attrib import attr
-from nose.tools import assert_raises
+import pytest
 
 import yt
-from yt.testing import ANSWER_TEST_TAG, assert_equal, fake_random_ds
+from yt.testing import assert_equal, fake_random_ds
 from yt.utilities.answer_testing.framework import GenericImageTest
 from yt.visualization.line_plot import _validate_point
 
@@ -25,41 +24,75 @@ def compare(ds, plot, test_prefix, test_name, decimals=12):
     return test
 
 
-@attr(ANSWER_TEST_TAG)
-def test_line_plot():
-    ds = fake_random_ds(4)
-    fields = [field for field in ds.field_list if field[0] == "stream"]
-    field_labels = {f: f[1] for f in fields}
-    plot = yt.LinePlot(
-        ds, fields, (0, 0, 0), (1, 1, 0), 1000, field_labels=field_labels
-    )
-    plot.annotate_legend(fields[0])
-    plot.annotate_legend(fields[1])
-    plot.set_x_unit("cm")
-    plot.set_unit(fields[0], "kg/cm**3")
-    plot.annotate_title(fields[0], "Density Plot")
-    yield compare(
-        ds, plot, test_prefix="answers_line_plot", test_name="answers_line_plot"
-    )
+class TestLinePlotSimple:
+    @classmethod
+    def setup_class(cls):
+        cls.ds = fake_random_ds(4)
+        fields = [field for field in cls.ds.field_list if field[0] == "stream"]
+        field_labels = {f: f[1] for f in fields}
+        plot = yt.LinePlot(
+            cls.ds, fields, (0, 0, 0), (1, 1, 0), 1000, field_labels=field_labels
+        )
+        plot.annotate_legend(fields[0])
+        plot.annotate_legend(fields[1])
+        plot.set_x_unit("cm")
+        plot.set_unit(fields[0], "kg/cm**3")
+        plot.annotate_title(fields[0], "Density Plot")
+        cls.plot = plot
+
+    @pytest.mark.mpl_image_compare(filename="line_plot_simple_density.png")
+    def test_line_plot_simple_density(self):
+        return self.plot.plots["stream", "density"].figure
+
+    @pytest.mark.mpl_image_compare(filename="line_plot_simple_velocity_x.png")
+    def test_line_plot_simple_velocity_x(self):
+        return self.plot.plots["stream", "velocity_x"].figure
+
+    @pytest.mark.mpl_image_compare(filename="line_plot_simple_velocity_y.png")
+    def test_line_plot_simple_velocity_y(self):
+        return self.plot.plots["stream", "velocity_y"].figure
+
+    @pytest.mark.mpl_image_compare(filename="line_plot_simple_velocity_z.png")
+    def test_line_plot_simple_velocity_z(self):
+        return self.plot.plots["stream", "velocity_z"].figure
 
 
-@attr(ANSWER_TEST_TAG)
-def test_multi_line_plot():
-    ds = fake_random_ds(4)
-    fields = [field for field in ds.field_list if field[0] == "stream"]
-    field_labels = {f: f[1] for f in fields}
-    lines = []
-    lines.append(yt.LineBuffer(ds, [0.25, 0, 0], [0.25, 1, 0], 100, label="x = 0.5"))
-    lines.append(yt.LineBuffer(ds, [0.5, 0, 0], [0.5, 1, 0], 100, label="x = 0.5"))
-    plot = yt.LinePlot.from_lines(ds, fields, lines, field_labels=field_labels)
-    plot.annotate_legend(fields[0])
-    plot.annotate_legend(fields[1])
-    yield compare(
-        ds,
-        plot,
-        test_prefix="answers_multi_line_plot",
-        test_name="answers_multi_line_plot",
-    )
+class TestLinePlotMulti:
+    @classmethod
+    def setup_class(cls):
+        cls.ds = fake_random_ds(4)
+        fields = [field for field in cls.ds.field_list if field[0] == "stream"]
+        field_labels = {f: f[1] for f in fields}
+        lines = []
+        lines.append(
+            yt.LineBuffer(cls.ds, [0.25, 0, 0], [0.25, 1, 0], 100, label="x = 0.5")
+        )
+        lines.append(
+            yt.LineBuffer(cls.ds, [0.5, 0, 0], [0.5, 1, 0], 100, label="x = 0.5")
+        )
+        plot = yt.LinePlot.from_lines(cls.ds, fields, lines, field_labels=field_labels)
+        plot.annotate_legend(fields[0])
+        plot.annotate_legend(fields[1])
+        plot.set_x_unit("cm")
+        plot.set_unit(fields[0], "kg/cm**3")
+        plot.annotate_title(fields[0], "Density Plot")
+        cls.plot = plot
+
+    @pytest.mark.mpl_image_compare(filename="line_plot_multi_density.png")
+    def test_line_plot_multi_density(self):
+        return self.plot.plots["stream", "density"].figure
+
+    @pytest.mark.mpl_image_compare(filename="line_plot_multi_velocity_x.png")
+    def test_line_plot_multi_velocity_x(self):
+        return self.plot.plots["stream", "velocity_x"].figure
+
+    @pytest.mark.mpl_image_compare(filename="line_plot_multi_velocity_y.png")
+    def test_line_plot_multi_velocity_y(self):
+        return self.plot.plots["stream", "velocity_y"].figure
+
+    @pytest.mark.mpl_image_compare(filename="line_plot_multi_velocity_z.png")
+    def test_line_plot_multi_velocity_z(self):
+        return self.plot.plots["stream", "velocity_z"].figure
 
 
 def test_line_buffer():
@@ -77,19 +110,16 @@ def test_line_buffer():
 
 def test_validate_point():
     ds = fake_random_ds(3)
-    with assert_raises(RuntimeError) as ex:
+    with pytest.raises(RuntimeError, match="Input point must be array-like"):
         _validate_point(0, ds, start=True)
-    assert_equal(str(ex.exception), "Input point must be array-like")
 
-    with assert_raises(RuntimeError) as ex:
+    with pytest.raises(RuntimeError, match="Input point must be a 1D array"):
         _validate_point(ds.arr([[0], [1]], "code_length"), ds, start=True)
-    assert_equal(str(ex.exception), "Input point must be a 1D array")
 
-    with assert_raises(RuntimeError) as ex:
+    with pytest.raises(
+        RuntimeError, match="Input point must have an element for each dimension"
+    ):
         _validate_point(ds.arr([0, 1], "code_length"), ds, start=True)
-    assert_equal(
-        str(ex.exception), "Input point must have an element for each dimension"
-    )
 
     ds = fake_random_ds([32, 32, 1])
     _validate_point(ds.arr([0, 1], "code_length"), ds, start=True)
