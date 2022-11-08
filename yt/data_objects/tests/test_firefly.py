a72ead1f373e5ded7b1dd447afdd796388569955
Michael Ryan <mtryan83@gmail.com>
2022-11-08 16:44:18 -0500

    Added/updated tests per comment
    Per PR comment:
      New field tests should be in test_firefly
      Switched to using @pytest.mark.parameter - note this also
     fixes the issue with calling the test function directly
      Switched to using the dataset as a fixture

--- yt/data_objects/tests/test_firefly.py
+++ yt/data_objects/tests/test_firefly.py
@@ -1,6 +1,12 @@
 import tempfile
+from contextlib import nullcontext as dnr
+
+import numpy as np
+import pytest
+from numpy.testing import assert_array_equal
 
 from yt.testing import fake_particle_ds, requires_module
+from yt.utilities.exceptions import YTFieldNotFound
 
 
 @requires_module("Firefly")
@@ -36,3 +42,127 @@ def test_firefly_write_to_disk():
     )
 
     reader.dumpToJSON()
+
+
+@pytest.fixture
+def firefly_test_dataset():
+    # create dataset
+    ds_fields = [
+        # Assumed present
+        ("PartType1", "particle_position_x"),
+        ("PartType1", "particle_position_y"),
+        ("PartType1", "particle_position_z"),
+        ("gas", "particle_position_x"),
+        ("gas", "particle_position_y"),
+        ("gas", "particle_position_z"),
+        # User input
+        ("PartType1", "Masses"),
+        ("gas", "Masses"),
+        ("gas", "Temperature"),
+    ]
+    ds_field_units = [
+        "code_length",
+        "code_length",
+        "code_length",
+        "code_length",
+        "code_length",
+        "code_length",
+        "code_mass",
+        "code_mass",
+        "code_temperature",
+    ]
+    ds_negative = [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+    ]
+    ds = fake_particle_ds(
+        fields=ds_fields,
+        units=ds_field_units,
+        negative=ds_negative,
+    )
+    dd = ds.all_data()
+    return (ds, dd)
+
+
+@requires_module("firefly")
+@pytest.mark.parametrize(
+    "fields_to_include,fields_units,pgs_to_test,expectation",
+    [
+        (None, None, None, dnr()),  # Test default values
+        ([], [], None, dnr()),  # Test empty fields
+        (["Masses"], ["code_mass"], None, dnr()),  # Test common field (Masses)
+        (
+            ["Temperature"],
+            ["code_temperature"],
+            "gas",
+            dnr(),
+        ),  # Test unique field (Temperature)
+        (
+            ["dinos"],
+            ["code_length"],
+            None,
+            pytest.raises(YTFieldNotFound),
+        ),  # Test nonexistent field (dinos)
+        (
+            [("gas", "Temperature")],
+            ["code_temperature"],
+            None,
+            dnr(),
+        ),  # Test existing field tuple (gas, Temperature)
+        (
+            [("PartType1", "Temperature")],
+            ["code_temperature"],
+            None,
+            pytest.raises(YTFieldNotFound),
+        ),  # Test nonexistent field tuple (PartType1, Temperature)
+    ],
+)
+def test_fields_specification(
+    firefly_test_dataset, fields_to_include, fields_units, pgs_to_test, expectation
+):
+    if pgs_to_test is None:
+        pgs_to_test = ["PartType1", "gas"]
+
+    (ds, dd) = firefly_test_dataset
+    with expectation:
+        reader = dd.create_firefly_object(
+            fields_to_include=fields_to_include,
+            fields_units=fields_units,
+            coordinate_units="code_length",
+        )
+    if not isinstance(expectation, dnr):
+        pass
+        return
+    assert_array_equal(
+        dd[("PartType1", "relative_particle_position")].d,
+        reader.particleGroups[0].coordinates,
+    )
+    assert_array_equal(
+        dd[("gas", "relative_particle_position")].d,
+        reader.particleGroups[1].coordinates,
+    )
+    if fields_to_include is None:
+        return
+    all_pgs = ["PartType1", "gas"]
+    for field in fields_to_include:
+        if isinstance(field, tuple):
+            pg = reader.particleGroups[0 if field[0] == "PartType1" else 1]
+            assert field[1] in pg.field_names
+            arrind = np.flatnonzero(pg.field_names == field[1])[0]
+            assert_array_equal(dd[field].d, pg.field_arrays[arrind])
+        else:
+            for ptype in all_pgs:
+                pg = reader.particleGroups[0 if ptype == "PartType1" else 1]
+                if ptype not in pgs_to_test:
+                    assert field not in pg.field_names
+                    continue
+                assert field in pg.field_names
+                arrind = np.flatnonzero(pg.field_names == field)[0]
+                assert_array_equal(dd[ptype, field].d, pg.field_arrays[arrind])
