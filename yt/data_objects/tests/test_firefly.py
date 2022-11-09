c18f2a63255a2cf0524c390ce756ef2e22169284
Michael Ryan <mtryan83@gmail.com>
2022-11-09 16:16:14 -0500

    Split test into three categories: field strings, field tuples, and mix

--- yt/data_objects/tests/test_firefly.py
+++ yt/data_objects/tests/test_firefly.py
@@ -114,21 +114,9 @@ def firefly_test_dataset():
             None,
             pytest.raises(YTFieldNotFound),
         ),  # Test nonexistent field (dinos)
-        (
-            [("gas", "Temperature")],
-            ["code_temperature"],
-            None,
-            does_not_raise(),
-        ),  # Test existing field tuple (gas, Temperature)
-        (
-            [("PartType1", "Temperature")],
-            ["code_temperature"],
-            None,
-            pytest.raises(YTFieldNotFound),
-        ),  # Test nonexistent field tuple (PartType1, Temperature)
     ],
 )
-def test_fields_specification(
+def test_field_string_specification(
     firefly_test_dataset, fields_to_include, fields_units, pgs_to_test, expectation
 ):
     if pgs_to_test is None:
@@ -156,17 +144,87 @@ def test_fields_specification(
         return
     all_pgs = ["PartType1", "gas"]
     for field in fields_to_include:
-        if isinstance(field, tuple):
-            pg = reader.particleGroups[0 if field[0] == "PartType1" else 1]
-            assert field[1] in pg.field_names
-            arrind = np.flatnonzero(pg.field_names == field[1])[0]
-            assert_array_equal(dd[field].d, pg.field_arrays[arrind])
-        else:
-            for ptype in all_pgs:
-                pg = reader.particleGroups[0 if ptype == "PartType1" else 1]
-                if ptype not in pgs_to_test:
-                    assert field not in pg.field_names
-                    continue
-                assert field in pg.field_names
-                arrind = np.flatnonzero(pg.field_names == field)[0]
-                assert_array_equal(dd[ptype, field].d, pg.field_arrays[arrind])
+        for ptype in all_pgs:
+            pg = reader.particleGroups[0 if ptype == "PartType1" else 1]
+            if ptype not in pgs_to_test:
+                assert field not in pg.field_names
+                continue
+            assert field in pg.field_names
+            arrind = np.flatnonzero(pg.field_names == field)[0]
+            assert_array_equal(dd[ptype, field].d, pg.field_arrays[arrind])
+
+
+@requires_module("firefly")
+@pytest.mark.parametrize(
+    "fields_to_include,fields_units,expectation",
+    [
+        (
+            [("gas", "Temperature")],
+            ["code_temperature"],
+            does_not_raise(),
+        ),  # Test existing field tuple (gas, Temperature)
+        (
+            [("PartType1", "Temperature")],
+            ["code_temperature"],
+            pytest.raises(YTFieldNotFound),
+        ),  # Test nonexistent field tuple (PartType1, Temperature)
+    ],
+)
+def test_field_tuple_specification(
+    firefly_test_dataset, fields_to_include, fields_units, expectation
+):
+
+    dd = firefly_test_dataset.all_data()
+    with expectation:
+        reader = dd.create_firefly_object(
+            fields_to_include=fields_to_include,
+            fields_units=fields_units,
+            coordinate_units="code_length",
+        )
+    if not isinstance(expectation, does_not_raise):
+        # constructed like this so we don't need to import RaiseError from pytest
+        return
+    assert_array_equal(
+        dd[("PartType1", "relative_particle_position")].d,
+        reader.particleGroups[0].coordinates,
+    )
+    assert_array_equal(
+        dd[("gas", "relative_particle_position")].d,
+        reader.particleGroups[1].coordinates,
+    )
+    all_pgs = reader.particleGroups
+    all_pgs_names = ["PartType1", "gas"]
+    for field in fields_to_include:
+        ftype, fname = field
+        for pgi in range(2):
+            pg = all_pgs[pgi]
+            if ftype == all_pgs_names[pgi]:
+                assert fname in pg.field_names
+                arrind = np.flatnonzero(pg.field_names == fname)[0]
+                assert_array_equal(dd[field].d, pg.field_arrays[arrind])
+            else:
+                assert fname not in pg.field_names
+
+
+def test_field_mixed_specification(firefly_test_dataset):
+    dd = firefly_test_dataset.all_data()
+
+    reader = dd.create_firefly_object(
+        fields_to_include=["Masses", ("gas", "Temperature")],
+        fields_units=["code_mass", "code_temperature"],
+        coordinate_units="code_length",
+    )
+
+    PartType1 = reader.particleGroups[0]
+    gas = reader.particleGroups[1]
+    assert "Masses" in PartType1.field_names
+    arrind = np.flatnonzero(PartType1.field_names == "Masses")[0]
+    assert_array_equal(dd[("PartType1", "Masses")].d, PartType1.field_arrays[arrind])
+    assert "Masses" in gas.field_names
+    arrind = np.flatnonzero(gas.field_names == "Masses")[0]
+    assert_array_equal(dd[("gas", "Masses")].d, gas.field_arrays[arrind])
+
+    assert "Temperature" not in PartType1.field_names
+    assert "Temperature" in gas.field_names
+    arrind = np.flatnonzero(gas.field_names == "Temperature")[0]
+    assert_array_equal(dd[("gas", "Temperature")].d, gas.field_arrays[arrind])
