adc60f253b5bc1f4722beebd1497435f48977d2c
Clément Robert <cr52@protonmail.com>
2022-11-18 22:15:17 +0100

    Backport PR #4221: CVE-2007-4559 Patch

--- yt/loaders.py
+++ yt/loaders.py
@@ -1478,7 +1478,26 @@ def load_sample(
     if tarfile.is_tarfile(tmp_file):
         mylog.info("Untaring downloaded file to '%s'", save_dir)
         with tarfile.open(tmp_file) as fh:
-            fh.extractall(save_dir)
+
+            def is_within_directory(directory, target):
+
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+
+                return prefix == abs_directory
+
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+
+                tar.extractall(path, members, numeric_owner=numeric_owner)
+
+            safe_extract(fh, save_dir)
         os.remove(tmp_file)
     else:
         os.replace(tmp_file, os.path.join(save_dir, fn))
