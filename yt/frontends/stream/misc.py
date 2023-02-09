5d4b5d651519de1e59877c54f5d4950b0735417a
Chris Havlin <chris.havlin@gmail.com>
2023-02-09 09:29:17 -0600

    Apply suggestions from code review
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- yt/frontends/stream/misc.py
+++ yt/frontends/stream/misc.py
@@ -8,28 +8,18 @@ from yt._typing import DomainDimensions
 def _validate_cell_widths(
     cell_widths: List[np.ndarray],
     domain_dimensions: DomainDimensions,
-) -> List[List[np.ndarray],]:
+) -> List[List[np.ndarray]]:
     # check dimensionality
-    ndims = len(domain_dimensions)
-    nwids = len(cell_widths)
-    if nwids != ndims:
+    if (nwids := len(cell_widths)) != (ndims := len(domain_dimensions)):
         raise ValueError(
             f"The number of elements in cell_widths ({nwids}) "
             f"must match the number of dimensions ({ndims})."
         )
 
     # check the dtypes for each dimension, upcast to float64 if needed
-    cast_dims = []
-    for idim, cell_wid in enumerate(cell_widths):
-        if cell_wid.dtype != np.float64:
-            cast_dims.append(idim)
-
-    for idim in cast_dims:
-        cell_widths[idim] = cell_widths[idim].astype(np.float64)
+    for idim in len(cell_widths):
+        cell_widths[idim] = cell_widths[idim].astype(np.float64, copy=False)
 
     # finally, need to return a list of the cell_widths for each grid object.
     # since there is only a single grid, just wrap it in a list.
-    cell_widths_out = [
-        cell_widths,
-    ]
-    return cell_widths_out
+    return [cell_widths]
