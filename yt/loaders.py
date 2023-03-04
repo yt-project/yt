52e03883a81057c2e17dd2afcce3d6dd1245c3cc
Suoqing Ji <jisuoqing@gmail.com>
2023-03-04 21:23:05 +0800

    fix typo

--- yt/loaders.py
+++ yt/loaders.py
@@ -991,7 +991,7 @@ def load_hexahedral_mesh(
         parameters=parameters,
     )
 
-    handler.basename = basename  # type: ignore [attr-defined]
+    handler.name = basename  # type: ignore [attr-defined]
     handler.domain_left_edge = domain_left_edge  # type: ignore [attr-defined]
     handler.domain_right_edge = domain_right_edge  # type: ignore [attr-defined]
     handler.refine_by = 2  # type: ignore [attr-defined]
