612dbf2bf00afa9071b7c6de48e15234b80de131
John ZuHone <jzuhone@gmail.com>
2023-05-01 14:41:32 -0400

    Simplify test

--- yt/geometry/tests/test_particle_octree.py
+++ yt/geometry/tests/test_particle_octree.py
@@ -411,63 +411,28 @@ def test_ewah_write_load(tmp_path):
     mock_file = tmp_path / "halo_59.hdf5"
     mock_file.symlink_to(_get_test_data_dir_path() / "TNGHalo" / "halo_59.hdf5")
 
-    ds = load(mock_file)
-    _, c = ds.find_max(("gas", "density"))
-    assert ds.index.pii.mutable_index
-    assert ds.index.pii.order1 == 6
-    assert ds.index.pii.order2_orig == 2
-    assert ds.index.pii.order2 == 4
-    assert not ds.index.pii.loaded
-    c += ds.quan(0.5, "Mpc")
-    sp = ds.sphere(c, (20.0, "kpc"))
-    mass = sp.sum(("gas", "mass"))
+    masses = []
+    opts = [
+        (None, True, (6, 2, 4), False),
+        (None, True, (6, 2, 4), True),
+        ((5, 3), False, (5, 3, 3), False),
+        ((5, 3), False, (5, 3, 3), True),
+    ]
 
-    del ds
+    for opt in opts:
+        ds = load(mock_file, index_order=opt[0])
+        _, c = ds.find_max(("gas", "density"))
+        assert ds.index.pii.mutable_index is opt[1]
+        assert ds.index.pii.order1 == opt[2][0]
+        assert ds.index.pii.order2_orig == opt[2][1]
+        assert ds.index.pii.order2 == opt[2][2]
+        assert ds.index.pii._is_loaded is opt[3]
+        c += ds.quan(0.5, "Mpc")
+        sp = ds.sphere(c, (20.0, "kpc"))
+        mass = sp.sum(("gas", "mass"))
+        masses.append(mass)
 
-    ds2 = load(mock_file)
-    _, c2 = ds2.find_max(("gas", "density"))
-    assert ds2.index.pii.mutable_index
-    assert ds2.index.pii.order1 == 6
-    assert ds2.index.pii.order2_orig == 2
-    assert ds2.index.pii.order2 == 4
-    assert ds2.index.pii.loaded
-    c2 += ds2.quan(0.5, "Mpc")
-    sp2 = ds2.sphere(c, (20.0, "kpc"))
-    mass2 = sp2.sum(("gas", "mass"))
-
-    assert_array_equal(mass, mass2)
-
-    del ds2
-
-    ds3 = load(mock_file, index_order=(5, 3))
-    _, c3 = ds3.find_max(("gas", "density"))
-    assert not ds3.index.pii.mutable_index
-    assert ds3.index.pii.order1 == 5
-    assert ds3.index.pii.order2_orig == 3
-    assert ds3.index.pii.order2 == 3
-    assert not ds3.index.pii.loaded
-    c3 += ds3.quan(0.5, "Mpc")
-    sp3 = ds3.sphere(c, (20.0, "kpc"))
-    mass3 = sp3.sum(("gas", "mass"))
-
-    assert_array_equal(mass, mass3)
-
-    del ds3
-
-    ds4 = load(mock_file, index_order=(5, 3))
-    _, c4 = ds4.find_max(("gas", "density"))
-    assert not ds4.index.pii.mutable_index
-    assert ds4.index.pii.order1 == 5
-    assert ds4.index.pii.order2_orig == 3
-    assert ds4.index.pii.order2 == 3
-    assert ds4.index.pii.loaded
-    c4 += ds4.quan(0.5, "Mpc")
-    sp4 = ds4.sphere(c, (20.0, "kpc"))
-    mass4 = sp4.sum(("gas", "mass"))
-
-    assert_array_equal(mass, mass4)
-
-    del ds4
+    assert_array_equal(masses[0], masses[1:])
 
 
 def cell_centers(order, left_edge, right_edge):
