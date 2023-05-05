e4e7db446c116f4246303b0dd67b1ea67db94bfc
Corentin Cadiou <contact@cphyc.me>
2023-05-05 14:18:25 +0200

    Take @neutrinoceros comments into account
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- yt/utilities/tests/test_cython_fortran_utils.py
+++ yt/utilities/tests/test_cython_fortran_utils.py
@@ -11,15 +11,13 @@ def test_raise_error_when_file_does_not_exist():
         FortranFile("/this/file/does/not/exist")
 
 
-def test_read(tmpdir):
-    dummy_file = os.path.join(tmpdir, "test.bin")
+def test_read(tmp_path):
+    dummy_file = tmp_path / "test.bin"
     # Write a Fortran-formatted file containing one record with 4 doubles
-    with open(dummy_file, "bw") as f:
-        f.write(
-            b" \x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00@\x00\x00\x00\x00\x00\x00\x08@\x00\x00\x00\x00\x00\x00\x10@ \x00\x00\x00"
-        )
-
-    with FortranFile(dummy_file) as f:
+    dummy_file.write_bytes(
+        b" \x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00@\x00\x00\x00\x00\x00\x00\x08@\x00\x00\x00\x00\x00\x00\x10@ \x00\x00\x00"
+    )
+    with FortranFile(str(dummy_file)) as f:
         np.testing.assert_equal(
             f.read_vector("d"),
             [1.0, 2.0, 3.0, 4.0],
