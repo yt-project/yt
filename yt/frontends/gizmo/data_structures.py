56e748c7879d8ed9010b0569f0f08dc3729334c6
Michael Ryan <mtryan83@gmail.com>
2023-06-04 15:58:11 -0400

    Two tweaks for updated GIZMO file snapshots
    dmetal check - gizmo runs with METALS enabled but all of
    	COOL_METAL_LINES_BY_SPECIES, GALSF_FB_FIRE_RPROCESS,
    	GALSF_FB_FIRE_AGE_TRACERS, and STARFORGE_FEEDBACK_TRACERS disabled will
    	have a metallicity field (aka dmetal) a length 1. This change accounts for
    	this possibility
    
    Overriding Gadget _parse_parameter_file - newer versions of GIZMO have changed
    	some of the header fields used by yt for determining the cosmology of the
    	simulation. Moving this function allows for improved cosmology detection.
    	Now check if ComovingIntegrationOn flag exists - if so, use that to
    	determine if cosmological run and pull cosmological parameters using
    	updated names. Otherwise, fall back to checking for OmegaLambda and setting
    	cosmological flag from that.

--- yt/frontends/gizmo/data_structures.py
+++ yt/frontends/gizmo/data_structures.py
@@ -1,6 +1,11 @@
 import os
 
+import numpy as np
+
 from yt.frontends.gadget.data_structures import GadgetHDF5Dataset
+from yt.funcs import only_on_root
+from yt.utilities.cosmology import Cosmology
+from yt.utilities.logger import ytLogger as mylog
 from yt.utilities.on_demand_imports import _h5py as h5py
 
 from .fields import GizmoFieldInfo
@@ -39,7 +44,7 @@ class GizmoDataset(GadgetHDF5Dataset):
                 vg in fh["/"] for vg in veto_groups
             )
             dmetal = "/PartType0/Metallicity"
-            if dmetal not in fh or fh[dmetal].shape[1] < 11:
+            if dmetal not in fh or (fh[dmetal].ndim > 1 and fh[dmetal].shape[1] < 11):
                 valid = False
             fh.close()
         except Exception:
@@ -49,3 +54,108 @@ class GizmoDataset(GadgetHDF5Dataset):
 
     def _set_code_unit_attributes(self):
         super()._set_code_unit_attributes()
+
+    def _parse_parameter_file(self):
+        hvals = self._get_hvals()
+
+        self.dimensionality = 3
+        self.refine_by = 2
+        self.parameters["HydroMethod"] = "sph"
+        # Set standard values
+
+        # We may have an overridden bounding box.
+        if self.domain_left_edge is None and hvals["BoxSize"] != 0:
+            self.domain_left_edge = np.zeros(3, "float64")
+            self.domain_right_edge = np.ones(3, "float64") * hvals["BoxSize"]
+
+        self.domain_dimensions = np.ones(3, "int32")
+        self._periodicity = (True, True, True)
+
+        self.cosmological_simulation = 1
+
+        try:
+            self.current_redshift = hvals["Redshift"]
+        except KeyError:
+            # Probably not a cosmological dataset, we should just set
+            # z = 0 and let the user know
+            self.current_redshift = 0.0
+            only_on_root(mylog.info, "Redshift is not set in Header. Assuming z=0.")
+
+        try:
+            # The current version of GIZMO has updated the names of the Omegas
+            # to include an _, added baryons and radiation and added the
+            # ComovingIntegrationOn field. ComovingIntegrationOn is always set,
+            # but the Omega's are only included if ComovingIntegrationOn is true
+            if "ComovingIntegrationOn" in hvals:
+                self.cosmological_simulation = hvals["ComovingIntegrationOn"]
+                self.omega_lambda = hvals["Omega_Lambda"]
+                self.omega_matter = hvals["Omega_Matter"]
+                self.omega_baryon = hvals["Omega_Baryon"]
+                self.omega_radiation = hvals["Omega_Radiation"]
+                self.hubble_constant = hvals["HubbleParam"]
+            else:
+                # Should still support old GIZMO versions too
+                self.omega_lambda = hvals["OmegaLambda"]
+                self.omega_matter = hvals["Omega0"]
+                self.hubble_constant = self.omega_lambda == 0.0
+        except KeyError:
+            # If these are not set it is definitely not a cosmological dataset.
+            self.omega_lambda = 0.0
+            self.omega_matter = 1.0  # Just in case somebody asks for it.
+            self.cosmological_simulation = 0
+            # Hubble is set below for Omega Lambda = 0.
+
+        # Since new versions of GIZMO output the ComovingIntegrationOn flag
+        # we will rely on its presence to determine if this is a cosmological
+        # dataset.
+        if not self.cosmological_simulation:
+            only_on_root(
+                mylog.info,
+                "ComovingIntegrationOn != 1 or (not found "
+                "and OmegaLambda is 0.0), so we are turning off Cosmology.",
+            )
+            self.hubble_constant = 1.0  # So that scaling comes out correct
+            self.cosmological_simulation = 0
+            self.current_redshift = 0.0
+            # This may not be correct.
+            self.current_time = hvals["Time"]
+        else:
+            # Now we calculate our time based on the cosmology, because in
+            # ComovingIntegration hvals["Time"] will in fact be the expansion
+            # factor, not the actual integration time, so we re-calculate
+            # global time from our Cosmology.
+            cosmo = Cosmology(
+                hubble_constant=self.hubble_constant,
+                omega_matter=self.omega_matter,
+                omega_lambda=self.omega_lambda,
+            )
+            self.current_time = cosmo.lookback_time(self.current_redshift, 1e6)
+            only_on_root(
+                mylog.info,
+                "Calculating time from %0.3e to be %0.3e seconds",
+                hvals["Time"],
+                self.current_time,
+            )
+        self.parameters = hvals
+
+        prefix = os.path.abspath(
+            os.path.join(
+                os.path.dirname(self.parameter_filename),
+                os.path.basename(self.parameter_filename).split(".", 1)[0],
+            )
+        )
+
+        if hvals["NumFiles"] > 1:
+            for t in (
+                f"{prefix}.%(num)s{self._suffix}",
+                f"{prefix}.gad.%(num)s{self._suffix}",
+            ):
+                if os.path.isfile(t % {"num": 0}):
+                    self.filename_template = t
+                    break
+            else:
+                raise RuntimeError("Could not determine correct data file template.")
+        else:
+            self.filename_template = self.parameter_filename
+
+        self.file_count = hvals["NumFiles"]
