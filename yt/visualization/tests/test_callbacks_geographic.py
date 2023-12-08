c9839cdd9bacc797246fefd855ae43c8f8718524
Chris Havlin <chris.havlin@gmail.com>
2023-12-08 16:28:01 -0600

    image comparison test

--- yt/visualization/tests/test_callbacks_geographic.py
+++ yt/visualization/tests/test_callbacks_geographic.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pytest
 
-from yt import SlicePlot
+from yt import SlicePlot, load_uniform_grid
 from yt.testing import assert_fname, fake_amr_ds, requires_module
 
 
@@ -18,3 +19,52 @@ def test_quiver_callback_geographic(geometry, tmp_path):
         slc.annotate_quiver(("stream", "velocity_ew"), ("stream", "velocity_ns"))
         slc.save(tmpfi)
         assert_fname(tmpfi)
+
+
+@pytest.fixture(scope="module")
+def ds_geo_uni_grid():
+    yc = 0.0
+    xc = 0.0
+
+    def _vel_calculator(grid, ax):
+        y_lat = grid.fcoords[:, 1].d
+        x_lon = grid.fcoords[:, 2].d
+        x_lon[x_lon > 180] = x_lon[x_lon > 180] - 360.0
+        dist = np.sqrt((y_lat - yc) ** 2 + (xc - x_lon) ** 2)
+        if ax == 1:
+            sgn = np.sign(y_lat - yc)
+        elif ax == 2:
+            sgn = np.sign(x_lon - xc)
+
+        vel = np.exp(-((dist / 45) ** 2)) * sgn
+        return vel.reshape(grid.shape)
+
+    def _calculate_u(grid, field):
+        return _vel_calculator(grid, 2)
+
+    def _calculate_v(grid, field):
+        return _vel_calculator(grid, 1)
+
+    data = {"u_vel": _calculate_u, "v_vel": _calculate_v}
+
+    bbox = [[10.0, 1000], [-90, 90], [0, 360]]
+    bbox = np.array(bbox)
+
+    ds = load_uniform_grid(
+        data,
+        (16, 16, 32),
+        bbox=bbox,
+        geometry="geographic",
+        axis_order=("altitude", "latitude", "longitude"),
+    )
+
+    return ds
+
+
+@pytest.mark.mpl_image_compare
+def test_geoquiver_answer(ds_geo_uni_grid):
+    slc = SlicePlot(ds_geo_uni_grid, "altitude", "u_vel")
+    slc.set_log("u_vel", False)
+    slc.annotate_quiver("u_vel", "v_vel", scale=50)
+    slc.render()
+    return slc.plots["u_vel"].figure
