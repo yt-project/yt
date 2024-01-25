72323522b9c31369735b7dfedd742b341501dcdf
chrishavlin <chris.havlin@gmail.com>
2024-01-25 13:18:05 -0600

    revert the failing test location for clarity

--- yt/visualization/volume_rendering/tests/test_scene.py
+++ yt/visualization/volume_rendering/tests/test_scene.py
@@ -5,8 +5,9 @@ from unittest import TestCase
 
 import numpy as np
 
-from yt.testing import assert_fname, fake_random_ds
+from yt.testing import assert_fname, fake_random_ds, fake_vr_orientation_test_ds
 from yt.visualization.volume_rendering.api import (
+    create_scene,
     create_volume_source,
     volume_render,
 )
@@ -76,3 +77,30 @@ class RotationTest(TestCase):
         sc.render()
         sc.save(fname, sigma_clip=6.0, render=False)
         assert_fname(fname)
+
+
+def test_annotations():
+    from matplotlib.image import imread
+
+    curdir = os.getcwd()
+    tmpdir = tempfile.mkdtemp()
+    os.chdir(tmpdir)
+    ds = fake_vr_orientation_test_ds(N=16)
+    sc = create_scene(ds)
+    sc.annotate_axes()
+    sc.annotate_domain(ds)
+    sc.render()
+    # ensure that there are actually red, green, blue, and white pixels
+    # in the image. see Issue #1595
+    im = sc._last_render
+    for c in ([1, 0, 0, 1], [0, 1, 0, 1], [0, 0, 1, 1], [1, 1, 1, 1]):
+        assert np.where((im == c).all(axis=-1))[0].shape[0] > 0
+    sc[0].tfh.tf.add_layers(10, colormap="cubehelix")
+    sc.save_annotated(
+        "test_scene_annotated.png",
+        text_annotate=[[(0.1, 1.05), "test_string"]],
+    )
+    image = imread("test_scene_annotated.png")
+    assert image.shape == sc.camera.resolution + (4,)
+    os.chdir(curdir)
+    shutil.rmtree(tmpdir)
