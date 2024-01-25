b1a2cf206caf2468aeaa1976fe1d57d99e8f28a3
chrishavlin <chris.havlin@gmail.com>
2024-01-25 15:41:07 -0600

    try a different tmp dir

--- yt/data_objects/tests/test_firefly.py
+++ yt/data_objects/tests/test_firefly.py
@@ -7,11 +7,14 @@ from yt.utilities.exceptions import YTFieldNotFound
 
 
 @requires_module("firefly")
-def test_firefly_JSON_string():
+def test_firefly_JSON_string(tmp_path):
+    tmpdir = str(
+        tmp_path / "firefly_data"
+    )  # create_firefly_object needs a str, not PosixPath
     ds = fake_particle_ds()
     ad = ds.all_data()
     reader = ad.create_firefly_object(
-        None,
+        tmpdir,
         velocity_units="cm/s",
         coordinate_units="cm",
     )
