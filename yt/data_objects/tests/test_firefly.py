e3ff5afd09acfbe431e74f66b4e99e461dc70d54
chrishavlin <chris.havlin@gmail.com>
2024-01-25 15:01:39 -0600

    use a file prefix

--- yt/data_objects/tests/test_firefly.py
+++ yt/data_objects/tests/test_firefly.py
@@ -7,15 +7,14 @@ from yt.utilities.exceptions import YTFieldNotFound
 
 
 @requires_module("firefly")
-def test_firefly_JSON_string(tmp_path):
-    tmpdir = str(tmp_path)
-
+def test_firefly_JSON_string():
     ds = fake_particle_ds()
     ad = ds.all_data()
     reader = ad.create_firefly_object(
-        tmpdir,
+        datadir=None,
         velocity_units="cm/s",
         coordinate_units="cm",
+        file_prefix="hello_there",
     )
 
     reader.writeToDisk(write_to_disk=False, file_extension=".json")
