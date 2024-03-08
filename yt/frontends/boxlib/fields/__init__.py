4a4208813b57ef578190b1d5701e50e22a471f34
Henry Jones <hjones@kong.dhcp.lbl.gov>
2024-03-08 11:56:15 -0800

    Issue deprecation warnings

--- yt/frontends/boxlib/fields/__init__.py
+++ yt/frontends/boxlib/fields/__init__.py
@@ -5,3 +5,12 @@ from ...amrex.fields import (
     NyxFieldInfo,
     WarpXFieldInfo,
 )
+from yt._maintenance.deprecation import issue_deprecation_warning
+
+issue_deprecation_warning(
+    "The historic 'boxlib' frontend is \n"
+    "deprecated as it has been renamed 'amrex'. "
+    "Future work should reference the 'amrex' frontend.",
+    stacklevel=3,
+    since="TBD",
+)
--- yt/frontends/boxlib/tests/__init__.py
+++ yt/frontends/boxlib/tests/__init__.py
@@ -1,1 +1,10 @@
 from ...amrex import tests
+from yt._maintenance.deprecation import issue_deprecation_warning
+
+issue_deprecation_warning(
+    "The historic 'boxlib' frontend is \n"
+    "deprecated as it has been renamed 'amrex'. "
+    "Future work should reference the 'amrex' frontend.",
+    stacklevel=3,
+    since="TBD",
+)
