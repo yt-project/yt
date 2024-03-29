a63ea77ddad62ac56f2b61863e3bd5025c393f2b
Henry Jones <hjones@kong.dhcp.lbl.gov>
2024-03-29 15:54:46 -0700

    Make test_imports() reload modules

--- yt/frontends/boxlib/tests/test_boxlib_deprecation.py
+++ yt/frontends/boxlib/tests/test_boxlib_deprecation.py
@@ -1,13 +1,15 @@
+from importlib import import_module, reload
+
 from yt._maintenance.deprecation import warnings
 
 
 def test_imports():
     with warnings.catch_warnings(record=True) as w:
-        from yt.frontends.boxlib import (
-            data_structures,
-            fields,
-            io,
-        )
+        warnings.simplefilter("always")
+        for index, mname in enumerate(["data_structures", "fields", "io"]):
+            mod_name = import_module("yt.frontends.boxlib." + mname)
+            if len(w) != index + 1:
+                reload(mod_name)
 
         assert len(w) == 3 and all(
             [
@@ -16,5 +18,4 @@ def test_imports():
                 issubclass(w[2].category, DeprecationWarning),
             ]
         )
-    del (data_structures, io, fields)
     warnings.resetwarnings()
