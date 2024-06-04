6ca5f291ffbcb60fbbeea8e2e215fa2407d0770c
smsutherland <sagan@farthinghalearms.com>
2024-06-04 15:02:41 -0400

    fix: SWIFT field naming

--- yt/frontends/swift/fields.py
+++ yt/frontends/swift/fields.py
@@ -1,5 +1,5 @@
-from yt.frontends.sph.fields import SPHFieldInfo
 from yt._typing import KnownFieldsT
+from yt.frontends.sph.fields import SPHFieldInfo
 
 
 class SwiftFieldInfo(SPHFieldInfo):
@@ -7,10 +7,13 @@ class SwiftFieldInfo(SPHFieldInfo):
         ("Coordinates", ("code_length", ["particle_position"], None)),
         ("Velocities", ("code_velocity", ["particle_velocity"], None)),
         ("ParticleIDs", ("", ["particle_index"], None)),
-        ("Masses", ("code_mass", ["particle_mass"], None)),
-        ("InternalEnergy", ("code_specific_energy", ["specific_thermal_energy"], None)),
-        ("Density", ("code_mass / code_length**3", ["density"], None)),
-        ("SmoothingLength", ("code_length", ["smoothing_length"], None)),
+        ("Mass", ("code_mass", ["particle_mass", "mass"], None)),
+        (
+            "InternalEnergies",
+            ("code_specific_energy", ["specific_thermal_energy"], None),
+        ),
+        ("Densities", ("code_mass / code_length**3", ["density"], None)),
+        ("SmoothingLengths", ("code_length", ["smoothing_length"], None)),
     )
 
     def __init__(self, ds, field_list, slice_info=None):
