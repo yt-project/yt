6155475d293756f2fe12748f3a8b9f3bfb7344c3
Jordi De Jonghe, PhD <jordi.dejonghe@outlook.com>
2024-07-02 13:54:02 +0100

    Inline He abundance
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- yt/frontends/amrvac/data_structures.py
+++ yt/frontends/amrvac/data_structures.py
@@ -378,10 +378,7 @@ class AMRVACDataset(Dataset):
         # get self.length_unit if overrides are supplied, otherwise use default
         length_unit = getattr(self, "length_unit", self.quan(1, "cm"))
         namelist = read_amrvac_namelist(self._parfiles)
-        if "mhd_list" in namelist:
-            He_abundance = namelist["mhd_list"].get("he_abundance", 0.1)
-        else:
-            He_abundance = 0.1  # default
+        He_abundance = namelist.get("mhd_list", {}).get("he_abundance", 0.1)
 
         # 1. calculations for mass, density, numberdensity
         if "mass_unit" in self.units_override:
