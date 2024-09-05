851d57e418c1288569d21e1e374877057421cfcd
Chris Havlin <chris.havlin@gmail.com>
2024-09-05 11:08:27 -0500

    fix typo in platform_dep.h comment

--- yt/utilities/lib/platform_dep.h
+++ yt/utilities/lib/platform_dep.h
@@ -3,7 +3,7 @@
 #include "malloc.h"
 /*
 note: the following implicitly sets a mininum VS version: conservative
-minumum is _MSC_VER >= 1928 (VS 2019, 16.8), but may work for VS 2015
+minimum is _MSC_VER >= 1928 (VS 2019, 16.8), but may work for VS 2015
 but that has not been tested. see https://github.com/yt-project/yt/pull/4980
 and https://learn.microsoft.com/en-us/cpp/overview/visual-cpp-language-conformance
 */
