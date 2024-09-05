b5932b637ffaa66946a4245bf08f002d3f1b4641
chrishavlin <chris.havlin@gmail.com>
2024-09-05 10:07:19 -0500

    update windows platform_dep comment

--- yt/utilities/lib/platform_dep.h
+++ yt/utilities/lib/platform_dep.h
@@ -1,7 +1,12 @@
 #include <math.h>
 #ifdef MS_WIN32
 #include "malloc.h"
-// note: the following implicitly requires _MSC_VER >= 1928 (VS 2019, 16.8)
+/*
+note: the following implicitly sets a mininum VS version: conservative
+minumum is _MSC_VER >= 1928 (VS 2019, 16.8), but may work for VS 2015
+but that has not been tested. see https://github.com/yt-project/yt/pull/4980
+and https://learn.microsoft.com/en-us/cpp/overview/visual-cpp-language-conformance
+*/
 #include <float.h>
 #include <stdint.h>
 #elif defined(__FreeBSD__)
