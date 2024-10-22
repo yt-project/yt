5fe77008803425938822db1efbf34019946c016b
barneygale <barney.gale@gmail.com>
2024-10-22 20:01:59 +0100

    Stop using deprecated `urllib.request.FancyURLopener`
    Replace use of `urllib.request.FancyURLopener` with `urlretrieve()`. The
    former has been deprecated since Python 3.3.

--- yt/funcs.py
+++ yt/funcs.py
@@ -618,14 +618,15 @@ def fancy_download_file(url, filename, requests=None):
 
 def simple_download_file(url, filename):
     import urllib.request
+    import urllib.error
 
-    class MyURLopener(urllib.request.FancyURLopener):
-        def http_error_default(self, url, fp, errcode, errmsg, headers):
-            raise RuntimeError(
-                f"Attempt to download file from {url} failed with error {errcode}: {errmsg}."
-            )
+    try:
+        fn, h = urllib.request.urlretrieve(url, filename)
+    except urllib.error.HTTPError as err:
+        raise RuntimeError(
+            f"Attempt to download file from {url} failed with error {err.code}: {err.msg}."
+        ) from None
 
-    fn, h = MyURLopener().retrieve(url, filename)
     return fn
 
 
