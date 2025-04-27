932b7c46d8c545caad9ec30ea45a2e9ee2b0454e
Rongjun Huang <u6569836@anu.edu.au>
2025-04-27 16:08:56 +1000

    test_quokka (#19)
    update the `yt/frontends/amrex/tests/test_outputs.py` to run pytest for
    quokka dataset
    
    ---------
    
    Co-authored-by: Rongjun-ANU <Rongjun.Huang@alumni.anu.edu.au>
    Co-authored-by: ChongChong He <chongchong.he@anu.edu.au>

--- yt/frontends/amrex/tests/test_outputs.py
+++ yt/frontends/amrex/tests/test_outputs.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from numpy.testing import assert_allclose, assert_equal
 
@@ -7,6 +8,7 @@ from yt.frontends.amrex.api import (
     MaestroDataset,
     NyxDataset,
     OrionDataset,
+    QuokkaDataset,
     WarpXDataset,
 )
 from yt.loaders import load
@@ -443,3 +445,65 @@ def test_maestro_parameters():
     # Check an int parameter
     assert ds.parameters["s0_interp_type"] == 3
     assert type(ds.parameters["s0_interp_type"]) is int  # noqa: E721
+
+
+quokka = "RadiatingParticles_plt026"
+
+
+@requires_file(quokka)
+def test_quokka():
+    # Test QuokkaDataset instance type
+    ds = data_dir_load(quokka)
+    assert isinstance(ds, QuokkaDataset)
+        
+    # Test basic parameters 
+    assert ds.parameters["HydroMethod"] == "Quokka"
+    assert ds.parameters["quokka_version"] == 25.03
+    assert ds.parameters["dimensionality"] == 2
+    assert_equal(ds.parameters["fields"], [
+        'gasDensity',
+        'x-GasMomentum',
+        'y-GasMomentum',
+        'z-GasMomentum',
+        'gasEnergy',
+        'gasInternalEnergy',
+        'radEnergy-Group0',
+        'x-RadFlux-Group0',
+        'y-RadFlux-Group0',
+        'z-RadFlux-Group0'
+    ])
+        
+    # Check domain dimensions and boundaries
+    assert_equal(ds.domain_dimensions, [64, 64, 1])
+    assert_equal(ds.domain_left_edge, [0.0, 0.0, 0.0])
+    assert_equal(ds.domain_right_edge, [1.0, 1.0, 1.0])
+        
+    # Test derived fields and unit conversions
+    ad = ds.all_data()
+    assert ("gas", "density") in ds.derived_field_list
+    density = ad["gas", "density"]
+    assert str(density.units) == "g/cm**3"
+        
+    # Check momentum/velocity relationship which confirms velocity is correctly derived
+    momentum_x = ad["boxlib", "x-GasMomentum"]
+    gas_density = ad["boxlib", "gasDensity"]
+    velocity_x = ad["gas", "velocity_x"]
+    assert_allclose(momentum_x / gas_density, velocity_x)
+        
+    # Test radiation fields specifically
+    assert ds.parameters["radiation_field_groups"] == 1
+    assert "rad" in ds.fluid_types
+    assert ("rad", "energy_density_0") in ds.derived_field_list
+        
+    # Test particle IO with specific expectations
+    assert ds.parameters["particle_types"] == ("Rad_particles",)
+        
+    # Verify particle fields and units
+    particle_info = ds.parameters["particle_info"]["Rad_particles"]
+    assert particle_info["num_particles"] == 2
+    assert particle_info["num_fields"] == 3
+    assert set(particle_info["fields"]) == {"luminosity", "birth_time", "end_time"}
+    assert particle_info["units"]["luminosity"] == "M^1 L^2 T^-3"
+    assert particle_info["units"]["birth_time"] == "T^1"
+    assert particle_info["units"]["end_time"] == "T^1"
+        
