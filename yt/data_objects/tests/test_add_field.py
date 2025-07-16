921c512355ccc3ddbbd4c543a8dbba2ef7eab169
Corentin Cadiou <corentin.cadiou@cphyc.me>
2025-07-16 15:24:20 +0100

    Make sure warnings are issues

--- yt/data_objects/tests/test_add_field.py
+++ yt/data_objects/tests/test_add_field.py
@@ -1,3 +1,4 @@
+import logging
 from functools import partial
 
 import numpy as np
@@ -8,6 +9,7 @@ import yt
 from yt import derived_field
 from yt.fields import local_fields
 from yt.testing import fake_random_ds
+from yt.utilities.logger import ytLogger
 
 
 def test_add_field_lambda():
@@ -186,3 +188,100 @@ def test_add_field_quick():
         np.testing.assert_allclose(
             ds.r["gas", "one_over_density"], 1 / ds.r["gas", "density"]
         )
+
+
+def test_add_field_quick_syntax2():
+    fields = ("density", "temperature")
+    units = ("g/cm**3", "K")
+    ds = fake_random_ds(16, fields=fields, units=units)
+
+    # Left multiplication
+    ds.fields.gas.density_two = ds.fields.gas.density * 2
+    np.testing.assert_allclose(ds.r["gas", "density_two"], ds.r["gas", "density"] * 2)
+    ds.fields.gas.density_two2 = ds.fields.gas.density + ds.fields.gas.density
+    np.testing.assert_allclose(ds.r["gas", "density_two2"], ds.r["gas", "density"] * 2)
+
+    # Right multiplication
+    ds.fields.gas.two_density = 2 * ds.fields.gas.density
+    np.testing.assert_allclose(ds.r["gas", "two_density"], ds.r["gas", "density"] * 2)
+
+    # Left division
+    ds.fields.gas.half_density = ds.fields.gas.density / 2
+    np.testing.assert_allclose(ds.r["gas", "half_density"], ds.r["gas", "density"] / 2)
+    ds.fields.gas.density_over_temperature = (
+        ds.fields.gas.density / ds.fields.gas.temperature
+    )
+    np.testing.assert_allclose(
+        ds.r["gas", "density_over_temperature"],
+        ds.r["gas", "density"] / ds.r["gas", "temperature"],
+    )
+
+    # Right division
+    ds.fields.gas.one_over_density = 1 / ds.fields.gas.density
+    np.testing.assert_allclose(
+        ds.r["gas", "one_over_density"], 1 / ds.r["gas", "density"]
+    )
+
+    # Subtraction
+    ds.fields.gas.neg_density = -ds.fields.gas.density
+    np.testing.assert_allclose(ds.r["gas", "neg_density"], -ds.r["gas", "density"])
+    ds.fields.gas.density_minus_twice_density = (
+        ds.fields.gas.density - 2 * ds.fields.gas.density
+    )
+    np.testing.assert_allclose(
+        ds.r["gas", "density_minus_twice_density"],
+        -ds.r["gas", "density"],
+    )
+
+    # Complex expression
+    ds.fields.gas.kbT_per_V = (
+        ds.fields.gas.temperature * ds.units.kb / ds.fields.gas.volume
+    )
+    np.testing.assert_allclose(
+        ds.r["gas", "kbT_per_V"],
+        ds.r["gas", "temperature"] * ds.units.kb / ds.r["gas", "volume"],
+    )
+
+
+@pytest.fixture()
+def capturable_logger(caplog):
+    """
+    This set the minimal conditions to make pytest's caplog fixture usable.
+    """
+
+    propagate = ytLogger.propagate
+    ytLogger.propagate = True
+
+    with caplog.at_level(logging.WARNING, "yt"):
+        yield
+
+    ytLogger.propagate = propagate
+
+
+@pytest.mark.usefixtures("capturable_logger")
+def test_add_field_quick_syntax_warnings(caplog):
+    # Make sure we get a warning when overriding
+    # an existing field with a different function
+    ds = fake_random_ds(16)
+
+    warn_str = "Field ('gas', 'density2') already exists. To override use `force_override=True`"
+    # First time, no warning
+    ds.add_field(("gas", "density2"), ds.fields.gas.density * 2)
+    assert warn_str not in caplog.text
+
+    # Second time, warning
+    caplog.clear()
+    ds.add_field(("gas", "density2"), ds.fields.gas.density * 2)
+    assert warn_str in caplog.text
+
+    warn_str = "Field ('gas', 'density3') already exists. To override use `force_override=True`"
+    # Third time, new definition, no warning
+    caplog.clear()
+    ds.fields.gas.density3 = ds.fields.gas.density * 3
+    assert warn_str not in caplog.text
+    caplog.clear()
+
+    # Fourth time: new definition, warning!
+    caplog.clear()
+    ds.fields.gas.density3 = ds.fields.gas.density * 3
+    assert warn_str in caplog.text
