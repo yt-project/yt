80bec7a203e5a36709261a71fbd240624cfbe443
Matthew Turk <matthewturk@gmail.com>
2025-07-18 16:30:57 +0100

    Add mypy ignore override.

--- yt/fields/derived_field.py
+++ yt/fields/derived_field.py
@@ -118,7 +118,7 @@ class DerivedFieldBase(abc.ABC):
     # def __eq__(self, other) -> "DerivedFieldCombination":
     #     return DerivedFieldCombination([self, other], op=operator.eq)
 
-    def __ne__(self, other) -> "DerivedFieldCombination":
+    def __ne__(self, other) -> "DerivedFieldCombination":  # type: ignore[override]
         return DerivedFieldCombination([self, other], op=operator.ne)
 
 
