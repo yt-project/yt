6f5645ee142930e0e4188f8d29e463f66d516fff
Corentin Cadiou <corentin.cadiou@cphyc.me>
2025-08-27 11:19:26 +0200

    Evaluate truthness field combination
    This implements bool(field1 == field2), so that it returns True iff field1 is field2. For all other operators, it returns True (since the truthness of an object is True). It raises an error if we have more than two terms to the equality.

--- yt/fields/derived_field.py
+++ yt/fields/derived_field.py
@@ -150,7 +150,7 @@ class DerivedFieldCombination(DerivedFieldBase):
         self.terms = terms
         self.op = op
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash((self.sampling_type, tuple(self.terms), self.op))
 
     def __call__(self, field, data):
@@ -188,6 +188,19 @@ class DerivedFieldCombination(DerivedFieldBase):
     @property
     def name(self):
         return f"{self!r}"
+
+    def __bool__(self):
+        match self.op:
+            # Special case for equality, check terms are aliases of each other
+            case operator.eq if len(self.terms) == 2:
+                return self.terms[0] is self.terms[1]
+            case operator.eq:
+                raise ValueError(
+                    "The truth value of a DerivedFieldCombination with more "
+                    "than two terms is ambiguous."
+                )
+            case _:
+                return True
 
 
 class DerivedField(DerivedFieldBase):
@@ -314,7 +327,7 @@ class DerivedField(DerivedFieldBase):
             self._shared_aliases_list = alias._shared_aliases_list
             self._shared_aliases_list.append(self)
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(
             (
                 self.name,
