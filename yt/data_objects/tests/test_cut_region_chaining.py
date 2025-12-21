049b37d5e81c629e2be500c814278fa5919ddcf5
maverickcodex18 <chitranshagrawal1804@gmail.com>
2025-12-21 17:06:32 +0530

    style: apply ruff format fixes

--- yt/data_objects/tests/test_cut_region_chaining.py
+++ yt/data_objects/tests/test_cut_region_chaining.py
@@ -3,13 +3,16 @@ from numpy.testing import assert_equal
 
 from yt.testing import fake_random_ds
 
+
 def test_chained_cut_region_with_locals():
     ds = fake_random_ds(16, fields=("density",), units=("g/cm**3",))
     ad = ds.all_data()
     cut_1 = ad.cut_region("obj['density'] > density_min", locals={"density_min": 0.2})
 
     # This chained cut region should inherit 'density_min' from cut_1's locals
-    cut_2 = cut_1.cut_region("obj['density'] < density_max", locals={"density_max": 0.8})
+    cut_2 = cut_1.cut_region(
+        "obj['density'] < density_max", locals={"density_max": 0.8}
+    )
 
     # Verify locals are merged
     assert "density_min" in cut_2.locals
